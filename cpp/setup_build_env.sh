252afcc86e7e87341dc6f9663eacc5bb3bb2da30
Micah Kornfield <emkornfield@gmail.com>
2016-03-16 07:14:14 +0000

    some fixes for errors encountered on not fully setup systems

--- cpp/setup_build_env.sh
+++ cpp/setup_build_env.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 SOURCE_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+check_run() {
+rc=eval $1
+echo $rc
+if [[ $rc ]]; then echo $1 failed with exit code $rc!; kill -INT $$; fi
+}
 
-./thirdparty/download_thirdparty.sh
-./thirdparty/build_thirdparty.sh
+check_run ./thirdparty/download_thirdparty.sh
+check_run ./thirdparty/build_thirdparty.sh
 source thirdparty/versions.sh
 
 export GTEST_HOME=$SOURCE_DIR/thirdparty/$GTEST_BASEDIR
