ea3101b8f113202043dded7b4b02469ddf4b2ad6
hzhang2 <zhanghuan929@163.com>
2016-05-15 09:27:18 +0800

    remove CMAKE_SKIP_INSTALL_ALL_DEPENDENCY for target install and fix typo

--- cpp/setup_build_env.sh
+++ cpp/setup_build_env.sh
@@ -4,6 +4,6 @@ SOURCE_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 ./thirdparty/download_thirdparty.sh || { echo "download_thirdparty.sh failed" ; return; }
 ./thirdparty/build_thirdparty.sh || { echo "build_thirdparty.sh failed" ; return; }
-source ./thirdparty/set_thirdparty_env.sh || { echo "build_thirdparty.sh failed" ; return; }
+source ./thirdparty/set_thirdparty_env.sh || { echo "source set_thirdparty_env.sh failed" ; return; }
 
 echo "Build env initialized"
