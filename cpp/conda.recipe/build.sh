58955e56d3799b43b922afeccca241b0ebfeb1d5
Wes McKinney <wesm@apache.org>
2016-05-17 20:33:23 -0700

    Draft of automated conda builds for libarrow, pyarrow. Remove unneeded thrift-cpp build dependency

--- cpp/conda.recipe/build.sh
+++ cpp/conda.recipe/build.sh
@@ -9,6 +9,19 @@ cd $RECIPE_DIR
 export FLATBUFFERS_HOME=$PREFIX
 export PARQUET_HOME=$PREFIX
 
+if [ "$(uname)" == "Darwin" ]; then
+  # C++11 finagling for Mac OSX
+  export CC=clang
+  export CXX=clang++
+  export MACOSX_VERSION_MIN="10.7"
+  CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+  export LINKFLAGS="${LDFLAGS}"
+  export MACOSX_DEPLOYMENT_TARGET=10.7
+fi
+
 cd ..
 
 rm -rf conda-build
@@ -33,7 +46,7 @@ elif [ `uname` == Darwin ]; then
 fi
 
 cmake \
-    -DCMAKE_BUILD_TYPE=debug \
+    -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_SHARED_LINKER_FLAGS=$SHARED_LINKER_FLAGS \
     -DARROW_IPC=on \
