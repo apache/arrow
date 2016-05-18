afd0582832af85ffc1b4e8e669274138f9239e76
Wes McKinney <wesm@apache.org>
2016-05-17 21:17:34 -0700

    Change encrypted token to apache/arrow, only upload on commits to master

--- ci/travis_conda_build.sh
+++ ci/travis_conda_build.sh
@@ -30,21 +30,23 @@ cd $TRAVIS_BUILD_DIR/cpp
 
 conda build conda.recipe --channel apache/channel/dev
 CONDA_PACKAGE=`conda build --output conda.recipe | grep bz2`
-anaconda --token $ANACONDA_TOKEN upload $CONDA_PACKAGE --user apache --channel dev;
 
-# if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
-#   anaconda --token $ANACONDA_TOKEN upload $CONDA_PACKAGE --user apache --channel dev;
-# fi
+if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+  anaconda --token $ANACONDA_TOKEN upload $CONDA_PACKAGE --user apache --channel dev;
+fi
 
 # Build pyarrow
 
 cd $TRAVIS_BUILD_DIR/python
 
 build_for_python_version() {
-    PY_VERSION=$1
-    conda build conda.recipe --python $PY_VERSION --channel apache/channel/dev
-    CONDA_PACKAGE=`conda build --python $PY_VERSION --output conda.recipe | grep bz2`
-    anaconda --token $ANACONDA_TOKEN upload $CONDA_PACKAGE --user apache --channel dev
+  PY_VERSION=$1
+  conda build conda.recipe --python $PY_VERSION --channel apache/channel/dev
+  CONDA_PACKAGE=`conda build --python $PY_VERSION --output conda.recipe | grep bz2`
+
+  if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+	anaconda --token $ANACONDA_TOKEN upload $CONDA_PACKAGE --user apache --channel dev;
+  fi
 }
 
 build_for_python_version 2.7
