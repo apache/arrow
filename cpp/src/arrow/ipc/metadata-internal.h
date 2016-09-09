60ee5c0aa816e19c03bb44114f66832b94c4403c
Wes McKinney <wes.mckinney@twosigma.com>
2016-09-08 23:04:35 -0400

    Rename current version to V1_SNAPSHOT to reflect changing nature
    Change-Id: I254bbd506bf4683a4b5ecd2de4e33db10acc2376

--- cpp/src/arrow/ipc/metadata-internal.h
+++ cpp/src/arrow/ipc/metadata-internal.h
@@ -37,7 +37,8 @@ class Status;
 
 namespace ipc {
 
-static constexpr flatbuf::MetadataVersion kMetadataVersion = flatbuf::MetadataVersion_V1;
+static constexpr flatbuf::MetadataVersion kMetadataVersion =
+  flatbuf::MetadataVersion_V1_SNAPSHOT;
 
 Status FieldFromFlatbuffer(const flatbuf::Field* field, std::shared_ptr<Field>* out);
 
