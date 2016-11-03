cc7a6b35d6420535f5a8877ad2d5bf47f8a4a0b6
Uwe L. Korn <uwelk@xhochy.com>
2016-11-03 18:32:45 +0100

    ARROW-362: Remove redunant reference count
    Change-Id: I7c9b6af682d569cf2a7c335b1da4fed39632eae9

--- python/src/pyarrow/adapters/pandas.cc
+++ python/src/pyarrow/adapters/pandas.cc
@@ -628,7 +628,6 @@ class ArrowDeserializer {
     PyAcquireGIL lock;
 
     // Zero-Copy. We can pass the data pointer directly to NumPy.
-    Py_INCREF(py_ref_);
     OwnedRef py_ref(py_ref_);
     npy_intp dims[1] = {col_->length()};
     out_ = reinterpret_cast<PyArrayObject*>(PyArray_SimpleNewFromData(1, dims,
