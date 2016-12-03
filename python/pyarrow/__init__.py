aa9bd4934492b6ad8be3aa619dc1211851b8721b
Uwe L. Korn <uwelk@xhochy.com>
2016-12-03 10:38:30 +0100

    ARROW-379: Use setuptools_scm for Python versioning
    Change-Id: I5e2c8e332b689fd75b6d1880f9b152f08b0655eb

--- python/pyarrow/__init__.py
+++ python/pyarrow/__init__.py
@@ -17,6 +17,14 @@
 
 # flake8: noqa
 
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+   # package is not installed
+   pass
+
+
 import pyarrow.config
 
 from pyarrow.array import (Array,
