0ac01a5bf6747a5855d20632c9c7874483b9830a
Uwe L. Korn <uwelk@xhochy.com>
2016-12-04 10:50:43 -0500

    ARROW-379: Use setuptools_scm for Python versioning
    Author: Uwe L. Korn <uwelk@xhochy.com>
    
    Closes #224 from xhochy/ARROW-379 and squashes the following commits:
    
    3a68d9f [Uwe L. Korn] Remove deprecated version import
    15fe9b2 [Uwe L. Korn] Add license header
    aa9bd49 [Uwe L. Korn] ARROW-379: Use setuptools_scm for Python versioning

--- python/pyarrow/__init__.py
+++ python/pyarrow/__init__.py
@@ -17,6 +17,14 @@
 
 # flake8: noqa
 
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+   # package is not installed
+   pass
+
+
 import pyarrow.config
 
 from pyarrow.array import (Array,
@@ -43,5 +51,3 @@ from pyarrow.schema import (null, bool_,
 
 from pyarrow.table import (Column, RecordBatch, dataframe_from_batches, Table,
                            from_pandas_dataframe)
-
-from pyarrow.version import version as __version__
