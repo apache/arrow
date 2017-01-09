75a11cff359c64153efb8813923b145e1289e99d
Wes McKinney <wes.mckinney@twosigma.com>
2017-01-09 15:44:09 -0500

    Add more metadata accessor scaffolding, to be tested
    Change-Id: Iad25cf265ffb15440b01e40717289a6cfba28dd5

--- python/pyarrow/_parquet.pyx
+++ python/pyarrow/_parquet.pyx
@@ -19,13 +19,15 @@
 # distutils: language = c++
 # cython: embedsignature = True
 
+from pyarrow._parquet cimport *
+
 from pyarrow.includes.libarrow cimport *
-from pyarrow.includes.parquet cimport *
-from pyarrow.includes.libarrow_io cimport ReadableFileInterface, OutputStream, FileOutputStream
+from pyarrow.includes.libarrow_io cimport (ReadableFileInterface, OutputStream,
+                                           FileOutputStream)
 cimport pyarrow.includes.pyarrow as pyarrow
 
 from pyarrow.array cimport Array
-from pyarrow.compat import tobytes
+from pyarrow.compat import tobytes, frombytes
 from pyarrow.error import ArrowException
 from pyarrow.error cimport check_status
 from pyarrow.io import NativeFile
@@ -35,24 +37,264 @@ from pyarrow.io cimport NativeFile, get_reader, get_writer
 
 import six
 
-__all__ = [
-    'read_table',
-    'write_table'
-]
+
+# cdef class ParquetRowGroupMetadata:
+#     cdef:
+#         unique_ptr[CRowGroupMetadata] metadata
+#         CRowGroupMetadata metadata
+#         object parent
+
+
+cdef class ParquetFileMetadata:
+    cdef:
+        shared_ptr[CFileMetaData] sp_metadata
+        CFileMetaData* metadata
+        object _schema
+
+    def __cinit__(self):
+        pass
+
+    cdef init(self, const shared_ptr[CFileMetaData]& metadata):
+        self.sp_metadata = metadata
+        self.metadata = metadata.get()
+
+    def __repr__(self):
+        return """{0}
+  created_by: {1}
+  num_columns: {2}
+  num_rows: {3}
+  num_row_groups: {4}
+  format_version: {5}
+  serialized_size: {6}""".format(object.__repr__(self),
+                                 self.created_by, self.num_columns,
+                                 self.num_rows, self.num_row_groups,
+                                 self.format_version,
+                                 self.serialized_size)
+
+    @property
+    def schema(self):
+        if self._schema is not None:
+            return self._schema
+
+        cdef ParquetSchema schema = ParquetSchema()
+        schema.init_from_filemeta(self)
+        self._schema = schema
+        return schema
+
+    property serialized_size:
+
+        def __get__(self):
+            return self.metadata.size()
+
+    property num_columns:
+
+        def __get__(self):
+            return self.metadata.num_columns()
+
+    property num_rows:
+
+        def __get__(self):
+            return self.metadata.num_rows()
+
+    property num_row_groups:
+
+        def __get__(self):
+            return self.metadata.num_row_groups()
+
+    property format_version:
+
+        def __get__(self):
+            if self.metadata.version() == 2:
+                return '2.0'
+            else:
+                return '1.0'
+
+    property created_by:
+
+        def __get__(self):
+            return self.metadata.created_by()
+
+    def row_group(self, int i):
+        raise NotImplementedError
+
+
+
+cdef class ParquetSchema:
+    cdef:
+        object parent  # the FileMetaData owning the SchemaDescriptor
+        const SchemaDescriptor* schema
+
+    def __cinit__(self):
+        self.parent = None
+        self.schema = NULL
+
+    def __repr__(self):
+        cdef const ColumnDescriptor* descr
+        elements = []
+        for i in range(self.schema.num_columns()):
+            col = self.column(i)
+            logical_type = col.logical_type
+            formatted = '{0}: {1}'.format(col.path, col.physical_type)
+            if logical_type != 'NONE':
+                formatted += ' {0}'.format(logical_type)
+            elements.append(formatted)
+
+        return """{0}
+{1}
+ """.format(object.__repr__(self), '\n'.join(elements))
+
+    cdef init_from_filemeta(self, ParquetFileMetadata container):
+        self.parent = container
+        self.schema = container.metadata.schema()
+
+    def __len__(self):
+        return self.schema.num_columns()
+
+    def __getitem__(self, i):
+        return self.column(i)
+
+    def column(self, i):
+        if i < 0 or i >= len(self):
+            raise IndexError('{0} out of bounds'.format(i))
+
+        cdef ParquetColumnSchema col = ParquetColumnSchema()
+        col.init_from_schema(self, i)
+        return col
+
+
+cdef class ParquetColumnSchema:
+    cdef:
+        object parent
+        const ColumnDescriptor* descr
+
+    def __cinit__(self):
+        self.descr = NULL
+
+    cdef init_from_schema(self, ParquetSchema schema, int i):
+        self.parent = schema
+        self.descr = schema.schema.Column(i)
+
+    def __repr__(self):
+        physical_type = self.physical_type
+        logical_type = self.logical_type
+        if logical_type == 'DECIMAL':
+            logical_type = 'DECIMAL({0}, {1})'.format(self.precision,
+                                                      self.scale)
+        elif physical_type == 'FIXED_LEN_BYTE_ARRAY':
+            logical_type = ('FIXED_LEN_BYTE_ARRAY(length={0})'
+                            .format(self.length))
+
+        return """<ParquetColumnSchema>
+  name: {0}
+  path: {1}
+  max_definition_level: {2}
+  max_repetition_level: {3}
+  physical_type: {4}
+  logical_type: {5}""".format(self.name, self.path, self.max_definition_level,
+                       self.max_repetition_level, physical_type,
+                       logical_type)
+
+    property name:
+
+        def __get__(self):
+            return frombytes(self.descr.name())
+
+    property path:
+
+        def __get__(self):
+            return frombytes(self.descr.path().get().ToDotString())
+
+    property max_definition_level:
+
+        def __get__(self):
+            return self.descr.max_definition_level()
+
+    property max_repetition_level:
+
+        def __get__(self):
+            return self.descr.max_repetition_level()
+
+    property physical_type:
+
+        def __get__(self):
+            return physical_type_name_from_enum(self.descr.physical_type())
+
+    property logical_type:
+
+        def __get__(self):
+            return logical_type_name_from_enum(self.descr.logical_type())
+
+    # FIXED_LEN_BYTE_ARRAY attribute
+    property length:
+
+        def __get__(self):
+            return self.descr.type_length()
+
+    # Decimal attributes
+    property precision:
+
+        def __get__(self):
+            return self.descr.type_precision()
+
+    property scale:
+
+        def __get__(self):
+            return self.descr.type_scale()
+
+
+cdef physical_type_name_from_enum(ParquetType type_):
+    return {
+        ParquetType_BOOLEAN: 'BOOLEAN',
+        ParquetType_INT32: 'INT32',
+        ParquetType_INT64: 'INT64',
+        ParquetType_INT96: 'INT96',
+        ParquetType_FLOAT: 'FLOAT',
+        ParquetType_DOUBLE: 'DOUBLE',
+        ParquetType_BYTE_ARRAY: 'BYTE_ARRAY',
+        ParquetType_FIXED_LEN_BYTE_ARRAY: 'FIXED_LEN_BYTE_ARRAY',
+    }.get(type_, 'UNKNOWN')
+
+
+cdef logical_type_name_from_enum(ParquetLogicalType type_):
+    return {
+        ParquetLogicalType_NONE: 'NONE',
+        ParquetLogicalType_UTF8: 'UTF8',
+        ParquetLogicalType_MAP: 'MAP',
+        ParquetLogicalType_MAP_KEY_VALUE: 'MAP_KEY_VALUE',
+        ParquetLogicalType_LIST: 'LIST',
+        ParquetLogicalType_ENUM: 'ENUM',
+        ParquetLogicalType_DECIMAL: 'DECIMAL',
+        ParquetLogicalType_DATE: 'DATE',
+        ParquetLogicalType_TIME_MILLIS: 'TIME_MILLIS',
+        ParquetLogicalType_TIME_MICROS: 'TIME_MICROS',
+        ParquetLogicalType_TIMESTAMP_MILLIS: 'TIMESTAMP_MILLIS',
+        ParquetLogicalType_TIMESTAMP_MICROS: 'TIMESTAMP_MICROS',
+        ParquetLogicalType_UINT_8: 'UINT_8',
+        ParquetLogicalType_UINT_16: 'UINT_16',
+        ParquetLogicalType_UINT_32: 'UINT_32',
+        ParquetLogicalType_UINT_64: 'UINT_64',
+        ParquetLogicalType_INT_8: 'INT_8',
+        ParquetLogicalType_INT_16: 'INT_16',
+        ParquetLogicalType_INT_32: 'INT_32',
+        ParquetLogicalType_INT_64: 'UINT_64',
+        ParquetLogicalType_JSON: 'JSON',
+        ParquetLogicalType_BSON: 'BSON',
+        ParquetLogicalType_INTERVAL: 'INTERVAL',
+    }.get(type_, 'UNKNOWN')
+
 
 cdef class ParquetReader:
     cdef:
         MemoryPool* allocator
         unique_ptr[FileReader] reader
         column_idx_map
+        ParquetFileMetadata _metadata
 
     def __cinit__(self):
         self.allocator = default_memory_pool()
+        self._metadata = None
 
-    def open(self, source):
-        self._open(source)
-
-    cdef _open(self, object source):
+    def open(self, object source):
         cdef:
             shared_ptr[ReadableFileInterface] rd_handle
             c_string path
@@ -70,6 +312,20 @@ cdef class ParquetReader:
         else:
             get_reader(source, &rd_handle)
             check_status(OpenFile(rd_handle, self.allocator, &self.reader))
+
+    @property
+    def metadata(self):
+        cdef:
+            shared_ptr[CFileMetaData] metadata
+            ParquetFileMetadata result
+        if self._metadata is not None:
+            return self._metadata
+
+        metadata = self.reader.get().parquet_reader().metadata()
+
+        self._metadata = result = ParquetFileMetadata()
+        result.init(metadata)
+        return result
 
     def read_all(self):
         cdef:
@@ -98,8 +354,8 @@ cdef class ParquetReader:
             Integer index of the position of the column
         """
         cdef:
-            const FileMetaData* metadata = (self.reader.get().parquet_reader()
-                                            .metadata().get())
+            ParquetFileMetadata container = self.metadata
+            const CFileMetaData* metadata = container.metadata
             int i = 0
 
         if self.column_idx_map is None:
@@ -124,121 +380,98 @@ cdef class ParquetReader:
         return array
 
 
-def read_table(source, columns=None):
-    """
-    Read a Table from Parquet format
+cdef check_compression_name(name):
+    if name.upper() not in ['NONE', 'SNAPPY', 'GZIP', 'LZO', 'BROTLI']:
+        raise ArrowException("Unsupported compression: " + name)
 
-    Parameters
-    ----------
-    source: str or pyarrow.io.NativeFile
-        Readable source. For passing Python file objects or byte buffers, see
-        pyarrow.io.PythonFileInterface or pyarrow.io.BytesReader.
-    columns: list
-        If not None, only these columns will be read from the file.
 
-    Returns
-    -------
-    pyarrow.table.Table
-        Content of the file as a table (of columns)
-    """
-    cdef ParquetReader reader = ParquetReader()
-    reader._open(source)
-
-    if columns is None:
-        return reader.read_all()
+cdef ParquetCompression compression_from_name(object name):
+    name = name.upper()
+    if name == "SNAPPY":
+        return ParquetCompression_SNAPPY
+    elif name == "GZIP":
+        return ParquetCompression_GZIP
+    elif name == "LZO":
+        return ParquetCompression_LZO
+    elif name == "BROTLI":
+        return ParquetCompression_BROTLI
     else:
-        column_idxs = [reader.column_name_idx(column) for column in columns]
-        arrays = [reader.read_column(column_idx) for column_idx in column_idxs]
-        return Table.from_arrays(columns, arrays)
+        return ParquetCompression_UNCOMPRESSED
 
 
-def write_table(table, sink, chunk_size=None, version=None,
-                use_dictionary=True, compression=None):
-    """
-    Write a Table to Parquet format
+cdef class ParquetWriter:
+    cdef:
+        shared_ptr[WriterProperties] properties
+        shared_ptr[OutputStream] sink
 
-    Parameters
-    ----------
-    table : pyarrow.Table
-    sink: string or pyarrow.io.NativeFile
-    chunk_size : int
-        The maximum number of rows in each Parquet RowGroup. As a default,
-        we will write a single RowGroup per file.
-    version : {"1.0", "2.0"}, default "1.0"
-        The Parquet format version, defaults to 1.0
-    use_dictionary : bool or list
-        Specify if we should use dictionary encoding in general or only for
-        some columns.
-    compression : str or dict
-        Specify the compression codec, either on a general basis or per-column.
-    """
-    cdef Table table_ = table
-    cdef CTable* ctable_ = table_.table
-    cdef shared_ptr[FileOutputStream] filesink_
-    cdef shared_ptr[OutputStream] sink_
+    cdef readonly:
+        object use_dictionary
+        object compression
+        object version
+        int row_group_size
 
-    cdef WriterProperties.Builder properties_builder
-    cdef int64_t chunk_size_ = 0
-    if chunk_size is None:
-        chunk_size_ = ctable_.num_rows()
-    else:
-        chunk_size_ = chunk_size
+    def __cinit__(self, where, use_dictionary=None, compression=None,
+                  version=None):
+        cdef shared_ptr[FileOutputStream] filestream
 
-    if version is not None:
-        if version == "1.0":
-            properties_builder.version(PARQUET_1_0)
-        elif version == "2.0":
-            properties_builder.version(PARQUET_2_0)
+        if isinstance(where, six.string_types):
+            check_status(FileOutputStream.Open(tobytes(where), &filestream))
+            self.sink = <shared_ptr[OutputStream]> filestream
         else:
-            raise ArrowException("Unsupported Parquet format version")
+            get_writer(where, &self.sink)
 
-    if isinstance(use_dictionary, bool):
-        if use_dictionary:
-            properties_builder.enable_dictionary()
-        else:
-            properties_builder.disable_dictionary()
-    else:
-        # Deactivate dictionary encoding by default
-        properties_builder.disable_dictionary()
-        for column in use_dictionary:
-            properties_builder.enable_dictionary(column)
+        self.use_dictionary = use_dictionary
+        self.compression = compression
+        self.version = version
+        self._setup_properties()
 
-    if isinstance(compression, basestring):
-        if compression == "NONE":
-            properties_builder.compression(UNCOMPRESSED)
-        elif compression == "SNAPPY":
-            properties_builder.compression(SNAPPY)
-        elif compression == "GZIP":
-            properties_builder.compression(GZIP)
-        elif compression == "LZO":
-            properties_builder.compression(LZO)
-        elif compression == "BROTLI":
-            properties_builder.compression(BROTLI)
-        else:
-            raise ArrowException("Unsupport compression codec")
-    elif compression is not None:
-        # Deactivate dictionary encoding by default
-        properties_builder.disable_dictionary()
-        for column, codec in compression.iteritems():
-            if codec == "NONE":
-                properties_builder.compression(column, UNCOMPRESSED)
-            elif codec == "SNAPPY":
-                properties_builder.compression(column, SNAPPY)
-            elif codec == "GZIP":
-                properties_builder.compression(column, GZIP)
-            elif codec == "LZO":
-                properties_builder.compression(column, LZO)
-            elif codec == "BROTLI":
-                properties_builder.compression(column, BROTLI)
+    cdef _setup_properties(self):
+        cdef WriterProperties.Builder properties_builder
+        self._set_version(&properties_builder)
+        self._set_compression_props(&properties_builder)
+        self._set_dictionary_props(&properties_builder)
+        self.properties = properties_builder.build()
+
+    cdef _set_version(self, WriterProperties.Builder* props):
+        if self.version is not None:
+            if self.version == "1.0":
+                props.version(ParquetVersion_V1)
+            elif self.version == "2.0":
+                props.version(ParquetVersion_V2)
             else:
-                raise ArrowException("Unsupport compression codec")
+                raise ArrowException("Unsupported Parquet format version")
 
-    if isinstance(sink, six.string_types):
-        check_status(FileOutputStream.Open(tobytes(sink), &filesink_))
-        sink_ = <shared_ptr[OutputStream]>filesink_
-    else:
-        get_writer(sink, &sink_)
+    cdef _set_compression_props(self, WriterProperties.Builder* props):
+        if isinstance(self.compression, basestring):
+            check_compression_name(self.compression)
+            props.compression(compression_from_name(self.compression))
+        elif self.compression is not None:
+            # Deactivate dictionary encoding by default
+            props.disable_dictionary()
+            for column, codec in self.compression.iteritems():
+                check_compression_name(codec)
+                props.compression(column, compression_from_name(codec))
 
-    with nogil:
-        check_status(WriteFlatTable(ctable_, default_memory_pool(), sink_,
-                                    chunk_size_, properties_builder.build()))
+    cdef _set_dictionary_props(self, WriterProperties.Builder* props):
+        if isinstance(self.use_dictionary, bool):
+            if self.use_dictionary:
+                props.enable_dictionary()
+            else:
+                props.disable_dictionary()
+        else:
+            # Deactivate dictionary encoding by default
+            props.disable_dictionary()
+            for column in self.use_dictionary:
+                props.enable_dictionary(column)
+
+    def write_table(self, Table table, row_group_size=None):
+        cdef CTable* ctable = table.table
+
+        if row_group_size is None:
+            row_group_size = ctable.num_rows()
+
+        cdef int c_row_group_size = row_group_size
+        with nogil:
+            check_status(WriteFlatTable(ctable, default_memory_pool(),
+                                        self.sink, c_row_group_size,
+                                        self.properties))
--- python/pyarrow/parquet.py
+++ python/pyarrow/parquet.py
@@ -15,4 +15,102 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from ._parquet import read_table, write_table, ParquetReader as _ParquetReader
+import pyarrow._parquet as _parquet
+from pyarrow.table import Table
+
+
+class ParquetFile(object):
+    """
+    Open a Parquet binary file for reading
+
+    Parameters
+    ----------
+    source : str or pyarrow.io.NativeFile
+        Readable source. For passing Python file objects or byte buffers,
+        see pyarrow.io.PythonFileInterface or pyarrow.io.BytesReader.
+    metadata : ParquetFileMetadata, default None
+        Use existing metadata object, rather than reading from file.
+    """
+    def __init__(self, source, metadata=None):
+        self.reader = _parquet.ParquetReader()
+        self.reader.open(source)
+
+    @property
+    def metadata(self):
+        return self.reader.metadata
+
+    @property
+    def schema(self):
+        return self.metadata.schema
+
+    def read(self, nrows=None, columns=None):
+        """
+        Read a Table from Parquet format
+
+        Parameters
+        ----------
+        columns: list
+            If not None, only these columns will be read from the file.
+
+        Returns
+        -------
+        pyarrow.table.Table
+            Content of the file as a table (of columns)
+        """
+        if nrows is not None:
+            raise NotImplementedError("nrows argument")
+
+        if columns is None:
+            return self.reader.read_all()
+        else:
+            column_idxs = [self.reader.column_name_idx(column)
+                           for column in columns]
+            arrays = [self.reader.read_column(column_idx)
+                      for column_idx in column_idxs]
+            return Table.from_arrays(columns, arrays)
+
+
+def read_table(source, columns=None):
+    """
+    Read a Table from Parquet format
+
+    Parameters
+    ----------
+    source: str or pyarrow.io.NativeFile
+        Readable source. For passing Python file objects or byte buffers, see
+        pyarrow.io.PythonFileInterface or pyarrow.io.BytesReader.
+    columns: list
+        If not None, only these columns will be read from the file.
+
+    Returns
+    -------
+    pyarrow.table.Table
+        Content of the file as a table (of columns)
+    """
+    return ParquetFile(source).read(columns=columns)
+
+
+def write_table(table, sink, chunk_size=None, version=None,
+                use_dictionary=True, compression=None):
+    """
+    Write a Table to Parquet format
+
+    Parameters
+    ----------
+    table : pyarrow.Table
+    sink: string or pyarrow.io.NativeFile
+    chunk_size : int
+        The maximum number of rows in each Parquet RowGroup. As a default,
+        we will write a single RowGroup per file.
+    version : {"1.0", "2.0"}, default "1.0"
+        The Parquet format version, defaults to 1.0
+    use_dictionary : bool or list
+        Specify if we should use dictionary encoding in general or only for
+        some columns.
+    compression : str or dict
+        Specify the compression codec, either on a general basis or per-column.
+    """
+    writer = _parquet.ParquetWriter(sink, use_dictionary=use_dictionary,
+                                    compression=compression,
+                                    version=version)
+    writer.write_table(table, row_group_size=chunk_size)
