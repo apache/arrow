a68af9d168e381d1730ae0cb4dc653bef42562d3
Nong Li <nongli@gmail.com>
2017-01-25 14:49:00 -0500

    ARROW-498 [C++] Add command line utilities that convert between stream and file.
    These are in the style of unix utilities using stdin/stdout for argument passing.
    This makes it easy to chain them together and I think are using for getting started
    or testing. As an example, this command line tests a round trip:
     $ build/debug/file-to-stream /tmp/arrow-file | build/debug/stream-to-file > /tmp/copy
     $ diff /tmp/arrow-file /tmp/copy
    
    If we had the same in java, this would make it pretty convenient for integration
    testing.
    
    Author: Nong Li <nongli@gmail.com>
    
    Closes #302 from nongli/utils and squashes the following commits:
    
    b970c75 [Nong Li] fix long -> int64_t
    a01ef4d [Nong Li] Fix style issues.
    da3d98d [Nong Li] ARROW-498 [C++] Add commandline utilities that convert between stream and file.

---
+++ cpp/src/arrow/util/io-util.h
@@ -0,0 +1,93 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#ifndef ARROW_UTIL_IO_UTIL_H
+#define ARROW_UTIL_IO_UTIL_H
+
+#include <iostream>
+#include "arrow/buffer.h"
+
+namespace arrow {
+namespace io {
+
+// Output stream that just writes to stdout.
+class StdoutStream : public OutputStream {
+ public:
+  StdoutStream() : pos_(0) {
+    set_mode(FileMode::WRITE);
+  }
+  virtual ~StdoutStream() {}
+
+  Status Close() { return Status::OK(); }
+  Status Tell(int64_t* position)  {
+    *position = pos_;
+    return Status::OK();
+  }
+
+  Status Write(const uint8_t* data, int64_t nbytes) {
+    pos_ += nbytes;
+    std::cout.write(reinterpret_cast<const char*>(data), nbytes);
+    return Status::OK();
+  }
+ private:
+  int64_t pos_;
+};
+
+// Input stream that just reads from stdin.
+class StdinStream : public InputStream {
+ public:
+  StdinStream() : pos_(0) {
+    set_mode(FileMode::READ);
+  }
+  virtual ~StdinStream() {}
+
+  Status Close() { return Status::OK(); }
+  Status Tell(int64_t* position)  {
+    *position = pos_;
+    return Status::OK();
+  }
+
+  virtual Status Read(int64_t nbytes, int64_t* bytes_read, uint8_t* out) {
+    std::cin.read(reinterpret_cast<char*>(out), nbytes);
+    if (std::cin) {
+      *bytes_read = nbytes;
+      pos_ += nbytes;
+    } else {
+      *bytes_read = 0;
+    }
+    return Status::OK();
+  }
+
+  virtual Status Read(int64_t nbytes, std::shared_ptr<Buffer>* out) {
+    auto buffer = std::make_shared<PoolBuffer>(nullptr);
+    RETURN_NOT_OK(buffer->Resize(nbytes));
+    int64_t bytes_read;
+    RETURN_NOT_OK(Read(nbytes, &bytes_read, buffer->mutable_data()));
+    RETURN_NOT_OK(buffer->Resize(bytes_read, false));
+    *out = buffer;
+    return Status::OK();
+  }
+
+ private:
+  int64_t pos_;
+};
+
+} // namespace io
+} // namespace arrow
+
+#endif // ARROW_UTIL_IO_UTIL_H
+
---
+++ cpp/src/arrow/util/stream-to-file.cc
@@ -0,0 +1,58 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include <iostream>
+#include "arrow/io/file.h"
+#include "arrow/ipc/file.h"
+#include "arrow/ipc/stream.h"
+#include "arrow/status.h"
+
+#include "arrow/util/io-util.h"
+
+namespace arrow {
+
+// Converts a stream from stdin to a file written to standard out.
+// A typical usage would be:
+// $ <program that produces streaming output> | stream-to-file > file.arrow
+Status ConvertToFile() {
+  std::shared_ptr<io::InputStream> input(new io::StdinStream);
+  std::shared_ptr<ipc::StreamReader> reader;
+  RETURN_NOT_OK(ipc::StreamReader::Open(input, &reader));
+
+  io::StdoutStream sink;
+  std::shared_ptr<ipc::FileWriter> writer;
+  RETURN_NOT_OK(ipc::FileWriter::Open(&sink, reader->schema(), &writer));
+
+  std::shared_ptr<RecordBatch> batch;
+  while (true) {
+    RETURN_NOT_OK(reader->GetNextRecordBatch(&batch));
+    if (batch == nullptr) break;
+    RETURN_NOT_OK(writer->WriteRecordBatch(*batch));
+  }
+  return writer->Close();
+}
+
+} // namespace arrow
+
+int main(int argc, char** argv) {
+  arrow::Status status = arrow::ConvertToFile();
+  if (!status.ok()) {
+    std::cerr << "Could not convert to file: " << status.ToString() << std::endl;
+    return 1;
+  }
+  return 0;
+}
