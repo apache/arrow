577403271d4dcfce081be1b18860fc2e4a9a97f9
Steven Phillips <steven@dremio.com>
2017-02-02 18:53:54 -0800

    Fix getField() for NullableMapVector

--- java/vector/src/main/java/org/apache/arrow/vector/complex/NullableMapVector.java
+++ java/vector/src/main/java/org/apache/arrow/vector/complex/NullableMapVector.java
@@ -36,6 +36,7 @@ import org.apache.arrow.vector.complex.reader.FieldReader;
 import org.apache.arrow.vector.holders.ComplexHolder;
 import org.apache.arrow.vector.schema.ArrowFieldNode;
 import org.apache.arrow.vector.types.pojo.DictionaryEncoding;
+import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.util.CallBack;
 import org.apache.arrow.vector.util.TransferPair;
 
@@ -67,6 +68,12 @@ public class NullableMapVector extends MapVector implements FieldVector {
     this.dictionary = dictionary;
     this.accessor = new Accessor();
     this.mutator = new Mutator();
+  }
+
+  @Override
+  public Field getField() {
+    Field f = super.getField();
+    return new Field(f.getName(), true, f.getType(), f.getChildren());
   }
 
   @Override
@@ -246,12 +253,15 @@ public class NullableMapVector extends MapVector implements FieldVector {
 
   public final class Mutator extends MapVector.Mutator implements NullableVectorDefinitionSetter {
 
+    int lastSet;
+
     private Mutator(){
     }
 
     @Override
     public void setIndexDefined(int index){
       bits.getMutator().setSafe(index, 1);
+      lastSet = index;
     }
 
     public void setNull(int index){
@@ -263,6 +273,7 @@ public class NullableMapVector extends MapVector implements FieldVector {
       assert valueCount >= 0;
       super.setValueCount(valueCount);
       bits.getMutator().setValueCount(valueCount);
+      lastSet = valueCount;
     }
 
     @Override
