9bf2e131cf27b7ecd38083c8344e04eb928d8236
Bryan Cutler <cutlerb@gmail.com>
2017-03-10 19:39:35 -0500

    ARROW-615: [Java] Moved ByteArrayReadableSeekableByteChannel to src main o.a.a.vector.util
    The ByteArrayReadableSeekableByteChannel is useful when reading an ArrowRecordBatch from a byte array with ArrowReader. Currently it is vector.file test package, this change moves the class to src/main/java/o.a.a.vector.util.  Updated test usage.
    
    Author: Bryan Cutler <cutlerb@gmail.com>
    
    Closes #370 from BryanCutler/move-ByteArrayReadableSeekableByteChannel-ARROW-615 and squashes the following commits:
    
    46f32a3 [Bryan Cutler] moved ByteArrayReadableSeekableByteChannel to src main o.a.a.vector.util

--- java/vector/src/main/java/org/apache/arrow/vector/util/ByteArrayReadableSeekableByteChannel.java
+++ java/vector/src/main/java/org/apache/arrow/vector/util/ByteArrayReadableSeekableByteChannel.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.arrow.vector.file;
+package org.apache.arrow.vector.util;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
