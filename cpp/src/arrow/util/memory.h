9dd6f3fd726f9c2a293468836d9bca579eb30dd9
Philipp Moritz <pcmoritz@gmail.com>
2017-04-23 17:17:24 -0700

    cleanup

--- cpp/src/arrow/util/memory.h
+++ cpp/src/arrow/util/memory.h
@@ -23,37 +23,41 @@
 
 namespace arrow {
 
+uint8_t* pointer_logical_and(const uint8_t* address, uintptr_t bits) {
+	uintptr_t value = reinterpret_cast<uintptr_t>(address);
+  return reinterpret_cast<uint8_t*>(value & bits);
+}
+
 // A helper function for doing memcpy with multiple threads. This is required
 // to saturate the memory bandwidth of modern cpus.
-void parallel_memcopy(
-    uint8_t* dst, const uint8_t* src, uint64_t nbytes, uint64_t block_size, uint64_t num_threads) {
+void parallel_memcopy(uint8_t* dst, const uint8_t* src, int64_t nbytes,
+                      uintptr_t block_size, int num_threads) {
   std::vector<std::thread> threadpool(num_threads);
-  uint64_t src_address = reinterpret_cast<uint64_t>(src);
-  uint64_t left_address = (src_address + block_size - 1) & ~(block_size - 1);
-  uint64_t right_address = (src_address + nbytes) & ~(block_size - 1);
-  uint64_t num_blocks = (right_address - left_address) / block_size;
+  uint8_t* left = pointer_logical_and(src + block_size - 1, ~(block_size - 1));
+  uint8_t* right = pointer_logical_and(src + nbytes, ~(block_size - 1));
+  int64_t num_blocks = (right - left) / block_size;
+
   // Update right address
-  right_address = right_address - (num_blocks % num_threads) * block_size;
+  right = right - (num_blocks % num_threads) * block_size;
+
   // Now we divide these blocks between available threads. The remainder is
   // handled on the main thread.
-
-  uint64_t chunk_size = (right_address - left_address) / num_threads;
-  uint64_t prefix = left_address - src_address;
-  uint64_t suffix = src_address + nbytes - right_address;
+  int64_t chunk_size = (right - left) / num_threads;
+  int64_t prefix = left - src;
+  int64_t suffix = src + nbytes - right;
   // Now the data layout is | prefix | k * num_threads * block_size | suffix |.
   // We have chunk_size = k * block_size, therefore the data layout is
   // | prefix | num_threads * chunk_size | suffix |.
   // Each thread gets a "chunk" of k blocks.
 
   // Start all threads first and handle leftovers while threads run.
-  for (uint64_t i = 0; i < num_threads; i++) {
+  for (int i = 0; i < num_threads; i++) {
     threadpool[i] = std::thread(memcpy, dst + prefix + i * chunk_size,
-        reinterpret_cast<uint8_t*>(left_address) + i * chunk_size, chunk_size);
+        left + i * chunk_size, chunk_size);
   }
 
   memcpy(dst, src, prefix);
-  memcpy(dst + prefix + num_threads * chunk_size,
-      reinterpret_cast<uint8_t*>(right_address), suffix);
+  memcpy(dst + prefix + num_threads * chunk_size, right, suffix);
 
   for (auto& t : threadpool) {
     if (t.joinable()) { t.join(); }
