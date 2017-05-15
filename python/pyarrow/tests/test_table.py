edfb2dca81db724dd2b771fa8a50da98b5fc4159
Phillip Cloud <cpcloud@gmail.com>
2017-05-15 09:17:27 -0400

    ARROW-1027: [Python] Allow negative indexing in fields/columns on pyarrow Table and Schema objects
    Negative indexing causes an interpreter segfault right now. After this PR, no segfaults, and reasonable error messages for out of bounds indexes.
    
    Author: Phillip Cloud <cpcloud@gmail.com>
    
    Closes #686 from cpcloud/ARROW-1027 and squashes the following commits:
    
    1bfcef4 [Phillip Cloud] ARROW-1027: [Python] Allow negative indexing in fields/columns on pyarrow Table and Schema objects

--- python/pyarrow/tests/test_table.py
+++ python/pyarrow/tests/test_table.py
@@ -254,3 +254,24 @@ def test_table_pandas():
     assert set(df.columns) == set(('a', 'b'))
     assert df.shape == (5, 2)
     assert df.loc[0, 'b'] == -10
+
+
+def test_table_negative_indexing():
+    data = [
+        pa.array(range(5)),
+        pa.array([-10, -5, 0, 5, 10]),
+        pa.array([1.0, 2.0, 3.0]),
+        pa.array(['ab', 'bc', 'cd']),
+    ]
+    table = pa.Table.from_arrays(data, names=tuple('abcd'))
+
+    assert table[-1].equals(table[3])
+    assert table[-2].equals(table[2])
+    assert table[-3].equals(table[1])
+    assert table[-4].equals(table[0])
+
+    with pytest.raises(IndexError):
+        table[-5]
+
+    with pytest.raises(IndexError):
+        table[4]
