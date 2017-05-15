416c876ad34d279792459ccdaa6e1076c6f037ec
Wes McKinney <wes.mckinney@twosigma.com>
2017-05-15 16:51:33 -0400

    Consolidated changelog for the website with new features/improvements shown first. Include git shortlog
    Change-Id: Ib261fc4caa04a4ec16f9cd965fb7ae3bb3dae71b

--- dev/make_changelog.py
+++ dev/make_changelog.py
@@ -52,34 +52,65 @@ def get_issues_for_version(version):
 LINK_TEMPLATE = '[{0}](https://issues.apache.org/jira/browse/{0})'
 
 
-def format_changelog_markdown(issues, out, links=False):
+def format_changelog_markdown(issues, out):
     issues_by_type = defaultdict(list)
     for issue in issues:
         issues_by_type[issue.fields.issuetype.name].append(issue)
-
 
     for issue_type, issue_group in sorted(issues_by_type.items()):
         issue_group.sort(key=lambda x: x.key)
 
         out.write('## {0}\n\n'.format(issue_type))
         for issue in issue_group:
-            if links:
-                name = LINK_TEMPLATE.format(issue.key)
-            else:
-                name = issue.key
-            out.write('* {0} - {1}\n'.format(name,
+            out.write('* {0} - {1}\n'.format(issue.key,
                                              issue.fields.summary))
+        out.write('\n')
+
+
+def format_changelog_website(issues, out):
+    NEW_FEATURE = 'New Features and Improvements'
+    BUGFIX = 'Bug Fixes'
+
+    CATEGORIES = {
+        'New Feature': NEW_FEATURE,
+        'Improvement': NEW_FEATURE,
+        'Task': NEW_FEATURE,
+        'Test': NEW_FEATURE,
+        'Bug': BUGFIX
+    }
+
+    issues_by_category = defaultdict(list)
+    for issue in issues:
+        issue_type = issue.fields.issuetype.name
+        website_category = CATEGORIES[issue_type]
+        issues_by_category[website_category].append(issue)
+
+    WEBSITE_ORDER = [NEW_FEATURE, BUGFIX]
+
+    for issue_category in WEBSITE_ORDER:
+        issue_group = issues_by_category[issue_category]
+        issue_group.sort(key=lambda x: x.key)
+
+        out.write('## {0}\n\n'.format(issue_category))
+        for issue in issue_group:
+            name = LINK_TEMPLATE.format(issue.key)
+            out.write('* {0} - {1}\n'.format(name, issue.fields.summary))
         out.write('\n')
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('Usage: make_changelog.py $FIX_VERSION [$LINKS]')
+        print('Usage: make_changelog.py $FIX_VERSION [$IS_WEBSITE]')
 
     buf = StringIO()
 
-    links = len(sys.argv) > 2 and sys.argv[2] == '1'
+    for_website = len(sys.argv) > 2 and sys.argv[2] == '1'
 
     issues_for_version = get_issues_for_version(sys.argv[1])
-    format_changelog_markdown(issues_for_version, buf, links=links)
+
+    if for_website:
+        format_changelog_website(issues_for_version, buf)
+    else:
+        format_changelog_markdown(issues_for_version, buf)
+
     print(buf.getvalue())
