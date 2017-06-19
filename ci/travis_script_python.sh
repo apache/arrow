4b714a144c23d92dc0b8ec9b3e2fc95246d490ff
Uwe L. Korn <uwelk@xhochy.com>
2017-06-19 15:33:05 +0200

    Don't force optional that is no longer there
    Change-Id: I862649f615e20cf98369f7dcbff89594686ff7c5

--- ci/travis_script_python.sh
+++ ci/travis_script_python.sh
@@ -106,7 +106,7 @@ python_version_tests() {
   # Other stuff pip install
   pip install -r requirements.txt
 
-  python setup.py build_ext --inplace --with-parquet --with-jemalloc
+  python setup.py build_ext --inplace --with-parquet
 
   python -c "import pyarrow.parquet"
   python -c "import pyarrow._jemalloc"
