d12eea48156a3917d54e4ea4918f866559ffb125
Wes McKinney <wes.mckinney@twosigma.com>
2017-07-27 11:18:09 -0400

    Fix license headers
    Change-Id: I9819fb4f79ae202164dc4cf41c8d35961cff2589

--- python/testing/set_env_common.sh
+++ python/testing/set_env_common.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -8,7 +8,7 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#   http:#www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
