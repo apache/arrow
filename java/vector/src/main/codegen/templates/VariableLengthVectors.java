c5a2707c9a7c1c91d3ae8ca91a7d129235686695
siddharth <siddharth@dremio.com>
2017-08-02 18:33:40 -0700

    ARROW-1310: revert changes made in ARROW-886

--- java/vector/src/main/codegen/templates/VariableLengthVectors.java
+++ java/vector/src/main/codegen/templates/VariableLengthVectors.java
@@ -352,7 +352,6 @@ public final class ${className} extends BaseDataValueVector implements VariableW
   }
 
   public void reAlloc() {
-    offsetVector.reAlloc();
     final long newAllocationSize = allocationSizeInBytes*2L;
     if (newAllocationSize > MAX_ALLOCATION_SIZE)  {
       throw new OversizedAllocationException("Unable to expand the buffer. Max allowed buffer size is reached.");
