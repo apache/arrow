a6105d2eb8e024737d2078397456f99ad55623f4
Philipp Moritz <pcmoritz@gmail.com>
2017-08-15 17:59:39 -0700

    lint fix

--- cpp/src/arrow/python/python_to_arrow.cc
+++ cpp/src/arrow/python/python_to_arrow.cc
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include "python_to_arrow.h"
+#include "arrow/python/python_to_arrow.h"
 
 #include <sstream>
 
