54d624b2eeb90afb2724b5137bed5dd0e5d8b0a1
Wes McKinney <wes.mckinney@twosigma.com>
2017-09-06 13:24:09 -0400

    ARROW-1405: [Python] Expose LoggingMemoryPool in Python API
    I removed some output of pointer contents in the LoggingMemoryPool implementation which was showing up as garbage in the console. Example:
    
    ```
    n [1]: import pyarrow as pa
    
    In [2]: import pyarrow.parquet as pq
    
    In [3]: pa.log_memory_allocations(True)
    
    In [4]: t = pq.read_table('/home/wesm/Downloads/part-00000-6570e34b-b42c-4a39-8adf-21d3a97fb87d.snappy.parquet')
    Allocate: size = 320
    Allocate: size = 64
    Allocate: size = 192
    Free: size = 192
    Allocate: size = 320
    Allocate: size = 64
    Allocate: size = 192
    Allocate: size = 192
    Allocate: size = 1152
    Allocate: size = 64
    Allocate: size = 64
    Allocate: size = 64
    Reallocate: old_size = 64 - new_size = 128
    Reallocate: old_size = 128 - new_size = 192
    Reallocate: old_size = 192 - new_size = 320
    Reallocate: old_size = 320 - new_size = 576
    Reallocate: old_size = 64 - new_size = 128
    Reallocate: old_size = 576 - new_size = 1088
    Reallocate: old_size = 128 - new_size = 256
    ```
    
    Author: Wes McKinney <wes.mckinney@twosigma.com>
    
    Closes #1044 from wesm/ARROW-1405 and squashes the following commits:
    
    33111644 [Wes McKinney] Restore includes to prior revision
    41d8506f [Wes McKinney] Docstring
    9d5e96a4 [Wes McKinney] Expose LoggingMemoryPool in Python API

--- cpp/src/arrow/memory_pool.cc
+++ cpp/src/arrow/memory_pool.cc
@@ -162,20 +162,20 @@ LoggingMemoryPool::LoggingMemoryPool(MemoryPool* pool) : pool_(pool) {}
 
 Status LoggingMemoryPool::Allocate(int64_t size, uint8_t** out) {
   Status s = pool_->Allocate(size, out);
-  std::cout << "Allocate: size = " << size << " - out = " << *out << std::endl;
+  std::cout << "Allocate: size = " << size << std::endl;
   return s;
 }
 
 Status LoggingMemoryPool::Reallocate(int64_t old_size, int64_t new_size, uint8_t** ptr) {
   Status s = pool_->Reallocate(old_size, new_size, ptr);
-  std::cout << "Reallocate: old_size = " << old_size << " - new_size = " << new_size
-            << " - ptr = " << *ptr << std::endl;
+  std::cout << "Reallocate: old_size = " << old_size
+            << " - new_size = " << new_size << std::endl;
   return s;
 }
 
 void LoggingMemoryPool::Free(uint8_t* buffer, int64_t size) {
   pool_->Free(buffer, size);
-  std::cout << "Free: buffer = " << buffer << " - size = " << size << std::endl;
+  std::cout << "Free: size = " << size << std::endl;
 }
 
 int64_t LoggingMemoryPool::bytes_allocated() const {
