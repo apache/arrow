344937c46070c0dfb965a74852a51012dc9cd6a8
Phillip Cloud <cpcloud@gmail.com>
2017-09-17 19:15:08 -0400

    Wrong value

--- cpp/src/arrow/util/decimal.cc
+++ cpp/src/arrow/util/decimal.cc
@@ -35,7 +35,7 @@
 
 namespace arrow {
 
-static constexpr uint64_t kIntMask = 0xFFFFFFFF00000000;
+static constexpr uint64_t kIntMask = 0xFFFFFFFF;
 static constexpr uint64_t kMinDecimalDigits = 1ULL;
 static constexpr uint64_t kMaxDecimalDigits = 16ULL;
 static constexpr auto kCarryBit = static_cast<uint64_t>(1) << static_cast<uint64_t>(32);
