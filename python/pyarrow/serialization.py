05788d035f4aa918d80c9db7a1bf74fe38309c60
Philipp Moritz <pcmoritz@gmail.com>
2017-10-21 12:26:08 -0400

    ARROW-1701: [Serialization] Support zero copy PyTorch Tensor serialization
    This also restructures the code such that it is easier to reset the default serialization context back to the initial state after more handlers have been registered.
    
    Author: Philipp Moritz <pcmoritz@gmail.com>
    
    Closes #1223 from pcmoritz/torch-tensor and squashes the following commits:
    
    db09afce [Philipp Moritz] fix test
    ba0856c1 [Philipp Moritz] don't run pytorch test on python 2.7
    264d1992 [Philipp Moritz] remove import that is not required
    882d9a56 [Philipp Moritz] small restructuring and support all PyTorch tensor types
    c6dac9e6 [Philipp Moritz] add -q flag
    23de67b6 [Philipp Moritz] add -y to torch installation
    9814897f [Philipp Moritz] test torch tensor conversion
    375bbfa5 [Philipp Moritz] support serializing torch tensors

--- python/pyarrow/serialization.py
+++ python/pyarrow/serialization.py
@@ -20,107 +20,136 @@ import sys
 
 import numpy as np
 
+import pyarrow as pa
 from pyarrow import serialize_pandas, deserialize_pandas
 from pyarrow.lib import _default_serialization_context
 
-# ----------------------------------------------------------------------
-# Set up serialization for primitive datatypes
+def register_default_serialization_handlers(serialization_context):
 
-# TODO(pcm): This is currently a workaround until arrow supports
-# arbitrary precision integers. This is only called on long integers,
-# see the associated case in the append method in python_to_arrow.cc
-_default_serialization_context.register_type(
-    int, "int",
-    custom_serializer=lambda obj: str(obj),
-    custom_deserializer=lambda data: int(data))
+    # ----------------------------------------------------------------------
+    # Set up serialization for primitive datatypes
 
-if (sys.version_info < (3, 0)):
-    _default_serialization_context.register_type(
-        long, "long",  # noqa: F821
+    # TODO(pcm): This is currently a workaround until arrow supports
+    # arbitrary precision integers. This is only called on long integers,
+    # see the associated case in the append method in python_to_arrow.cc
+    serialization_context.register_type(
+        int, "int",
         custom_serializer=lambda obj: str(obj),
-        custom_deserializer=lambda data: long(data))  # noqa: F821
+        custom_deserializer=lambda data: int(data))
+
+    if (sys.version_info < (3, 0)):
+        serialization_context.register_type(
+            long, "long",  # noqa: F821
+            custom_serializer=lambda obj: str(obj),
+            custom_deserializer=lambda data: long(data))  # noqa: F821
 
 
-def _serialize_ordered_dict(obj):
-    return list(obj.keys()), list(obj.values())
+    def _serialize_ordered_dict(obj):
+        return list(obj.keys()), list(obj.values())
 
 
-def _deserialize_ordered_dict(data):
-    return OrderedDict(zip(data[0], data[1]))
+    def _deserialize_ordered_dict(data):
+        return OrderedDict(zip(data[0], data[1]))
 
 
-_default_serialization_context.register_type(
-    OrderedDict, "OrderedDict",
-    custom_serializer=_serialize_ordered_dict,
-    custom_deserializer=_deserialize_ordered_dict)
+    serialization_context.register_type(
+        OrderedDict, "OrderedDict",
+        custom_serializer=_serialize_ordered_dict,
+        custom_deserializer=_deserialize_ordered_dict)
 
 
-def _serialize_default_dict(obj):
-    return list(obj.keys()), list(obj.values()), obj.default_factory
+    def _serialize_default_dict(obj):
+        return list(obj.keys()), list(obj.values()), obj.default_factory
 
 
-def _deserialize_default_dict(data):
-    return defaultdict(data[2], zip(data[0], data[1]))
+    def _deserialize_default_dict(data):
+        return defaultdict(data[2], zip(data[0], data[1]))
 
 
-_default_serialization_context.register_type(
-     defaultdict, "defaultdict",
-     custom_serializer=_serialize_default_dict,
-     custom_deserializer=_deserialize_default_dict)
+    serialization_context.register_type(
+        defaultdict, "defaultdict",
+        custom_serializer=_serialize_default_dict,
+        custom_deserializer=_deserialize_default_dict)
 
 
-_default_serialization_context.register_type(
-     type(lambda: 0), "function",
-     pickle=True)
+    serialization_context.register_type(
+        type(lambda: 0), "function",
+        pickle=True)
 
-# ----------------------------------------------------------------------
-# Set up serialization for numpy with dtype object (primitive types are
-# handled efficiently with Arrow's Tensor facilities, see python_to_arrow.cc)
-
-
-def _serialize_numpy_array(obj):
-    return obj.tolist(), obj.dtype.str
+    # ----------------------------------------------------------------------
+    # Set up serialization for numpy with dtype object (primitive types are
+    # handled efficiently with Arrow's Tensor facilities, see python_to_arrow.cc)
 
 
-def _deserialize_numpy_array(data):
-    return np.array(data[0], dtype=np.dtype(data[1]))
+    def _serialize_numpy_array(obj):
+        return obj.tolist(), obj.dtype.str
 
 
-_default_serialization_context.register_type(
-    np.ndarray, 'np.array',
-    custom_serializer=_serialize_numpy_array,
-    custom_deserializer=_deserialize_numpy_array)
+    def _deserialize_numpy_array(data):
+        return np.array(data[0], dtype=np.dtype(data[1]))
 
 
-# ----------------------------------------------------------------------
-# Set up serialization for pandas Series and DataFrame
+    serialization_context.register_type(
+        np.ndarray, 'np.array',
+        custom_serializer=_serialize_numpy_array,
+        custom_deserializer=_deserialize_numpy_array)
 
-try:
-    import pandas as pd
 
-    def _serialize_pandas_series(obj):
-        # TODO: serializing Series without extra copy
-        return serialize_pandas(pd.DataFrame({obj.name: obj})).to_pybytes()
+    # ----------------------------------------------------------------------
+    # Set up serialization for pandas Series and DataFrame
 
-    def _deserialize_pandas_series(data):
-        deserialized = deserialize_pandas(data)
-        return deserialized[deserialized.columns[0]]
+    try:
+        import pandas as pd
 
-    def _serialize_pandas_dataframe(obj):
-        return serialize_pandas(obj).to_pybytes()
+        def _serialize_pandas_series(obj):
+            # TODO: serializing Series without extra copy
+            return serialize_pandas(pd.DataFrame({obj.name: obj})).to_pybytes()
 
-    def _deserialize_pandas_dataframe(data):
-        return deserialize_pandas(data)
+        def _deserialize_pandas_series(data):
+            deserialized = deserialize_pandas(data)
+            return deserialized[deserialized.columns[0]]
 
-    _default_serialization_context.register_type(
-        pd.Series, 'pd.Series',
-        custom_serializer=_serialize_pandas_series,
-        custom_deserializer=_deserialize_pandas_series)
+        def _serialize_pandas_dataframe(obj):
+            return serialize_pandas(obj).to_pybytes()
 
-    _default_serialization_context.register_type(
-        pd.DataFrame, 'pd.DataFrame',
-        custom_serializer=_serialize_pandas_dataframe,
-        custom_deserializer=_deserialize_pandas_dataframe)
-except ImportError:
-    # no pandas
-    pass
+        def _deserialize_pandas_dataframe(data):
+            return deserialize_pandas(data)
+
+        serialization_context.register_type(
+            pd.Series, 'pd.Series',
+            custom_serializer=_serialize_pandas_series,
+            custom_deserializer=_deserialize_pandas_series)
+
+        serialization_context.register_type(
+            pd.DataFrame, 'pd.DataFrame',
+            custom_serializer=_serialize_pandas_dataframe,
+            custom_deserializer=_deserialize_pandas_dataframe)
+    except ImportError:
+        # no pandas
+        pass
+
+    # ----------------------------------------------------------------------
+    # Set up serialization for pytorch tensors
+
+    try:
+        import torch
+
+        def _serialize_torch_tensor(obj):
+            return obj.numpy()
+
+        def _deserialize_torch_tensor(data):
+            return torch.from_numpy(data)
+
+        for t in [torch.FloatTensor, torch.DoubleTensor, torch.HalfTensor,
+                  torch.ByteTensor, torch.CharTensor, torch.ShortTensor,
+                  torch.IntTensor, torch.LongTensor]:
+            serialization_context.register_type(
+                t, "torch." + t.__name__,
+                custom_serializer=_serialize_torch_tensor,
+                custom_deserializer=_deserialize_torch_tensor)
+    except ImportError:
+        # no torch
+        pass
+
+
+register_default_serialization_handlers(_default_serialization_context)
