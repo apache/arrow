3a5e3f6ffbf05c80437c356175bdbe28a6a7038d
Phillip Cloud <cpcloud@gmail.com>
2017-11-08 10:33:35 -0500

    DCHECK and format

--- cpp/src/arrow/util/decimal.cc
+++ cpp/src/arrow/util/decimal.cc
@@ -350,6 +350,9 @@ Status Decimal128::FromString(const std::string& s, Decimal128* out, int* precis
     if (scale != NULLPTR) {
       // compute the scale from the adjusted exponent
       std::string adjusted_exponent_string(digit_start, end);
+      DCHECK(std::all_of(adjusted_exponent_string.cbegin(),
+                         adjusted_exponent_string.cend(), std::isdigit))
+          << "Non decimal digit character found in " << adjusted_exponent_string;
       const auto adjusted_exponent =
           static_cast<int32_t>(std::stol(adjusted_exponent_string));
       const auto len = static_cast<int32_t>(whole_part.size() + fractional_part.size());
