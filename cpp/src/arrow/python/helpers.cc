3b91e550fa6dbbdb5e36d2f98a550c21a2743b1c
Wes McKinney <wes.mckinney@twosigma.com>
2017-12-03 16:24:25 -0500

    Rename Rescale to RescaleDecimal
    Change-Id: I82c79ceae9206efde356ea241badb7a25e59adbf

--- cpp/src/arrow/python/helpers.cc
+++ cpp/src/arrow/python/helpers.cc
@@ -163,7 +163,7 @@ Status DecimalFromPythonDecimal(PyObject* python_decimal, const DecimalType& arr
 
   if (scale != inferred_scale) {
     DCHECK_NE(out, NULLPTR);
-    RETURN_NOT_OK(Rescale(*out, inferred_scale, scale, out));
+    RETURN_NOT_OK(RescaleDecimal(*out, inferred_scale, scale, out));
   }
   return Status::OK();
 }
--- cpp/src/arrow/util/decimal.cc
+++ cpp/src/arrow/util/decimal.cc
@@ -854,8 +854,8 @@ static const Decimal128 ScaleMultipliers[] = {
     Decimal128("10000000000000000000000000000000000000"),
     Decimal128("100000000000000000000000000000000000000")};
 
-Status Rescale(const Decimal128& value, int32_t original_scale, int32_t new_scale,
-               Decimal128* out) {
+Status RescaleDecimal(const Decimal128& value, int32_t original_scale, int32_t new_scale,
+                      Decimal128* out) {
   DCHECK_NE(out, NULLPTR);
   DCHECK_NE(original_scale, new_scale);
   const int32_t delta_scale = original_scale - new_scale;
