ade030e59c654026d4e046e59f13569d3f6b7061
Phillip Cloud <cpcloud@gmail.com>
2017-12-03 13:05:33 -0500

    Clarify randdecimal contract

--- python/pyarrow/tests/util.py
+++ python/pyarrow/tests/util.py
@@ -24,16 +24,20 @@ def random_seed(seed):
 
 
 def randdecimal(precision, scale):
-    abs_scale = abs(scale)
-    max_whole_value = 10 ** (precision - abs_scale) - 1
+    if scale < 0:
+        raise ValueError(
+            'randdecimal does not yet support generating decimals with '
+            'negative scale'
+        )
+    max_whole_value = 10 ** (precision - scale) - 1
     whole = random.randint(-max_whole_value, max_whole_value)
 
     if not scale:
         return decimal.Decimal(whole)
 
-    max_fractional_value = 10 ** abs_scale - 1
+    max_fractional_value = 10 ** scale - 1
     fractional = random.randint(0, max_fractional_value)
 
     return decimal.Decimal(
-        '{}.{}'.format(whole, str(fractional).rjust(abs_scale, '0'))
+        '{}.{}'.format(whole, str(fractional).rjust(scale, '0'))
     )
