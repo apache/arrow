fd972f0e552ec4b85c4a0e3d8a358cbff4580fc6
Robert Nishihara <robertnishihara@gmail.com>
2018-02-19 20:55:20 -0500

    ARROW-2121: [Python] Handle object arrays directly in pandas serializer.
    The goal here is to get the best of both the `pandas_serialization_context` (speed at serializing pandas dataframes containing strings and other objects) and the `default_serialization_context` (correctly serializing a large class of numpy object arrays).
    
    This PR sort of messes up the function `pa.pandas_compat.dataframe_to_serialized_dict`. Is that function just a helper function for implementing the custom pandas serializers? Or is it intended to be used in other places.
    
    TODO in this PR (assuming you think this approach is reasonable):
    
    - [x] remove `pandas_serialization_context`
    - [x] make sure this code path is tested
    - [x] double check performance/behavior
    
    cc @wesm @pcmoritz @devin-petersohn
    
    Author: Robert Nishihara <robertnishihara@gmail.com>
    Author: Wes McKinney <wes.mckinney@twosigma.com>
    
    Closes #1581 from robertnishihara/pandasserialization and squashes the following commits:
    
    c551ed6c [Wes McKinney] Add benchmark for zero-copy pandas deserialization
    e12cd721 [Robert Nishihara] Use highest protocol.
    6e3eceb6 [Robert Nishihara] Fix bug.
    31b3cbee [Robert Nishihara] Fixes.
    0fc9ecfd [Robert Nishihara] Update documentation and testing.
    b796fbdb [Robert Nishihara] Handle object arrays directly in pandas serializer.

--- python/pyarrow/pandas_compat.py
+++ python/pyarrow/pandas_compat.py
@@ -27,7 +27,7 @@ import pandas as pd
 import six
 
 import pyarrow as pa
-from pyarrow.compat import PY2, zip_longest  # noqa
+from pyarrow.compat import builtin_pickle, PY2, zip_longest  # noqa
 
 
 def infer_dtype(column):
@@ -424,11 +424,19 @@ def dataframe_to_serialized_dict(frame):
             block_data.update(dictionary=values.categories,
                               ordered=values.ordered)
             values = values.codes
-
         block_data.update(
             placement=block.mgr_locs.as_array,
             block=values
         )
+
+        # If we are dealing with an object array, pickle it instead. Note that
+        # we do not use isinstance here because _int.CategoricalBlock is a
+        # subclass of _int.ObjectBlock.
+        if type(block) == _int.ObjectBlock:
+            block_data['object'] = None
+            block_data['block'] = builtin_pickle.dumps(
+                values, protocol=builtin_pickle.HIGHEST_PROTOCOL)
+
         blocks.append(block_data)
 
     return {
@@ -463,6 +471,9 @@ def _reconstruct_block(item):
         block = _int.make_block(block_arr, placement=placement,
                                 klass=_int.DatetimeTZBlock,
                                 dtype=dtype)
+    elif 'object' in item:
+        block = _int.make_block(builtin_pickle.loads(block_arr),
+                                placement=placement, klass=_int.ObjectBlock)
     else:
         block = _int.make_block(block_arr, placement=placement)
 
