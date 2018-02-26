655eb74e7a275b5861246baa0641851df3ca2ffd
Uwe L. Korn <uwelk@xhochy.com>
2018-02-26 10:54:12 -0500

    ARROW-2212: [C++/Python] Build Protobuf in base manylinux 1 docker image
    Saves ~6min build time.
    
    Author: Uwe L. Korn <uwelk@xhochy.com>
    
    Closes #1662 from xhochy/ARROW-2212 and squashes the following commits:
    
    acf2319d <Uwe L. Korn> ARROW-2212:  Build Protobuf in base manylinux1 docker image

---
+++ python/manylinux1/scripts/build_protobuf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ex
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+PROTOBUF_VERSION="2.6.0"
+
+curl -sL https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-${PROTOBUF_VERSION}.tar.gz -o protobuf-${PROTOBUF_VERSION}.tar.gz
+tar xf protobuf-${PROTOBUF_VERSION}.tar.gz
+pushd protobuf-${PROTOBUF_VERSION}
+./configure --disable-shared --prefix=/usr "CXXFLAGS=-O2 -fPIC"
+make -j5
+make install
+popd
+rm -rf protobuf-${PROTOBUF_VERSION}.tar.gz protobuf-${PROTOBUF_VERSION}
