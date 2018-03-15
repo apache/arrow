722273fedec069cd62fc147a440b23f7481cdee9
Wes McKinney <wes.mckinney@twosigma.com>
2018-03-15 11:37:26 -0400

    clang-format
    Change-Id: I979f5c7f91a555a58dade14a3e0acb8d22c56978

--- cpp/src/arrow/buffer.cc
+++ cpp/src/arrow/buffer.cc
@@ -70,9 +70,7 @@ Status Buffer::FromString(const std::string& data, std::shared_ptr<Buffer>* out)
   return FromString(data, default_memory_pool(), out);
 }
 
-void Buffer::CheckMutable() const {
-  DCHECK(is_mutable()) << "buffer not mutable";
-}
+void Buffer::CheckMutable() const { DCHECK(is_mutable()) << "buffer not mutable"; }
 
 PoolBuffer::PoolBuffer(MemoryPool* pool) : ResizableBuffer(nullptr, 0) {
   if (pool == nullptr) {
