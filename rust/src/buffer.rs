b6e8b4b10dd3804ee65b636bc8f12ac53fa1e1fd
Andy Grove <andygrove73@gmail.com>
2018-04-03 06:44:00 +0200

    ARROW-2381: [Rust] Adds iterator support to Buffer<T>
    Author: Andy Grove <andygrove73@gmail.com>
    
    Closes #1823 from andygrove/buffer_iter and squashes the following commits:
    
    d1e22df <Andy Grove> Merge branch 'master' into buffer_iter
    4ff8d74 <Andy Grove> implement iterator for Buffer<T>
    43d3988 <Andy Grove> implement Drop for Buffer so memory is released

--- rust/src/buffer.rs
+++ rust/src/buffer.rs
@@ -54,6 +54,40 @@ impl<T> Buffer<T> {
             *p.offset(i as isize) = v;
         }
     }
+
+    pub fn iter(&self) -> BufferIterator<T> {
+        BufferIterator {
+            data: self.data,
+            len: self.len,
+            index: 0
+        }
+    }
+
+}
+
+impl<T> Drop for Buffer<T> {
+    fn drop(&mut self) {
+        mem::drop(self.data)
+    }
+}
+
+pub struct BufferIterator<T> {
+    data: *const T,
+    len: i32,
+    index: isize
+}
+
+impl<T> Iterator for BufferIterator<T> where T: Copy {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.len as isize {
+            self.index += 1;
+            Some(unsafe { *self.data.offset(self.index-1) })
+        } else {
+            None
+        }
+    }
 }
 
 macro_rules! array_from_primitive {
@@ -93,9 +127,18 @@ array_from_primitive!(i64);
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_buffer_i32() {
         let b: Buffer<i32> = Buffer::from(vec![1, 2, 3, 4, 5]);
         assert_eq!(5, b.len);
+    }
+
+    #[test]
+    fn test_iterator_i32() {
+        let b: Buffer<i32> = Buffer::from(vec![1, 2, 3, 4, 5]);
+        let it = b.iter();
+        let v : Vec<i32> = it.map(|n| n+1).collect();
+        assert_eq!(vec![2,3,4,5,6], v);
     }
 }
