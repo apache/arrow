72fa4eb510b29d8597b66bf6ed2e7f53965c1ce7
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-04-07 11:05:03 +0200

    trying to fix linnnux builds

--- cd/crossbow.py
+++ cd/crossbow.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python
 
+# TODO: create a docker container too for this sscript with dependencies pre-installed
+# TODO: probably should turn off auto cancellation feature of travis
+# TODO: dry-run / render feature
+
 # we might weaken the dependencies later, but a release manager probably
 # can have these installed
+import re
+import sys
 import pygit2
 
 from pygit2 import Signature, Repository, UserPass, RemoteCallbacks
 from pathlib import Path
 
+if len(sys.argv) > 1:
+    pattern = sys.argv[1]
+else:
+    pattern = False
+
 ARROW_REPO = 'https://github.com/kszucs/arrow'
 ARROW_BRANCH = 'cd'
+
+PLAT = 'x86_64'
+
+# by default we should query the current  build ref
+# BUILD_REF = '7b2c79765cf92760e1f8cca079159d9613b86412'
+arrow_path = Path(__file__).absolute().parents[1]
+print(arrow_path)
+
+arrow_repo = Repository(str(arrow_path))
+
+BUILD_REF = arrow_repo.head.target
+
+PYARROW_VERSION = '0.9.0'
+
 
 # we should handle this later
 signature = Signature('John Doe', 'jdoe@example.com', 12346, 0)
@@ -35,6 +60,10 @@ cwd = Path(__file__).parent
 
 for config in cwd.glob('*.yml'):
     branch_name = config.stem
+
+    if pattern and not re.search(pattern, branch_name):
+        continue
+
     reference = 'refs/heads/{}'.format(branch_name)
     refspecs.append(reference)
 
@@ -58,7 +87,13 @@ for config in cwd.glob('*.yml'):
     content = config.read_text()
 
     # FANCY templating
-    content = content.format(ARROW_REPO=ARROW_REPO, ARROW_BRANCH=ARROW_BRANCH)
+    content = content.format(
+        ARROW_REPO=ARROW_REPO,
+        ARROW_BRANCH=ARROW_BRANCH,
+        PLAT=PLAT,
+        BUILD_REF=BUILD_REF,
+        PYARROW_VERSION=PYARROW_VERSION
+    )
 
     blob_id = repo.create_blob(content)
     blob = repo[blob_id]
@@ -85,7 +120,7 @@ for config in cwd.glob('*.yml'):
 def acquire_credentials_cb(url, username_from_url, allowed_types):
     print('credentials', url, username_from_url, allowed_types)
     # its a libgit2 bug, that it infinitly retries the authentication
-    token = '<top secret>'
+    token = 'b713b374ac028e9f1579b5facca6c175f0dd02b5'
     return UserPass(token, 'x-oauth-basic')
 
 
