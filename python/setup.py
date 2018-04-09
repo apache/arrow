01c0f43481e48c831cb7b9b1109017c4efd475a6
Antoine Pitrou <antoine@python.org>
2018-04-09 14:36:13 +0200

    ARROW-2100: [Python] Drop Python 3.4 support

--- python/setup.py
+++ python/setup.py
@@ -500,7 +500,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
         ],
