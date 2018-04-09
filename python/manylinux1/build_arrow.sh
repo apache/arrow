1bb7fbabb73acacbb8e80fec913ab3c19503698a
Antoine Pitrou <antoine@python.org>
2018-04-09 17:43:50 +0200

    ARROW-2100: [Python] Drop Python 3.4 support
    I'm not sure I missed any occurrence. `grep` seems to suggest I didn't.
    
    Author: Antoine Pitrou <antoine@python.org>
    
    Closes #1862 from pitrou/ARROW-2100-drop-python-34 and squashes the following commits:
    
    01c0f43 <Antoine Pitrou> ARROW-2100:  Drop Python 3.4 support

--- python/manylinux1/build_arrow.sh
+++ python/manylinux1/build_arrow.sh
@@ -26,7 +26,7 @@
 # * Copyright (c) 2013-2016, Matt Terry and Matthew Brett (BSD 2-clause)
 
 # Build different python versions with various unicode widths
-PYTHON_VERSIONS="${PYTHON_VERSIONS:-2.7,16 2.7,32 3.4,16 3.5,16 3.6,16}"
+PYTHON_VERSIONS="${PYTHON_VERSIONS:-2.7,16 2.7,32 3.5,16 3.6,16}"
 
 source /multibuild/manylinux_utils.sh
 
