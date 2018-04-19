733cc81c2cd4b9f6e4a06a85ee99fdd0ea44eba2
Antoine Pitrou <antoine@python.org>
2018-04-19 16:17:30 +0200

    ARROW-1928: [C++] Add BitmapReader/BitmapWriter benchmarks
    Also improve the BitmapWriter implementation (around 18% faster here)

--- cpp/src/arrow/util/bit-util.h
+++ cpp/src/arrow/util/bit-util.h
@@ -428,7 +428,7 @@ class BitmapReader {
   void Next() {
     ++bit_offset_;
     ++position_;
-    if (bit_offset_ == 8) {
+    if (ARROW_PREDICT_FALSE(bit_offset_ == 8)) {
       bit_offset_ = 0;
       ++byte_offset_;
       if (ARROW_PREDICT_TRUE(position_ < length_)) {
@@ -453,23 +453,23 @@ class BitmapWriter {
       : bitmap_(bitmap), position_(0), length_(length) {
     current_byte_ = 0;
     byte_offset_ = start_offset / 8;
-    bit_offset_ = start_offset % 8;
+    bit_mask_ = static_cast<uint8_t>(1 << (start_offset % 8));
     if (length > 0) {
       current_byte_ = bitmap[byte_offset_];
     }
   }
 
-  void Set() { current_byte_ |= BitUtil::kBitmask[bit_offset_]; }
+  void Set() { current_byte_ |= bit_mask_; }
 
-  void Clear() { current_byte_ &= BitUtil::kFlippedBitmask[bit_offset_]; }
+  void Clear() { current_byte_ &= bit_mask_ ^ 0xFF; }
 
   void Next() {
-    ++bit_offset_;
+    bit_mask_ = static_cast<uint8_t>(bit_mask_ << 1);
     ++position_;
-    bitmap_[byte_offset_] = current_byte_;
-    if (bit_offset_ == 8) {
-      bit_offset_ = 0;
-      ++byte_offset_;
+    if (bit_mask_ == 0) {
+      // Finished this byte, need advancing
+      bit_mask_ = 0x01;
+      bitmap_[byte_offset_++] = current_byte_;
       if (ARROW_PREDICT_TRUE(position_ < length_)) {
         current_byte_ = bitmap_[byte_offset_];
       }
@@ -477,10 +477,9 @@ class BitmapWriter {
   }
 
   void Finish() {
-    if (ARROW_PREDICT_TRUE(position_ < length_)) {
-      if (bit_offset_ != 0) {
-        bitmap_[byte_offset_] = current_byte_;
-      }
+    // Store current byte if we didn't went past bitmap storage
+    if (bit_mask_ != 0x01 || position_ < length_) {
+      bitmap_[byte_offset_] = current_byte_;
     }
   }
 
@@ -492,8 +491,8 @@ class BitmapWriter {
   int64_t length_;
 
   uint8_t current_byte_;
+  uint8_t bit_mask_;
   int64_t byte_offset_;
-  int64_t bit_offset_;
 };
 
 }  // namespace internal
