c19b1f066e8673feccc7da87f1ff839051ef2beb
Andy Grove <andygrove73@gmail.com>
2018-04-21 14:11:09 +0100

    ARROW-2481: [Rust] Move all calls to free() into memory.rs
    Author: Andy Grove <andygrove73@gmail.com>
    
    Closes #1919 from andygrove/memory_refactor and squashes the following commits:
    
    153d65d3 <Andy Grove> move calls to free memory to memory.rs
    589ef71d <Andy Grove> Merge remote-tracking branch 'upstream/master'
    bd4fbb55 <Andy Grove> Merge remote-tracking branch 'upstream/master'
    9c8a10a4 <Andy Grove> Merge remote-tracking branch 'upstream/master'
    05592f8c <Andy Grove> Merge remote-tracking branch 'upstream/master'
    8c0e6982 <Andy Grove> Merge remote-tracking branch 'upstream/master'
    31ef90ba <Andy Grove> Merge remote-tracking branch 'upstream/master'
    2f87c703 <Andy Grove> Fix build - add missing import

--- rust/src/builder.rs
+++ rust/src/builder.rs
@@ -24,12 +24,6 @@ use std::slice;
 use super::buffer::*;
 use super::memory::*;
 
-#[cfg(windows)]
-#[link(name = "msvcrt")]
-extern "C" {
-    fn _aligned_free(prt: *const u8);
-}
-
 /// Buffer builder with zero-copy build method
 pub struct Builder<T> {
     data: *mut T,
@@ -143,22 +137,11 @@ impl<T> Builder<T> {
 }
 
 impl<T> Drop for Builder<T> {
-    #[cfg(windows)]
     fn drop(&mut self) {
         if !self.data.is_null() {
             unsafe {
                 let p = mem::transmute::<*const T, *const u8>(self.data);
-                _aligned_free(p);
-            }
-        }
-    }
-
-    #[cfg(not(windows))]
-    fn drop(&mut self) {
-        if !self.data.is_null() {
-            unsafe {
-                let p = mem::transmute::<*const T, *mut libc::c_void>(self.data);
-                libc::free(p);
+                free_aligned(p);
             }
         }
     }
