c26100ff81d2c8289e32c233a9cde4de658aa0a6
Paddy Horan <paddyhoran@hotmail.com>
2018-04-29 23:03:39 -0400

    Restoring quotes.

--- ci/appveyor-install.bat
+++ ci/appveyor-install.bat
@@ -17,7 +17,7 @@
 
 @echo on
 
-if %JOB% == "Rust_Stable" (
+if "%JOB%" == "Rust_Stable" (
     curl -sSf -o rustup-init.exe https://win.rustup.rs/
     rustup-init.exe -y --default-host %TARGET% --default-toolchain %RUST_VERSION%
     set PATH=%PATH%;C:\Users\Appveyor\.cargo\bin
