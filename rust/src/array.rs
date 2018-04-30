f38d3c00eb53344291978541786beeb71aa39f3d
Andy Grove <andygrove73@gmail.com>
2018-04-30 16:19:25 -0600

    Add RecordBatch

--- rust/src/array.rs
+++ rust/src/array.rs
@@ -17,8 +17,6 @@
 
 use std::any::Any;
 use std::convert::From;
-use std::fmt;
-use std::fmt::{Error, Formatter};
 use std::iter::Iterator;
 use std::rc::Rc;
 use std::str;
@@ -27,7 +25,8 @@ use std::string::String;
 use super::bitmap::Bitmap;
 use super::buffer::Buffer;
 use super::datatypes::*;
-use super::list::List;
+use super::list::*;
+use super::list_builder::*;
 
 /// Array data type
 pub trait ArrayData {
@@ -203,30 +202,26 @@ array_from_optional_primitive!(i32, 0_i32);
 array_from_optional_primitive!(i64, 0_i64);
 
 ///// This method mostly just used for unit tests
-//impl From<Vec<&'static str>> for Array {
-//    fn from(v: Vec<&'static str>) -> Self {
-//        unimplemented!()
-////        Array::from(v.iter().map(|s| s.to_string()).collect::<Vec<String>>())
-//    }
-//}
-//
-//impl From<Vec<String>> for Array {
-//    fn from(v: Vec<String>) -> Self {
-//        unimplemented!()
-//    }
-//}
+impl From<Vec<&'static str>> for Array {
+    fn from(v: Vec<&'static str>) -> Self {
+        let mut builder: ListBuilder<u8> = ListBuilder::with_capacity(v.len());
+        for s in v {
+            builder.push(s.as_bytes())
+        }
+        Array::from(builder.finish())
+    }
+}
 
-//impl From<Vec<Rc<Array>>> for Array {
-//    fn from(v: Vec<Rc<Array>>) -> Self {
-//        Array {
-//            len: v.len() as i32,
-//            null_count: 0,
-//            validity_bitmap: None,
-//            data: ArrayData::Struct(v.iter().map(|a| a.clone()).collect()),
-//        }
-//    }
-//}
-//
+impl From<Vec<String>> for Array {
+    fn from(v: Vec<String>) -> Self {
+        let mut builder: ListBuilder<u8> = ListBuilder::with_capacity(v.len());
+        for s in v {
+            builder.push(s.as_bytes())
+        }
+        Array::from(builder.finish())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::super::array::*;
@@ -256,38 +251,6 @@ mod tests {
         assert_eq!("World!", str::from_utf8(array_list_u8.get(1)).unwrap());
     }
 
-    //    #[test]
-//    fn test_utf8_offsets() {
-//        let a = Array::from(vec!["this", "is", "a", "test"]);
-//        assert_eq!(4, a.len());
-//        match *a.data() {
-//            ArrayData::Utf8(ref list) => {
-//                assert_eq!(11, list.data().len());
-//                assert_eq!(0, *list.offsets().get(0));
-//                assert_eq!(4, *list.offsets().get(1));
-//                assert_eq!(6, *list.offsets().get(2));
-//                assert_eq!(7, *list.offsets().get(3));
-//                assert_eq!(11, *list.offsets().get(4));
-//            }
-//            _ => panic!(),
-//        }
-//    }
-//
-//    #[test]
-//    fn test_utf8_slices() {
-//        let a = Array::from(vec!["this", "is", "a", "test"]);
-//        match *a.data() {
-//            ArrayData::Utf8(ref d) => {
-//                assert_eq!(4, d.len());
-//                assert_eq!("this", str::from_utf8(d.slice(0)).unwrap());
-//                assert_eq!("is", str::from_utf8(d.slice(1)).unwrap());
-//                assert_eq!("a", str::from_utf8(d.slice(2)).unwrap());
-//                assert_eq!("test", str::from_utf8(d.slice(3)).unwrap());
-//            }
-//            _ => panic!(),
-//        }
-//    }
-//
     #[test]
     fn test_from_bool() {
         let a = Array::from(vec![false, false, true, false]);
@@ -336,11 +299,6 @@ mod tests {
 
     #[test]
     fn test_struct() {
-        let _schema = DataType::Struct(vec![
-            Field::new("a", DataType::Int32, false),
-            Field::new("b", DataType::Float32, false),
-        ]);
-
         let a = Rc::new(BufferArrayData::from(Buffer::from(vec![1, 2, 3, 4, 5])));
         let b = Rc::new(BufferArrayData::from(Buffer::from(vec![1.1, 2.2, 3.3, 4.4, 5.5])));
 
