22b283643203667c0b16ee2c00d857f0d553fda0
Zhijun Fu <zhijun.fu@outlook.com>
2018-05-09 10:07:23 +0800

    [Plasma] improve notification push logic when a new client subscribes

--- cpp/src/plasma/store.h
+++ cpp/src/plasma/store.h
@@ -164,6 +164,8 @@ class PlasmaStore {
  private:
   void push_notification(ObjectInfoT* object_notification);
 
+  void push_notification(ObjectInfoT* object_notification, int client_fd);
+
   void add_client_to_object_clients(ObjectTableEntry* entry, Client* client);
 
   void return_from_get(GetRequest* get_req);
