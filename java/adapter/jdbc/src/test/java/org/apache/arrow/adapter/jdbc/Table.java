840970cab8125e5b74174c0523a3849f75f7ab2c
yashpal <yashpal.thakur@xoriant.com>
2018-05-09 06:23:54 -0400

    file committed for review comment implementation

--- java/adapter/jdbc/src/test/java/org/apache/arrow/adapter/jdbc/Table.java
+++ java/adapter/jdbc/src/test/java/org/apache/arrow/adapter/jdbc/Table.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,254 +30,203 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Table {
 
     private String name;
+    private String type;
+    private String vector;
+    private String timezone;
     private String create;
     private String[] data;
     private String query;
     private String drop;
-	private int [] ints;
-	private int [] booleans;
-	private int [] tinyInts;
-	private int [] smallInts;
-	private int [] bigInts;
-	private long [] times;
-	private long [] dates;
-	private long [] timestamps;
-	private String [] bytes;
-	private String [] varchars; 
-	private String [] chars;
-	private String [] clobs;
-	private float [] reals;
-	private double [] doubles;
-	private BigDecimal [] decimals;
-	private String allColumns;	
-	private long [] pstTime;
-	private long [] estTime;
-	private long [] gmtTime;
-	private long [] pstDate;
-	private long [] estDate;
-	private long [] gmtDate;
-	private long [] pstTimestamp;
-	private long [] estTimestamp;
-	private long [] gmtTimestamp;
-	
-	public Table() {
+    private String[] values;
+    private String[] vectors;
+    private int rowCount;
+
+    public Table() {
     }
 
-	public String getName() {
+    public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getVector() {
+        return vector;
+    }
+
+    public void setVector(String vector) {
+        this.vector = vector;
+    }
+
+    public String[] getValues() {
+        return values;
+    }
+
+    public void setValues(String[] values) {
+        this.values = values;
+    }
+
+    public Long[] getLongValues() {
+        Long[] arr = new Long[values.length];
+        int i = 0;
+        for (String str: values) {
+            arr[i++] = Long.parseLong(str);
+        }
+        return arr;
+    }
+
+    public Integer[] getIntValues() {
+        Integer[] arr = new Integer[values.length];
+        int i = 0;
+        for (String str: values) {
+            arr[i++] = Integer.parseInt(str);
+        }
+        return arr;
+    }
+    public Boolean[] getBoolValues() {
+        Boolean[] arr = new Boolean[values.length];
+        int i = 0;
+        for (String str: values) {
+            arr[i++] = Boolean.parseBoolean(str);
+        }
+        return arr;
+    }
+    public BigDecimal[] getBigDecimalValues() {
+        BigDecimal[] arr = new BigDecimal[values.length];
+        int i = 0;
+        for (String str: values) {
+            arr[i++] = new BigDecimal(str);
+        }
+        return arr;
+    }
+    public Double[] getDoubleValues() {
+        Double[] arr = new Double[values.length];
+        int i = 0;
+        for (String str: values) {
+            arr[i++] = Double.parseDouble(str);
+        }
+        return arr;
+    }
+
+    public Float[] getFloatValues() {
+        Float[] arr = new Float[values.length];
+        int i = 0;
+        for (String str: values) {
+            arr[i++] = Float.parseFloat(str);
+        }
+        return arr;
+    }
+
+    public byte[][] getBinaryValues() {
+        return getHexToByteArray(values);
+    }
+
+    public byte[][] getVarCharValues() {
+        return getByteArray(values);
+    }
+
+    public byte[][] getBlobValues() {
+        return getBinaryValues();
+    }
+
+    public byte[][] getClobValues() {
+        return getByteArray(values);
+    }
+
+    public byte[][] getCharValues() {
+        return getByteArray(values);
+    }
+
     public String getCreate() {
         return create;
     }
+
     public void setCreate(String create) {
         this.create = create;
     }
+
     public String[] getData() {
         return data;
     }
+
     public void setData(String[] data) {
         this.data = data;
     }
+
     public String getQuery() {
         return query;
     }
+
     public void setQuery(String query) {
         this.query = query;
     }
+
     public String getDrop() {
         return drop;
     }
+
     public void setDrop(String drop) {
         this.drop = drop;
     }
-	public int[] getInts() {
-		return ints;
+
+    public String getTimezone() {
+		return timezone;
 	}
-	public void setInts(int[] ints) {
-		this.ints = ints;
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
 	}
-	public int[] getBooleans() {
-		return booleans;
+
+	public String[] getVectors() {
+		return vectors;
 	}
-	public void setBooleans(int[] booleans) {
-		this.booleans = booleans;
-	}
-	public BigDecimal [] getDecimals() {
-		return decimals;
-	}
-	public void setDecimals(BigDecimal [] decimals) {
-		this.decimals = decimals;
-	}
-	public double[] getDoubles() {
-		return doubles;
-	}
-	public void setDoubles(double[] doubles) {
-		this.doubles = doubles;
-	}
-	public int[] getTinyInts() {
-		return tinyInts;
-	}
-	public void setTinyInts(int[] tinyInts) {
-		this.tinyInts = tinyInts;
-	}
-	public int[] getSmallInts() {
-		return smallInts;
-	}
-	public void setSmallInts(int[] smallInts) {
-		this.smallInts = smallInts;
-	}
-	public int[] getBigInts() {
-		return bigInts;
-	}
-	public void setBigInts(int[] bigInts) {
-		this.bigInts = bigInts;
-	}
-	public long[] getTimes() {
-		return times;
-	}
-	public void setTimes(long[] times) {
-		this.times = times;
-	}
-	public long[] getDates() {
-		return dates;
-	}
-	public void setDates(long[] dates) {
-		this.dates = dates;
-	}
-	public long[] getTimestamps() {
-		return timestamps;
-	}
-	public void setTimestamps(long[] timestamps) {
-		this.timestamps = timestamps;
-	}
-	public String [] getBytes() {
-		return bytes;
-	}
-	public void setBytes(String [] bytes) {
-		this.bytes = bytes;
-	}
-	public String []getVarchars() {
-		return varchars;
-	}
-	public void setVarchars(String [] varchars) {
-		this.varchars = varchars;
-	}
-	public String [] getChars() {
-		return chars;
-	}
-	public void setChars(String [] chars) {
-		this.chars = chars;
-	}
-	public String [] getClobs() {
-		return clobs;
-	}
-	public void setClobs(String [] clobs) {
-		this.clobs = clobs;
-	}
-	public float[] getReals() {
-		return reals;
-	}
-	public void setReals(float[] reals) {
-		this.reals = reals;
-	}
-	public String getAllColumns() {
-		return allColumns;
-	}
-	public void setAllColumns(String allColumns) {
-		this.allColumns = allColumns;
-	}
-	public byte [][] getHexStringAsByte () {
-		return getHexToByteArray (bytes);
-	}
-	public byte [][] getClobAsByte () {
-		return getByteArray (clobs);
-	}
-	public byte [][] getCharAsByte () {
-		return getByteArray (chars);
-	}
-	public byte [][] getVarCharAsByte () {
-		return getByteArray (varchars);
-	}
-	public long[] getPstTime() {
-		return pstTime;
-	}
-	public void setPstTime(long[] pstTime) {
-		this.pstTime = pstTime;
-	}
-	public long[] getEstTime() {
-		return estTime;
-	}
-	public void setEstTime(long[] estTime) {
-		this.estTime = estTime;
-	}
-	public long[] getGmtTime() {
-		return gmtTime;
-	}
-	public void setGmtTime(long[] gmtTime) {
-		this.gmtTime = gmtTime;
-	}
-	public long[] getPstDate() {
-		return pstDate;
-	}
-	public void setPstDate(long[] pstDate) {
-		this.pstDate = pstDate;
-	}
-	public long[] getEstDate() {
-		return estDate;
-	}
-	public void setEstDate(long[] estDate) {
-		this.estDate = estDate;
-	}
-	public long[] getGmtDate() {
-		return gmtDate;
-	}
-	public void setGmtDate(long[] gmtDate) {
-		this.gmtDate = gmtDate;
-	}
-	public long[] getPstTimestamp() {
-		return pstTimestamp;
-	}
-	public void setPstTimestamp(long[] pstTimestamp) {
-		this.pstTimestamp = pstTimestamp;
-	}
-	public long[] getEstTimestamp() {
-		return estTimestamp;
-	}
-	public void setEstTimestamp(long[] estTimestamp) {
-		this.estTimestamp = estTimestamp;
-	}
-	public long[] getGmtTimestamp() {
-		return gmtTimestamp;
-	}
-	public void setGmtTimestamp(long[] gmtTimestamp) {
-		this.gmtTimestamp = gmtTimestamp;
+
+	public void setVectors(String[] vectors) {
+		this.vectors = vectors;
 	}
 	
-	private byte [][] getByteArray (String [] data) {
-		byte [][] byteArr = new byte [data.length][];
-		
-		for (int i = 0; i < data.length; i++) {
-			byteArr [i] = data [i].getBytes(StandardCharsets.UTF_8);
-		}
-		return 	byteArr;	
-    }
-	private byte [][] getHexToByteArray (String [] data){
-		byte [][] byteArr = new byte [data.length][];
-		
-		for (int i = 0; i < data.length; i++) {
-			byteArr [i] = hexStringToByteArray(data [i]);
-		}
-		return 	byteArr;
+	public int getRowCount() {
+		return rowCount;
 	}
-	private static byte[] hexStringToByteArray(String s) {
+
+	public void setRowCount(int rowCount) {
+		this.rowCount = rowCount;
+	}
+
+	private byte[][] getByteArray(String[] data) {
+        byte[][] byteArr = new byte[data.length][];
+
+        for (int i = 0; i < data.length; i++) {
+            byteArr[i] = data[i].getBytes(StandardCharsets.UTF_8);
+        }
+        return byteArr;
+    }
+
+    private byte[][] getHexToByteArray(String[] data) {
+        byte[][] byteArr = new byte[data.length][];
+
+        for (int i = 0; i < data.length; i++) {
+            byteArr[i] = hexStringToByteArray(data[i]);
+        }
+        return byteArr;
+    }
+
+    private static byte[] hexStringToByteArray(String s) {
         int len = s.length();
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i+1), 16));
+                    + Character.digit(s.charAt(i + 1), 16));
         }
         return data;
     }
