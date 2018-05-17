d5e5e5dbeb9fb23e895b9ac654fafb764a164c0b
Peter Schafhalter <pschafhalter@berkeley.edu>
2018-05-16 17:00:11 -0700

    Fix python2 integer serialization bug

--- cpp/src/arrow/python/arrow_to_python.cc
+++ cpp/src/arrow/python/arrow_to_python.cc
@@ -112,9 +112,15 @@ Status GetValue(PyObject* context, const UnionArray& parent, const Array& arr,
     case Type::BOOL:
       *result = PyBool_FromLong(checked_cast<const BooleanArray&>(arr).Value(index));
       return Status::OK();
-    case Type::INT64:
-      *result = PyLong_FromSsize_t(checked_cast<const Int64Array&>(arr).Value(index));
+    case Type::INT64: {
+      const std::string& child_name = parent.type()->child(type)->name();
+      if (child_name == "py2_int") {
+        *result = PyInt_FromSsize_t(checked_cast<const Int64Array&>(arr).Value(index));
+      } else {
+        *result = PyLong_FromSsize_t(checked_cast<const Int64Array&>(arr).Value(index));
+      }
       return Status::OK();
+    }
     case Type::BINARY: {
       int32_t nchars;
       const uint8_t* str = checked_cast<const BinaryArray&>(arr).GetValue(index, &nchars);
