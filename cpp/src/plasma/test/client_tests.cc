5e1d82b7d3b5321f3493515960d87998efff928d
Philipp Moritz <pcmoritz@gmail.com>
2018-05-17 18:13:50 -0700

    add test

--- cpp/src/plasma/test/client_tests.cc
+++ cpp/src/plasma/test/client_tests.cc
@@ -430,6 +430,11 @@ TEST_F(TestPlasmaStore, ManyObjectTest) {
   }
 }
 
+TEST_F(TestPlasmaStore, DeprecatedApiTest) {
+  int64_t default_delay = PLASMA_DEFAULT_RELEASE_DELAY;
+  ARROW_CHECK(default_delay == plasma::kPlasmaDefaultReleaseDelay);
+}
+
 #ifdef PLASMA_GPU
 using arrow::gpu::CudaBuffer;
 using arrow::gpu::CudaBufferReader;
