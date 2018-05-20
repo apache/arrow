d381ff31f59bff3ddf2c5dae77f5e56d7f09f2bb
Philipp Moritz <pcmoritz@gmail.com>
2018-05-20 10:55:42 -0700

    fixes

--- python/pyarrow/tests/test_plasma_tf_op.py
+++ python/pyarrow/tests/test_plasma_tf_op.py
@@ -37,18 +37,16 @@ def test_plasma_tf_op(use_gpu=False):
         object_id = np.random.bytes(20)
 
         data = np.random.randn(3, 244, 244).astype(np.float32)
-        kOnes = np.ones((3, 244, 244)).astype(np.float32)
+        ones = np.ones((3, 244, 244)).astype(np.float32)
 
         sess = tf.Session(config=tf.ConfigProto(
             allow_soft_placement=True, log_device_placement=True))
 
         def ToPlasma():
-            data_t = tf.constant(data)
-            data_t = tf.Print(data_t, [data_t], "data_t = ")
-            kOnes_t = tf.constant(kOnes)
-            kOnes_t = tf.Print(kOnes_t, [kOnes_t], "kOnes_t = ")
+            data_tensor = tf.constant(data)
+            ones_tensor = tf.constant(ones)
             return plasma.tf_plasma_op.tensor_to_plasma(
-                [data_t, kOnes_t],
+                [data_tensor, ones_tensor],
                 object_id,
                 plasma_store_socket_name=plasma_store_name,
                 plasma_manager_socket_name="")
@@ -66,23 +64,15 @@ def test_plasma_tf_op(use_gpu=False):
         z = from_plasma + 1
 
         sess.run(to_plasma)
-        print('Getting object...')
         # NOTE(zongheng): currently it returns a flat 1D tensor.
         # So reshape manually.
         out = sess.run(from_plasma)
-        # print('out.shape: %s' % out.shape)
-        # print('out: %s' % out)
 
         out = np.split(out, 2)
         out0 = out[0].reshape(3, 244, 244)
         out1 = out[1].reshape(3, 244, 244)
 
-        # print('data: %s' % data)
-        # print('out0: %s' % out0)
-        # print('out1: %s' % out1)
-
         sess.run(z)
 
         assert np.array_equal(data, out0), "Data not equal!"
-        assert np.array_equal(kOnes, out1), "Data not equal!"
-        print('OK: data all equal')
+        assert np.array_equal(ones, out1), "Data not equal!"
