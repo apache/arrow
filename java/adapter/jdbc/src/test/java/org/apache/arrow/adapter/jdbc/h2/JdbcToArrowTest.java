f685b932acbae78edd4e99b1ffd898bf704508f5
yashpal <yashpal.thakur@xoriant.com>
2018-05-22 07:42:48 -0400

    Files committed Charset and Resultset based testcases

--- java/adapter/jdbc/src/test/java/org/apache/arrow/adapter/jdbc/h2/JdbcToArrowTest.java
+++ java/adapter/jdbc/src/test/java/org/apache/arrow/adapter/jdbc/h2/JdbcToArrowTest.java
@@ -18,8 +18,7 @@
 
 package org.apache.arrow.adapter.jdbc.h2;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.apache.arrow.adapter.jdbc.AbstractJdbcToArrowTest;
 import org.apache.arrow.adapter.jdbc.JdbcToArrow;
 import org.apache.arrow.adapter.jdbc.Table;
 import org.apache.arrow.memory.RootAllocator;
@@ -37,17 +36,12 @@ import org.apache.arrow.vector.TimeStampVector;
 import org.apache.arrow.vector.TinyIntVector;
 import org.apache.arrow.vector.VarBinaryVector;
 import org.apache.arrow.vector.VarCharVector;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
@@ -68,17 +62,20 @@ import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.assertTimeVect
 import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.assertTinyIntVectorValues;
 import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.assertVarBinaryVectorValues;
 import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.assertVarcharVectorValues;
-import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.hexStringToByteArray;
-
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getLongValues;
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getIntValues;
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getBooleanValues;
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getDecimalValues;
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getDoubleValues;
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getFloatValues;
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getCharArray;
+import static org.apache.arrow.adapter.jdbc.JdbcToArrowTestHelper.getBinaryValues;
 
 /**
  *
  */
 @RunWith(Parameterized.class)
-public class JdbcToArrowTest {
-
-	private Connection conn = null;
-	private Table table;
+public class JdbcToArrowTest extends AbstractJdbcToArrowTest {
 	
     private static final String BIGINT = "BIGINT_FIELD5";
     private static final String BINARY = "BINARY_FIELD12";
@@ -107,48 +104,6 @@ public class JdbcToArrowTest {
     public JdbcToArrowTest(Table table) {
         this.table = table;
     }
-     
-    /**
-     * This method creates Table object after reading YAML file
-     * @param ymlFilePath
-     * @return
-     * @throws IOException
-     */
-    private static Table getTable(String ymlFilePath) throws IOException {
-        return new ObjectMapper(new YAMLFactory()).readValue(
-                JdbcToArrowDataTypesTest.class.getClassLoader().getResourceAsStream(ymlFilePath), Table.class);
-    }
-    
-    /**
-     * This method creates Connection object and DB table and also populate data into table for test
-     * @throws SQLException
-     * @throws ClassNotFoundException
-     */
-    @Before
-    public void setUp() throws SQLException, ClassNotFoundException {
-        String url = "jdbc:h2:mem:JdbcToArrowTest";
-        String driver = "org.h2.Driver";
-        Class.forName(driver);
-        conn = DriverManager.getConnection(url);
-        try (Statement stmt = conn.createStatement();) {
-            stmt.executeUpdate(table.getCreate());
-            for (String insert : table.getData()) {
-                stmt.executeUpdate(insert);
-            }
-        }
-    }
-    
-    /**
-     * Clean up method to close connection after test completes
-     * @throws SQLException
-     */
-    @After
-    public void destroy() throws SQLException {
-        if (conn != null) {
-            conn.close();
-            conn = null;
-        }
-    }
     
   /**
    * This method returns collection of Table object for each test iteration
@@ -159,12 +114,7 @@ public class JdbcToArrowTest {
    */
     @Parameters
     public static Collection<Object[]> getTestData() throws SQLException, ClassNotFoundException, IOException {
-      	Object[][] tableArr = new Object[testFiles.length][];
-        int i = 0;
-        for (String testFile: testFiles) {
-        	tableArr[i++] = new Object[]{getTable(testFile)};
-        }
-        return Arrays.asList(tableArr);
+        return Arrays.asList(prepareTestData(testFiles, JdbcToArrowTest.class));
     }
     
     /**
@@ -177,145 +127,93 @@ public class JdbcToArrowTest {
         try (VectorSchemaRoot root = JdbcToArrow.sqlToArrow(conn, table.getQuery(),
                 new RootAllocator(Integer.MAX_VALUE), Calendar.getInstance())) {
         	
-        	assertBigIntVectorValues((BigIntVector) root.getVector(BIGINT), table.getRowCount(),
-        			getLongValues(table.getValues(), BIGINT));
-        	  
-        	assertTinyIntVectorValues((TinyIntVector) root.getVector(TINYINT), table.getRowCount(),
-        			getIntValues(table.getValues(), TINYINT));
-        	
-        	assertSmallIntVectorValues((SmallIntVector) root.getVector(SMALLINT), table.getRowCount(),
-        			getIntValues(table.getValues(), SMALLINT));
-        	
-        	assertVarBinaryVectorValues((VarBinaryVector) root.getVector(BINARY), table.getRowCount(),
-        			getBinaryValues(table.getValues(), BINARY));
-        	
-        	assertVarBinaryVectorValues((VarBinaryVector) root.getVector(BLOB), table.getRowCount(),
-        			getBinaryValues(table.getValues(), BLOB));
-        	    
-        	assertVarcharVectorValues((VarCharVector) root.getVector(CLOB), table.getRowCount(),
-        			getCharArrays(table.getValues(), CLOB));
-
-        	assertVarcharVectorValues((VarCharVector) root.getVector(VARCHAR), table.getRowCount(),
-        			getCharArrays(table.getValues(), VARCHAR));
-        	
-        	assertVarcharVectorValues((VarCharVector) root.getVector(CHAR), table.getRowCount(),
-        			getCharArrays(table.getValues(), CHAR));
-        	
-        	assertIntVectorValues((IntVector) root.getVector(INT), table.getRowCount(),
-        			getIntValues(table.getValues(), INT));
-        	
-        	assertBitVectorValues((BitVector) root.getVector( BIT), table.getRowCount(),
-        			getIntValues(table.getValues(), BIT));
-        	
-            assertBooleanVectorValues((BitVector) root.getVector(BOOL), table.getRowCount(),
-            		getBooleanValues(table.getValues(), BOOL));
-              
-            assertDateVectorValues((DateMilliVector) root.getVector(DATE), table.getRowCount(),
-            		getLongValues(table.getValues(), DATE));
-            
-            assertTimeVectorValues((TimeMilliVector) root.getVector(TIME), table.getRowCount(),
-            		getLongValues(table.getValues(), TIME));
-            
-            assertTimeStampVectorValues((TimeStampVector) root.getVector(TIMESTAMP), table.getRowCount(),
-            		getLongValues(table.getValues(), TIMESTAMP));
-
-            assertDecimalVectorValues((DecimalVector) root.getVector(DECIMAL), table.getRowCount(),
-            		getDecimalValues(table.getValues(), DECIMAL));
-
-            assertFloat8VectorValues((Float8Vector) root.getVector(DOUBLE), table.getRowCount(),
-            		getDoubleValues(table.getValues(), DOUBLE));
-
-            assertFloat4VectorValues((Float4Vector) root.getVector(REAL), table.getRowCount(),
-            		getFloatValues(table.getValues(), REAL));
+        	testDataSets(root);
+        }
+    }
+    /**
+     * This method tests various datatypes converted into Arrow vector for H2 database using ResultSet
+     * @throws SQLException
+     * @throws IOException
+     */
+    @Test
+    public void testDBValuesUsingResultSet() throws SQLException, IOException {
+        try (Statement stmt = conn.createStatement();
+        		VectorSchemaRoot root = JdbcToArrow.sqlToArrow(stmt.executeQuery(table.getQuery()),
+        				Calendar.getInstance())) {
+        	testDataSets(root);
         }
     }
     
-    private Integer [] getIntValues(String[] values, String dataType) {
-    	String[] dataArr= getValues(values, dataType);
-    	Integer [] valueArr = new Integer [dataArr.length];
-    	int i =0;
-    	for(String data : dataArr) {
-    		valueArr [i++] = Integer.parseInt(data);
-    	}
-    	return valueArr;
+    /**
+     * This method tests various datatypes converted into Arrow vector for H2 database using ResultSet And Allocator
+     * @throws SQLException
+     * @throws IOException
+     */
+    @Test
+    public void testDBValuesUsingResultSetAndAllocator() throws SQLException, IOException {
+        try (Statement stmt = conn.createStatement();
+        		VectorSchemaRoot root = JdbcToArrow.sqlToArrow(stmt.executeQuery(table.getQuery()),
+        				new RootAllocator(Integer.MAX_VALUE), Calendar.getInstance())) {
+        	
+        	testDataSets(root);
+        }
     }
     
-    private Boolean [] getBooleanValues(String[] values, String dataType) {
-    	String[] dataArr= getValues(values, dataType);
-    	Boolean [] valueArr = new Boolean [dataArr.length];
-    	int i =0;
-    	for(String data : dataArr) { 
-    		valueArr [i++] = data.trim().equals("1");
-    	}
-    	return valueArr;
-    }
-    
-    private BigDecimal [] getDecimalValues(String[] values, String dataType) {
-    	String[] dataArr= getValues(values, dataType);
-    	BigDecimal [] valueArr = new BigDecimal [dataArr.length];
-    	int i =0;
-    	for(String data : dataArr) {
-    		valueArr[i++] = new BigDecimal(data);
-    	}
-    	return valueArr;
-    }
+    /**
+     * This method calls the assert methods for various DataSets
+     * @param root
+     */
+    public void testDataSets(VectorSchemaRoot root) {
+    	assertBigIntVectorValues((BigIntVector) root.getVector(BIGINT), table.getRowCount(),
+    			getLongValues(table.getValues(), BIGINT));
+    	  
+    	assertTinyIntVectorValues((TinyIntVector) root.getVector(TINYINT), table.getRowCount(),
+    			getIntValues(table.getValues(), TINYINT));
+    	
+    	assertSmallIntVectorValues((SmallIntVector) root.getVector(SMALLINT), table.getRowCount(),
+    			getIntValues(table.getValues(), SMALLINT));
+    	
+    	assertVarBinaryVectorValues((VarBinaryVector) root.getVector(BINARY), table.getRowCount(),
+    			getBinaryValues(table.getValues(), BINARY));
+    	
+    	assertVarBinaryVectorValues((VarBinaryVector) root.getVector(BLOB), table.getRowCount(),
+    			getBinaryValues(table.getValues(), BLOB));
+    	    
+    	assertVarcharVectorValues((VarCharVector) root.getVector(CLOB), table.getRowCount(),
+    			getCharArray(table.getValues(), CLOB));
 
-    private Double [] getDoubleValues(String[] values, String dataType) {
-    	String[] dataArr= getValues(values, dataType);
-    	Double [] valueArr = new Double [dataArr.length];
-    	int i =0;
-    	for(String data : dataArr) {
-    		valueArr [i++] = Double.parseDouble(data);
-    	}
-    	return valueArr;
-    }  
-    private Float [] getFloatValues(String[] values, String dataType) { 
-    	String[] dataArr= getValues(values, dataType);
-    	Float [] valueArr = new Float [dataArr.length];
-    	int i =0;
-    	for(String data : dataArr) {
-    		valueArr [i++] = Float.parseFloat(data);
-    	}
-    	return valueArr;
-    } 
-    private Long [] getLongValues(String[] values, String dataType) {
-    	String[] dataArr= getValues(values, dataType);
-    	Long [] valueArr = new Long [dataArr.length];
-    	int i =0;
-    	for(String data : dataArr) {    
-    		valueArr [i++] = Long.parseLong(data);
-    	}
-    	return valueArr;
+    	assertVarcharVectorValues((VarCharVector) root.getVector(VARCHAR), table.getRowCount(),
+    			getCharArray(table.getValues(), VARCHAR));
+    	
+    	assertVarcharVectorValues((VarCharVector) root.getVector(CHAR), table.getRowCount(),
+    			getCharArray(table.getValues(), CHAR));
+    	
+    	assertIntVectorValues((IntVector) root.getVector(INT), table.getRowCount(),
+    			getIntValues(table.getValues(), INT));
+    	
+    	assertBitVectorValues((BitVector) root.getVector( BIT), table.getRowCount(),
+    			getIntValues(table.getValues(), BIT));
+    	
+        assertBooleanVectorValues((BitVector) root.getVector(BOOL), table.getRowCount(),
+        		getBooleanValues(table.getValues(), BOOL));
+          
+        assertDateVectorValues((DateMilliVector) root.getVector(DATE), table.getRowCount(),
+        		getLongValues(table.getValues(), DATE));
+        
+        assertTimeVectorValues((TimeMilliVector) root.getVector(TIME), table.getRowCount(),
+        		getLongValues(table.getValues(), TIME));
+        
+        assertTimeStampVectorValues((TimeStampVector) root.getVector(TIMESTAMP), table.getRowCount(),
+        		getLongValues(table.getValues(), TIMESTAMP));
+
+        assertDecimalVectorValues((DecimalVector) root.getVector(DECIMAL), table.getRowCount(),
+        		getDecimalValues(table.getValues(), DECIMAL));
+
+        assertFloat8VectorValues((Float8Vector) root.getVector(DOUBLE), table.getRowCount(),
+        		getDoubleValues(table.getValues(), DOUBLE));
+
+        assertFloat4VectorValues((Float4Vector) root.getVector(REAL), table.getRowCount(),
+        		getFloatValues(table.getValues(), REAL));
     }
     
-    private byte [][] getCharArrays(String[] values, String dataType) {
-    	String[] dataArr= getValues(values, dataType);
-    	byte [][] valueArr = new byte [dataArr.length][];
-    	int i =0;
-    	for(String data : dataArr) {     
-    		valueArr [i++] = data.trim().getBytes();
-    	}
-    	return valueArr;
-    }   
-    
-    private byte [][] getBinaryValues(String[] values, String dataType) {
-    	String[] dataArr= getValues(values, dataType);
-    	byte [][] valueArr = new byte [dataArr.length][];
-    	int i =0;
-    	for(String data : dataArr) {     
-    		valueArr [i++] = hexStringToByteArray(data.trim());
-    	}
-    	return valueArr;
-    }  
-    
-    private String [] getValues(String[] values, String dataType) {
-    	String value = "";
-    	for(String val : values) {
-    		if(val.startsWith(dataType)) {
-    			value = val.split("=")[1];
-    			break;
-    		}
-    	}
-    	return value.split(",");
-    }
 }
