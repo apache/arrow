9f3c1b3e5e9f6723cdbeabc7608c616433161d88
Wes McKinney <wesm+git@apache.org>
2018-06-12 16:31:07 -0400

    Add Boost 1.67 and 1.68 as recognized versions

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -137,6 +137,8 @@ if (MSVC AND ARROW_USE_STATIC_CRT)
   set(Boost_USE_STATIC_RUNTIME ON)
 endif()
 set(Boost_ADDITIONAL_VERSIONS
+  "1.68.0" "1.68"
+  "1.67.0" "1.67"
   "1.66.0" "1.66"
   "1.65.0" "1.65"
   "1.64.0" "1.64"
