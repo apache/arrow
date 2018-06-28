43d097b4e6fcd8bb9e6da6eb3b52f91a0bda8215
Robert Gruener <robbieg@uber.com>
2018-06-28 12:40:25 -0400

    fix indentation of unit test

--- python/pyarrow/tests/test_parquet.py
+++ python/pyarrow/tests/test_parquet.py
@@ -1183,6 +1183,43 @@ def test_inclusive_integer(tmpdir):
 
 
 @parquet
+def test_inclusive_set(tmpdir):
+    fs = LocalFileSystem.get_instance()
+    base_path = str(tmpdir)
+
+    import pyarrow.parquet as pq
+
+    integer_keys = [0, 1]
+    string_keys = ['a', 'b', 'c']
+    boolean_keys = [True, False]
+    partition_spec = [
+        ['integer', integer_keys],
+        ['string', string_keys],
+        ['boolean', boolean_keys]
+    ]
+
+    df = pd.DataFrame({
+        'integer': np.array(integer_keys, dtype='i4').repeat(15),
+        'string': np.tile(np.tile(np.array(string_keys, dtype=object), 5), 2),
+        'boolean': np.tile(np.tile(np.array(boolean_keys, dtype='bool'), 5),
+                           3),
+    }, columns=['integer', 'string', 'boolean'])
+
+    _generate_partition_directories(fs, base_path, partition_spec, df)
+
+    dataset = pq.ParquetDataset(
+        base_path, filesystem=fs,
+        filters=[('integer', 'in', {1}), ('string', 'in', {'a', 'b'}),
+                 ('boolean', 'in', {True})]
+    )
+    table = dataset.read()
+    result_df = (table.to_pandas().reset_index(drop=True))
+
+    assert 0 not in result_df['integer'].values
+    assert 'c' not in result_df['string'].values
+    assert False not in result_df['boolean'].values
+
+@parquet
 def test_invalid_pred_op(tmpdir):
     fs = LocalFileSystem.get_instance()
     base_path = str(tmpdir)
