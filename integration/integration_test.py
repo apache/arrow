0f0a607533bfd94e6c64b2aa0068c6258b075be7
Bryan Cutler <cutlerb@gmail.com>
2018-07-03 16:36:22 -0700

    revert accidental change to integration tests made during testing

--- integration/integration_test.py
+++ integration/integration_test.py
@@ -1150,8 +1150,7 @@ def get_static_json_files():
 
 
 def run_all_tests(debug=False):
-    #testers = [CPPTester(debug=debug), JavaTester(debug=debug), JSTester(debug=debug)]
-    testers = [JavaTester(debug=debug)]
+    testers = [CPPTester(debug=debug), JavaTester(debug=debug), JSTester(debug=debug)]
     static_json_files = get_static_json_files()
     generated_json_files = get_generated_json_files()
     json_files = static_json_files + generated_json_files
