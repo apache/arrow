1135b51b090de26afb5dbc0067fa92bb0fde2351
Philipp Moritz <pcmoritz@gmail.com>
2018-07-02 17:46:43 -0700

    silence tensorflow installation

--- ci/travis_script_manylinux.sh
+++ ci/travis_script_manylinux.sh
@@ -39,6 +39,6 @@ CONDA_ENV_DIR=$TRAVIS_BUILD_DIR/pyarrow-test-$PYTHON_VERSION
 conda create -y -q -p $CONDA_ENV_DIR python=$PYTHON_VERSION
 source activate $CONDA_ENV_DIR
 
-pip install tensorflow
+pip install -q tensorflow
 pip install "dist/`ls dist/ | grep cp36`"
 python -c "import pyarrow; import tensorflow"
