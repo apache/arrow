d8d5f93f120cc7b52034f1feac17beed914770f1
songqing <zhangsongqing164@163.com>
2018-07-06 13:35:30 +0800

    lint fix

--- cpp/src/plasma/common.cc
+++ cpp/src/plasma/common.cc
@@ -70,14 +70,14 @@ std::string UniqueID::hex() const {
 
 // This code is from https://sites.google.com/site/murmurhash/
 // and is public domain.
-uint64_t MurmurHash64A(const void *key, int len, unsigned int seed) {
+uint64_t MurmurHash64A(const void* key, int len, unsigned int seed) {
   const uint64_t m = 0xc6a4a7935bd1e995;
   const int r = 47;
 
   uint64_t h = seed ^ (len * m);
 
-  const uint64_t *data = reinterpret_cast<const uint64_t *>(key);
-  const uint64_t *end = data + (len / 8);
+  const uint64_t* data = reinterpret_cast<const uint64_t*>(key);
+  const uint64_t* end = data + (len / 8);
 
   while (data != end) {
     uint64_t k = *data++;
@@ -90,24 +90,24 @@ uint64_t MurmurHash64A(const void *key, int len, unsigned int seed) {
     h *= m;
   }
 
-  const unsigned char *data2 = reinterpret_cast<const unsigned char *>(data);
+  const unsigned char* data2 = reinterpret_cast<const unsigned char*>(data);
 
   switch (len & 7) {
-  case 7:
-    h ^= uint64_t(data2[6]) << 48;
-  case 6:
-    h ^= uint64_t(data2[5]) << 40;
-  case 5:
-    h ^= uint64_t(data2[4]) << 32;
-  case 4:
-    h ^= uint64_t(data2[3]) << 24;
-  case 3:
-    h ^= uint64_t(data2[2]) << 16;
-  case 2:
-    h ^= uint64_t(data2[1]) << 8;
-  case 1:
-    h ^= uint64_t(data2[0]);
-    h *= m;
+    case 7:
+      h ^= uint64_t(data2[6]) << 48;
+    case 6:
+      h ^= uint64_t(data2[5]) << 40;
+    case 5:
+      h ^= uint64_t(data2[4]) << 32;
+    case 4:
+      h ^= uint64_t(data2[3]) << 24;
+    case 3:
+      h ^= uint64_t(data2[2]) << 16;
+    case 2:
+      h ^= uint64_t(data2[1]) << 8;
+    case 1:
+      h ^= uint64_t(data2[0]);
+      h *= m;
   }
 
   h ^= h >> r;
