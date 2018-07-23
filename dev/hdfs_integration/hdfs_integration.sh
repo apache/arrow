eaa60538ae776a0e4c984e6c661ba3f64def98ab
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-07-23 19:30:21 -0400

    ARROW-2300: [C++/Python] Integration test for HDFS
    Author: Krisztián Szűcs <szucs.krisztian@gmail.com>
    Author: Wes McKinney <wesm+git@apache.org>
    
    Closes #1889 from kszucs/ARROW-2300 and squashes the following commits:
    
    6dbbfb52 <Wes McKinney> Apply Krisztian's errno 2 fix, nicer formatting for libhdfs errors
    3f95b226 <Wes McKinney> More robust to state of local dev area
    5d3e6ff6 <Wes McKinney> Give HDFS integration C++ build a distinct subdir name
    78857353 <Wes McKinney> Remove python/testing/*hdfs*, add instructions to dev/README.md for running hdfs tests
    3f8318e1 <Krisztián Szűcs> display errno in error msg; add pkgconfig; export hadoop env vars
    cde939a9 <Krisztián Szűcs> fix path_or_paths checking in _make_manifest
    c910538f <Krisztián Szűcs> use filesystem.read_parquet for both directory and single file path
    7e3c0332 <Krisztián Szűcs> deepend on hadoop image to have libhdfs preinstalled
    891ea36c <Krisztián Szűcs> configure libhdfs3
    2eb227f9 <Krisztián Szűcs> install pinned libhdfs3; compile successfully
    bcbd1f35 <Krisztián Szűcs> dockerfile format
    25becee8 <Krisztián Szűcs> build with newer gcc and ninja
    6cbfdad6 <Krisztián Szűcs> remove conflict garbage
    e08d9890 <Krisztián Szűcs> ccache
    e8cb7dca <Krisztián Szűcs> reproduced segfault
    3a840e8d <Krisztián Szűcs> ARROW-2300:  Integration test for HDFS

---
+++ dev/hdfs_integration/hdfs_integration.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Exit on any error
+set -e
+
+# cwd is mounted from host machine to
+# and contains both arrow and parquet-cpp
+
+# Activate conda environment
+source activate pyarrow-dev
+
+# Arrow build variables
+export ARROW_BUILD_TYPE=debug
+export ARROW_BUILD_TOOLCHAIN=$CONDA_PREFIX
+export PARQUET_BUILD_TOOLCHAIN=$CONDA_PREFIX
+export ARROW_HOME=$CONDA_PREFIX
+export PARQUET_HOME=$CONDA_PREFIX
+
+# Hadoop variables
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HADOOP_HOME/lib/native/
+export CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath --glob`
+
+# For newer GCC per https://arrow.apache.org/docs/python/development.html#known-issues
+export CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"
+export PYARROW_CXXFLAGS=$CXXFLAGS
+export PYARROW_CMAKE_GENERATOR=Ninja
+
+# Install arrow-cpp
+mkdir -p arrow/cpp/hdfs-integration-build
+pushd arrow/cpp/hdfs-integration-build
+
+cmake -GNinja \
+      -DCMAKE_BUILD_TYPE=$ARROW_BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
+      -DARROW_PYTHON=ON \
+      -DARROW_PLASMA=ON \
+      -DARROW_HDFS=ON \
+      -DARROW_BUILD_TESTS=ON \
+      -DCMAKE_CXX_FLAGS=$CXXFLAGS \
+      ..
+ninja
+ninja install
+
+popd
+
+# Install parquet-cpp
+mkdir -p parquet-cpp/hdfs-integration-build
+pushd parquet-cpp/hdfs-integration-build
+
+cmake -GNinja \
+      -DCMAKE_BUILD_TYPE=$ARROW_BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$PARQUET_HOME \
+      -DPARQUET_BUILD_BENCHMARKS=OFF \
+      -DPARQUET_BUILD_EXECUTABLES=OFF \
+      -DPARQUET_BUILD_TESTS=ON \
+      -DCMAKE_CXX_FLAGS=$CXXFLAGS \
+      ..
+ninja
+ninja install
+
+popd
+
+# Install pyarrow
+pushd arrow/python
+
+# Clear the build directory so we are guaranteed a fresh set of extensions
+rm -rf build/
+
+python setup.py build_ext \
+    --build-type=$ARROW_BUILD_TYPE \
+    --with-parquet \
+    --with-plasma \
+    --inplace
+
+popd
+
+# Run tests
+export LIBHDFS3_CONF=arrow/dev/hdfs_integration/libhdfs3-client-config.xml
+
+# Python
+python -m pytest -vv -r sxX -s arrow/python/pyarrow \
+       --only-parquet --only-hdfs
+
+# C++
+arrow/cpp/hdfs-integration-build/debug/io-hdfs-test
