cde939a9ae1e6f4850ffe932be5b5f4e310efb38
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-07-23 11:55:30 +0200

    fix path_or_paths checking in _make_manifest

--- python/pyarrow/parquet.py
+++ python/pyarrow/parquet.py
@@ -944,7 +944,7 @@ def _make_manifest(path_or_paths, fs, pathsep='/'):
     common_metadata_path = None
     metadata_path = None
 
-    if len(path_or_paths) == 1:
+    if isinstance(path_or_paths, list) and len(path_or_paths) == 1:
         # Dask passes a directory as a list of length 1
         path_or_paths = path_or_paths[0]
 
