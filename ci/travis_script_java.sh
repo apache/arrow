d9d1f6b695a0691b4d53669db28bf2a63785119e
Antoine Pitrou <antoine@python.org>
2018-08-06 19:10:08 +0200

    ARROW-2815: [CI] Skip Java tests and style checks on C++ job [skip appveyor]
    This omits all warning and debug logs from previous Maven output.
    
    Author: Antoine Pitrou <antoine@python.org>
    
    Closes #2378 from pitrou/ARROW-2815-strip-java-logging and squashes the following commits:
    
    603db64 <Antoine Pitrou> ARROW-2815:  Skip Java tests and style checks on C++ job

--- ci/travis_script_java.sh
+++ ci/travis_script_java.sh
@@ -24,6 +24,11 @@ JAVA_DIR=${TRAVIS_BUILD_DIR}/java
 pushd $JAVA_DIR
 
 export MAVEN_OPTS="$MAVEN_OPTS -Dorg.slf4j.simpleLogger.defaultLogLevel=warn"
-mvn -B install
+if [ $ARROW_TRAVIS_JAVA_BUILD_ONLY == "1" ]; then
+    # Save time and make build less verbose by skipping tests and style checks
+    mvn -DskipTests=true -Dcheckstyle.skip=true -B install
+else
+    mvn -B install
+fi
 
 popd
