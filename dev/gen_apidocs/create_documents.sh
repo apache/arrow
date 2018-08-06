53cf1039c037718f773743766bc081547a517b5c
Li Jin <ice.xelloss@gmail.com>
2018-08-06 22:12:40 +0000

    Enable Java doc

--- dev/gen_apidocs/create_documents.sh
+++ dev/gen_apidocs/create_documents.sh
@@ -34,9 +34,9 @@ export CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"
 # Make Java documentation
 # Override user.home to cache dependencies outside the Docker container
 pushd arrow/java
-#mvn -Duser.home=`pwd`/.apidocs-m2 -Drat.skip=true -Dcheckstyle.skip=true install site
-#mkdir -p ../site/asf-site/docs/java/
-#rsync -r target/site/apidocs/ ../site/asf-site/docs/java/
+mvn -Duser.home=`pwd`/.apidocs-m2 -Drat.skip=true -Dcheckstyle.skip=true install site
+mkdir -p ../site/asf-site/docs/java/
+rsync -r target/site/apidocs/ ../site/asf-site/docs/java/
 popd
 
 # Make Javascript documentation
