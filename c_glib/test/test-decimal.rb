b401b7478e82341231d15b6c98406a4185384055
Kouhei Sutou <kou@clear-code.com>
2018-08-11 18:11:57 +0900

    [GLib] Use more meaningful name

--- c_glib/test/test-decimal.rb
+++ c_glib/test/test-decimal.rb
@@ -88,9 +88,9 @@ class TestDecimal128 < Test::Unit::TestCase
     integer_data2 = 5443
     decimal1 = Arrow::Decimal128.new(integer_data1)
     decimal2 = Arrow::Decimal128.new(integer_data2)
-    decimal3 = decimal1.divide(decimal2)
-    assert_equal(integer_data1 / integer_data2, decimal3[0].to_i)
-    assert_equal(integer_data1 % integer_data2, decimal3[1].to_i)
+    result, remainder = decimal1.divide(decimal2)
+    assert_equal(integer_data1 / integer_data2, result.to_i)
+    assert_equal(integer_data1 % integer_data2, remainder.to_i)
   end
 
   def test_divide_zero
