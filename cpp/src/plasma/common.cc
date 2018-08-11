2cb3c0fb4dae2df8c4ad70e857f51a89886f11bf
Philipp Moritz <pcmoritz@gmail.com>
2018-08-10 17:46:46 -0700

    fix

--- cpp/src/plasma/common.cc
+++ cpp/src/plasma/common.cc
@@ -36,7 +36,7 @@ std::mt19937 RandomlySeededMersenneTwister() {
   // (RDRND) which crashes the valgrind shipped with Ubuntu 16.04.
   std::random_device urandom("/dev/urandom");
   std::vector<std::mt19937::result_type> seeds;
-  for (int i = 0; i < std::mt19937::state_size; ++i) {
+  for (size_t i = 0; i < std::mt19937::state_size; ++i) {
     seeds.push_back(urandom());
   }
   std::seed_seq seq(seeds.begin(), seeds.end());
