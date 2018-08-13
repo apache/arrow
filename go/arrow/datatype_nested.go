2dfb01fd8632618315e2d9e215b5cfafb0cdd1a8
Sebastien Binet <binet@cern.ch>
2018-08-13 19:59:57 +0200

    ARROW-3022: [Go] add support for Struct arrays
    Author: Sebastien Binet <binet@cern.ch>
    
    Closes #2411 from sbinet/issue-3022 and squashes the following commits:
    
    101752a <Sebastien Binet> ARROW-3022:  add support for Struct arrays

--- go/arrow/datatype_nested.go
+++ go/arrow/datatype_nested.go
@@ -16,6 +16,8 @@
 
 package arrow
 
+import "fmt"
+
 // ListType describes a nested type in which each array slot contains
 // a variable-size sequence of values, all having the same relative type.
 type ListType struct {
@@ -39,6 +41,91 @@ func (*ListType) Name() string { return "list" }
 // Elem returns the ListType's element type.
 func (t *ListType) Elem() DataType { return t.elem }
 
+// StructType describes a nested type parameterized by an ordered sequence
+// of relative types, called its fields.
+type StructType struct {
+	fields []Field
+	index  map[string]int
+	meta   KeyValueMetadata
+}
+
+// StructOf returns the struct type with fields fs.
+//
+// StructOf panics if there are duplicated fields.
+// StructOf panics if there is a field with an invalid DataType.
+func StructOf(fs ...Field) *StructType {
+	n := len(fs)
+	if n == 0 {
+		return &StructType{}
+	}
+
+	t := &StructType{
+		fields: make([]Field, n),
+		index:  make(map[string]int, n),
+	}
+	for i, f := range fs {
+		if f.Type == nil {
+			panic("arrow: field with nil DataType")
+		}
+		t.fields[i] = Field{
+			Name:     f.Name,
+			Type:     f.Type,
+			Nullable: f.Nullable,
+			Metadata: f.Metadata.clone(),
+		}
+		if _, dup := t.index[f.Name]; dup {
+			panic(fmt.Errorf("arrow: duplicate field with name %q", f.Name))
+		}
+		t.index[f.Name] = i
+	}
+
+	return t
+}
+
+func (*StructType) ID() Type     { return STRUCT }
+func (*StructType) Name() string { return "struct" }
+
+func (t *StructType) Fields() []Field   { return t.fields }
+func (t *StructType) Field(i int) Field { return t.fields[i] }
+
+func (t *StructType) FieldByName(name string) (Field, bool) {
+	i, ok := t.index[name]
+	if !ok {
+		return Field{}, false
+	}
+	return t.fields[i], true
+}
+
+type Field struct {
+	Name     string           // Field name
+	Type     DataType         // The field's data type
+	Nullable bool             // Fields can be nullable
+	Metadata KeyValueMetadata // The field's metadata, if any
+}
+
+func (f Field) HasMetadata() bool { return len(f.Metadata.keys) != 0 }
+
+type KeyValueMetadata struct {
+	keys   []string
+	values []string
+}
+
+func (kv KeyValueMetadata) clone() KeyValueMetadata {
+	if len(kv.keys) == 0 {
+		return KeyValueMetadata{}
+	}
+
+	o := KeyValueMetadata{
+		keys:   make([]string, len(kv.keys)),
+		values: make([]string, len(kv.values)),
+	}
+	copy(o.keys, kv.keys)
+	copy(o.values, kv.values)
+
+	return o
+}
+
 var (
 	_ DataType = (*ListType)(nil)
+	_ DataType = (*StructType)(nil)
 )
