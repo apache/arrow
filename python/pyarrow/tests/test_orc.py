20c04051d1ad54121b28b1704aa49f3ae4b6e1d5
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-09-01 14:18:13 -0400

    ARROW-3153: [Packaging] Fix broken nightly package builds introduced with recent cmake changes and orc tests
    Don't merge yet, currently running the packaging tasks.
    
    - [x] passing [conda builds](https://github.com/kszucs/crossbow/branches/all?utf8=%E2%9C%93&query=build-303)
    - [x] passing [wheel builds](https://github.com/kszucs/crossbow/branches/all?utf8=%E2%9C%93&query=build-304)
    
    Author: Krisztián Szűcs <szucs.krisztian@gmail.com>
    
    Closes #2502 from kszucs/ARROW-3153 and squashes the following commits:
    
    a5869287 <Krisztián Szűcs> flake8
    b00cb97d <Krisztián Szűcs> update pandas test dependency in wheel builds; minor orc test cleanup
    c9d59991 <Krisztián Szűcs> add libprotobuf dependency

--- python/pyarrow/tests/test_orc.py
+++ python/pyarrow/tests/test_orc.py
@@ -17,7 +17,6 @@
 
 import datetime
 import decimal
-import gzip
 import os
 
 from pandas.util.testing import assert_frame_equal
@@ -120,7 +119,12 @@ def check_example_file(orc_path, expected_df, need_fix=False):
     assert json_pos == orc_file.nrows
 
 
-def check_example_using_json(example_name):
+@pytest.mark.parametrize('example_name', [
+    'TestOrcFile.test1',
+    'TestOrcFile.testDate1900',
+    'decimal'
+])
+def test_example_using_json(example_name):
     """
     Check a ORC file example against the equivalent JSON file, as given
     in the Apache ORC repository (the JSON file has one JSON object per
@@ -128,12 +132,7 @@ def check_example_using_json(example_name):
     """
     # Read JSON file
     json_path = path_for_json_example(example_name)
-    if json_path.endswith('.gz'):
-        f = gzip.open(json_path, 'r')
-    else:
-        f = open(json_path, 'r')
-
-    table = pd.read_json(f, lines=True)
+    table = pd.read_json(json_path, lines=True)
 
     check_example_file(path_for_orc_example(example_name), table,
                        need_fix=True)
@@ -174,20 +173,3 @@ def test_orcfile_empty():
             ]))),
         ])
     assert schema == expected_schema
-
-
-def test_orcfile_test1_json():
-    # Exercise the JSON test path
-    check_example_using_json('TestOrcFile.test1')
-
-
-def test_orcfile_test1_pickle():
-    check_example_using_json('TestOrcFile.test1')
-
-
-def test_orcfile_dates():
-    check_example_using_json('TestOrcFile.testDate1900')
-
-
-def test_orcfile_decimals():
-    check_example_using_json('decimal')
