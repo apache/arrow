80cbce34f93084a0f082728a8ab309eba88de2d3
Kevin Gurney <kevin.p.gurney@gmail.com>
2018-09-06 14:47:53 -0400

    Clean up and refactor tfeather.m. Add tfeathermex.m numeric nulls round trip test. Create test utilities directory.

--- matlab/test/tfeather.m
+++ matlab/test/tfeather.m
@@ -18,218 +18,213 @@ classdef tfeather < matlab.unittest.TestCase
     
     methods(TestClassSetup)
         
-        function addFeatherFunctionsToMATLABPath(testcase)
+        function addFeatherFunctionsToMATLABPath(testCase)
             import matlab.unittest.fixtures.PathFixture
+            % Add Feather test utilities to the MATLAB path.
+            testCase.applyFixture(PathFixture('util'));
             % Add featherread and featherwrite to the MATLAB path.
-            testcase.applyFixture(PathFixture(fullfile('..', 'src')));
+            testCase.applyFixture(PathFixture(fullfile('..', 'src')));
             % featherreadmex must be on the MATLAB path.
-            testcase.assertTrue(~isempty(which('featherreadmex')), ...
+            testCase.assertTrue(~isempty(which('featherreadmex')), ...
                 '''featherreadmex'' must be on the MATLAB path. Use ''addpath'' to add folders to the MATLAB path.');
             % featherwritemex must be on the MATLAB path.
-            testcase.assertTrue(~isempty(which('featherwritemex')), ...
+            testCase.assertTrue(~isempty(which('featherwritemex')), ...
                 '''featherwritemex'' must be on to the MATLAB path. Use ''addpath'' to add folders to the MATLAB path.');
+        end
+        
+    end
+    
+    methods(TestMethodSetup)
+    
+        function setupTempWorkingDirectory(testCase)
+            import matlab.unittest.fixtures.WorkingFolderFixture;
+            testCase.applyFixture(WorkingFolderFixture);
         end
         
     end
     
     methods(Test)
 
-        function NumericDatatypesNoNulls(testcase)
-            actTable=generatetable;
-            expectedTable=featherRoundtrip(actTable);
-            testcase.verifyEqual(actTable,expectedTable);
+        function NumericDatatypesNoNulls(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+            
+            actualTable = createTable;
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyEqual(actualTable, expectedTable);
         end
 
-        function NumericDatatypesWithNaNRow(testcase)
-            t = generatetable;
+        function NumericDatatypesWithNaNRow(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+            
+            t = createTable;
             
             variableNames = {'single', ...
-                'double', ...
-                'int8', ...
-                'int16', ...
-                'int32', ...
-                'int64', ...
-                'uint8', ...
-                'uint16', ...
-                'uint32', ...
-                'uint64'};
-            variableTypes = repmat({'double'},10,1)';
+                             'double', ...
+                             'int8', ...
+                             'int16', ...
+                             'int32', ...
+                             'int64', ...
+                             'uint8', ...
+                             'uint16', ...
+                             'uint32', ...
+                             'uint64'};
+            variableTypes = repmat({'double'}, 10, 1)';
             numRows = 1;
             numVariables = 10;
             
             addRow = table('Size', [numRows, numVariables], 'VariableTypes', variableTypes, 'VariableNames', variableNames);
-            addRow(1,:)={NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN};
-            actTable=[t;addRow];
-            expectedTable=featherRoundtrip(actTable);
-            testcase.verifyEqual(actTable,expectedTable);
+            addRow(1,:) = {NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN};
+            actualTable = [t; addRow];
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyEqual(actualTable, expectedTable);
         end
 
-        function NumericDatatypesWithNaNColumns(testcase)
-            actTable=generatetable;
-            actTable.double=[NaN;NaN];
-            actTable.int64=[NaN;NaN];
+        function NumericDatatypesWithNaNColumns(testCase)
+            filename = fullfile(pwd, 'temp.feather');
             
-            expectedTable=featherRoundtrip(actTable);
-            testcase.verifyEqual(actTable,expectedTable);
+            actualTable = createTable;
+            actualTable.double = [NaN; NaN; NaN];
+            actualTable.int64  = [NaN; NaN; NaN];
+            
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyEqual(actualTable, expectedTable);
         end
         
-        function NumericDatatypesWithExpInfSciNotation(testcase)
-            actTable=generatetable;
-            actTable.single(2)=1.0418e+06;
+        function NumericDatatypesWithExpInfSciNotation(testCase)
+            filename = fullfile(pwd, 'temp.feather');
             
-            actTable.double(1)=Inf;
-            actTable.double(2)=exp(9);
+            actualTable = createTable;
+            actualTable.single(2) = 1.0418e+06;
             
-            actTable.int64(2)=1.0418e+03;
+            actualTable.double(1) = Inf;
+            actualTable.double(2) = exp(9);
+            
+            actualTable.int64(2) = 1.0418e+03;
            
-            expectedTable=featherRoundtrip(actTable);
-            testcase.verifyEqual(actTable,expectedTable);
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyEqual(actualTable, expectedTable);
         end
         
-        function IgnoreRowVarNames(testcase)
-            actTable=generatetable;
-            time={'day1','day2'};
-            actTable.Properties.RowNames=time;
-            expectedTable=featherRoundtrip(actTable);
-            actTable=generatetable;
-            testcase.verifyEqual(actTable,expectedTable);
-        end
-        
-        function UnsupportedFileFormat(testcase)
-            actTable=generatetable;
-            filename=[tempname,'.txt'];
-            featherwrite(filename,actTable);
-            expectedTable=featherread(filename);
-            testcase.verifyEqual(actTable,expectedTable);
-        end
-        
-        function EmptyTable(testcase)
-            actTable=table;
-            expectedTable=featherRoundtrip(actTable);
-            testcase.verifyEqual(actTable,expectedTable);
+        function IgnoreRowVarNames(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+            
+            actualTable = createTable;
+            time = {'day1', 'day2', 'day3'};
+            actualTable.Properties.RowNames = time;
+            expectedTable = featherRoundTrip(filename, actualTable);
+            actualTable = createTable;
+            testCase.verifyEqual(actualTable, expectedTable);
         end
 
-        function zeroByNTable(testcase)
-            actTable=generatetable;
-            actTable([1,2],:)=[];
-            expectedTable=featherRoundtrip(actTable);
-            testcase.verifyEqual(actTable,expectedTable);
+        function NotFeatherExtension(testCase)
+            filename = fullfile(pwd, 'temp.txt');
+            
+            actualTable = createTable;
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyEqual(actualTable, expectedTable);
+        end
+        
+        function EmptyTable(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+            
+            actualTable = table;
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyEqual(actualTable, expectedTable);
+        end
+
+        function zeroByNTable(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+            
+            actualTable = createTable;
+            actualTable([1, 2], :) = [];
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyEqual(actualTable, expectedTable);
         end
 
         % %%%%%%%%%%%%%%%%%%%
         % Negative test cases
         % %%%%%%%%%%%%%%%%%%%
-      
-        function ErrorIfNotAFeatherFile(testcase)
-            t=generatetable;
-            testcase.verifyError(@() featherread(t), 'MATLAB:arrow:InvalidFilenameDatatype');
+
+        function ErrorIfUnableToOpenFile(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+
+            testCase.verifyError(@() featherread(filename), 'MATLAB:arrow:UnableToOpenFile');
         end
 
-        function ErrorIfUnableToOpenFile(testcase)
-            filename = 'nonexistent.feather';
-
-            testcase.verifyError(@() featherread(filename), 'MATLAB:arrow:UnableToOpenFile');
-        end
-
-        function ErrorIfCorruptedFeatherFile(testcase)
-            t=generatetable;
-            filename=[tempname,'.feather'];
-            featherwrite(filename,t);
+        function ErrorIfCorruptedFeatherFile(testCase)
+            filename = fullfile(pwd, 'temp.feather');
             
-            fileID=fopen(filename,'w');
-            fwrite(fileID,[1;5]);
+            t = createTable;
+            featherwrite(filename, t);
+            
+            fileID = fopen(filename, 'w');
+            fwrite(fileID, [1; 5]);
             fclose(fileID);
             
-            testcase.verifyError(@() featherread(filename), 'MATLAB:arrow:status:Invalid');
-        end
-        function ErrorIfInvalidFilenameDatatype(testcase)
-            t=generatetable;
-            filename=[tempname,'.feather'];
-            featherwrite(filename,t);
-
-            testcase.verifyError(@() featherread({filename}), 'MATLAB:arrow:InvalidFilenameDatatype');
-        end
-
-        function ErrorIfTooManyInputs(testcase)
-            t=generatetable;
-            filename=[tempname,'.feather'];
-            featherwrite(filename,t);
-
-            testcase.verifyError(@() featherread(filename, 'SomeValue'), 'MATLAB:TooManyInputs');
-        end
-
-        function ErrorIfTooFewInputs(testcase)
-            testcase.verifyError(@() featherread(), 'MATLAB:narginchk:notEnoughInputs');
+            testCase.verifyError(@() featherread(filename), 'MATLAB:arrow:status:Invalid');
         end
         
-        function ErrorIfMultiColVarExist(testcase)
-            Age = [38;43;38;40;49];
-            Smoker = logical([1;0;1;0;1]);
-            Height = [71;69;64;67;64];
-            Weight = [176;163;131;133;119];
-            BloodPressure = [124 93; 109 77; 125 83; 117 75; 122 80];
+        function ErrorIfInvalidFilenameDatatype(testCase)
+            filename = fullfile(pwd, 'temp.feather');
             
-            t = table(Age,Smoker,Height,Weight,BloodPressure);
-            multiColVar=@()featherwrite([tempname,'.feather'],t);
-            testcase.verifyError(multiColVar, 'MATLAB:arrow:UnsupportedVariableType');
-        end
-        
-        function unsupportedDatatypes(testcase)
-            actTable=generatetable;
-            time=[calendarDuration(1,7,9);calendarDuration(2,1,1)];
-            actTable=addvars(actTable,time);
-            tableWithUnsupportedDatatypes=@()featherwrite([tempname,'.feather'],actTable);
-            testcase.verifyError(tableWithUnsupportedDatatypes,'MATLAB:arrow:UnsupportedVariableType');
-        end
-        
-        function ErrorForComplexNum(testcase)
-            actTable=generatetable;
-            actTable.single(1)=1.0418+2i;
-           
-            actTable.double(2)=exp(9)+5i;
+            t = createTable;
             
-            actTable.int64(2)=1.0418e+03;
+            testCase.verifyError(@() featherwrite({filename}, t), 'MATLAB:arrow:InvalidFilenameDatatype');
+            testCase.verifyError(@() featherread({filename}), 'MATLAB:arrow:InvalidFilenameDatatype');
+        end
+
+        function ErrorIfTooManyInputs(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+            
+            t = createTable;
+
+            testCase.verifyError(@() featherwrite(filename, t, 'SomeValue', 'SomeOtherValue'), 'MATLAB:TooManyInputs');
+            testCase.verifyError(@() featherread(filename, 'SomeValue', 'SomeOtherValue'), 'MATLAB:TooManyInputs');
+        end
+
+        function ErrorIfTooFewInputs(testCase)
+            testCase.verifyError(@() featherwrite(), 'MATLAB:narginchk:notEnoughInputs');
+            testCase.verifyError(@() featherread(), 'MATLAB:narginchk:notEnoughInputs');
+        end
+        
+        function ErrorIfMultiColVarExist(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+            
+            age           = [38; 43; 38; 40; 49];
+            smoker        = logical([1; 0; 1; 0; 1]);
+            height        = [71; 69; 64; 67; 64];
+            weight        = [176; 163; 131; 133; 119];
+            bloodPressure = [124, 93; 109, 77; 125, 83; 117, 75; 122, 80];
+            
+            t = table(age, smoker, height, weight, bloodPressure);
+            
+            testCase.verifyError(@() featherwrite(filename, t), 'MATLAB:arrow:UnsupportedVariableType');
+        end
+        
+        function UnsupportedMATLABDatatypes(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+
+            actualTable = createTable;
+            calendarDurationVariable = [calendarDuration(1, 7, 9); ...
+                                        calendarDuration(2, 1, 1); ...
+                                        calendarDuration(5, 3, 2)];
+            actualTable = addvars(actualTable, calendarDurationVariable);
+
+            testCase.verifyError(@() featherwrite(filename, actualTable) ,'MATLAB:arrow:UnsupportedVariableType');
+        end
+        
+        function NumericComplexUnsupported(testCase)
+            filename = fullfile(pwd, 'temp.feather');
+
+            actualTable = createTable;
+            actualTable.single(1) = 1.0418 + 2i;
+            actualTable.double(2) = exp(9) + 5i;
+            actualTable.int64(2) = 1.0418e+03;
            
-            expectedTable=featherRoundtrip(actTable);
-            testcase.verifyNotEqual(actTable,expectedTable);
-        end        
+            expectedTable = featherRoundTrip(filename, actualTable);
+            testCase.verifyNotEqual(actualTable, expectedTable);
+        end
+        
     end
-end
-
-%%%Roundtrip%%%%%%%%%%%%
-function expectedTable=featherRoundtrip(t)
-    % Generate a temporary file path in your system's temporary folder
-    filename=[tempname,'.feather'];
-    % Call featherwrite() and featherread()
-    featherwrite(filename,t);
-    expectedTable=featherread(filename);
-end
     
-function t=generatetable
-    variableNames = {'single', ...
-        'double', ...
-        'int8', ...
-        'int16', ...
-        'int32', ...
-        'int64', ...
-        'uint8', ...
-        'uint16', ...
-        'uint32', ...
-        'uint64'};
-    variableTypes = {'single', ...
-        'double', ...
-        'int8', ...
-        'int16', ...
-        'int32', ...
-        'int64', ...
-        'uint8', ...
-        'uint16', ...
-        'uint32', ...
-        'uint64'};
-    numRows = 2;
-    numVariables = 10;
-
-    t = table('Size', [numRows, numVariables], 'VariableTypes', variableTypes, 'VariableNames', variableNames);
-    t(1, :) = {1/3, 2/3, 1, 2, 3, 4,  9, 10, 11, 12};
-    t(2, :) = {4,   5,   5, 6, 7, 8, 13, 14, 15, 16};
 end
