1fc3cc2f80317d96475165999c8f6df3b753f14c
Romain Francois <romain@purrple.cat>
2018-09-07 12:13:25 +0200

    no longer need this

--- r/src/array.cpp
+++ r/src/array.cpp
@@ -102,26 +102,3 @@ bool Array_ApproxEquals(const std::shared_ptr<arrow::Array>& lhs, const std::sha
 std::shared_ptr<arrow::ArrayData> Array_data(const std::shared_ptr<arrow::Array>& array){
   return array->data();
 }
-
-// //' @export
-// // [[Rcpp::export]]
-// xptr_ArrayBuilder ArrayBuilder(xptr_DataType xptr_type) {
-//   if (!Rf_inherits(xptr_type, "arrow::DataType")) stop("incompatible");
-//
-//   std::shared_ptr<arrow::DataType>& type = *xptr_type;
-//
-//   auto memory_pool = arrow::default_memory_pool();
-//   std::unique_ptr<arrow::ArrayBuilder>* arrow_builder =
-//       new std::unique_ptr<arrow::ArrayBuilder>;
-//   auto status = arrow::MakeBuilder(memory_pool, type, arrow_builder);
-//
-//   xptr_ArrayBuilder res(arrow_builder);
-//   res.attr("class") =
-//       CharacterVector::create(DEMANGLE(**arrow_builder), "arrow::ArrayBuilder");
-//   return res;
-// }
-//
-// // [[Rcpp::export]]
-// int ArrayBuilder__num_children(xptr_ArrayBuilder xptr_type) {
-//   return (*xptr_type)->num_children();
-// }
