1d3b7ec0307ca90f48d69573b6a50e5a9c11037f
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-09-10 00:01:23 +0200

    unsafe cast assertion; py2 compatible tests

--- python/pyarrow/tests/test_table.py
+++ python/pyarrow/tests/test_table.py
@@ -775,18 +775,18 @@ def test_table_cast_to_incompatible_schema():
 
 def test_table_safe_casting():
     data = [
-        pa.array(range(5)),
-        pa.array([-10, -5, 0, 5, 10]),
-        pa.array([1.0, 2.0, 3.0]),
-        pa.array(['ab', 'bc', 'cd']),
+        pa.array(range(5), type=pa.int64()),
+        pa.array([-10, -5, 0, 5, 10], type=pa.int32()),
+        pa.array([1.0, 2.0, 3.0], type=pa.float64()),
+        pa.array(['ab', 'bc', 'cd'], type=pa.string())
     ]
     table = pa.Table.from_arrays(data, names=tuple('abcd'))
 
     expected_data = [
         pa.array(range(5), type=pa.int32()),
         pa.array([-10, -5, 0, 5, 10], type=pa.int16()),
-        pa.array([1.0, 2.0, 3.0], type=pa.int64()),
-        pa.array(['ab', 'bc', 'cd'], type=pa.string()),
+        pa.array([1, 2, 3], type=pa.int64()),
+        pa.array(['ab', 'bc', 'cd'], type=pa.string())
     ]
     expected_table = pa.Table.from_arrays(expected_data, names=tuple('abcd'))
 
@@ -803,12 +803,20 @@ def test_table_safe_casting():
 
 def test_table_unsafe_casting():
     data = [
-        pa.array(range(5)),
-        pa.array([-10, -5, 0, 5, 10]),
-        pa.array([1.1, 2.2, 3.3]),
-        pa.array(['ab', 'bc', 'cd']),
+        pa.array(range(5), type=pa.int64()),
+        pa.array([-10, -5, 0, 5, 10], type=pa.int32()),
+        pa.array([1.1, 2.2, 3.3], type=pa.float64()),
+        pa.array(['ab', 'bc', 'cd'], type=pa.string())
     ]
     table = pa.Table.from_arrays(data, names=tuple('abcd'))
+
+    expected_data = [
+        pa.array(range(5), type=pa.int32()),
+        pa.array([-10, -5, 0, 5, 10], type=pa.int16()),
+        pa.array([1, 2, 3], type=pa.int64()),
+        pa.array(['ab', 'bc', 'cd'], type=pa.string())
+    ]
+    expected_table = pa.Table.from_arrays(expected_data, names=tuple('abcd'))
 
     target_schema = pa.schema([
         pa.field('a', pa.int32()),
@@ -820,3 +828,6 @@ def test_table_unsafe_casting():
     with pytest.raises(pa.ArrowInvalid,
                        match='Floating point value truncated'):
         table.cast(target_schema)
+
+    casted_table = table.cast(target_schema, safe=False)
+    assert casted_table.equals(expected_table)
