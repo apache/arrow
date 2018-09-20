cf9f5fc48c8ef9073b848465b8e5a46f12cfa579
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-09-20 13:28:45 +0200

    intall flake8

--- ci/travis_lint.sh
+++ ci/travis_lint.sh
@@ -45,10 +45,12 @@ fi
 # Fail fast on style checks
 
 if [ "$ARROW_CI_DEV_AFFECTED" != "0" ]; then
+  sudo pip install -q flake8
   flake8 --count $ARROW_CROSSBOW_DIR
 fi
 
 if [ "$ARROW_CI_INTEGRATION_AFFECTED" != "0" ]; then
+  sudo pip install -q flake8
   flake8 --count $ARROW_INTEGRATION_DIR
 fi
 
