135a2d789c16edb897de9d38eb9a43463ab76847
Jacques Nadeau <jacques@apache.org>
2018-09-25 15:22:00 -0700

    Update NOTICE for additional source code included.

--- java/memory/src/main/java/org/apache/arrow/util/Preconditions.java
+++ java/memory/src/main/java/org/apache/arrow/util/Preconditions.java
@@ -10,6 +10,8 @@
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
+ *
+ * Lifted from Guava 20.0 to avoid dependency for core Arrow libraries.
  */
 
 package org.apache.arrow.util;
