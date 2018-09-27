8068dbe48b235038f2abfe83c4ac48bfdd6417e1
Eric Conlon <econlon@lyft.com>
2018-09-27 08:47:53 -0700

    pylint

--- python/pyarrow/tests/test_parquet.py
+++ python/pyarrow/tests/test_parquet.py
@@ -767,15 +767,14 @@ def test_coerce_timestamps(tempdir):
                      coerce_timestamps='unknown')
 
 
-    _write_table(arrow_table, filename, version="2.0", coerce_timestamps='ms')
-
-
 def test_coerce_timestamps_truncated(tempdir):
     """
     ARROW-2555: Test that we can truncate timestamps when coercing if explicitly allowed.
     """
-    dt_us = datetime.datetime(year=2017, month=1, day=1, hour=1, minute=1, second=1, microsecond=1)
-    dt_ms = datetime.datetime(year=2017, month=1, day=1, hour=1, minute=1, second=1)
+    dt_us = datetime.datetime(year=2017, month=1, day=1, hour=1, minute=1,
+                              second=1, microsecond=1)
+    dt_ms = datetime.datetime(year=2017, month=1, day=1, hour=1, minute=1,
+                              second=1)
 
     fields_us = [pa.field('datetime64', pa.timestamp('us'))]
     arrays_us = {'datetime64': [dt_us, dt_ms]}
@@ -786,7 +785,8 @@ def test_coerce_timestamps_truncated(tempdir):
     filename = tempdir / 'pandas_truncated.parquet'
     table_us = pa.Table.from_pandas(df_us, schema=schema_us)
 
-    _write_table(table_us, filename, version="2.0", coerce_timestamps='ms', allow_truncated_timestamps=True)
+    _write_table(table_us, filename, version="2.0", coerce_timestamps='ms',
+                 allow_truncated_timestamps=True)
     table_ms = _read_table(filename)
     df_ms = table_ms.to_pandas()
 
