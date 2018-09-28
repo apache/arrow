bf6c9d668fa0f713a756c5213544e359a1a415e4
Animesh Trivedi <atrivedi@apache.org>
2018-09-28 23:04:47 +0200

    (i) space after while; (ii) typos in the comment
    Signed-off-by: Animesh Trivedi <atrivedi@apache.org>

--- java/vector/src/main/java/org/apache/arrow/vector/ipc/WriteChannel.java
+++ java/vector/src/main/java/org/apache/arrow/vector/ipc/WriteChannel.java
@@ -35,9 +35,9 @@ import io.netty.buffer.ArrowBuf;
  * Wrapper around a WritableByteChannel that maintains the position as well adding
  * some common serialization utilities.
  *
- * All write methods in this class follows full write semantics, i.e., write calls
+ * All write methods in this class follow full write semantics, i.e., write calls
  * only return after requested data has been fully written. Note this is different
- * from java WritableByteChannel interface where partial write is allow
+ * from java WritableByteChannel interface where partial write is allowed
  */
 public class WriteChannel implements AutoCloseable {
   private static final Logger LOGGER = LoggerFactory.getLogger(WriteChannel.class);
@@ -77,7 +77,7 @@ public class WriteChannel implements AutoCloseable {
   public long write(ByteBuffer buffer) throws IOException {
     long length = buffer.remaining();
     LOGGER.debug("Writing buffer with size: " + length);
-    while(buffer.hasRemaining()) {
+    while (buffer.hasRemaining()) {
       out.write(buffer);
     }
     currentPosition += length;
