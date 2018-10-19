b26c5ae61867bac34ade1ffed7f940b6176b70bd
Antoine Pitrou <antoine@python.org>
2018-10-19 10:38:47 -0400

    ARROW-3556: [CI] Disable optimizations on Windows
    Author: Antoine Pitrou <antoine@python.org>
    
    Closes #2790 from pitrou/ARROW-3556-disable-opts-ci-windows and squashes the following commits:
    
    a10df41c0 <Antoine Pitrou> ARROW-3556:  Disable optimizations on Windows

--- ci/appveyor-cpp-build.bat
+++ ci/appveyor-cpp-build.bat
@@ -17,6 +17,9 @@
 
 @echo on
 
+@rem In release mode, disable optimizations (/Od) for faster compiling
+set CMAKE_CXX_FLAGS_RELEASE=/Od
+
 if "%JOB%" == "Static_Crt_Build" (
   @rem Since we link the CRT statically, we should also disable building
   @rem the Arrow shared library to link the tests statically, otherwise
@@ -46,6 +49,7 @@ if "%JOB%" == "Static_Crt_Build" (
         -DARROW_BOOST_USE_SHARED=OFF ^
         -DARROW_BUILD_SHARED=OFF ^
         -DCMAKE_BUILD_TYPE=Release ^
+        -DCMAKE_CXX_FLAGS_RELEASE="/MT %CMAKE_CXX_FLAGS_RELEASE%" ^
         -DARROW_CXXFLAGS="/WX /MP" ^
         ..  || exit /B
 
@@ -72,7 +76,7 @@ if "%JOB%" == "Build_Debug" (
         -DARROW_CXXFLAGS="/MP" ^
         ..  || exit /B
 
-  cmake --build . --config Debug || exit /B
+  cmake --build . --config %CONFIGURATION% || exit /B
   ctest --output-on-failure -j2 || exit /B
   popd
 
@@ -108,80 +112,4 @@ if "%JOB%" == "Toolchain" (
   set ARROW_BUILD_TOOLCHAIN=%CONDA_PREFIX%\Library
 )
 
-set ARROW_HOME=%CONDA_PREFIX%\Library
-
-@rem Retrieve git submodules, configure env var for Parquet unit tests
-git submodule update --init
-set PARQUET_TEST_DATA=%CD%\cpp\submodules\parquet-testing\data
-
-@rem Build and test Arrow C++ libraries
-
-mkdir cpp\build
-pushd cpp\build
-
-cmake -G "%GENERATOR%" ^
-      -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX%\Library ^
-      -DARROW_BOOST_USE_SHARED=OFF ^
-      -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
-      -DARROW_BUILD_STATIC=OFF ^
-      -DARROW_CXXFLAGS="/WX /MP" ^
-      -DARROW_PARQUET=ON ^
-      -DARROW_PYTHON=ON ^
-      ..  || exit /B
-cmake --build . --target install --config %CONFIGURATION%  || exit /B
-
-@rem Needed so python-test.exe works
-set OLD_PYTHONHOME=%PYTHONHOME%
-set PYTHONHOME=%CONDA_PREFIX%
-
-ctest --output-on-failure -j2 || exit /B
-
-set PYTHONHOME=%OLD_PYTHONHOME%
-popd
-
-@rem Build and install pyarrow
-
-pushd python
-
-pip install pickle5
-
-set PYARROW_CXXFLAGS=/WX
-set PYARROW_CMAKE_GENERATOR=%GENERATOR%
-set PYARROW_BUNDLE_ARROW_CPP=ON
-set PYARROW_BUNDLE_BOOST=OFF
-set PYARROW_WITH_STATIC_BOOST=ON
-set PYARROW_WITH_PARQUET=ON
-
-@rem ARROW-3075; pkgconfig is broken for Parquet for now
-set PARQUET_HOME=%CONDA_PREFIX%\Library
-
-python setup.py build_ext ^
-    install -q --single-version-externally-managed --record=record.text ^
-    bdist_wheel -q || exit /B
-
-for /F %%i in ('dir /B /S dist\*.whl') do set WHEEL_PATH=%%i
-
-@rem Test directly from installed location
-
-@rem Needed for test_cython
-SET PYARROW_PATH=%CONDA_PREFIX%\Lib\site-packages\pyarrow
-py.test -r sxX --durations=15 -v %PYARROW_PATH% --parquet || exit /B
-
-popd
-
-@rem Test pyarrow wheel from pristine environment
-
-call deactivate
-
-conda create -n wheel_test -q -y python=%PYTHON%
-
-call activate wheel_test
-
-pip install %WHEEL_PATH% || exit /B
-
-python -c "import pyarrow" || exit /B
-python -c "import pyarrow.parquet" || exit /B
-
-pip install pandas pickle5 pytest pytest-faulthandler
-
-py.test -r sxX --durations=15 --pyargs pyarrow.tests || exit /B
+call ci\cpp-msvc-build-main.bat
