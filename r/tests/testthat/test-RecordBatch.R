089f951b9316c64c0037d8430a5cdd49baa65e6c
Romain Francois <romain@purrple.cat>
2018-10-19 13:53:01 +0200

    additional check

--- r/tests/testthat/test-RecordBatch.R
+++ r/tests/testthat/test-RecordBatch.R
@@ -135,9 +135,11 @@ test_that("read_record_batch handles various streams (ARROW-3450, ARROW-3505)", 
 
   mmap_file <- close_on_exit(mmap_open(tf))
   batch4 <- read_record_batch(mmap_file)
+  expect_null(read_record_batch(mmap_file))
 
   batch5 <- read_record_batch(bytes)
   batch6 <- read_record_batch(buf_reader)
+  expect_error(read_record_batch(buf_reader))
 
   stream_reader <- record_batch_stream_reader(bytes)
   batch7 <- read_record_batch(stream_reader)
