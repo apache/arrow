d1f24e3f43e218af6a16a570fec915a74e83fb2e
Praveen <praveen@dremio.com>
2018-10-24 23:38:22 +0530

    ARROW-3489: [Gandiva][C++] Remove debug messages.

--- cpp/src/gandiva/projector.cc
+++ cpp/src/gandiva/projector.cc
@@ -59,8 +59,6 @@ Status Projector::Make(SchemaPtr schema, const ExpressionVector& exprs,
   std::shared_ptr<Projector> cached_projector = cache.GetModule(cache_key);
   if (cached_projector != nullptr) {
     *projector = cached_projector;
-    std::cout << "llvm projector module cache hit : " << cache_key.ToString()
-              << std::endl;
     return Status::OK();
   }
 
