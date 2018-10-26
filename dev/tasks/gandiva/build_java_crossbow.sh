28097c0e3641e0399d5ef54869cadd7d84ed9311
Praveen <praveen@dremio.com>
2018-10-26 17:01:52 +0530

    ARROW-3385: [Gandiva][Crossbow] Refactoring and adrressed review comments.

---
+++ dev/tasks/gandiva/build_java_crossbow.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+set -e
+
+cd $TRAVIS_BUILD_DIR/java
+# build the entire project
+mvn clean install -DskipTests -P gandiva -Dgandiva.cpp.build.dir=../../cpp/build/release
+# test only gandiva
+mvn test -P gandiva -pl gandiva -Dgandiva.cpp.build.dir=../../cpp/build/release
+# copy the jars to distribution folder
+find gandiva/target/ -name "*.jar" -not -name "*tests*" -exec cp  {} ../../dist/ \;
+# setup for upload to github
+cd $TRAVIS_BUILD_DIR/..
