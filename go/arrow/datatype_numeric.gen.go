7f7ad787eb95ea07bc36923bb5cbfd9fedff3e76
Sebastien Binet <binet@cern.ch>
2018-10-28 15:18:30 +0100

    [Go] make numeric types implement FixedWidthDataType

--- go/arrow/datatype_numeric.gen.go
+++ go/arrow/datatype_numeric.gen.go
@@ -20,53 +20,63 @@ package arrow
 
 type Int8Type struct{}
 
-func (t *Int8Type) ID() Type     { return INT8 }
-func (t *Int8Type) Name() string { return "int8" }
+func (t *Int8Type) ID() Type      { return INT8 }
+func (t *Int8Type) Name() string  { return "int8" }
+func (t *Int8Type) BitWidth() int { return 8 }
 
 type Int16Type struct{}
 
-func (t *Int16Type) ID() Type     { return INT16 }
-func (t *Int16Type) Name() string { return "int16" }
+func (t *Int16Type) ID() Type      { return INT16 }
+func (t *Int16Type) Name() string  { return "int16" }
+func (t *Int16Type) BitWidth() int { return 16 }
 
 type Int32Type struct{}
 
-func (t *Int32Type) ID() Type     { return INT32 }
-func (t *Int32Type) Name() string { return "int32" }
+func (t *Int32Type) ID() Type      { return INT32 }
+func (t *Int32Type) Name() string  { return "int32" }
+func (t *Int32Type) BitWidth() int { return 32 }
 
 type Int64Type struct{}
 
-func (t *Int64Type) ID() Type     { return INT64 }
-func (t *Int64Type) Name() string { return "int64" }
+func (t *Int64Type) ID() Type      { return INT64 }
+func (t *Int64Type) Name() string  { return "int64" }
+func (t *Int64Type) BitWidth() int { return 64 }
 
 type Uint8Type struct{}
 
-func (t *Uint8Type) ID() Type     { return UINT8 }
-func (t *Uint8Type) Name() string { return "uint8" }
+func (t *Uint8Type) ID() Type      { return UINT8 }
+func (t *Uint8Type) Name() string  { return "uint8" }
+func (t *Uint8Type) BitWidth() int { return 8 }
 
 type Uint16Type struct{}
 
-func (t *Uint16Type) ID() Type     { return UINT16 }
-func (t *Uint16Type) Name() string { return "uint16" }
+func (t *Uint16Type) ID() Type      { return UINT16 }
+func (t *Uint16Type) Name() string  { return "uint16" }
+func (t *Uint16Type) BitWidth() int { return 16 }
 
 type Uint32Type struct{}
 
-func (t *Uint32Type) ID() Type     { return UINT32 }
-func (t *Uint32Type) Name() string { return "uint32" }
+func (t *Uint32Type) ID() Type      { return UINT32 }
+func (t *Uint32Type) Name() string  { return "uint32" }
+func (t *Uint32Type) BitWidth() int { return 32 }
 
 type Uint64Type struct{}
 
-func (t *Uint64Type) ID() Type     { return UINT64 }
-func (t *Uint64Type) Name() string { return "uint64" }
+func (t *Uint64Type) ID() Type      { return UINT64 }
+func (t *Uint64Type) Name() string  { return "uint64" }
+func (t *Uint64Type) BitWidth() int { return 64 }
 
 type Float32Type struct{}
 
-func (t *Float32Type) ID() Type     { return FLOAT32 }
-func (t *Float32Type) Name() string { return "float32" }
+func (t *Float32Type) ID() Type      { return FLOAT32 }
+func (t *Float32Type) Name() string  { return "float32" }
+func (t *Float32Type) BitWidth() int { return 32 }
 
 type Float64Type struct{}
 
-func (t *Float64Type) ID() Type     { return FLOAT64 }
-func (t *Float64Type) Name() string { return "float64" }
+func (t *Float64Type) ID() Type      { return FLOAT64 }
+func (t *Float64Type) Name() string  { return "float64" }
+func (t *Float64Type) BitWidth() int { return 64 }
 
 var (
 	PrimitiveTypes = struct {
