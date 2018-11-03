0cb8ccc95442d45d77682c52731020a8f786ed9c
Kouhei Sutou <kou@clear-code.com>
2018-11-03 19:52:09 +0900

    Fix typos

--- dev/release/post-02-binary.sh
+++ dev/release/post-02-binary.sh
@@ -148,8 +148,8 @@ for target in debian ubuntu centos python; do
   download_files ${version} ${rc} ${target}
   mv ${target}-rc ${target}
   pushd ${target}
-  if [ ${target} = "python"]; then
-    mv ${target}-rc{$rc} ${target}
+  if [ ${target} = "python" ]; then
+    mv ${version}-rc${rc} ${version}
   fi
   for file in $(find . -type f); do
     upload_file ${version} ${target} ${file}
