100a8eb61947326fa3e2aa8c8bac2145d61bd09c
Yosuke Shiro <ys2014hs@gmail.com>
2018-11-05 10:07:42 +0900

    ARROW-3697: [Ruby] Add schema#[]
    Author: Yosuke Shiro <ys2014hs@gmail.com>
    Author: Kouhei Sutou <kou@clear-code.com>
    
    Closes #2900 from shiro615/ruby-add-convenient-methods-in-schema and squashes the following commits:
    
    82396ff1 <Kouhei Sutou> Show invalid object in error message
    c1a0b3cb <Kouhei Sutou> Remove needless to_s
    c0caf756 <Yosuke Shiro>  Extend # to support both index and name
    787a2bb5 <Yosuke Shiro>  Delete unneeded method
    3705d793 <Yosuke Shiro>  Refactor SchemaTest
    165ee70e <Yosuke Shiro>  Add #each to Arrow::Schema
    c502ae7e <Yosuke Shiro>  Use '' with an alias_method

---
+++ ruby/red-arrow/lib/arrow/schema.rb
@@ -0,0 +1,35 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Arrow
+  class Schema
+    def [](name_or_index)
+      case name_or_index
+      when String, Symbol
+        name = name_or_index
+        get_field_by_name(name)
+      when Integer
+        index = name_or_index
+        fields[index]
+      else
+        message = "field name or index must be String, Symbol or Integer"
+        message << ": <#{name_or_index.inspect}>"
+        raise ArgumentError, message
+      end
+    end
+  end
+end
