e621b220f10a3593c00f646b9e08adbdc9b5189f
Antoine Pitrou <antoine@python.org>
2018-11-08 15:25:11 +0100

    Use memcpy for safe aligned reads, and improve speed of non-ASCII runs

--- cpp/src/arrow/util/utf8.h
+++ cpp/src/arrow/util/utf8.h
@@ -19,6 +19,7 @@
 #define ARROW_UTIL_UTF8_H
 
 #include <cstdint>
+#include <cstring>
 #include <memory>
 
 #include "arrow/util/macros.h"
@@ -77,33 +78,73 @@ static inline uint8_t ValidateOneUTF8Byte(uint8_t byte, uint8_t state) {
 }  // namespace internal
 
 inline bool ValidateUTF8(const uint8_t* data, int64_t size) {
-  static constexpr int64_t high_bits = 0x8080808080808080LL;
+  static constexpr uint64_t high_bits_64 = 0x8080808080808080ULL;
 
   while (size >= 8) {
     // XXX This is doing an unaligned access.  Contemporary architectures
-    // accept it and often have good performance nevertheless.
-    int64_t mask = *reinterpret_cast<const int64_t*>(data) & high_bits;
-    if (ARROW_PREDICT_TRUE(mask == 0)) {
+    // (x86-64, AArch64, PPC64) support it natively and often have good
+    // performance nevertheless.
+    uint64_t mask;
+    memcpy(&mask, data, 8);
+    if (ARROW_PREDICT_TRUE((mask & high_bits_64) == 0)) {
       // 8 bytes of pure ASCII, move forward
       size -= 8;
       data += 8;
-    } else {
-      // Loop over individual bytes until we either get a full char or a rejection
-      uint8_t state = internal::kUTF8Accept;
-      do {
-        state = internal::ValidateOneUTF8Byte(*data++, state);
-        --size;
-      } while (state > internal::kUTF8Reject);
-      if (ARROW_PREDICT_FALSE(state == internal::kUTF8Reject)) {
-        return false;
-      }
+      continue;
     }
+    // Non-ASCII run detected.
+    // We process at least 4 bytes, to avoid too many spurious 64-bit reads
+    // in case the non-ASCII bytes are at the end of the tested 64-bit word.
+    // We also only check for rejection at the end since that state is stable
+    // (once in reject state, we always remain in reject state).
+    // It is guaranteed that size >= 8 when arriving here, which allows
+    // us to avoid size checks.
+    uint8_t state = internal::kUTF8Accept;
+    // Byte 0
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    // Byte 1
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    // Byte 2
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    // Byte 3
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    // Byte 4
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    if (state == internal::kUTF8Accept) {
+      continue;  // Got full char, switch back to ASCII detection
+    }
+    // Byte 5
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    if (state == internal::kUTF8Accept) {
+      continue;  // Got full char, switch back to ASCII detection
+    }
+    // Byte 6
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    if (state == internal::kUTF8Accept) {
+      continue;  // Got full char, switch back to ASCII detection
+    }
+    // Byte 7
+    state = internal::ValidateOneUTF8Byte(*data++, state);
+    --size;
+    if (state == internal::kUTF8Accept) {
+      continue;  // Got full char, switch back to ASCII detection
+    }
+    // kUTF8Accept not reached along 4 transitions has to mean a rejection
+    assert(state == internal::kUTF8Reject);
+    return false;
   }
 
   // Validate string tail one byte at a time
   // Note the state table is designed so that, once in the reject state,
   // we remain in that state until the end.  So we needn't check for
-  // rejection at each char (we don't gain much by short-circuiting at the end).
+  // rejection at each char (we don't gain much by short-circuiting here).
   uint8_t state = internal::kUTF8Accept;
   while (size-- > 0) {
     state = internal::ValidateOneUTF8Byte(*data++, state);
