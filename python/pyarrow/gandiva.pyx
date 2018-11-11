c02dc7c0b2dec3126c0dd63d2a6aea0d22b4cdd8
Philipp Moritz <pcmoritz@gmail.com>
2018-11-10 16:40:28 -0800

    fix

--- python/pyarrow/gandiva.pyx
+++ python/pyarrow/gandiva.pyx
@@ -19,7 +19,6 @@
 # distutils: language = c++
 # cython: embedsignature = True
 
-from cython.operator cimport dereference as deref
 from libcpp cimport bool as c_bool, nullptr
 from libcpp.memory cimport shared_ptr, unique_ptr, make_shared
 from libcpp.string cimport string as c_string
