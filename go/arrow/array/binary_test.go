0e464373d9e2e965a85e52ed5df06a600db74d32
alexandreyc <alexandre.crayssac@gmail.com>
2018-11-22 14:18:55 +0100

    add test for binary array

---
+++ go/arrow/array/binary_test.go
@@ -0,0 +1,38 @@
+package array
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/apache/arrow/go/arrow"
+	"github.com/apache/arrow/go/arrow/memory"
+)
+
+func TestBinary(t *testing.T) {
+	mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
+	defer mem.AssertSize(t, 0)
+
+	b := NewBinaryBuilder(mem, arrow.BinaryTypes.Binary)
+
+	values := [][]byte{
+		[]byte("AAA"),
+		nil,
+		[]byte("BBBB"),
+	}
+	b.AppendValues(values, []bool{true, false, true})
+
+	t.Log(b.offsets.Values())
+	a := b.NewBinaryArray()
+	t.Log(a.valueOffsets)
+
+	assert.Equal(t, 3, a.Len())
+	assert.Equal(t, 1, a.NullN())
+
+	assert.Equal(t, []byte("AAA"), a.Value(0))
+	assert.Equal(t, []byte{}, a.Value(1))
+	assert.Equal(t, []byte("BBBB"), a.Value(2))
+
+	b.Release()
+	a.Release()
+}
