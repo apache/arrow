1995e8858f7cd365f6df7512823f8062d96386b0
Kouhei Sutou <kou@clear-code.com>
2018-11-23 07:35:46 +0900

    ARROW-3854: [GLib] Deprecate garrow_gio_{input,output}_stream_get_raw()
    Because we use `_get_raw()` name for functions that return C++ object.
    
    We can provide getter for raw `GInputStream`/`GOutputStream` via GObject property mechanism. We used the mechanism for `GArrowCompressed{Input,Output}Stream`.
    
    This pull request includes some implementation cleanups (sorry):
    
      * Removed internal `garrow_buffer_input_stream_new_raw_buffer()` API
      * Fixed indent.
      * Changed to use `auto`.
      * Moved `GParamSpec *spec` position.
    
    Author: Kouhei Sutou <kou@clear-code.com>
    
    Closes #3014 from kou/glib-clean-io-stream and squashes the following commits:
    
    e5dc7fe9 <Kouhei Sutou> Close definition and use place
    aed730e9 <Kouhei Sutou> Use auto
    c23ed351 <Kouhei Sutou>  Deprecate gio_{input,output}_get_raw()
    8a1f5556 <Kouhei Sutou>  Fix indent
    9c695b3f <Kouhei Sutou>  Remove needless internal API

--- c_glib/arrow-glib/input-stream.cpp
+++ c_glib/arrow-glib/input-stream.cpp
@@ -113,9 +113,7 @@ G_DEFINE_TYPE_WITH_CODE(GArrowInputStream,
 static void
 garrow_input_stream_finalize(GObject *object)
 {
-  GArrowInputStreamPrivate *priv;
-
-  priv = GARROW_INPUT_STREAM_GET_PRIVATE(object);
+  auto priv = GARROW_INPUT_STREAM_GET_PRIVATE(object);
 
   priv->input_stream = nullptr;
 
@@ -128,9 +126,7 @@ garrow_input_stream_set_property(GObject *object,
                                  const GValue *value,
                                  GParamSpec *pspec)
 {
-  GArrowInputStreamPrivate *priv;
-
-  priv = GARROW_INPUT_STREAM_GET_PRIVATE(object);
+  auto priv = GARROW_INPUT_STREAM_GET_PRIVATE(object);
 
   switch (prop_id) {
   case PROP_INPUT_STREAM:
@@ -164,15 +160,13 @@ garrow_input_stream_init(GArrowInputStream *object)
 static void
 garrow_input_stream_class_init(GArrowInputStreamClass *klass)
 {
-  GObjectClass *gobject_class;
-  GParamSpec *spec;
-
-  gobject_class = G_OBJECT_CLASS(klass);
+  auto gobject_class = G_OBJECT_CLASS(klass);
 
   gobject_class->finalize     = garrow_input_stream_finalize;
   gobject_class->set_property = garrow_input_stream_set_property;
   gobject_class->get_property = garrow_input_stream_get_property;
 
+  GParamSpec *spec;
   spec = g_param_spec_pointer("input-stream",
                               "Input stream",
                               "The raw std::shared<arrow::io::InputStream> *",
@@ -406,14 +400,13 @@ garrow_buffer_input_stream_init(GArrowBufferInputStream *object)
 static void
 garrow_buffer_input_stream_class_init(GArrowBufferInputStreamClass *klass)
 {
-  GParamSpec *spec;
-
   auto gobject_class = G_OBJECT_CLASS(klass);
 
   gobject_class->dispose      = garrow_buffer_input_stream_dispose;
   gobject_class->set_property = garrow_buffer_input_stream_set_property;
   gobject_class->get_property = garrow_buffer_input_stream_get_property;
 
+  GParamSpec *spec;
   spec = g_param_spec_object("buffer",
                              "Buffer",
                              "The data",
@@ -435,7 +428,7 @@ garrow_buffer_input_stream_new(GArrowBuffer *buffer)
   auto arrow_buffer = garrow_buffer_get_raw(buffer);
   auto arrow_buffer_reader =
     std::make_shared<arrow::io::BufferReader>(arrow_buffer);
-  return garrow_buffer_input_stream_new_raw_buffer(&arrow_buffer_reader, buffer);
+  return garrow_buffer_input_stream_new_raw(&arrow_buffer_reader, buffer);
 }
 
 /**
@@ -567,14 +560,14 @@ namespace garrow {
     }
 
     arrow::Status ReadAt(int64_t position, int64_t n_bytes,
-			 int64_t *n_read_bytes, void* out) override {
-	return arrow::io::RandomAccessFile::ReadAt(
-	    position, n_bytes, n_read_bytes, out);
+                         int64_t *n_read_bytes, void* out) override {
+      return arrow::io::RandomAccessFile::ReadAt(
+        position, n_bytes, n_read_bytes, out);
     }
 
     arrow::Status ReadAt(int64_t position, int64_t n_bytes,
-			 std::shared_ptr<arrow::Buffer>* out) override {
-	return arrow::io::RandomAccessFile::ReadAt(position, n_bytes, out);
+                         std::shared_ptr<arrow::Buffer>* out) override {
+      return arrow::io::RandomAccessFile::ReadAt(position, n_bytes, out);
     }
 
     arrow::Status Read(int64_t n_bytes,
@@ -669,9 +662,72 @@ namespace garrow {
 
 G_BEGIN_DECLS
 
-G_DEFINE_TYPE(GArrowGIOInputStream,
-              garrow_gio_input_stream,
-              GARROW_TYPE_SEEKABLE_INPUT_STREAM);
+
+typedef struct GArrowGIOInputStreamPrivate_ {
+  GInputStream *raw;
+} GArrowGIOInputStreamPrivate;
+
+enum {
+  PROP_GIO_RAW = 1
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE(GArrowGIOInputStream,
+                           garrow_gio_input_stream,
+                           GARROW_TYPE_SEEKABLE_INPUT_STREAM);
+
+#define GARROW_GIO_INPUT_STREAM_GET_PRIVATE(object)     \
+  static_cast<GArrowGIOInputStreamPrivate *>(           \
+    garrow_gio_input_stream_get_instance_private(       \
+      GARROW_GIO_INPUT_STREAM(object)))
+
+static void
+garrow_gio_input_stream_dispose(GObject *object)
+{
+  auto priv = GARROW_GIO_INPUT_STREAM_GET_PRIVATE(object);
+
+  if (priv->raw) {
+    g_object_unref(priv->raw);
+    priv->raw = nullptr;
+  }
+
+  G_OBJECT_CLASS(garrow_gio_input_stream_parent_class)->dispose(object);
+}
+
+static void
+garrow_gio_input_stream_set_property(GObject *object,
+                                     guint prop_id,
+                                     const GValue *value,
+                                     GParamSpec *pspec)
+{
+  auto priv = GARROW_GIO_INPUT_STREAM_GET_PRIVATE(object);
+
+  switch (prop_id) {
+  case PROP_GIO_RAW:
+    priv->raw = G_INPUT_STREAM(g_value_dup_object(value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+    break;
+  }
+}
+
+static void
+garrow_gio_input_stream_get_property(GObject *object,
+                                     guint prop_id,
+                                     GValue *value,
+                                     GParamSpec *pspec)
+{
+  auto priv = GARROW_GIO_INPUT_STREAM_GET_PRIVATE(object);
+
+  switch (prop_id) {
+  case PROP_GIO_RAW:
+    g_value_set_object(value, priv->raw);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+    break;
+  }
+}
 
 static void
 garrow_gio_input_stream_init(GArrowGIOInputStream *object)
@@ -681,6 +737,20 @@ garrow_gio_input_stream_init(GArrowGIOInputStream *object)
 static void
 garrow_gio_input_stream_class_init(GArrowGIOInputStreamClass *klass)
 {
+  auto gobject_class = G_OBJECT_CLASS(klass);
+
+  gobject_class->dispose      = garrow_gio_input_stream_dispose;
+  gobject_class->set_property = garrow_gio_input_stream_set_property;
+  gobject_class->get_property = garrow_gio_input_stream_get_property;
+
+  GParamSpec *spec;
+  spec = g_param_spec_object("raw",
+                             "Raw",
+                             "The raw GInputStream *",
+                             G_TYPE_INPUT_STREAM,
+                             static_cast<GParamFlags>(G_PARAM_READWRITE |
+                                                      G_PARAM_CONSTRUCT_ONLY));
+  g_object_class_install_property(gobject_class, PROP_GIO_RAW, spec);
 }
 
 /**
@@ -698,6 +768,7 @@ garrow_gio_input_stream_new(GInputStream *gio_input_stream)
     std::make_shared<garrow::GIOInputStream>(gio_input_stream);
   auto object = g_object_new(GARROW_TYPE_GIO_INPUT_STREAM,
                              "input-stream", &arrow_input_stream,
+                             "raw", gio_input_stream,
                              NULL);
   auto input_stream = GARROW_GIO_INPUT_STREAM(object);
   return input_stream;
@@ -710,16 +781,14 @@ garrow_gio_input_stream_new(GInputStream *gio_input_stream)
  * Returns: (transfer none): The wrapped #GInputStream.
  *
  * Since: 0.5.0
+ *
+ * Deprecated: 0.12.0: Use GArrowGIOInputStream::raw property instead.
  */
 GInputStream *
 garrow_gio_input_stream_get_raw(GArrowGIOInputStream *input_stream)
 {
-  auto arrow_input_stream =
-    garrow_input_stream_get_raw(GARROW_INPUT_STREAM(input_stream));
-  auto arrow_gio_input_stream =
-    std::static_pointer_cast<garrow::GIOInputStream>(arrow_input_stream);
-  auto gio_input_stream = arrow_gio_input_stream->get_input_stream();
-  return gio_input_stream;
+  auto priv = GARROW_GIO_INPUT_STREAM_GET_PRIVATE(input_stream);
+  return priv->raw;
 }
 
 typedef struct GArrowCompressedInputStreamPrivate_ {
@@ -809,14 +878,13 @@ garrow_compressed_input_stream_init(GArrowCompressedInputStream *object)
 static void
 garrow_compressed_input_stream_class_init(GArrowCompressedInputStreamClass *klass)
 {
-  GParamSpec *spec;
-
   auto gobject_class = G_OBJECT_CLASS(klass);
 
   gobject_class->dispose      = garrow_compressed_input_stream_dispose;
   gobject_class->set_property = garrow_compressed_input_stream_set_property;
   gobject_class->get_property = garrow_compressed_input_stream_get_property;
 
+  GParamSpec *spec;
   spec = g_param_spec_object("codec",
                              "Codec",
                              "The codec for the stream",
@@ -879,9 +947,7 @@ garrow_input_stream_new_raw(std::shared_ptr<arrow::io::InputStream> *arrow_input
 std::shared_ptr<arrow::io::InputStream>
 garrow_input_stream_get_raw(GArrowInputStream *input_stream)
 {
-  GArrowInputStreamPrivate *priv;
-
-  priv = GARROW_INPUT_STREAM_GET_PRIVATE(input_stream);
+  auto priv = GARROW_INPUT_STREAM_GET_PRIVATE(input_stream);
   return priv->input_stream;
 }
 
@@ -896,14 +962,8 @@ garrow_seekable_input_stream_get_raw(GArrowSeekableInputStream *seekable_input_s
 }
 
 GArrowBufferInputStream *
-garrow_buffer_input_stream_new_raw(std::shared_ptr<arrow::io::BufferReader> *arrow_buffer_reader)
-{
-  return garrow_buffer_input_stream_new_raw_buffer(arrow_buffer_reader, nullptr);
-}
-
-GArrowBufferInputStream *
-garrow_buffer_input_stream_new_raw_buffer(std::shared_ptr<arrow::io::BufferReader> *arrow_buffer_reader,
-                                          GArrowBuffer *buffer)
+garrow_buffer_input_stream_new_raw(std::shared_ptr<arrow::io::BufferReader> *arrow_buffer_reader,
+                                   GArrowBuffer *buffer)
 {
   auto buffer_input_stream =
     GARROW_BUFFER_INPUT_STREAM(g_object_new(GARROW_TYPE_BUFFER_INPUT_STREAM,
