1fd893202ce5ee12038e9e493fd5dc3b1c303f9b
Wes McKinney <wesm+git@apache.org>
2018-11-21 23:59:30 -0500

    Fix env path in test-cmake-script.bat
    Change-Id: I33b81ec9c34a4698b7db2e962bbee5676e6eda05

--- ci/appveyor-cpp-test-cmake-script.bat
+++ ci/appveyor-cpp-test-cmake-script.bat
@@ -152,7 +152,7 @@ pushd %BUILD_DIR%
 set CONDA_ENV=arrow-cmake-tests-toolchain
 conda create -n %CONDA_ENV% -q -y
 conda install -n %CONDA_ENV% -q -y -c conda-forge ^
-      --file=ci\env\conda_env_cpp.yml
+      --file=ci\conda_env_cpp.yml
 call activate %CONDA_ENV%
 
 set ARROW_BUILD_TOOLCHAIN=%CONDA_PREFIX%\Library
