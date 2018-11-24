3c19de3e0bc5ad4dd3c13c6780bf067e248f79d5
Kouhei Sutou <kou@clear-code.com>
2018-11-24 17:09:41 +0100

    ARROW-3070: [Packaging] Use Bintray
    dev/release/03-binary.sh uploads binaries (.deb, .rpm, .whl and conda
    .tar.bz2) to https://bintray.com/apache/arrow/ . It uses debian-rc,
    ubuntu-rc, centos-rc and python-rc packages on Bintray.
    
    dev/release/03-binary.sh signs and generates checksums binaries. This
    ignores signs and checksums generated by crossbow. Because we need
    some special signing. For example, .dsc and .changes should use inline
    sign instead of separated sign file. debsign command does this. .rpm
    should be signed by "rpm --addsign".
    
    dev/release/03-binary.sh also generates metadata to install .deb and
    .rpm files by apt and yum. See the changes for site/install.md how to
    use https://bintray.com/apache/arrow/ as APT and Yum repository.
    
    This means that this change includes work for
    https://issues.apache.org/jira/browse/ARROW-1349 .
    
    We can install not only RC binaries but also released binaries by apt
    and yum. It will help us to verify binaries.
    
    dev/release/post-02-binary.sh downloads verified binaries at
    debian-rc, ubuntu-rc, centos-rc and python-rc packages on Bintray and
    uploads them to debian, ubuntu, centos and python packages on Bintray.
    
    APT and Yum repositories for RC and release are separated. So RC
    doesn't affect users.
    
    Release manager needs write access to https://bintray.com/apache/arrow .
    
    Author: Kouhei Sutou <kou@clear-code.com>
    
    Closes #2879 from kou/packaging-use-bintray and squashes the following commits:
    
    575a2d20 <Kouhei Sutou> Fix license position
    456ec8c7 <Kouhei Sutou> Add missing license header
    8214abdc <Kouhei Sutou> Clean
    395c873b <Kouhei Sutou> Ensure right permission
    d8fc63be <Kouhei Sutou> Export only the target GPG key
    4bfc6e08 <Kouhei Sutou> Use gpg-agent via SSH
    3dc2d44e <Kouhei Sutou> Add support for changing Bintray repository
    0cb8ccc9 <Kouhei Sutou> Fix typos
    66ca3d11 <Kouhei Sutou> Fix upload path for Python binaries
    5e094ed5 <Kouhei Sutou> Ensure updating APT repository
    41b355c1 <Kouhei Sutou> Fix path
    9965a063 <Kouhei Sutou> Uncomment
    e49ca7cc <Kouhei Sutou> Stop to upload binaries to dist.apache.org
    e71a7742 <Kouhei Sutou> Reduce API request
    12e2239e <Kouhei Sutou> Fix function name
    a9cb7b83 <Kouhei Sutou> Unify how to install
    1fb9083b <Kouhei Sutou> Force to resign
    d0b2b059 <Kouhei Sutou> Remove needless quotations
    a7fd26ba <Kouhei Sutou> Fix a typo
    d2d6c3b2 <Kouhei Sutou> Fix a typo
    42d3836e <Kouhei Sutou> Sign
    b3c11872 <Kouhei Sutou> Replace existing files
    238ee605 <Kouhei Sutou> Update install document
    65554a32 <Kouhei Sutou> Sign .dec and .changes
    3c0bafc9 <Kouhei Sutou> Fix a typo
    9e39341c <Kouhei Sutou> Support Python
    c22ebbac <Kouhei Sutou> Support APT
    18d7e112 <Kouhei Sutou>  Use Bintray for binary artifacts

---
+++ dev/release/post-02-binary.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+# -*- indent-tabs-mode: nil; sh-indentation: 2; sh-basic-offset: 2 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+set -e
+set -o pipefail
+
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <version> <rc-num>"
+  exit
+fi
+
+version=$1
+rc=$2
+
+if [ -z "${BINTRAY_PASSWORD}" ]; then
+  echo "BINTRAY_PASSWORD is empty"
+  exit 1
+fi
+
+: ${BINTRAY_REPOSITORY:=apache/arrow}
+
+docker_image_name=apache-arrow/release-binary
+
+jq() {
+  docker \
+    run \
+    --rm \
+    --interactive \
+    ${docker_image_name} \
+    jq "$@"
+}
+
+bintray() {
+  local command=$1
+  shift
+  local path=$1
+  shift
+  local url=https://bintray.com/api/v1${path}
+  echo "${command} ${url}" 1>&2
+  curl \
+    --fail \
+    --basic \
+    --user "${BINTRAY_USER:-$USER}:${BINTRAY_PASSWORD}" \
+    --header "Content-Type: application/json" \
+    --request ${command} \
+    ${url} \
+    "$@" | \
+      jq .
+}
+
+ensure_version() {
+  local version=$1
+  local target=$2
+
+  if ! bintray \
+         GET \
+         /packages/${BINTRAY_REPOSITORY}/${target}/versions/${version}; then
+    bintray \
+      POST /packages/${BINTRAY_REPOSITORY}/${target}/versions \
+      --data-binary "
+{
+  \"name\": \"${version}\",
+  \"desc\": \"Apache Arrow ${version}.\"
+}
+"
+  fi
+}
+
+download_files() {
+  local version=$1
+  local rc=$2
+  local target=$3
+
+  local version_name=${version}-rc${rc}
+
+  local files=$(
+    bintray \
+      GET /packages/${BINTRAY_REPOSITORY}/${target}-rc/versions/${version_name}/files | \
+      jq -r ".[].path")
+
+  for file in ${files}; do
+    mkdir -p "$(dirname ${file})"
+    curl \
+      --fail \
+      --location \
+      --output ${file} \
+      https://dl.bintray.com/${BINTRAY_REPOSITORY}/${file}
+  done
+}
+
+delete_file() {
+  local version=$1
+  local target=$2
+  local path=$3
+
+  bintray \
+    DELETE /content/${BINTRAY_REPOSITORY}/${target}/${path}
+}
+
+upload_file() {
+  local version=$1
+  local target=$2
+  local path=$3
+
+  local sha256=$(shasum -a 256 ${path} | awk '{print $1}')
+  local request_path=/content/${BINTRAY_REPOSITORY}/${target}/${version}/${target}/${path}
+  if ! bintray \
+         PUT ${request_path} \
+         --header "X-Bintray-Publish: 1" \
+         --header "X-Bintray-Override: 1" \
+         --header "X-Checksum-Sha2: ${sha256}" \
+         --data-binary "@${path}"; then
+    delete_file ${version} ${target} ${path}
+    bintray \
+      PUT ${request_path} \
+      --header "X-Bintray-Publish: 1" \
+      --header "X-Bintray-Override: 1" \
+      --header "X-Checksum-Sha2: ${sha256}" \
+      --data-binary "@${path}"
+  fi
+}
+
+docker build -t ${docker_image_name} ${SOURCE_DIR}/binary
+
+for target in debian ubuntu centos python; do
+  tmp_dir=tmp/${target}
+  rm -rf ${tmp_dir}
+  mkdir -p ${tmp_dir}
+  pushd ${tmp_dir}
+  ensure_version ${version} ${target}
+  download_files ${version} ${rc} ${target}
+  mv ${target}-rc ${target}
+  pushd ${target}
+  if [ ${target} = "python" ]; then
+    mv ${version}-rc${rc} ${version}
+  fi
+  for file in $(find . -type f); do
+    upload_file ${version} ${target} ${file}
+  done
+  popd
+  popd
+  rm -rf ${tmp_dir}
+done
+
+echo "Success! The release binaries are available here:"
+echo "  https://bintray.com/${BINTRAY_REPOSITORY}/debian/${version}"
+echo "  https://bintray.com/${BINTRAY_REPOSITORY}/ubuntu/${version}"
+echo "  https://bintray.com/${BINTRAY_REPOSITORY}/centos/${version}"
+echo "  https://bintray.com/${BINTRAY_REPOSITORY}/python/${version}"
