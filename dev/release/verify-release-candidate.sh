a602c6ea6c59e1ecce85b3042edb5fe541ac4ae3
Andy Grove <andygrove73@gmail.com>
2018-11-27 07:12:01 -0700

    fix bug

--- dev/release/verify-release-candidate.sh
+++ dev/release/verify-release-candidate.sh
@@ -268,12 +268,14 @@ test_rust() {
   # build and test rust
   pushd rust
 
+  # we are targeting Rust nightly for releases
+  rustup default nightly
+
   # raises on any formatting errors
   rustup component add rustfmt-preview
   cargo fmt --all -- --check
   # raises on any warnings
 
-  rustup default nightly
   cargo rustc -- -D warnings
 
   cargo build
