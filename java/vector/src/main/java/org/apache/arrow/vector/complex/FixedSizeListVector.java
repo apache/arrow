0d9ceeb607cfbbac2b4fe30fd346740aa6c1bbce
Animesh Trivedi <atrivedi@apache.org>
2018-11-27 12:44:23 +0100

    ARROW-3891: [Java] Remove Long.bitCount with simple bitmap operations
    to check if a bit is set or not, there is no need to count
    number of bits set in a long. A simple shift and bitwise AND
    is sufficient (and faster!).
    
    Signed-off-by: Animesh Trivedi <atrivedi@apache.org>

--- java/vector/src/main/java/org/apache/arrow/vector/complex/FixedSizeListVector.java
+++ java/vector/src/main/java/org/apache/arrow/vector/complex/FixedSizeListVector.java
@@ -406,7 +406,7 @@ public class FixedSizeListVector extends BaseValueVector implements FieldVector,
     final int byteIndex = index >> 3;
     final byte b = validityBuffer.getByte(byteIndex);
     final int bitIndex = index & 7;
-    return Long.bitCount(b & (1L << bitIndex));
+    return (b >> bitIndex) & 0x01;
   }
 
   @Override
