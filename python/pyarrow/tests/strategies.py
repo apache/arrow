6bd5b21eaa4fc496ff981657076fec9729abb497
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-11-29 12:45:30 +0100

    license header

--- python/pyarrow/tests/strategies.py
+++ python/pyarrow/tests/strategies.py
@@ -1,9 +1,25 @@
-import hypothesis.strategies as st
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 import pyarrow as pa
+import hypothesis.strategies as st
 
 
-# TODO alphanum_text, surrogate_text
+# TODO(kszucs): alphanum_text, surrogate_text
 custom_text = st.text(
     alphabet=st.characters(
         min_codepoint=int('41', 16),
