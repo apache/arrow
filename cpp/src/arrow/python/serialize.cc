a667fca3b71772886bb2595986266d2039823dcc
Robert Nishihara <robertnishihara@gmail.com>
2018-12-01 00:26:51 -0800

    ARROW-3920: [plasma] Fix reference counting in custom tensorflow plasma operator.
    There is an issue here where `Release` was never being called in the plasma TF operator.
    
    Note that I also changed the release delay in the plasma operator to 0.
    
    Author: Robert Nishihara <robertnishihara@gmail.com>
    Author: Philipp Moritz <pcmoritz@gmail.com>
    
    Closes #3061 from robertnishihara/extrareleaseinplasmaop and squashes the following commits:
    
    c10956692 <Philipp Moritz> add include guards
    f89d5df8c <Philipp Moritz> lint
    4836342e0 <Philipp Moritz> unregister memory
    e3b3864ef <Robert Nishihara> Linting
    b948ce0f4 <Robert Nishihara> Add test.
    75f2bd99c <Robert Nishihara> Remove logging statement.
    f04a7d26d <Robert Nishihara> Fix
    574c03532 <Robert Nishihara> Fix ndarray/tensor confusion in plasma op.
    06985cd1c <Robert Nishihara> Have plasma op deserialize as numpy array.
    a2a9c36b3 <Robert Nishihara> Add release call into wrapped_callback.
    0db9154bd <Robert Nishihara> Change release delay to 0.
    f4340946c <Robert Nishihara> Add Release call in plasma op.

--- cpp/src/arrow/python/serialize.cc
+++ cpp/src/arrow/python/serialize.cc
@@ -752,23 +752,23 @@ Status SerializeObject(PyObject* context, PyObject* sequence, SerializedPyObject
   return Status::OK();
 }
 
-Status SerializeTensor(std::shared_ptr<Tensor> tensor, SerializedPyObject* out) {
+Status SerializeNdarray(std::shared_ptr<Tensor> tensor, SerializedPyObject* out) {
   std::shared_ptr<Array> array;
   SequenceBuilder builder;
-  RETURN_NOT_OK(builder.AppendTensor(static_cast<int32_t>(out->tensors.size())));
-  out->tensors.push_back(tensor);
+  RETURN_NOT_OK(builder.AppendNdarray(static_cast<int32_t>(out->ndarrays.size())));
+  out->ndarrays.push_back(tensor);
   RETURN_NOT_OK(builder.Finish(nullptr, nullptr, nullptr, nullptr, &array));
   out->batch = MakeBatch(array);
   return Status::OK();
 }
 
-Status WriteTensorHeader(std::shared_ptr<DataType> dtype,
-                         const std::vector<int64_t>& shape, int64_t tensor_num_bytes,
-                         io::OutputStream* dst) {
+Status WriteNdarrayHeader(std::shared_ptr<DataType> dtype,
+                          const std::vector<int64_t>& shape, int64_t tensor_num_bytes,
+                          io::OutputStream* dst) {
   auto empty_tensor = std::make_shared<Tensor>(
       dtype, std::make_shared<Buffer>(nullptr, tensor_num_bytes), shape);
   SerializedPyObject serialized_tensor;
-  RETURN_NOT_OK(SerializeTensor(empty_tensor, &serialized_tensor));
+  RETURN_NOT_OK(SerializeNdarray(empty_tensor, &serialized_tensor));
   return serialized_tensor.WriteTo(dst);
 }
 
