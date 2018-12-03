98bdde873a9f7ff3854a52f096dabe3c52be6e3a
Wes McKinney <wesm+git@apache.org>
2018-12-02 21:44:36 -0600

    PARQUET-1467: [C++] Remove defunct ChunkedAllocator code
    It does not seem that memory allocation on the dictionary encoding path requires something so elaborate right now
    
    Author: Wes McKinney <wesm+git@apache.org>
    
    Closes #3069 from wesm/PARQUET-1467 and squashes the following commits:
    
    f37ed0756 <Wes McKinney> Remove defunct memory allocator code

--- cpp/src/parquet/test-util.h
+++ cpp/src/parquet/test-util.h
@@ -247,10 +247,10 @@ class DictionaryPageBuilder {
   // This class writes data and metadata to the passed inputs
   explicit DictionaryPageBuilder(const ColumnDescriptor* d)
       : num_dict_values_(0), have_values_(false) {
-    encoder_.reset(new DictEncoder<TYPE>(d, &pool_));
+    encoder_.reset(new DictEncoder<TYPE>(d));
   }
 
-  ~DictionaryPageBuilder() { pool_.FreeAll(); }
+  ~DictionaryPageBuilder() {}
 
   shared_ptr<Buffer> AppendValues(const vector<TC>& values) {
     int num_values = static_cast<int>(values.size());
@@ -271,7 +271,6 @@ class DictionaryPageBuilder {
   int32_t num_values() const { return num_dict_values_; }
 
  private:
-  ChunkedAllocator pool_;
   shared_ptr<DictEncoder<TYPE>> encoder_;
   int32_t num_dict_values_;
   bool have_values_;
