694e7e6ef9d9b8abc78bf1806b4b356cb53bd787
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-12-04 13:41:00 +0100

    docker-compose setup for dask integration

---
+++ integration/dask/runtest.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+set -e
+
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Set up environment and working directory
+cd /apache-arrow
+
+conda activate pyarrow-dev
+
+# install pytables from defaults for now
+# conda install -y pytables
+#
+# pip install -q git+https://github.com/dask/partd --upgrade --no-deps
+# pip install -q git+https://github.com/dask/zict --upgrade --no-deps
+# pip install -q git+https://github.com/dask/distributed --upgrade --no-deps
+# pip install -q git+https://github.com/mrocklin/sparse --upgrade --no-deps
+# pip install -q git+https://github.com/dask/s3fs --upgrade --no-deps
+#
+# conda install -y -q -c conda-forge numba cython \
+#     bcolz \
+#     blosc \
+#     bokeh \
+#     boto3 \
+#     chest \
+#     cloudpickle \
+#     coverage \
+#     cytoolz \
+#     distributed \
+#     graphviz \
+#     h5py \
+#     partd \
+#     psutil \
+#     "pytest<=3.1.1" \
+#     scikit-image \
+#     scikit-learn \
+#     sqlalchemy \
+#     toolz
+#
+# pip install -q git+https://github.com/dask/fastparquet
+#
+# pip install -q \
+#     cachey \
+#     graphviz \
+#     moto \
+#     pyarrow \
+#     --upgrade --no-deps
+#
+# pip install -q \
+#     cityhash \
+#     flake8 \
+#     mmh3 \
+#     pandas_datareader \
+#     pytest-xdist \
+#     xxhash \
+#     pycodestyle
+#
+# export LD_LIBRARY_PATH=$(pwd)/dist/lib:${CONDA_BASE}/lib:${LD_LIBRARY_PATH}
+
+# # Clean up and get the dask master branch from github
+# rm -rf dask .local
+# export GIT_COMMITTER_NAME="Nobody"
+# export GIT_COMMITTER_EMAIL="nobody@nowhere.com"
+# git clone https://github.com/dask/dask.git
+# pushd dask
+# pip install --user -e .[complete]
+
+# pytest arrow/python/testing/dask_tests
+
+pytest --pyargs dask.dataframe.tests.test_parquet
+pytest --pyargs dask.dataframe.tests.test_dataframe
