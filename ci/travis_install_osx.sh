7dce4f0a383de736f909bb13331989b0ade7bf0c
Kouhei Sutou <kou@clear-code.com>
2018-12-12 17:05:07 +0900

    travis_wait isn't available in custom shell script

--- ci/travis_install_osx.sh
+++ ci/travis_install_osx.sh
@@ -27,9 +27,7 @@ if [ "$ARROW_CI_RUBY_AFFECTED" = "1" ]; then
         local n_tries=3
         while [[ $((i++)) < ${n_tries} ]]; do
             echo "${i}: brew" "$@" >> ${brew_log_path}
-            if travis_wait 15 \
-                   gtimeout --signal=KILL 14m brew "$@" \
-                       >> ${brew_log_path} 2>&1; then
+            if gtimeout --signal=KILL 9m brew "$@" >> ${brew_log_path} 2>&1; then
                 break
             elif [[ ${i} == ${n_tries} ]]; then
                 cat ${brew_log_path}
