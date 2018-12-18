e832df36c2d44d02273de851db3cfcd8c231f479
François Saint-Jacques <fsaintjacques@gmail.com>
2018-12-18 16:43:39 +0100

    ARROW-3387: [C++] Implement Binary to String cast
    Author: François Saint-Jacques <fsaintjacques@gmail.com>
    
    Closes #3211 from fsaintjacques/ARROW-3387-cast-binary-to-string and squashes the following commits:
    
    16cdb8ef <François Saint-Jacques> ARROE-3387:  clang-format
    1949d377 <François Saint-Jacques> ARROW-3387:  Address review comments
    31092b9f <François Saint-Jacques> ARROW-3387:  Implement Binary to String cast
    f045d64f <François Saint-Jacques> ARROW-3387:  Partition compute-test.cc in separate files
    5358148e <François Saint-Jacques> ARROW-3387:  Rename CopyData to ZeroCopyData

--- cpp/src/arrow/compute/kernels/cast.h
+++ cpp/src/arrow/compute/kernels/cast.h
@@ -38,12 +38,14 @@ struct ARROW_EXPORT CastOptions {
   CastOptions()
       : allow_int_overflow(false),
         allow_time_truncate(false),
-        allow_float_truncate(false) {}
+        allow_float_truncate(false),
+        allow_invalid_utf8(false) {}
 
   explicit CastOptions(bool safe)
       : allow_int_overflow(!safe),
         allow_time_truncate(!safe),
-        allow_float_truncate(!safe) {}
+        allow_float_truncate(!safe),
+        allow_invalid_utf8(!safe) {}
 
   static CastOptions Safe() { return CastOptions(true); }
 
@@ -52,6 +54,9 @@ struct ARROW_EXPORT CastOptions {
   bool allow_int_overflow;
   bool allow_time_truncate;
   bool allow_float_truncate;
+  // Indicate if conversions from Binary/FixedSizeBinary to string must
+  // validate the utf8 payload.
+  bool allow_invalid_utf8;
 };
 
 /// \since 0.7.0
