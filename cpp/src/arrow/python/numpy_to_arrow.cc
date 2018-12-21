48dd1724ddf2354391f6b7b3fdb043ab780b2c27
Wes McKinney <wesm+git@apache.org>
2018-12-21 10:36:55 -0600

    ARROW-2970: [Python] Support conversions of NumPy string arrays requiring chunked binary output
    Author: Wes McKinney <wesm+git@apache.org>
    
    Closes #3240 from wesm/ARROW-2970 and squashes the following commits:
    
    8b04eb3c4 <Wes McKinney> Make the test data a bit more diverse
    60d35f0e4 <Wes McKinney> Use internal::ChunkedBinaryBuilder for converting NumPy string/binary array to Arrow

--- cpp/src/arrow/python/numpy_to_arrow.cc
+++ cpp/src/arrow/python/numpy_to_arrow.cc
@@ -534,8 +534,11 @@ inline Status NumPyConverter::ConvertData<Date64Type>(std::shared_ptr<Buffer>* d
   return Status::OK();
 }
 
+// Create 16MB chunks for binary data
+constexpr int32_t kBinaryChunksize = 1 << 24;
+
 Status NumPyConverter::Visit(const BinaryType& type) {
-  BinaryBuilder builder(pool_);
+  ::arrow::internal::ChunkedBinaryBuilder builder(kBinaryChunksize, pool_);
 
   auto data = reinterpret_cast<const uint8_t*>(PyArray_DATA(arr_));
 
@@ -564,9 +567,12 @@ Status NumPyConverter::Visit(const BinaryType& type) {
     }
   }
 
-  std::shared_ptr<Array> result;
+  ArrayVector result;
   RETURN_NOT_OK(builder.Finish(&result));
-  return PushArray(result->data());
+  for (auto arr : result) {
+    RETURN_NOT_OK(PushArray(arr->data()));
+  }
+  return Status::OK();
 }
 
 Status NumPyConverter::Visit(const FixedSizeBinaryType& type) {
