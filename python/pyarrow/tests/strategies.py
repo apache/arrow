ea41d9a2260924df8bf2fb5f2003fbd2b2edf01e
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-12-22 23:23:08 +0100

    ARROW-4106: [Python] Tests fail to run because hypothesis update broke its API
    Author: Krisztián Szűcs <szucs.krisztian@gmail.com>
    
    Closes #3250 from kszucs/ARROW-4106 and squashes the following commits:
    
    d87cc14c <Krisztián Szűcs> don't use defines_strategy

--- python/pyarrow/tests/strategies.py
+++ python/pyarrow/tests/strategies.py
@@ -89,43 +89,35 @@ primitive_types = st.one_of(
 metadata = st.dictionaries(st.text(), st.text())
 
 
-@st.defines_strategy
 def fields(type_strategy=primitive_types):
     return st.builds(pa.field, name=custom_text, type=type_strategy,
                      nullable=st.booleans(), metadata=metadata)
 
 
-@st.defines_strategy
 def list_types(item_strategy=primitive_types):
     return st.builds(pa.list_, item_strategy)
 
 
-@st.defines_strategy
 def struct_types(item_strategy=primitive_types):
     return st.builds(pa.struct, st.lists(fields(item_strategy)))
 
 
-@st.defines_strategy
 def complex_types(inner_strategy=primitive_types):
     return list_types(inner_strategy) | struct_types(inner_strategy)
 
 
-@st.defines_strategy
 def nested_list_types(item_strategy=primitive_types):
     return st.recursive(item_strategy, list_types)
 
 
-@st.defines_strategy
 def nested_struct_types(item_strategy=primitive_types):
     return st.recursive(item_strategy, struct_types)
 
 
-@st.defines_strategy
 def nested_complex_types(inner_strategy=primitive_types):
     return st.recursive(inner_strategy, complex_types)
 
 
-@st.defines_strategy
 def schemas(type_strategy=primitive_types):
     return st.builds(pa.schema, st.lists(fields(type_strategy)))
 
