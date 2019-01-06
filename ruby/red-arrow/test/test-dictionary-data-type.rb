18c480879daf74d7ea86356f6515e4ad515b81b8
Kouhei Sutou <kou@clear-code.com>
2019-01-06 21:21:15 +0900

    ARROW-4162: [Ruby] Add support for creating data types from description
    Author: Kouhei Sutou <kou@clear-code.com>
    
    Closes #3318 from kou/ruby-data-type-new-by-description and squashes the following commits:
    
    e7231e69 <Kouhei Sutou>  Add support for creating data types from description

---
+++ ruby/red-arrow/test/test-dictionary-data-type.rb
@@ -0,0 +1,40 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+class DictionaryDataTypeTest < Test::Unit::TestCase
+  sub_test_case(".new") do
+    def setup
+      @index_data_type = :int8
+      @dictionary = Arrow::StringArray.new(["Hello", "World"])
+      @ordered = true
+    end
+
+    test("ordered arguments") do
+      assert_equal("dictionary<values=string, indices=int8, ordered=1>",
+                   Arrow::DictionaryDataType.new(@index_data_type,
+                                                 @dictionary,
+                                                 @ordered).to_s)
+    end
+
+    test("description") do
+      assert_equal("dictionary<values=string, indices=int8, ordered=1>",
+                   Arrow::DictionaryDataType.new(index_data_type: @index_data_type,
+                                                 dictionary: @dictionary,
+                                                 ordered: @ordered).to_s)
+    end
+  end
+end
