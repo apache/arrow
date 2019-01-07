c0b24e60ced3129b3ee627fab35c326acdee88f5
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-01-07 13:53:31 +0100

    patch spark 2.4.0

--- integration/spark/runtest.sh
+++ integration/spark/runtest.sh
@@ -18,7 +18,7 @@
 # exit on any error
 set -e
 
-export MAVEN_OPTS="-q -Xmx2g -XX:ReservedCodeCacheSize=512m"
+export MAVEN_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m"
 
 SPARK_VERSION=${SPARK_VERSION:-2.4.0}
 pushd arrow/java
@@ -26,7 +26,7 @@ pushd arrow/java
 popd
 
 cache_dir="/build/spark"
-spark_dir="$cache_dir/spark-$SPARK_VERSION"
+spark_dir="${cache_dir}/spark-${SPARK_VERSION}"
 
 if [ ! -d $spark_dir ]; then
   echo "Downloading Spark source archive to $cache_dir..."
@@ -34,14 +34,24 @@ if [ ! -d $spark_dir ]; then
   wget -q -O /tmp/spark.tar.gz https://github.com/apache/spark/archive/v$SPARK_VERSION.tar.gz
   tar -xzf /tmp/spark.tar.gz -C $cache_dir
   rm /tmp/spark.tar.gz
+
+  # - avoid too long filenames error https://issues.apache.org/jira/browse/SPARK-4820
+  # - fix python compatibility issues in pyspark testing suite
+  spark_patch="/arrow/integration/spark/${SPARK_VERSION}.patch"
+  if [ -f $spark_patch ]; then
+    pushd $spark_dir
+      patch -p1 < $spark_patch
+    popd
+  fi
 fi
 
 # build Spark with Arrow
 pushd $spark_dir
   # update Spark pom with the Arrow version just installed and build Spark, need package phase for pyspark
-  sed -i -e "s/\(.*<arrow.version>\).*\(<\/arrow.version>\)/\1$ARROW_VERSION\2/g" ./pom.xml
   echo "Building Spark with Arrow $ARROW_VERSION"
-  build/mvn -DskipTests package -pl sql/core -am
+  mvn versions:set-property -Dproperty=arrow.version -DnewVersion=$ARROW_VERSION
+
+  build/mvn -q -T 1C -DskipTests package #-pl sql/core -am
 
   SPARK_SCALA_TESTS=(
     "org.apache.spark.sql.execution.arrow"
@@ -51,18 +61,9 @@ pushd $spark_dir
   (echo "Testing Spark:"; IFS=$'\n'; echo "${SPARK_SCALA_TESTS[*]}")
 
   # TODO: should be able to only build spark-sql tests with adding "-pl sql/core" but not currently working
-  build/mvn -Dtest=none -DwildcardSuites=$(IFS=,; echo "${SPARK_SCALA_TESTS[*]}") test
+  build/mvn -q -T 1C -Dtest=none -DwildcardSuites=$(IFS=,; echo "${SPARK_SCALA_TESTS[*]}") test
 
   # Run pyarrow related Python tests only
-  SPARK_PYTHON_TESTS=(
-      "pyspark.sql.tests.test_arrow ArrowTests"
-      "pyspark.sql.tests.test_pandas_udf PandasUDFTests"
-      "pyspark.sql.tests.test_pandas_udf_scalar ScalarPandasUDFTests"
-      "pyspark.sql.tests.test_pandas_udf_grouped_map GroupedMapPandasUDFTests"
-      "pyspark.sql.tests.test_pandas_udf_grouped_agg GroupedAggPandasUDFTests"
-      "pyspark.sql.tests.test_pandas_udf_window WindowPandasUDFTests")
-
-  (echo "Testing PySpark:"; IFS=$'\n'; echo "${SPARK_PYTHON_TESTS[*]}")
-
-  python/run-tests --testnames "$(IFS=,; echo "${SPARK_PYTHON_TESTS[*]}")"
+  echo "Testing PySpark:"
+  python/run-tests --testnames pyspark-sql
 popd
