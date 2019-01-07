77117f08e746b580edb2d384f102e27726611540
Yosuke Shiro <yosuke.shiro615@gmail.com>
2019-01-07 09:00:54 +0900

    Call g_object_unref() for GArrowDataType

--- c_glib/arrow-glib/column.cpp
+++ c_glib/arrow-glib/column.cpp
@@ -322,8 +322,11 @@ garrow_column_get_field(GArrowColumn *column)
   } else {
     const auto arrow_column = garrow_column_get_raw(column);
     auto arrow_field = arrow_column->field();
-    return garrow_field_new_raw(&arrow_field,
-                                garrow_column_get_data_type(column));
+    auto data_type = garrow_column_get_data_type(column);
+    auto field = garrow_field_new_raw(&arrow_field,
+                                      data_type);
+    g_object_unref(data_type);
+    return field;
   }
 }
 
