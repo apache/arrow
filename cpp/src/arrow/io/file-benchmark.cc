50118a6395595d253923630763d1409b0a55368e
Wes McKinney <wesm+git@apache.org>
2019-01-09 19:46:03 -0600

    Fix API in file-benchmark.cc
    Change-Id: I13964f60852c9c3ce7ce02f250b9436c1af7d4a6

--- cpp/src/arrow/io/file-benchmark.cc
+++ cpp/src/arrow/io/file-benchmark.cc
@@ -163,7 +163,7 @@ static void BM_BufferedOutputStreamSmallWritesToNull(
   ABORT_NOT_OK(io::FileOutputStream::Open(GetNullFile(), &file));
 
   std::shared_ptr<io::BufferedOutputStream> buffered_file;
-  ABORT_NOT_OK(io::BufferedOutputStream::Create(file, kBufferSize, default_memory_pool(),
+  ABORT_NOT_OK(io::BufferedOutputStream::Create(kBufferSize, default_memory_pool(), file,
                                                 &buffered_file));
   BenchmarkStreamingWrites(state, small_sizes, buffered_file.get());
 }
@@ -197,8 +197,8 @@ static void BM_BufferedOutputStreamSmallWritesToPipe(
   SetupPipeWriter(&stream, &reader);
 
   std::shared_ptr<io::BufferedOutputStream> buffered_stream;
-  ABORT_NOT_OK(io::BufferedOutputStream::Create(stream, kBufferSize,
-                                                default_memory_pool(), &buffered_stream));
+  ABORT_NOT_OK(io::BufferedOutputStream::Create(kBufferSize, default_memory_pool(),
+                                                stream, &buffered_stream));
   BenchmarkStreamingWrites(state, small_sizes, buffered_stream.get(), reader.get());
 }
 
@@ -209,8 +209,8 @@ static void BM_BufferedOutputStreamLargeWritesToPipe(
   SetupPipeWriter(&stream, &reader);
 
   std::shared_ptr<io::BufferedOutputStream> buffered_stream;
-  ABORT_NOT_OK(io::BufferedOutputStream::Create(stream, kBufferSize,
-                                                default_memory_pool(), &buffered_stream));
+  ABORT_NOT_OK(io::BufferedOutputStream::Create(kBufferSize, default_memory_pool(),
+                                                stream, &buffered_stream));
 
   BenchmarkStreamingWrites(state, large_sizes, buffered_stream.get(), reader.get());
 }
