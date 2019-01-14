6452792f20da7bb1e49adc26ac77c2f35b057e74
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-01-14 22:05:11 +0100

    lint

--- python/pyarrow/tests/test_convert_pandas.py
+++ python/pyarrow/tests/test_convert_pandas.py
@@ -2225,7 +2225,12 @@ class TestConvertMisc(object):
 
 
 def test_safe_cast_from_float_with_nans_to_int():
-    pa.Array.from_pandas(pd.Series([1, None]), type=pa.int32(), safe=True)
+    # TODO(kszucs): write tests for creating Date32 and Date64 arrays, see
+    #               ARROW-4258 and https://github.com/apache/arrow/pull/3395
+    values = pd.Series([1, 2, None, 4])
+    arr = pa.Array.from_pandas(values, type=pa.int32(), safe=True)
+    expected = pa.array([1, 2, None, 4], type=pa.int32())
+    assert arr.equals(expected)
 
 
 def _fully_loaded_dataframe_example():
