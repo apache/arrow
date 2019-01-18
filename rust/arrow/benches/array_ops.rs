0cb88ea0cbf2cbaedf91cf6885c1b3fd73caf574
Paddy Horan <paddyhoran@hotmail.com>
2019-01-17 22:56:49 -0500

    sse and avx2 with benchmark

--- rust/arrow/benches/array_ops.rs
+++ rust/arrow/benches/array_ops.rs
@@ -21,7 +21,8 @@ use criterion::Criterion;
 
 extern crate arrow;
 
-use arrow::array_ops::*;
+use arrow::compute::array_ops::*;
+use arrow::compute::add::{add_sse, add_avx2};
 use arrow::builder::*;
 use arrow::array::*;
 
@@ -40,21 +41,31 @@ fn primitive_array_add(size: usize) {
     criterion::black_box(add(&arr_a, &arr_b).unwrap());
 }
 
-fn primitive_array_add_simd(size: usize) {
+fn primitive_array_add_sse(size: usize) {
     let arr_a = create_array(size);
     let arr_b = create_array(size);
-    criterion::black_box(simd_add(&arr_a, &arr_b).unwrap());
+    criterion::black_box(add_sse(&arr_a, &arr_b).unwrap());
+}
+
+fn primitive_array_add_avx2(size: usize) {
+    let arr_a = create_array(size);
+    let arr_b = create_array(size);
+    criterion::black_box(add_avx2(&arr_a, &arr_b).unwrap());
 }
 
 fn add_benchmark(c: &mut Criterion) {
     c.bench_function("add 128", |b| b.iter(|| primitive_array_add(128)));
-    c.bench_function("add 128 simd", |b| b.iter(|| primitive_array_add_simd(128)));
+    c.bench_function("add 128 simd", |b| b.iter(|| primitive_array_add_sse(128)));
+    c.bench_function("add 128 simd", |b| b.iter(|| primitive_array_add_avx2(128)));
     c.bench_function("add 256", |b| b.iter(|| primitive_array_add(256)));
-    c.bench_function("add 256 simd", |b| b.iter(|| primitive_array_add_simd(256)));
+    c.bench_function("add 256 simd", |b| b.iter(|| primitive_array_add_sse(256)));
+    c.bench_function("add 256 simd", |b| b.iter(|| primitive_array_add_avx2(256)));
     c.bench_function("add 512", |b| b.iter(|| primitive_array_add(512)));
-    c.bench_function("add 512 simd", |b| b.iter(|| primitive_array_add_simd(512)));
+    c.bench_function("add 512 simd", |b| b.iter(|| primitive_array_add_sse(512)));
+    c.bench_function("add 512 simd", |b| b.iter(|| primitive_array_add_avx2(512)));
     c.bench_function("add 1024", |b| b.iter(|| primitive_array_add(1024)));
-    c.bench_function("add 1024 simd", |b| b.iter(|| primitive_array_add_simd(1024)));
+    c.bench_function("add 1024 simd", |b| b.iter(|| primitive_array_add_sse(1024)));
+    c.bench_function("add 1024 simd", |b| b.iter(|| primitive_array_add_avx2(1024)));
 }
 
 criterion_group!(benches, add_benchmark);
--- rust/arrow/src/compute/add.rs
+++ rust/arrow/src/compute/add.rs
@@ -22,7 +22,7 @@ use std::mem;
 use std::slice::from_raw_parts_mut;
 
 use num::Zero;
-use packed_simd::f32x4;
+use packed_simd::*;
 
 use crate::compute::array_ops::math_op;
 use crate::buffer::MutableBuffer;
@@ -30,40 +30,47 @@ use crate::array::*;
 use crate::datatypes;
 use crate::error::{ArrowError, Result};
 
-pub fn simd_add(left: &Float32Array, right: &Float32Array) -> Result<Float32Array>
-{
-    if left.len() != right.len() {
-        return Err(ArrowError::ComputeError(
-            "Cannot perform math operation on arrays of different length".to_string(),
-        ));
-    }
+macro_rules! simd_add {
+    ($instruction_set:ident, $simd_ty:ident, $native_ty:ty, $array_ty:ident) => {
+        pub fn $instruction_set(left: &$array_ty, right: &$array_ty) -> Result<$array_ty>
+        {
+            if left.len() != right.len() {
+                return Err(ArrowError::ComputeError(
+                    "Cannot perform math operation on arrays of different length".to_string(),
+                ));
+            }
 
-    let lanes = f32x4::lanes();
-    let buffer_size = left.len() * mem::size_of::<f32>();
-    let mut result = MutableBuffer::new(buffer_size).with_bitset(buffer_size, false);
+            let lanes = $simd_ty::lanes();
+            let buffer_size = left.len() * mem::size_of::<$native_ty>();
+            let mut result = MutableBuffer::new(buffer_size).with_bitset(buffer_size, false);
 
-    for i in (0..left.len()).step_by(lanes) {
-        let simd_left = unsafe {f32x4::from_slice_unaligned_unchecked(left.value_slice(i, lanes))};
-        let simd_right = unsafe {f32x4::from_slice_unaligned_unchecked(right.value_slice(i, lanes))};
-        let simd_result = simd_left + simd_right;
+            for i in (0..left.len()).step_by(lanes) {
+                let simd_left = unsafe {$simd_ty::from_slice_unaligned_unchecked(left.value_slice(i, lanes))};
+                let simd_right = unsafe {$simd_ty::from_slice_unaligned_unchecked(right.value_slice(i, lanes))};
+                let simd_result = simd_left + simd_right;
 
-        let result_slice: &mut [f32] = unsafe {
-            from_raw_parts_mut(
-                (result.data_mut().as_mut_ptr() as *mut f32).offset(i as isize),
-                lanes
-            )
-        };
-        unsafe {
-            simd_result.write_to_slice_unaligned_unchecked(result_slice);
+                let result_slice: &mut [$native_ty] = unsafe {
+                    from_raw_parts_mut(
+                        (result.data_mut().as_mut_ptr() as *mut $native_ty).offset(i as isize),
+                        lanes
+                    )
+                };
+                unsafe {
+                    simd_result.write_to_slice_unaligned_unchecked(result_slice);
+                }
+            }
+
+            Ok($array_ty::new(
+                left.len(),
+                result.freeze(),
+                0,
+                0))
         }
-    }
-
-    Ok(Float32Array::new(
-        left.len(),
-        result.freeze(),
-        0,
-        0))
+    };
 }
+
+simd_add!(add_avx2, f32x8, f32, Float32Array);
+simd_add!(add_sse, f32x4, f32, Float32Array);
 
 /// Perform `left + right` operation on two arrays. If either left or right value is null then the result is also null.
 pub fn add<T>(left: &PrimitiveArray<T>, right: &PrimitiveArray<T>) -> Result<PrimitiveArray<T>>
@@ -98,10 +105,27 @@ mod tests {
     }
 
     #[test]
-    fn test_simd() {
+    fn test_simd_avx2() {
         let a = Float32Array::from(vec![5.0, 6.0, 7.0, 8.0, 9.0, 10.0]);
         let b = Float32Array::from(vec![6.0, 7.0, 8.0, 9.0, 8.0, 20.0]);
-        let c = simd_add(&a, &b).unwrap();
+        let c = add_avx2(&a, &b).unwrap();
+
+        for i in 0..c.len() {
+            println!("{}: {}", i, c.value(i));
+        }
+        assert_eq!(11.0, c.value(0));
+        assert_eq!(13.0, c.value(1));
+        assert_eq!(15.0, c.value(2));
+        assert_eq!(17.0, c.value(3));
+        assert_eq!(17.0, c.value(4));
+        assert_eq!(30.0, c.value(5));
+    }
+
+    #[test]
+    fn test_simd_sse() {
+        let a = Float32Array::from(vec![5.0, 6.0, 7.0, 8.0, 9.0, 10.0]);
+        let b = Float32Array::from(vec![6.0, 7.0, 8.0, 9.0, 8.0, 20.0]);
+        let c = add_sse(&a, &b).unwrap();
 
         for i in 0..c.len() {
             println!("{}: {}", i, c.value(i));
