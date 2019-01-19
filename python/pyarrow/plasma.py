207a8faec8ea551523fa4238b09cf20e9e9aa363
Anurag Khandelwal <anuragk@berkeley.edu>
2019-01-18 18:22:31 -0800

    Translate plasma changes to pyarrow, add pyarrow tests

--- python/pyarrow/plasma.py
+++ python/pyarrow/plasma.py
@@ -79,7 +79,8 @@ def build_plasma_tensorflow_op():
 def start_plasma_store(plasma_store_memory,
                        use_valgrind=False, use_profiler=False,
                        use_one_memory_mapped_file=False,
-                       plasma_directory=None, use_hugepages=False):
+                       plasma_directory=None, use_hugepages=False,
+                       external_store=None, external_store_parallelism=None):
     """Start a plasma store process.
     Args:
         plasma_store_memory (int): Capacity of the plasma store in bytes.
@@ -92,6 +93,9 @@ def start_plasma_store(plasma_store_memory,
         plasma_directory (str): Directory where plasma memory mapped files
             will be stored.
         use_hugepages (bool): True if the plasma store should use huge pages.
+        external_store (str): External store to use for evicted objects.
+        external_store_parallelism (int): Number of threads to use for reading
+            from/writing to external store.
     Return:
         A tuple of the name of the plasma store socket and the process ID of
             the plasma store process.
@@ -113,6 +117,10 @@ def start_plasma_store(plasma_store_memory,
             command += ["-d", plasma_directory]
         if use_hugepages:
             command += ["-h"]
+        if external_store is not None:
+            command += ["-e", external_store]
+        if external_store_parallelism is not None:
+            command += ["-p", str(external_store_parallelism)]
         stdout_file = None
         stderr_file = None
         if use_valgrind:
