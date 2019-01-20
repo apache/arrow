4d1ee0aaa6993505c91d80931f789c5fa92ee406
Uwe L. Korn <uwelk@xhochy.com>
2019-01-20 19:55:32 +0100

    ARROW-4303: [Gandiva/Python] Build LLVM with RTTI in manylinux1 container
    Author: Uwe L. Korn <uwelk@xhochy.com>
    
    Closes #3436 from xhochy/ARROW-4303 and squashes the following commits:
    
    0f1f4aae <Uwe L. Korn> ARROW-4303:  Build LLVM with RTTI in manylinux1 container

--- python/manylinux1/scripts/build_virtualenvs.sh
+++ python/manylinux1/scripts/build_virtualenvs.sh
@@ -34,7 +34,7 @@ for PYTHON_TUPLE in ${PYTHON_VERSIONS}; do
 
     echo "=== (${PYTHON}, ${U_WIDTH}) Installing build dependencies ==="
     $PIP install "numpy==1.14.5"
-    $PIP install "cython==0.28.1"
+    $PIP install "cython==0.29.3"
     $PIP install "pandas==0.23.4"
     $PIP install "virtualenv==15.1.0"
 
