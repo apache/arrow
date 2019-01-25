108238f7856e17189c063268d49a771ce23581d4
Paddy Horan <paddyhoran@hotmail.com>
2019-01-24 22:24:55 -0500

    Cleaned up testing.

--- rust/arrow/src/compute/arithmetic_kernels.rs
+++ rust/arrow/src/compute/arithmetic_kernels.rs
@@ -84,42 +84,27 @@ mod tests {
     use crate::array::Int32Array;
 
     #[test]
-    fn test_supported() {
-        println!("SSE: {}", is_x86_feature_detected!("sse"));
-        println!("SSE2: {}", is_x86_feature_detected!("sse2"));
-        println!("SSE3: {}", is_x86_feature_detected!("sse3"));
-        println!("SSSE3: {}", is_x86_feature_detected!("ssse3"));
-        println!("SSE4.1: {}", is_x86_feature_detected!("sse4.1"));
-        println!("SSE4.2: {}", is_x86_feature_detected!("sse4.2"));
-        println!("AVX: {}", is_x86_feature_detected!("avx"));
-        println!("AVX2: {}", is_x86_feature_detected!("avx2"));
-        println!("AVX512f: {}", is_x86_feature_detected!("avx512f"));
-    }
-
-    #[test]
-    fn test_simd() {
-        let a = Float32Array::from(vec![5.0, 6.0, 7.0, 8.0, 9.0, 10.0]);
-        let b = Float32Array::from(vec![6.0, 7.0, 8.0, 9.0, 8.0, 20.0]);
-        let c = add_simd(&a, &b).unwrap();
-
-        assert_eq!(11.0, c.value(0));
-        assert_eq!(13.0, c.value(1));
-        assert_eq!(15.0, c.value(2));
-        assert_eq!(17.0, c.value(3));
-        assert_eq!(17.0, c.value(4));
-        assert_eq!(30.0, c.value(5));
-    }
-
-    #[test]
     fn test_primitive_array_add() {
         let a = Int32Array::from(vec![5, 6, 7, 8, 9]);
         let b = Int32Array::from(vec![6, 7, 8, 9, 8]);
-        let c = add(&a, &b).unwrap();
-        assert_eq!(11, c.value(0));
-        assert_eq!(13, c.value(1));
-        assert_eq!(15, c.value(2));
-        assert_eq!(17, c.value(3));
-        assert_eq!(17, c.value(4));
+
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            let c = add_simd(&a, &b).unwrap();
+
+            assert_eq!(11, c.value(0));
+            assert_eq!(13, c.value(1));
+            assert_eq!(15, c.value(2));
+            assert_eq!(17, c.value(3));
+            assert_eq!(17, c.value(4));
+        }
+
+        let d = add(&a, &b).unwrap();
+        assert_eq!(11, d.value(0));
+        assert_eq!(13, d.value(1));
+        assert_eq!(15, d.value(2));
+        assert_eq!(17, d.value(3));
+        assert_eq!(17, d.value(4));
     }
 
     #[test]
