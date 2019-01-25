f08a21756c3d2ce2d854c0b20f5de0b57ee74cfb
Robert Nishihara <robertnishihara@gmail.com>
2019-01-25 14:48:04 -0800

    Register serializers for collections.Counter and collections.deque.

--- python/pyarrow/serialization.py
+++ python/pyarrow/serialization.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from collections import OrderedDict, defaultdict
+import collections
 import six
 import sys
 
@@ -227,6 +227,52 @@ def register_torch_serialization_handlers(serialization_context):
         pass
 
 
+def _register_collections_serialization_handlers(serialization_context):
+    def _serialize_deque(obj):
+        return list(obj)
+
+    def _deserialize_deque(data):
+        return collections.deque(data)
+
+    serialization_context.register_type(
+        collections.deque, "collections.deque",
+        custom_serializer=_serialize_deque,
+        custom_deserializer=_deserialize_deque)
+
+    def _serialize_ordered_dict(obj):
+        return list(obj.keys()), list(obj.values())
+
+    def _deserialize_ordered_dict(data):
+        return collections.OrderedDict(zip(data[0], data[1]))
+
+    serialization_context.register_type(
+        collections.OrderedDict, "collections.OrderedDict",
+        custom_serializer=_serialize_ordered_dict,
+        custom_deserializer=_deserialize_ordered_dict)
+
+    def _serialize_default_dict(obj):
+        return list(obj.keys()), list(obj.values()), obj.default_factory
+
+    def _deserialize_default_dict(data):
+        return collections.defaultdict(data[2], zip(data[0], data[1]))
+
+    serialization_context.register_type(
+        collections.defaultdict, "collections.defaultdict",
+        custom_serializer=_serialize_default_dict,
+        custom_deserializer=_deserialize_default_dict)
+
+    def _serialize_counter(obj):
+        return list(obj.keys()), list(obj.values())
+
+    def _deserialize_counter(data):
+        return collections.Counter(dict(zip(data[0], data[1])))
+
+    serialization_context.register_type(
+        collections.Counter, "collections.Counter",
+        custom_serializer=_serialize_counter,
+        custom_deserializer=_deserialize_counter)
+
+
 def register_default_serialization_handlers(serialization_context):
 
     # ----------------------------------------------------------------------
@@ -245,28 +291,6 @@ def register_default_serialization_handlers(serialization_context):
             long, "long",  # noqa: F821
             custom_serializer=lambda obj: str(obj),
             custom_deserializer=lambda data: long(data))  # noqa: F821
-
-    def _serialize_ordered_dict(obj):
-        return list(obj.keys()), list(obj.values())
-
-    def _deserialize_ordered_dict(data):
-        return OrderedDict(zip(data[0], data[1]))
-
-    serialization_context.register_type(
-        OrderedDict, "OrderedDict",
-        custom_serializer=_serialize_ordered_dict,
-        custom_deserializer=_deserialize_ordered_dict)
-
-    def _serialize_default_dict(obj):
-        return list(obj.keys()), list(obj.values()), obj.default_factory
-
-    def _deserialize_default_dict(data):
-        return defaultdict(data[2], zip(data[0], data[1]))
-
-    serialization_context.register_type(
-        defaultdict, "defaultdict",
-        custom_serializer=_serialize_default_dict,
-        custom_deserializer=_deserialize_default_dict)
 
     serialization_context.register_type(
         type(lambda: 0), "function",
@@ -294,6 +318,7 @@ def register_default_serialization_handlers(serialization_context):
         custom_serializer=_serialize_pyarrow_table,
         custom_deserializer=_deserialize_pyarrow_table)
 
+    _register_collections_serialization_handlers(serialization_context)
     _register_custom_pandas_handlers(serialization_context)
 
 
