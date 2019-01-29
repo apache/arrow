df1bfc0169a18c8cd26056db7254ff8c3df53160
Wes McKinney <wesm+git@apache.org>
2019-01-29 10:26:57 -0600

    lint
    Change-Id: Ic7faef17bc54e81d4975eece5d2f7d316d87f03d

--- cpp/src/parquet/arrow/record_reader.cc
+++ cpp/src/parquet/arrow/record_reader.cc
@@ -434,7 +434,8 @@ class RecordReader::RecordReaderImpl {
   int64_t levels_capacity_;
 
   std::shared_ptr<::arrow::ResizableBuffer> values_;
-  // In the case of false, don't allocate the values buffer (when we directly read into builder classes).
+  // In the case of false, don't allocate the values buffer (when we directly read into
+  // builder classes).
   bool uses_values_;
 
   template <typename T>
