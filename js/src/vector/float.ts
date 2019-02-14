854ae66f3ca1d0babe980b563a7d1ca65cda2c12
ptaylor <paul.e.taylor@me.com>
2019-02-14 07:36:32 -0800

    ensure Int/FloatVector.from return signatures are as specific as possible, and accept Iterable<number>

--- js/src/vector/float.ts
+++ js/src/vector/float.ts
@@ -18,20 +18,38 @@
 import { Data } from '../data';
 import { Vector } from '../vector';
 import { BaseVector } from './base';
+import { Vector as V } from '../interfaces';
 import { Float, Float16, Float32, Float64 } from '../type';
+import { toFloat16Array, toFloat32Array, toFloat64Array } from '../util/buffer';
 
 export class FloatVector<T extends Float = Float> extends BaseVector<T> {
-    public static from<T extends Float16>(data: T['TArray']): Float16Vector;
-    public static from<T extends Float32>(data: T['TArray']): Float32Vector;
-    public static from<T extends Float64>(data: T['TArray']): Float64Vector;
+
+    public static from(this: typeof FloatVector, data: Float16['TArray']): Float16Vector;
+    public static from(this: typeof FloatVector, data: Float32['TArray']): Float32Vector;
+    public static from(this: typeof FloatVector, data: Float64['TArray']): Float64Vector;
+    public static from<T extends Float>(this: typeof FloatVector, data: T['TArray']): V<T>;
+
+    public static from(this: typeof Float16Vector, data: Float16['TArray'] | Iterable<number>): Float16Vector;
+    public static from(this: typeof Float32Vector, data: Float32['TArray'] | Iterable<number>): Float32Vector;
+    public static from(this: typeof Float64Vector, data: Float64['TArray'] | Iterable<number>): Float64Vector;
     /** @nocollapse */
     public static from<T extends Float>(data: T['TArray']) {
+        let type: Float | null = null;
+        switch (this) {
+            case Float16Vector: data = toFloat16Array(data); break;
+            case Float32Vector: data = toFloat32Array(data); break;
+            case Float64Vector: data = toFloat64Array(data); break;
+        }
         switch (data.constructor) {
             case Uint16Array: return Vector.new(Data.Float(new Float16(), 0, data.length, 0, null, data));
-            case Float32Array: return Vector.new(Data.Float(new Float32(), 0, data.length, 0, null, data));
-            case Float64Array: return Vector.new(Data.Float(new Float64(), 0, data.length, 0, null, data));
+            case Uint16Array:  type = new Float16(); break;
+            case Float32Array: type = new Float32(); break;
+            case Float64Array: type = new Float64(); break;
         }
         throw new TypeError('Unrecognized Float data');
+        return type !== null
+            ? Vector.new(Data.Float(type, 0, data.length, 0, null, data))
+            : (() => { throw new TypeError('Unrecognized FloatVector input'); })();
     }
 }
 
