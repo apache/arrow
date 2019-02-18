69abf40676e0b43e31e7475fecfc475b31fc3ba1
ptaylor <paul.e.taylor@me.com>
2019-02-17 18:18:35 -0800

    add initial RecordBatch.new and select tests

--- js/src/recordbatch.ts
+++ js/src/recordbatch.ts
@@ -100,7 +100,7 @@ export class RecordBatch<T extends { [key: string]: DataType } = any>
     }
     public selectAt<K extends T[keyof T] = any>(...columnIndices: number[]) {
         const schema = this._schema.selectAt(...columnIndices);
-        const children = columnIndices.map((i) => this.data.childData[i]);
-        return new RecordBatch<{ [key: string]: K }>(schema, this.length, children);
+        const childData = columnIndices.map((i) => this.data.childData[i]).filter(Boolean);
+        return new RecordBatch<{ [key: string]: K }>(schema, this.length, childData);
     }
 }
