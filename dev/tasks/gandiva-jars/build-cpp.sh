717328f6aa5fb4a7041c9f8215edd340a20d6f08
Pindikura Ravindra <ravindra@dremio.com>
2019-02-21 13:57:04 +0900

    ARROW-4639: [CI] Switch off GFLAGS_SHARED for osx
    Author: Pindikura Ravindra <ravindra@dremio.com>
    
    Closes #3720 from pravindra/gflags and squashes the following commits:
    
    77373951 <Pindikura Ravindra> ARROW-4639:  Switch off GFLAGS_SHARED for osx

--- dev/tasks/gandiva-jars/build-cpp.sh
+++ dev/tasks/gandiva-jars/build-cpp.sh
@@ -25,14 +25,19 @@ source arrow/ci/travis_env_common.sh
 pushd arrow/cpp
   mkdir build
   pushd build
-    cmake -DCMAKE_BUILD_TYPE=Release \
+    CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release \
           -DARROW_GANDIVA=ON \
           -DARROW_GANDIVA_JAVA=ON \
           -DARROW_GANDIVA_STATIC_LIBSTDCPP=ON \
           -DARROW_BUILD_TESTS=ON \
           -DARROW_BUILD_UTILITIES=OFF \
-          -DARROW_BOOST_USE_SHARED=OFF \
-          ..
+          -DARROW_BOOST_USE_SHARED=OFF"
+
+    if [ $TRAVIS_OS_NAME == "osx" ]; then
+      CMAKE_FLAGS="$CMAKE_FLAGS -DARROW_GFLAGS_USE_SHARED=OFF"
+    fi
+
+    cmake $CMAKE_FLAGS ..
     make -j4
     ctest
   popd
