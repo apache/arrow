7624a972f49af71d8ad0f4d88f516471e1fc93e1
Anson Qian <abq@uber.com>
2019-03-03 14:36:01 -0800

    Add example and bump up test coverage

--- go/arrow/csv/writer_test.go
+++ go/arrow/csv/writer_test.go
@@ -18,6 +18,8 @@ package csv_test
 
 import (
 	"bytes"
+	"fmt"
+	"log"
 	"strings"
 	"testing"
 
@@ -27,11 +29,10 @@ import (
 	"github.com/apache/arrow/go/arrow/memory"
 )
 
-func TestCSVWriter(t *testing.T) {
+func Example_writer() {
 	f := new(bytes.Buffer)
 
-	pool := memory.NewCheckedAllocator(memory.NewGoAllocator())
-	defer pool.AssertSize(t, 0)
+	pool := memory.NewGoAllocator()
 	schema := arrow.NewSchema(
 		[]arrow.Field{
 			{Name: "i64", Type: arrow.PrimitiveTypes.Int64},
@@ -51,22 +52,108 @@ func TestCSVWriter(t *testing.T) {
 	rec := b.NewRecord()
 	defer rec.Release()
 
+	w := csv.NewWriter(f, schema, csv.WithComma(';'))
+	err := w.Write(rec)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(f, schema, csv.WithComment('#'), csv.WithComma(';'))
+	defer r.Release()
+
+	n := 0
+	for r.Next() {
+		rec := r.Record()
+		for i, col := range rec.Columns() {
+			fmt.Printf("rec[%d][%q]: %v\n", i, rec.ColumnName(i), col)
+		}
+		n++
+	}
+
+	// Output:
+	// rec[0]["i64"]: [0]
+	// rec[1]["f64"]: [0]
+	// rec[2]["str"]: ["str-0"]
+	// rec[0]["i64"]: [1]
+	// rec[1]["f64"]: [1]
+	// rec[2]["str"]: ["str-1"]
+	// rec[0]["i64"]: [2]
+	// rec[1]["f64"]: [2]
+	// rec[2]["str"]: ["str-2"]
+	// rec[0]["i64"]: [3]
+	// rec[1]["f64"]: [3]
+	// rec[2]["str"]: ["str-3"]
+	// rec[0]["i64"]: [4]
+	// rec[1]["f64"]: [4]
+	// rec[2]["str"]: ["str-4"]
+	// rec[0]["i64"]: [5]
+	// rec[1]["f64"]: [5]
+	// rec[2]["str"]: ["str-5"]
+	// rec[0]["i64"]: [6]
+	// rec[1]["f64"]: [6]
+	// rec[2]["str"]: ["str-6"]
+	// rec[0]["i64"]: [7]
+	// rec[1]["f64"]: [7]
+	// rec[2]["str"]: ["str-7"]
+	// rec[0]["i64"]: [8]
+	// rec[1]["f64"]: [8]
+	// rec[2]["str"]: ["str-8"]
+	// rec[0]["i64"]: [9]
+	// rec[1]["f64"]: [9]
+	// rec[2]["str"]: ["str-9"]
+}
+
+func TestCSVWriter(t *testing.T) {
+	f := new(bytes.Buffer)
+
+	pool := memory.NewCheckedAllocator(memory.NewGoAllocator())
+	defer pool.AssertSize(t, 0)
+	schema := arrow.NewSchema(
+		[]arrow.Field{
+			{Name: "bool", Type: arrow.FixedWidthTypes.Boolean},
+			{Name: "i8", Type: arrow.PrimitiveTypes.Int8},
+			{Name: "i16", Type: arrow.PrimitiveTypes.Int16},
+			{Name: "i32", Type: arrow.PrimitiveTypes.Int32},
+			{Name: "i64", Type: arrow.PrimitiveTypes.Int64},
+			{Name: "u8", Type: arrow.PrimitiveTypes.Uint8},
+			{Name: "u16", Type: arrow.PrimitiveTypes.Uint16},
+			{Name: "u32", Type: arrow.PrimitiveTypes.Uint32},
+			{Name: "u64", Type: arrow.PrimitiveTypes.Uint64},
+			{Name: "f32", Type: arrow.PrimitiveTypes.Float32},
+			{Name: "f64", Type: arrow.PrimitiveTypes.Float64},
+			{Name: "str", Type: arrow.BinaryTypes.String},
+		},
+		nil,
+	)
+
+	b := array.NewRecordBuilder(pool, schema)
+	defer b.Release()
+
+	b.Field(0).(*array.BooleanBuilder).AppendValues([]bool{true, false, true}, nil)
+	b.Field(1).(*array.Int8Builder).AppendValues([]int8{-1, 0, 1}, nil)
+	b.Field(2).(*array.Int16Builder).AppendValues([]int16{-1, 0, 1}, nil)
+	b.Field(3).(*array.Int32Builder).AppendValues([]int32{-1, 0, 1}, nil)
+	b.Field(4).(*array.Int64Builder).AppendValues([]int64{-1, 0, 1}, nil)
+	b.Field(5).(*array.Uint8Builder).AppendValues([]uint8{0, 1, 2}, nil)
+	b.Field(6).(*array.Uint16Builder).AppendValues([]uint16{0, 1, 2}, nil)
+	b.Field(7).(*array.Uint32Builder).AppendValues([]uint32{0, 1, 2}, nil)
+	b.Field(8).(*array.Uint64Builder).AppendValues([]uint64{0, 1, 2}, nil)
+	b.Field(9).(*array.Float32Builder).AppendValues([]float32{0.0, 0.1, 0.2}, nil)
+	b.Field(10).(*array.Float64Builder).AppendValues([]float64{0.0, 0.1, 0.2}, nil)
+	b.Field(11).(*array.StringBuilder).AppendValues([]string{"str-0", "str-1", "str-2"}, nil)
+
+	rec := b.NewRecord()
+	defer rec.Release()
+
 	w := csv.NewWriter(f, schema, csv.WithComma(';'), csv.WithCRLF(false))
 	err := w.Write(rec)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	want := `0;0;str-0
-1;1;str-1
-2;2;str-2
-3;3;str-3
-4;4;str-4
-5;5;str-5
-6;6;str-6
-7;7;str-7
-8;8;str-8
-9;9;str-9
+	want := `true;-1;-1;-1;-1;0;0;0;0;0;0;str-0
+false;0;0;0;0;1;1;1;1;0.1;0.1;str-1
+true;1;1;1;1;2;2;2;2;0.2;0.2;str-2
 `
 
 	if got, want := f.String(), want; strings.Compare(got, want) != 0 {
