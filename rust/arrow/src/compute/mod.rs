fef57b282237b6becb6070e7a17dd6b7ff090c64
Paddy Horan <paddyhoran@hotmail.com>
2019-03-03 22:27:56 -0500

    Initial implementation

--- rust/arrow/src/compute/mod.rs
+++ rust/arrow/src/compute/mod.rs
@@ -20,9 +20,11 @@
 pub mod arithmetic_kernels;
 pub mod array_ops;
 pub mod boolean_kernels;
+pub mod comparison_kernels;
 
 mod util;
 
 pub use self::arithmetic_kernels::*;
 pub use self::array_ops::*;
 pub use self::boolean_kernels::*;
+pub use self::comparison_kernels::*;
