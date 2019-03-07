a18da7ad4d5331cc305046aeed55b4220366c482
Prashanth Govindarajan <prgovi@microsoft.com>
2019-03-07 10:04:54 -0800

    Address commend and update Eric's PR to use the new compare

--- csharp/test/Apache.Arrow.Tests/ArrowStreamReaderTests.cs
+++ csharp/test/Apache.Arrow.Tests/ArrowStreamReaderTests.cs
@@ -64,7 +64,7 @@ namespace Apache.Arrow.Tests
         private void CompareSchemas(Schema expectedSchema, Schema actualSchema)
         {
             Assert.Equal(expectedSchema.Fields.Count, actualSchema.Fields.Count);
-            // TODO: compare fields once https://github.com/apache/arrow/pull/3662 is in
+            Assert.True(SchemaBuilderTests.SchemaComparer.Equals(expectedSchema, actualSchema));
         }
 
         private class ArrayComparer :
