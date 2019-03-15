5506695011419e47c5764debcbc0d082d8891988
Kouhei Sutou <kou@clear-code.com>
2019-03-16 01:13:50 +0900

    Add primitive array tests

--- ruby/red-arrow/test/test-record-batch-raw-records.rb
+++ ruby/red-arrow/test/test-record-batch-raw-records.rb
@@ -163,6 +163,190 @@ class RecordBatchRawRecordsTest < Test::Unit::TestCase
     assert_equal(records, record_batch.raw_records)
   end
 
+  test("StringArray") do
+    records = [
+      ["apple"],
+      [nil],
+      ["\u3042"], # U+3042 HIRAGANA LETTER A
+    ]
+    record_batch = Arrow::RecordBatch.new({column: :string},
+                                          records)
+    assert_equal(records, record_batch.raw_records)
+  end
+
+  test("Date32Array") do
+    records = [
+      [Date.new(1960, 1, 1)],
+      [nil],
+      [Date.new(2017, 8, 23)],
+    ]
+    record_batch = Arrow::RecordBatch.new({column: :date32},
+                                          records)
+    assert_equal(records, record_batch.raw_records)
+  end
+
+  test("Date64Array") do
+    records = [
+      [DateTime.new(1960, 1, 1, 2, 9, 30)],
+      [nil],
+      [DateTime.new(2017, 8, 23, 14, 57, 2)],
+    ]
+    record_batch = Arrow::RecordBatch.new({column: :date64},
+                                          records)
+    assert_equal(records, record_batch.raw_records)
+  end
+
+  sub_test_case("TimestampArray") do
+    test("second") do
+      records = [
+        [Time.parse("1960-01-01T02:09:30Z")],
+        [nil],
+        [Time.parse("2017-08-23T14:57:02Z")],
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :timestamp,
+                                                unit: :second,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("milli") do
+      records = [
+        [Time.parse("1960-01-01T02:09:30.123Z")],
+        [nil],
+        [Time.parse("2017-08-23T14:57:02.987Z")],
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :timestamp,
+                                                unit: :milli,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("micro") do
+      records = [
+        [Time.parse("1960-01-01T02:09:30.123456Z")],
+        [nil],
+        [Time.parse("2017-08-23T14:57:02.987654Z")],
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :timestamp,
+                                                unit: :micro,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("nano") do
+      records = [
+        [Time.parse("1960-01-01T02:09:30.123456789Z")],
+        [nil],
+        [Time.parse("2017-08-23T14:57:02.987654321Z")],
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :timestamp,
+                                                unit: :nano,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+  end
+
+  sub_test_case("Time32Array") do
+    test("second") do
+      records = [
+        [60 * 10], # 00:10:00
+        [nil],
+        [60 * 60 * 2 + 9], # 02:00:09
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :time32,
+                                                unit: :second,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("milli") do
+      records = [
+        [(60 * 10) * 1000 + 123], # 00:10:00.123
+        [nil],
+        [(60 * 60 * 2 + 9) * 1000 + 987], # 02:00:09.987
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :time32,
+                                                unit: :milli,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+  end
+
+  sub_test_case("Time64Array") do
+    test("micro") do
+      records = [
+        [(60 * 10) * 1_000_000 + 123_456], # 00:10:00.123456
+        [nil],
+        [(60 * 60 * 2 + 9) * 1_000_000 + 987_654], # 02:00:09.987654
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :time64,
+                                                unit: :micro,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("nano") do
+      records = [
+        [(60 * 10) * 1_000_000_000 + 123_456_789], # 00:10:00.123456789
+        [nil],
+        [(60 * 60 * 2 + 9) * 1_000_000_000 + 987_654_321], # 02:00:09.987654321
+      ]
+      record_batch = Arrow::RecordBatch.new({
+                                              column: {
+                                                type: :time64,
+                                                unit: :nano,
+                                              }
+                                            },
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+  end
+
+  test("Decimal128Array") do
+    records = [
+      [BigDecimal("92.92")],
+      [nil],
+      [BigDecimal("29.29")],
+    ]
+    record_batch = Arrow::RecordBatch.new({
+                                            column: {
+                                              type: :decimal128,
+                                              precision: 8,
+                                              scale: 2,
+                                            }
+                                          },
+                                          records)
+    assert_equal(records, record_batch.raw_records)
+  end
+
   sub_test_case("with basic arrays") do
     def setup
       @string_values = ["apple", "orange", "watermelon", nil, "タコ"]
