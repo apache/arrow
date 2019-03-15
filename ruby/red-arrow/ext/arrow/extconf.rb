ce67b5bb8bb17fcd471e8e10013ee598d9b9ec75
Kouhei Sutou <kou@clear-code.com>
2019-03-15 23:51:24 +0900

    Stop reusing block argument name

--- ruby/red-arrow/ext/arrow/extconf.rb
+++ ruby/red-arrow/ext/arrow/extconf.rb
@@ -36,9 +36,9 @@ end
 
 [
   ["glib2", "ext/glib2"],
-].each do |name, source_dir|
+].each do |name, relative_source_dir|
   spec = find_gem_spec(name)
-  source_dir = File.join(spec.full_gem_path, source_dir)
+  source_dir = File.join(spec.full_gem_path, relative_source_dir)
   build_dir = source_dir
   add_depend_package_path(name, source_dir, build_dir)
 end
