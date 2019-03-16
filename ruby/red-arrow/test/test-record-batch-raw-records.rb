c065a8a232b1fd93a55c15aaa6782c602a6c1cef
Kouhei Sutou <kou@clear-code.com>
2019-03-16 13:05:44 +0900

    Add tests for StructArray#raw_records

--- ruby/red-arrow/test/test-record-batch-raw-records.rb
+++ ruby/red-arrow/test/test-record-batch-raw-records.rb
@@ -165,7 +165,7 @@ class RecordBatchRawRecordsTest < Test::Unit::TestCase
 
   test("StringArray") do
     records = [
-      ["apple"],
+      ["Ruby"],
       [nil],
       ["\u3042"], # U+3042 HIRAGANA LETTER A
     ]
@@ -500,7 +500,7 @@ class RecordBatchRawRecordsTest < Test::Unit::TestCase
       records = [
         [
           [
-            "apple",
+            "Ruby",
             nil,
             "\u3042", # U+3042 HIRAGANA LETTER A
           ],
@@ -720,6 +720,433 @@ class RecordBatchRawRecordsTest < Test::Unit::TestCase
           ],
         ],
         [nil],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(type: :list,
+                                                   field: {
+                                                     name: :sub_element,
+                                                     type: :boolean,
+                                                   }),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("StructArray") do
+      records = [
+        [
+          [
+            {"field" => true},
+            nil,
+            {"field" => nil},
+          ],
+        ],
+        [nil],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(type: :struct,
+                                                   fields: [
+                                                     {
+                                                       name: :field,
+                                                       type: :boolean,
+                                                     },
+                                                   ]),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("SparseUnionArray") do
+      omit("Need to add support for SparseUnionArrayBuilder")
+      records = [
+        [
+          [
+            {"field1" => true},
+            nil,
+            {"field2" => nil},
+          ],
+        ],
+        [nil],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(type: :sparse_union,
+                                                   fields: [
+                                                     {
+                                                       name: :field1,
+                                                       type: :boolean,
+                                                     },
+                                                     {
+                                                       name: :field2,
+                                                       type: :uint8,
+                                                     },
+                                                   ],
+                                                   type_codes: [0, 1]),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("DenseUnionArray") do
+      omit("Need to add support for DenseUnionArrayBuilder")
+      records = [
+        [
+          [
+            {"field1" => true},
+            nil,
+            {"field2" => nil},
+          ],
+        ],
+        [nil],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(type: :dense_union,
+                                                   fields: [
+                                                     {
+                                                       name: :field1,
+                                                       type: :boolean,
+                                                     },
+                                                     {
+                                                       name: :field2,
+                                                       type: :uint8,
+                                                     },
+                                                   ],
+                                                   type_codes: [0, 1]),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("DictionaryArray") do
+      omit("Need to add support for DictionaryArrayBuilder")
+      records = [
+        [
+          [
+            "Ruby",
+            nil,
+            "GLib",
+          ],
+        ],
+        [nil],
+      ]
+      dictionary = Arrow::StringArray.new(["GLib", "Ruby"])
+      record_batch = Arrow::RecordBatch.new(fields(type: :dictionary,
+                                                   index_data_type: :int8,
+                                                   dictionary: dictionary,
+                                                   ordered: true),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+  end
+
+  sub_test_case("StructArray") do
+    def fields(type)
+      field_description = {
+        name: :field,
+      }
+      if type.is_a?(Hash)
+        field_description = field_description.merge(type)
+      else
+        field_description[:type] = type
+      end
+      {
+        column: {
+          type: :struct,
+          fields: [
+            field_description,
+          ],
+        },
+      }
+    end
+
+    test("NullArray") do
+      omit("Need to add support for NullArrayBuilder")
+      records = [
+        [{"field" => nil}],
+        nil,
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:null),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("BooleanArray") do
+      records = [
+        [{"field" => true}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:boolean),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("Int8Array") do
+      records = [
+        [{"field" => -(2 ** 7)}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:int8),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("UInt8Array") do
+      records = [
+        [{"field" => (2 ** 8) - 1}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:uint8),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("Int16Array") do
+      records = [
+        [{"field" => -(2 ** 15)}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:int16),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("UInt16Array") do
+      records = [
+        [{"field" => (2 ** 16) - 1}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:uint16),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("Int32Array") do
+      records = [
+        [{"field" => -(2 ** 31)}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:int32),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("UInt32Array") do
+      records = [
+        [{"field" => (2 ** 32) - 1}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:uint32),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("Int64Array") do
+      records = [
+        [{"field" => -(2 ** 63)}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:int64),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("UInt64Array") do
+      records = [
+        [{"field" => (2 ** 64) - 1}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:uint64),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("FloatArray") do
+      records = [
+        [{"field" => -1.0}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:float),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("DoubleArray") do
+      records = [
+        [{"field" => -1.0}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:double),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("BinaryArray") do
+      records = [
+        [{"field" => "\xff".b}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:binary),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("StringArray") do
+      records = [
+        [{"field" => "Ruby"}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:string),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("Date32Array") do
+      records = [
+        [{"field" => Date.new(1960, 1, 1)}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:date32),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("Date64Array") do
+      records = [
+        [{"field" => DateTime.new(1960, 1, 1, 2, 9, 30)}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(:date64),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    sub_test_case("TimestampArray") do
+      test("second") do
+        records = [
+          [{"field" => Time.parse("1960-01-01T02:09:30Z")}],
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :timestamp,
+                                                     unit: :second),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+
+      test("milli") do
+        records = [
+          [{"field" => Time.parse("1960-01-01T02:09:30.123Z")}],
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :timestamp,
+                                                     unit: :milli),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+
+      test("micro") do
+        records = [
+          [{"field" => Time.parse("1960-01-01T02:09:30.123456Z")}],
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :timestamp,
+                                                     unit: :micro),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+
+      test("nano") do
+        records = [
+          [{"field" => Time.parse("1960-01-01T02:09:30.123456789Z")}],
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :timestamp,
+                                                     unit: :nano),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+    end
+
+    sub_test_case("Time32Array") do
+      test("second") do
+        records = [
+          [{"field" => 60 * 10}], # 00:10:00
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :time32,
+                                                     unit: :second),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+
+      test("milli") do
+        records = [
+          [{"field" => (60 * 10) * 1000 + 123}], # 00:10:00.123
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :time32,
+                                                     unit: :milli),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+    end
+
+    sub_test_case("Time64Array") do
+      test("micro") do
+        records = [
+          [{"field" => (60 * 10) * 1_000_000 + 123_456}], # 00:10:00.123456
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :time64,
+                                                     unit: :micro),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+
+      test("nano") do
+        records = [
+          # 00:10:00.123456789
+          [{"field" => (60 * 10) * 1_000_000_000 + 123_456_789}],
+          [nil],
+          [{"field" => nil}],
+        ]
+        record_batch = Arrow::RecordBatch.new(fields(type: :time64,
+                                                     unit: :nano),
+                                              records)
+        assert_equal(records, record_batch.raw_records)
+      end
+    end
+
+    test("Decimal128Array") do
+      records = [
+        [{"field" => BigDecimal("92.92")}],
+        [nil],
+        [{"field" => nil}],
+      ]
+      record_batch = Arrow::RecordBatch.new(fields(type: :decimal128,
+                                                   precision: 8,
+                                                   scale: 2),
+                                            records)
+      assert_equal(records, record_batch.raw_records)
+    end
+
+    test("ListArray") do
+      records = [
+        [{"field" => [true, nil, false]}],
+        [nil],
+        [{"field" => nil}],
       ]
       record_batch = Arrow::RecordBatch.new(fields(type: :list,
                                                    field: {
