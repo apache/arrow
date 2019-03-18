81e53ac76f07418792f4b60894f8b58e1ce2a0dd
Philipp Moritz <pcmoritz@gmail.com>
2019-03-18 14:36:00 -0700

    increase timeout

--- cpp/src/plasma/io.cc
+++ cpp/src/plasma/io.cc
@@ -32,7 +32,7 @@ using arrow::Status;
 /// Number of times we try connecting to a socket.
 constexpr int64_t kNumConnectAttempts = 50;
 /// Time to wait between connection attempts to a socket.
-constexpr int64_t kConnectTimeoutMs = 100;
+constexpr int64_t kConnectTimeoutMs = 200;
 
 namespace plasma {
 
