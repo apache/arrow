b7579405bcb668fadb7b09f47110920ab2c275e9
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-03-21 09:49:46 +0100

    missing re2 [skip ci]

--- dev/tasks/python-wheels/win-build.bat
+++ dev/tasks/python-wheels/win-build.bat
@@ -27,7 +27,7 @@ conda install -n arrow -q -y -c conda-forge ^
       cmake ^
       boost-cpp thrift-cpp ^
       gflags snappy zlib brotli zstd lz4-c double-conversion ^
-      llvmdev libprotobuf
+      llvmdev libprotobuf re2
 
 call activate arrow
 
