b3e70b36683300c0d4c452e6834e477d60414918
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2019-03-21 16:55:13 +0100

    ARROW-4356: [CI] Add integration (docker) test for turbodbc

---
+++ integration/turbodbc/runtest.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+set -e
+
+# check that optional pyarrow modules are available
+# because pytest would just skip the pyarrow tests
+python -c "import pyarrow.orc"
+python -c "import pyarrow.parquet"
+
+pushd /tmp
+git clone https://github.com/xhochy/turbodbc.git
+pushd turbodbc
+git checkout arrow-0.13.0-prep
+git submodule update --init --recursive
+
+service postgresql start
+sudo -u postgres ./travis/setup_test_dbs.sh
+
+mkdir build
+pushd build
+cmake -DCMAKE_INSTALL_PREFIX=./dist -DPYTHON_EXECUTABLE=`which python` -GNinja ..
+ninja install
+ctest --output-on-failure
