193898a270c5244b70e43844a37b85156b969f1a
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-03-21 14:26:32 +0100

    try ldd pyarrow on appveyor [skip ci]

--- dev/tasks/python-wheels/win-build.bat
+++ dev/tasks/python-wheels/win-build.bat
@@ -93,6 +93,8 @@ pip install -vv --no-index --find-links=%ARROW_SRC%\python\dist\ pyarrow
 
 ls -lah C:\Miniconda35-x64\envs\wheel-test\lib\site-packages\pyarrow
 
+ldd C:\Miniconda35-x64\envs\wheel-test\lib\site-packages\pyarrow\lib.cp36-win_amd64.pyd
+
 @rem test the imports
 python -c "import pyarrow; import pyarrow.parquet; import pyarrow.gandiva;"
 
