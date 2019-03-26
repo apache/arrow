676cd9364abdd03a94e8869b053c48281e475c0c
Kouhei Sutou <kou@clear-code.com>
2019-03-27 06:17:59 +0900

    ARROW-5018: [Release] Include JavaScript implementation
    Author: Kouhei Sutou <kou@clear-code.com>
    
    Closes #4039 from kou/release-include-javascript and squashes the following commits:
    
    e7dcdc4b <Kouhei Sutou>  Include JavaScript implementation

--- dev/release/02-source.sh
+++ dev/release/02-source.sh
@@ -101,7 +101,53 @@ svn ci -m "Apache Arrow ${version} RC${rc}" tmp/${tagrc}
 # clean up
 rm -rf tmp
 
+rc_url="https://dist.apache.org/repos/dist/dev/arrow/${tagrc}"
 echo "Success! The release candidate is available here:"
-echo "  https://dist.apache.org/repos/dist/dev/arrow/${tagrc}"
+echo "  ${rc_url}"
 echo ""
 echo "Commit SHA1: ${release_hash}"
+echo ""
+echo "The following draft email has been created to send to the"
+echo "dev@arrow.apache.org mailing list"
+echo ""
+echo "---------------------------------------------------------"
+jira_url="https://issues.apache.org/jira"
+jql="project%20%3D%20ARROW%20AND%20status%20in%20(Resolved%2C%20Closed)%20AND%20fixVersion%20%3D%20${version}"
+n_resolved_issues=$(curl "${jira_url}/rest/api/2/search/?jql=${jql}" | jq ".total")
+cat <<MAIL
+To: dev@arrow.apache.org
+Subject: [VOTE] Release Apache Arrow ${version} - RC${rc}
+
+Hi,
+
+I would like to propose the following release candidate (RC${rc}) of Apache
+Arrow version ${version}. This is a release consiting of ${n_resolved_issues}
+resolved JIRA issues[1].
+
+This release candidate is based on commit:
+${release_hash} [2]
+
+The source release rc${rc} is hosted at [3].
+The binary artifacts are hosted at [4][5][6][7].
+The changelog is located at [8].
+
+Please download, verify checksums and signatures, run the unit tests,
+and vote on the release. See [9] for how to validate a release candidate.
+
+The vote will be open for at least 72 hours.
+
+[ ] +1 Release this as Apache Arrow ${version}
+[ ] +0
+[ ] -1 Do not release this as Apache Arrow ${version} because...
+
+[1]: ${jira_url}/issues/?jql=${jql}
+[2]: https://github.com/apache/arrow/tree/${release_hash}
+[3]: ${rc_url}
+[4]: https://bintray.com/apache/arrow/centos-rc/${version}-rc${rc}
+[5]: https://bintray.com/apache/arrow/debian-rc/${version}-rc${rc}
+[6]: https://bintray.com/apache/arrow/python-rc/${version}-rc${rc}
+[7]: https://bintray.com/apache/arrow/ubuntu-rc/${version}-rc${rc}
+[8]: https://github.com/apache/arrow/blob/${release_hash}/CANGELOG.md
+[9]: https://cwiki.apache.org/confluence/display/ARROW/How+to+Verify+Release+Candidates
+MAIL
+echo "---------------------------------------------------------"
