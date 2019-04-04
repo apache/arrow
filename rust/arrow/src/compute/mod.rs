7a9331689fdffcfead2e5f6be664f1676d489548
Chao Sun <sunchao@apache.org>
2019-04-04 11:28:31 +0200

    ARROW-5116: [Rust] move kernel related files under compute/kernels
    Author: Chao Sun <sunchao@apache.org>
    
    Closes #4108 from sunchao/ARROW-5116 and squashes the following commits:
    
    ca3584f00 <Chao Sun> ARROW-5116:  move kernel related files under compute/kernels

--- rust/arrow/src/compute/mod.rs
+++ rust/arrow/src/compute/mod.rs
@@ -17,17 +17,14 @@
 
 //! Computation kernels on Arrow Arrays
 
-pub mod arithmetic_kernels;
 pub mod array_ops;
-pub mod boolean_kernels;
-pub mod comparison_kernels;
 pub mod kernels;
 
 mod util;
 
-pub use self::arithmetic_kernels::*;
 pub use self::array_ops::*;
-pub use self::boolean_kernels::*;
-pub use self::comparison_kernels::*;
+pub use self::kernels::arithmetic::*;
+pub use self::kernels::boolean::*;
 pub use self::kernels::cast::*;
+pub use self::kernels::comparison::*;
 pub use self::kernels::temporal::*;
