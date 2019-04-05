744c0aeab55e640b59cee2da5cc9c8d650872bf9
Anson Qian <anson627@gmail.com>
2019-04-05 15:49:07 -0700

    Fix header and typo

--- go/arrow/array/half_float_builder_test.go
+++ go/arrow/array/half_float_builder_test.go
@@ -1,3 +1,19 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package array_test
 
 import (
@@ -24,16 +40,16 @@ func TestNewFloat16Builder(t *testing.T) {
 	ab.Append(9)
 	ab.Append(10)
 
-	// check state of builder before NewFloat64Array
+	// check state of builder before NewFloat16Array
 	assert.Equal(t, 10, ab.Len(), "unexpected Len()")
 	assert.Equal(t, 2, ab.NullN(), "unexpected NullN()")
 
 	a := ab.NewFloat16Array()
 
-	// check state of builder after NewFloat64Array
+	// check state of builder after NewFloat16Array
 	assert.Zero(t, ab.Len(), "unexpected ArrayBuilder.Len(), NewFloat16Array did not reset state")
 	assert.Zero(t, ab.Cap(), "unexpected ArrayBuilder.Cap(), NewFloat16Array did not reset state")
-	assert.Zero(t, ab.NullN(), "unexpected ArrayBuilder.NullN(), NewFloat64Array did not reset state")
+	assert.Zero(t, ab.NullN(), "unexpected ArrayBuilder.NullN(), NewFloat16Array did not reset state")
 
 	// check state of array
 
