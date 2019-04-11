e7fdbc4f330316eac3bcb20f604450d3c48e9b2e
Xavier Lange <xrlange@gmail.com>
2019-04-10 21:13:29 -0400

    Licenses

--- rust/parquet_derive/src/lib.rs
+++ rust/parquet_derive/src/lib.rs
@@ -14,6 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
 #![recursion_limit = "128"]
 
 extern crate proc_macro;
