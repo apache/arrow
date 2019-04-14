9676f750c03562adce32405f283cc6723af9d998
Yosuke Shiro <yosuke.shiro615@gmail.com>
2019-04-14 13:10:42 +0900

    Add GArrowTakeOptions

--- c_glib/arrow-glib/compute.hpp
+++ c_glib/arrow-glib/compute.hpp
@@ -30,3 +30,6 @@ GArrowCountOptions *
 garrow_count_options_new_raw(arrow::compute::CountOptions *arrow_count_options);
 arrow::compute::CountOptions *
 garrow_count_options_get_raw(GArrowCountOptions *count_options);
+
+arrow::compute::TakeOptions *
+garrow_take_options_get_raw(GArrowTakeOptions *take_options);
