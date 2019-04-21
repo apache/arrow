f01999bce079021ec765be9ab02cd53ec882c801
Pearu Peterson <pearu.peterson@gmail.com>
2019-04-21 15:47:27 +0300

    Fix python 2.x issue.

--- python/pyarrow/tests/test_parquet_dataset.py
+++ python/pyarrow/tests/test_parquet_dataset.py
@@ -1,3 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 import pyarrow as pa
 import pandas as pd
@@ -5,14 +21,14 @@ import pyarrow.parquet as pq
 
 
 def get_props(obj):
-    """Collect properties of object in a dictionary.
+    """Collect properties of an object into a dictionary.
     """
     import inspect
     d = {}
     for n in dir(obj):
         if n.startswith('_'):
             continue
-        a = inspect.getattr_static(obj, n)
+        a = getattr(type(obj), n)
         if inspect.isdatadescriptor(a):
             try:
                 v = getattr(obj, n)
@@ -49,6 +65,7 @@ def test_dataset_metadata(tempdir):
     metadata_list2 = pq.read_metadata_list(md_path)
 
     # compare metadata list content:
+    assert len(metadata_list) == len(metadata_list2)
     for md, md2 in zip(metadata_list, metadata_list2):
         assert get_props(md) == get_props(md2)
         for r in range(md.num_row_groups):
