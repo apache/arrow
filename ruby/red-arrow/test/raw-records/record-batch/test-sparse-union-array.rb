f6551fdcb67fd10d5bcffec1b37417e2b1513490
Kenta Murata <mrkn@mrkn.jp>
2019-04-25 13:42:03 +0900

    Add tests for SparseUnionArray

--- ruby/red-arrow/test/raw-records/record-batch/test-sparse-union-array.rb
+++ ruby/red-arrow/test/raw-records/record-batch/test-sparse-union-array.rb
@@ -15,58 +15,10 @@
 # specific language governing permissions and limitations
 # under the License.
 
-class RawRecordsRecordBatchSparseUnionArrayTest < Test::Unit::TestCase
-  def fields(type, type_codes)
-    field_description = {}
-    if type.is_a?(Hash)
-      field_description = field_description.merge(type)
-    else
-      field_description[:type] = type
-    end
-    {
-      column: {
-        type: :sparse_union,
-        fields: [
-          field_description.merge(name: "0"),
-          field_description.merge(name: "1"),
-        ],
-        type_codes: type_codes,
-      },
-    }
-  end
+require_relative '../helper/sparse-union-array'
 
-  # TODO: Use Arrow::RecordBatch.new(fields(type), records)
-  def build_record_batch(type, records)
-    type_codes = [0, 1]
-    schema = Arrow::Schema.new(fields(type, type_codes))
-    type_ids = []
-    arrays = schema.fields[0].data_type.fields.collect do |field|
-      sub_schema = Arrow::Schema.new([field])
-      sub_records = records.collect do |record|
-        [record[0].nil? ? nil : record[0][field.name]]
-      end
-      sub_record_batch = Arrow::RecordBatch.new(sub_schema,
-                                                sub_records)
-      sub_record_batch.columns[0]
-    end
-    records.each do |record|
-      column = record[0]
-      if column.nil?
-        type_ids << nil
-      elsif column.key?("0")
-        type_ids << type_codes[0]
-      elsif column.key?("1")
-        type_ids << type_codes[1]
-      end
-    end
-    union_array = Arrow::SparseUnionArray.new(schema.fields[0].data_type,
-                                              Arrow::Int8Array.new(type_ids),
-                                              arrays)
-    schema = Arrow::Schema.new(column: union_array.value_data_type)
-    Arrow::RecordBatch.new(schema,
-                           records.size,
-                           [union_array])
-  end
+class RawRecordsRecordBatchSparseUnionArrayTest < Test::Unit::TestCase
+  include RawRecordsSparseUnionArrayHelper
 
   test("NullArray") do
     records = [
