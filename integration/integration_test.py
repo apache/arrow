0118362ce628e86f1a00f021b5e5d8e66ed32812
Brian Hulette <hulettbh@gmail.com>
2019-04-29 08:16:42 -0700

    skip nested dictionary test

--- integration/integration_test.py
+++ integration/integration_test.py
@@ -957,7 +957,8 @@ def get_generated_json_files(tempdir=None, flight=False):
         generate_datetime_case(),
         generate_nested_case(),
         generate_dictionary_case().skip_category(SKIP_FLIGHT),
-        generate_nested_dictionary_case()
+        generate_nested_dictionary_case().skip_category(SKIP_ARROW)
+                                         .skip_category(SKIP_FLIGHT),
     ]
 
     if flight:
