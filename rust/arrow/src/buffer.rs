1c01fe4fdf758ad2e4f1483129adc7f1c5d84042
Chao Sun <sunchao@apache.org>
2019-05-07 23:12:47 -0700

    Restore padding to 64 bytes

--- rust/arrow/src/buffer.rs
+++ rust/arrow/src/buffer.rs
@@ -137,7 +137,8 @@ impl<T: AsRef<[u8]>> From<T> for Buffer {
         // allocate aligned memory buffer
         let slice = p.as_ref();
         let len = slice.len() * mem::size_of::<u8>();
-        let buffer = memory::allocate_aligned(len);
+        let capacity = bit_util::round_upto_multiple_of_64(len);
+        let buffer = memory::allocate_aligned(capacity);
         unsafe {
             memory::memcpy(buffer, slice.as_ptr(), len);
         }
