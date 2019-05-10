f7113c2cc97b4ca4c762fb2207c527bf0535d397
shengjun.li <shengjun.li@zilliz.com>
2019-05-10 10:32:21 +0800

    ARROW-5283: [C++][Plasma] erase object id in client when abort object

--- cpp/src/plasma/store.cc
+++ cpp/src/plasma/store.cc
@@ -604,6 +604,7 @@ int PlasmaStore::AbortObject(const ObjectID& object_id, Client* client) {
   } else {
     // The client requesting the abort is the creator. Free the object.
     EraseFromObjectTable(object_id);
+    client->object_ids.erase(it);
     return 1;
   }
 }
@@ -727,7 +728,7 @@ void PlasmaStore::DisconnectClient(int client_fd) {
       sealed_objects[it->first] = it->second.get();
     } else {
       // Abort unsealed object.
-      AbortObject(it->first, client);
+      EraseFromObjectTable(object_id);
     }
   }
 
