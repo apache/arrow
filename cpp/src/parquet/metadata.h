9fd10b1bb41c2bbcbc361253d2232e7ce5cb709c
Revital1 Eres <eres@iris-tes-cloud.sl.cloud9.ibm.com>
2019-05-14 15:20:08 +0300

    make format

--- cpp/src/parquet/metadata.h
+++ cpp/src/parquet/metadata.h
@@ -127,8 +127,7 @@ class PARQUET_EXPORT ColumnChunkMetaData {
   static std::unique_ptr<ColumnChunkMetaData> Make(
       const void* metadata, const ColumnDescriptor* descr,
       const ApplicationVersion* writer_version = NULLPTR,
-      InternalFileDecryptor* file_decryptor = NULLPTR,
-      int16_t row_group_ordinal = -1,
+      InternalFileDecryptor* file_decryptor = NULLPTR, int16_t row_group_ordinal = -1,
       int16_t column_ordinal = -1);
 
   ~ColumnChunkMetaData();
@@ -204,7 +203,8 @@ class PARQUET_EXPORT FileMetaData {
 
   ~FileMetaData();
 
-  bool verify_signature(std::shared_ptr<FooterSigningEncryptor> encryptor, const void* tail);
+  bool verify_signature(std::shared_ptr<FooterSigningEncryptor> encryptor,
+                        const void* tail);
   // file metadata
   uint32_t size() const;
   int num_columns() const;
