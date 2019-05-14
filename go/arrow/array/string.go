8611ec0a0b56fca33f3799df6298ac2595efd7c6
Jim <jimwalker@capsule8.com>
2019-05-14 13:23:58 -0400

    arrow/string: move offset logic to value method

--- go/arrow/array/string.go
+++ go/arrow/array/string.go
@@ -46,7 +46,10 @@ func NewStringData(data *Data) *String {
 }
 
 // Value returns the slice at index i. This value should not be mutated.
-func (a *String) Value(i int) string    { return a.values[a.offsets[i]:a.offsets[i+1]] }
+func (a *String) Value(i int) string {
+	i = i + a.array.data.offset
+	return a.values[a.offsets[i]:a.offsets[i+1]]
+}
 func (a *String) ValueOffset(i int) int { return int(a.offsets[i]) }
 
 func (a *String) String() string {
@@ -60,7 +63,7 @@ func (a *String) String() string {
 		case a.IsNull(i):
 			o.WriteString("(null)")
 		default:
-			fmt.Fprintf(o, "%q", a.Value(i+a.array.data.offset))
+			fmt.Fprintf(o, "%q", a.Value(i))
 		}
 	}
 	o.WriteString("]")
