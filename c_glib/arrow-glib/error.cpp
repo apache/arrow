b973a7791415f1a880c2861ddaa2601e0ab44bdc
Yosuke Shiro <yosuke.shiro615@gmail.com>
2019-05-18 07:25:02 +0900

    ARROW-5363: [GLib] Fix coding styles
    This is follow-up of https://github.com/apache/arrow/pull/4317/files#r284887186.
    
    Author: Yosuke Shiro <yosuke.shiro615@gmail.com>
    
    Closes #4339 from shiro615/glib-fix-coding-style and squashes the following commits:
    
    149756ee <Yosuke Shiro>  Fix coding styles

--- c_glib/arrow-glib/error.cpp
+++ c_glib/arrow-glib/error.cpp
@@ -18,7 +18,7 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#  include <config.h>
 #endif
 
 #include <arrow-glib/error.hpp>
@@ -37,68 +37,80 @@ G_BEGIN_DECLS
  * values.
  */
 
-G_DEFINE_QUARK(garrow - error - quark, garrow_error)
+G_DEFINE_QUARK(garrow-error-quark, garrow_error)
 
-static GArrowError garrow_error_code(const arrow::Status& status) {
+static GArrowError
+garrow_error_code(const arrow::Status &status)
+{
   switch (status.code()) {
-    case arrow::StatusCode::OK:
-      return GARROW_ERROR_UNKNOWN;
-    case arrow::StatusCode::OutOfMemory:
-      return GARROW_ERROR_OUT_OF_MEMORY;
-    case arrow::StatusCode::KeyError:
-      return GARROW_ERROR_KEY;
-    case arrow::StatusCode::TypeError:
-      return GARROW_ERROR_TYPE;
-    case arrow::StatusCode::Invalid:
-      return GARROW_ERROR_INVALID;
-    case arrow::StatusCode::IOError:
-      return GARROW_ERROR_IO;
-    case arrow::StatusCode::CapacityError:
-      return GARROW_ERROR_CAPACITY;
-    case arrow::StatusCode::IndexError:
-      return GARROW_ERROR_INDEX;
-    case arrow::StatusCode::UnknownError:
-      return GARROW_ERROR_UNKNOWN;
-    case arrow::StatusCode::NotImplemented:
-      return GARROW_ERROR_NOT_IMPLEMENTED;
-    case arrow::StatusCode::SerializationError:
-      return GARROW_ERROR_SERIALIZATION;
-    case arrow::StatusCode::PythonError:
-      return GARROW_ERROR_PYTHON;
-    case arrow::StatusCode::PlasmaObjectExists:
-      return GARROW_ERROR_PLASMA_OBJECT_EXISTS;
-    case arrow::StatusCode::PlasmaObjectNonexistent:
-      return GARROW_ERROR_PLASMA_OBJECT_NONEXISTENT;
-    case arrow::StatusCode::PlasmaStoreFull:
-      return GARROW_ERROR_PLASMA_STORE_FULL;
-    case arrow::StatusCode::PlasmaObjectAlreadySealed:
-      return GARROW_ERROR_PLASMA_OBJECT_ALREADY_SEALED;
-    case arrow::StatusCode::CodeGenError:
-      return GARROW_ERROR_CODE_GENERATION;
-    case arrow::StatusCode::ExpressionValidationError:
-      return GARROW_ERROR_EXPRESSION_VALIDATION;
-    case arrow::StatusCode::ExecutionError:
-      return GARROW_ERROR_EXECUTION;
-    default:
-      return GARROW_ERROR_UNKNOWN;
+  case arrow::StatusCode::OK:
+    return GARROW_ERROR_UNKNOWN;
+  case arrow::StatusCode::OutOfMemory:
+    return GARROW_ERROR_OUT_OF_MEMORY;
+  case arrow::StatusCode::KeyError:
+    return GARROW_ERROR_KEY;
+  case arrow::StatusCode::TypeError:
+    return GARROW_ERROR_TYPE;
+  case arrow::StatusCode::Invalid:
+    return GARROW_ERROR_INVALID;
+  case arrow::StatusCode::IOError:
+    return GARROW_ERROR_IO;
+  case arrow::StatusCode::CapacityError:
+    return GARROW_ERROR_CAPACITY;
+  case arrow::StatusCode::IndexError:
+    return GARROW_ERROR_INDEX;
+  case arrow::StatusCode::UnknownError:
+    return GARROW_ERROR_UNKNOWN;
+  case arrow::StatusCode::NotImplemented:
+    return GARROW_ERROR_NOT_IMPLEMENTED;
+  case arrow::StatusCode::SerializationError:
+    return GARROW_ERROR_SERIALIZATION;
+  case arrow::StatusCode::PythonError:
+    return GARROW_ERROR_PYTHON;
+  case arrow::StatusCode::PlasmaObjectExists:
+    return GARROW_ERROR_PLASMA_OBJECT_EXISTS;
+  case arrow::StatusCode::PlasmaObjectNonexistent:
+    return GARROW_ERROR_PLASMA_OBJECT_NONEXISTENT;
+  case arrow::StatusCode::PlasmaStoreFull:
+    return GARROW_ERROR_PLASMA_STORE_FULL;
+  case arrow::StatusCode::PlasmaObjectAlreadySealed:
+    return GARROW_ERROR_PLASMA_OBJECT_ALREADY_SEALED;
+  case arrow::StatusCode::CodeGenError:
+    return GARROW_ERROR_CODE_GENERATION;
+  case arrow::StatusCode::ExpressionValidationError:
+    return GARROW_ERROR_EXPRESSION_VALIDATION;
+  case arrow::StatusCode::ExecutionError:
+    return GARROW_ERROR_EXECUTION;
+  default:
+    return GARROW_ERROR_UNKNOWN;
   }
 }
 
 G_END_DECLS
 
-gboolean garrow_error_check(GError** error, const arrow::Status& status,
-                            const char* context) {
+gboolean
+garrow_error_check(GError **error,
+                   const arrow::Status &status,
+                   const char *context)
+{
   if (status.ok()) {
     return TRUE;
   } else {
-    g_set_error(error, GARROW_ERROR, garrow_error_code(status), "%s: %s", context,
+    g_set_error(error,
+                GARROW_ERROR,
+                garrow_error_code(status),
+                "%s: %s",
+                context,
                 status.ToString().c_str());
     return FALSE;
   }
 }
 
-arrow::Status garrow_error_to_status(GError* error, arrow::StatusCode code,
-                                     const char* context) {
+arrow::Status
+garrow_error_to_status(GError *error,
+                       arrow::StatusCode code,
+                       const char *context)
+{
   std::stringstream message;
   message << context << ": " << g_quark_to_string(error->domain);
   message << "(" << error->code << "): ";
