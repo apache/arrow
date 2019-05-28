a6840e15b7235391e6c4117eed5a5f67b27294be
liyafan82 <fan_li_ya@foxmail.com>
2019-05-28 17:55:54 +0800

    [ARROW-5429][Java]Remove useless import

--- java/memory/src/test/java/org/apache/arrow/memory/TestBaseAllocator.java
+++ java/memory/src/test/java/org/apache/arrow/memory/TestBaseAllocator.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
