f9575045f1d9e49a0e05b0aabc676c5790e9eaff
Romain Francois <romain@rstudio.com>
2019-06-06 13:27:27 +0200

    update test_that shim

--- r/tests/testthat/helper-arrow.R
+++ r/tests/testthat/helper-arrow.R
@@ -1,7 +1,7 @@
 
-test_that <- function(what, ...) {
+test_that <- function(what, code) {
   testthat::test_that(what, {
     skip_if(!arrow_available(), "arrow C++ library not available")
-    ...
+    code
   })
 }
