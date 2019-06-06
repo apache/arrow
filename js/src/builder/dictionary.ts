ba755ad6b051a6a160be9448ce264e38db0540c9
ptaylor <paul.e.taylor@me.com>
2019-06-06 16:16:48 -0700

    use 53 bit hash fn to further avoid collisions

--- js/src/builder/dictionary.ts
+++ js/src/builder/dictionary.ts
@@ -79,11 +79,15 @@ export class DictionaryBuilder<T extends Dictionary, TNull = any> extends Builde
         return super.clear();
     }
     public valueToKey(val: any) {
-        typeof val === 'string' || (val = `${val}`);
-        let key = 5381, i = val.length;
-        while (i > 0) {
-            key = (key * 33) ^ val.charCodeAt(--i);
+        let str = typeof val === 'string' ? val : `${val}`;
+        let h1 = 0xdeadbeef ^ 0, h2 = 0x41c6ce57 ^ 0;
+        for (let i = -1, n = str.length, ch; ++i < n;) {
+            ch = str.charCodeAt(i);
+            h1 = Math.imul(h1 ^ ch, 2654435761);
+            h2 = Math.imul(h2 ^ ch, 1597334677);
         }
-        return key >>> 0 as any;
+        h1 = Math.imul(h1 ^ h1 >>> 16, 2246822507) ^ Math.imul(h2 ^ h2 >>> 13, 3266489909);
+        h2 = Math.imul(h2 ^ h2 >>> 16, 2246822507) ^ Math.imul(h1 ^ h1 >>> 13, 3266489909);
+        return 4294967296 * (2097151 & h2) + (h1 >>> 0) as any;
     }
 }
