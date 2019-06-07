1a00cf61aba51ab58196128749c0d123c50f400d
HyukjinKwon <gurwls223@apache.org>
2019-06-07 14:24:40 -0500

    ARROW-4723: [Python] Ignore "hidden" files that starts with underscore
    This PR proposes to ignore "hidden" files that start with underscore as well. This is a Hadoop convention.
    
    Author: HyukjinKwon <gurwls223@apache.org>
    
    Closes #4478 from HyukjinKwon/regex-support and squashes the following commits:
    
    bb6218f53 <HyukjinKwon> Ignore "hidden" files that starts with underscore

--- python/pyarrow/tests/test_parquet.py
+++ python/pyarrow/tests/test_parquet.py
@@ -1953,7 +1953,7 @@ def test_ignore_private_directories(tempdir):
 
 
 @pytest.mark.pandas
-def test_ignore_hidden_files(tempdir):
+def test_ignore_hidden_files_dot(tempdir):
     dirpath = tempdir / guid()
     dirpath.mkdir()
 
@@ -1965,6 +1965,24 @@ def test_ignore_hidden_files(tempdir):
 
     with (dirpath / '.private').open('wb') as f:
         f.write(b'gibberish')
+
+    dataset = pq.ParquetDataset(dirpath)
+    assert set(map(str, paths)) == set(x.path for x in dataset.pieces)
+
+
+@pytest.mark.pandas
+def test_ignore_hidden_files_underscore(tempdir):
+    dirpath = tempdir / guid()
+    dirpath.mkdir()
+
+    paths = _make_example_multifile_dataset(dirpath, nfiles=10,
+                                            file_nrows=5)
+
+    with (dirpath / '_committed_123').open('wb') as f:
+        f.write(b'abcd')
+
+    with (dirpath / '_started_321').open('wb') as f:
+        f.write(b'abcd')
 
     dataset = pq.ParquetDataset(dirpath)
     assert set(map(str, paths)) == set(x.path for x in dataset.pieces)
