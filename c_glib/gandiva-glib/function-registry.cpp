14d0c2208f7cb9abc18f46cbac5d35c29b6a5290
Kenta Murata <mrkn@mrkn.jp>
2019-06-10 20:20:55 +0900

    Fix variable name

--- c_glib/gandiva-glib/function-registry.cpp
+++ c_glib/gandiva-glib/function-registry.cpp
@@ -72,11 +72,11 @@ ggandiva_function_registry_new(void)
 GList *
 ggandiva_function_registry_get_native_functions(GGandivaFunctionRegistry *function_registry)
 {
-  gandiva::FunctionRegistry registry;
+  gandiva::FunctionRegistry gandiva_registry;
 
   GList *native_function_list = nullptr;
-  for (auto gandiva_native_function = registry.begin();
-      gandiva_native_function != registry.end();
+  for (auto gandiva_native_function = gandiva_registry.begin();
+      gandiva_native_function != gandiva_registry.end();
       ++gandiva_native_function) {
     auto native_function = ggandiva_native_function_new_raw(gandiva_native_function);
     native_function_list = g_list_prepend(native_function_list, native_function);
