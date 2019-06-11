ce11144a6d2d30fc0821a9ff94f2694d8f0b6e5a
Kouhei Sutou <kou@clear-code.com>
2019-06-11 16:21:44 +0900

    Add missing config.h include

--- c_glib/gandiva-glib/function-signature.cpp
+++ c_glib/gandiva-glib/function-signature.cpp
@@ -17,6 +17,10 @@
  * under the License.
  */
 
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
 #include <arrow-glib/basic-data-type.hpp>
 
 #include <gandiva-glib/function-signature.hpp>
--- c_glib/gandiva-glib/native-function.cpp
+++ c_glib/gandiva-glib/native-function.cpp
@@ -17,6 +17,10 @@
  * under the License.
  */
 
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
 #include <gandiva-glib/native-function.hpp>
 
 #include <gandiva-glib/function-signature.hpp>
