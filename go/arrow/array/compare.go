a67379b607807402a80475ad9f00a70a26a0eb11
Sebastien Binet <binet@cern.ch>
2019-06-13 12:11:47 +0200

    ARROW-5582: [Go] implement RecordEqual

--- go/arrow/array/compare.go
+++ go/arrow/array/compare.go
@@ -24,6 +24,25 @@ import (
 	"github.com/pkg/errors"
 )
 
+// RecordEqual reports whether the two provided records are equal.
+func RecordEqual(left, right Record) bool {
+	switch {
+	case left.NumCols() != right.NumCols():
+		return false
+	case left.NumRows() != right.NumRows():
+		return false
+	}
+
+	for i := range left.Columns() {
+		lc := left.Column(i)
+		rc := right.Column(i)
+		if !ArrayEqual(lc, rc) {
+			return false
+		}
+	}
+	return true
+}
+
 // ArrayEqual reports whether the two provided arrays are equal.
 func ArrayEqual(left, right Interface) bool {
 	switch {
