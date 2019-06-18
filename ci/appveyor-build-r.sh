5a10965c2468ae9b13b037bb77558e2b2e3452c9
Neal Richardson <neal.p.richardson@gmail.com>
2019-06-17 22:41:01 -0700

    More verbosity

--- ci/appveyor-build-r.sh
+++ ci/appveyor-build-r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -x
 
 # - rmdir /s /Q C:\OpenSSL-Win32 C:\OpenSSL-Win64
-pacman --noconfirm --ask 20 --sync --refresh --refresh --sysupgrade --sysupgrade
 pacman --noconfirm -Rcsu mingw-w64-{i686,x86_64}-toolchain gcc pkg-config
 pacman --noconfirm -Scc
 pacman --noconfirm -Syyu
@@ -10,4 +10,5 @@ pacman --noconfirm --needed -S git base-devel binutils
 # Install core build stuff
 pacman --noconfirm --needed -S mingw-w64-{i686,x86_64}-{crt,winpthreads,gcc,libtre,pkg-config,xz}
 
+cd $APPVEYOR_BUILD_FOLDER
 makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild
