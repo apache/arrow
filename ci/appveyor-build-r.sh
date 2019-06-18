46181a0fd8d93a11232e518ff18b95dcb6d3868b
Neal Richardson <neal.p.richardson@gmail.com>
2019-06-18 12:25:31 -0700

    more sigh

--- ci/appveyor-build-r.sh
+++ ci/appveyor-build-r.sh
@@ -13,7 +13,7 @@ pacman --noconfirm --needed -S git base-devel binutils
 # Install core build stuff
 pacman --noconfirm --needed -S mingw-w64-{i686,x86_64}-{crt,winpthreads,gcc,libtre,pkg-config,xz}
 
-#  Force static linking
+# Force static linking
 rm -f /mingw32/lib/*.dll.a
 rm -f /mingw64/lib/*.dll.a
 export PKG_CONFIG="/${MINGW_INSTALLS}/bin/pkg-config --static"
