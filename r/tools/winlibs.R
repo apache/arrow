e4e127bfff581f90430156f68350a9c3f5753246
Neal Richardson <neal.p.richardson@gmail.com>
2019-06-19 16:01:52 -0700

    Assemble built libraries for rwinlib. Adjust configure.win flags. Temporary hack to test out the R package with the built libs

--- r/tools/winlibs.R
+++ r/tools/winlibs.R
@@ -19,7 +19,8 @@
 VERSION <- commandArgs(TRUE)
 if(!file.exists(sprintf("windows/arrow-%s/include/arrow/api.h", VERSION))){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/arrow/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+  # Temporary hack to test local built lib
+  # download.file(sprintf("https://github.com/rwinlib/arrow/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "windows")
   unlink("lib.zip")
