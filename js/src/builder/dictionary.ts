07fa443c41d54365d055d5319db6d3bc8a0fd16a
ptaylor <paul.e.taylor@me.com>
2019-06-19 11:43:36 -0700

    remove default dictionary hash function

--- js/src/builder/dictionary.ts
+++ js/src/builder/dictionary.ts
@@ -78,16 +78,7 @@ export class DictionaryBuilder<T extends Dictionary, TNull = any> extends Builde
         this.indices.clear();
         return super.clear();
     }
-    public valueToKey(val: any) {
-        let str = typeof val === 'string' ? val : `${val}`;
-        let h1 = 0xdeadbeef ^ 0, h2 = 0x41c6ce57 ^ 0;
-        for (let i = -1, n = str.length, ch; ++i < n;) {
-            ch = str.charCodeAt(i);
-            h1 = Math.imul(h1 ^ ch, 2654435761);
-            h2 = Math.imul(h2 ^ ch, 1597334677);
-        }
-        h1 = Math.imul(h1 ^ h1 >>> 16, 2246822507) ^ Math.imul(h2 ^ h2 >>> 13, 3266489909);
-        h2 = Math.imul(h2 ^ h2 >>> 16, 2246822507) ^ Math.imul(h1 ^ h1 >>> 13, 3266489909);
-        return 4294967296 * (2097151 & h2) + (h1 >>> 0) as any;
+    public valueToKey(val: any): string | number {
+        return typeof val === 'string' ? val : `${val}`;
     }
 }
