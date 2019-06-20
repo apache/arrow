af34401c92612da1ff47e3b9f9bbf047cd6e4f1b
Neal Richardson <neal.p.richardson@gmail.com>
2019-06-20 16:53:53 -0700

    Sketch of install_arrow()

--- r/R/arrow-package.R
+++ r/R/arrow-package.R
@@ -24,11 +24,60 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' Is the C++ Arrow library available
+#' Is the C++ Arrow library available?
 #'
+#' You won't generally need to call this function, but it's here in case it
+#' helps for development purposes.
+#' @return `TRUE` or `FALSE` depending on whether the package was installed
+#' with the Arrow C++ library. If `FALSE`, you'll need to install the C++
+#' library and then reinstall the R package. See [install_arrow()] for help.
 #' @export
 arrow_available <- function() {
   .Call(`_arrow_available`)
+}
+
+#' Help installing the Arrow C++ library
+#'
+#' Binary package installations should come with a working Arrow C++ library,
+#' but when installing from source, you'll need to obtain the C++ library
+#' first. This function offers guidance on how to get the C++ library depending
+#' on your operating system and package version.
+#' @export
+#' @importFrom utils packageVersion
+install_arrow <- function() {
+  if(arrow_available()) {
+    # Respond that you already have it. See README for alternative installation instructions (like if you want a dev version). Please report an issue if you see arrow_available but can't do something you should be able to
+  }
+
+  os <- tolower(Sys.info()[["sysname"]])
+  # c("windows", "darwin", "linux", "sunos") # win/mac/linux/solaris
+  dev_version <- length(packageVersion("arrow")) > 3
+  # From CRAN check:
+  rep <- installed.packages(fields="Repository")["arrow", "Repository"]
+  from_cran <- identical(rep, "CRAN")
+  # Is it possible to tell if was a binary install from CRAN vs. source?
+
+  # Cases (excluding where arrow_available):
+  if(os == "sunos") {
+    # Good luck with that. See C++ dev guide
+  } else if(os == "linux") {
+    if(dev_version) {
+      # Point to compilation instructions on readme
+    } else {
+      # Suggest arrow.apache.org/install for PPAs, or compilation instructions
+    }
+  } else if(!dev_version && !from_cran) {
+    # Windows or Mac with a released version but not from CRAN
+    # Recommend installing released binary package from CRAN
+  } else {
+    # Windows or Mac, most likely a dev version
+    # for each OS, recommend dev installation, refer to readme
+    if(os == "windows") {
+
+    } else {
+      # macOS
+    }
+  }
 }
 
 option_use_threads <- function() {
