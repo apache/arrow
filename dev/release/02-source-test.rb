e7513a980ffa36e549b888cdba79a2a64226076a
Yosuke Shiro <yosuke.shiro615@gmail.com>
2019-06-22 12:34:58 +0900

    Add spaces

--- dev/release/02-source-test.rb
+++ dev/release/02-source-test.rb
@@ -39,7 +39,7 @@ class SourceTest < Test::Unit::TestCase
   end
 
   def prepare
-    env = {"SOURCE_DEFAULT"=>"0"}
+    env = {"SOURCE_DEFAULT" => "0"}
     env["release_hash"] = @current_commit
     sh(env, "dev/release/02-source.sh", @current_version, "0")
   end
