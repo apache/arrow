3533213cd57cf09b96dbc862db8d18dc1fbd1ad4
Sebastien Binet <binet@cern.ch>
2019-06-22 23:58:07 +0200

    ARROW-5693: [Go] skip IPC integration tests for Decimal128
    Author: Sebastien Binet <binet@cern.ch>
    
    Closes #4659 from sbinet/issue-5693 and squashes the following commits:
    
    4f206d07f <Sebastien Binet> ARROW-5693:  skip IPC integration tests for Decimal128

--- go/arrow/internal/arrjson/arrjson_test.go
+++ go/arrow/internal/arrjson/arrjson_test.go
@@ -30,6 +30,9 @@ import (
 func TestReadWrite(t *testing.T) {
 	for name, recs := range arrdata.Records {
 		t.Run(name, func(t *testing.T) {
+			if name == "decimal128" {
+				t.Skip() // FIXME(sbinet): implement full decimal128 support
+			}
 			mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
 			defer mem.AssertSize(t, 0)
 
