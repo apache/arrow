ce109840498f8954167656baf19c71eea510f644
liyafan82 <fan_li_ya@foxmail.com>
2019-06-24 15:03:46 +0800

    [ARROW-5581][Java] Surround sorted index in try resouce block

--- java/algorithm/src/main/java/org/apache/arrow/algorithm/sort/FixedWidthOutOfPlaceVectorSorter.java
+++ java/algorithm/src/main/java/org/apache/arrow/algorithm/sort/FixedWidthOutOfPlaceVectorSorter.java
@@ -45,24 +45,24 @@ public class FixedWidthOutOfPlaceVectorSorter<V extends BaseFixedWidthVector> im
     ArrowBuf dstValueBuffer = dstVector.getDataBuffer();
 
     // sort value indices
-    IntVector sortedIndices = new IntVector("", srcVector.getAllocator());
-    sortedIndices.allocateNew(srcVector.getValueCount());
-    sortedIndices.setValueCount(srcVector.getValueCount());
-    indexSorter.sort(srcVector, sortedIndices, comparator);
+    try (IntVector sortedIndices = new IntVector("", srcVector.getAllocator())) {
+      sortedIndices.allocateNew(srcVector.getValueCount());
+      sortedIndices.setValueCount(srcVector.getValueCount());
+      indexSorter.sort(srcVector, sortedIndices, comparator);
 
-    // copy sorted values to the output vector
-    for (int dstIndex = 0; dstIndex < sortedIndices.getValueCount(); dstIndex++) {
-      int srcIndex = sortedIndices.get(dstIndex);
-      if (srcVector.isNull(srcIndex)) {
-        BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 0);
-      } else {
-        BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 1);
-        PlatformDependent.copyMemory(
-                srcValueBuffer.memoryAddress() + srcIndex * valueWidth,
-                dstValueBuffer.memoryAddress() + dstIndex * valueWidth,
-                valueWidth);
+      // copy sorted values to the output vector
+      for (int dstIndex = 0; dstIndex < sortedIndices.getValueCount(); dstIndex++) {
+        int srcIndex = sortedIndices.get(dstIndex);
+        if (srcVector.isNull(srcIndex)) {
+          BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 0);
+        } else {
+          BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 1);
+          PlatformDependent.copyMemory(
+                  srcValueBuffer.memoryAddress() + srcIndex * valueWidth,
+                  dstValueBuffer.memoryAddress() + dstIndex * valueWidth,
+                  valueWidth);
+        }
       }
     }
-    sortedIndices.close();
   }
 }
--- java/algorithm/src/main/java/org/apache/arrow/algorithm/sort/VariableWidthOutOfPlaceVectorSorter.java
+++ java/algorithm/src/main/java/org/apache/arrow/algorithm/sort/VariableWidthOutOfPlaceVectorSorter.java
@@ -46,31 +46,31 @@ public class VariableWidthOutOfPlaceVectorSorter<V extends BaseVariableWidthVect
     ArrowBuf dstOffsetBuffer = dstVector.getOffsetBuffer();
 
     // sort value indices
-    IntVector sortedIndices = new IntVector("", srcVector.getAllocator());
-    sortedIndices.allocateNew(srcVector.getValueCount());
-    sortedIndices.setValueCount(srcVector.getValueCount());
-    indexSorter.sort(srcVector, sortedIndices, comparator);
+    try (IntVector sortedIndices = new IntVector("", srcVector.getAllocator())) {
+      sortedIndices.allocateNew(srcVector.getValueCount());
+      sortedIndices.setValueCount(srcVector.getValueCount());
+      indexSorter.sort(srcVector, sortedIndices, comparator);
 
-    int dstOffset = 0;
-    dstOffsetBuffer.setInt(0, 0);
+      int dstOffset = 0;
+      dstOffsetBuffer.setInt(0, 0);
 
-    // copy sorted values to the output vector
-    for (int dstIndex = 0; dstIndex < sortedIndices.getValueCount(); dstIndex++) {
-      int srcIndex = sortedIndices.get(dstIndex);
-      if (srcVector.isNull(srcIndex)) {
-        BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 0);
-      } else {
-        BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 1);
-        int srcOffset = srcOffsetBuffer.getInt(srcIndex * BaseVariableWidthVector.OFFSET_WIDTH);
-        int valueLength = srcOffsetBuffer.getInt((srcIndex + 1) * BaseVariableWidthVector.OFFSET_WIDTH) - srcOffset;
-        PlatformDependent.copyMemory(
-                srcValueBuffer.memoryAddress() + srcOffset,
-                dstValueBuffer.memoryAddress() + dstOffset,
-                valueLength);
-        dstOffset += valueLength;
+      // copy sorted values to the output vector
+      for (int dstIndex = 0; dstIndex < sortedIndices.getValueCount(); dstIndex++) {
+        int srcIndex = sortedIndices.get(dstIndex);
+        if (srcVector.isNull(srcIndex)) {
+          BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 0);
+        } else {
+          BitVectorHelper.setValidityBit(dstValidityBuffer, dstIndex, 1);
+          int srcOffset = srcOffsetBuffer.getInt(srcIndex * BaseVariableWidthVector.OFFSET_WIDTH);
+          int valueLength = srcOffsetBuffer.getInt((srcIndex + 1) * BaseVariableWidthVector.OFFSET_WIDTH) - srcOffset;
+          PlatformDependent.copyMemory(
+                  srcValueBuffer.memoryAddress() + srcOffset,
+                  dstValueBuffer.memoryAddress() + dstOffset,
+                  valueLength);
+          dstOffset += valueLength;
+        }
+        dstOffsetBuffer.setInt((dstIndex + 1) * BaseVariableWidthVector.OFFSET_WIDTH, dstOffset);
       }
-      dstOffsetBuffer.setInt((dstIndex + 1) * BaseVariableWidthVector.OFFSET_WIDTH, dstOffset);
     }
-    sortedIndices.close();
   }
 }
