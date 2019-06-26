53780b8ed16b66d60430e75e106b24a7587a1bde
Wes McKinney <wesm+git@apache.org>
2019-06-25 23:55:11 -0500

    Suppress warning earlier

--- cpp/src/arrow/flight/test-util.cc
+++ cpp/src/arrow/flight/test-util.cc
@@ -25,6 +25,11 @@
 #include <cstdlib>
 #include <sstream>
 
+// HACK(ARROW-5735): Silence C4244 generated with Boost 1.69.0
+#if defined(_MSC_VER)
+#pragma warning(disable : 4244)
+#endif
+
 #include <boost/filesystem.hpp>
 #include <boost/process.hpp>
 
@@ -40,11 +45,6 @@
 
 namespace arrow {
 namespace flight {
-
-// HACK(ARROW-5735): Silence C4244 generated with Boost 1.69.0
-#if defined(_MSC_VER)
-#pragma warning(disable : 4244)
-#endif
 
 namespace bp = boost::process;
 namespace fs = boost::filesystem;
