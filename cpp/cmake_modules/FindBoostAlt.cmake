b8cadbbfce2f01095c5bbd6b816de70d36bf071f
Antoine Pitrou <antoine@python.org>
2019-06-27 17:18:01 -0500

    ARROW-5490: [C++] Remove ARROW_BOOST_HEADER_ONLY
    That CMake variable isn't exposed as an option and probably doesn't work anymore.
    
    Author: Antoine Pitrou <antoine@python.org>
    
    Closes #4723 from pitrou/ARROW-5490-remove-boost-header-only and squashes the following commits:
    
    172fe52eb <Antoine Pitrou> ARROW-5490:  Remove ARROW_BOOST_HEADER_ONLY

--- cpp/cmake_modules/FindBoostAlt.cmake
+++ cpp/cmake_modules/FindBoostAlt.cmake
@@ -38,24 +38,16 @@ if(ARROW_BOOST_USE_SHARED)
   set(BUILD_SHARED_LIBS_KEEP ${BUILD_SHARED_LIBS})
   set(BUILD_SHARED_LIBS ON)
 
-  if(ARROW_BOOST_HEADER_ONLY)
-    find_package(Boost ${BoostAlt_FIND_VERSION_OPTIONS})
-  else()
-    find_package(Boost ${BoostAlt_FIND_VERSION_OPTIONS}
-                 COMPONENTS regex system filesystem)
-  endif()
+  find_package(Boost ${BoostAlt_FIND_VERSION_OPTIONS}
+               COMPONENTS regex system filesystem)
   set(BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS_KEEP})
   unset(BUILD_SHARED_LIBS_KEEP)
 else()
   # Find static boost headers and libs
   # TODO Differentiate here between release and debug builds
   set(Boost_USE_STATIC_LIBS ON)
-  if(ARROW_BOOST_HEADER_ONLY)
-    find_package(Boost ${BoostAlt_FIND_VERSION_OPTIONS})
-  else()
-    find_package(Boost ${BoostAlt_FIND_VERSION_OPTIONS}
-                 COMPONENTS regex system filesystem)
-  endif()
+  find_package(Boost ${BoostAlt_FIND_VERSION_OPTIONS}
+               COMPONENTS regex system filesystem)
 endif()
 
 if(Boost_FOUND)
