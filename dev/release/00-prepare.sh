0ab97c618814eb9116d50583da9e02e821865c2e
Pindikura Ravindra <ravindra@dremio.com>
2019-06-29 06:29:12 +0900

    ARROW-4301: [Java] use arrow-jni profile for both gandiva/orc
    Author: Pindikura Ravindra <ravindra@dremio.com>
    Author: Sutou Kouhei <kou@clear-code.com>
    
    Closes #4717 from pravindra/arrow-4301 and squashes the following commits:
    
    aa904ed3c <Sutou Kouhei> Sort arguments
    284cd642f <Sutou Kouhei> Remove needless dependency
    69adf7a9c <Pindikura Ravindra> correct args passed to mvn release
    426e23dd5 <Pindikura Ravindra> move output dir arg to the end
    ac2fb1c1b <Sutou Kouhei> Use the common output directory for ORC JNI
    04e4fc19b <Sutou Kouhei> Build C++ for Java Gandiva and ORC
    e3e4b4481 <Pindikura Ravindra> update gandiva profile
    2622b1a34 <Pindikura Ravindra> ARROW-4301:  use arrow-jni profile for both gandiva/orc

--- dev/release/00-prepare.sh
+++ dev/release/00-prepare.sh
@@ -208,11 +208,33 @@ if [ ${PREPARE_VERSION_PRE_TAG} -gt 0 ]; then
 fi
 
 if [ ${PREPARE_TAG} -gt 0 ]; then
-  cd "${SOURCE_DIR}/../../java"
+  profile=arrow-jni # this includes components which depend on arrow cpp.
+  pushd "${SOURCE_DIR}/../../java"
   git submodule update --init --recursive
+  cpp_dir="${PWD}/../cpp"
+  cpp_build_dir=$(mktemp -d -t "apache-arrow-cpp.XXXXX")
+  pushd ${cpp_build_dir}
+  cmake \
+    -DARROW_GANDIVA=ON \
+    -DARROW_GANDIVA_JAVA=ON \
+    -DARROW_JNI=ON \
+    -DARROW_ORC=ON \
+    -DCMAKE_BUILD_TYPE=release \
+    -G Ninja \
+    "${cpp_dir}"
+  ninja
+  popd
   mvn release:clean
-  mvn release:prepare -Dtag=${tag} -DreleaseVersion=${version} -DautoVersionSubmodules -DdevelopmentVersion=${next_version_snapshot}
-  cd -
+  mvn \
+    release:prepare \
+    -Darguments=-Darrow.cpp.build.dir=${cpp_build_dir}/release \
+    -DautoVersionSubmodules \
+    -DdevelopmentVersion=${next_version_snapshot} \
+    -DreleaseVersion=${version} \
+    -Dtag=${tag} \
+    -P ${profile}
+  rm -rf ${cpp_build_dir}
+  popd
 fi
 
 ############################## Post-Tag Commits #############################
