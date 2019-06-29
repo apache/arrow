4d25902f3795597e89b38347ebfe41ab510b9d2e
Sutou Kouhei <kou@clear-code.com>
2019-06-29 15:09:44 +0900

    ARROW-5787: [Release][Rust] Use local modules to verify RC
    Because we don't publish modules to crates.io yet.
    
    Author: Sutou Kouhei <kou@clear-code.com>
    
    Closes #4747 from kou/release-verify-rust and squashes the following commits:
    
    c424af6b5 <Sutou Kouhei>  Use local modules to verify RC

--- dev/release/verify-release-candidate.sh
+++ dev/release/verify-release-candidate.sh
@@ -458,6 +458,14 @@ test_rust() {
   # we are targeting Rust nightly for releases
   rustup default nightly
 
+  # use local modules because we don't publish modules to crates.io yet
+  sed \
+    -i.bak \
+    -E \
+    -e 's/^arrow = "([^"]*)"/arrow = { version = "\1", path = "..\/arrow" }/g' \
+    -e 's/^parquet = "([^"]*)"/parquet = { version = "\1", path = "..\/parquet" }/g' \
+    */Cargo.toml
+
   # raises on any warnings
   RUSTFLAGS="-D warnings" cargo build
   cargo test
