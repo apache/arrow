77a560be55d7c6517d4063fc7590da671fa37e24
Artem Alekseev <artem.alekseev@intel.com>
2019-07-12 12:52:29 +0300

    [C++] Implement Sort kernel

---
+++ cpp/src/arrow/compute/kernels/sort-test.cc
@@ -0,0 +1,74 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include <memory>
+#include <vector>
+
+#include "arrow/compute/context.h"
+#include "arrow/compute/kernels/sort.h"
+#include "arrow/compute/test-util.h"
+#include "arrow/testing/gtest_common.h"
+#include "arrow/testing/gtest_util.h"
+#include "arrow/testing/random.h"
+#include "arrow/testing/util.h"
+#include "arrow/type_traits.h"
+namespace arrow {
+namespace compute {
+
+using internal::checked_pointer_cast;
+using util::string_view;
+
+constexpr auto kSeed = 0x0ff1ce;
+
+template <typename ArrowType>
+class TestSortKernel : public ComputeFixture, public TestBase {
+ protected:
+  void AssertSortArrays(const std::shared_ptr<Array> values,
+                        const std::shared_ptr<Array> expected) {
+    std::shared_ptr<Array> actual;
+    ASSERT_OK(arrow::compute::Sort(&this->ctx_, *values, &actual));
+    ASSERT_OK(ValidateArray(*actual));
+    AssertArraysEqual(*expected, *actual);
+  }
+
+  virtual void AssertSort(const std::string& values, const std::string& expected) {
+    auto type = TypeTraits<ArrowType>::type_singleton();
+    AssertSortArrays(ArrayFromJSON(type, values), ArrayFromJSON(uint64(), expected));
+  }
+};
+
+class TestSortKernelWithInt32 : public TestSortKernel<Int32Type> {
+ protected:
+  void AssertSort(const std::string& values, const std::string& expected) override {
+    TestSortKernel<Int32Type>::AssertSort(values, expected);
+  }
+};
+
+TEST_F(TestSortKernelWithInt32, SortInt32) {
+  this->AssertSort("[]", "[]");
+
+  this->AssertSort("[3,2,6]", "[1, 0, 2]");
+
+  this->AssertSort("[1,2,3,4,5,6,7]", "[0,1,2,3,4,5,6]");
+
+  this->AssertSort("[7,6,5,4,3,2,1]", "[6,5,4,3,2,1,0]");
+
+  this->AssertSort("[10,12,4,50,100,32,11]", "[2,0,6,1,5,3,4]");
+}
+
+}  // namespace compute
+}  // namespace arrow
---
+++ cpp/src/arrow/compute/kernels/sort.cc
@@ -0,0 +1,123 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "arrow/compute/kernels/sort.h"
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include "arrow/builder.h"
+#include "arrow/compute/context.h"
+#include "arrow/compute/expression.h"
+#include "arrow/compute/logical_type.h"
+#include "arrow/type_traits.h"
+namespace arrow {
+
+class Array;
+
+namespace compute {
+
+template <typename ArrowType>
+class SortKernelImpl : public SortKernel {
+  using ArrayType = typename TypeTraits<ArrowType>::ArrayType;
+  using BuilderType = typename TypeTraits<ArrowType>::BuilderType;
+  using CType = typename TypeTraits<ArrowType>::CType;
+
+ private:
+  Status SortImpl(FunctionContext* ctx, const std::shared_ptr<ArrayType>& values,
+                  std::shared_ptr<Array>* offsets) {
+    std::vector<uint64_t> ind(values->length());
+    std::iota(ind.begin(), ind.end(), 0);
+    std::sort(ind.begin(), ind.end(), [&values](uint64_t left, uint64_t right) {
+      return values->Value(left) < values->Value(right);
+    });
+    UInt64Builder builder(ctx->memory_pool());
+    builder.AppendValues(ind.begin(), ind.end());
+    return builder.Finish(offsets);
+  }
+
+ public:
+  explicit SortKernelImpl(const std::shared_ptr<DataType>& type) : SortKernel(type) {}
+
+  Status Sort(FunctionContext* ctx, const std::shared_ptr<Array>& values,
+              std::shared_ptr<Array>* offsets) {
+    return SortImpl(ctx, std::static_pointer_cast<ArrayType>(values), offsets);
+  }
+
+  Status Call(FunctionContext* ctx, const Datum& values, Datum* offsets) {
+    if (!values.is_array()) {
+      return Status::Invalid("SortKernel expects array values");
+    }
+    auto values_array = values.make_array();
+    std::shared_ptr<Array> offsets_array;
+    RETURN_NOT_OK(this->Sort(ctx, values_array, &offsets_array));
+    *offsets = offsets_array;
+    return Status::OK();
+  }
+
+  std::shared_ptr<DataType> out_type() const { return type_; }
+};
+
+Status SortKernel::Make(const std::shared_ptr<DataType>& value_type,
+                        std::unique_ptr<SortKernel>* out) {
+  SortKernel* kernel;
+  switch (value_type->id()) {
+    case Type::UINT8:
+      kernel = new SortKernelImpl<UInt8Type>(value_type);
+      break;
+    case Type::INT8:
+      kernel = new SortKernelImpl<Int8Type>(value_type);
+      break;
+    case Type::UINT16:
+      kernel = new SortKernelImpl<UInt16Type>(value_type);
+      break;
+    case Type::INT16:
+      kernel = new SortKernelImpl<Int16Type>(value_type);
+      break;
+    case Type::UINT32:
+      kernel = new SortKernelImpl<UInt32Type>(value_type);
+      break;
+    case Type::INT32:
+      kernel = new SortKernelImpl<Int32Type>(value_type);
+      break;
+    case Type::UINT64:
+      kernel = new SortKernelImpl<UInt64Type>(value_type);
+      break;
+    case Type::INT64:
+      kernel = new SortKernelImpl<Int64Type>(value_type);
+      break;
+    default:
+      return Status::Invalid("Use of unsaported array type in SortKernel");
+  }
+  out->reset(kernel);
+  return Status::OK();
+}
+
+Status Sort(FunctionContext* ctx, const Array& values, std::shared_ptr<Array>* offsets) {
+  Datum offsets_datum;
+  RETURN_NOT_OK(Sort(ctx, Datum(values.data()), &offsets_datum));
+  *offsets = offsets_datum.make_array();
+  return Status::OK();
+}
+
+Status Sort(FunctionContext* ctx, const Datum& values, Datum* offsets) {
+  std::unique_ptr<SortKernel> kernel;
+  RETURN_NOT_OK(SortKernel::Make(values.type(), &kernel));
+  return kernel->Call(ctx, values, offsets);
+}
+
+}  // namespace compute
+}  // namespace arrow
