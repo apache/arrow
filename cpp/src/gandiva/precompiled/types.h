d6a3d608a438da14bc868aef45c590b3f6086d32
Prudhvi Porandla <prudhvi.porandla@icloud.com>
2019-07-16 16:23:27 +0530

    lint

--- cpp/src/gandiva/precompiled/types.h
+++ cpp/src/gandiva/precompiled/types.h
@@ -126,7 +126,7 @@ float64 mod_float64_float64(int64 context, float64 left, float64 right);
 int64 divide_int64_int64(int64 context, int64 in1, int64 in2);
 
 int64 div_int64_int64(int64 context, int64 in1, int64 in2);
-  float64 div_float64_float64(int64 context, float64 in1, float64 in2);
+float64 div_float64_float64(int64 context, float64 in1, float64 in2);
 
 float64 cbrt_int32(int32);
 float64 cbrt_int64(int64);
