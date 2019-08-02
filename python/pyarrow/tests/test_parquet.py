8e2b70b1ad86c2b9c95353dbc415fd825f8e92d0
Wes McKinney <wesm+git@apache.org>
2019-08-02 14:53:48 -0500

    Expand read_dictionary with ParquetDataset test for multiple files

--- python/pyarrow/tests/test_parquet.py
+++ python/pyarrow/tests/test_parquet.py
@@ -2884,14 +2884,24 @@ def test_direct_read_dictionary():
 
 def test_dataset_read_dictionary(tempdir):
     path = tempdir / "ARROW-3325-dataset"
-    data = [
-        [tm.rands(10) for i in range(5)] * 10,
-    ]
-    table = pa.table(data, names=['f0'])
-    pq.write_to_dataset(table, root_path=str(path))
+    t1 = pa.table([[tm.rands(10) for i in range(5)] * 10], names=['f0'])
+    t2 = pa.table([[tm.rands(10) for i in range(5)] * 10], names=['f0'])
+    pq.write_to_dataset(t1, root_path=str(path))
+    pq.write_to_dataset(t2, root_path=str(path))
+
     result = pq.ParquetDataset(path, read_dictionary=['f0']).read()
-    expected = pa.table([table[0].dictionary_encode()], names=['f0'])
-    assert result.equals(expected)
+
+    # The order of the chunks is non-deterministic
+    ex_chunks = [t1[0].chunk(0).dictionary_encode(),
+                 t2[0].chunk(0).dictionary_encode()]
+
+    assert result[0].num_chunks == 2
+    c0, c1 = result[0].chunk(0), result[0].chunk(1)
+    if c0.equals(ex_chunks[0]):
+        assert c1.equals(ex_chunks[1])
+    else:
+        assert c0.equals(ex_chunks[1])
+        assert c1.equals(ex_chunks[0])
 
 
 def test_direct_read_dictionary_subfield():
