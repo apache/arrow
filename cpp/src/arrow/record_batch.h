016fa86c08b14d5c1c2467f5347f22cfc77c5aba
François Saint-Jacques <fsaintjacques@gmail.com>
2019-08-14 14:35:10 -0400

    Remove superfluous destructor

--- cpp/src/arrow/record_batch.h
+++ cpp/src/arrow/record_batch.h
@@ -166,8 +166,6 @@ class ARROW_EXPORT RecordBatch {
 /// \brief Abstract interface for reading stream of record batches
 class ARROW_EXPORT RecordBatchReader : public Iterator<std::shared_ptr<RecordBatch>> {
  public:
-  virtual ~RecordBatchReader() = default;
-
   /// \return the shared schema of the record batches in the stream
   virtual std::shared_ptr<Schema> schema() const = 0;
 
