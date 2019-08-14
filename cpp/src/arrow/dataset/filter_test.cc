3499fd2ec70aa1b24a4def3104611fa924ff2428
Benjamin Kietzman <bengilgit@gmail.com>
2019-08-14 17:42:03 -0400

    add an expression simplification test

--- cpp/src/arrow/dataset/filter_test.cc
+++ cpp/src/arrow/dataset/filter_test.cc
@@ -93,6 +93,8 @@ TEST_F(ExpressionsTest, Simplification) {
   AssertSimplifiesTo("b"_ == 3 or "b"_ == 4, "b"_ == 3, *always);
   AssertSimplifiesTo("b"_ == 3 or "b"_ == 4, "b"_ > 3, "b"_ == 4);
   AssertSimplifiesTo("b"_ == 3 or "b"_ == 4, "b"_ >= 3, "b"_ == 3 or "b"_ == 4);
+
+  AssertSimplifiesTo("a"_ == 3 or "b"_ == 4, "a"_ == 0, "b"_ == 4);
 }
 
 class FilterTest : public ::testing::Test {
