d37af300cf595ac61ca99da2fc412828497b3f46
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2019-08-22 15:30:18 -0700

    address line length

--- dev/tasks/crossbow.py
+++ dev/tasks/crossbow.py
@@ -724,7 +724,9 @@ def crossbow(ctx, github_token, arrow_path, queue_path):
               help='Path of changelog to update')
 @click.option('--arrow-version', '-v', default=None,
               help='Set target version explicitly')
-@click.option('--is-website', '-w', default=False, is_flag=True, help='Whether to use website format for changelog. Uses markdown format if false.')
+@click.option('--is-website', '-w', default=False, is_flag=True,
+              help='Whether to use website format for changelog. '
+                   'Uses markdown format if false.')
 @click.option('--jira-username', '-u', default=None, help='JIRA username')
 @click.option('--jira-password', '-P', default=None, help='JIRA password')
 @click.option('--dry-run/--write', default=False,
