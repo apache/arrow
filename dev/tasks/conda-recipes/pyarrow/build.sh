501cb523943f0606c4a6db88ce5303f876af30e3
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-09-04 18:25:52 +0200

    update recipes and variant files

--- dev/tasks/conda-recipes/pyarrow/build.sh
+++ dev/tasks/conda-recipes/pyarrow/build.sh
@@ -7,15 +7,16 @@ set -x
 export ARROW_HOME=$PREFIX
 export PARQUET_HOME=$PREFIX
 export SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION
+export PYARROW_BUILD_TYPE=release
+export PYARROW_WITH_FLIGHT=1
+export PYARROW_WITH_PLASMA=1
+export PYARROW_WITH_PARQUET=1
+export PYARROW_WITH_ORC=1
+export PYARROW_WITH_GANDIVA=1
 
 cd python
 
 $PYTHON setup.py \
-        build_ext --build-type=release \
-                  --with-flight \
-                  --with-orc \
-                  --with-plasma \
-                  --with-parquet \
-                  --with-gandiva \
+        build_ext \
         install --single-version-externally-managed \
                 --record=record.txt
