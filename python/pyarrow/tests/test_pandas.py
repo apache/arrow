243d4885c5287ec91ff4acd2195b36917d10320c
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-09-04 21:39:59 -0500

    ARROW-6431: [Python] Test suite fails without pandas installed
    Closes #5276 from kszucs/ARROW-6431 and squashes the following commits:
    
    45c9f076a <Krisztián Szűcs> mark pandas tests in test_parquet.py
    cd7415242 <Krisztián Szűcs> skip from the test case instead of decorating
    
    Authored-by: Krisztián Szűcs <szucs.krisztian@gmail.com>
    Signed-off-by: Wes McKinney <wesm+git@apache.org>

--- python/pyarrow/tests/test_pandas.py
+++ python/pyarrow/tests/test_pandas.py
@@ -2891,9 +2891,9 @@ def test_variable_dictionary_with_pandas():
 # Array protocol in pandas conversions tests
 
 
-@pytest.mark.skipif(LooseVersion(pd.__version__) < '0.24.0',
-                    reason='IntegerArray only introduced in 0.24')
 def test_array_protocol():
+    if LooseVersion(pd.__version__) < '0.24.0':
+        pytest.skip(reason='IntegerArray only introduced in 0.24')
 
     def __arrow_array__(self, type=None):
         return pa.array(self._data, mask=self._mask, type=type)
