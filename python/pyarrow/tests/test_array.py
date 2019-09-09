9463e98c9011c2de4f8a16e8274cdf3bcde7c282
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-09-09 17:39:31 +0200

    add test case for all-nan array

--- python/pyarrow/tests/test_array.py
+++ python/pyarrow/tests/test_array.py
@@ -1254,10 +1254,24 @@ def test_array_from_numpy_unicode():
     assert arrow_arr.equals(expected)
 
 
-def test_array_from_non_string():
+def test_array_string_from_non_string():
     # ARROW-5682 - when converting to string raise on non string-like dtype
     with pytest.raises(ValueError):
         pa.array(np.array([1, 2, 3]), type=pa.string())
+
+
+def test_array_string_from_all_null():
+    # ARROW-5682
+    vals = np.array([None, None], dtype=object)
+    arr = pa.array(vals, type=pa.string())
+    assert arr.null_count == 2
+
+    vals = np.array([np.nan, np.nan], dtype='float64')
+    # by default raises, but accept as all-null when from_pandas=True
+    with pytest.raises(ValueError):
+        pa.array(vals, type=pa.string())
+    arr = pa.array(vals, type=pa.string(), from_pandas=True)
+    assert arr.null_count == 2
 
 
 def test_array_from_masked():
