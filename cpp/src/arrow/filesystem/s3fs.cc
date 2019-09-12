05d8508c075cab51f54cc518d2cf1474577b881c
Antoine Pitrou <antoine@python.org>
2019-09-12 18:12:07 +0200

    ARROW-6362: [C++] Allow customizing S3 credentials provider
    Also provide API facilities to use the default provider chain
    (looks up environment variables and config files), or a hard-coded
    access / secret key pair.

--- cpp/src/arrow/filesystem/s3fs.cc
+++ cpp/src/arrow/filesystem/s3fs.cc
@@ -36,6 +36,7 @@
 
 #include <aws/core/Aws.h>
 #include <aws/core/auth/AWSCredentials.h>
+#include <aws/core/auth/AWSCredentialsProviderChain.h>
 #include <aws/core/client/RetryStrategy.h>
 #include <aws/core/utils/logging/ConsoleLogSystem.h>
 #include <aws/core/utils/stream/PreallocatedStreamBuf.h>
@@ -130,6 +131,30 @@ Status FinalizeS3() {
   Aws::ShutdownAPI(aws_options);
   aws_initialized.store(false);
   return Status::OK();
+}
+
+void S3Options::ConfigureDefaultCredentials() {
+  credentials_provider =
+      std::make_shared<Aws::Auth::DefaultAWSCredentialsProviderChain>();
+}
+
+void S3Options::ConfigureAccessKey(const std::string& access_key,
+                                   const std::string& secret_key) {
+  credentials_provider = std::make_shared<Aws::Auth::SimpleAWSCredentialsProvider>(
+      ToAwsString(access_key), ToAwsString(secret_key));
+}
+
+S3Options S3Options::Defaults() {
+  S3Options options;
+  options.ConfigureDefaultCredentials();
+  return options;
+}
+
+S3Options S3Options::FromAccessKey(const std::string& access_key,
+                                   const std::string& secret_key) {
+  S3Options options;
+  options.ConfigureAccessKey(access_key, secret_key);
+  return options;
 }
 
 namespace {
@@ -704,7 +729,7 @@ class S3FileSystem::Impl {
   explicit Impl(S3Options options) : options_(std::move(options)) {}
 
   Status Init() {
-    credentials_ = {ToAwsString(options_.access_key), ToAwsString(options_.secret_key)};
+    credentials_ = options_.credentials_provider->GetAWSCredentials();
     client_config_.region = ToAwsString(options_.region);
     client_config_.endpointOverride = ToAwsString(options_.endpoint_override);
     if (options_.scheme == "http") {
