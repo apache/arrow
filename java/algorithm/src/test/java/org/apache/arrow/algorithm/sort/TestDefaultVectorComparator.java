a321784e619cbf314f09a78cf234392f5e041f44
liyafan82 <fan_li_ya@foxmail.com>
2019-09-17 21:07:58 +0800

    [ArrowARROW-6580][Java] Support comparison for unsigned integers

--- java/algorithm/src/test/java/org/apache/arrow/algorithm/sort/TestDefaultVectorComparator.java
+++ java/algorithm/src/test/java/org/apache/arrow/algorithm/sort/TestDefaultVectorComparator.java
@@ -23,6 +23,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.IntVector;
+import org.apache.arrow.vector.UInt1Vector;
+import org.apache.arrow.vector.UInt2Vector;
+import org.apache.arrow.vector.UInt4Vector;
+import org.apache.arrow.vector.UInt8Vector;
 import org.apache.arrow.vector.complex.ListVector;
 import org.apache.arrow.vector.types.Types;
 import org.apache.arrow.vector.types.pojo.FieldType;
@@ -105,6 +109,122 @@ public class TestDefaultVectorComparator {
 
       // list vector elements equal
       assertTrue(comparator.compare(0, 0) == 0);
+    }
+  }
+
+  @Test
+  public void testCompareUInt1() {
+    try (UInt1Vector vec = new UInt1Vector("", allocator)) {
+      vec.allocateNew(8);
+      vec.setValueCount(8);
+
+      vec.setNull(0);
+      vec.set(1, -2);
+      vec.set(2, -1);
+      vec.set(3, 0);
+      vec.set(4, 1);
+      vec.set(5, 2);
+      vec.set(6, -2);
+      vec.setNull(7);
+
+      VectorValueComparator<UInt1Vector> comparator =
+              DefaultVectorComparators.createDefaultComparator(vec);
+      comparator.attachVector(vec);
+
+      assertTrue(comparator.compare(0, 1) < 0);
+      assertTrue(comparator.compare(1, 2) < 0);
+      assertTrue(comparator.compare(1, 3) > 0);
+      assertTrue(comparator.compare(2, 5) > 0);
+      assertTrue(comparator.compare(4, 5) < 0);
+      assertTrue(comparator.compare(1, 6) == 0);
+      assertTrue(comparator.compare(0, 7) == 0);
+    }
+  }
+
+  @Test
+  public void testCompareUInt2() {
+    try (UInt2Vector vec = new UInt2Vector("", allocator)) {
+      vec.allocateNew(8);
+      vec.setValueCount(8);
+
+      vec.setNull(0);
+      vec.set(1, -2);
+      vec.set(2, -1);
+      vec.set(3, 0);
+      vec.set(4, 1);
+      vec.set(5, 2);
+      vec.set(6, -2);
+      vec.setNull(7);
+
+      VectorValueComparator<UInt2Vector> comparator =
+              DefaultVectorComparators.createDefaultComparator(vec);
+      comparator.attachVector(vec);
+
+      assertTrue(comparator.compare(0, 1) < 0);
+      assertTrue(comparator.compare(1, 2) < 0);
+      assertTrue(comparator.compare(1, 3) > 0);
+      assertTrue(comparator.compare(2, 5) > 0);
+      assertTrue(comparator.compare(4, 5) < 0);
+      assertTrue(comparator.compare(1, 6) == 0);
+      assertTrue(comparator.compare(0, 7) == 0);
+    }
+  }
+
+  @Test
+  public void testCompareUInt4() {
+    try (UInt4Vector vec = new UInt4Vector("", allocator)) {
+      vec.allocateNew(8);
+      vec.setValueCount(8);
+
+      vec.setNull(0);
+      vec.set(1, -2);
+      vec.set(2, -1);
+      vec.set(3, 0);
+      vec.set(4, 1);
+      vec.set(5, 2);
+      vec.set(6, -2);
+      vec.setNull(7);
+
+      VectorValueComparator<UInt4Vector> comparator =
+              DefaultVectorComparators.createDefaultComparator(vec);
+      comparator.attachVector(vec);
+
+      assertTrue(comparator.compare(0, 1) < 0);
+      assertTrue(comparator.compare(1, 2) < 0);
+      assertTrue(comparator.compare(1, 3) > 0);
+      assertTrue(comparator.compare(2, 5) > 0);
+      assertTrue(comparator.compare(4, 5) < 0);
+      assertTrue(comparator.compare(1, 6) == 0);
+      assertTrue(comparator.compare(0, 7) == 0);
+    }
+  }
+
+  @Test
+  public void testCompareUInt8() {
+    try (UInt8Vector vec = new UInt8Vector("", allocator)) {
+      vec.allocateNew(8);
+      vec.setValueCount(8);
+
+      vec.setNull(0);
+      vec.set(1, -2);
+      vec.set(2, -1);
+      vec.set(3, 0);
+      vec.set(4, 1);
+      vec.set(5, 2);
+      vec.set(6, -2);
+      vec.setNull(7);
+
+      VectorValueComparator<UInt8Vector> comparator =
+              DefaultVectorComparators.createDefaultComparator(vec);
+      comparator.attachVector(vec);
+
+      assertTrue(comparator.compare(0, 1) < 0);
+      assertTrue(comparator.compare(1, 2) < 0);
+      assertTrue(comparator.compare(1, 3) > 0);
+      assertTrue(comparator.compare(2, 5) > 0);
+      assertTrue(comparator.compare(4, 5) < 0);
+      assertTrue(comparator.compare(1, 6) == 0);
+      assertTrue(comparator.compare(0, 7) == 0);
     }
   }
 }
