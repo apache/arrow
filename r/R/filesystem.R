5959b76ce56c5844620d6bb8f1da8a26a942a72f
Neal Richardson <neal.p.richardson@gmail.com>
2019-09-19 12:17:29 -0700

    Fix documentation xrefs and skip test that fails on windows

--- r/R/filesystem.R
+++ r/R/filesystem.R
@@ -16,21 +16,22 @@
 # under the License.
 
 #' @include arrow-package.R
-
 #' @title FileSystem entry stats
 #' @usage NULL
 #' @format NULL
 #'
 #' @section Methods:
 #'
-#' - `base_name()` : The file base name (component after the last directory separator).
+#' - `base_name()` : The file base name (component after the last directory
+#'    separator).
 #' - `extension()` : The file extension
 #'
 #' @section Active bindings:
 #'
 #' - `$type`: The file type
 #' - `$path`: The full file path in the filesystem
-#' - `$size`: The size in bytes, if available.  Only regular files are guaranteed to have a size.
+#' - `$size`: The size in bytes, if available.  Only regular files are
+#'    guaranteed to have a size.
 #' - `$mtime`: The time of last modification, if available.
 #'
 #' @rdname FileStats
@@ -83,12 +84,16 @@ FileStats <- R6Class("FileStats",
 #'
 #' @section Factory:
 #'
-#' The `$create()` factory method instantiates a `Selector` given the 3 fields described below.
+#' The `$create()` factory method instantiates a `Selector` given the 3 fields
+#' described below.
 #'
 #' @section Fields:
 #'
-#' - `base_dir`: The directory in which to select files. If the path exists but doesn't point to a directory, this should be an error.
-#' - `allow_non_existent`: The behavior if `base_dir` doesn't exist in the filesystem. If `FALSE`, an error is returned.  If `TRUE`, an empty selection is returned
+#' - `base_dir`: The directory in which to select files. If the path exists but
+#'    doesn't point to a directory, this should be an error.
+#' - `allow_non_existent`: The behavior if `base_dir` doesn't exist in the
+#'    filesystem. If `FALSE`, an error is returned.  If `TRUE`, an empty
+#'    selection is returned
 #' - `recursive`: Whether to recurse into subdirectories.
 #'
 #' @rdname Selector
@@ -109,8 +114,8 @@ Selector$create <- function(base_dir, allow_non_existent = FALSE, recursive = FA
 #' @title FileSystem classes
 #' @description `FileSystem` is an abstract file system API,
 #' `LocalFileSystem` is an implementation accessing files
-#' on the local machine. `SubTreeFileSystem` is an implementation that delegates to another
-#' implementation after prepending a fixed base path
+#' on the local machine. `SubTreeFileSystem` is an implementation that delegates
+#' to another implementation after prepending a fixed base path
 #'
 #' @section Factory:
 #'
@@ -122,23 +127,32 @@ Selector$create <- function(base_dir, allow_non_existent = FALSE, recursive = FA
 #'
 #' @section Methods:
 #'
-#' - `$GetTargetStats(x)`: `x` may be a [Selector][Selector] or a character vector of paths. Returns a list of [FileStats][FileStats]
+#' - `$GetTargetStats(x)`: `x` may be a [Selector][Selector] or a character
+#'    vector of paths. Returns a list of [FileStats][FileStats]
 #' - `$CreateDir(path, recursive = TRUE)`: Create a directory and subdirectories.
 #' - `$DeleteDir(path)`: Delete a directory and its contents, recursively.
-#' - `$DeleteDirContents(path)`: Delete a directory's contents, recursively. Like `$DeleteDir()`,
-#'    but doesn't delete the directory itself. Passing an empty path (`""`) will wipe the entire filesystem tree.
+#' - `$DeleteDirContents(path)`: Delete a directory's contents, recursively.
+#'    Like `$DeleteDir()`,
+#'    but doesn't delete the directory itself. Passing an empty path (`""`) will
+#'    wipe the entire filesystem tree.
 #' - `$DeleteFile(path)` : Delete a file.
-#' - `$DeleteFiles(paths)` : Delete many files. The default implementation issues individual delete operations in sequence.
-#' - `$Move(src, dest)`: Move / rename a file or directory. If the destination exists:
+#' - `$DeleteFiles(paths)` : Delete many files. The default implementation
+#'    issues individual delete operations in sequence.
+#' - `$Move(src, dest)`: Move / rename a file or directory. If the destination
+#'    exists:
 #'      if it is a non-empty directory, an error is returned
 #'      otherwise, if it has the same type as the source, it is replaced
 #'      otherwise, behavior is unspecified (implementation-dependent).
-#' - `$CopyFile(src, dest)`: Copy a file. If the destination exists and is a directory, an error is returned.
-#'      Otherwise, it is replaced.
-#' - `$OpenInputStream(path)`: Open an [InputStream][input stream] for sequential reading.
-#' - `$OpenInputFile(path)`: On an [RandomAccessFile][input file] for random access reading.
-#' - `$OpenOutputStream(path)`: Open an [OutputStream][output stream] for sequential writing.
-#' - `$OpenAppendStream(path)`: Open an [OutputStream][output stream] for appending.
+#' - `$CopyFile(src, dest)`: Copy a file. If the destination exists and is a
+#'    directory, an error is returned. Otherwise, it is replaced.
+#' - `$OpenInputStream(path)`: Open an [input stream][InputStream] for
+#'    sequential reading.
+#' - `$OpenInputFile(path)`: Open an [input file][RandomAccessFile] for random
+#'    access reading.
+#' - `$OpenOutputStream(path)`: Open an [output stream][OutputStream] for
+#'    sequential writing.
+#' - `$OpenAppendStream(path)`: Open an [output stream][OutputStream] for
+#'    appending.
 #'
 #' @usage NULL
 #' @format NULL
@@ -148,59 +162,58 @@ Selector$create <- function(base_dir, allow_non_existent = FALSE, recursive = FA
 #' @name FileSystem
 #' @export
 FileSystem <- R6Class("FileSystem", inherit = Object,
-    public = list(
-      GetTargetStats = function(x) {
-        if (inherits(x, "Selector")) {
-          map(fs___FileSystem__GetTargetStats_Selector(self, x), shared_ptr, class = FileStats)
-        } else if (is.character(x)){
-          map(fs___FileSystem__GetTargetStats_Paths(self, x), shared_ptr, class = FileStats)
-        } else {
-          abort("incompatible type for FileSystem$GetTargetStarts()")
-        }
-      },
-
-      CreateDir = function(path, recursive = TRUE) {
-        fs___FileSystem__CreateDir(self, path, isTRUE(recursive))
-      },
-
-      DeleteDir = function(path) {
-        fs___FileSystem__DeleteDir(self, path)
-      },
-
-      DeleteDirContents = function(path) {
-        fs___FileSystem__DeleteDirContents(self, path)
-      },
-
-      DeleteFile = function(path) {
-        fs___FileSystem__DeleteFile(self, path)
-      },
-
-      DeleteFiles = function(paths) {
-        fs___FileSystem__DeleteFiles(self, paths)
-      },
-
-      Move = function(src, dest) {
-        fs___FileSystem__Move(self, src, dest)
-      },
-
-      CopyFile = function(src, dest) {
-        fs___FileSystem__CopyFile(self, src, dest)
-      },
-
-      OpenInputStream = function(path) {
-        shared_ptr(InputStream, fs___FileSystem__OpenInputStream(self, path))
-      },
-      OpenInputFile = function(path) {
-        shared_ptr(InputStream, fs___FileSystem__OpenInputFile(self, path))
-      },
-      OpenOutputStream = function(path) {
-        shared_ptr(OutputStream, fs___FileSystem__OpenOutputStream(self, path))
-      },
-      OpenAppendStream = function(path) {
-        shared_ptr(OutputStream, fs___FileSystem__OpenAppendStream(self, path))
+  public = list(
+    GetTargetStats = function(x) {
+      if (inherits(x, "Selector")) {
+        map(fs___FileSystem__GetTargetStats_Selector(self, x), shared_ptr, class = FileStats)
+      } else if (is.character(x)){
+        map(fs___FileSystem__GetTargetStats_Paths(self, x), shared_ptr, class = FileStats)
+      } else {
+        abort("incompatible type for FileSystem$GetTargetStarts()")
       }
+    },
 
-    )
+    CreateDir = function(path, recursive = TRUE) {
+      fs___FileSystem__CreateDir(self, path, isTRUE(recursive))
+    },
+
+    DeleteDir = function(path) {
+      fs___FileSystem__DeleteDir(self, path)
+    },
+
+    DeleteDirContents = function(path) {
+      fs___FileSystem__DeleteDirContents(self, path)
+    },
+
+    DeleteFile = function(path) {
+      fs___FileSystem__DeleteFile(self, path)
+    },
+
+    DeleteFiles = function(paths) {
+      fs___FileSystem__DeleteFiles(self, paths)
+    },
+
+    Move = function(src, dest) {
+      fs___FileSystem__Move(self, src, dest)
+    },
+
+    CopyFile = function(src, dest) {
+      fs___FileSystem__CopyFile(self, src, dest)
+    },
+
+    OpenInputStream = function(path) {
+      shared_ptr(InputStream, fs___FileSystem__OpenInputStream(self, path))
+    },
+    OpenInputFile = function(path) {
+      shared_ptr(InputStream, fs___FileSystem__OpenInputFile(self, path))
+    },
+    OpenOutputStream = function(path) {
+      shared_ptr(OutputStream, fs___FileSystem__OpenOutputStream(self, path))
+    },
+    OpenAppendStream = function(path) {
+      shared_ptr(OutputStream, fs___FileSystem__OpenAppendStream(self, path))
+    }
+  )
 )
 
 #' @usage NULL
@@ -208,7 +221,6 @@ FileSystem <- R6Class("FileSystem", inherit = Object,
 #' @rdname FileSystem
 #' @export
 LocalFileSystem <- R6Class("LocalFileSystem", inherit = FileSystem)
-
 LocalFileSystem$create <- function() {
   shared_ptr(LocalFileSystem, fs___LocalFileSystem__create())
 }
@@ -218,9 +230,7 @@ LocalFileSystem$create <- function() {
 #' @format NULL
 #' @rdname FileSystem
 #' @export
-
 SubTreeFileSystem <- R6Class("SubTreeFileSystem", inherit = FileSystem)
-
 SubTreeFileSystem$create <- function(base_path, base_fs) {
   xp <- fs___SubTreeFileSystem__create(base_path, base_fs)
   shared_ptr(SubTreeFileSystem, xp)
