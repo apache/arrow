36ce1c688b5d30dd3fa0bfabaf62e22eab529b34
Neal Richardson <neal.p.richardson@gmail.com>
2019-09-20 07:28:57 -0700

    ARROW-6544: [R] Documentation/polishing for 0.15 release
    Closes #5444 from nealrichardson/docs-0.15 and squashes the following commits:
    
    4a7f4a464 <Neal Richardson> Add to NEWS; fix bad rebase; add fs classes to pkgdown
    c708f6aaa <Neal Richardson> Consolidate csv read option docs/classes. Some pkgdown cleanup
    
    Authored-by: Neal Richardson <neal.p.richardson@gmail.com>
    Signed-off-by: Neal Richardson <neal.p.richardson@gmail.com>

--- r/R/csv.R
+++ r/R/csv.R
@@ -30,7 +30,7 @@
 #' If you need to control Arrow-specific reader parameters that don't have an
 #' equivalent in `readr::read_csv()`, you can either provide them in the
 #' `parse_options`, `convert_options`, or `read_options` arguments, or you can
-#' call [csv_table_reader()] directly for lower-level access.
+#' use [CsvTableReader] directly for lower-level access.
 #'
 #' @inheritParams make_readable_file
 #' @param delim Single character used to separate fields within a record.
@@ -59,10 +59,11 @@
 #' `TRUE`, blank rows will not be represented at all. If `FALSE`, they will be
 #' filled with missings.
 #' @param skip Number of lines to skip before reading data.
-#' @param parse_options see [csv_parse_options()]. If given, this overrides any
+#' @param parse_options see [file reader options][CsvReadOptions].
+#' If given, this overrides any
 #' parsing options provided in other arguments (e.g. `delim`, `quote`, etc.).
-#' @param convert_options see [csv_convert_options()]
-#' @param read_options see [csv_read_options()]
+#' @param convert_options see [file reader options][CsvReadOptions]
+#' @param read_options see [file reader options][CsvReadOptions]
 #' @param as_data_frame Should the function return a `data.frame` or an
 #' [arrow::Table][Table]?
 #'
@@ -70,7 +71,6 @@
 #' @export
 #' @examples
 #' \donttest{
-#' try({
 #'   tf <- tempfile()
 #'   on.exit(unlink(tf))
 #'   write.csv(iris, file = tf)
@@ -78,7 +78,6 @@
 #'   dim(df)
 #'   # Can select columns
 #'   df <- read_csv_arrow(tf, col_select = starts_with("Sepal"))
-#' })
 #' }
 read_delim_arrow <- function(file,
                              delim = ",",
@@ -111,11 +110,11 @@ read_delim_arrow <- function(file,
     read_options <- readr_to_csv_read_options(skip, col_names)
   }
   if (is.null(convert_options)) {
-    # TODO: col_types (needs wiring in csv_convert_options)
+    # TODO: col_types (needs wiring in CsvConvertOptions)
     convert_options <- readr_to_csv_convert_options(na, quoted_na)
   }
 
-  reader <- csv_table_reader(
+  reader <- CsvTableReader$create(
     file,
     read_options = read_options,
     parse_options = parse_options,
@@ -179,18 +178,41 @@ read_tsv_arrow <- function(file,
   eval.parent(mc)
 }
 
+#' @title Arrow CSV and JSON table reader classes
+#' @rdname CsvTableReader
+#' @name CsvTableReader
+#' @docType class
+#' @usage NULL
+#' @format NULL
+#' @description `CsvTableReader` and `JsonTableReader` wrap the Arrow C++ CSV
+#' and JSON table readers. See their usage in [read_csv_arrow()] and
+#' [read_json_arrow()], respectively.
+#'
+#' @section Factory:
+#'
+#' The `CsvTableReader$create()` and `JsonTableReader$create()` factory methods
+#' take the following arguments:
+#'
+#' - `file` A character path to a local file, or an Arrow input stream
+#' - `convert_options` (CSV only), `parse_options`, `read_options`: see
+#'    [CsvReadOptions]
+#' - `...` additional parameters.
+#'
+#' @section Methods:
+#'
+#' - `$Read()`: returns an Arrow Table.
+#'
 #' @include arrow-package.R
-
+#' @export
 CsvTableReader <- R6Class("CsvTableReader", inherit = Object,
   public = list(
     Read = function() shared_ptr(Table, csv___TableReader__Read(self))
   )
 )
-
 CsvTableReader$create <- function(file,
-                                  read_options = csv_read_options(),
-                                  parse_options = csv_parse_options(),
-                                  convert_options = csv_convert_options(),
+                                  read_options = CsvReadOptions$create(),
+                                  parse_options = CsvParseOptions$create(),
+                                  convert_options = CsvConvertOptions$create(),
                                   ...) {
   file <- make_readable_file(file)
   shared_ptr(
@@ -199,23 +221,68 @@ CsvTableReader$create <- function(file,
   )
 }
 
-#' Arrow CSV and JSON table readers
+#' @title File reader options
+#' @rdname CsvReadOptions
+#' @name CsvReadOptions
+#' @docType class
+#' @usage NULL
+#' @format NULL
+#' @description `CsvReadOptions`, `CsvParseOptions`, `CsvConvertOptions`,
+#' `JsonReadOptions`, and `JsonParseOptions` are containers for various
+#' file reading options. See their usage in [read_csv_arrow()] and
+#' [read_json_arrow()], respectively.
 #'
-#' These methods wrap the Arrow C++ CSV and JSON table readers.
-#' For an interface to the CSV reader that's more familiar for R users, see
-#' [read_csv_arrow()]
+#' @section Factory:
 #'
-#' @param file A character path to a local file, or an Arrow input stream
-#' @param read_options see [csv_read_options()]
-#' @param parse_options see [csv_parse_options()]
-#' @param convert_options see [csv_convert_options()]
-#' @param ... additional parameters.
+#' The `CsvReadOptions$create()` and `JsonReadOptions$create()` factory methods
+#' take the following arguments:
 #'
-#' @return An CsvTableReader or JsonTableReader R6
-#' object. Call `$Read()` on it to get an Arrow Table.
+#' - `use_threads` Whether to use the global CPU thread pool
+#' - `block_size` Block size we request from the IO layer; also determines
+#' the size of chunks when use_threads is `TRUE`. NB: if `FALSE`, JSON input
+#' must end with an empty line.
+#'
+#' `CsvReadOptions$create()` further accepts these additional arguments:
+#'
+#' - `skip_rows` Number of lines to skip before reading data (default 0)
+#' - `column_names` Character vector to supply column names. If length-0
+#' (the default), the first non-skipped row will be parsed to generate column
+#' names, unless `autogenerate_column_names` is `TRUE`.
+#' - `autogenerate_column_names` Logical: generate column names instead of
+#' using the first non-skipped row (the default)? If `TRUE`, column names will
+#' be "f0", "f1", ..., "fN".
+#'
+#' `CsvParseOptions$create()` takes the following arguments:
+#'
+#' - `delimiter` Field delimiting character (default `","`)
+#' - `quoting` Logical: are strings quoted? (default `TRUE`)
+#' - `quote_char` Quoting character, if `quoting` is `TRUE`
+#' - `double_quote` Logical: are quotes inside values double-quoted? (default `TRUE`)
+#' - `escaping` Logical: whether escaping is used (default `FALSE`)
+#' - `escape_char` Escaping character, if `escaping` is `TRUE`
+#' - `newlines_in_values` Logical: are values allowed to contain CR (`0x0d`)
+#'    and LF (`0x0a`) characters? (default `FALSE`)
+#' - `ignore_empty_lines` Logical: should empty lines be ignored (default) or
+#'    generate a row of missing values (if `FALSE`)?
+#'
+#' `JsonParseOptions$create()` accepts only the `newlines_in_values` argument.
+#'
+#' `CsvConvertOptions$create()` takes the following arguments:
+#'
+#' - `check_utf8` Logical: check UTF8 validity of string columns? (default `TRUE`)
+#' - `null_values` character vector of recognized spellings for null values.
+#'    Analogous to the `na.strings` argument to
+#'    [`read.csv()`][utils::read.csv()] or `na` in `readr::read_csv()`.
+#' - `strings_can_be_null` Logical: can string / binary columns have
+#'    null values? Similar to the `quoted_na` argument to `readr::read_csv()`.
+#'    (default `FALSE`)
+#'
+#' @section Methods:
+#'
+#' These classes have no implemented methods. They are containers for the
+#' options.
+#'
 #' @export
-csv_table_reader <- CsvTableReader$create
-
 CsvReadOptions <- R6Class("CsvReadOptions", inherit = Object)
 CsvReadOptions$create <- function(use_threads = option_use_threads(),
                                   block_size = 1048576L,
@@ -233,35 +300,23 @@ CsvReadOptions$create <- function(use_threads = option_use_threads(),
   ))
 }
 
-#' Read options for the Arrow file readers
-#'
-#' @param use_threads Whether to use the global CPU thread pool
-#' @param block_size Block size we request from the IO layer; also determines
-#' the size of chunks when use_threads is `TRUE`. NB: if `FALSE`, JSON input
-#' must end with an empty line.
-#' @param skip_rows Number of lines to skip before reading data.
-#' @param column_names Character vector to supply column names. If length-0
-#' (the default), the first non-skipped row will be parsed to generate column
-#' names, unless `autogenerate_column_names` is `TRUE`.
-#' @param autogenerate_column_names Logical: generate column names instead of
-#' using the first non-skipped row (the default)? If `TRUE`, column names will
-#' be "f0", "f1", ..., "fN".
-#'
-#' @export
-csv_read_options <- CsvReadOptions$create
-
 readr_to_csv_read_options <- function(skip, col_names) {
   if (isTRUE(col_names)) {
     # C++ default to parse is 0-length string array
     col_names <- character(0)
   }
   if (identical(col_names, FALSE)) {
-    csv_read_options(skip_rows = skip, autogenerate_column_names = TRUE)
+    CsvReadOptions$create(skip_rows = skip, autogenerate_column_names = TRUE)
   } else {
-    csv_read_options(skip_rows = skip, column_names = col_names)
+    CsvReadOptions$create(skip_rows = skip, column_names = col_names)
   }
 }
 
+#' @rdname CsvReadOptions
+#' @usage NULL
+#' @format NULL
+#' @docType class
+#' @export
 CsvParseOptions <- R6Class("CsvParseOptions", inherit = Object)
 CsvParseOptions$create <- function(delimiter = ",",
                                    quoting = TRUE,
@@ -286,20 +341,6 @@ CsvParseOptions$create <- function(delimiter = ",",
   ))
 }
 
-#' Parsing options for Arrow file readers
-#'
-#' @param delimiter Field delimiter
-#' @param quoting Whether quoting is used
-#' @param quote_char Quoting character (if `quoting` is `TRUE`)
-#' @param double_quote Whether a quote inside a value is double-quoted
-#' @param escaping Whether escaping is used
-#' @param escape_char Escaping character (if `escaping` is `TRUE`)
-#' @param newlines_in_values Whether values are allowed to contain CR (`0x0d`) and LF (`0x0a`) characters
-#' @param ignore_empty_lines Whether empty lines are ignored.  If `FALSE`, an empty line represents
-#'
-#' @export
-csv_parse_options <- CsvParseOptions$create
-
 readr_to_csv_parse_options <- function(delim = ",",
                                        quote = '"',
                                        escape_double = TRUE,
@@ -307,7 +348,7 @@ readr_to_csv_parse_options <- function(delim = ",",
                                        skip_empty_rows = TRUE) {
   # This function translates from the readr argument list to the arrow arg names
   # TODO: validate inputs
-  csv_parse_options(
+  CsvParseOptions$create(
     delimiter = delim,
     quoting = nzchar(quote),
     quote_char = quote,
@@ -319,6 +360,11 @@ readr_to_csv_parse_options <- function(delim = ",",
   )
 }
 
+#' @rdname CsvReadOptions
+#' @usage NULL
+#' @format NULL
+#' @docType class
+#' @export
 CsvConvertOptions <- R6Class("CsvConvertOptions", inherit = Object)
 CsvConvertOptions$create <- function(check_utf8 = TRUE,
                                      null_values = c("", "NA"),
@@ -339,18 +385,6 @@ CsvConvertOptions$create <- function(check_utf8 = TRUE,
   ))
 }
 
-#' Conversion options for the CSV reader
-#'
-#' @param check_utf8 Whether to check UTF8 validity of string columns
-#' @param null_values character vector of recognized spellings for null values.
-#' Analogous to the `na.strings` argument to [read.csv()][utils::read.csv()] or
-#' `na` in `readr::read_csv()`.
-#' @param strings_can_be_null Logical: whether string / binary columns can have
-#' null values. Similar to the `quoted_na` argument to `readr::read_csv()`.
-#'
-#' @export
-csv_convert_options <- CsvConvertOptions$create
-
 readr_to_csv_convert_options <- function(na, quoted_na) {
-    csv_convert_options(null_values = na, strings_can_be_null = quoted_na)
+    CsvConvertOptions$create(null_values = na, strings_can_be_null = quoted_na)
 }
