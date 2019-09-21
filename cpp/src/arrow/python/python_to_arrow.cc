8f3e2d6d60386ecbfa33c1797662a054e7aa51bd
Sutou Kouhei <kou@clear-code.com>
2019-09-21 19:16:36 +0900

    Remove needless constructor

--- cpp/src/arrow/python/python_to_arrow.cc
+++ cpp/src/arrow/python/python_to_arrow.cc
@@ -868,8 +868,6 @@ class DecimalConverter
   using BASE =
       TypedConverter<arrow::Decimal128Type, DecimalConverter<null_coding>, null_coding>;
 
-  DecimalConverter() : BASE(), decimal_type_(nullptr) {}
-
   Status Init(ArrayBuilder* builder) override {
     RETURN_NOT_OK(BASE::Init(builder));
     decimal_type_ = checked_pointer_cast<DecimalType>(this->typed_builder_->type());
