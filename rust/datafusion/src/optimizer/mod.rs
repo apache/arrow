33ce7d3699b65910b978294e89874282f77c9486
Andy Grove <andygrove73@gmail.com>
2019-09-22 18:02:27 -0400

    ARROW-6660: [Rust] [DataFusion] Minor docs update for 0.15.0 release
    Change the top-level description of DataFusion to better reflect what it actually does today.
    
    Also added a little more detail to some other docs.
    
    Closes #5466 from andygrove/datafusion-docs-update and squashes the following commits:
    
    c5df6e14d <Andy Grove> Minor docs update for 0.15.0 release
    
    Authored-by: Andy Grove <andygrove73@gmail.com>
    Signed-off-by: Paddy Horan <paddyhoran@hotmail.com>

--- rust/datafusion/src/optimizer/mod.rs
+++ rust/datafusion/src/optimizer/mod.rs
@@ -15,7 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//! Query optimizer module
+//! This module contains a query optimizer that operates against a logical plan and applies
+//! some simple rules to a logical plan, such as "Projection Push Down" and "Type Coercion".
 
 pub mod optimizer;
 pub mod projection_push_down;
