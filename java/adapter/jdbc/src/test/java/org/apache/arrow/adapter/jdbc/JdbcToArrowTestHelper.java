bc2c4588f020a307ecb33684a4bd0b0746d888b4
tianchen <niki.lj@alibaba-inc.com>
2019-09-27 12:52:57 +0800

    fix style

--- java/adapter/jdbc/src/test/java/org/apache/arrow/adapter/jdbc/JdbcToArrowTestHelper.java
+++ java/adapter/jdbc/src/test/java/org/apache/arrow/adapter/jdbc/JdbcToArrowTestHelper.java
@@ -20,7 +20,6 @@ package org.apache.arrow.adapter.jdbc;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
