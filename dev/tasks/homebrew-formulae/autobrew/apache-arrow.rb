59a6788c76330cf055bdbcbc7bdae7b0106c6656
Neal Richardson <neal.p.richardson@gmail.com>
2019-10-10 12:57:41 -0700

    ARROW-6833: [R][CI] Add crossbow job for full R autobrew macOS build
    Closes #5613 from nealrichardson/brew-s3-followup and squashes the following commits:
    
    06e16f48a <Neal Richardson> Merge branch 'master' into brew-s3-followup
    220ac99b4 <Neal Richardson> Turn off ARROW_S3 for autobrew because it fails
    7ec4e9210 <Neal Richardson> Add full autobrew job to crossbow tasks and try to debug aws-sdk-cpp failure
    
    Authored-by: Neal Richardson <neal.p.richardson@gmail.com>
    Signed-off-by: Neal Richardson <neal.p.richardson@gmail.com>

--- dev/tasks/homebrew-formulae/autobrew/apache-arrow.rb
+++ dev/tasks/homebrew-formulae/autobrew/apache-arrow.rb
@@ -29,9 +29,9 @@ class ApacheArrow < Formula
     root_url "https://autobrew.github.io/bottles"
   end
 
+  # NOTE: if you add something here, be sure to add to PKG_LIBS in r/tools/autobrew
   depends_on "cmake" => :build
   depends_on "flatbuffers" => :build
-  depends_on "aws-sdk-cpp"
   depends_on "boost"
   depends_on "double-conversion"
   depends_on "lz4"
@@ -47,7 +47,7 @@ class ApacheArrow < Formula
       -DARROW_JEMALLOC=OFF
       -DARROW_USE_GLOG=OFF
       -DARROW_PYTHON=OFF
-      -DARROW_S3=ON
+      -DARROW_S3=OFF
       -DARROW_WITH_LZ4=ON
       -DARROW_WITH_ZLIB=ON
       -DARROW_WITH_SNAPPY=ON
