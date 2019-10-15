018e1ff4a9a0d430bdb332b86a4a3dac677c3945
Wes McKinney <wesm+git@apache.org>
2019-10-15 12:51:39 +0900

    ARROW-6877: [C++] Add additional Boost versions to support 1.71 and the presumed next 2 future versions
    Boost 1.71 was released and July and landed in conda-forge about 3 weeks ago.
    
    Closes #5654 from wesm/ARROW-6877 and squashes the following commits:
    
    dcf192713 <Wes McKinney> Add additional Boost versions
    
    Authored-by: Wes McKinney <wesm+git@apache.org>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -586,6 +586,12 @@ if(MSVC AND ARROW_USE_STATIC_CRT)
   set(Boost_USE_STATIC_RUNTIME ON)
 endif()
 set(Boost_ADDITIONAL_VERSIONS
+    "1.73.0"
+    "1.73"
+    "1.72.0"
+    "1.72"
+    "1.71.0"
+    "1.71"
     "1.70.0"
     "1.70"
     "1.69.0"
