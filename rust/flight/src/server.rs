d4b960c1b8564b9cf363d040eb33df63bf0e0e9a
Andy Grove <andygrove73@gmail.com>
2019-11-17 10:16:10 -0700

    save

--- rust/flight/src/server.rs
+++ rust/flight/src/server.rs
@@ -1,10 +1,33 @@
 use tonic::{transport::Server, Request, Response, Status};
+use tonic::codegen::*;
 
 pub mod arrow_flight {
     tonic::include_proto!("arrow.flight.protocol"); // The string specified here must match the proto package name
 }
 
 use arrow_flight::{
-//    server::{Greeter, GreeterServer},
-//    HelloReply, HelloRequest,
+    server,
+    server::FlightService,
+    FlightData,
+    PutResult,
+    HandshakeRequest
 };
+
+pub struct FlightServiceImpl {}
+
+#[tonic::async_trait]
+impl FlightService for FlightServiceImpl {
+
+    //TODO the generated code declares a HandshakeStream type within the trait and I don't
+    // know how to import it here
+
+    async fn handshake(
+        &self,
+        request: tonic::Request<tonic::Streaming<HandshakeRequest>>,
+    ) -> Result<tonic::Response<arrow_flight::server::FlightService::HandshakeStream>, tonic::Status> {
+        unimplemented!()
+    }
+
+    //TODO implement the other methods
+
+}
