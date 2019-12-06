f27683a259cb064251f78ca9e55759238bc6a2e5
Renjie Liu <liurenjie2008@gmail.com>
2019-12-06 11:07:24 +0800

    Fix comment

--- rust/arrow/src/error.rs
+++ rust/arrow/src/error.rs
@@ -73,7 +73,21 @@ impl From<::std::string::FromUtf8Error> for ArrowError {
 
 impl Display for ArrowError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Arrow error happened!")
+        match self {
+            &ArrowError::MemoryError(ref desc) => write!(f, "Memory error: {}", desc),
+            &ArrowError::ParseError(ref desc) => write!(f, "Parser error: {}", desc),
+            &ArrowError::ComputeError(ref desc) => write!(f, "Compute error: {}", desc),
+            &ArrowError::DivideByZero => write!(f, "Divide by zero error"),
+            &ArrowError::CsvError(ref desc) => write!(f, "Csv error: {}", desc),
+            &ArrowError::JsonError(ref desc) => write!(f, "Json error: {}", desc),
+            &ArrowError::IoError(ref desc) => write!(f, "Io error: {}", desc),
+            &ArrowError::InvalidArgumentError(ref desc) => {
+                write!(f, "Invalid argument error: {}", desc)
+            }
+            &ArrowError::ParquetError(ref desc) => {
+                write!(f, "Parquet argument error: {}", desc)
+            }
+        }
     }
 }
 
