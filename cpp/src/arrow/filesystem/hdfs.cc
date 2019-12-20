0c66726e438dfa4c143869392581e7a0cbc2a8f4
Fabian Höring <f.horing@criteo.com>
2019-12-20 18:05:08 +0100

    Fix linter

--- cpp/src/arrow/filesystem/hdfs.cc
+++ cpp/src/arrow/filesystem/hdfs.cc
@@ -317,7 +317,8 @@ Result<HdfsOptions> HdfsOptions::FromUri(const Uri& uri) {
 
   std::string host;
   if (useHdfs3) {
-    ARROW_LOG(WARNING) << "viewfs://namenode resolves to hdfs://namenode with hdfs3 driver.";
+    ARROW_LOG(WARNING)
+        << "viewfs://namenode resolves to hdfs://namenode with hdfs3 driver.";
     host = uri.host();
   } else {
     host = uri.scheme() + "://" + uri.host();
