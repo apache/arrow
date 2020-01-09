acfcdee75acb4b1814f2e727c150a7403d618e8f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-01-09 14:27:28 +0100

    ARROW-7528: [Python] Remove usage of deprecated pd.np and pd.datetime in tests
    Closes #6148 from jorisvandenbossche/ARROW-7528 and squashes the following commits:
    
    549244d6e <Joris Van den Bossche> ARROW-7528:  Remove usage of deprecated pd.np and pd.datetime in tests
    
    Authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>
    Signed-off-by: Krisztián Szűcs <szucs.krisztian@gmail.com>

--- python/pyarrow/tests/test_feather.py
+++ python/pyarrow/tests/test_feather.py
@@ -441,9 +441,9 @@ class TestFeatherReader(unittest.TestCase):
         self._check_pandas_roundtrip(df)
 
     def test_timestamp_with_nulls(self):
-        df = pd.DataFrame({'test': [pd.datetime(2016, 1, 1),
+        df = pd.DataFrame({'test': [pd.Timestamp(2016, 1, 1),
                                     None,
-                                    pd.datetime(2016, 1, 3)]})
+                                    pd.Timestamp(2016, 1, 3)]})
         df['with_tz'] = df.test.dt.tz_localize('utc')
 
         self._check_pandas_roundtrip(df, null_counts=[1, 1])
