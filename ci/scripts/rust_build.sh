ac669170a2d5a6a5f54e8e2c59945fb76bc08333
Andy Grove <andygrove73@gmail.com>
2020-01-21 17:39:41 -0700

    Test

--- ci/scripts/rust_build.sh
+++ ci/scripts/rust_build.sh
@@ -36,17 +36,10 @@ pushd ${source_dir}
 
 # flatbuffer codegen
 echo "Generating IPC"
-pwd
-
-echo "generating from fbs files:"
-ls ../format/*.fbs
-
 flatc --rust -o arrow/src/ipc/gen/ ../format/*.fbs
 
 # work around some bugs in flatbuffers
-find arrow/src/ipc/gen/
-
-#find arrow/src/ipc/gen/ -name "*_generated.rs" -exec sed -i 's/type__type/type_type/g' {} \; 2>/dev/null
+#find arrow/src/ipc/gen/ -name "*_generated.rs" -exec sed -i 's/type__type/type_type/g' {} \;
 
 # build entire project
 RUSTFLAGS="-D warnings" cargo build --all-targets
