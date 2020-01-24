331fa3d1eba54bc5c14dcc650f2cdf0e084d0483
Michael Chirico <michael.chirico@grabtaxi.com>
2020-01-25 02:28:07 +0800

    linting

--- r/src/array_from_vector.cpp
+++ r/src/array_from_vector.cpp
@@ -870,8 +870,9 @@ std::shared_ptr<arrow::DataType> InferType(SEXP x) {
         return std::make_shared<StructType>(std::move(fields));
       } else {
         R_xlen_t n = XLENGTH(x);
-        if (n == 0)
+        if (n == 0) {
           Rcpp::stop("received length-0 list");
+        }
         std::shared_ptr<arrow::DataType> element_type = InferType(VECTOR_ELT(x, 0));
         for (R_xlen_t i=1; i<n; i++) {
           if (InferType(VECTOR_ELT(x, i)) != element_type) {
