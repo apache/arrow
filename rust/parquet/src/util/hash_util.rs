5a1558997297d54af574a70247e3f77f2fa449cb
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-01-24 12:00:01 +0100

    fix: murmur hash is only safe on platforms with unaligned loads
    Might be other platforms this works on as well, but those could be added
    as they are found

--- rust/parquet/src/util/hash_util.rs
+++ rust/parquet/src/util/hash_util.rs
@@ -20,10 +20,14 @@ use crate::data_type::AsBytes;
 /// Computes hash value for `data`, with a seed value `seed`.
 /// The data type `T` must implement the `AsBytes` trait.
 pub fn hash<T: AsBytes>(data: &T, seed: u32) -> u32 {
+    hash_(data.as_bytes(), seed)
+}
+
+fn hash_(data: &[u8], seed: u32) -> u32 {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-    {
+    unsafe {
         if is_x86_feature_detected!("sse4.2") {
-            unsafe { crc32_hash(data, seed) }
+            crc32_hash(data, seed)
         } else {
             murmur_hash2_64a(data, seed as u64) as u32
         }
@@ -34,16 +38,15 @@ const MURMUR_PRIME: u64 = 0xc6a4a7935bd1e995;
 const MURMUR_R: i32 = 47;
 
 /// Rust implementation of MurmurHash2, 64-bit version for 64-bit platforms
-fn murmur_hash2_64a<T: AsBytes>(data: &T, seed: u64) -> u64 {
-    let data_bytes = data.as_bytes();
+///
+/// SAFTETY Only safe on platforms which support unaligned loads (like x86_64)
+unsafe fn murmur_hash2_64a(data_bytes: &[u8], seed: u64) -> u64 {
     let len = data_bytes.len();
     let len_64 = (len / 8) * 8;
-    let data_bytes_64 = unsafe {
-        std::slice::from_raw_parts(
-            &data_bytes[0..len_64] as *const [u8] as *const u64,
-            len / 8,
-        )
-    };
+    let data_bytes_64 = std::slice::from_raw_parts(
+        &data_bytes[0..len_64] as *const [u8] as *const u64,
+        len / 8,
+    );
 
     let mut h = seed ^ (MURMUR_PRIME.wrapping_mul(data_bytes.len() as u64));
     for v in data_bytes_64 {
@@ -92,13 +95,12 @@ fn murmur_hash2_64a<T: AsBytes>(data: &T, seed: u64) -> u64 {
 /// CRC32 hash implementation using SSE4 instructions. Borrowed from Impala.
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 #[target_feature(enable = "sse4.2")]
-unsafe fn crc32_hash<T: AsBytes>(data: &T, seed: u32) -> u32 {
+unsafe fn crc32_hash(bytes: &[u8], seed: u32) -> u32 {
     #[cfg(target_arch = "x86")]
     use std::arch::x86::*;
     #[cfg(target_arch = "x86_64")]
     use std::arch::x86_64::*;
 
-    let bytes: &[u8] = data.as_bytes();
     let u32_num_bytes = std::mem::size_of::<u32>();
     let mut num_bytes = bytes.len();
     let num_words = num_bytes / u32_num_bytes;
@@ -134,14 +136,16 @@ mod tests {
 
     #[test]
     fn test_murmur2_64a() {
-        let result = murmur_hash2_64a(&"hello", 123);
-        assert_eq!(result, 2597646618390559622);
+        unsafe {
+            let result = murmur_hash2_64a(b"hello", 123);
+            assert_eq!(result, 2597646618390559622);
 
-        let result = murmur_hash2_64a(&"helloworld", 123);
-        assert_eq!(result, 4934371746140206573);
+            let result = murmur_hash2_64a(b"helloworld", 123);
+            assert_eq!(result, 4934371746140206573);
 
-        let result = murmur_hash2_64a(&"helloworldparquet", 123);
-        assert_eq!(result, 2392198230801491746);
+            let result = murmur_hash2_64a(b"helloworldparquet", 123);
+            assert_eq!(result, 2392198230801491746);
+        }
     }
 
     #[test]
@@ -149,13 +153,13 @@ mod tests {
     fn test_crc32() {
         if is_x86_feature_detected!("sse4.2") {
             unsafe {
-                let result = crc32_hash(&"hello", 123);
+                let result = crc32_hash(b"hello", 123);
                 assert_eq!(result, 2927487359);
 
-                let result = crc32_hash(&"helloworld", 123);
+                let result = crc32_hash(b"helloworld", 123);
                 assert_eq!(result, 314229527);
 
-                let result = crc32_hash(&"helloworldparquet", 123);
+                let result = crc32_hash(b"helloworldparquet", 123);
                 assert_eq!(result, 667078870);
             }
         }
