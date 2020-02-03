cb81f7d84c26cd75f4568ae1b29744cb4218b578
Krisztián Szűcs <szucs.krisztian@gmail.com>
2020-02-03 17:30:19 +0100

    ARROW-7735: [Release][Python] Use pip to install dependencies for wheel verification
    The wheel verification script fails for python 3.5.
    At the same time the wheel properly works for python 3.5 docker
    images without conda environment.
    
    Conda forge doesn't maintain packages for python 3.5 anymore
    and something must have mixed with the numpy versions.
    
    This change fixed the wheel verification locally for me.
    
    Closes #6339 from kszucs/wheel-verification and squashes the following commits:
    
    3e9694958 <Krisztián Szűcs> remove pytest verbose flags
    026e5fb34 <Krisztián Szűcs> use pip to install dependencies for wheel verification
    
    Authored-by: Krisztián Szűcs <szucs.krisztian@gmail.com>
    Signed-off-by: Krisztián Szűcs <szucs.krisztian@gmail.com>

--- dev/release/verify-release-candidate.sh
+++ dev/release/verify-release-candidate.sh
@@ -609,8 +609,10 @@ test_linux_wheels() {
       pip install python-rc/${VERSION}-rc${RC_NUMBER}/pyarrow-${VERSION}-cp${py_arch//[mu.]/}-cp${py_arch//./}-manylinux${ml_spec}_x86_64.whl
       check_python_imports py_arch
 
-      # execute the python unit tests
-      conda install -y --file ${ARROW_DIR}/ci/conda_env_python.yml pandas
+      # install test requirements
+      pip install -r ${ARROW_DIR}/python/requirements-test.txt
+
+      # execute the tests
       pytest --pyargs pyarrow
     done
 
@@ -640,8 +642,10 @@ test_macos_wheels() {
     pip install python-rc/${VERSION}-rc${RC_NUMBER}/pyarrow-${VERSION}-cp${py_arch//[m.]/}-cp${py_arch//./}-${macos_suffix}.whl
     check_python_imports py_arch
 
-    # execute the python unit tests
-    conda install -y --file ${ARROW_DIR}/ci/conda_env_python.yml pandas
+    # install test requirements
+    pip install -r ${ARROW_DIR}/python/requirements-test.txt
+
+    # execute the tests
     pytest --pyargs pyarrow
 
     conda deactivate
