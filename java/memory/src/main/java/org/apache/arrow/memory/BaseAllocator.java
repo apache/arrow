0a9f743d32982de847cbb6e1cce63b530c707d3d
Projjal Chanda <iam@pchanda.com>
2020-02-11 11:32:36 +0530

    Release accounted-for reservation memory to parent in case of leak

--- java/memory/src/main/java/org/apache/arrow/memory/BaseAllocator.java
+++ java/memory/src/main/java/org/apache/arrow/memory/BaseAllocator.java
@@ -429,7 +429,6 @@ public abstract class BaseAllocator extends Accountant implements BufferAllocato
 
     isClosed = true;
 
-    StringBuilder outstandingChildAllocators = new StringBuilder();
     if (DEBUG) {
       synchronized (DEBUG_LOCK) {
         verifyAllocator();
@@ -465,20 +464,14 @@ public abstract class BaseAllocator extends Accountant implements BufferAllocato
         }
 
       }
-    } else {
-      if (!childAllocators.isEmpty()) {
-        outstandingChildAllocators.append("Outstanding child allocators : \n");
-        synchronized (childAllocators) {
-          for (final BaseAllocator childAllocator : childAllocators.keySet()) {
-            outstandingChildAllocators.append(String.format("  %s", childAllocator.toString()));
-          }
-        }
-      }
     }
 
     // Is there unaccounted-for outstanding allocation?
     final long allocated = getAllocatedMemory();
     if (allocated > 0) {
+      if (parent != null && reservation > allocated) {
+        parent.releaseBytes(reservation - allocated);
+      }
       String msg = String.format("Memory was leaked by query. Memory leaked: (%d)\n%s", allocated,
           toString());
       logger.error(msg);
