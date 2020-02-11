cddd55afacfb491b69eb5ff26c968f9ed6139a06
Jorge C. Leitao <jorgecarleitao@gmail.com>
2020-02-10 18:21:20 -0700

    ARROW-7787: [Rust] Added .collect to Table API
    This commit adds the method ".collect" to Table, thus
    reducing the code necessary to execute a query constructed
    from the Table API.
    
    This also adds an example to the examples on how to use the Table API on a table declared in-Memory.
    
    Closes #6375 from jorgecarleitao/table_collect and squashes the following commits:
    
    8a6efaa33 <Jorge C. Leitao> ARROW-7787:  Added .collect to Table API
    
    Authored-by: Jorge C. Leitao <jorgecarleitao@gmail.com>
    Signed-off-by: Andy Grove <andygrove73@gmail.com>

---
+++ rust/datafusion/examples/memory_table_api.rs
@@ -0,0 +1,97 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::boxed::Box;
+use std::sync::Arc;
+
+extern crate arrow;
+extern crate datafusion;
+
+use arrow::array::{Int32Array, StringArray};
+use arrow::datatypes::{DataType, Field, Schema};
+use arrow::record_batch::RecordBatch;
+
+use datafusion::datasource::MemTable;
+use datafusion::execution::context::ExecutionContext;
+use datafusion::logicalplan::{Expr, ScalarValue};
+
+/// This example demonstrates basic uses of the Table API on an in-memory table
+fn main() {
+    // define a schema.
+    let schema = Arc::new(Schema::new(vec![
+        Field::new("a", DataType::Utf8, false),
+        Field::new("b", DataType::Int32, false),
+    ]));
+
+    // define data.
+    let batch = RecordBatch::try_new(
+        schema.clone(),
+        vec![
+            Arc::new(StringArray::from(vec!["a", "b", "c", "d"])),
+            Arc::new(Int32Array::from(vec![1, 10, 10, 100])),
+        ],
+    )
+    .unwrap();
+
+    // declare a new context. In spark API, this corresponds to a new spark SQLsession
+    let mut ctx = ExecutionContext::new();
+
+    // declare a table in memory. In spark API, this corresponds to createDataFrame(...).
+    let provider = MemTable::new(schema, vec![batch]).unwrap();
+    ctx.register_table("t", Box::new(provider));
+    let t = ctx.table("t").unwrap();
+
+    // construct an expression corresponding to "SELECT a, b FROM t WHERE b = 10" in SQL
+    let filter = t
+        .col("b")
+        .unwrap()
+        .eq(&Expr::Literal(ScalarValue::Int32(10)));
+
+    let t = t
+        .select_columns(vec!["a", "b"])
+        .unwrap()
+        .filter(filter)
+        .unwrap();
+
+    // execute
+    let results = t.collect(&mut ctx, 10).unwrap();
+
+    // print results
+    results.iter().for_each(|batch| {
+        println!(
+            "RecordBatch has {} rows and {} columns",
+            batch.num_rows(),
+            batch.num_columns()
+        );
+
+        let c1 = batch
+            .column(0)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .expect("String type");
+
+        let c2 = batch
+            .column(1)
+            .as_any()
+            .downcast_ref::<Int32Array>()
+            .expect("Int type");
+
+        for i in 0..batch.num_rows() {
+            println!("{}, {}", c1.value(i), c2.value(i),);
+        }
+    });
+}
