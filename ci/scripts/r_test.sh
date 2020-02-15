adab7070ee2303c3da9c830437cf48c8c140286c
Neal Richardson <neal.p.richardson@gmail.com>
2020-02-14 17:33:23 -0800

    Move home dir check

--- ci/scripts/r_test.sh
+++ ci/scripts/r_test.sh
@@ -37,6 +37,16 @@ export _R_CHECK_CRAN_INCOMING_REMOTE_=FALSE
 export _R_CHECK_LIMIT_CORES_=FALSE
 export VERSION=$(grep ^Version DESCRIPTION | sed s/Version:\ //)
 
+# Make sure we aren't writing to the home dir (CRAN _hates_ this but there is no official check)
+BEFORE=$(ls -alh ~/)
+echo $BEFORE
+
 ${R_BIN} -e "rcmdcheck::rcmdcheck(build_args = '--no-build-vignettes', args = c('--no-manual', '--as-cran', '--ignore-vignettes', '--run-donttest'), error_on = 'warning', check_dir = 'check')"
 
+AFTER=$(ls -alh ~/)
+echo $AFTER
+if [ "$BEFORE" != "$AFTER"]; then
+  ls -alh ~/.cmake
+  exit 1
+fi
 popd
