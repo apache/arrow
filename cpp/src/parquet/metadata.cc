cabe494f14a08f4791c6b09ebf88ee7ef5112662
Antoine Pitrou <antoine@python.org>
2020-03-31 15:32:14 +0200

    Avoid throwing when matching fails for complexity reasons

--- cpp/src/parquet/metadata.cc
+++ cpp/src/parquet/metadata.cc
@@ -38,13 +38,39 @@
 #if defined(PARQUET_USE_BOOST_REGEX)
 #include <boost/regex.hpp>  // IWYU pragma: keep
 using ::boost::regex;
-using ::boost::regex_match;
 using ::boost::smatch;
+
+template <typename... Args>
+static bool regex_match(Args&&... args) {
+  try {
+    return boost::regex_match(std::forward<Args>(args)...);
+  } catch (const boost::regex_error& e) {
+    if (e.code() == boost::regex_constants::error_complexity ||
+        e.code() == boost::regex_constants::error_stack) {
+      // Input-dependent error => return as if matching failed
+      return false;
+    }
+    throw;
+  }
+}
 #else
 #include <regex>
 using ::std::regex;
-using ::std::regex_match;
 using ::std::smatch;
+
+template <typename... Args>
+static bool regex_match(Args&&... args) {
+  try {
+    return std::regex_match(std::forward<Args>(args)...);
+  } catch (const std::regex_error& e) {
+    if (e.code() == std::regex_constants::error_complexity ||
+        e.code() == std::regex_constants::error_stack) {
+      // Input-dependent error => return as if matching failed
+      return false;
+    }
+    throw;
+  }
+}
 #endif
 
 namespace parquet {
