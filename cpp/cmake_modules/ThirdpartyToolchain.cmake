ac0b3cd98a6b2dc5d4414825027b0b9a4f02f8fe
Benjamin Kietzman <bengilgit@gmail.com>
2020-04-10 17:50:52 -0400

    revert boost_ep url preferences, add TODO(ARROW-6407) comments

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -266,8 +266,11 @@ else()
     BOOST_SOURCE_URL
     # These are trimmed boost bundles we maintain.
     # See cpp/build_support/trim-boost.sh
-    "https://github.com/ursa-labs/thirdparty/releases/download/latest/boost_${ARROW_BOOST_BUILD_VERSION_UNDERSCORES}.tar.gz"
     "https://dl.bintray.com/ursalabs/arrow-boost/boost_${ARROW_BOOST_BUILD_VERSION_UNDERSCORES}.tar.gz"
+    # FIXME(ARROW-6407) automate uploading this archive to ensure it reflects
+    # our currently used packages and doesn't fall out of sync with
+    # ${ARROW_BOOST_BUILD_VERSION_UNDERSCORES}
+    "https://github.com/ursa-labs/thirdparty/releases/download/latest/boost_${ARROW_BOOST_BUILD_VERSION_UNDERSCORES}.tar.gz"
     # Fallback: full boost bundles.
     "https://github.com/boostorg/boost/archive/boost-${ARROW_BOOST_BUILD_VERSION}.tar.gz"
     "https://dl.bintray.com/boostorg/release/${ARROW_BOOST_BUILD_VERSION}/source/boost_${ARROW_BOOST_BUILD_VERSION_UNDERSCORES}.tar.gz"
