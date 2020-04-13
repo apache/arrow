81319602a95c61d0467596a0ad6d7502f65ed38a
Micah Kornfield <emkornfield@gmail.com>
2020-04-13 04:26:41 +0000

    PARQUET-1840: [C++] Don't double assign memory

--- cpp/src/parquet/encoding.h
+++ cpp/src/parquet/encoding.h
@@ -296,13 +296,16 @@ class TypedDecoder : virtual public Decoder {
 
     // Depending on the number of nulls, some of the value slots in buffer may
     // be uninitialized, and this will cause valgrind warnings / potentially UB
+    // TODO: With SIMD shuffle in the loop below we might be able to get better
+    // performance on doing the memset only on values that aren't copied, and fill
+    // in all the values below.
     memset(static_cast<void*>(buffer + values_read), 0,
            (num_values - values_read) * sizeof(T));
 
     // Add spacing for null entries. As we have filled the buffer from the front,
     // we need to add the spacing from the back.
     int values_to_move = values_read;
-    for (int i = num_values - 1; i >= 0; i--) {
+    for (int i = num_values - 1; i > (values_to_move - 1) && values_to_move > 0; i--) {
       if (BitUtil::GetBit(valid_bits, valid_bits_offset + i)) {
         buffer[i] = buffer[--values_to_move];
       }
