88c455ecdbdc8f6c1a2d452a351204afdfdec3ae
Antoine Pitrou <antoine@python.org>
2020-04-14 11:00:37 +0200

    Fix comment wording

--- cpp/src/parquet/encoding.h
+++ cpp/src/parquet/encoding.h
@@ -303,11 +303,11 @@ class TypedDecoder : virtual public Decoder {
     // we need to add the spacing from the back.
     int values_to_move = values_read - 1;
     // We stop early on one of two conditions:
-    // 1. There are more null values that need spacing. Note we infer this backwards,
-    //    when 'i' is equal to values to move it indicates all nulls have been consumed.
-    //    to move it implies all nulls have been spaced.
-    // 2. There are no more value values that need to values to move which indicates
-    //    all remaining slots are null, so there exact value doesn't matter.
+    // 1. There are no more null values that need spacing.  Note we infer this
+    //     backwards, when 'i' is equal to 'values_to_move' it indicates
+    //    all nulls have been consumed.
+    // 2. There are no more non-null values that need to move which indicates
+    //    all remaining slots are null, so their exact value doesn't matter.
     for (int i = num_values - 1; (i > values_to_move) && (values_to_move >= 0); i--) {
       if (BitUtil::GetBit(valid_bits, valid_bits_offset + i)) {
         buffer[i] = buffer[values_to_move];
