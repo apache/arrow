d5dfa0ec083163f5d4b62dd35d9c305bdcb856b2
Prudhvi Porandla <prudhvi.porandla@icloud.com>
2020-04-14 03:05:45 +0200

    ARROW-8415: [C++][Packaging] Fix gandiva linux job
    - use ubuntu xenial instead of trusty
    - use crossbow to deploy
    
    Closes #6910 from pprudhvi/movefix
    
    Authored-by: Prudhvi Porandla <prudhvi.porandla@icloud.com>
    Signed-off-by: Krisztián Szűcs <szucs.krisztian@gmail.com>

--- cpp/src/arrow/testing/gtest_common.h
+++ cpp/src/arrow/testing/gtest_common.h
@@ -50,7 +50,7 @@ class TestBase : public ::testing::Test {
     for (int64_t i = 0; i < null_count; i++) {
       BitUtil::ClearBit(null_bitmap->mutable_data(), i * (length / null_count));
     }
-    return null_bitmap;
+    return std::move(null_bitmap);
   }
 
   template <typename ArrayType>
