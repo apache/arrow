ed59ab61eb49c79988f703bf26b1a4255efbcdd4
Krisztián Szűcs <szucs.krisztian@gmail.com>
2020-04-15 14:03:52 +0200

    windows wheel tests

--- dev/tasks/python-wheels/win-build.bat
+++ dev/tasks/python-wheels/win-build.bat
@@ -18,14 +18,14 @@
 @echo on
 
 @rem create conda environment for compiling
-conda update --yes --quiet conda
+call conda update --yes --quiet conda
 
-conda create -n wheel-build -q -y -c conda-forge ^
+call conda create -n wheel-build -q -y -c conda-forge ^
     --file=%ARROW_SRC%\ci\conda_env_cpp.yml ^
     --file=%ARROW_SRC%\ci\conda_env_gandiva.yml ^
     python=%PYTHON_VERSION% || exit /B
 
-call conda.bat activate wheel-build
+call conda activate wheel-build
 
 @rem Cannot use conda_env_python.yml here because conda-forge has
 @rem ceased providing up-to-date packages for Python 3.5
@@ -82,14 +82,14 @@ pushd %ARROW_SRC%\python
 python setup.py bdist_wheel || exit /B
 popd
 
-call conda.bat deactivate
+call conda deactivate
 
 set ARROW_TEST_DATA=%ARROW_SRC%\testing\data
 
 @rem test the wheel
 @rem TODO For maximum reliability, we should test in a plain virtualenv instead.
-conda create -n wheel-test -c conda-forge -q -y python=%PYTHON_VERSION% || exit /B
-call conda.bat activate wheel-test
+call conda create -n wheel-test -c conda-forge -q -y python=%PYTHON_VERSION% || exit /B
+call conda activate wheel-test
 
 @rem install the built wheel
 pip install -vv --no-index --find-links=%ARROW_SRC%\python\dist\ pyarrow || exit /B
