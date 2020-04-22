479b992a8455b55f7a35676aed8b06c3d5dfd614
Yibo Cai <yibo.cai@arm.com>
2020-04-22 02:28:28 +0000

    Remove ARROW_USE_SIMD

--- cpp/cmake_modules/DefineOptions.cmake
+++ cpp/cmake_modules/DefineOptions.cmake
@@ -95,9 +95,6 @@ if("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
   define_option(ARROW_USE_PRECOMPILED_HEADERS "Use precompiled headers when compiling"
                 OFF)
 
-  # Disable this option to exercise non-SIMD fallbacks
-  define_option(ARROW_USE_SIMD "Build with SIMD optimizations" ON)
-
   define_option_string(ARROW_SIMD_LEVEL
                        "SIMD compiler optimization level"
                        "SSE4_2" # default to SSE4.2
