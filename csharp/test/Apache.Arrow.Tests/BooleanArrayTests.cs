2a4100ec905f3591d6ff08c2336bde5742ad0d8e
Zachary Gramana <zachary.gramana@ge.com>
2020-04-30 22:29:53 -0700

    Throw an exception rather than swallow a null bool

--- csharp/test/Apache.Arrow.Tests/BooleanArrayTests.cs
+++ csharp/test/Apache.Arrow.Tests/BooleanArrayTests.cs
@@ -48,13 +48,13 @@ namespace Apache.Arrow.Tests
                         .Append(false)
                         .Build();
 
-                    Assert.False(array1.GetValue(0).GetValueOrDefault());
+                    Assert.False(array1.GetValue(0).Value);
 
                     var array2 = new BooleanArray.Builder()
                         .Append(true)
                         .Build();
 
-                    Assert.True(array2.GetValue(0).GetValueOrDefault());
+                    Assert.True(array2.GetValue(0).Value);
                 }
             }
 
@@ -72,7 +72,7 @@ namespace Apache.Arrow.Tests
 
                     for (var i = 0; i < array.Length; i++)
                     {
-                        Assert.False(array.GetValue(i).GetValueOrDefault());
+                        Assert.False(array.GetValue(i).Value);
                     }
                 }
             }
@@ -90,7 +90,7 @@ namespace Apache.Arrow.Tests
                         .Toggle(index)
                         .Build();
 
-                    Assert.False(array.GetValue(index).GetValueOrDefault());
+                    Assert.False(array.GetValue(index).Value);
                 }
 
                 [Theory]
@@ -104,7 +104,7 @@ namespace Apache.Arrow.Tests
                         .Toggle(index)
                         .Build();
 
-                    Assert.True(array.GetValue(index).GetValueOrDefault());
+                    Assert.True(array.GetValue(index).Value);
                 }
 
                 [Fact]
@@ -129,8 +129,8 @@ namespace Apache.Arrow.Tests
                         .Swap(0, 7)
                         .Build();
 
-                    Assert.False(array.GetValue(0).GetValueOrDefault());
-                    Assert.True(array.GetValue(7).GetValueOrDefault());
+                    Assert.False(array.GetValue(0).Value);
+                    Assert.True(array.GetValue(7).Value);
                     #pragma warning disable CS0618
                     Assert.False(array.GetBoolean(0));
                     Assert.True(array.GetBoolean(7));
@@ -163,7 +163,7 @@ namespace Apache.Arrow.Tests
                         .Set(index, true)
                         .Build();
 
-                    Assert.True(array.GetValue(index).GetValueOrDefault());
+                    Assert.True(array.GetValue(index).Value);
                 }
 
                 [Theory]
@@ -179,7 +179,7 @@ namespace Apache.Arrow.Tests
                         .Set(index, false)
                         .Build();
 
-                    Assert.False(array.GetValue(index).GetValueOrDefault());
+                    Assert.False(array.GetValue(index).Value);
                 }
 
                 [Theory]
@@ -195,7 +195,7 @@ namespace Apache.Arrow.Tests
                     {
                         if (i != index)
                         {
-                            Assert.False(array.GetValue(i).GetValueOrDefault());
+                            Assert.False(array.GetValue(i).Value);
                             #pragma warning disable CS0618
                             Assert.False(array.GetBoolean(i));
                             #pragma warning restore CS0618
