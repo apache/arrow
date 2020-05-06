bcc783cfaf9d54c52c2f93bcfaeb0d4ae6744d80
Sutou Kouhei <kou@clear-code.com>
2020-05-07 06:40:00 +0900

    [Packaging][deb][RPM] Use directory in host as build directory
    Travis CI uses different partition for workspace (/) and
    Docker (/var/lib/docker/) and /var/lib/docker/ doesn't have enough
    disk space. If we use directory in host as build directory, we can use
    disk space in /.

--- dev/tasks/linux-packages/package-task.rb
+++ dev/tasks/linux-packages/package-task.rb
@@ -139,6 +139,12 @@ class PackageTask
       "--tty",
       "--volume", "#{Dir.pwd}:/host:rw",
     ]
+    build_dir = ENV["BUILD_DIR"]
+    if build_dir
+      build_dir = "#{File.expand_path(build_dir)}/#{id}"
+      mkdir_p(build_dir)
+      run_command_line.concat(["--volume", "#{build_dir}:/build:rw"])
+    end
     if debug_build?
       build_command_line.concat(["--build-arg", "DEBUG=yes"])
       run_command_line.concat(["--env", "DEBUG=yes"])
