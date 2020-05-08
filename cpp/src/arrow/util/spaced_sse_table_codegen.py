9f5800686f706686c3fd790cffa5533aea5bd059
Frank Du <frank.du@intel.com>
2020-05-08 01:53:53 +0000

    More note for how the table is generated.
    Signed-off-by: Frank Du <frank.du@intel.com>

--- cpp/src/arrow/util/spaced_sse_table_codegen.py
+++ cpp/src/arrow/util/spaced_sse_table_codegen.py
@@ -28,33 +28,62 @@
 # (c) Daniel Lemire
 
 # Usage: python3 spaced_sse_table_codegen.py > spaced_sse_table_generated.cc
+# We use below API to achieve the expand/compress function, and a generated
+# lookup table to avoid the slow loop over the valit bits mask.
+#
+#   __m128i _mm_shuffle_epi8 (__m128i a, __m128i b)
+#   Description
+#   Shuffle packed 8-bit integers in a according to shuffle control mask in
+#     the corresponding 8-bit element of b, and store the results in dst.
+#   Operation
+#   FOR j := 0 to 15
+#     i := j*8
+#     IF b[i+7] == 1
+#       dst[i+7:i] := 0
+#     ELSE
+#       index[3:0] := b[i+3:i]
+#       dst[i+7:i] := a[index*8+7:index*8]
+#     FI
+#   ENDFOR
+
+NullMask = 0x80  # Top significant bit of a byte mask
 
 
-def print_mask_expand_table(pack_width, mask_length):
+def print_mask_expand_table(dt_byte_width, item_byte_width):
     """
-    Generate the lookup mask table for SSE expand shuffle control mask.
-    Ex, for epi32 full table(pack_width = 4(32/8), mask_length = 16(128/8)), the available mask
-    should be in [0, 1 << 4], for mask 0b0101(each bit represent if one epi32 is valid),
-    the target mask128 lookup item is:
+    Generate the lookup table for SSE expand shuffle control mask.
+
+    Parameters
+    ----------
+    dt_byte_width : int
+        Byte width of the data type
+    item_byte_width:
+        Byte width of each shuffle control item in the table
+
+    Notes
+    -----
+    Ex, for epi32 full table, the data type is 32 bits(4 bytes), the shuffle control item in the table is 128 bits(16 bytes).
+    The available input valid bits mask of table is in [0, 1 << (item_byte_width / dt_byte_width)].
+    Then for mask 0b0101(each bit represent if each epi32 is valid), the target mask128 shuffle control lookup item is:
     [0x00, 0x01, 0x02, 0x03, 0x80, 0x80, 0x80, 0x80, 0x04, 0x05, 0x06, 0x07, 0x80, 0x80, 0x80, 0x80]
     """
-    loop = int(mask_length / pack_width)
-    null_mask = 0x80
+
+    loop = int(item_byte_width / dt_byte_width)
     for index in range(1 << loop):
         maps = []
         lastbit = 0x00
 
         for bit in range(loop):
             if (index & (1 << bit)):
-                for n in range(pack_width):
-                    maps.append(pack_width * lastbit + n)
+                for n in range(dt_byte_width):
+                    maps.append(dt_byte_width * lastbit + n)
                 lastbit += 1
             else:
-                for n in range(pack_width):
-                    maps.append(null_mask)
+                for n in range(dt_byte_width):
+                    maps.append(NullMask)
 
         out = ""
-        for item in range(mask_length):
+        for item in range(item_byte_width):
             out += " 0x" + format(maps[item], '02x') + ","
         print(" " + out)
 
@@ -80,28 +109,38 @@ def print_mask_expand_epi8():
     print("};")
 
 
-def print_mask_compress_table(pack_width, mask_length):
+def print_mask_compress_table(dt_byte_width, item_byte_width):
     """
-    Generate the lookup mask table for SSE compress shuffle control mask.
-    Ex, for epi32 full table(pack_width = 4(32/8), mask_length = 16(128/8)), the available mask
-    should be in [0, 1 << 4], for mask 0b0101(each bit represent if one epi32 is valid),
-    the target mask128 lookup item is:
+    Generate the lookup table for SSE compress shuffle control mask.
+
+    Parameters
+    ----------
+    dt_byte_width : int
+        Byte width of the data type
+    item_byte_width:
+        Byte width of each shuffle control item in the table
+
+    Notes
+    -----
+    Ex, for epi32 full table, the data type is 32 bits(4 bytes), the shuffle control item in the table is 128 bits(16 bytes).
+    The available input valid bits mask of table is in [0, 1 << (item_byte_width / dt_byte_width)].
+    Then for mask 0b0101(each bit represent if each epi32 is valid), the target mask128 shuffle control lookup item is:
     [0x00, 0x01, 0x02, 0x03, 0x08, 0x09, 0x0a, 0x0b, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80]
     """
-    loop = int(mask_length / pack_width)
-    null_mask = 0x80
+
+    loop = int(item_byte_width / dt_byte_width)
     for index in range(1 << loop):
         maps = []
 
         for bit in range(loop):
             if (index & (1 << bit)):
-                for n in range(pack_width):
-                    maps.append(pack_width * bit + n)
-        while(len(maps) < mask_length):
-            maps.append(null_mask)
+                for n in range(dt_byte_width):
+                    maps.append(dt_byte_width * bit + n)
+        while(len(maps) < item_byte_width):
+            maps.append(NullMask)
 
         out = ""
-        for item in range(mask_length):
+        for item in range(item_byte_width):
             out += " 0x" + format(maps[item], '02x') + ","
         print(" " + out)
 
@@ -129,14 +168,17 @@ def print_mask_compress_epi8():
 
 def print_epi8_thin_compact():
     """
-    Generate the lookup mask table for SSE epi8 thin compact shuffle control mask.
-    The available mask should be in [0, 9], for mask 5, the target mask128 lookup item is 5 byte from
-    low part, 8 byte from high part, remaining set to null(0x80).
+    Generate the lookup table for SSE epi8 thin compact shuffle control mask.
+
+    Notes
+    -----
+    The available input number of table is in [0, 9], for input 5, the target mask128 lookup item is
+        5 byte from low part, 8 byte from high part, remaining set to null(0x80).
     [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x80, 0x80]
     """
+
     print("extern const uint8_t kMask128SseEpi8ThinCompact[] = {")
 
-    null_mask = 0x80
     for i in range(9):
         maps = []
         for j in range(i):
@@ -144,7 +186,7 @@ def print_epi8_thin_compact():
         for j in range(8):
             maps.append(j + 8)
         while(len(maps) < 16):
-            maps.append(null_mask)
+            maps.append(NullMask)
         out = ""
         for s in range(16):
             out += " 0x" + format(maps[s], '02x') + ","
