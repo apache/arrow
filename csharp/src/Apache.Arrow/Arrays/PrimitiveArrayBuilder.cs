d53cb102de43d387d6e0e52a3ad2310e4b9a3939
Eric Erhardt <eric.erhardt@microsoft.com>
2020-05-21 17:26:07 -0500

    Update code to reflect editorconfig settings.

--- csharp/src/Apache.Arrow/Arrays/PrimitiveArrayBuilder.cs
+++ csharp/src/Apache.Arrow/Arrays/PrimitiveArrayBuilder.cs
@@ -46,7 +46,7 @@ namespace Apache.Arrow
         public TBuilder Append(ReadOnlySpan<TFrom> span)
         {
             ArrayBuilder.Reserve(span.Length);
-            foreach (var value in span)
+            foreach (TFrom value in span)
             {
                 Append(value);
             }
@@ -139,7 +139,7 @@ namespace Apache.Arrow
 
         public TBuilder Append(ReadOnlySpan<T> span)
         {
-            var len = ValueBuffer.Length;
+            int len = ValueBuffer.Length;
             ValueBuffer.Append(span);
             ValidityBuffer.AppendRange(Enumerable.Repeat(true, ValueBuffer.Length - len));
             return Instance;
@@ -147,7 +147,7 @@ namespace Apache.Arrow
 
         public TBuilder AppendRange(IEnumerable<T> values)
         {
-            var len = ValueBuffer.Length;
+            int len = ValueBuffer.Length;
             ValueBuffer.AppendRange(values);
             ValidityBuffer.AppendRange(Enumerable.Repeat(true, ValueBuffer.Length - len));
             return Instance;
@@ -177,7 +177,7 @@ namespace Apache.Arrow
 
         public TBuilder Swap(int i, int j)
         {
-            var x = ValueBuffer.Span[i];
+            T x = ValueBuffer.Span[i];
             ValueBuffer.Span[i] = ValueBuffer.Span[j];
             ValueBuffer.Span[j] = x;
             ValidityBuffer.Swap(i, j);
@@ -186,7 +186,7 @@ namespace Apache.Arrow
 
         public TArray Build(MemoryAllocator allocator = default)
         {
-            var validityBuffer = NullCount > 0
+            ArrowBuffer validityBuffer = NullCount > 0
                                     ? ValidityBuffer.Build(allocator).ValueBuffer
                                     : ArrowBuffer.Empty;
 
