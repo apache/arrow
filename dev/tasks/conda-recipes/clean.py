164aac2e582b84bff6f7c2e06ba3e4a23325aac0
Uwe L. Korn <uwe.korn@quantco.com>
2020-05-30 12:22:59 +0200

    ARROW-8941: [C++/Python] Add cleanup script for arrow-nightlies conda repository

---
+++ dev/tasks/conda-recipes/clean.py
@@ -0,0 +1,66 @@
+from distutils.version import LooseVersion
+from subprocess import check_output, check_call
+from typing import List
+
+import json
+import os
+import pandas as pd
+import sys
+
+
+VERSIONS_TO_KEEP = 5
+PLATFORMS = ["linux-64", "osx-64", "win-64"]
+PACKAGES = ["pyarrow", "arrow-cpp"]
+
+
+def packages_to_delete(package_name: str, platform: str) -> List[str]:
+    env = os.environ.copy()
+    env["CONDA_SUBDIR"] = platform
+    pkgs_json = check_output(
+        [
+            "mamba",
+            "repoquery",
+            "--json",
+            "-c",
+            "arrow-nightlies",
+            "--override-channels",
+            "search",
+            package_name,
+        ],
+        env=env,
+    )
+    pkgs = pd.DataFrame(json.loads(pkgs_json)["result"]["pkgs"])
+    pkgs["version"] = pkgs["version"].map(LooseVersion)
+    pkgs["py_version"] = pkgs["build_string"].str.slice(0, 4)
+
+    to_delete = []
+
+    for (subdir, python), group in pkgs.groupby(["subdir", "py_version"]):
+        # https://github.com/QuantStack/mamba/issues/304
+        if not subdir:
+            continue
+        group = group.sort_values(by="version", ascending=False)
+
+        if len(group) > VERSIONS_TO_KEEP:
+            del_candidates = group[VERSIONS_TO_KEEP:]
+            to_delete += (
+                f"arrow-nightlies/{package_name}/"
+                + del_candidates["version"].astype(str)
+                + del_candidates["url"].str.replace(
+                    "https://conda.anaconda.org/arrow-nightlies", ""
+                )
+            ).to_list()
+
+    return to_delete
+
+
+if __name__ == "__main__":
+    to_delete = []
+    for platform in PLATFORMS:
+        for package in PACKAGES:
+            to_delete += packages_to_delete(package, platform)
+
+    for name in to_delete:
+        print(f"Deleting {name} …")
+        if "FORCE" in sys.argv:
+            check_call(["anaconda", "remove", name])
