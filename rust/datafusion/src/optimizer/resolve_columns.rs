cf4b85ff168c971ac18cad74fde9be8128a04bb7
Qingping Hou <dave2008713@gmail.com>
2020-06-07 00:11:23 -0700

    fix: optimize sort column in resolve_column optimizer

--- rust/datafusion/src/optimizer/resolve_columns.rs
+++ rust/datafusion/src/optimizer/resolve_columns.rs
@@ -55,13 +55,16 @@ impl OptimizerRule for ResolveColumnsRule {
                     rewrite_expr_list(aggr_expr, &input.schema())?,
                 )?
                 .build()?),
-            LogicalPlan::Sort { input, expr, .. } => Ok(LogicalPlanBuilder::from(input)
-                .sort(rewrite_expr_list(expr, &input.schema())?)?
-                .build()?),
+            LogicalPlan::Sort { input, expr, .. } => {
+                Ok(LogicalPlanBuilder::from(&self.optimize(input)?)
+                    .sort(rewrite_expr_list(expr, &input.schema())?)?
+                    .build()?)
+            }
             _ => Ok(plan.clone()),
         }
     }
 }
+
 fn rewrite_expr_list(expr: &[Expr], schema: &Schema) -> Result<Vec<Expr>> {
     Ok(expr
         .iter()
