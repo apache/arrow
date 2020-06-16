c4dd470cfed4d8bf8aebae1a283875fd476cd3ba
Wes McKinney <wesm@apache.org>
2020-06-16 17:03:16 -0500

    Handle shifting offsets for ascii batch string transforms

--- cpp/src/arrow/compute/kernels/scalar_string_test.cc
+++ cpp/src/arrow/compute/kernels/scalar_string_test.cc
@@ -56,12 +56,14 @@ TYPED_TEST(TestStringKernels, AsciiLength) {
                    "[3, null, 0, 1]");
 }
 
-TYPED_TEST(TestStringKernels, DISABLED_AsciiUpper) {
+TYPED_TEST(TestStringKernels, AsciiUpper) {
+  this->CheckUnary("ascii_upper", "[]", this->string_type(), "[]");
   this->CheckUnary("ascii_upper", "[\"aAazZæÆ&\", null, \"\", \"b\"]",
                    this->string_type(), "[\"AAAZZæÆ&\", null, \"\", \"B\"]");
 }
 
-TYPED_TEST(TestStringKernels, DISABLED_AsciiLower) {
+TYPED_TEST(TestStringKernels, AsciiLower) {
+  this->CheckUnary("ascii_lower", "[]", this->string_type(), "[]");
   this->CheckUnary("ascii_lower", "[\"aAazZæÆ&\", null, \"\", \"b\"]",
                    this->string_type(), "[\"aaazzæÆ&\", null, \"\", \"b\"]");
 }
