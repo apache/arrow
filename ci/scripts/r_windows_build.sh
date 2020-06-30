092207ad5376ad12f3d72ec9e314546efc7a6de2
Neal Richardson <neal.p.richardson@gmail.com>
2020-06-30 12:26:35 -0700

    ARROW-9281: [R] Turn off utf8proc in R builds
    We don't have bindings for anything that would use it yet, so don't bother building it.
    
    Closes #7595 from nealrichardson/no-utf8proc
    
    Authored-by: Neal Richardson <neal.p.richardson@gmail.com>
    Signed-off-by: Neal Richardson <neal.p.richardson@gmail.com>

--- ci/scripts/r_windows_build.sh
+++ ci/scripts/r_windows_build.sh
@@ -81,8 +81,8 @@ cp $MSYS_LIB_DIR/mingw64/lib/lib{thrift,snappy}.a $DST_DIR/${RWINLIB_LIB_DIR}/x6
 cp $MSYS_LIB_DIR/mingw32/lib/lib{thrift,snappy}.a $DST_DIR/${RWINLIB_LIB_DIR}/i386
 
 # These are from https://dl.bintray.com/rtools/mingw{32,64}/
-cp $MSYS_LIB_DIR/mingw64/lib/lib{zstd,lz4,crypto,utf8proc}.a $DST_DIR/lib/x64
-cp $MSYS_LIB_DIR/mingw32/lib/lib{zstd,lz4,crypto,utf8proc}.a $DST_DIR/lib/i386
+cp $MSYS_LIB_DIR/mingw64/lib/lib{zstd,lz4,crypto}.a $DST_DIR/lib/x64
+cp $MSYS_LIB_DIR/mingw32/lib/lib{zstd,lz4,crypto}.a $DST_DIR/lib/i386
 
 # Create build artifact
 zip -r ${DST_DIR}.zip $DST_DIR
