6074a07b257ea686db4811778ef5d805ed3405fa
Projjal Chanda <iam@pchanda.com>
2020-07-08 14:15:14 +0530

    ARROW-9343: [C++][Gandiva] CastInt/Float from string functions should handle leading/trailing white spaces
    Also refactored the code to remove the parse_float helper function from context. This was done earlier to save the constructor cost when the stringconverter object from arrow/util/parsing was needed to be constructed first. Now that it has changed to parametric functions, its not needed to keep them in the executioncontext.
    
    Closes #7653 from projjal/parsefloat and squashes the following commits:
    
    073061d3a <Projjal Chanda> added the string to error message
    2b089dd95 <Projjal Chanda> trim strings before parsing float/int
    14de7e61d <Projjal Chanda> refactored gandiva parse float code
    
    Authored-by: Projjal Chanda <iam@pchanda.com>
    Signed-off-by: Praveen <praveen@dremio.com>

--- cpp/src/gandiva/precompiled/string_ops.cc
+++ cpp/src/gandiva/precompiled/string_ops.cc
@@ -17,6 +17,8 @@
 
 // String functions
 
+#include "arrow/util/value_parsing.h"
+
 extern "C" {
 
 #include <limits.h>
@@ -671,5 +673,29 @@ const char* replace_utf8_utf8_utf8(gdv_int64 context, const char* text,
                                              from_str_len, to_str, to_str_len, 65535,
                                              out_len);
 }
+
+#define CAST_NUMERIC_FROM_STRING(OUT_TYPE, ARROW_TYPE, TYPE_NAME)                       \
+  FORCE_INLINE                                                                          \
+  gdv_##OUT_TYPE cast##TYPE_NAME##_utf8(int64_t context, const char* data,              \
+                                        int32_t len) {                                  \
+    gdv_##OUT_TYPE val = 0;                                                             \
+    int32_t trimmed_len;                                                                \
+    data = trim_utf8(context, data, len, &trimmed_len);                                 \
+    if (!arrow::internal::StringConverter<ARROW_TYPE>::Convert(data, trimmed_len,       \
+                                                               &val)) {                 \
+      std::string err = "Failed to cast the string " + std::string(data, trimmed_len) + \
+                        " to " #OUT_TYPE;                                               \
+      gdv_fn_context_set_error_msg(context, err.c_str());                               \
+    }                                                                                   \
+    return val;                                                                         \
+  }
+
+CAST_NUMERIC_FROM_STRING(int32, arrow::Int32Type, INT)
+CAST_NUMERIC_FROM_STRING(int64, arrow::Int64Type, BIGINT)
+CAST_NUMERIC_FROM_STRING(float32, arrow::FloatType, FLOAT4)
+CAST_NUMERIC_FROM_STRING(float64, arrow::DoubleType, FLOAT8)
+
+#undef CAST_INT_FROM_STRING
+#undef CAST_FLOAT_FROM_STRING
 
 }  // extern "C"
