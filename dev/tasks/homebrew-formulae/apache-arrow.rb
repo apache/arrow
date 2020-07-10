1dca8416883c6939c149bb71a71bab34406e112d
Neal Richardson <neal.p.richardson@gmail.com>
2020-07-10 13:31:30 -0700

    Pin python version in homebrew formula to pass brew audit

--- dev/tasks/homebrew-formulae/apache-arrow.rb
+++ dev/tasks/homebrew-formulae/apache-arrow.rb
@@ -15,7 +15,7 @@ class ApacheArrow < Formula
   depends_on "numpy"
   depends_on "openssl@1.1"
   depends_on "protobuf"
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "rapidjson"
   depends_on "snappy"
   depends_on "thrift"
@@ -38,7 +38,7 @@ class ApacheArrow < Formula
       -DARROW_WITH_SNAPPY=ON
       -DARROW_WITH_BROTLI=ON
       -DARROW_INSTALL_NAME_RPATH=OFF
-      -DPYTHON_EXECUTABLE=#{Formula["python"].bin/"python3"}
+      -DPYTHON_EXECUTABLE=#{Formula["python@3.8"].bin/"python3"}
     ]
     # Re-enable -DARROW_S3=ON and add back aws-sdk-cpp to depends_on in ARROW-6437
 
