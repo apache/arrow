04f80607d70cd4a7c892512a14a76e145de50486
Neal Richardson <neal.p.richardson@gmail.com>
2020-07-10 13:36:28 -0700

    Add license

--- dev/tasks/homebrew-formulae/apache-arrow.rb
+++ dev/tasks/homebrew-formulae/apache-arrow.rb
@@ -3,6 +3,7 @@ class ApacheArrow < Formula
   homepage "https://arrow.apache.org/"
   url "https://www.apache.org/dyn/closer.lua?path=arrow/arrow-1.0.0-SNAPSHOT/apache-arrow-1.0.0-SNAPSHOT.tar.gz"
   sha256 "9948ddb6d4798b51552d0dca3252dd6e3a7d0f9702714fc6f5a1b59397ce1d28"
+  license "Apache-2.0"
   head "https://github.com/apache/arrow.git"
 
   depends_on "boost" => :build
