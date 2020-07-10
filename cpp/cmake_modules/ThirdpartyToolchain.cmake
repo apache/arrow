f43ce4606996ed44980dba514c824786e70729d3
Wes McKinney <wesm@apache.org>
2020-07-10 16:02:10 -0500

    cmake-format

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -2416,29 +2416,34 @@ macro(build_grpc)
   # continuation character in these scripts, so we have to create a copy of the
   # static lib that we will bundle later
 
-  set(GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR
-    "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}grpcpp${CMAKE_STATIC_LIBRARY_SUFFIX}")
+  set(
+    GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR
+    "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}grpcpp${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
   add_custom_command(OUTPUT ${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}
-    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:gRPC::grpc++>
-    ${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}
-    DEPENDS grpc_ep)
+                     COMMAND ${CMAKE_COMMAND}
+                             -E
+                             copy
+                             $<TARGET_FILE:gRPC::grpc++>
+                             ${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}
+                     DEPENDS grpc_ep)
   add_library(gRPC::grpcpp_for_bundling STATIC IMPORTED)
-  set_target_properties(
-    gRPC::grpcpp_for_bundling
-    PROPERTIES IMPORTED_LOCATION
-               "${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}")
+  set_target_properties(gRPC::grpcpp_for_bundling
+                        PROPERTIES IMPORTED_LOCATION
+                                   "${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}")
 
-  set_source_files_properties("${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}" PROPERTIES GENERATED TRUE)
+  set_source_files_properties("${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}" PROPERTIES GENERATED
+                              TRUE)
   add_custom_target(grpc_copy_grpc++ ALL DEPENDS "${GRPC_STATIC_LIBRARY_GRPCPP_FOR_AR}")
   add_dependencies(gRPC::grpcpp_for_bundling grpc_copy_grpc++)
 
   list(APPEND ARROW_BUNDLED_STATIC_LIBS
-    ${ABSL_LIBRARIES}
-    gRPC::upb
-    gRPC::gpr
-    gRPC::grpc
-    gRPC::address_sorting
-    gRPC::grpcpp_for_bundling)
+              ${ABSL_LIBRARIES}
+              gRPC::upb
+              gRPC::gpr
+              gRPC::grpc
+              gRPC::address_sorting
+              gRPC::grpcpp_for_bundling)
 endmacro()
 
 if(ARROW_WITH_GRPC)
