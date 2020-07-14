6128fa423d5b2dcd8c5d4f3eac129fc4af0520e1
Adam Szmigin <adam.szmigin@jetstoneam.com>
2020-07-14 09:07:46 -0500

    ARROW-8344: [C#] Bug-fixes to binary array plus other improvements
    This PR fixes a few bugs in `BinaryArray.Builder()`:
    * Fixes the `Clear()` method, which previously would break all subsequently-appended values (see JIRA ticket for examples).
    * Makes the `Build()` method idempotent, by making sure the builder's internal arrays are not modified when it is called.
    
    And makes a few other improvements:
    * Comprehensive test coverage of the builder's key methods (this was written in proper TDD-style, so these tests would fail if run on `master`).
    * XML docstrings for all methods affected.
    
    Closes #7671 from mr-smidge/arrow-8344/string-array-builder-clear-bugfix
    
    Authored-by: Adam Szmigin <adam.szmigin@jetstoneam.com>
    Signed-off-by: Eric Erhardt <eric.erhardt@microsoft.com>

--- csharp/src/Apache.Arrow/Arrays/BinaryArray.cs
+++ csharp/src/Apache.Arrow/Arrays/BinaryArray.cs
@@ -66,87 +66,175 @@ namespace Apache.Arrow
                 ValueOffsets = new ArrowBuffer.Builder<int>();
                 ValueBuffer = new ArrowBuffer.Builder<byte>();
                 ValidityBuffer = new ArrowBuffer.BitmapBuilder();
+
+                // From the docs:
+                //
+                // The offsets buffer contains length + 1 signed integers (either 32-bit or 64-bit, depending on the
+                // logical type), which encode the start position of each slot in the data buffer. The length of the
+                // value in each slot is computed using the difference between the offset at that slot’s index and the
+                // subsequent offset.
+                //
+                // In this builder, we choose to append the first offset (zero) upon construction, and each trailing
+                // offset is then added after each individual item has been appended.
+                ValueOffsets.Append(this.Offset);
             }
 
             protected abstract TArray Build(ArrayData data);
 
-            public int Length => ValueOffsets.Length;
+            /// <summary>
+            /// Gets the length of the array built so far.
+            /// </summary>
+            public int Length => ValueOffsets.Length - 1;
 
+            /// <summary>
+            /// Build an Arrow array from the appended contents so far.
+            /// </summary>
+            /// <param name="allocator">Optional memory allocator.</param>
+            /// <returns>Returns an array of type <typeparamref name="TArray"/>.</returns>
             public TArray Build(MemoryAllocator allocator = default)
             {
-                ValueOffsets.Append(Offset);
-
-                ArrowBuffer validityBuffer = NullCount > 0
-                                        ? ValidityBuffer.Build(allocator)
-                                        : ArrowBuffer.Empty;
-
-                var data = new ArrayData(DataType, ValueOffsets.Length - 1, NullCount, 0,
-                    new[] { validityBuffer, ValueOffsets.Build(allocator), ValueBuffer.Build(allocator) });
+                var bufs = new[]
+                {
+                    NullCount > 0 ? ValidityBuffer.Build(allocator) : ArrowBuffer.Empty,
+                    ValueOffsets.Build(allocator),
+                    ValueBuffer.Build(allocator),
+                };
+                var data = new ArrayData(
+                    DataType,
+                    length: Length,
+                    NullCount,
+                    offset: 0,
+                    bufs);
 
                 return Build(data);
             }
 
+            /// <summary>
+            /// Append a single null value to the array.
+            /// </summary>
+            /// <returns>Returns the builder (for fluent-style composition).</returns>
             public TBuilder AppendNull()
             {
-                ValueOffsets.Append(Offset);
+                // Do not add to the value buffer in the case of a null.
+                // Note that we do not need to increment the offset as a result.
                 ValidityBuffer.Append(false);
+                ValueOffsets.Append(Offset);
                 return Instance;
             }
 
+            /// <summary>
+            /// Appends a value, consisting of a single byte, to the array.
+            /// </summary>
+            /// <param name="value">Byte value to append.</param>
+            /// <returns>Returns the builder (for fluent-style composition).</returns>
             public TBuilder Append(byte value)
             {
-                ValueOffsets.Append(Offset);
                 ValueBuffer.Append(value);
-                Offset++;
                 ValidityBuffer.Append(true);
+                Offset++;
+                ValueOffsets.Append(Offset);
                 return Instance;
             }
 
+            /// <summary>
+            /// Append a value, consisting of a span of bytes, to the array.
+            /// </summary>
+            /// <remarks>
+            /// Note that a single value is added, which consists of arbitrarily many bytes.  If multiple values are
+            /// to be added, use the <see cref="AppendRange"/> method.
+            /// </remarks>
+            /// <param name="span">Span of bytes to add.</param>
+            /// <returns>Returns the builder (for fluent-style composition).</returns>
             public TBuilder Append(ReadOnlySpan<byte> span)
             {
-                ValueOffsets.Append(Offset);
                 ValueBuffer.Append(span);
                 ValidityBuffer.Append(true);
                 Offset += span.Length;
+                ValueOffsets.Append(Offset);
                 return Instance;
             }
 
+            /// <summary>
+            /// Append a value, consisting of an enumerable collection of bytes, to the array.
+            /// </summary>
+            /// <remarks>
+            /// Note that this method appends a single value, which may consist of arbitrarily many bytes.  If multiple
+            /// values are to be added, use the <see cref="AppendRange(IEnumerable{byte})"/> method instead.
+            /// </remarks>
+            /// <param name="value">Enumerable collection of bytes to add.</param>
+            /// <returns>Returns the builder (for fluent-style composition).</returns>
+            public TBuilder Append(IEnumerable<byte> value)
+            {
+                if (value == null)
+                {
+                    return AppendNull();
+                }
+
+                // Note: by looking at the length of the value buffer before and after, we avoid having to iterate
+                // through the enumerable multiple times to get both length and contents.
+                int priorLength = ValueBuffer.Length;
+                ValueBuffer.AppendRange(value);
+                int valueLength = ValueBuffer.Length - priorLength;
+                Offset += valueLength;
+                ValidityBuffer.Append(true);
+                ValueOffsets.Append(Offset);
+                return Instance;
+            }
+
+            /// <summary>
+            /// Append an enumerable collection of single-byte values to the array.
+            /// </summary>
+            /// <remarks>
+            /// Note that this method appends multiple values, each of which is a single byte.  If a single value is
+            /// to be added, use the <see cref="Append(IEnumerable{byte})"/> method instead.
+            /// </remarks>
+            /// <param name="values">Single-byte values to add.</param>
+            /// <returns>Returns the builder (for fluent-style composition).</returns>
+            public TBuilder AppendRange(IEnumerable<byte> values)
+            {
+                if (values == null)
+                {
+                    throw new ArgumentNullException(nameof(values));
+                }
+
+                foreach (byte b in values)
+                {
+                    Append(b);
+                }
+
+                return Instance;
+            }
+
+            /// <summary>
+            /// Append an enumerable collection of values to the array.
+            /// </summary>
+            /// <param name="values">Values to add.</param>
+            /// <returns>Returns the builder (for fluent-style composition).</returns>
             public TBuilder AppendRange(IEnumerable<byte[]> values)
             {
+                if (values == null)
+                {
+                    throw new ArgumentNullException(nameof(values));
+                }
+
                 foreach (byte[] arr in values)
                 {
                     if (arr == null)
                     {
                         AppendNull();
-                        continue;
                     }
-                    int len = ValueBuffer.Length;
-                    ValueOffsets.Append(Offset);
-                    ValueBuffer.Append(arr);
-                    ValidityBuffer.Append(true);
-                    Offset += ValueBuffer.Length - len;
+                    else
+                    {
+                        Append((ReadOnlySpan<byte>)arr);
+                    }
                 }
 
-                return Instance;
-            }
-
-            public TBuilder AppendRange(IEnumerable<byte> values)
-            {
-                if (values == null)
-                {
-                    return AppendNull();
-                }
-                int len = ValueBuffer.Length;
-                ValueBuffer.AppendRange(values);
-                int valOffset = ValueBuffer.Length - len;
-                ValueOffsets.Append(Offset);
-                Offset += valOffset;
-                ValidityBuffer.Append(true);
                 return Instance;
             }
 
             public TBuilder Reserve(int capacity)
             {
+                // TODO: [ARROW-9366] Reserve capacity in the value buffer in a more sensible way.
                 ValueOffsets.Reserve(capacity + 1);
                 ValueBuffer.Reserve(capacity);
                 ValidityBuffer.Reserve(capacity + 1);
@@ -155,6 +243,7 @@ namespace Apache.Arrow
 
             public TBuilder Resize(int length)
             {
+                // TODO: [ARROW-9366] Resize the value buffer to a safe length based on offsets, not `length`.
                 ValueOffsets.Resize(length + 1);
                 ValueBuffer.Resize(length);
                 ValidityBuffer.Resize(length + 1);
@@ -173,11 +262,19 @@ namespace Apache.Arrow
                 throw new NotImplementedException();
             }
 
+            /// <summary>
+            /// Clear all contents appended so far.
+            /// </summary>
+            /// <returns>Returns the builder (for fluent-style composition).</returns>
             public TBuilder Clear()
             {
                 ValueOffsets.Clear();
                 ValueBuffer.Clear();
                 ValidityBuffer.Clear();
+
+                // Always write the first offset before anything has been written.
+                Offset = 0;
+                ValueOffsets.Append(Offset);
                 return Instance;
             }
         }
@@ -228,6 +325,18 @@ namespace Apache.Arrow
             return offsets[index + 1] - offsets[index];
         }
 
+        /// <summary>
+        /// Get the collection of bytes, as a read-only span, at a given index in the array.
+        /// </summary>
+        /// <remarks>
+        /// Note that this method cannot reliably identify null values, which are indistinguishable from empty byte
+        /// collection values when seen in the context of this method's return type of <see cref="ReadOnlySpan{Byte}"/>.
+        /// Use the <see cref="Array.IsNull"/> method instead to reliably determine null values.
+        /// </remarks>
+        /// <param name="index">Index at which to get bytes.</param>
+        /// <returns>Returns a <see cref="ReadOnlySpan{Byte}"/> object.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If the index is negative or beyond the length of the array.
+        /// </exception>
         public ReadOnlySpan<byte> GetBytes(int index)
         {
             if (index < 0 || index >= Length)
@@ -237,7 +346,9 @@ namespace Apache.Arrow
 
             if (IsNull(index))
             {
-                return null;
+                // Note that `return null;` is valid syntax, but would be misleading as `null` in the context of a span
+                // is actually returned as an empty span.
+                return ReadOnlySpan<byte>.Empty;
             }
 
             return ValueBuffer.Span.Slice(ValueOffsets[index], GetValueLength(index));
