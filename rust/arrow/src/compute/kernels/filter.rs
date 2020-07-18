a5bf99484bd32fb815f5c2cf7a9402798a4e92fc
Yordan Pavlov <yordan.pavlov@outlook.com>
2020-07-18 22:08:03 +0100

    fix rustfmt issues

--- rust/arrow/src/compute/kernels/filter.rs
+++ rust/arrow/src/compute/kernels/filter.rs
@@ -17,13 +17,17 @@
 
 //! Defines miscellaneous array kernels.
 
-use std::{mem, sync::Arc};
-use std::io::Write;
 use crate::array::*;
-use crate::datatypes::{DataType, ArrowNumericType, TimeUnit};
+use crate::datatypes::{ArrowNumericType, DataType, TimeUnit};
 use crate::error::{ArrowError, Result};
-use crate::{buffer::{MutableBuffer, Buffer}, bitmap::Bitmap, util::bit_util};
 use crate::record_batch::RecordBatch;
+use crate::{
+    bitmap::Bitmap,
+    buffer::{Buffer, MutableBuffer},
+    util::bit_util,
+};
+use std::io::Write;
+use std::{mem, sync::Arc};
 
 /// trait for copying filtered null bitmap bits
 trait CopyNullBit {
@@ -32,7 +36,7 @@ trait CopyNullBit {
     fn null_buffer(&mut self) -> Buffer;
 }
 
-/// no-op null bitmap copy implementation, 
+/// no-op null bitmap copy implementation,
 /// used when the filtered data array doesn't have a null bitmap
 struct NullBitNoop {}
 
@@ -48,31 +52,35 @@ impl CopyNullBit for NullBitNoop {
         // do nothing
     }
 
-    fn null_count(&self) -> usize { 0 }
+    fn null_count(&self) -> usize {
+        0
+    }
 
-    fn null_buffer(&mut self) -> Buffer { Buffer::from([0u8; 0]) }
+    fn null_buffer(&mut self) -> Buffer {
+        Buffer::from([0u8; 0])
+    }
 }
 
-/// null bitmap copy implementation, 
+/// null bitmap copy implementation,
 /// used when the filtered data array has a null bitmap
 struct NullBitSetter<'a> {
     target_buffer: MutableBuffer,
     source_bytes: &'a [u8],
     target_index: usize,
-    null_count: usize
+    null_count: usize,
 }
 
 impl<'a> NullBitSetter<'a> {
     fn new(null_bitmap: &'a Bitmap) -> Self {
         let null_bytes = null_bitmap.buffer_ref().data();
         // create null bitmap buffer with same length and initialize null bitmap buffer to 1s
-        let null_buffer = MutableBuffer::new(null_bytes.len())
-            .with_bitset(null_bytes.len(), true);
+        let null_buffer =
+            MutableBuffer::new(null_bytes.len()).with_bitset(null_bytes.len(), true);
         NullBitSetter {
             source_bytes: null_bytes,
             target_buffer: null_buffer,
             target_index: 0,
-            null_count: 0
+            null_count: 0,
         }
     }
 }
@@ -82,13 +90,20 @@ impl<'a> CopyNullBit for NullBitSetter<'a> {
     fn copy_null_bit(&mut self, source_index: usize) {
         if bit_util::get_bit(self.source_bytes, source_index) == false {
             // this is not actually unsafe because of the condition above + target_buffer.len() == source_bytes.len()
-            unsafe { bit_util::unset_bit_raw(self.target_buffer.raw_data_mut(), self.target_index); }
+            unsafe {
+                bit_util::unset_bit_raw(
+                    self.target_buffer.raw_data_mut(),
+                    self.target_index,
+                );
+            }
             self.null_count += 1;
         }
         self.target_index += 1;
     }
 
-    fn null_count(&self) -> usize { self.null_count }
+    fn null_count(&self) -> usize {
+        self.null_count
+    }
 
     fn null_buffer(&mut self) -> Buffer {
         self.target_buffer.resize(self.target_index).unwrap();
@@ -98,12 +113,11 @@ impl<'a> CopyNullBit for NullBitSetter<'a> {
     }
 }
 
-fn get_null_bit_setter<'a>(data_array: &'a impl Array ) -> Box<CopyNullBit + 'a> {
+fn get_null_bit_setter<'a>(data_array: &'a impl Array) -> Box<CopyNullBit + 'a> {
     if let Some(null_bitmap) = data_array.data_ref().null_bitmap() {
         // only return an actual null bit copy implementation if null_bitmap is set
         Box::new(NullBitSetter::new(null_bitmap))
-    }
-    else {
+    } else {
         // otherwise return a no-op copy null bit implementation
         // for improved performance when the filtered array doesn't contain NULLs
         Box::new(NullBitNoop::new())
@@ -112,10 +126,14 @@ fn get_null_bit_setter<'a>(data_array: &'a impl Array ) -> Box<CopyNullBit + 'a>
 
 // transmute filter array to u64
 // - optimize filtering using highly selective filters by skipping entire batches of 64 filter bits
-// - copy null bit immediately 
+// - copy null bit immediately
 // - copy data values in batches
-fn filter_array_impl(filter_context: &FilterContext, data_array: &impl Array, array_type: DataType, value_size: usize) -> Result<ArrayDataBuilder> 
-{
+fn filter_array_impl(
+    filter_context: &FilterContext,
+    data_array: &impl Array,
+    array_type: DataType,
+    value_size: usize,
+) -> Result<ArrayDataBuilder> {
     let filtered_count = filter_context.filtered_count;
     let filter_mask = &filter_context.filter_mask;
     let filter_u64 = &filter_context.filter_u64;
@@ -124,7 +142,7 @@ fn filter_array_impl(filter_context: &FilterContext, data_array: &impl Array, ar
     let mut null_bit_setter = get_null_bit_setter(data_array);
     let null_bit_setter = null_bit_setter.as_mut();
     let mut copy_buffer: Vec<u8> = vec![0; 64 * value_size];
-    
+
     for i in 0..filter_u64.len() {
         // foreach u64 batch
         let filter_batch = filter_u64[i];
@@ -142,15 +160,19 @@ fn filter_array_impl(filter_context: &FilterContext, data_array: &impl Array, ar
                 // if filter bit == 1: copy data value to temp array
                 // let copy_buffer_start = value_size * copy_count;
                 // let data_buffer_start = value_size * data_index;
-                copy_buffer[(value_size * copy_count)..((value_size * copy_count) + value_size)]
-                    .copy_from_slice(&data_bytes[(value_size * data_index)..((value_size * data_index) + value_size)]);
+                copy_buffer
+                    [(value_size * copy_count)..((value_size * copy_count) + value_size)]
+                    .copy_from_slice(
+                        &data_bytes[(value_size * data_index)
+                            ..((value_size * data_index) + value_size)],
+                    );
                 copy_count += 1;
             }
         }
         // copy temp array to output buffer
         value_buffer.write(&copy_buffer[0..(value_size * copy_count)])?;
     }
-    
+
     let mut array_data_builder = ArrayDataBuilder::new(array_type)
         .len(filtered_count)
         .add_buffer(value_buffer.freeze());
@@ -163,13 +185,13 @@ fn filter_array_impl(filter_context: &FilterContext, data_array: &impl Array, ar
     Ok(array_data_builder)
 }
 
-/// FilterContext can be used to improve performance when 
+/// FilterContext can be used to improve performance when
 /// filtering multiple data arrays with the same filter array.
 #[derive(Debug)]
 pub struct FilterContext {
     filter_u64: Vec<u64>,
     filtered_count: usize,
-    filter_mask: Vec<u64>
+    filter_mask: Vec<u64>,
 }
 
 macro_rules! filter_primitive_array {
@@ -196,12 +218,14 @@ impl FilterContext {
         let filtered_count = bit_util::count_set_bits(filter_bytes);
         // transmute filter_bytes to &[u64]
         let mut u64_buffer = MutableBuffer::new(filter_bytes.len());
-        u64_buffer.write_bytes(filter_bytes, u64_buffer.capacity() - filter_bytes.len()).unwrap();
+        u64_buffer
+            .write_bytes(filter_bytes, u64_buffer.capacity() - filter_bytes.len())
+            .unwrap();
         let filter_u64 = u64_buffer.typed_data_mut::<u64>().to_owned();
         FilterContext {
             filter_u64,
             filtered_count,
-            filter_mask
+            filter_mask,
         }
     }
 
@@ -347,30 +371,39 @@ impl FilterContext {
         }
     }
 
-    /// Returns a new PrimitiveArray<T> containing only those values from the array passed as the data_array parameter, 
+    /// Returns a new PrimitiveArray<T> containing only those values from the array passed as the data_array parameter,
     /// selected by the BooleanArray passed as the filter_array parameter
-    pub fn filter_primitive_array<T>(&self, data_array: &PrimitiveArray<T>) -> Result<PrimitiveArray<T>> 
+    pub fn filter_primitive_array<T>(
+        &self,
+        data_array: &PrimitiveArray<T>,
+    ) -> Result<PrimitiveArray<T>>
     where
-        T: ArrowNumericType
+        T: ArrowNumericType,
     {
         let array_type = T::get_data_type();
         let value_size = mem::size_of::<T::Native>();
-        let array_data_builder = filter_array_impl(self, data_array, array_type, value_size)?;
+        let array_data_builder =
+            filter_array_impl(self, data_array, array_type, value_size)?;
         let data = array_data_builder.build();
         Ok(PrimitiveArray::<T>::from(data))
     }
 
-    /// Returns a new DictionaryArray<T> containing only those keys from the array passed as the data_array parameter, 
+    /// Returns a new DictionaryArray<T> containing only those keys from the array passed as the data_array parameter,
     /// selected by the BooleanArray passed as the filter_array parameter. The values are cloned from the data_array.
-    pub fn filter_dictionary_array<T>(&self, data_array: &DictionaryArray<T>) -> Result<DictionaryArray<T>> 
+    pub fn filter_dictionary_array<T>(
+        &self,
+        data_array: &DictionaryArray<T>,
+    ) -> Result<DictionaryArray<T>>
     where
-        T: ArrowNumericType
+        T: ArrowNumericType,
     {
         let array_type = data_array.data_type().clone();
         let value_size = mem::size_of::<T::Native>();
-        let mut array_data_builder = filter_array_impl(self, data_array, array_type, value_size)?;
+        let mut array_data_builder =
+            filter_array_impl(self, data_array, array_type, value_size)?;
         // copy dictionary values from input array
-        array_data_builder = array_data_builder.add_child_data(data_array.values().data());
+        array_data_builder =
+            array_data_builder.add_child_data(data_array.values().data());
         let data = array_data_builder.build();
         Ok(DictionaryArray::<T>::from(data))
     }
@@ -381,30 +414,42 @@ pub fn filter(array: &Array, filter: &BooleanArray) -> Result<ArrayRef> {
     FilterContext::new(filter).filter(array)
 }
 
-/// Returns a new PrimitiveArray<T> containing only those values from the array passed as the data_array parameter, 
+/// Returns a new PrimitiveArray<T> containing only those values from the array passed as the data_array parameter,
 /// selected by the BooleanArray passed as the filter_array parameter
-pub fn filter_primitive_array<T>(data_array: &PrimitiveArray<T>, filter_array: &BooleanArray) -> Result<PrimitiveArray<T>> 
+pub fn filter_primitive_array<T>(
+    data_array: &PrimitiveArray<T>,
+    filter_array: &BooleanArray,
+) -> Result<PrimitiveArray<T>>
 where
-    T: ArrowNumericType
+    T: ArrowNumericType,
 {
     FilterContext::new(filter_array).filter_primitive_array(data_array)
 }
 
-/// Returns a new DictionaryArray<T> containing only those keys from the array passed as the data_array parameter, 
+/// Returns a new DictionaryArray<T> containing only those keys from the array passed as the data_array parameter,
 /// selected by the BooleanArray passed as the filter_array parameter. The values are cloned from the data_array.
-pub fn filter_dictionary_array<T>(data_array: &DictionaryArray<T>, filter_array: &BooleanArray) -> Result<DictionaryArray<T>> 
+pub fn filter_dictionary_array<T>(
+    data_array: &DictionaryArray<T>,
+    filter_array: &BooleanArray,
+) -> Result<DictionaryArray<T>>
 where
-    T: ArrowNumericType
+    T: ArrowNumericType,
 {
     FilterContext::new(filter_array).filter_dictionary_array(data_array)
 }
 
-/// Returns a new RecordBatch with arrays containing only values matching the filter. 
+/// Returns a new RecordBatch with arrays containing only values matching the filter.
 /// The same FilterContext is re-used when filtering arrays in the RecordBatch for better performance.
-pub fn filter_record_batch(record_batch: &RecordBatch, filter_array: &BooleanArray) -> Result<RecordBatch> {
+pub fn filter_record_batch(
+    record_batch: &RecordBatch,
+    filter_array: &BooleanArray,
+) -> Result<RecordBatch> {
     let filter_context = FilterContext::new(filter_array);
-    let filtered_arrays = record_batch.columns().iter()
-        .map(|a| filter_context.filter(a.as_ref()).unwrap()).collect();
+    let filtered_arrays = record_batch
+        .columns()
+        .iter()
+        .map(|a| filter_context.filter(a.as_ref()).unwrap())
+        .collect();
     RecordBatch::try_new(record_batch.schema().clone(), filtered_arrays)
 }
 
@@ -536,7 +581,11 @@ mod tests {
         let a: Int8DictionaryArray = values.iter().map(|&x| x).collect();
         let b = BooleanArray::from(vec![false, true, true, false]);
         let c = filter(&a, &b).unwrap();
-        let d = c.as_ref().as_any().downcast_ref::<Int8DictionaryArray>().unwrap();
+        let d = c
+            .as_ref()
+            .as_any()
+            .downcast_ref::<Int8DictionaryArray>()
+            .unwrap();
         let value_array = d.values();
         let values = value_array.as_any().downcast_ref::<StringArray>().unwrap();
         // values are cloned in the filtered dictionary array
@@ -544,6 +593,9 @@ mod tests {
         // but keys are filtered
         assert_eq!(2, d.len());
         assert_eq!(true, d.is_null(0));
-        assert_eq!("world", values.value(d.keys().nth(1).unwrap().unwrap() as usize));
+        assert_eq!(
+            "world",
+            values.value(d.keys().nth(1).unwrap().unwrap() as usize)
+        );
     }
 }
