621022a99122277494b14693dd085b61ee02d1cb
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-20 20:50:11 +0200

    Verbose Windows build

--- dev/tasks/conda-recipes/arrow-cpp/bld-pyarrow.bat
+++ dev/tasks/conda-recipes/arrow-cpp/bld-pyarrow.bat
@@ -1,3 +1,4 @@
+@echo on
 pushd "%SRC_DIR%"\python
 
 @rem the symlinks for cmake modules don't work here
@@ -7,10 +8,15 @@ del cmake_modules\CompilerInfo.cmake
 del cmake_modules\FindNumPy.cmake
 del cmake_modules\FindPythonLibsNew.cmake
 copy /Y "%SRC_DIR%\cpp\cmake_modules\BuildUtils.cmake" cmake_modules\
+if errorlevel 1 exit 1
 copy /Y "%SRC_DIR%\cpp\cmake_modules\SetupCxxFlags.cmake" cmake_modules\
+if errorlevel 1 exit 1
 copy /Y "%SRC_DIR%\cpp\cmake_modules\CompilerInfo.cmake" cmake_modules\
+if errorlevel 1 exit 1
 copy /Y "%SRC_DIR%\cpp\cmake_modules\FindNumPy.cmake" cmake_modules\
+if errorlevel 1 exit 1
 copy /Y "%SRC_DIR%\cpp\cmake_modules\FindPythonLibsNew.cmake" cmake_modules\
+if errorlevel 1 exit 1
 
 SET ARROW_HOME=%LIBRARY_PREFIX%
 SET SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
