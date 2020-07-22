a35c8bb19002e7fe0acf0fda0d7356b8b15dcb48
Ha Thi Tham <thamht01188@gmail.com>
2020-07-22 22:06:24 +0700

    KeyEncryptionKey class

---
+++ cpp/src/parquet/key_encryption_key.h
@@ -0,0 +1,52 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "arrow/util/base64.h"
+
+class KeyEncryptionKey {
+ public:
+  KeyEncryptionKey(const std::vector<uint8_t>& kek_bytes,
+                   const std::vector<uint8_t>& kek_id,
+                   const std::string& encoded_wrapped_kek)
+      : kek_bytes_(kek_bytes),
+        kek_id_(kek_id),
+        encoded_wrapped_kek_(encoded_wrapped_kek) {}
+
+  const std::vector<uint8_t>& kek_bytes() const { return kek_bytes_; }
+
+  const std::vector<uint8_t>& kek_id() const { return kek_id_; }
+
+  const std::string& encoded_kek_id() {
+    if (encoded_kek_id_.empty()) {
+      encoded_kek_id_ = arrow::util::base64_encode(&kek_id_[0], kek_id_.size());
+    }
+    return encoded_kek_id_;
+  }
+
+  const std::string& encoded_wrapped_kek() const { return encoded_wrapped_kek_; }
+
+ private:
+  std::vector<uint8_t> kek_bytes_;
+  std::vector<uint8_t> kek_id_;
+  std::string encoded_kek_id_;
+  std::string encoded_wrapped_kek_;
+};
