2029ea51098c3d01183b4995cf5c2a0b1417e4c0
Tobias Mayer <tobim@fastmail.fm>
2020-07-23 14:09:33 +0200

    Attempt to fix FindLz4 for msvc

--- cpp/cmake_modules/FindLz4.cmake
+++ cpp/cmake_modules/FindLz4.cmake
@@ -21,11 +21,18 @@ endif()
 
 set(LZ4_STATIC_LIB_SUFFIX "${LZ4_MSVC_STATIC_LIB_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-if(ARROW_LZ4_USE_SHARED)
-  set(LZ4_LIB_NAMES lz4)
-else()
-  set(LZ4_LIB_NAMES "${CMAKE_STATIC_LIBRARY_PREFIX}lz4${LZ4_STATIC_LIB_SUFFIX}" lz4)
+set(LZ4_LIB_NAMES lz4 liblz4)
+
+if(NOT ARROW_LZ4_USE_SHARED)
+  foreach(name_ ${LZ4_LIB_NAMES})
+    list(APPEND static_names_
+                "${CMAKE_STATIC_LIBRARY_PREFIX}${name_}${LZ4_STATIC_LIB_SUFFIX}")
+  endforeach()
+  set(LZ4_LIB_NAMES ${static_names_} ${LZ4_LIB_NAMES})
+  undef(name_)
+  undef(static_names_)
 endif()
+
 if(LZ4_ROOT)
   find_library(LZ4_LIB
                NAMES ${LZ4_LIB_NAMES}
