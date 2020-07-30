16d0764a66291433ca8eddf8e26923f2223d6a7a
Krisztián Szűcs <szucs.krisztian@gmail.com>
2020-07-31 06:05:35 +0900

    ARROW-9599: [CI] Appveyor toolchain build fails because CMake detects different C and C++ compilers
    CMake released 3.18 version two weeks ago. We may want to report this issue upstream, until it is resolved pinning cmake to version 3.17 fixes the toolchain build.
    
    Closes #7865 from kszucs/appveyor
    
    Authored-by: Krisztián Szűcs <szucs.krisztian@gmail.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- ci/appveyor-cpp-setup.bat
+++ ci/appveyor-cpp-setup.bat
@@ -61,6 +61,7 @@ if "%JOB%" NEQ "Build_Debug" (
   conda create -n arrow -q -y -c conda-forge ^
     --file=ci\conda_env_python.yml ^
     %CONDA_PACKAGES%  ^
+    "cmake=3.17" ^
     "boost-cpp<1.70" ^
     "ninja" ^
     "nomkl" ^
