492a261abac2c55e23b7c2b4b47a4d9eb548aaf9
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2020-08-02 06:31:08 +0000

    fix lint error

--- dev/archery/tests/generate_files_for_endian_test.sh
+++ dev/archery/tests/generate_files_for_endian_test.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-arrow_dir=<top directory of cloned Arrow repository>
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+: ${ARROW_DIR:=/arrow}
 
 json_file=`archery integration --with-cpp=1 | grep "Testing file" | tail -1 | awk '{ print $3 }'`
 json_dir=`dirname $json_file`
-for f in $json_dir/*.json ; do $arrow_dir/cpp/build/debug/arrow-json-integration-test -mode JSON_TO_ARROW -json $f -arrow ${f%.*}.arrow_file -integration true ; done
-for f in $json_dir/*.arrow_file ; do $arrow_dir/cpp/build/debug/arrow-file-to-stream $f > ${f%.*}.stream; done
+for f in $json_dir/*.json ; do $ARROW_DIR/cpp/build/debug/arrow-json-integration-test -mode JSON_TO_ARROW -json $f -arrow ${f%.*}.arrow_file -integration true ; done
+for f in $json_dir/*.arrow_file ; do $ARROW_DIR/cpp/build/debug/arrow-file-to-stream $f > ${f%.*}.stream; done
 for f in $json_dir/*.json ; do gzip $f ; done
 echo "The files are under $json_dir"
