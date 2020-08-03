c84c7ae2b6292d659038084640e237178ade5a90
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-08-04 04:44:55 +0900

    ARROW-9621: [Python] Skip test_move_file for in-memory fsspec filesystem
    Awaiting a proper fix (ARROW-9621, https://github.com/intake/filesystem_spec/issues/367), let's disable the test so it doesn't cause noise in our CI.
    
    Closes #7890 from jorisvandenbossche/ARROW-9621-skip-test
    
    Lead-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- python/pyarrow/tests/test_fs.py
+++ python/pyarrow/tests/test_fs.py
@@ -789,6 +789,10 @@ def test_move_directory(fs, pathfn, allow_move_dir):
 
 
 def test_move_file(fs, pathfn):
+    if fs.type_name == "py::fsspec+memory":
+        # https://issues.apache.org/jira/browse/ARROW-9621
+        # https://github.com/intake/filesystem_spec/issues/367
+        pytest.xfail(reason='Not working with in-memory fsspec')
     s = pathfn('test-move-source-file')
     t = pathfn('test-move-target-file')
 
