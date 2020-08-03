1aacf0f70c362b535ae7784199bf67f16814b832
Romain Francois <romain@rstudio.com>
2020-08-03 13:04:35 +0200

    move test helper function to helper file

--- r/tests/testthat/test-Array.R
+++ r/tests/testthat/test-Array.R
@@ -17,33 +17,6 @@
 
 context("Array")
 
-expect_array_roundtrip <- function(x, type, as = NULL) {
-  a <- Array$create(x, type = as)
-  expect_type_equal(a$type, type)
-  expect_identical(length(a), length(x))
-  if (!inherits(type, c("ListType", "LargeListType"))) {
-    # TODO: revisit how missingness works with ListArrays
-    # R list objects don't handle missingness the same way as other vectors.
-    # Is there some vctrs thing we should do on the roundtrip back to R?
-    expect_equal(as.vector(is.na(a)), is.na(x))
-  }
-  expect_equivalent(as.vector(a), x)
-  # Make sure the storage mode is the same on roundtrip (esp. integer vs. numeric)
-  expect_identical(typeof(as.vector(a)), typeof(x))
-
-  if (length(x)) {
-    a_sliced <- a$Slice(1)
-    x_sliced <- x[-1]
-    expect_type_equal(a_sliced$type, type)
-    expect_identical(length(a_sliced), length(x_sliced))
-    if (!inherits(type, c("ListType", "LargeListType"))) {
-      expect_equal(as.vector(is.na(a_sliced)), is.na(x_sliced))
-    }
-    expect_equivalent(as.vector(a_sliced), x_sliced)
-  }
-  invisible(a)
-}
-
 test_that("Integer Array", {
   ints <- c(1:10, 1:10, 1:5)
   x <- expect_array_roundtrip(ints, int32())
