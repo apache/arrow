f376f5e01917045ebb70fa1301a1ecfb81b8ad3d
Neal Richardson <neal.p.richardson@gmail.com>
2020-08-04 15:34:48 -0700

    Move flight code into main R package

--- r/R/flight.R
+++ r/R/flight.R
@@ -1,13 +1,39 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+#' Load a Python Flight server
+#'
+#' @param name string Python module name
+#' @param path file system path where the Python module is found. Default is
+#' to look in the `inst/` directory for included modules.
+#' @export
+load_flight_server <- function(name, path = system.file(package = "arrow")) {
+  reticulate::import_from_path(name, path)
+}
+
 #' Connect to a Flight server
 #'
 #' @param host string hostname to connect to
 #' @param port integer port to connect on
 #' @param scheme URL scheme, default is "grpc+tcp"
 #' @return A `pyarrow.flight.FlightClient`.
-#' @importFrom reticulate import
 #' @export
 flight_connect <- function(host = "localhost", port, scheme = "grpc+tcp") {
-  pa <- import("pyarrow")
+  pa <- reticulate::import("pyarrow")
   location <- paste0(scheme, "://", host, ":", port)
   pa$flight$FlightClient(location)
 }
@@ -15,18 +41,16 @@ flight_connect <- function(host = "localhost", port, scheme = "grpc+tcp") {
 #' Send data to a Flight server
 #'
 #' @param client `pyarrow.flight.FlightClient`, as returned by [flight_connect()]
-#' @param data `data.frame` or [arrow::RecordBatch] to upload
+#' @param data `data.frame` or [RecordBatch] to upload
 #' @param path string identifier to store the data under
 #' @return `client`, invisibly.
-#' @importFrom arrow record_batch
-#' @importFrom reticulate r_to_py
 #' @export
 push_data <- function(client, data, path) {
   if (inherits(data, "data.frame")) {
     data <- record_batch(data)
   }
   # TODO: this is only RecordBatch; handle Table
-  py_data <- r_to_py(data)
+  py_data <- reticulate::r_to_py(data)
   writer <- client$do_put(descriptor_for_path(path), py_data$schema)[[1]]
   writer$write_batch(py_data)
   writer$close()
@@ -37,7 +61,7 @@ push_data <- function(client, data, path) {
 #'
 #' @param client `pyarrow.flight.FlightClient`, as returned by [flight_connect()]
 #' @param path string identifier under which the data is stored
-#' @return An [arrow::RecordBatch]
+#' @return A [RecordBatch]
 #' @export
 flight_get <- function(client, path) {
   info <- client$get_flight_info(descriptor_for_path(path))
@@ -52,6 +76,6 @@ flight_get <- function(client, path) {
 }
 
 descriptor_for_path <- function(path) {
-  pa <- import("pyarrow")
+  pa <- reticulate::import("pyarrow")
   pa$flight$FlightDescriptor$for_path(path)
 }
--- r/inst/demo_flight_server.py
+++ r/inst/demo_flight_server.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 """
     An example Flight Python server.
     See https://github.com/apache/arrow/blob/master/python/examples/flight/server.py
@@ -16,7 +33,7 @@ class DemoFlightServer(pyarrow.flight.FlightServerBase):
         if isinstance(port, float):
             # Because R is looser with integer vs. float
             port = int(port)
-        location = "grpc+tcp://{}:{}".format(host, port) 
+        location = "grpc+tcp://{}:{}".format(host, port)
         super(DemoFlightServer, self).__init__(location)
         self.flights = {}
         self.host = host
