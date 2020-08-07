b3eda751bd96c32c2b6b6b164a3b61edecced592
Micah Kornfield <micahk@google.com>
2020-08-07 14:58:14 -0700

    lint is wrong

--- cpp/src/arrow/util/basic_decimal.h
+++ cpp/src/arrow/util/basic_decimal.h
@@ -54,7 +54,7 @@ class ARROW_EXPORT BasicDecimal128 {
             typename = typename std::enable_if<
                 std::is_integral<T>::value && (sizeof(T) <= sizeof(uint64_t)), T>::type>
   constexpr BasicDecimal128(T value) noexcept
-      : BasicDecimal128(value >= T{0} ? 0 : -1, static_cast<uint64_t>(value)) {}
+      : BasicDecimal128(value >= T{0} ? 0 : -1, static_cast<uint64_t>(value)) {} // NOLINT
 
   /// \brief Create a BasicDecimal128 from an array of bytes. Bytes are assumed to be in
   /// native-endian byte order.
