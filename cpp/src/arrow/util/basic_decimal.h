44db339d059561011b8192fba00d952f0e2903af
Mingyu Zhong <myzhong@google.com>
2020-08-07 11:56:10 -0700

    Fix a bug BasicDecimal128 constructor that interprets unsigned integers with highest bit set as negative.

--- cpp/src/arrow/util/basic_decimal.h
+++ cpp/src/arrow/util/basic_decimal.h
@@ -51,9 +51,10 @@ class ARROW_EXPORT BasicDecimal128 {
 
   /// \brief Convert any integer value into a BasicDecimal128.
   template <typename T,
-            typename = typename std::enable_if<std::is_integral<T>::value, T>::type>
+            typename = typename std::enable_if<std::is_integral<T>::value &&
+                                               (sizeof(T) <= sizeof(uint64_t)), T>::type>
   constexpr BasicDecimal128(T value) noexcept
-      : BasicDecimal128(static_cast<int64_t>(value) >= 0 ? 0 : -1,
+      : BasicDecimal128(value >= T{0} ? 0 : -1,
                         static_cast<uint64_t>(value)) {}
 
   /// \brief Create a BasicDecimal128 from an array of bytes. Bytes are assumed to be in
