0c735999d60829e4385eaf70529e38bbecc25067
Micah Kornfield <micahk@google.com>
2020-08-07 14:38:46 -0700

    fix formatting

--- cpp/src/arrow/util/basic_decimal.h
+++ cpp/src/arrow/util/basic_decimal.h
@@ -51,11 +51,10 @@ class ARROW_EXPORT BasicDecimal128 {
 
   /// \brief Convert any integer value into a BasicDecimal128.
   template <typename T,
-            typename = typename std::enable_if<std::is_integral<T>::value &&
-                                               (sizeof(T) <= sizeof(uint64_t)), T>::type>
+            typename = typename std::enable_if<
+                std::is_integral<T>::value && (sizeof(T) <= sizeof(uint64_t)), T>::type>
   constexpr BasicDecimal128(T value) noexcept
-      : BasicDecimal128(value >= T{0} ? 0 : -1,
-                        static_cast<uint64_t>(value)) {}
+      : BasicDecimal128(value >= T{0} ? 0 : -1, static_cast<uint64_t>(value)) {}
 
   /// \brief Create a BasicDecimal128 from an array of bytes. Bytes are assumed to be in
   /// native-endian byte order.
