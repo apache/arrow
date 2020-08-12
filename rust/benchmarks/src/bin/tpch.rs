6822d1b2716778ef3e9e8a3ae7026eae38418feb
Andy Grove <andygrove73@gmail.com>
2020-08-12 16:29:21 -0600

    save

--- rust/benchmarks/src/bin/tpch.rs
+++ rust/benchmarks/src/bin/tpch.rs
@@ -87,14 +87,14 @@ fn main() -> Result<()> {
             "select
                 l_returnflag,
                 l_linestatus,
-                sum(l_quantity) as sum_qty,
-                sum(l_extendedprice) as sum_base_price,
-                sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-                sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
-                avg(l_quantity) as avg_qty,
-                avg(l_extendedprice) as avg_price,
-                avg(l_discount) as avg_disc,
-                count(*) as count_order
+                sum(l_quantity),
+                sum(l_extendedprice),
+                sum(l_extendedprice * (1 - l_discount)),
+                sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)),
+                avg(l_quantity),
+                avg(l_extendedprice),
+                avg(l_discount),
+                count(*)
             from
                 lineitem
             where
