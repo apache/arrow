1ef2b93d636e6ef34fca204e1c895c152ea8e0b9
mubai <chaokun.yck@antfin.com>
2020-08-13 11:23:04 +0800

    refine get_symlink_path

--- python/pyarrow/__init__.py
+++ python/pyarrow/__init__.py
@@ -380,9 +380,7 @@ def create_library_symlinks():
         bundled_libs = glob.glob(_os.path.join(package_cwd, '*.*.dylib'))
 
         def get_symlink_path(hard_path):
-            splits = hard_path.split('.')
-            splits.pop(-2)
-            return '.'.join(splits)
+            return '.'.join((hard_path.rsplit('.', 2)[0], 'dylib'))
 
     for lib_hard_path in bundled_libs:
         symlink_path = get_symlink_path(lib_hard_path)
