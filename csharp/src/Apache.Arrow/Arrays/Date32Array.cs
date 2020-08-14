5677f9e7aeb5458d22ea799dc3d0bf23a3a388d6
Adam Szmigin <adam.szmigin@jetstoneam.com>
2020-08-14 18:51:56 -0500

    ARROW-8581: [C#] Accept and return DateTime from DateXXArray
    This PR introduces additions to the public API for `Date32Array` and `Date64Array` by accepting and returning `System.DateTime` in additio to `System.DateTimeOffset`.
    
    The rationale for making this change is explained in detail on the [JIRA ticket](https://issues.apache.org/jira/browse/ARROW-8581), but briefly: making this change avoids a class of bugs that manifest based on the user's timezone, and where it is very easy to unknowingly fall into a trap.
    
    Note that the array builders no longer derive from `PrimitiveArrayBuilder<TFrom, TTo, ...>`: that base class is designed for conversion from exactly one outward-facing type to the underlying type, but these builders now have two outward-facing types.  As a replacement, I have introduced abstract types `DateArrayBuilder` (specific to dates) and `DelegatingArrayBuilder` (more general-purpose).   See inline comments for further details.
    
    Closes #7654 from mr-smidge/arrow-8581/date-array-builder-type-change
    
    Authored-by: Adam Szmigin <adam.szmigin@jetstoneam.com>
    Signed-off-by: Eric Erhardt <eric.erhardt@microsoft.com>

--- csharp/src/Apache.Arrow/Arrays/Date32Array.cs
+++ csharp/src/Apache.Arrow/Arrays/Date32Array.cs
@@ -18,13 +18,20 @@ using System;
 
 namespace Apache.Arrow
 {
+    /// <summary>
+    /// The <see cref="Date32Array"/> class holds an array of dates in the <c>Date32</c> format, where each date is
+    /// stored as the number of days since the dawn of (UNIX) time.
+    /// </summary>
     public class Date32Array : PrimitiveArray<int>
     {
-        private const long MillisecondsPerDay = 86400000;
+        private static readonly DateTime _epochDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Unspecified);
 
-        public class Builder : PrimitiveArrayBuilder<DateTimeOffset, int, Date32Array, Builder>
+        /// <summary>
+        /// The <see cref="Builder"/> class can be used to fluently build <see cref="Date32Array"/> objects.
+        /// </summary>
+        public class Builder : DateArrayBuilder<int, Date32Array, Builder>
         {
-            internal class DateBuilder : PrimitiveArrayBuilder<int, Date32Array, DateBuilder>
+            private class DateBuilder : PrimitiveArrayBuilder<int, Date32Array, DateBuilder>
             {
                 protected override Date32Array Build(
                     ArrowBuffer valueBuffer, ArrowBuffer nullBitmapBuffer,
@@ -32,11 +39,23 @@ namespace Apache.Arrow
                     new Date32Array(valueBuffer, nullBitmapBuffer, length, nullCount, offset);
             }
 
+            /// <summary>
+            /// Construct a new instance of the <see cref="Builder"/> class.
+            /// </summary>
             public Builder() : base(new DateBuilder()) { }
 
-            protected override int ConvertTo(DateTimeOffset value)
+            protected override int Convert(DateTime dateTime)
             {
-                return (int)(value.ToUnixTimeMilliseconds() / MillisecondsPerDay);
+                return (int)(dateTime.Date - _epochDate).TotalDays;
+            }
+
+            protected override int Convert(DateTimeOffset dateTimeOffset)
+            {
+                // The internal value stored for a DateTimeOffset can be thought of as the number of 24-hour "blocks"
+                // of time that have elapsed since the UNIX epoch.  This is the same as converting it to UTC first and
+                // then taking the date element from that.  It is not the same as what would result from looking at the
+                // DateTimeOffset.Date property.
+                return (int)(dateTimeOffset.UtcDateTime.Date - _epochDate).TotalDays;
             }
         }
 
@@ -55,16 +74,39 @@ namespace Apache.Arrow
 
         public override void Accept(IArrowArrayVisitor visitor) => Accept(this, visitor);
 
-        public DateTimeOffset? GetDate(int index)
+        [Obsolete("Use `GetDateTimeOffset()` instead")]
+        public DateTimeOffset? GetDate(int index) => GetDateTimeOffset(index);
+
+        /// <summary>
+        /// Get the date at the specified index in the form of a <see cref="DateTime"/> object.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="DateTime.Kind"/> property of the returned object is set to
+        /// <see cref="DateTimeKind.Unspecified"/>.
+        /// </remarks>
+        /// <param name="index">Index at which to get the date.</param>
+        /// <returns>Returns a <see cref="DateTime"/> object, or <c>null</c> if there is no object at that index.
+        /// </returns>
+        public DateTime? GetDateTime(int index)
         {
             int? value = GetValue(index);
+            return value.HasValue
+                ? _epochDate.AddDays(value.Value)
+                : default(DateTime?);
+        }
 
-            if (!value.HasValue)
-            {
-                return default;
-            }
-
-            return DateTimeOffset.FromUnixTimeMilliseconds(value.Value * MillisecondsPerDay);
+        /// <summary>
+        /// Get the date at the specified index in the form of a <see cref="DateTimeOffset"/> object.
+        /// </summary>
+        /// <param name="index">Index at which to get the date.</param>
+        /// <returns>Returns a <see cref="DateTimeOffset"/> object, or <c>null</c> if there is no object at that index.
+        /// </returns>
+        public DateTimeOffset? GetDateTimeOffset(int index)
+        {
+            int? value = GetValue(index);
+            return value.HasValue
+                ? new DateTimeOffset(_epochDate.AddDays(value.Value), TimeSpan.Zero)
+                : default(DateTimeOffset?);
         }
     }
 }
--- csharp/test/Apache.Arrow.Tests/Date64ArrayTests.cs
+++ csharp/test/Apache.Arrow.Tests/Date64ArrayTests.cs
@@ -20,8 +20,10 @@ using Xunit;
 
 namespace Apache.Arrow.Tests
 {
-    public class Date32ArrayTests
+    public class Date64ArrayTests
     {
+        private const long MillisecondsPerDay = 86400000;
+
         public static IEnumerable<object[]> GetDatesData() =>
             TestDateAndTimeData.ExampleDates.Select(d => new object[] { d });
 
@@ -37,7 +39,7 @@ namespace Apache.Arrow.Tests
             public void AppendThenGetGivesNull()
             {
                 // Arrange
-                var builder = new Date32Array.Builder();
+                var builder = new Date64Array.Builder();
 
                 // Act
                 builder = builder.AppendNull();
@@ -54,15 +56,15 @@ namespace Apache.Arrow.Tests
         public class AppendDateTime
         {
             [Theory]
-            [MemberData(nameof(GetDatesData), MemberType = typeof(Date32ArrayTests))]
+            [MemberData(nameof(GetDatesData), MemberType = typeof(Date64ArrayTests))]
             public void AppendDateGivesSameDate(DateTime date)
             {
                 // Arrange
-                var builder = new Date32Array.Builder();
+                var builder = new Date64Array.Builder();
                 var expectedDateTime = date;
                 var expectedDateTimeOffset =
                     new DateTimeOffset(DateTime.SpecifyKind(date, DateTimeKind.Unspecified), TimeSpan.Zero);
-                int expectedValue = (int)date.Subtract(new DateTime(1970, 1, 1)).TotalDays;
+                long expectedValue = (long)date.Subtract(new DateTime(1970, 1, 1)).TotalDays * MillisecondsPerDay;
 
                 // Act
                 builder = builder.Append(date);
@@ -73,18 +75,20 @@ namespace Apache.Arrow.Tests
                 Assert.Equal(expectedDateTime, array.GetDateTime(0));
                 Assert.Equal(expectedDateTimeOffset, array.GetDateTimeOffset(0));
                 Assert.Equal(expectedValue, array.GetValue(0));
+                Assert.Equal(0, array.GetValue(0).Value % MillisecondsPerDay);
             }
 
             [Theory]
-            [MemberData(nameof(GetDateTimesData), MemberType = typeof(Date32ArrayTests))]
+            [MemberData(nameof(GetDateTimesData), MemberType = typeof(Date64ArrayTests))]
             public void AppendWithTimeGivesSameWithTimeIgnored(DateTime dateTime)
             {
                 // Arrange
-                var builder = new Date32Array.Builder();
+                var builder = new Date64Array.Builder();
                 var expectedDateTime = dateTime.Date;
                 var expectedDateTimeOffset =
                     new DateTimeOffset(DateTime.SpecifyKind(dateTime.Date, DateTimeKind.Unspecified), TimeSpan.Zero);
-                int expectedValue = (int)dateTime.Date.Subtract(new DateTime(1970, 1, 1)).TotalDays;
+                long expectedValue =
+                    (long)dateTime.Date.Subtract(new DateTime(1970, 1, 1)).TotalDays * MillisecondsPerDay;
 
                 // Act
                 builder = builder.Append(dateTime);
@@ -95,20 +99,23 @@ namespace Apache.Arrow.Tests
                 Assert.Equal(expectedDateTime, array.GetDateTime(0));
                 Assert.Equal(expectedDateTimeOffset, array.GetDateTimeOffset(0));
                 Assert.Equal(expectedValue, array.GetValue(0));
+                Assert.Equal(0, array.GetValue(0).Value % MillisecondsPerDay);
             }
         }
 
         public class AppendDateTimeOffset
         {
             [Theory]
-            [MemberData(nameof(GetDateTimeOffsetsData), MemberType = typeof(Date32ArrayTests))]
+            [MemberData(nameof(GetDateTimeOffsetsData), MemberType = typeof(Date64ArrayTests))]
             public void AppendGivesUtcDate(DateTimeOffset dateTimeOffset)
             {
                 // Arrange
-                var builder = new Date32Array.Builder();
+                var builder = new Date64Array.Builder();
                 var expectedDateTime = dateTimeOffset.UtcDateTime.Date;
                 var expectedDateTimeOffset = new DateTimeOffset(dateTimeOffset.UtcDateTime.Date, TimeSpan.Zero);
-                int expectedValue = (int)dateTimeOffset.UtcDateTime.Date.Subtract(new DateTime(1970, 1, 1)).TotalDays;
+                long expectedValue =
+                    (long)dateTimeOffset.UtcDateTime.Date.Subtract(new DateTime(1970, 1, 1)).TotalDays *
+                    MillisecondsPerDay;
 
                 // Act
                 builder = builder.Append(dateTimeOffset);
@@ -119,6 +126,7 @@ namespace Apache.Arrow.Tests
                 Assert.Equal(expectedDateTime, array.GetDateTime(0));
                 Assert.Equal(expectedDateTimeOffset, array.GetDateTimeOffset(0));
                 Assert.Equal(expectedValue, array.GetValue(0));
+                Assert.Equal(0, array.GetValue(0).Value % MillisecondsPerDay);
             }
         }
     }
