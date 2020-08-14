2d821d8c9bf7bb5ab7527b0afdc3db933ec34edb
Neal Richardson <neal.p.richardson@gmail.com>
2020-08-14 11:07:00 -0700

    Tweaks to get autobrew working (On My Machine at least)

--- dev/tasks/homebrew-formulae/autobrew/apache-arrow.rb
+++ dev/tasks/homebrew-formulae/autobrew/apache-arrow.rb
@@ -58,7 +58,7 @@ class ApacheArrow < Formula
       -DARROW_WITH_UTF8PROC=OFF
       -DARROW_BUILD_UTILITIES=ON
       -DARROW_VERBOSE_THIRDPARTY_BUILD=ON
-      -DCMAKE_UNITY_BUILD=ON
+      -DCMAKE_UNITY_BUILD=OFF
       -DPARQUET_BUILD_EXECUTABLES=ON
       -DAWSSDK_HOME=#{Formula["aws-sdk-cpp"].prefix}
       -DLZ4_HOME=#{Formula["lz4"].prefix}
