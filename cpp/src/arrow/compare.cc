c4f8436e2532524c2a2f9cc26f73c6b7dfababaa
liyafan82 <fan_li_ya@foxmail.com>
2020-08-17 16:27:46 +0200

    ARROW-9495: [C++] Equality assertions don't handle Inf / -Inf properly
    See https://issues.apache.org/jira/browse/ARROW-9495
    
    Closes #7826 from liyafan82/fly_0724_eq
    
    Authored-by: liyafan82 <fan_li_ya@foxmail.com>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/src/arrow/compare.cc
+++ cpp/src/arrow/compare.cc
@@ -110,11 +110,12 @@ inline bool FloatingApproxEquals(const NumericArray<ArrowType>& left,
 
   if (opts.nans_equal()) {
     return BaseFloatingEquals<ArrowType>(left, right, [epsilon](T x, T y) -> bool {
-      return (fabs(x - y) <= epsilon) || (std::isnan(x) && std::isnan(y));
+      return (fabs(x - y) <= epsilon) || (x == y) || (std::isnan(x) && std::isnan(y));
     });
   } else {
-    return BaseFloatingEquals<ArrowType>(
-        left, right, [epsilon](T x, T y) -> bool { return fabs(x - y) <= epsilon; });
+    return BaseFloatingEquals<ArrowType>(left, right, [epsilon](T x, T y) -> bool {
+      return (fabs(x - y) <= epsilon) || (x == y);
+    });
   }
 }
 
