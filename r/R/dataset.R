b7e6da0d409d69fb1c43beb226dcc68817930af6
Neal Richardson <neal.p.richardson@gmail.com>
2020-08-28 12:02:43 -0700

    Update function signature

--- r/R/dataset.R
+++ r/R/dataset.R
@@ -133,7 +133,7 @@ open_dataset <- function(sources,
 #'   may also replace the dataset's schema by using `ds$schema <- new_schema`.
 #'   This method currently supports only adding, removing, or reordering
 #'   fields in the schema: you cannot alter or cast the field types.
-#' - `$write(path, filesystem, schema, format, partitioning)`: writes the
+#' - `$write(path, filesystem, schema, format, partitioning, ...)`: writes the
 #'   dataset to `path` in the `format` file format, partitioned by `partitioning`,
 #'   and invisibly returns `self`. See [write_dataset()].
 #'
