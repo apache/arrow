96564a2c82e29e9ddcecce0dfc8f761582dd589a
Mark Rushakoff <mark.rushakoff@influxdata.com>
2020-09-04 13:59:12 -0400

    Fix Go checkptr failure
    When running the unit tests for github.com/influxdata/flux with the race
    detector enabled on Go 1.14 or newer, checkptr detects failures like
    "fatal error: checkptr: pointer arithmetic computed bad pointer value"
    which are fixed by this change.
    
    I did not have sufficient time to identify how the Flux tests were
    different from the Arrow tests -- I did look at the Arrow tests and I
    did not identify a difference -- but applying this change eliminated the
    checkptr failures in the Flux test suite.

--- go/arrow/math/float64_avx2_amd64.go
+++ go/arrow/math/float64_avx2_amd64.go
@@ -27,13 +27,13 @@ import (
 )
 
 //go:noescape
-func _sum_float64_avx2(buf, len, res unsafe.Pointer)
+func _sum_float64_avx2(buf unsafe.Pointer, len uintptr, res unsafe.Pointer)
 
 func sum_float64_avx2(a *array.Float64) float64 {
 	buf := a.Float64Values()
 	var (
 		p1  = unsafe.Pointer(&buf[0])
-		p2  = unsafe.Pointer(uintptr(len(buf)))
+		p2  = uintptr(len(buf))
 		res float64
 	)
 	_sum_float64_avx2(p1, p2, unsafe.Pointer(&res))
