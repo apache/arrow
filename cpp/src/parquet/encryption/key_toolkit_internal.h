5358ebd896e7bb7f6cdd15ff1a21040b0f5a7ae1
Ha Thi Tham <thamht01188@gmail.com>
2020-09-15 23:01:45 +0700

    try fixing warning decorated name too long C4503

--- cpp/src/parquet/encryption/key_toolkit_internal.h
+++ cpp/src/parquet/encryption/key_toolkit_internal.h
@@ -24,34 +24,6 @@ namespace parquet {
 namespace encryption {
 namespace internal {
 
-// in miliseconds
-using TimePoint = std::chrono::system_clock::time_point;
-
-static inline TimePoint CurrentTimePoint() { return std::chrono::system_clock::now(); }
-
-template <typename E>
-class ExpiringCacheEntry {
- public:
-  ExpiringCacheEntry() = default;
-
-  ExpiringCacheEntry(const E& cached_item, uint64_t expiration_interval_millis)
-      : cached_item_(cached_item) {
-    expiration_timestamp_ =
-        CurrentTimePoint() + std::chrono::milliseconds(expiration_interval_millis);
-  }
-
-  bool IsExpired() {
-    auto now = CurrentTimePoint();
-    return (now > expiration_timestamp_);
-  }
-
-  E& cached_item() { return cached_item_; }
-
- private:
-  TimePoint expiration_timestamp_;
-  E cached_item_;
-};
-
 // "data encryption key" and "master key identifier" are paired together as output when
 // parsing from "key material"
 class KeyWithMasterId {
--- cpp/src/parquet/encryption/two_level_cache_with_expiration.h
+++ cpp/src/parquet/encryption/two_level_cache_with_expiration.h
@@ -26,6 +26,56 @@
 namespace parquet {
 namespace encryption {
 
+namespace internal {
+
+// in miliseconds
+using TimePoint = std::chrono::system_clock::time_point;
+
+static inline TimePoint CurrentTimePoint() { return std::chrono::system_clock::now(); }
+
+template <typename E>
+class ExpiringCacheEntry {
+ public:
+  ExpiringCacheEntry() = default;
+
+  ExpiringCacheEntry(const E& cached_item, uint64_t expiration_interval_millis)
+      : cached_item_(cached_item) {
+    expiration_timestamp_ =
+        CurrentTimePoint() + std::chrono::milliseconds(expiration_interval_millis);
+  }
+
+  bool IsExpired() {
+    auto now = CurrentTimePoint();
+    return (now > expiration_timestamp_);
+  }
+
+  E& cached_item() { return cached_item_; }
+
+ private:
+  TimePoint expiration_timestamp_;
+  E cached_item_;
+};
+
+// This class is to avoid the below warning when compiling KeyToolkit class with VS2015
+// warning C4503: decorated name length exceeded, name was truncated
+template <typename V>
+class ExpiringCacheMapEntry {
+ public:
+  ExpiringCacheMapEntry() = default;
+
+  explicit ExpiringCacheMapEntry(uint64_t expiration_interval_millis)
+      : map_cache_(std::unordered_map<std::string, V>(), expiration_interval_millis) {}
+
+  bool IsExpired() { return map_cache_.IsExpired(); }
+
+  std::unordered_map<std::string, V>& cached_item() { return map_cache_.cached_item(); }
+
+ private:
+  ExpiringCacheEntry<std::unordered_map<std::string, V>> map_cache_;
+};
+
+}  // namespace internal
+
 // Two-level cache with expiration of internal caches according to token lifetime.
 // External cache is per token, internal is per string key.
 // Wrapper class around:
@@ -46,9 +96,8 @@ class TwoLevelCacheWithExpiration {
     auto external_cache_entry = cache_.find(access_token);
     if (external_cache_entry == cache_.end() ||
         external_cache_entry->second.IsExpired()) {
-      cache_.insert({access_token,
-                     internal::ExpiringCacheEntry<std::unordered_map<std::string, V>>(
-                         std::unordered_map<std::string, V>(), cache_entry_lifetime_ms)});
+      cache_.insert(
+          {access_token, internal::ExpiringCacheMapEntry<V>(cache_entry_lifetime_ms)});
     }
 
     return cache_[access_token].cached_item();
@@ -85,7 +134,7 @@ class TwoLevelCacheWithExpiration {
 
   void Remove(const std::string& access_token) {
     auto lock = mutex_.Lock();
-    cache_.remove(access_token);
+    cache_.erase(access_token);
   }
 
   void Clear() {
@@ -103,9 +152,7 @@ class TwoLevelCacheWithExpiration {
       }
     }
   }
-  std::unordered_map<std::string,
-                     internal::ExpiringCacheEntry<std::unordered_map<std::string, V>>>
-      cache_;
+  std::unordered_map<std::string, internal::ExpiringCacheMapEntry<V>> cache_;
   internal::TimePoint last_cache_cleanup_timestamp_;
   arrow::util::Mutex mutex_;
 };
