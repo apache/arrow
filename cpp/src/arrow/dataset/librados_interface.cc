36cb1369bd0488f04d05833c20b51ca34503829c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-09-17 12:30:48 +0530

    update librados interface

--- cpp/src/arrow/dataset/librados_interface.cc
+++ cpp/src/arrow/dataset/librados_interface.cc
@@ -3,15 +3,18 @@
 
 #include "arrow/dataset/librados_interface.h"
 
-int IoCtxInterface::write_full(const std::string& oid, librados::bufferlist& bl) override{
+namespace arrow {
+namespace dataset {
+
+int IoCtxInterface::write_full(const std::string& oid, librados::bufferlist& bl) {
     return this->ioCtx.write_full(oid,bl);
 }
 
-int IoCtxInterface::read(const std::string& oid, librados::bufferlist& bl, size_t len, uint64_t off) override{
+int IoCtxInterface::read(const std::string& oid, librados::bufferlist& bl, size_t len, uint64_t off) {
     return this->ioCtx.read(oid, bl, len, off);
 }
 
-int IoCtxInterface::exec(const std::string& oid, const char *cls, const char *method, librados::bufferlist& inbl, librados::bufferlist& outbl) override{
+int IoCtxInterface::exec(const std::string& oid, const char *cls, const char *method, librados::bufferlist& inbl, librados::bufferlist& outbl) {
     return this->ioCtx.exec(oid, cls, method, inbl, outbl);
 }
 
@@ -19,18 +22,21 @@ librados::IoCtx& IoCtxInterface::getIoCtx(){
     return this->ioCtx;
 }
 
-Radosinterface::init2(const char * const name, const char * const clustername, uint64_t flags) override {
-    return cluster.init2(name, clustername, flags);
+int RadosInterface::init2(const char * const name, const char * const clustername, uint64_t flags) {
+    return this->cluster.init2(name, clustername, flags);
 }
 
-int RadosInterface::conf_read_file(const char * const path) override{
-    return cluster.conf_read_file(path);
+int RadosInterface::conf_read_file(const char * const path) {
+    return this->cluster.conf_read_file(path);
 }
 
 int RadosInterface::connect() {
-    return cluster.connect();
+    return this->cluster.connect();
 }
 
 librados::Rados& RadosInterface::getCluster(){
-    return cluster;
+    return this->cluster;
+}
+
+}
 }
--- cpp/src/arrow/dataset/librados_interface.h
+++ cpp/src/arrow/dataset/librados_interface.h
@@ -3,29 +3,35 @@
 #include <iostream>
 #include <rados/librados.hpp>
 
+#include "arrow/dataset/type_fwd.h"
+#include "arrow/dataset/visibility.h"
+#include "arrow/util/macros.h"
+
 namespace arrow {
 namespace dataset {
 
 class ARROW_DS_EXPORT IoCtxInterface {
   public:
-    virtual int write_full(const std::string& oid, librados::bufferlist& bl) = 0;
-    virtual int read(const std::string& oid, librados::bufferlist& bl, size_t len, uint64_t off) = 0;
-    virtual int exec(const std::string& oid, const char *cls, const char *method, librados::bufferlist& inbl, librados::bufferlist& outbl) = 0;
+    IoCtxInterface();
+    int write_full(const std::string& oid, librados::bufferlist& bl);
+    int read(const std::string& oid, librados::bufferlist& bl, size_t len, uint64_t off);
+    int exec(const std::string& oid, const char *cls, const char *method, librados::bufferlist& inbl, librados::bufferlist& outbl);
     librados::IoCtx& getIoCtx();
 
-  protected:
-    librados::IoCtx& ioCtx
+  private:
+    librados::IoCtx &ioCtx;
 };
 
 class ARROW_DS_EXPORT RadosInterface {
   public:
-    virtual int init2(const char * const name, const char * const clustername, uint64_t flags) = 0;
-    virtual int conf_read_file(const char * const path) = 0;
-    virtual int connect() = 0;
+    RadosInterface();
+    int init2(const char * const name, const char * const clustername, uint64_t flags);
+    int conf_read_file(const char * const path);
+    int connect();
     librados::Rados& getCluster();
   
-  protected:
-    librados::Rados& cluster;
+  private:
+    librados::Rados &cluster;
 };
 
 }
