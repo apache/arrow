fc3696a5c9267d6bd73662f370e19c48c9e100ab
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-09-17 13:32:33 +0200

    ARROW-10027: [C++] Fix Take array kernel for NullType

--- cpp/src/arrow/compute/kernels/vector_selection.cc
+++ cpp/src/arrow/compute/kernels/vector_selection.cc
@@ -1171,7 +1171,9 @@ void NullTake(KernelContext* ctx, const ExecBatch& batch, Datum* out) {
   if (TakeState::Get(ctx).boundscheck) {
     KERNEL_RETURN_IF_ERROR(ctx, CheckIndexBounds(*batch[1].array(), batch[0].length()));
   }
-  out->value = std::make_shared<NullArray>(batch.length)->data();
+  // batch.length doesn't take into account the take indices
+  auto new_length = batch[1].array()->length;
+  out->value = std::make_shared<NullArray>(new_length)->data();
 }
 
 void NullFilter(KernelContext* ctx, const ExecBatch& batch, Datum* out) {
