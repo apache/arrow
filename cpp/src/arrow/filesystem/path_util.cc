37e182277a23d64b1b386d4a61bc9f1b5c77482e
Benjamin Kietzman <bengilgit@gmail.com>
2020-09-24 15:09:07 -0400

    move #include <algorithm>

--- cpp/src/arrow/filesystem/path_util.cc
+++ cpp/src/arrow/filesystem/path_util.cc
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <algorithm>
+
 #include "arrow/filesystem/path_util.h"
 #include "arrow/result.h"
 #include "arrow/status.h"
