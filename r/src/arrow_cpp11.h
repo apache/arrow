87c26f7a7e5a393681057923a790f6448ea07817
Benjamin Kietzman <bengilgit@gmail.com>
2020-09-25 20:26:49 -0400

    add nameof utility to replace typeid().name()

--- r/src/arrow_cpp11.h
+++ r/src/arrow_cpp11.h
@@ -16,11 +16,14 @@
 // under the License.
 
 #include <cstring>  // for strlen
+#include <iostream>
 #include <limits>
 #include <memory>
 #include <utility>
 #include <vector>
 #undef Free
+
+#include "arrow/util/nameof.h"
 
 namespace cpp11 {
 
@@ -158,7 +161,8 @@ struct ns {
 template <typename Pointer>
 Pointer r6_to_pointer(SEXP self) {
   if (!Rf_inherits(self, "ArrowObject")) {
-    std::string type_name = typeid(typename std::remove_pointer<Pointer>::type).name();
+    std::string type_name =
+        arrow::util::nameof<typename std::remove_pointer<Pointer>::type>();
     cpp11::stop("Invalid R object for %s, must be an ArrowObject", type_name.c_str());
   }
   void* p = R_ExternalPtrAddr(Rf_findVarInFrame(self, arrow::r::symbols::xp));
