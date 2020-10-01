9bff7c4f734c9aa5d7d2d0df133c8dde5f9321c4
David Li <li.davidm96@gmail.com>
2020-10-01 17:33:33 -0500

    ARROW-10054: [Python] don't crash when slice offset > length
    Adjust offsets in Python so that the caller gets an empty array, matching expectations for Python users.
    
    Closes #8241 from lidavidm/arrow-10054
    
    Authored-by: David Li <li.davidm96@gmail.com>
    Signed-off-by: Wes McKinney <wesm@apache.org>

--- python/pyarrow/tests/test_table.py
+++ python/pyarrow/tests/test_table.py
@@ -177,6 +177,30 @@ def test_chunked_array_getitem():
     assert data_slice.to_pylist() == []
 
 
+def test_chunked_array_slice():
+    data = [
+        pa.array([1, 2, 3]),
+        pa.array([4, 5, 6])
+    ]
+    data = pa.chunked_array(data)
+
+    data_slice = data.slice(len(data))
+    assert data_slice.type == data.type
+    assert data_slice.to_pylist() == []
+
+    data_slice = data.slice(len(data) + 10)
+    assert data_slice.type == data.type
+    assert data_slice.to_pylist() == []
+
+    table = pa.Table.from_arrays([data], names=["a"])
+    table_slice = table.slice(len(table))
+    assert len(table_slice) == 0
+
+    table = pa.Table.from_arrays([data], names=["a"])
+    table_slice = table.slice(len(table) + 10)
+    assert len(table_slice) == 0
+
+
 def test_chunked_array_iter():
     data = [
         pa.array([0]),
