90670e9e0ecc3116052f1447386651c4b5464998
Sutou Kouhei <kou@clear-code.com>
2020-10-02 06:39:13 +0900

    Apply no Git patch only if git doesn't exist

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -2676,12 +2676,18 @@ macro(build_awssdk)
     list(APPEND AWSSDK_LIBRARIES ${_AWSSDK_LIB})
   endforeach()
 
+  set(AWSSDK_PATCH_COMMAND)
+  find_program(GIT git)
+  if(NOT GIT)
+    list(APPEND AWSSDK_PATCH_COMMAND
+      patch -p1 < "${CMAKE_SOURCE_DIR}/cmake_modules/aws-sdk-cpp-no-git.patch")
+  endif()
   externalproject_add(
     awssdk_ep
     ${EP_LOG_OPTIONS}
     URL ${AWSSDK_SOURCE_URL}
     CMAKE_ARGS ${AWSSDK_CMAKE_ARGS}
-    PATCH_COMMAND patch -p1 < "${CMAKE_SOURCE_DIR}/cmake_modules/aws-sdk-cpp-no-git.patch"
+    PATCH_COMMAND ${AWSSDK_PATCH_COMMAND}
     BUILD_BYPRODUCTS ${AWSSDK_BUILD_BYPRODUCTS})
 
   add_dependencies(toolchain awssdk_ep)
