241f3c9390c7594ae6d0099d576fa772fc6ba2dc
Krisztián Szűcs <szucs.krisztian@gmail.com>
2020-10-05 14:14:36 +0200

    hypothesis only nightly task

--- ci/scripts/python_test.sh
+++ ci/scripts/python_test.sh
@@ -29,4 +29,4 @@ export LD_LIBRARY_PATH=${ARROW_HOME}/lib:${LD_LIBRARY_PATH}
 # Enable some checks inside Python itself
 export PYTHONDEVMODE=1
 
-pytest -r s --pyargs pyarrow
+pytest -r s ${PYTEST_ARGS} --pyargs pyarrow
