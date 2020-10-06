81712f4b7b5d775af0161db96e1e18e58a4947d7
Jorge C. Leitao <jorgecarleitao@gmail.com>
2020-10-06 06:00:26 +0200

    Renamed example

--- rust/datafusion/examples/dataframe_in_memory.rs
+++ rust/datafusion/examples/dataframe_in_memory.rs
@@ -27,8 +27,7 @@ use datafusion::datasource::MemTable;
 use datafusion::error::Result;
 use datafusion::prelude::*;
 
-/// This example demonstrates how to use the DataFrame API.
-/// We will use in-memory data to perform the calculation.
+/// This example demonstrates how to use the DataFrame API against in-memory data.
 #[tokio::main]
 async fn main() -> Result<()> {
     // define a schema.
