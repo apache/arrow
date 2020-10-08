5d0b5e334b50f48554ad17dce1da9de73feab55b
Uwe L. Korn <xhochy@users.noreply.github.com>
2020-10-08 11:46:10 +0200

    Apply suggestions from code review
    Co-authored-by: Neal Richardson <neal.p.richardson@gmail.com>

--- dev/tasks/conda-recipes/r-arrow/build_win.sh
+++ dev/tasks/conda-recipes/r-arrow/build_win.sh
@@ -6,7 +6,3 @@ set -exuo pipefail
 # Rename arrow.dll to lib_arrow.dll to avoid conflicts with the arrow-cpp arrow.dll
 sed -i -e 's/R_init_arrow/R_init_lib_arrow/g' r/src/arrowExports.cpp
 sed -i -e 's/useDynLib(arrow/useDynLib(lib_arrow/g' r/NAMESPACE
-
-# Make exported symbols visible
-# This needs to be patched in https://github.com/wch/r-source/blob/trunk/src/include/R_ext/Visibility.h
-sed -i -e 's/attribute_visible/__declspec(dllexport)/g' $BUILD_PREFIX/Lib/R/library/Rcpp/include/RcppCommon.h
