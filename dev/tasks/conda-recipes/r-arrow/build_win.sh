667a43a0fee03abc2a25d043047c9bf0343b9ab6
Uwe L. Korn <xhochy@users.noreply.github.com>
2020-10-13 10:51:50 +0200

    Update dev/tasks/conda-recipes/r-arrow/build_win.sh
    Co-authored-by: Benjamin Kietzman <bengilgit@gmail.com>

--- dev/tasks/conda-recipes/r-arrow/build_win.sh
+++ dev/tasks/conda-recipes/r-arrow/build_win.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 
 
 # Rename arrow.dll to lib_arrow.dll to avoid conflicts with the arrow-cpp arrow.dll
-sed -i -e 's/R_init_arrow/R_init_lib_arrow/g' r/src/arrowExports.cpp
+sed -i -e 's/void R_init_arrow/__declspec(dllexport) void R_init_lib_arrow/g' r/src/arrowExports.cpp
 sed -i -e 's/useDynLib(arrow/useDynLib(lib_arrow/g' r/NAMESPACE
