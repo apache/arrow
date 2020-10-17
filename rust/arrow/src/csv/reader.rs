5bc913b716bdbea29b94677084ce6e4b48d15c02
Jorge C. Leitao <jorgecarleitao@gmail.com>
2020-10-17 11:20:49 +0200

    Fixed error.

--- rust/arrow/src/csv/reader.rs
+++ rust/arrow/src/csv/reader.rs
@@ -36,7 +36,7 @@
 //!
 //! let file = File::open("test/data/uk_cities.csv").unwrap();
 //!
-//! let mut csv = csv::Reader::new(file, Arc::new(schema), false, None, 1024, None);
+//! let mut csv = csv::Reader::new(file, Arc::new(schema), false, None, 1024, None, None);
 //! let batch = csv.next().unwrap().unwrap();
 //! ```
 
