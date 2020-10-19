457935e1efbb9152bd862ba4c056ad67b208cab8
James Duong <jduong@dremio.com>
2020-10-19 08:39:34 -0400

    ARROW-10106: [FlightRPC][Java] Expose onIsReady() callback
    Expose onIsReady() callback on OutboundStreamListener
    
    - Add callback to run when the client is ready to receive new data during getStream.
    - This removes the need for FlightProducers to implement polling code on isReady.
    
    Closes #8476 from jduo/ARROW-10106
    
    Lead-authored-by: James Duong <jduong@dremio.com>
    Co-authored-by: James Duong <duong.james@gmail.com>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- java/flight/flight-core/src/main/java/org/apache/arrow/flight/FlightService.java
+++ java/flight/flight-core/src/main/java/org/apache/arrow/flight/FlightService.java
@@ -35,7 +35,6 @@ import org.apache.arrow.flight.impl.Flight;
 import org.apache.arrow.flight.impl.FlightServiceGrpc.FlightServiceImplBase;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.util.AutoCloseables;
-import org.apache.arrow.vector.VectorUnloader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -88,6 +87,7 @@ class FlightService extends FlightServiceImplBase {
   public void doGetCustom(Flight.Ticket ticket, StreamObserver<ArrowMessage> responseObserverSimple) {
     final ServerCallStreamObserver<ArrowMessage> responseObserver =
         (ServerCallStreamObserver<ArrowMessage>) responseObserverSimple;
+
     final GetListener listener = new GetListener(responseObserver, this::handleExceptionWithMiddleware);
     try {
       producer.getStream(makeContext(responseObserver), new Ticket(ticket), listener);
@@ -127,8 +127,7 @@ class FlightService extends FlightServiceImplBase {
     private ServerCallStreamObserver<ArrowMessage> responseObserver;
     private final Consumer<Throwable> errorHandler;
     private Runnable onCancelHandler = null;
-    // null until stream started
-    private volatile VectorUnloader unloader;
+    private Runnable onReadyHandler = null;
     private boolean completed;
 
     public GetListener(ServerCallStreamObserver<ArrowMessage> responseObserver, Consumer<Throwable> errorHandler) {
@@ -137,6 +136,7 @@ class FlightService extends FlightServiceImplBase {
       this.completed = false;
       this.responseObserver = responseObserver;
       this.responseObserver.setOnCancelHandler(this::onCancel);
+      this.responseObserver.setOnReadyHandler(this::onReady);
       this.responseObserver.disableAutoInboundFlowControl();
     }
 
@@ -147,9 +147,20 @@ class FlightService extends FlightServiceImplBase {
       }
     }
 
+    private void onReady() {
+      if (onReadyHandler != null) {
+        onReadyHandler.run();
+      }
+    }
+
     @Override
     public void setOnCancelHandler(Runnable handler) {
       this.onCancelHandler = handler;
+    }
+
+    @Override
+    public void setOnReadyHandler(Runnable handler) {
+      this.onReadyHandler = handler;
     }
 
     @Override
