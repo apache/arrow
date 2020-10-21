c55a106245874513333c99f320f0e50be0238241
Micah Kornfield <micahk@google.com>
2020-10-21 09:18:19 -0700

    change constructor

--- cpp/src/arrow/util/basic_decimal.cc
+++ cpp/src/arrow/util/basic_decimal.cc
@@ -267,9 +267,7 @@ struct uint128_t {
     val_ = (static_cast<__uint128_t>(decimal.high_bits()) << 64) | decimal.low_bits();
   }
 
-  explicit uint128_t(std::array<uint64_t, 2> value) {
-    val_ = (static_cast<__uint128_t>(value[1] << 64)) | values[0];
-  }
+  explicit uint128_t(uint64_t value) : val_(value) {}
 
   uint64_t hi() { return val_ >> 64; }
   uint64_t lo() { return val_ & kInt64Mask; }
