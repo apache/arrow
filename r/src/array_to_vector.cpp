d6037ccce5f7081db8d164618ff40a27c1f49b85
Neal Richardson <neal.p.richardson@gmail.com>
2020-10-27 08:11:03 -0700

    Apply @bkietz's suggestions

--- r/src/array_to_vector.cpp
+++ r/src/array_to_vector.cpp
@@ -292,25 +292,30 @@ struct Converter_String : public Converter {
       // need to watch for nulls
       arrow::internal::BitmapReader null_reader(array->null_bitmap_data(),
                                                 array->offset(), n);
-      for (int i = 0; i < n; i++, null_reader.Next()) {
-        if (null_reader.IsSet()) {
-          SET_STRING_ELT(data, start + i, r_string_from_view(string_array->GetView(i)));
-        } else {
-          SET_STRING_ELT(data, start + i, NA_STRING);
+      cpp11::unwind_protect([&] {
+        for (int i = 0; i < n; i++, null_reader.Next()) {
+          if (null_reader.IsSet()) {
+            SET_STRING_ELT(data, start + i, r_string_from_view(string_array->GetView(i)));
+          } else {
+            SET_STRING_ELT(data, start + i, NA_STRING);
+          }
         }
-      }
-
+      });
     } else {
-      for (int i = 0; i < n; i++) {
-        SET_STRING_ELT(data, start + i, r_string_from_view(string_array->GetView(i)));
-      }
+      cpp11::unwind_protect([&] {
+        for (int i = 0; i < n; i++) {
+          SET_STRING_ELT(data, start + i, r_string_from_view(string_array->GetView(i)));
+        }
+      });
     }
 
     return Status::OK();
   }
 
   bool Parallel() const { return false; }
-  inline SEXP r_string_from_view(const arrow::util::string_view& view) const {
+
+ private:
+  static SEXP r_string_from_view(const arrow::util::string_view& view) {
     return Rf_mkCharLenCE(view.data(), view.size(), CE_UTF8);
   }
 };
