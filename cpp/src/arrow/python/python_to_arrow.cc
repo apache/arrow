8b76c43a1c575c3224a3fe9e4d085f8ee5b9f3e8
emkornfield <micahk@google.com>
2020-11-02 11:02:44 -0800

    Update python_to_arrow.cc
    add comment.

--- cpp/src/arrow/python/python_to_arrow.cc
+++ cpp/src/arrow/python/python_to_arrow.cc
@@ -262,6 +262,8 @@ class PyValue {
           break;
         case TimeUnit::NANO:
           if (internal::IsPandasTimestamp(obj)) {
+            // pd.Timestamp value attribute contains the offset from unix epoch
+            // so no adjustment for timezone is need.
             OwnedRef nanos(PyObject_GetAttrString(obj, "value"));
             RETURN_IF_PYERROR();
             RETURN_NOT_OK(internal::CIntFromPython(nanos.obj(), &value));
