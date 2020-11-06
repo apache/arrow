6a00509ce76db57107ae4a4ab8b7b19fa1947f53
Micah Kornfield <emkornfield@gmail.com>
2020-11-06 06:03:31 +0000

    Add SlicedValues API to array

--- cpp/src/arrow/array/array_base.h
+++ cpp/src/arrow/array/array_base.h
@@ -207,6 +207,12 @@ static inline std::ostream& operator<<(std::ostream& os, const Array& x) {
 
 /// Base class for non-nested arrays
 class ARROW_EXPORT FlatArray : public Array {
+ public:
+  /// Returns the slice buffer for the second buffer in the
+  /// array (values buffer for primitives, offsets buffers
+  /// for variable length binary types.
+  virtual Result<std::shared_ptr<const Buffer>> SlicedValues(MemoryPool* pool) const = 0;
+
  protected:
   using Array::Array;
 };
@@ -242,6 +248,10 @@ class ARROW_EXPORT NullArray : public FlatArray {
 
   explicit NullArray(const std::shared_ptr<ArrayData>& data) { SetData(data); }
   explicit NullArray(int64_t length);
+
+  Result<std::shared_ptr<const Buffer>> SlicedValues(MemoryPool* pool) const override {
+    return NULLPTR;
+  }
 
  private:
   void SetData(const std::shared_ptr<ArrayData>& data) {
