ca76c8be19bb6b563e35c4593a591b64bfafcf0f
James Duong <jduong@dremio.com>
2020-11-12 16:05:51 -0800

    Client cookie middleware with stubs for parsing cookies

--- cpp/src/arrow/flight/client_cookie_middleware.cc
+++ cpp/src/arrow/flight/client_cookie_middleware.cc
@@ -20,33 +20,105 @@
 // Platform-specific defines
 #include "arrow/flight/platform.h"
 
+namespace {
+  class Cookie {
+   public:
+    static Cookie parse(const util::string_view& cookie_value) {
+      // Parse the cookie string. If the cookie has an expiration, record it.
+      // If the cookie has a max-age, calculate the current time + max_age and set that as the expiration.
+      return Cookie();
+    }
+
+    bool IsExpired() {
+      // Check if current-time is less than creation time.
+      return false;
+    }
+
+    std::string AsCookieString() {
+      // Return the string for the cookie as it would appear in a Cookie header.
+      // Keys must be wrapped in quotes depending on if this is a v1 or v2 cookie.
+      return "";
+    }
+
+   private:
+    std::string cookie_name_;
+    std::string cookie_value_;
+    time_t expiration_time_;
+    bool is_v1_cookie_;
+  };
+} // end of anonymous namespace
+
 namespace arrow {
 
 namespace flight {
 
+using CookieCache = std::map<std::string, std::string>;
+
 class ClientCookieMiddleware::Impl {
  public:
   void SendingHeaders(AddCallHeaders* outgoing_headers) {
+    const std::string& cookie_string = factory_impl_.GetValidCookiesAsString();
+    if (!cookie_string.empty()) {
+      outgoing_headers->AddHeader("Cookie", cookie_string);
+    }
   }
 
   void ReceivedHeaders(const CallHeaders& incoming_headers) {
-      // Parse the Set-Cookie header values here, and write to the collection managed in Factory::Impl
+    const std::pair<CallHeaders::const_iterator, CallHeaders::const_iterator>& iter_pair =
+      incoming_headers.equal_range("Set-Cookie");
+    for (auto it = iter_pair.first; it != iter_pair.second; ++it) {
+      // Alternatively we can send every cookie to the factory in one call to reduce
+      // the amount of locking (by sending the interval of Set-Cookie values).
+      const util::string_view& value = (*iter_pair.first).second;
+      factory_impl_.UpdateCachedCookie(value);
+    }
+
+    factory_impl_.DiscardExpiredCookies();
   }
 
   void CallCompleted(const Status& status) {
   }
+ private:
+  Factory::Impl factory_impl_;
+  Impl(Factory::Impl& factory_impl) :
+   factory_impl_(factory_impl) {
+   }
 };
 
 class ClientCookieMiddleware::Factory::Impl {
  public:
   void StartCall(const CallInfo& info,
-                            std::unique_ptr<ClientMiddleware::Impl>* middleware) {
-    // Instantiate middleware and have it store the 
+                            std::unique_ptr<ClientMiddleware>* middleware) {
+    ARROW_UNUSED(info);
+    std::unique_ptr<ClientCookieMiddleware> cookie_middleware = std::make_shared<ClientCookieMiddleware>();
+    cookie_middleware->impl_ = std::make_shared(*this);
+    *middleware = std::move(cookie_middleware);
   }
  private:
-      // Create a object class and store a collection of them here.
-      // Needs to be a concurrent data structure, or use locks to protected it.
+  std::string GetValidCookiesAsString() const {
+    return "";
+  }
 
+  void UpdateCachedCookie(const util::string_view& cookie_value) {
+    std::lock_guard<std::mutex> guard(mutex_);
+
+    // Parse the Set-Cookie header values here
+  }
+
+  void DiscardExpiredCookies() {
+    std::lock_guard<std::mutex> guard(mutex_);
+
+    for (auto it = cookie_cache_.begin(); it != cookie_cache_.end(); ) {
+      if (it->second.IsExpired()) {
+        it = cookie_cache_.erase(it);
+      } else {
+        ++it;
+      }
+    }
+  }
+
+  CookieCache cookie_cache_;
+  std::mutex mutex_;
 };
 
 void ClientCookieMiddleware::SendingHeaders(AddCallHeaders* outgoing_headers) {
@@ -59,14 +131,15 @@ void ClientCookieMiddleware::ReceivedHeaders(const CallHeaders& incoming_headers
 
 void ClientCookieMiddleware::CallCompleted(const Status& status) {
   impl_->CallCompleted(status);
+}
 
 ClientCookieMiddleware::ClientCookieMiddleware() {
 }
 
-ClientCookieMiddleware::Factory::Factory() {
-}
+ClientCookieMiddleware::Factory::Factory() :
+ impl_(std::make_shared<>()) {
 
-Cli
+}
 
 }  // namespace flight
 }  // namespace arrow
