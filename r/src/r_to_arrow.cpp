3967f46c951769ec6b4133be4eb52aa493dce422
Romain Francois <romain@rstudio.com>
2020-11-13 14:42:02 +0100

    going a little further with an initial conversion from INTSXP -> int32

--- r/src/r_to_arrow.cpp
+++ r/src/r_to_arrow.cpp
@@ -52,38 +52,192 @@ struct RConversionOptions {
   int64_t size;
 };
 
-struct RObject {};
+enum RVectorType {
+  BOOLEAN,
+  UINT8,
+  INT32,
+  FLOAT64,
+  INTEGER64,
+  COMPLEX,
+  STRING,
+  DATAFRAME,
+  DATE,
+  TIME,
+  TIMESTAMP,
+
+  OTHER
+};
+
+RVectorType GetVectorType(SEXP x) {
+  switch (TYPEOF(x)) {
+    case LGLSXP:
+      return BOOLEAN;
+    case RAWSXP:
+      return UINT8;
+    case INTSXP:
+      return INT32;
+    case STRSXP:
+      return STRING;
+    case CPLXSXP:
+      return COMPLEX;
+    case REALSXP: {
+      if (Rf_inherits(x, "integer64")) {
+        return INTEGER64;
+      } else if (Rf_inherits(x, "POSIXct")) {
+        return TIMESTAMP;
+      } else if (Rf_inherits(x, "difftime")) {
+        return TIME;
+      } else {
+        return FLOAT64;
+      }
+    }
+    case VECSXP: {
+      if (Rf_inherits(x, "data.frame")) {
+        return DATAFRAME;
+      }
+      // TODO: binary, list, POSIXlt
+      break;
+    }
+    default:
+      break;
+  }
+  return OTHER;
+}
+
+struct RObject {
+  RVectorType rtype;
+  void* data;
+  bool null;
+};
+
+class RValue {
+ public:
+  static bool IsNull(RObject* obj) { return obj->null; }
+
+  // TODO: generalise
+  static Result<int32_t> Convert(const Int32Type*, const RConversionOptions&,
+                                 RObject* value) {
+    // TODO: handle conversion from other types
+    if (value->rtype == INT32) {
+      return *reinterpret_cast<int32_t*>(value->data);
+    }
+
+    // TODO: improve error
+    return Status::Invalid("invalid conversion");
+  }
+};
+
+template <class VisitorFunc>
+inline Status VisitVector_Int32(SEXP x, R_xlen_t size, VisitorFunc&& func) {
+  RObject obj{INT32, nullptr, false};
+  cpp11::integers values(x);
+  for (int value : values) {
+    obj.data = reinterpret_cast<void*>(&value);
+    obj.null = value == NA_INTEGER;
+    RETURN_NOT_OK(func(&obj));
+  }
+  return Status::OK();
+}
+
+template <class VisitorFunc>
+inline Status VisitVector(SEXP x, R_xlen_t size, VisitorFunc&& func) {
+  RVectorType rtype = GetVectorType(x);
+
+  switch (rtype) {
+    case INT32:
+      return VisitVector_Int32(x, size, std::forward<VisitorFunc>(func));
+    default:
+      break;
+  }
+
+  return Status::OK();
+}
 
 using RConverter = Converter<RObject*, RConversionOptions>;
 
+template <typename T, typename Enable = void>
+class RPrimitiveConverter;
+
 // TODO: this needs various versions as what python does:
-// class PyPrimitiveConverter<T, enable_if_null<T>>
-//
-// class PyPrimitiveConverter<
-// T, enable_if_t<is_boolean_type<T>::value || is_number_type<T>::value ||
-//   is_decimal_type<T>::value || is_date_type<T>::value ||
-//   is_time_type<T>::value>> : public PrimitiveConverter<T, PyConverter> {
-//
+
 // class PyPrimitiveConverter<
 //   T, enable_if_t<is_boolean_type<T>::value || is_number_type<T>::value ||
 //   is_decimal_type<T>::value || is_date_type<T>::value ||
 //   is_time_type<T>::value>> : public PrimitiveConverter<T, PyConverter> {
-//
-// class PyPrimitiveConverter<T, enable_if_binary<T>>
-//   : public PrimitiveConverter<T, PyConverter> {
-//
-// class PyPrimitiveConverter<T, enable_if_t<std::is_same<T, FixedSizeBinaryType>::value>>
-//   :  public PrimitiveConverter<T, PyConverter> {
-//
-// class PyPrimitiveConverter<T, enable_if_string_like<T>>
-//   : public PrimitiveConverter<T, PyConverter> {
 
-template <typename T, typename Enable = void>
-class RPrimitiveConverter : public PrimitiveConverter<T, RConverter> {
+template <typename T>
+class RPrimitiveConverter<T, enable_if_null<T>>
+    : public PrimitiveConverter<T, RConverter> {
+ public:
+  Status Append(RObject* value) override {
+    return this->primitive_builder_->AppendNull();
+  }
+};
+
+// Temporary (this only handles int32 for now)
+template <typename T>
+class RPrimitiveConverter<
+    T, enable_if_t<!std::is_same<T, Int32Type>::value &&
+                   (is_boolean_type<T>::value || is_number_type<T>::value ||
+                    is_decimal_type<T>::value || is_date_type<T>::value ||
+                    is_time_type<T>::value)>> : public PrimitiveConverter<T, RConverter> {
  public:
   Status Append(RObject* value) {
-    Rprintf("T = %s\n", arrow::util::nameof<T>().c_str());
+    return Status::NotImplemented("conversion to fixed size binary not yet implemented");
+  }
+};
+
+template <typename T>
+class RPrimitiveConverter<T, enable_if_t<std::is_same<T, Int32Type>::value>>
+    : public PrimitiveConverter<T, RConverter> {
+ public:
+  Status Append(RObject* value) {
+    if (RValue::IsNull(value)) {
+      return this->primitive_builder_->AppendNull();
+    } else {
+      ARROW_ASSIGN_OR_RAISE(
+          auto converted, RValue::Convert(this->primitive_type_, this->options_, value));
+      return this->primitive_builder_->Append(converted);
+    }
     return Status::OK();
+  }
+};
+
+template <typename T>
+class RPrimitiveConverter<T, enable_if_binary<T>>
+    : public PrimitiveConverter<T, RConverter> {
+ public:
+  Status Append(RObject* value) {
+    return Status::NotImplemented("conversion to binary not yet implemented");
+  }
+};
+
+template <typename T>
+class RPrimitiveConverter<T, enable_if_t<std::is_same<T, FixedSizeBinaryType>::value>>
+    : public PrimitiveConverter<T, RConverter> {
+ public:
+  Status Append(RObject* value) {
+    return Status::NotImplemented("conversion to fixed size binary not yet implemented");
+  }
+};
+
+template <typename T>
+class RPrimitiveConverter<T, enable_if_string_like<T>>
+    : public PrimitiveConverter<T, RConverter> {
+ public:
+  Status Append(RObject* value) {
+    return Status::NotImplemented("conversion to string not yet implemented");
+  }
+};
+
+template <typename T>
+class RPrimitiveConverter<
+    T, enable_if_t<is_timestamp_type<T>::value || is_duration_type<T>::value>>
+    : public PrimitiveConverter<T, RConverter> {
+ public:
+  Status Append(RObject* value) {
+    return Status::NotImplemented(
+        "conversion to timestamp or duration not yet implemented");
   }
 };
 
@@ -135,16 +289,14 @@ struct RConverterTrait<StructType> {
 };
 
 class RStructConverter : public StructConverter<RConverter, RConverterTrait> {
-public:
-  Status Append(RObject* value) override {
+ public:
+  Status Append(RObject* value) override { return Status::OK(); }
+
+ protected:
+  Status Init(MemoryPool* pool) override {
+    RETURN_NOT_OK((StructConverter<RConverter, RConverterTrait>::Init(pool)));
     return Status::OK();
   }
-
-protected:
-Status Init(MemoryPool* pool) override {
-  RETURN_NOT_OK((StructConverter<RConverter, RConverterTrait>::Init(pool)));
-  return Status::OK();
-}
 };
 
 template <>
@@ -170,8 +322,8 @@ std::shared_ptr<arrow::Array> vec_to_arrow(SEXP x, SEXP s_type) {
       options.type, options, gc_memory_pool()));
 
   StopIfNotOk(converter->Reserve(options.size));
-  // TODO: iterate and call Append on each value
-  // StopIfNotOk(converter->Append(x));
+  StopIfNotOk(VisitVector(x, options.size,
+                          [&converter](RObject* obj) { return converter->Append(obj); }));
   return ValueOrStop(converter->ToArray());
 }
 
