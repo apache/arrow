cc362a4f16fec63e115486f61b4d57eac3dd9392
Östman Alexander <alexander.ostman@sweco.se>
2020-11-17 23:55:09 +0100

    Added extension for ToListAsync.
    This was required to remove dependency from Grpc package.

---
+++ csharp/test/Apache.Arrow.Flight.TestWeb/Extensions/AsyncStreamExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grpc.Core.Utils
+{
+    public static class AsyncStreamExtensions
+    {
+        /// <summary>
+        /// Reads the entire stream and creates a list containing all the elements read.
+        /// </summary>
+        public static async Task<List<T>> ToListAsync<T>(this IAsyncStreamReader<T> streamReader)
+            where T : class
+        {
+            var result = new List<T>();
+            while (await streamReader.MoveNext().ConfigureAwait(false))
+            {
+                result.Add(streamReader.Current);
+            }
+            return result;
+        }
+    }
+}
