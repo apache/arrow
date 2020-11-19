b30beae90258c09b9ef6d4d62152bc33b2fa44f9
Projjal Chanda <iam@pchanda.com>
2020-11-19 19:08:54 +0530

    Make gandiva cache size configurable through environment variable

--- cpp/src/gandiva/cache.h
+++ cpp/src/gandiva/cache.h
@@ -17,6 +17,8 @@
 
 #pragma once
 
+#include <cstdlib>
+#include <iostream>
 #include <mutex>
 
 #include "gandiva/lru_cache.h"
@@ -26,7 +28,12 @@ namespace gandiva {
 template <class KeyType, typename ValueType>
 class Cache {
  public:
-  explicit Cache(size_t capacity = CACHE_SIZE) : cache_(capacity) {}
+  explicit Cache(size_t capacity) : cache_(capacity) {
+    std::cout << "Creating gandiva cache with capacity: " << capacity << std::endl;
+  }
+
+  Cache() : Cache(GetCapacity()) {}
+
   ValueType GetModule(KeyType cache_key) {
     arrow::util::optional<ValueType> result;
     mtx_.lock();
@@ -42,8 +49,24 @@ class Cache {
   }
 
  private:
+  static int GetCapacity() {
+    int capacity;
+    const char* env_cache_size = std::getenv("GANDIVA_CACHE_SIZE");
+    if (env_cache_size != nullptr) {
+      capacity = std::atoi(env_cache_size);
+      if (capacity <= 0) {
+        std::cout << "Invalid cache size provided. Using default cache size."
+                  << std::endl;
+        capacity = DEFAULT_CACHE_SIZE;
+      }
+    } else {
+      capacity = DEFAULT_CACHE_SIZE;
+    }
+    return capacity;
+  }
+
   LruCache<KeyType, ValueType> cache_;
-  static const int CACHE_SIZE = 250;
+  static const int DEFAULT_CACHE_SIZE = 500;
   std::mutex mtx_;
 };
 }  // namespace gandiva
