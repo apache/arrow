15313199fc7d3de74bacb833e7bad36ac5cd53ec
Projjal Chanda <iam@pchanda.com>
2020-11-23 12:29:32 +0530

    move condition from list writer writer to list vector

--- java/vector/src/main/codegen/templates/UnionListWriter.java
+++ java/vector/src/main/codegen/templates/UnionListWriter.java
@@ -178,9 +178,6 @@ public class Union${listName}Writer extends AbstractFieldWriter {
   <#if listName == "LargeList">
   @Override
   public void startList() {
-    if (vector.getLastSet() >= idx()){
-      vector.setLastSet(idx() - 1);
-    }
     vector.startNewValue(idx());
     writer.setPosition(checkedCastToInt(vector.getOffsetBuffer().getLong(((long) idx() + 1L) * OFFSET_WIDTH)));
   }
@@ -193,9 +190,6 @@ public class Union${listName}Writer extends AbstractFieldWriter {
   <#else>
   @Override
   public void startList() {
-    if (vector.getLastSet() >= idx()){
-      vector.setLastSet(idx() - 1);
-    }
     vector.startNewValue(idx());
     writer.setPosition(vector.getOffsetBuffer().getInt((idx() + 1) * OFFSET_WIDTH));
   }
