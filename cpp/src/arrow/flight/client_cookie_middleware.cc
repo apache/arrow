182ff87a5bad4edb91d731c835dfada5955dbfde
Lyndon Bauto <lyndonb@bitquilltech.com>
2020-11-25 12:10:13 -0800

    [1] Fixed type casting warning generated so windows can be tested.

--- cpp/src/arrow/flight/client_cookie_middleware.cc
+++ cpp/src/arrow/flight/client_cookie_middleware.cc
@@ -129,13 +129,13 @@ struct Cookie {
       } else if (arrow::internal::AsciiEqualsCaseInsensitive(cookie_attr_name,
                                                              "expires")) {
         cookie.has_expiry_ = true;
-        uint64_t seconds = 0;
         const char* COOKIE_EXPIRES_FORMAT = "%a, %d %b %Y %H:%M:%S GMT";
+        int64_t seconds = 0;
         if (arrow::internal::ParseTimestampStrptime(
                 cookie_attr_value.c_str(), cookie_attr_value.size(),
                 COOKIE_EXPIRES_FORMAT, false, true, arrow::TimeUnit::SECOND, &seconds)) {
           cookie.expiration_time_ = std::chrono::time_point<std::chrono::system_clock>(
-              std::chrono::seconds(seconds));
+              std::chrono::seconds(static_cast<uint64_t>(seconds)));
         }
       }
     }
