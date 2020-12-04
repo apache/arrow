d855815e6d1b6aba237c6091d62de5bd14821348
Heres, Daniel <danielheres@gmail.com>
2020-12-04 12:33:07 +0100

    Avoid double hashing using raw_entry api

--- rust/datafusion/src/physical_plan/hash_join.rs
+++ rust/datafusion/src/physical_plan/hash_join.rs
@@ -214,12 +214,13 @@ fn update_hash(
     // update the hash map
     for row in 0..batch.num_rows() {
         create_key(&keys_values, row, &mut key)?;
-        match hash.get_mut(&key) {
-            Some(v) => v.push((index, row)),
-            None => {
-                hash.insert(key.clone(), vec![(index, row)]);
-            }
-        };
+
+        hash
+        .raw_entry_mut()
+        .from_key(&key)
+        .and_modify(|_, v| v.push((index, row)))
+        .or_insert_with(|| (key.clone(),  vec![(index, row)]));
+
     }
     Ok(())
 }
