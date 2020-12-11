9c2828c2dc2c8168c0f521b0306b7f3c16f6bfc7
Micah Kornfield <micahk@google.com>
2020-12-10 22:40:26 -0800

    fix tests for real

--- cpp/src/arrow/util/decimal_test.cc
+++ cpp/src/arrow/util/decimal_test.cc
@@ -1372,6 +1372,44 @@ TEST(Decimal256Test, Add) {
           << " x: " << x << " y: " << y;
     }
   }
+}
+
+TEST(Decimal256Test, Divide) {
+  ASSERT_EQ(Decimal256(33), Decimal256(100) / Decimal256(3));
+  ASSERT_EQ(Decimal256(66), Decimal256(200) / Decimal256(3));
+  ASSERT_EQ(Decimal256(66), Decimal256(20100) / Decimal256(301));
+  ASSERT_EQ(Decimal256(-66), Decimal256(-20100) / Decimal256(301));
+  ASSERT_EQ(Decimal256(-66), Decimal256(20100) / Decimal256(-301));
+  ASSERT_EQ(Decimal256(66), Decimal256(-20100) / Decimal256(-301));
+  ASSERT_EQ(Decimal256("-5192296858534827628530496329343552"),
+            Decimal256("-269599466671506397946670150910580797473777870509761363"
+                       "24636208709184") /
+                Decimal256("5192296858534827628530496329874417"));
+  ASSERT_EQ(Decimal256("5192296858534827628530496329343552"),
+            Decimal256("-269599466671506397946670150910580797473777870509761363"
+                       "24636208709184") /
+                Decimal256("-5192296858534827628530496329874417"));
+  ASSERT_EQ(Decimal256("5192296858534827628530496329343552"),
+            Decimal256("2695994666715063979466701509105807974737778705097613632"
+                       "4636208709184") /
+                Decimal256("5192296858534827628530496329874417"));
+  ASSERT_EQ(Decimal256("-5192296858534827628530496329343552"),
+            Decimal256("2695994666715063979466701509105807974737778705097613632"
+                       "4636208709184") /
+                Decimal256("-5192296858534827628530496329874417"));
+
+  // Test some random numbers.
+  for (auto x : GetRandomNumbers<Int32Type>(16)) {
+    for (auto y : GetRandomNumbers<Int32Type>(16)) {
+      if (y == 0) {
+        continue;
+      }
+
+      Decimal256 result = Decimal256(x) / Decimal256(y);
+      ASSERT_EQ(Decimal256(static_cast<int64_t>(x) / y), result)
+          << " x: " << x << " y: " << y;
+    }
+  }
 
   // Test some edge cases
   for (auto x :
@@ -1381,44 +1419,10 @@ TEST(Decimal256Test, Add) {
                                         -1, 1, 2, 32, INT32_MAX, INT64_MAX}) {
       Decimal256 decimal_x = Decimal256FromInt128(x);
       Decimal256 decimal_y = Decimal256FromInt128(y);
-      Decimal256 result = decimal_x + decimal_y;
-      EXPECT_EQ(Decimal256FromInt128(x + y), result)
+      Decimal256 result = decimal_x / decimal_y;
+      EXPECT_EQ(Decimal256FromInt128(x / y), result)
           << " x: " << decimal_x.ToIntegerString()
           << " y: " << decimal_y.ToIntegerString();
-    }
-  }
-}
-
-TEST(Decimal128Test, Divide) {
-  ASSERT_EQ(Decimal128(66), Decimal128(20100) / Decimal128(301));
-
-  ASSERT_EQ(Decimal128(-66), Decimal128(-20100) / Decimal128(301));
-
-  ASSERT_EQ(Decimal128(-66), Decimal128(20100) / Decimal128(-301));
-
-  ASSERT_EQ(Decimal128(66), Decimal128(-20100) / Decimal128(-301));
-
-  // Test some random numbers.
-  for (auto x : GetRandomNumbers<Int32Type>(16)) {
-    for (auto y : GetRandomNumbers<Int32Type>(16)) {
-      if (y == 0) {
-        continue;
-      }
-
-      Decimal128 result = Decimal128(x) / Decimal128(y);
-      ASSERT_EQ(Decimal128(static_cast<int64_t>(x) / y), result)
-          << " x: " << x << " y: " << y;
-    }
-  }
-
-  // Test some edge cases
-  for (auto x : std::vector<int128_t>{-INT64_MAX, -INT32_MAX, 0, INT32_MAX, INT64_MAX}) {
-    for (auto y : std::vector<int128_t>{-INT32_MAX, -32, -2, -1, 1, 2, 32, INT32_MAX}) {
-      Decimal128 decimal_x = Decimal128FromInt128(x);
-      Decimal128 decimal_y = Decimal128FromInt128(y);
-      Decimal128 result = decimal_x / decimal_y;
-      EXPECT_EQ(Decimal128FromInt128(x / y), result)
-          << " x: " << decimal_x << " y: " << decimal_y;
     }
   }
 }
