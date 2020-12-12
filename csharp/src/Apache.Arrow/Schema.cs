6e7c9c4201096a8c8ae234fe3d40a49b60dc7df2
Eric Erhardt <eric.erhardt@microsoft.com>
2020-12-12 16:21:25 -0600

    Add support for reading and writing Schema and Field metadata
    Also, reducing some allocations in a few spots.

--- csharp/src/Apache.Arrow/Schema.cs
+++ csharp/src/Apache.Arrow/Schema.cs
@@ -15,6 +15,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Apache.Arrow
@@ -51,6 +52,20 @@ namespace Apache.Arrow
                 StringComparer.OrdinalIgnoreCase);
 
             Metadata = metadata?.ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        internal Schema(List<Field> fields, IReadOnlyDictionary<string, string> metadata, bool copyCollections)
+        {
+            Debug.Assert(fields != null);
+            Debug.Assert(copyCollections == false, "This internal constructor is to not copy the collections.");
+
+            _fields = fields;
+
+            _fieldsDictionary = fields.ToDictionary(
+                field => field.Name, field => field,
+                StringComparer.OrdinalIgnoreCase);
+
+            Metadata = metadata;
         }
 
         public Field GetFieldByIndex(int i)
