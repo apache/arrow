c84a166ed24278d5b4515da953dfef1f3dc65a0a
Neville Dipale <nevilledips@gmail.com>
2020-12-13 02:51:41 +0200

    Save progress
    List definition algo still has some quirks.
    Masks and OOB panics.
    Ported list write code

--- rust/parquet/src/arrow/arrow_writer.rs
+++ rust/parquet/src/arrow/arrow_writer.rs
@@ -486,7 +486,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "ARROW-10766: list support is incomplete"]
     fn arrow_writer_list() {
         // define schema
         let schema = Schema::new(vec![Field::new(
@@ -586,7 +585,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "ARROW-10766: list support is incomplete"]
     fn arrow_writer_complex() {
         // define schema
         let struct_field_d = Field::new("d", DataType::Float64, true);
@@ -1092,7 +1090,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "ARROW-10766: list support is incomplete"]
     fn list_single_column() {
         let a_values = Int32Array::from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         let a_value_offsets =
@@ -1117,7 +1114,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "ARROW-10766: list support is incomplete"]
     fn large_list_single_column() {
         let a_values = Int32Array::from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         let a_value_offsets =
