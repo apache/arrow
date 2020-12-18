4893b43244c0fd4c898075413cbe4ec33d6966dc
mqy <meng.qingyou@gmail.com>
2020-12-18 10:22:56 +0800

    Move the hashbang to first line

--- rust/pre-commit.sh
+++ rust/pre-commit.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,8 +16,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-#!/bin/sh
 
 # This file is git pre-commit hook.
 # Copy or soft link it as file .git/hooks/pre-commit.
