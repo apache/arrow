640df6cce4f0ea61898e4c32b3d2da82eceecf2f
Elena Henderson <elena@Elenas-MacBook-Pro.local>
2020-12-30 18:47:34 -0800

    WIP

--- .buildkite/scripts/build_docker_container.sh
+++ .buildkite/scripts/build_docker_container.sh
@@ -25,4 +25,4 @@ cd ..
 chmod -R a+rwx .
 
 # Build docker image and container
-archery docker run -v "$(pwd)"/.:/arrow -v "$(pwd)"/workspace:/workspace --no-pull ubuntu-python /arrow/.buildkite/scripts/run_benchmarks
+archery docker run -v "$(pwd)"/.:/arrow -v "$(pwd)"/workspace:/workspace --no-pull ubuntu-python /arrow/.buildkite/scripts/run_benchmarks.sh
