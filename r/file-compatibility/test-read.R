83d2c071dfcde1211d28bc50c875f53a41878345
Jonathan Keane <jkeane@gmail.com>
2021-01-06 11:39:17 -0600

    What happens in 1.0.1?

--- r/file-compatibility/test-read.R
+++ r/file-compatibility/test-read.R
@@ -1,16 +1,24 @@
 library(arrow)
 library(testthat)
 
-skip_if_version <- function(version, msg, op = `<`) {
-  if (op(version, numeric_version(Sys.getenv("OLD_ARROW_VERSION", "0.0.0")))) {
+skip_if_version <- function(version, msg, op = `<=`) {
+  if (op(numeric_version(Sys.getenv("OLD_ARROW_VERSION", "0.0.0")), version)) {
     skip(msg)
   }
 }
 
 pq_file <- "files/ex_data.parquet"
 
+test_that("Can sread the file", {
+  skip_if_version("1.0.1", "Version 1.0.1 can't read new version metadata.")
+
+  df <- read_parquet(pq_file)
+  print(str(df))
+  expect_true(TRUE)
+})
+
 test_that("Can see the metadata", {
-  skip_if_version("2.0.0", "Version 1.0.1 can't read new version metadata.")
+  skip_if_version("1.0.1", "Version 1.0.1 can't read new version metadata.")
 
   df <- read_parquet(pq_file)
   expect_s3_class(df, "tbl")
