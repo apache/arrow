f0ce0f185c010641f89191742f022b83dbc94025
Jonathan Keane <jkeane@gmail.com>
2021-01-05 18:57:10 -0600

    Trying on GHA

---
+++ r/file-compatibility/write.R
@@ -0,0 +1,14 @@
+library(arrow)
+
+example_with_metadata <- tibble::tibble(
+  a = structure("one", class = "special_string"),
+  b = 2,
+  c = tibble::tibble(
+    c1 = structure("inner", extra_attr = "something"),
+    c2 = 4,
+    c3 = 50
+  ),
+  d = "four"
+)
+
+write_parquet(example_with_metadata, "file-compatibility/files/ex_data.parquet")
