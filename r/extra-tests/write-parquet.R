5cd59d5592ce76fb6ce78abdb8fcce1407aa7e04
Jonathan Keane <jkeane@gmail.com>
2021-01-06 19:22:58 -0600

    Escape jinja

--- r/extra-tests/write-parquet.R
+++ r/extra-tests/write-parquet.R
@@ -4,20 +4,6 @@ if (!dir.exists("extra-tests/files")) {
   dir.create("extra-tests/files")
 }
 
-example_with_metadata <- tibble::tibble(
-  a = structure("one", class = "special_string"),
-  b = 2,
-  c = tibble::tibble(
-    c1 = structure("inner", extra_attr = "something"),
-    c2 = 4,
-    c3 = 50
-  ),
-  d = "four"
-)
-
-attr(example_with_metadata, "top_level") <- list(
-  field_one = 12,
-  field_two = "more stuff"
-)
+source("tests/testthat/helper-data.R")
 
 write_parquet(example_with_metadata, "extra-tests/files/ex_data.parquet")
