77aa40ef472eb3bc4bb6c55cfa3b054d1e7e6776
Andrew Lamb <andrew@nerdnetworks.org>
2021-01-07 12:54:09 -0500

    improve comments

--- rust/arrow/src/compute/kernels/sort.rs
+++ rust/arrow/src/compute/kernels/sort.rs
@@ -41,7 +41,7 @@ pub fn sort(values: &ArrayRef, options: Option<SortOptions>) -> Result<ArrayRef>
     take(values.as_ref(), &indices, None)
 }
 
-// sorts f32 in IEEE 754 total ordering
+// implements comparison using IEEE 754 total ordering for f32
 // Original implementation from https://doc.rust-lang.org/std/primitive.f64.html#method.total_cmp
 // TODO to change to use std when it becomes stable
 fn total_cmp_32(l: f32, r: f32) -> std::cmp::Ordering {
@@ -54,7 +54,7 @@ fn total_cmp_32(l: f32, r: f32) -> std::cmp::Ordering {
     left.cmp(&right)
 }
 
-// sorts f64 in IEEE 754 total ordering
+// implements comparison using IEEE 754 total ordering for f64
 // Original implementation from https://doc.rust-lang.org/std/primitive.f64.html#method.total_cmp
 // TODO to change to use std when it becomes stable
 fn total_cmp_64(l: f64, r: f64) -> std::cmp::Ordering {
