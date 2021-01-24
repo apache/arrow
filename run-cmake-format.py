73a237489fdaa2671fb5b967b5b475a28656afb4
Marco Gorelli <marcogorelli@protonmail.com>
2021-01-24 09:30:33 +0000

    fixup pre-commit cmake-format hook

--- run-cmake-format.py
+++ run-cmake-format.py
@@ -17,62 +17,30 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import argparse
 import hashlib
+import fnmatch
 import pathlib
 import subprocess
 import sys
 
-
-patterns = [
-    'cpp/CMakeLists.txt',
-    # Keep an explicit list of files to format as we don't want to reformat
-    # files we imported from other location.
-    'cpp/cmake_modules/BuildUtils.cmake',
-    'cpp/cmake_modules/DefineOptions.cmake',
-    'cpp/cmake_modules/FindArrow.cmake',
-    'cpp/cmake_modules/FindArrowCUDA.cmake',
-    'cpp/cmake_modules/FindArrowDataset.cmake',
-    'cpp/cmake_modules/FindArrowFlight.cmake',
-    'cpp/cmake_modules/FindArrowFlightTesting.cmake',
-    'cpp/cmake_modules/FindArrowPython.cmake',
-    'cpp/cmake_modules/FindArrowPythonFlight.cmake',
-    'cpp/cmake_modules/FindArrowTesting.cmake',
-    'cpp/cmake_modules/FindBrotli.cmake',
-    'cpp/cmake_modules/FindClangTools.cmake',
-    'cpp/cmake_modules/FindFlatbuffersAlt.cmake',
-    'cpp/cmake_modules/FindGLOG.cmake',
-    'cpp/cmake_modules/FindGandiva.cmake',
-    'cpp/cmake_modules/FindInferTools.cmake',
-    'cpp/cmake_modules/FindLLVMAlt.cmake',
-    'cpp/cmake_modules/FindLz4.cmake',
-    'cpp/cmake_modules/FindParquet.cmake',
-    'cpp/cmake_modules/FindPlasma.cmake',
-    'cpp/cmake_modules/FindPython3Alt.cmake',
-    'cpp/cmake_modules/FindRE2.cmake',
-    'cpp/cmake_modules/FindRapidJSONAlt.cmake',
-    'cpp/cmake_modules/FindSnappyAlt.cmake',
-    'cpp/cmake_modules/FindThrift.cmake',
-    'cpp/cmake_modules/FindZSTD.cmake',
-    'cpp/cmake_modules/Findc-aresAlt.cmake',
-    'cpp/cmake_modules/FindgRPCAlt.cmake',
-    'cpp/cmake_modules/FindgflagsAlt.cmake',
-    'cpp/cmake_modules/Findjemalloc.cmake',
-    'cpp/cmake_modules/SetupCxxFlags.cmake',
-    'cpp/cmake_modules/ThirdpartyToolchain.cmake',
-    'cpp/cmake_modules/san-config.cmake',
+# Keep an explicit list of files to format as we don't want to reformat
+# files we imported from other location.
+PATTERNS = [
+    'cpp/cmake_modules/*/.cmake',
+    '*CMakeLists.txt',
+]
+EXCLUDE = [
+    'cpp/cmake_modules/FindNumPy.cmake',
+    'cpp/cmake_modules/FindPythonLibsNew.cmake',
     'cpp/cmake_modules/UseCython.cmake',
-    'cpp/cmake_modules/Usevcpkg.cmake',
-    'cpp/src/**/CMakeLists.txt',
-    'cpp/tools/**/CMakeLists.txt',
-    'java/gandiva/CMakeLists.txt',
-    'python/CMakeLists.txt',
 ]
 
 here = pathlib.Path(__file__).parent
 
 
 def find_cmake_files():
-    for pat in patterns:
+    for pat in PATTERNS:
         yield from here.glob(pat)
 
 
@@ -119,8 +87,21 @@ def check_cmake_format(paths):
 
 
 if __name__ == "__main__":
-    paths = list(find_cmake_files())
-    if "--check" in sys.argv:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--check', action='store_true')
+    parser.add_argument('paths', nargs='*', type=pathlib.Path)
+    args = parser.parse_args()
+
+    if not args.paths:
+        paths = find_cmake_files()
+    else:
+        paths = args.paths
+    paths = [
+        i for i in paths
+        if any(fnmatch.fnmatch(i.as_posix(), pattern) for pattern in PATTERNS)
+        and i.as_posix() not in EXCLUDE
+    ]
+    if args.check:
         check_cmake_format(paths)
     else:
         run_cmake_format(paths)
