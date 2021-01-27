dded02c9ce74a77db39dadba1fc11ea3f54178ee
Qingping Hou <dave2008713@gmail.com>
2021-01-26 21:36:24 -0800

    add expression tess

--- rust/datafusion/src/optimizer/boolean_comparison.rs
+++ rust/datafusion/src/optimizer/boolean_comparison.rs
@@ -170,6 +170,100 @@ mod tests {
     use crate::logical_plan::{col, lit, LogicalPlanBuilder};
     use crate::test::*;
 
+    #[test]
+    fn optimize_expr_eq() -> Result<()> {
+        assert_eq!(
+            optimize_expr(&Expr::BinaryExpr {
+                left: Box::new(lit(1)),
+                op: Operator::Eq,
+                right: Box::new(lit(true)),
+            }),
+            lit(1),
+        );
+
+        assert_eq!(
+            optimize_expr(&Expr::BinaryExpr {
+                left: Box::new(lit("a")),
+                op: Operator::Eq,
+                right: Box::new(lit(false)),
+            }),
+            lit("a").not(),
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn optimize_expr_not_eq() -> Result<()> {
+        assert_eq!(
+            optimize_expr(&Expr::BinaryExpr {
+                left: Box::new(lit(1)),
+                op: Operator::NotEq,
+                right: Box::new(lit(true)),
+            }),
+            lit(1).not(),
+        );
+
+        assert_eq!(
+            optimize_expr(&Expr::BinaryExpr {
+                left: Box::new(lit("a")),
+                op: Operator::NotEq,
+                right: Box::new(lit(false)),
+            }),
+            lit("a"),
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn optimize_expr_not_not_eq() -> Result<()> {
+        assert_eq!(
+            optimize_expr(&Expr::Not(Box::new(Expr::BinaryExpr {
+                left: Box::new(lit(1)),
+                op: Operator::NotEq,
+                right: Box::new(lit(true)),
+            }))),
+            lit(1).not().not(),
+        );
+
+        assert_eq!(
+            optimize_expr(&Expr::Not(Box::new(Expr::BinaryExpr {
+                left: Box::new(lit("a")),
+                op: Operator::NotEq,
+                right: Box::new(lit(false)),
+            }))),
+            lit("a").not(),
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn optimize_expr_case_when_then_else() -> Result<()> {
+        assert_eq!(
+            optimize_expr(&Box::new(Expr::Case {
+                expr: None,
+                when_then_expr: vec![(
+                    Box::new(Expr::BinaryExpr {
+                        left: Box::new(lit("a")),
+                        op: Operator::NotEq,
+                        right: Box::new(lit(false)),
+                    }),
+                    Box::new(lit("ok")),
+                )],
+                else_expr: Some(Box::new(lit("not ok"))),
+            })),
+            Expr::Case {
+                expr: None,
+                when_then_expr: vec![(Box::new(lit("a")), Box::new(lit("ok")))],
+                else_expr: Some(Box::new(lit("not ok"))),
+            }
+        );
+
+        Ok(())
+    }
+
     fn assert_optimized_plan_eq(plan: &LogicalPlan, expected: &str) {
         let mut rule = BooleanComparison::new();
         let optimized_plan = rule.optimize(plan).expect("failed to optimize plan");
@@ -178,7 +272,7 @@ mod tests {
     }
 
     #[test]
-    fn simplify_eq_expr() -> Result<()> {
+    fn optimize_plan_eq_expr() -> Result<()> {
         let table_scan = test_table_scan()?;
         let plan = LogicalPlanBuilder::from(&table_scan)
             .filter(col("a").eq(lit(true)))?
@@ -197,7 +291,7 @@ mod tests {
     }
 
     #[test]
-    fn simplify_not_eq_expr() -> Result<()> {
+    fn optimize_plan_not_eq_expr() -> Result<()> {
         let table_scan = test_table_scan()?;
         let plan = LogicalPlanBuilder::from(&table_scan)
             .filter(col("a").not_eq(lit(true)))?
@@ -218,7 +312,7 @@ mod tests {
     }
 
     #[test]
-    fn simplify_and_expr() -> Result<()> {
+    fn optimize_plan_and_expr() -> Result<()> {
         let table_scan = test_table_scan()?;
         let plan = LogicalPlanBuilder::from(&table_scan)
             .filter(col("a").not_eq(lit(true)).and(col("b").eq(lit(true))))?
@@ -235,7 +329,7 @@ mod tests {
     }
 
     #[test]
-    fn simplify_or_expr() -> Result<()> {
+    fn optimize_plan_or_expr() -> Result<()> {
         let table_scan = test_table_scan()?;
         let plan = LogicalPlanBuilder::from(&table_scan)
             .filter(col("a").not_eq(lit(true)).or(col("b").eq(lit(false))))?
@@ -252,7 +346,7 @@ mod tests {
     }
 
     #[test]
-    fn simplify_not_expr() -> Result<()> {
+    fn optimize_plan_not_expr() -> Result<()> {
         let table_scan = test_table_scan()?;
         let plan = LogicalPlanBuilder::from(&table_scan)
             .filter(col("a").eq(lit(false)).not())?
