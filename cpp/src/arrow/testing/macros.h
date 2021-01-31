484581e908ae84a2eb1776d9924cba8c439a208a
Micah Kornfield <emkornfield@gmail.com>
2021-01-31 00:54:12 +0000

    format and try to fix windows with msvc specific pragma

--- cpp/src/arrow/testing/macros.h
+++ cpp/src/arrow/testing/macros.h
@@ -2,16 +2,12 @@
 
 #ifdef __GNUC__
 #define SUPPRESS_DEPRECATION_WARNING \
-  _Pragma("GCC diagnostic push") \
+  _Pragma("GCC diagnostic push");    \
   _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
 #define UNSUPPRESS_DEPRECATION_WARNING _Pragma("GCC diagnostic pop")
 #elif defined(_MSC_VER)
 #define SUPPRESS_DEPRECATION_WARNING \
-   _Pragma("warning(push)") \
-   _Pragma("warning(disable : 4996)") 
-#define UNSUPPRESS_DEPRECATION_WARNING _Pragma("warning(pop)")
+  __pragma("warning(push)");         \
+  _pragma("warning(disable : 4996)")
+#define UNSUPPRESS_DEPRECATION_WARNING __pragma("warning(pop)")
 #endif
-
-
-
-
