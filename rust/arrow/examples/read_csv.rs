6e750e1eb83be92837d61fee7251ab26a0aaeec4
Heres, Daniel <danielheres@gmail.com>
2021-02-13 17:45:45 +0100

    Spacing

--- rust/arrow/examples/read_csv.rs
+++ rust/arrow/examples/read_csv.rs
@@ -36,7 +36,7 @@ fn main() {
 
     let mut csv = csv::Reader::new(file, Arc::new(schema), false, None, 1024, None, None);
     let _batch = csv.next().unwrap().unwrap();
-    #[cfg(feature = "prett  yprint")]
+    #[cfg(feature = "prettyprint")]
     {
         print_batches(&[_batch]).unwrap();
     }
