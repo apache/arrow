1e573703d24ebc6a804998f5aa27f788793743df
Qingping Hou <dave2008713@gmail.com>
2021-02-13 00:14:38 -0800

    recursive into all logical plan nodes and expression types

--- rust/datafusion/src/optimizer/constant_folding.rs
+++ rust/datafusion/src/optimizer/constant_folding.rs
@@ -50,26 +50,30 @@ impl OptimizerRule for ConstantFolding {
     fn optimize(&self, plan: &LogicalPlan) -> Result<LogicalPlan> {
         match plan {
             LogicalPlan::Filter { predicate, input } => Ok(LogicalPlan::Filter {
-                predicate: optimize_expr(predicate, plan.schema())?,
+                predicate: optimize_expr(predicate, &plan.all_schemas())?,
                 input: Arc::new(self.optimize(input)?),
             }),
             // Rest: recurse into plan, apply optimization where possible
             LogicalPlan::Projection { .. }
             | LogicalPlan::Aggregate { .. }
-            | LogicalPlan::Limit { .. }
             | LogicalPlan::Repartition { .. }
             | LogicalPlan::CreateExternalTable { .. }
             | LogicalPlan::Extension { .. }
             | LogicalPlan::Sort { .. }
             | LogicalPlan::Explain { .. }
+            | LogicalPlan::Limit { .. }
             | LogicalPlan::Join { .. } => {
-                let expr = utils::expressions(plan);
-
                 // apply the optimization to all inputs of the plan
                 let inputs = utils::inputs(plan);
                 let new_inputs = inputs
                     .iter()
                     .map(|plan| self.optimize(plan))
+                    .collect::<Result<Vec<_>>>()?;
+
+                let schemas = plan.all_schemas();
+                let expr = utils::expressions(plan)
+                    .iter()
+                    .map(|e| optimize_expr(e, &schemas))
                     .collect::<Result<Vec<_>>>()?;
 
                 utils::from_plan(plan, &expr, &new_inputs)
@@ -85,12 +89,27 @@ impl OptimizerRule for ConstantFolding {
     }
 }
 
+fn is_boolean_type(expr: &Expr, schemas: &[&DFSchemaRef]) -> bool {
+    for schema in schemas {
+        match expr.get_type(schema) {
+            Ok(dt) if dt == DataType::Boolean => {
+                return true;
+            }
+            _ => {
+                continue;
+            }
+        }
+    }
+
+    false
+}
+
 /// Recursively transverses the logical plan.
-fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
+fn optimize_expr(e: &Expr, schemas: &[&DFSchemaRef]) -> Result<Expr> {
     Ok(match e {
         Expr::BinaryExpr { left, op, right } => {
-            let left = optimize_expr(left, schema)?;
-            let right = optimize_expr(right, schema)?;
+            let left = optimize_expr(left, schemas)?;
+            let right = optimize_expr(right, schemas)?;
             match op {
                 Operator::Eq => match (&left, &right) {
                     (
@@ -103,7 +122,7 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
                         _ => Expr::Literal(ScalarValue::Boolean(None)),
                     },
                     (Expr::Literal(ScalarValue::Boolean(b)), _)
-                        if right.get_type(schema)? == DataType::Boolean =>
+                        if is_boolean_type(&right, schemas) =>
                     {
                         match b {
                             Some(true) => right,
@@ -112,7 +131,7 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
                         }
                     }
                     (_, Expr::Literal(ScalarValue::Boolean(b)))
-                        if left.get_type(schema)? == DataType::Boolean =>
+                        if is_boolean_type(&left, schemas) =>
                     {
                         match b {
                             Some(true) => left,
@@ -137,7 +156,7 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
                         _ => Expr::Literal(ScalarValue::Boolean(None)),
                     },
                     (Expr::Literal(ScalarValue::Boolean(b)), _)
-                        if right.get_type(schema)? == DataType::Boolean =>
+                        if is_boolean_type(&right, schemas) =>
                     {
                         match b {
                             Some(true) => Expr::Not(Box::new(right)),
@@ -146,7 +165,7 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
                         }
                     }
                     (_, Expr::Literal(ScalarValue::Boolean(b)))
-                        if left.get_type(schema)? == DataType::Boolean =>
+                        if is_boolean_type(&left, schemas) =>
                     {
                         match b {
                             Some(true) => Expr::Not(Box::new(left)),
@@ -168,8 +187,8 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
             }
         }
         Expr::Not(expr) => match &**expr {
-            Expr::Not(inner) => optimize_expr(&inner, schema)?,
-            _ => Expr::Not(Box::new(optimize_expr(&expr, schema)?)),
+            Expr::Not(inner) => optimize_expr(&inner, schemas)?,
+            _ => Expr::Not(Box::new(optimize_expr(&expr, schemas)?)),
         },
         Expr::Case {
             expr,
@@ -184,13 +203,13 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
                         .iter()
                         .map(|(when, then)| {
                             Ok((
-                                Box::new(optimize_expr(when, schema)?),
-                                Box::new(optimize_expr(then, schema)?),
+                                Box::new(optimize_expr(when, schemas)?),
+                                Box::new(optimize_expr(then, schemas)?),
                             ))
                         })
                         .collect::<Result<_>>()?,
                     else_expr: match else_expr {
-                        Some(e) => Some(Box::new(optimize_expr(e, schema)?)),
+                        Some(e) => Some(Box::new(optimize_expr(e, schemas)?)),
                         None => None,
                     },
                 }
@@ -200,21 +219,84 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
                 e.clone()
             }
         }
-        Expr::Alias { .. }
-        | Expr::Negative { .. }
-        | Expr::Column { .. }
-        | Expr::InList { .. }
-        | Expr::IsNotNull { .. }
-        | Expr::IsNull { .. }
-        | Expr::Cast { .. }
+        Expr::Alias(expr, name) => {
+            Expr::Alias(Box::new(optimize_expr(expr, schemas)?), name.clone())
+        }
+        Expr::Negative(expr) => Expr::Negative(Box::new(optimize_expr(expr, schemas)?)),
+        Expr::InList {
+            expr,
+            list,
+            negated,
+        } => Expr::InList {
+            expr: Box::new(optimize_expr(expr, schemas)?),
+            list: list
+                .iter()
+                .map(|e| optimize_expr(e, schemas))
+                .collect::<Result<_>>()?,
+            negated: *negated,
+        },
+        Expr::IsNotNull(expr) => Expr::IsNotNull(Box::new(optimize_expr(expr, schemas)?)),
+        Expr::IsNull(expr) => Expr::IsNull(Box::new(optimize_expr(expr, schemas)?)),
+        Expr::Cast { expr, data_type } => Expr::Cast {
+            expr: Box::new(optimize_expr(expr, schemas)?),
+            data_type: data_type.clone(),
+        },
+        Expr::Between {
+            expr,
+            negated,
+            low,
+            high,
+        } => Expr::Between {
+            expr: Box::new(optimize_expr(expr, schemas)?),
+            negated: *negated,
+            low: Box::new(optimize_expr(low, schemas)?),
+            high: Box::new(optimize_expr(high, schemas)?),
+        },
+        Expr::ScalarFunction { fun, args } => Expr::ScalarFunction {
+            fun: fun.clone(),
+            args: args
+                .iter()
+                .map(|e| optimize_expr(e, schemas))
+                .collect::<Result<_>>()?,
+        },
+        Expr::ScalarUDF { fun, args } => Expr::ScalarUDF {
+            fun: fun.clone(),
+            args: args
+                .iter()
+                .map(|e| optimize_expr(e, schemas))
+                .collect::<Result<_>>()?,
+        },
+        Expr::AggregateFunction {
+            fun,
+            args,
+            distinct,
+        } => Expr::AggregateFunction {
+            fun: fun.clone(),
+            args: args
+                .iter()
+                .map(|e| optimize_expr(e, schemas))
+                .collect::<Result<_>>()?,
+            distinct: *distinct,
+        },
+        Expr::AggregateUDF { fun, args } => Expr::AggregateUDF {
+            fun: fun.clone(),
+            args: args
+                .iter()
+                .map(|e| optimize_expr(e, schemas))
+                .collect::<Result<_>>()?,
+        },
+        Expr::Sort {
+            expr,
+            asc,
+            nulls_first,
+        } => Expr::Sort {
+            expr: Box::new(optimize_expr(expr, schemas)?),
+            asc: *asc,
+            nulls_first: *nulls_first,
+        },
+        Expr::Column { .. }
         | Expr::ScalarVariable { .. }
-        | Expr::Between { .. }
         | Expr::Literal { .. }
-        | Expr::ScalarFunction { .. }
-        | Expr::ScalarUDF { .. }
-        | Expr::AggregateFunction { .. }
-        | Expr::AggregateUDF { .. }
-        | Expr::Sort { .. }
         | Expr::Wildcard => e.clone(),
     })
 }
@@ -222,7 +304,9 @@ fn optimize_expr(e: &Expr, schema: &DFSchemaRef) -> Result<Expr> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::logical_plan::{col, lit, DFField, DFSchema, LogicalPlanBuilder};
+    use crate::logical_plan::{
+        col, lit, max, min, DFField, DFSchema, LogicalPlanBuilder,
+    };
 
     use arrow::datatypes::*;
 
@@ -254,7 +338,7 @@ mod tests {
                 &Expr::Not(Box::new(Expr::Not(Box::new(Expr::Not(Box::new(col(
                     "c2"
                 ))))))),
-                &schema
+                &[&schema],
             )?,
             col("c2").not(),
         );
@@ -273,7 +357,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(Expr::Literal(ScalarValue::Boolean(None))),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::Literal(ScalarValue::Boolean(None)),
         );
@@ -285,7 +369,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(Expr::Literal(ScalarValue::Boolean(None))),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::Literal(ScalarValue::Boolean(None)),
         );
@@ -297,7 +381,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(Expr::Literal(ScalarValue::Boolean(None))),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::Literal(ScalarValue::Boolean(None)),
         );
@@ -309,7 +393,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(col("c2")),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::Literal(ScalarValue::Boolean(None)),
         );
@@ -329,7 +413,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(true)),
                 },
-                &schema
+                &[&schema],
             )?,
             lit(true),
         );
@@ -341,7 +425,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(false)),
                 },
-                &schema
+                &[&schema],
             )?,
             lit(false),
         );
@@ -353,7 +437,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(true)),
                 },
-                &schema
+                &[&schema],
             )?,
             col("c2"),
         );
@@ -365,7 +449,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(false)),
                 },
-                &schema
+                &[&schema],
             )?,
             col("c2").not(),
         );
@@ -388,7 +472,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(true)),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::BinaryExpr {
                 left: Box::new(col("c1")),
@@ -404,7 +488,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(false)),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::BinaryExpr {
                 left: Box::new(col("c1")),
@@ -421,7 +505,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(true)),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::BinaryExpr {
                 left: Box::new(lit(1)),
@@ -437,7 +521,7 @@ mod tests {
                     op: Operator::Eq,
                     right: Box::new(lit(false)),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::BinaryExpr {
                 left: Box::new(lit("a")),
@@ -461,7 +545,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(true)),
                 },
-                &schema
+                &[&schema],
             )?,
             col("c2").not(),
         );
@@ -473,7 +557,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(false)),
                 },
-                &schema
+                &[&schema],
             )?,
             col("c2"),
         );
@@ -486,7 +570,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(true)),
                 },
-                &schema
+                &[&schema],
             )?,
             lit(false),
         );
@@ -498,7 +582,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(false)),
                 },
-                &schema
+                &[&schema],
             )?,
             lit(true),
         );
@@ -521,7 +605,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(true)),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::BinaryExpr {
                 left: Box::new(col("c1")),
@@ -537,7 +621,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(false)),
                 },
-                &schema
+                &[&schema],
             )?,
             Expr::BinaryExpr {
                 left: Box::new(col("c1")),
@@ -554,7 +638,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(true)),
                 })),
-                &schema
+                &[&schema],
             )?,
             Expr::Not(Box::new(Expr::BinaryExpr {
                 left: Box::new(lit(1)),
@@ -570,7 +654,7 @@ mod tests {
                     op: Operator::NotEq,
                     right: Box::new(lit(false)),
                 })),
-                &schema
+                &[&schema],
             )?,
             Expr::Not(Box::new(Expr::BinaryExpr {
                 left: Box::new(lit("a")),
@@ -600,7 +684,7 @@ mod tests {
                     )],
                     else_expr: Some(Box::new(col("c2").eq(lit(true)))),
                 }),
-                &schema
+                &[&schema],
             )?,
             Expr::Case {
                 expr: None,
@@ -707,6 +791,41 @@ mod tests {
         let expected = "\
         Projection: #a\
         \n  Filter: NOT NOT #b\
+        \n    TableScan: test projection=None";
+
+        assert_optimized_plan_eq(&plan, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn optimize_plan_support_projection() -> Result<()> {
+        let table_scan = test_table_scan()?;
+        let plan = LogicalPlanBuilder::from(&table_scan)
+            .project(&[col("a"), col("d"), col("b").eq(lit(false))])?
+            .build()?;
+
+        let expected = "\
+        Projection: #a, #d, NOT #b\
+        \n  TableScan: test projection=None";
+
+        assert_optimized_plan_eq(&plan, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn optimize_plan_support_aggregate() -> Result<()> {
+        let table_scan = test_table_scan()?;
+        let plan = LogicalPlanBuilder::from(&table_scan)
+            .project(&[col("a"), col("c"), col("b")])?
+            .aggregate(
+                &[col("a"), col("c")],
+                &[max(col("b").eq(lit(true))), min(col("b"))],
+            )?
+            .build()?;
+
+        let expected = "\
+        Aggregate: groupBy=[[#a, #c]], aggr=[[MAX(#b), MIN(#b)]]\
+        \n  Projection: #a, #c, #b\
         \n    TableScan: test projection=None";
 
         assert_optimized_plan_eq(&plan, expected);
