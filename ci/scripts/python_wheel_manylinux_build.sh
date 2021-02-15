a1950d6fdaf69e5f34523f8fc4523f2af8b0163f
Ian Cook <ianmcook@gmail.com>
2021-02-15 16:01:50 -0500

    Set cmake option _VCPKG_INSTALLED_DIR

--- ci/scripts/python_wheel_manylinux_build.sh
+++ ci/scripts/python_wheel_manylinux_build.sh
@@ -107,6 +107,7 @@ cmake \
     -DCMAKE_UNITY_BUILD=ON \
     -DOPENSSL_USE_STATIC_LIBS=ON \
     -DThrift_ROOT=/opt/vcpkg/installed/x64-linux/lib \
+    -D_VCPKG_INSTALLED_DIR=/opt/vcpkg/installed \
     -DVCPKG_MANIFEST_MODE=OFF \
     -DVCPKG_TARGET_TRIPLET=x64-linux-static-${CMAKE_BUILD_TYPE} \
     -G ${CMAKE_GENERATOR} \
