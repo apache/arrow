4153d0496d656ab571d78b8c502241c63d388e3d
Ian Cook <ianmcook@gmail.com>
2021-02-21 11:57:10 -0500

    Add message when VCPKG_MANIFEST_MODE set to ON

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -203,6 +203,7 @@ if(ARROW_DEPENDENCY_SOURCE STREQUAL "VCPKG")
   # explicitly set manifest mode on if it is not set and vcpkg.json exists
   if(NOT DEFINED VCPKG_MANIFEST_MODE AND EXISTS "${ARROW_SOURCE_DIR}/vcpkg.json")
     set(VCPKG_MANIFEST_MODE ON CACHE BOOL "Use vcpkg.json manifest")
+    message(STATUS "vcpkg.json manifest found. Using VCPKG_MANIFEST_MODE: ON")
   endif()
   # vcpkg can install packages in two different places
   set(_INST_ARROW_SOURCE_DIR "${ARROW_SOURCE_DIR}/vcpkg_installed") # try here first
