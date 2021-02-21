43f448f2fa5ec736ca3e5769daada62997b150fb
Ian Cook <ianmcook@gmail.com>
2021-02-21 14:39:56 -0500

    Move vcpkg code to separate cmake file and include earlier

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -96,187 +96,13 @@ if("${re2_SOURCE}" STREQUAL "" AND NOT "${RE2_SOURCE}" STREQUAL "")
   set(re2_SOURCE ${RE2_SOURCE})
 endif()
 
-# macro to list subdirectirectories (non-recursive)
-macro(list_subdirs SUBDIRS DIR)
-  file(
-    GLOB children_
-    RELATIVE ${DIR}
-    ${DIR}/*)
-  set(subdirs_ "")
-  foreach(child_ ${children_})
-    if(IS_DIRECTORY "${DIR}/${child_}")
-      list(APPEND subdirs_ ${child_})
-    endif()
-  endforeach()
-  set("${SUBDIRS}" ${subdirs_})
-  unset(children_)
-  unset(subdirs_)
-endmacro()
-
 message(STATUS "Using ${ARROW_DEPENDENCY_SOURCE} approach to find dependencies")
 
-if(ARROW_DEPENDENCY_SOURCE STREQUAL "VCPKG")
+# NOTE(ianmcook): the case when ARROW_DEPENDENCY_SOURCE=VCPKG is
+# handled separately in Findvcpkg.cmake, because that code must
+# run before the project() command in CMakeLists.txt
 
-  # Get VCPKG_ROOT
-  #
-  if(DEFINED CMAKE_TOOLCHAIN_FILE)
-    # Get it from the CMake variable CMAKE_TOOLCHAIN_FILE
-    get_filename_component(_VCPKG_DOT_CMAKE "${CMAKE_TOOLCHAIN_FILE}" NAME)
-    if(EXISTS "${CMAKE_TOOLCHAIN_FILE}" AND _VCPKG_DOT_CMAKE STREQUAL "vcpkg.cmake")
-      get_filename_component(_VCPKG_BUILDSYSTEMS_DIR "${CMAKE_TOOLCHAIN_FILE}" DIRECTORY)
-      get_filename_component(VCPKG_ROOT "${_VCPKG_BUILDSYSTEMS_DIR}/../.." ABSOLUTE)
-    else()
-      message(
-        FATAL_ERROR
-          "vcpkg toolchain file not found at path specified in -DCMAKE_TOOLCHAIN_FILE")
-    endif()
-  else()
-    if(DEFINED VCPKG_ROOT)
-      # Get it from the CMake variable VCPKG_ROOT
-      find_program(
-        _VCPKG_BIN vcpkg
-        PATHS "${VCPKG_ROOT}"
-        NO_DEFAULT_PATH)
-      if(NOT _VCPKG_BIN)
-        message(FATAL_ERROR "vcpkg not found in directory specified in -DVCPKG_ROOT")
-      endif()
-    elseif(DEFINED ENV{VCPKG_ROOT})
-      # Get it from the environment variable VCPKG_ROOT
-      set(VCPKG_ROOT ENV{VCPKG_ROOT})
-      find_program(
-        _VCPKG_BIN vcpkg
-        PATHS "${VCPKG_ROOT}"
-        NO_DEFAULT_PATH)
-      if(NOT _VCPKG_BIN)
-        message(
-          FATAL_ERROR "vcpkg not found in directory in environment variable VCPKG_ROOT")
-      endif()
-    else()
-      # Get it from the file vcpkg.path.txt
-      find_program(_VCPKG_BIN vcpkg)
-      if(_VCPKG_BIN)
-        get_filename_component(_VCPKG_REAL_BIN "${_VCPKG_BIN}" REALPATH)
-        get_filename_component(VCPKG_ROOT "${_VCPKG_REAL_BIN}" DIRECTORY)
-      else()
-        if(WIN32)
-          set(_VCPKG_PATH_TXT "$ENV{LOCALAPPDATA}/vcpkg/vcpkg.path.txt")
-        else()
-          set(_VCPKG_PATH_TXT "$ENV{HOME}/.vcpkg/vcpkg.path.txt")
-        endif()
-        if(EXISTS "${_VCPKG_PATH_TXT}")
-          file(STRINGS "${_VCPKG_PATH_TXT}" VCPKG_ROOT)
-        else()
-          message(
-            FATAL_ERROR
-              "vcpkg not found. Install vcpkg if not installed, "
-              "then run vcpkg integrate install "
-              "or set environment variable VCPKG_ROOT.")
-        endif()
-        find_program(
-          _VCPKG_BIN vcpkg
-          PATHS "${VCPKG_ROOT}"
-          NO_DEFAULT_PATH)
-        if(NOT _VCPKG_BIN)
-          message(FATAL_ERROR "vcpkg not found. Re-run vcpkg integrate install "
-                              "or set environment variable VCPKG_ROOT.")
-        endif()
-      endif()
-    endif()
-    set(CMAKE_TOOLCHAIN_FILE
-        "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
-        CACHE STRING "Path to vcpkg CMake toolchain file" FORCE)
-    set(EP_COMMON_CMAKE_ARGS
-        ${EP_COMMON_CMAKE_ARGS}
-        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE})
-  endif()
-  message(STATUS "Using CMAKE_TOOLCHAIN_FILE: ${CMAKE_TOOLCHAIN_FILE}")
-  message(STATUS "Using VCPKG_ROOT: ${VCPKG_ROOT}")
-
-  # Get VCPKG_TARGET_TRIPLET and _VCPKG_INSTALLED_DIR
-  #
-  if(DEFINED ENV{VCPKG_DEFAULT_TRIPLET} AND NOT DEFINED VCPKG_TARGET_TRIPLET)
-    set(VCPKG_TARGET_TRIPLET "$ENV{VCPKG_DEFAULT_TRIPLET}")
-  endif()
-  # explicitly set manifest mode on if it is not set and vcpkg.json exists
-  if(NOT DEFINED VCPKG_MANIFEST_MODE AND EXISTS "${ARROW_SOURCE_DIR}/vcpkg.json")
-    set(VCPKG_MANIFEST_MODE ON CACHE BOOL "Use vcpkg.json manifest")
-    message(STATUS "vcpkg.json manifest found. Using VCPKG_MANIFEST_MODE: ON")
-  endif()
-  # vcpkg can install packages in two different places
-  set(_INST_ARROW_SOURCE_DIR "${ARROW_SOURCE_DIR}/vcpkg_installed") # try here first
-  set(_INST_VCPKG_ROOT "${VCPKG_ROOT}/installed")
-  # Iterate over the places
-  foreach(_INST_DIR IN LISTS _INST_ARROW_SOURCE_DIR _INST_VCPKG_ROOT "notfound")
-    if(_INST_DIR STREQUAL "notfound")
-      message(FATAL_ERROR "vcpkg installed directory not found. "
-                          "Install packages with vcpkg before executing cmake.")
-    elseif(NOT EXISTS "${_INST_DIR}")
-      continue()
-    elseif(_INST_DIR STREQUAL _INST_ARROW_SOURCE_DIR AND NOT VCPKG_MANIFEST_MODE)
-      # Do not look for packages in _INST_ARROW_SOURCE_DIR if manifest mode is off
-      message(STATUS "Skipped looking for installed packages in ${_INST_DIR} "
-                     "because -DVCPKG_MANIFEST_MODE=OFF")
-      continue()
-    else()
-      message(STATUS "Looking for installed packages in ${_INST_DIR}")
-    endif()
-    if(DEFINED VCPKG_TARGET_TRIPLET)
-      # Check if a subdirectory named VCPKG_TARGET_TRIPLET
-      # exists in the vcpkg installed directory
-      if(EXISTS "${_INST_DIR}/${VCPKG_TARGET_TRIPLET}")
-        set(_VCPKG_INSTALLED_DIR
-            "${_INST_DIR}"
-            CACHE STRING "Path to vcpkg installed directory" FORCE)
-        break()
-      endif()
-    else()
-      # Infer VCPKG_TARGET_TRIPLET from the name of the
-      # subdirectory in the vcpkg installed directory
-      list_subdirs(_VCPKG_TRIPLET_SUBDIRS "${_INST_DIR}")
-      list(REMOVE_ITEM _VCPKG_TRIPLET_SUBDIRS "vcpkg")
-      list(LENGTH _VCPKG_TRIPLET_SUBDIRS _NUM_VCPKG_TRIPLET_SUBDIRS)
-      if(_NUM_VCPKG_TRIPLET_SUBDIRS EQUAL 1)
-        list(GET _VCPKG_TRIPLET_SUBDIRS 0 VCPKG_TARGET_TRIPLET)
-        set(_VCPKG_INSTALLED_DIR
-            "${_INST_DIR}"
-            CACHE STRING "Path to vcpkg installed directory" FORCE)
-        break()
-      endif()
-    endif()
-  endforeach()
-  if(NOT DEFINED VCPKG_TARGET_TRIPLET)
-    message(FATAL_ERROR "Could not infer VCPKG_TARGET_TRIPLET. "
-                        "Specify triplet with -DVCPKG_TARGET_TRIPLET.")
-  elseif(NOT DEFINED _VCPKG_INSTALLED_DIR)
-    message(
-      FATAL_ERROR
-        "Could not find installed vcpkg packages "
-        "for triplet ${VCPKG_TARGET_TRIPLET}. "
-        "Install packages with vcpkg before executing cmake.")
-  endif()
-  set(VCPKG_TARGET_TRIPLET
-      "${VCPKG_TARGET_TRIPLET}"
-      CACHE STRING "vcpkg triplet for the target environment" FORCE)
-  set(ARROW_VCPKG_PREFIX
-      "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
-      CACHE STRING
-      "Path to target triplet subdirectory in vcpkg installed directory")
-  message(STATUS "Found vcpkg installed packages directory: ${ARROW_VCPKG_PREFIX}")
-  # include_directories(SYSTEM "${ARROW_VCPKG_PREFIX}")
-  # include_directories(SYSTEM "${ARROW_VCPKG_PREFIX}/lib")
-  # include_directories(SYSTEM "${ARROW_VCPKG_PREFIX}/include")
-  set(ARROW_PACKAGE_PREFIX "${ARROW_VCPKG_PREFIX}")
-  message(STATUS "Using ARROW_PACKAGE_PREFIX: ${ARROW_PACKAGE_PREFIX}")
-  message(STATUS "Using VCPKG_TARGET_TRIPLET: ${VCPKG_TARGET_TRIPLET}")
-  message(STATUS "Using _VCPKG_INSTALLED_DIR: ${_VCPKG_INSTALLED_DIR}")
-  set(EP_COMMON_CMAKE_ARGS
-    ${EP_COMMON_CMAKE_ARGS}
-    -DVCPKG_TARGET_TRIPLET=${VCPKG_TARGET_TRIPLET}
-    -D_VCPKG_INSTALLED_DIR=${_VCPKG_INSTALLED_DIR})
-  set(ARROW_VCPKG ON) # this is used in other CMake scripts
-  set(ARROW_ACTUAL_DEPENDENCY_SOURCE "SYSTEM")
-
-elseif(ARROW_DEPENDENCY_SOURCE STREQUAL "CONDA")
+if(ARROW_DEPENDENCY_SOURCE STREQUAL "CONDA")
   if(MSVC)
     set(ARROW_PACKAGE_PREFIX "$ENV{CONDA_PREFIX}/Library")
   else()
@@ -1642,7 +1468,7 @@ if(ARROW_WITH_PROTOBUF)
     set(ARROW_PROTOBUF_LIBPROTOC protobuf::libprotoc)
   endif()
   if(ARROW_VCPKG)
-    if(WIN32)
+    if(CMAKE_HOST_WIN32)
       set(ARROW_PROTOBUF_PROTOC "${ARROW_VCPKG_PREFIX}/tools/protobuf/protoc.exe")
     else()
       set(ARROW_PROTOBUF_PROTOC "${ARROW_VCPKG_PREFIX}/tools/protobuf/protoc")
