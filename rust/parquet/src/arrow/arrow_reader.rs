a83bc1792f53791ea2000a972a3c868d29b6f875
Max Burke <max@urbanlogiq.com>
2021-02-24 15:30:07 +0200

    ARROW-11452: [Rust] Fix issue with Parquet Arrow reader not following type path
    Not sure where the test data file should go, but I've attached it.
    
    [structs.parquet.zip](https://github.com/apache/arrow/files/5906689/structs.parquet.zip)
    
    Closes #9390 from maxburke/ARROW-11452
    
    Lead-authored-by: Max Burke <max@urbanlogiq.com>
    Co-authored-by: Neville Dipale <nevilledips@gmail.com>
    Signed-off-by: Neville Dipale <nevilledips@gmail.com>

--- rust/parquet/src/arrow/arrow_reader.rs
+++ rust/parquet/src/arrow/arrow_reader.rs
@@ -649,4 +649,23 @@ mod tests {
             }
         }
     }
+
+    #[test]
+    fn test_read_structs() {
+        // This particular test file has columns of struct types where there is
+        // a column that has the same name as one of the struct fields
+        // (see: ARROW-11452)
+        let testdata = arrow::util::test_util::parquet_test_data();
+        let path = format!("{}/nested_structs.rust.parquet", testdata);
+        let parquet_file_reader =
+            SerializedFileReader::try_from(File::open(&path).unwrap()).unwrap();
+        let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(parquet_file_reader));
+        let record_batch_reader = arrow_reader
+            .get_record_reader(60)
+            .expect("Failed to read into array!");
+
+        for batch in record_batch_reader {
+            batch.unwrap();
+        }
+    }
 }
