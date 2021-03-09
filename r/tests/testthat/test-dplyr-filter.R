8ef62100b120a13280851fb0696fc99b3ed1bfe3
Mauricio Vargas <mvargas@dcc.uchile.cl>
2021-03-09 15:26:03 -0300

    skip dplyr filter() with string ops

--- r/tests/testthat/test-dplyr-filter.R
+++ r/tests/testthat/test-dplyr-filter.R
@@ -164,6 +164,9 @@ test_that("filter() with string ops", {
   #    (presumably legitimate) places
   # 2) Wrap the test in expect_warning(expr, NA) to catch the warning
 
+  skip_if(as.numeric(sessionInfo()$R.version$minor) < 3.4,
+          "R < 3.4")
+
   with_no_as_character <- function(expr) {
     trace(
       "as.character",
