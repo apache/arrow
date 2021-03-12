cfc16171266ea77daff8fc6996a1d147ba2d1c0d
Sam Albers <sam.albers@gov.bc.ca>
2021-03-12 14:02:15 -0800

    remove input validation

--- r/R/dplyr.R
+++ r/R/dplyr.R
@@ -300,12 +300,6 @@ build_function_list <- function(FUN) {
       )
     },
     between = function(x, left, right) {
-      if (length(left) != 1) {
-        abort("`left` must be length 1")
-      }
-      if (length(right) != 1) {
-        abort("`right` must be length 1")
-      }
       x >= left & x <= right
     },
     # Now also include all available Arrow Compute functions,
