a150c7056af55e71084929572eeb9c6b3e9688bb
Anthony Louis <anthony@simbioseventures.com>
2021-03-15 09:49:12 -0300

    Refactor names for sha functions macros

--- cpp/src/gandiva/function_registry_common.h
+++ cpp/src/gandiva/function_registry_common.h
@@ -213,7 +213,7 @@ typedef std::unordered_map<const FunctionSignature*, const NativeFunction*, KeyH
 // - can return errors
 //
 // The function name includes the base name & input type name. gdv_fn_sha128_from_float64
-#define HASH_SHA128_SAFE_NULL_NEVER(NAME, ALIASES, TYPE)                           \
+#define HASH_SHA128_NULL_NEVER(NAME, ALIASES, TYPE)                           \
   NativeFunction(#NAME, std::vector<std::string> ALIASES,                          \
                  DataTypeVector{TYPE()}, utf8(), kResultNullNever,                 \
                  ARROW_STRINGIFY(gdv_fn_sha128_from_##TYPE),                       \
@@ -224,7 +224,7 @@ typedef std::unordered_map<const FunctionSignature*, const NativeFunction*, KeyH
 // - can return errors
 //
 // The function name includes the base name & input type name. gdv_fn_sha256_from_float64
-#define HASH_SHA256_SAFE_NULL_NEVER(NAME, ALIASES, TYPE)                           \
+#define HASH_SHA256_NULL_NEVER(NAME, ALIASES, TYPE)                           \
   NativeFunction(#NAME, std::vector<std::string> ALIASES,                          \
                  DataTypeVector{TYPE()}, utf8(), kResultNullNever,                 \
                  ARROW_STRINGIFY(gdv_fn_sha256_from_##TYPE),                       \
