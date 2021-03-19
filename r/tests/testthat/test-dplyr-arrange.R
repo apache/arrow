5c8b70e26ba5cd04470b4ec2cba116e205ff38e8
Ian Cook <ianmcook@gmail.com>
2021-03-19 10:59:31 -0400

    Add arrange() tests

---
+++ r/tests/testthat/test-dplyr-arrange.R
@@ -0,0 +1,61 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+library(dplyr)
+
+tbl <- example_data_for_sorting
+
+test_that("arrange", {
+  expect_dplyr_equal(
+    input %>%
+      arrange(int, chr) %>%
+      collect(),
+    tbl %>%
+      slice_sample(prop = 1L)
+  )
+  expect_dplyr_equal(
+    input %>%
+      arrange(int, desc(dbl)) %>%
+      collect(),
+    tbl %>%
+      slice_sample(prop = 1L)
+  )
+  expect_dplyr_equal(
+    input %>%
+      arrange(int) %>%
+      arrange(desc(dbl)) %>%
+      collect(),
+    tbl %>%
+      slice_sample(prop = 1L)
+  )
+  expect_error(
+    tbl %>%
+      Table$create() %>%
+      arrange(int + dbl) %>%
+      collect(),
+    "Only bare field names are supported in arrange",
+    fixed = TRUE
+  )
+  expect_error(
+    tbl %>%
+      Table$create() %>%
+      arrange(1),
+    "does not contain any field names",
+    fixed = TRUE
+  )
+  # TODO: test the other unsupported cases
+})
