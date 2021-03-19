9f250045cece7e413ff8d3d83dbecfe9b986ab39
Anthony Louis <anthony@simbioseventures.com>
2021-03-19 12:36:22 -0300

    Add gandiva export in decimal functions

--- cpp/src/gandiva/gdv_function_stubs.h
+++ cpp/src/gandiva/gdv_function_stubs.h
@@ -67,10 +67,12 @@ bool in_expr_lookup_utf8(int64_t ptr, const char* data, int data_len, bool in_va
 int gdv_fn_time_with_zone(int* time_fields, const char* zone, int zone_len,
                           int64_t* ret_time);
 
+GANDIVA_EXPORT
 const char* gdv_fn_sha256_decimal128(int64_t context, int64_t x_high, uint64_t x_low,
                                      int32_t x_precision, int32_t x_scale,
                                      gdv_boolean x_isvalid, int32_t *out_length);
 
+GANDIVA_EXPORT
 const char* gdv_fn_sha1_decimal128(int64_t context, int64_t x_high, uint64_t x_low,
                                    int32_t x_precision, int32_t x_scale,
                                    gdv_boolean x_isvalid, int32_t *out_length);
