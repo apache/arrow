3391776c35e7ae3417dc067ef7eab38552aaadaa
João Pedro <joaop@simbioseventures.com>
2021-03-22 08:47:04 -0300

    Correct missing identation

--- cpp/src/gandiva/function_registry_math_ops.cc
+++ cpp/src/gandiva/function_registry_math_ops.cc
@@ -72,7 +72,7 @@ std::vector<NativeFunction> GetMathOpsFunctionRegistry() {
       MATH_UNARY_OPS(acos, {}), MATH_UNARY_OPS(tan, {}), MATH_UNARY_OPS(atan, {}),
       MATH_UNARY_OPS(sinh, {}), MATH_UNARY_OPS(cosh, {}), MATH_UNARY_OPS(tanh, {}),
       MATH_UNARY_OPS(cot, {}), MATH_UNARY_OPS(radians, {}), MATH_UNARY_OPS(degrees, {}),
-			MATH_BINARY_SAFE(atan2, {}),
+      MATH_BINARY_SAFE(atan2, {}),
 
       // decimal functions
       UNARY_SAFE_NULL_IF_NULL(abs, {}, decimal128, decimal128),
