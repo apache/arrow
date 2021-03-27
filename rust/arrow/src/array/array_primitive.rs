426258c513acd0a67767a6b67eddd75c844767cf
Heres, Daniel <danielheres@gmail.com>
2021-03-27 12:05:26 +0100

    Rename

--- rust/arrow/src/array/array_primitive.rs
+++ rust/arrow/src/array/array_primitive.rs
@@ -114,7 +114,7 @@ impl<T: ArrowPrimitiveType> PrimitiveArray<T> {
     }
 
     /// Creates a PrimitiveArray based on a constant value with `count` elements
-    pub fn from_constant(value: T::Native, count: usize) -> Self {
+    pub fn from_value(value: T::Native, count: usize) -> Self {
         // # Safety: length is known
         let val_buf = unsafe { Buffer::from_trusted_len_iter((0..count).map(|_| value)) };
         let data = ArrayData::new(
