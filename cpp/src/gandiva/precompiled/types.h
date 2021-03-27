1c62117285f69e2bb15de50af9bbea2b7a215f4d
Anthony Louis <anthony@simbioseventures.com>
2021-03-27 16:29:25 +0530

    ARROW-11988: [C++][Gandiva] Implements last_day function
    Implements the `last_day` function inside the Gandiva. The function gets a timestamp and returns the date of the last day of the month defined in timestamp.
    
    JIRA issue: https://issues.apache.org/jira/browse/ARROW-11988
    
    Closes #9727 from anthonylouisbsb/feature/add-last-day-function and squashes the following commits:
    
    9f22dc3ca <Anthony Louis> Fix problems with formatter
    3cdb3c534 <Anthony Louis> Fix linter warnings about the line length
    95bb36e6f <Anthony Louis> Fix identation for code
    21b504f0a <Anthony Louis> Fix integration tests for last day function
    7abc05e87 <Anthony Louis> Add integration test for last day function
    e017ceb5e <Anthony Louis> Add functions to retrieve last day inside the registry
    c87e24837 <Anthony Louis> Add macro for last_day func
    3be2982f0 <Anthony Louis> Add unit test for the las day method
    9dbb50192 <Anthony Louis> Create method to retrieve last time for timestamp
    
    Authored-by: Anthony Louis <anthony@simbioseventures.com>
    Signed-off-by: Praveen <praveen@dremio.com>

--- cpp/src/gandiva/precompiled/types.h
+++ cpp/src/gandiva/precompiled/types.h
@@ -252,6 +252,7 @@ gdv_timestamp castTIMESTAMP_date64(gdv_date64);
 gdv_timestamp castTIMESTAMP_int64(gdv_int64);
 gdv_date64 castDATE_timestamp(gdv_timestamp);
 const char* castVARCHAR_timestamp_int64(int64_t, gdv_timestamp, gdv_int64, gdv_int32*);
+gdv_date64 last_day_from_timestamp(gdv_date64 millis);
 
 gdv_int64 truncate_int64_int32(gdv_int64 in, gdv_int32 out_scale);
 
