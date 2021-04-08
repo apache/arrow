b8e172668397deea2747f3187f2ad8650096488f
João Pedro <joaop@simbioseventures.com>
2021-04-08 08:01:27 -0300

    Revert correction on pr to do it on a separate one

--- cpp/src/gandiva/gdv_function_stubs.h
+++ cpp/src/gandiva/gdv_function_stubs.h
@@ -39,12 +39,11 @@ uint8_t* gdv_fn_context_arena_malloc(int64_t context_ptr, int32_t data_len);
 
 void gdv_fn_context_arena_reset(int64_t context_ptr);
 
-bool gdv_fn_in_expr_lookup_int32(int64_t ptr, int32_t value, bool in_validity);
+bool in_expr_lookup_int32(int64_t ptr, int32_t value, bool in_validity);
 
-bool gdv_fn_in_expr_lookup_int64(int64_t ptr, int64_t value, bool in_validity);
+bool in_expr_lookup_int64(int64_t ptr, int64_t value, bool in_validity);
 
-bool gdv_fn_in_expr_lookup_utf8(int64_t ptr, const char* data, int data_len,
-                                bool in_validity);
+bool in_expr_lookup_utf8(int64_t ptr, const char* data, int data_len, bool in_validity);
 
 bool gdv_fn_is_date_utf8(int64_t ptr, const char* data, int64_t data_len);
 
