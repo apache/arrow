3258f123e3db6b795973e247908fe84e75de3ec5
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-04-08 09:11:13 -0400

    ARROW-12058: [Python] Enable arithmetic operations on Expressions
    Closes #9780 from jorisvandenbossche/ARROW-12058-expressions-arithmetic
    
    Authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- python/pyarrow/tests/test_dataset.py
+++ python/pyarrow/tests/test_dataset.py
@@ -487,6 +487,23 @@ def test_expression_boolean_operators():
         not true
 
 
+def test_expression_arithmetic_operators():
+    dataset = ds.dataset(pa.table({'a': [1, 2, 3], 'b': [2, 2, 2]}))
+    a = ds.field("a")
+    b = ds.field("b")
+    result = dataset.to_table(columns={
+        "a+1": a + 1,
+        "b-a": b - a,
+        "a*2": a * 2,
+        "a/b": a.cast("float64") / b,
+    })
+    expected = pa.table({
+        "a+1": [2, 3, 4], "b-a": [1, 0, -1],
+        "a*2": [2, 4, 6], "a/b": [0.5, 1.0, 1.5],
+    })
+    assert result.equals(expected)
+
+
 def test_partition_keys():
     a, b, c = [ds.field(f) == f for f in 'abc']
     assert ds._get_partition_keys(a) == {'a': 'a'}
