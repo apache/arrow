83f944ce6abb70342771db27b8fe7583f9a8144f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-04-08 13:30:09 +0200

    update test

--- python/pyarrow/tests/test_dataset.py
+++ python/pyarrow/tests/test_dataset.py
@@ -488,11 +488,20 @@ def test_expression_boolean_operators():
 
 
 def test_expression_arithmetic_operators():
-    field = ds.field("field")
-    field + 1
-    field - 1
-    field * 1
-    field / 1
+    dataset = ds.dataset(pa.table({'a': [1, 2, 3], 'b': [2, 2, 2]}))
+    a = ds.field("a")
+    b = ds.field("b")
+    result = dataset.to_table(columns={
+        "a+1": a + 1,
+        "b-a": b - a,
+        "a*2": a * 2,
+        "a/b": a.cast("float64") / b,
+    })
+    expected = pa.table({
+        "a+1": [2, 3, 4], "b-a": [1, 0, -1],
+        "a*2": [2, 4, 6], "a/b": [0.5, 1.0, 1.5],
+    })
+    assert result.equals(expected)
 
 
 def test_partition_keys():
