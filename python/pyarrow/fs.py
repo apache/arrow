b59663996777ac51206071668d405549795f342a
sjgupta2 <sjgupta2@illinois.edu>
2021-04-13 08:52:03 -0400

    remove extra ,

--- python/pyarrow/fs.py
+++ python/pyarrow/fs.py
@@ -45,7 +45,7 @@ except ImportError:
 
 try:
     from pyarrow._s3fs import (  # noqa
-        S3FileSystem, S3LogLevel, initialize_s3, finalize_s3,)
+        S3FileSystem, S3LogLevel, initialize_s3, finalize_s3)
 except ImportError:
     _not_imported.append("S3FileSystem")
 else:
