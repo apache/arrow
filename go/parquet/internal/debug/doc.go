b2ceb8f22471f0c4654d6477117d01e897e12088
Matthew Topol <mtopol@factset.com>
2021-04-15 20:20:11 -0700

    ARROW-12104: [Go][Parquet] Second chunk of Ported Go Parquet code
    Following up from #9671 this is the next chunk of ported code consisting of the generated Thrift Code and the utilities for supporting Encryption, Compression and Reader/Writer Property handling.
    
    Thankfully this is much smaller than the previous chunk, and so should be much easier to review and read.
    
    Closes #9817 from zeroshade/arrow-12104
    
    Authored-by: Matthew Topol <mtopol@factset.com>
    Signed-off-by: Micah Kornfield <emkornfield@gmail.com>

--- go/parquet/internal/debug/doc.go
+++ go/parquet/internal/debug/doc.go
@@ -14,22 +14,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-module github.com/apache/arrow/go/parquet
-
-go 1.15
-
-require (
-	github.com/andybalholm/brotli v1.0.1
-	github.com/apache/arrow/go/arrow v0.0.0-20210310173904-5de02e3697aa
-	github.com/apache/thrift/lib/go/thrift v0.0.0-20210120171102-e27e82c46ba4
-	github.com/golang/snappy v0.0.3
-	github.com/klauspost/asmfmt v1.2.3
-	github.com/klauspost/compress v1.11.12
-	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8
-	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3
-	github.com/stretchr/testify v1.7.0
-	golang.org/x/exp v0.0.0-20210220032938-85be41e4509f
-	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
-	gonum.org/v1/gonum v0.8.2
-)
+// Package debug provides APIs for conditional runtime assertions and debug logging.
+//
+// Using Assert
+//
+// To enable runtime assertions, build with the assert tag. When the assert tag is omitted,
+// the code for the assertion will be omitted from the binary.
+package debug
