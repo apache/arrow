a41064ced720cb790f85295ac5d47bd68291c513
Nic Crane <thisisnic@gmail.com>
2021-04-19 15:50:25 +0100

    Implement SelectColumns for RecordBatch in C++

--- cpp/src/arrow/record_batch.cc
+++ cpp/src/arrow/record_batch.cc
@@ -253,6 +253,26 @@ bool RecordBatch::ApproxEquals(const RecordBatch& other) const {
   return true;
 }
 
+Result<std::shared_ptr<RecordBatch>> RecordBatch::SelectColumns(const std::vector<int>& indices) const{
+  int n = static_cast<int>(indices.size());
+
+  std::vector<std::shared_ptr<Field>> fields(n);
+  std::vector<std::shared_ptr<Array>> columns(n);
+  
+  for (int i = 0; i < n; i++) {
+    int pos = indices[i];
+    if (pos < 0 || pos > num_columns() - 1) {
+      return Status::Invalid("Invalid column index ", pos, " to select columns.");
+    }
+    fields[i] = schema()->field(pos);
+    columns[i] = column(pos);
+  }
+  
+  auto schema = std::make_shared<arrow::Schema>(std::move(fields));
+  return RecordBatch::Make(schema, num_rows(), columns);
+}
+
+
 std::shared_ptr<RecordBatch> RecordBatch::Slice(int64_t offset) const {
   return Slice(offset, this->num_rows() - offset);
 }
