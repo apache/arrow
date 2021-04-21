be258f2aa646f28f71108332e84f5e3adf1d2637
Jonathan Keane <jkeane@gmail.com>
2021-04-21 14:01:50 +0200

    ARROW-12485: [C++] Use mimalloc as the default memory allocator on macOS
    Closes #10117 from jonkeane/ARROW-12485
    
    Authored-by: Jonathan Keane <jkeane@gmail.com>
    Signed-off-by: Krisztián Szűcs <szucs.krisztian@gmail.com>

--- cpp/src/arrow/memory_pool.cc
+++ cpp/src/arrow/memory_pool.cc
@@ -170,13 +170,8 @@ MemoryPoolBackend DefaultBackend() {
   if (backend.has_value()) {
     return backend.value();
   }
-#ifdef ARROW_JEMALLOC
-  return MemoryPoolBackend::Jemalloc;
-#elif defined(ARROW_MIMALLOC)
-  return MemoryPoolBackend::Mimalloc;
-#else
-  return MemoryPoolBackend::System;
-#endif
+  struct SupportedBackend default_backend = SupportedBackends().front();
+  return default_backend.backend;
 }
 
 // A static piece of memory for 0-size allocations, so as to return
