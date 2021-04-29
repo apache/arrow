b44aea07f10d92461015be364e219a0c739d4718
João Pedro <joaop@simbioseventures.com>
2021-04-29 08:12:48 -0300

    Fix local lint errors

--- cpp/src/gandiva/precompiled/time.cc
+++ cpp/src/gandiva/precompiled/time.cc
@@ -851,10 +851,10 @@ gdv_int64 castBIGINT_daytimeinterval(gdv_day_time_interval in) {
 NUMERIC_TYPES(TO_TIMESTAMP)
 
 // Convert the seconds since epoch argument to time
-#define TO_TIME(TYPE)                               \
-  FORCE_INLINE                                      \
-  gdv_time32 to_time##_##TYPE(gdv_##TYPE seconds) { \
-    return static_cast<gdv_time32>(seconds) * MILLIS_IN_SEC;        \
+#define TO_TIME(TYPE)                                        \
+  FORCE_INLINE                                               \
+  gdv_time32 to_time##_##TYPE(gdv_##TYPE seconds) {          \
+    return static_cast<gdv_time32>(seconds) * MILLIS_IN_SEC; \
   }
 
 NUMERIC_TYPES(TO_TIME)
