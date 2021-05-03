78e993bb8c8cb3afe5fd72d33391545ad45d2855
Yibo Cai <yibo.cai@arm.com>
2021-05-03 16:23:39 +0200

    ARROW-12240: [Python] Fix invalid-offsetof warning
    arrow::csv::ConvertOptions has stl container data members which are
    not guaranteed to be standard layout type. It causes invalid-offsetof
    warning when compiled with clang-12.
    
    This patch changes directly embedded CCSVConvertOptions to unique_ptr
    to fix the issue. Parse and Read options are also updated.
    
    Closes #10154 from cyb70289/12240-invalid-offsetof
    
    Lead-authored-by: Yibo Cai <yibo.cai@arm.com>
    Co-authored-by: Antoine Pitrou <antoine@python.org>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- python/pyarrow/_dataset.pyx
+++ python/pyarrow/_dataset.pyx
@@ -1710,7 +1710,7 @@ cdef class CsvFileFormat(FileFormat):
 
     @parse_options.setter
     def parse_options(self, ParseOptions parse_options not None):
-        self.csv_format.parse_options = parse_options.options
+        self.csv_format.parse_options = deref(parse_options.options)
 
     cdef _set_default_fragment_scan_options(self, FragmentScanOptions options):
         if options.type_name == 'csv':
@@ -1760,7 +1760,7 @@ cdef class CsvFragmentScanOptions(FragmentScanOptions):
 
     @convert_options.setter
     def convert_options(self, ConvertOptions convert_options not None):
-        self.csv_options.convert_options = convert_options.options
+        self.csv_options.convert_options = deref(convert_options.options)
 
     @property
     def read_options(self):
@@ -1768,7 +1768,7 @@ cdef class CsvFragmentScanOptions(FragmentScanOptions):
 
     @read_options.setter
     def read_options(self, ReadOptions read_options not None):
-        self.csv_options.read_options = read_options.options
+        self.csv_options.read_options = deref(read_options.options)
 
     def equals(self, CsvFragmentScanOptions other):
         return (
