653ca8a00897c8279989468b841a043ac792e718
Krisztián Szűcs <szucs.krisztian@gmail.com>
2021-05-03 17:41:41 +0200

    Check missing dependency

--- dev/tasks/conda-recipes/drone-steps.sh
+++ dev/tasks/conda-recipes/drone-steps.sh
@@ -9,16 +9,15 @@ TASK_TAG=$4
 UPLOAD_TO_ANACONDA=$5
 
 conda install -y mamba
-$FEEDSTOCK_ROOT/build_steps.sh ${OUTPUT_DIR}
+# $FEEDSTOCK_ROOT/build_steps.sh ${OUTPUT_DIR}
 
 # Upload as Github release
 mamba install -y anaconda-client shyaml pygit2 -c conda-forge
-mamba list
 
 pushd $DRONE_WORKSPACE
 
 pip install arrow/dev/archery[crossbow]
-archery crossbow submit \
+archery crossbow \
   --queue-path . \
   --queue-remote ${QUEUE_REMOTE_URL} \
   upload-artifacts \
