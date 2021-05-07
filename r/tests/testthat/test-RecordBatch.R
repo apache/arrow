7489ab58f3879d9c9c4240deb11e03dd0d825f8a
Nic <thisisnic@gmail.com>
2021-05-07 14:55:19 +0100

    Remove library call
    Co-authored-by: Ian Cook <ianmcook@gmail.com>

--- r/tests/testthat/test-RecordBatch.R
+++ r/tests/testthat/test-RecordBatch.R
@@ -15,7 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-library(dplyr)
 
 test_that("RecordBatch", {
   # Note that we're reusing `tbl` and `batch` throughout the tests in this file
