724e6acf1320317ca5537b0f48feee57140d6689
sgilmore <sgilmore@mathworks.com>
2021-05-11 16:09:02 -0400

    Update featherwrite C++ code to use the latest Arrow C++ APIs
    Co-authored-by: Kevin Gurney <kevin.p.gurney@gmail.com>

--- matlab/src/featherwritemex.cc
+++ matlab/src/featherwritemex.cc
@@ -15,9 +15,9 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include <string>
-
 #include <mex.h>
+
+#include <string>
 
 #include "feather_writer.h"
 #include "util/handle_status.h"
@@ -32,6 +32,5 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
       arrow::matlab::FeatherWriter::Open(filename, &feather_writer));
 
   // Write the Feather file table variables and table metadata from MATLAB.
-  feather_writer->WriteMetadata(prhs[2]);
-  arrow::matlab::util::HandleStatus(feather_writer->WriteVariables(prhs[1]));
+  arrow::matlab::util::HandleStatus(feather_writer->WriteVariables(prhs[1], prhs[2]));
 }
