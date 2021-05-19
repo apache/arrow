aa9b15936d54dcf09ee1857e04515ec21698fa93
Weston Pace <weston.pace@gmail.com>
2021-05-19 09:24:48 -1000

    ARROW-12468: Disable new use_async test on Windows since use_async implies threads and thus no RTools3.5

--- r/tests/testthat/test-dataset.R
+++ r/tests/testthat/test-dataset.R
@@ -1035,6 +1035,9 @@ test_that("Scanner$ScanBatches", {
   table <- Table$create(!!!batches)
   expect_equivalent(as.data.frame(table), rbind(df1, df2))
 
+  # use_async will always use the thread pool (even if it only uses
+  # one thread) and RTools 3.5 on Windows doesn't support this
+  skip_on_os("windows")
   batches <- ds$NewScan()$UseAsync(TRUE)$Finish()$ScanBatches()
   table <- Table$create(!!!batches)
   expect_equivalent(as.data.frame(table), rbind(df1, df2))
