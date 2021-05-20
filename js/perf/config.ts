db1301ea7a5ab5bc0805a5d70dae5f05b935c923
Dominik Moritz <domoritz@gmail.com>
2021-05-19 21:45:24 -0700

    ARROW-12833: [JS] Construct perf data in JS

--- js/perf/config.ts
+++ js/perf/config.ts
@@ -15,23 +15,53 @@
 // specific language governing permissions and limitations
 // under the License.
 
-import {readFileSync} from 'fs';
-import {resolve, parse} from 'path';
-import {sync} from 'glob';
+import * as Arrow from '../src/Arrow.dom';
 
-const filenames = sync(resolve(__dirname, `../test/data/tables/`, `*.arrow`));
+console.time('Prepare Data');
 
-export default filenames.map(filename => {
-    const { name } = parse(filename);
-    return {
-        name,
-        buffers: [readFileSync(filename)],
+const LENGTH = 100000;
+const NUM_BATCHES = 10;
+
+const values = Arrow.Utf8Vector.from(['Charlottesville', 'New York', 'San Francisco', 'Seattle', 'Terre Haute', 'Washington, DC']);
+
+const batches = Array.from({length: NUM_BATCHES}).map(() => {
+    const lat = Float32Array.from(
+        { length: LENGTH },
+        () => ((Math.random() - 0.5) * 2 * 90));
+    const lng = Float32Array.from(
+        { length: LENGTH },
+        () => ((Math.random() - 0.5) * 2 * 90));
+
+    const origin = Uint8Array.from(
+        { length: LENGTH },
+        () => (Math.random() * 6));
+    const destination = Uint8Array.from(
+        { length: LENGTH },
+        () => (Math.random() * 6));
+
+    return Arrow.RecordBatch.new({
+        'lat': Arrow.Float32Vector.from(lat),
+        'lng': Arrow.Float32Vector.from(lng),
+        'origin': Arrow.DictionaryVector.from(values, new Arrow.Int8(), origin),
+        'destination': Arrow.DictionaryVector.from(values, new Arrow.Int8(), destination),
+    });
+});
+
+const {schema} = batches[0];
+const tracks = new Arrow.DataFrame(schema, batches);
+
+console.timeEnd('Prepare Data');
+
+export default [
+    {
+        name: 'tracks',
+        df: tracks,
+        ipc: tracks.serialize(),
         countBys: ['origin', 'destination'],
         counts: [
             {column: 'lat',    test: 'gt' as 'gt' | 'eq', value: 0        },
             {column: 'lng',    test: 'gt' as 'gt' | 'eq', value: 0        },
             {column: 'origin', test: 'eq' as 'gt' | 'eq', value: 'Seattle'},
         ],
-    };
-});
-
+    }
+];
