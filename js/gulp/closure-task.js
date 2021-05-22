e7e3784d8aa6b27fb199a5a74805e8e923714fb7
Dominik Moritz <domoritz@gmail.com>
2021-05-22 17:24:30 +0900

    ARROW-12800: [JS] Remove text encoder and decoder polyfills
    This drops IE support as well.
    
    Closes #10340 from domoritz/remove-text-encoding-utf-8
    
    Authored-by: Dominik Moritz <domoritz@gmail.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- js/gulp/closure-task.js
+++ js/gulp/closure-task.js
@@ -72,8 +72,6 @@ const closureTask = ((cache) => memoizeTask(cache, async function closure(target
                 /* external libs first */
                 `node_modules/flatbuffers/package.json`,
                 `node_modules/flatbuffers/js/flatbuffers.mjs`,
-                `node_modules/text-encoding-utf-8/package.json`,
-                `node_modules/text-encoding-utf-8/src/encoding.js`,
                 `${src}/**/*.js` /* <-- then source globs */
             ], { base: `./` }),
             sourcemaps.init(),
