85c7f46163b776a6f73c37ae9674249b0a3ad8e6
Dominik Moritz <domoritz@gmail.com>
2021-05-26 12:26:36 -0700

    Fix MultiStream

--- js/test/unit/ipc/reader/streams-node-tests.ts
+++ js/test/unit/ipc/reader/streams-node-tests.ts
@@ -36,8 +36,8 @@ import { validateRecordBatchAsyncIterator } from '../validate';
 
     const { Readable, PassThrough } = require('stream');
     const { parse: bignumJSONParse } = require('json-bignum');
-    const concatStream = ((multistream) => (...xs: any[]) =>
-        new Readable().wrap(multistream(...xs))
+    const concatStream = ((MultiStream) => (...xs: any[]) =>
+        new Readable().wrap(new MultiStream(...xs))
     )(require('multistream'));
 
     for (const table of generateRandomTables([10, 20, 30])) {
