97ad27c38ff65d8e12a75ed99f19d8c5f2eb5130
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-05-30 10:59:29 +0530

    Use flatbuffers for serializing scan request for RADOS parquet

--- cpp/src/arrow/dataset/rados_utils.cc
+++ cpp/src/arrow/dataset/rados_utils.cc
@@ -14,172 +14,69 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+#include <flatbuffers/flatbuffers.h> 
 
 #include "arrow/dataset/rados_utils.h"
 #include "arrow/util/compression.h"
-
-#include <iostream>
+#include "generated/Request_generated.h"
 
 namespace arrow {
+
+namespace flatbuf = org::apache::arrow::flatbuf;
+
 namespace dataset {
-
-/// \brief A union for convertions between char buffer
-/// and a 64-bit integer. The conversion always
-/// happen in Little-Endian format.
-union {
-  int64_t integer_;
-  char bytes_[8];
-} converter_;
-
-Status Int64ToChar(char* buffer, int64_t num) {
-  /// Pass the integer through the union to
-  /// get the byte representation.
-  num = BitUtil::ToLittleEndian(num);
-  converter_.integer_ = num;
-  memcpy(buffer, converter_.bytes_, 8);
-  return Status::OK();
-}
-
-Status CharToInt64(char* buffer, int64_t& num) {
-  /// Pass the byte representation through the union to
-  /// get the integer.
-  memcpy(converter_.bytes_, buffer, 8);
-  num = BitUtil::ToLittleEndian(converter_.integer_);
-  return Status::OK();
-}
 
 Status SerializeScanRequestToBufferlist(std::shared_ptr<ScanOptions> options,
                                         int64_t file_size, ceph::bufferlist& bl) {
-  // serialize the filter expression's and the schema's.
-  ARROW_ASSIGN_OR_RAISE(auto filter_buffer, compute::Serialize(options->filter));
-  ARROW_ASSIGN_OR_RAISE(auto part_expr_buffer,
+  ARROW_ASSIGN_OR_RAISE(auto filter, compute::Serialize(options->filter));
+  ARROW_ASSIGN_OR_RAISE(auto partition,
                         compute::Serialize(options->partition_expression));
-  ARROW_ASSIGN_OR_RAISE(auto projection_schema_buffer,
+  ARROW_ASSIGN_OR_RAISE(auto projection,
                         ipc::SerializeSchema(*options->projected_schema));
-  ARROW_ASSIGN_OR_RAISE(auto dataset_schema_buffer,
+  ARROW_ASSIGN_OR_RAISE(auto schema,
                         ipc::SerializeSchema(*options->dataset_schema));
 
-  // convert filter Expression size to buffer.
-  char* filter_size_buffer = new char[8];
-  ARROW_RETURN_NOT_OK(Int64ToChar(filter_size_buffer, filter_buffer->size()));
+  flatbuffers::FlatBufferBuilder builder(1024);
 
-  // convert partition expression size to buffer.
-  char* part_expr_size_buffer = new char[8];
-  ARROW_RETURN_NOT_OK(Int64ToChar(part_expr_size_buffer, part_expr_buffer->size()));
+  auto filter_vec = builder.CreateVector(filter->data(), filter->size());
+  auto partition_vec = builder.CreateVector(partition->data(), partition->size());
+  auto projected_schema_vec = builder.CreateVector(projection->data(), projection->size());
+  auto dataset_schema_vec = builder.CreateVector(schema->data(), schema->size());
 
-  // convert projection schema size to buffer.
-  char* projection_schema_size_buffer = new char[8];
-  ARROW_RETURN_NOT_OK(
-      Int64ToChar(projection_schema_size_buffer, projection_schema_buffer->size()));
+  auto request = flatbuf::CreateRequest(builder, file_size, filter_vec, partition_vec, dataset_schema_vec, projected_schema_vec);
+  builder.Finish(request);
+  uint8_t *buf = builder.GetBufferPointer();
+  int size = builder.GetSize();
 
-  // convert dataset schema to buffer
-  char* dataset_schema_size_buffer = new char[8];
-  ARROW_RETURN_NOT_OK(
-      Int64ToChar(dataset_schema_size_buffer, dataset_schema_buffer->size()));
-
-  char* file_size_buffer = new char[8];
-  ARROW_RETURN_NOT_OK(Int64ToChar(file_size_buffer, file_size));
-
-  // append the filter expression size and data.
-  bl.append(filter_size_buffer, 8);
-  bl.append((char*)filter_buffer->data(), filter_buffer->size());
-
-  // append the partition expression size and data
-  bl.append(part_expr_size_buffer, 8);
-  bl.append((char*)part_expr_buffer->data(), part_expr_buffer->size());
-
-  // append the projection schema size and data.
-  bl.append(projection_schema_size_buffer, 8);
-  bl.append((char*)projection_schema_buffer->data(), projection_schema_buffer->size());
-
-  // append the dataset schema size and data.
-  bl.append(dataset_schema_size_buffer, 8);
-  bl.append((char*)dataset_schema_buffer->data(), dataset_schema_buffer->size());
-
-  bl.append(file_size_buffer, 8);
-
-  delete[] filter_size_buffer;
-  delete[] part_expr_size_buffer;
-  delete[] projection_schema_size_buffer;
-  delete[] dataset_schema_size_buffer;
-  delete[] file_size_buffer;
-
+  bl.append((char*)buf, size);
   return Status::OK();
 }
 
 Status DeserializeScanRequestFromBufferlist(compute::Expression* filter,
-                                            compute::Expression* part_expr,
-                                            std::shared_ptr<Schema>* projection_schema,
+                                            compute::Expression* partition,
+                                            std::shared_ptr<Schema>* projected_schema,
                                             std::shared_ptr<Schema>* dataset_schema,
                                             int64_t& file_size, ceph::bufferlist& bl) {
-  ceph::bufferlist::iterator itr = bl.begin();
+  
+  auto request = flatbuf::GetRequest((uint8_t*)bl.c_str());
 
-  int64_t filter_size = 0;
-  char* filter_size_buffer = new char[8];
-  itr.copy(8, filter_size_buffer);
-  ARROW_RETURN_NOT_OK(CharToInt64(filter_size_buffer, filter_size));
-  char* filter_buffer = new char[filter_size];
-  itr.copy(filter_size, filter_buffer);
-
-  int64_t part_expr_size = 0;
-  char* part_expr_size_buffer = new char[8];
-  itr.copy(8, part_expr_size_buffer);
-  ARROW_RETURN_NOT_OK(CharToInt64(part_expr_size_buffer, part_expr_size));
-  char* part_expr_buffer = new char[part_expr_size];
-  itr.copy(part_expr_size, part_expr_buffer);
-
-  int64_t projection_schema_size = 0;
-  char* projection_schema_size_buffer = new char[8];
-  itr.copy(8, projection_schema_size_buffer);
-  ARROW_RETURN_NOT_OK(CharToInt64(projection_schema_size_buffer, projection_schema_size));
-  char* projection_schema_buffer = new char[projection_schema_size];
-  itr.copy(projection_schema_size, projection_schema_buffer);
-
-  int64_t dataset_schema_size = 0;
-  char* dataset_schema_size_buffer = new char[8];
-  itr.copy(8, dataset_schema_size_buffer);
-  ARROW_RETURN_NOT_OK(CharToInt64(dataset_schema_size_buffer, dataset_schema_size));
-  char* dataset_schema_buffer = new char[dataset_schema_size];
-  itr.copy(dataset_schema_size, dataset_schema_buffer);
-
-  int64_t size = 0;
-  char* file_size_buffer = new char[8];
-  itr.copy(8, file_size_buffer);
-  ARROW_RETURN_NOT_OK(CharToInt64(file_size_buffer, size));
-  file_size = size;
-
-  ARROW_ASSIGN_OR_RAISE(auto filter_, compute::Deserialize(std::make_shared<Buffer>(
-                                          (uint8_t*)filter_buffer, filter_size)));
+  ARROW_ASSIGN_OR_RAISE(auto filter_, compute::Deserialize(std::make_shared<Buffer>(request->filter()->data(), request->filter()->size())));
   *filter = filter_;
 
-  ARROW_ASSIGN_OR_RAISE(auto part_expr_,
-                        compute::Deserialize(std::make_shared<Buffer>(
-                            (uint8_t*)part_expr_buffer, part_expr_size)));
-  *part_expr = part_expr_;
+  ARROW_ASSIGN_OR_RAISE(auto partition_, compute::Deserialize(std::make_shared<Buffer>(request->partition()->data(), request->partition()->size())));
+  *partition = partition_;
 
   ipc::DictionaryMemo empty_memo;
-  io::BufferReader projection_schema_reader((uint8_t*)projection_schema_buffer,
-                                            projection_schema_size);
-  io::BufferReader dataset_schema_reader((uint8_t*)dataset_schema_buffer,
-                                         dataset_schema_size);
+  io::BufferReader projection_reader(request->projection_schema()->data(), request->projection_schema()->size());
+  io::BufferReader schema_reader(request->dataset_schema()->data(), request->dataset_schema()->size());
 
-  ARROW_ASSIGN_OR_RAISE(auto projection_schema_,
-                        ipc::ReadSchema(&projection_schema_reader, &empty_memo));
-  *projection_schema = projection_schema_;
+  ARROW_ASSIGN_OR_RAISE(auto projected_schema_, ipc::ReadSchema(&projection_reader, &empty_memo));
+  *projected_schema = projected_schema_;
 
-  ARROW_ASSIGN_OR_RAISE(auto dataset_schema_,
-                        ipc::ReadSchema(&dataset_schema_reader, &empty_memo));
+  ARROW_ASSIGN_OR_RAISE(auto dataset_schema_, ipc::ReadSchema(&schema_reader, &empty_memo));
   *dataset_schema = dataset_schema_;
 
-  delete[] filter_size_buffer;
-  delete[] filter_buffer;
-  delete[] part_expr_size_buffer;
-  delete[] part_expr_buffer;
-  delete[] projection_schema_size_buffer;
-  delete[] projection_schema_buffer;
-  delete[] dataset_schema_size_buffer;
-  delete[] dataset_schema_buffer;
-
+  file_size = request->file_size();
   return Status::OK();
 }
 
--- cpp/src/arrow/dataset/rados_utils.h
+++ cpp/src/arrow/dataset/rados_utils.h
@@ -39,23 +39,15 @@
 namespace arrow {
 namespace dataset {
 
-/// \brief Convert a 64-bit integer to a buffer.
-ARROW_DS_EXPORT Status Int64ToChar(char* buffer, int64_t num);
-
-/// \brief Convert a buffer to 64-bit integer.
-ARROW_DS_EXPORT Status CharToInt64(char* buffer, int64_t& num);
-
-/// \brief Serialize Expression(s) and Schema to a bufferlist.
 ARROW_DS_EXPORT Status SerializeScanRequestToBufferlist(
     std::shared_ptr<ScanOptions> options, int64_t file_size, ceph::bufferlist& bl);
 
-/// \brief Deserialize Expression(s) and Schema from a bufferlist.
-ARROW_DS_EXPORT Status DeserializeScanRequestFromBufferlist(
-    compute::Expression* filter, compute::Expression* part_expr,
-    std::shared_ptr<Schema>* projection_schema, std::shared_ptr<Schema>* dataset_schema,
-    int64_t& file_size, ceph::bufferlist& bl);
-
-/// \brief Serialize a Table to an Arrow IPC binary buffer.
+ARROW_DS_EXPORT Status DeserializeScanRequestFromBufferlist(compute::Expression* filter,
+                                            compute::Expression* partition,
+                                            std::shared_ptr<Schema>* projected_schema,
+                                            std::shared_ptr<Schema>* dataset_schema,
+                                            int64_t& file_size, ceph::bufferlist& bl);
+  
 ARROW_DS_EXPORT Status SerializeTableToBufferlist(std::shared_ptr<Table>& table,
                                                   ceph::bufferlist& bl);
 
