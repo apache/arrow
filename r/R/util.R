8ead5c4d34b4fd2771d1dbdf3ca18794a3d729fc
Nic Crane <thisisnic@gmail.com>
2021-06-02 22:29:25 +0100

    Explain func attributes and how it works wieth assertthat::assert_that

--- r/R/util.R
+++ r/R/util.R
@@ -115,6 +115,8 @@ is_writable_table <- function(x) {
   inherits(x, c("data.frame", "ArrowTabular"))
 }
 
+# This attribute is used when is_writable is passed into assert_that, and allows 
+# the call to form part of the error message when is_writable is FALSE
 attr(is_writable_table, "fail") <- function(call, env){
   paste0(
     deparse(call$x),
