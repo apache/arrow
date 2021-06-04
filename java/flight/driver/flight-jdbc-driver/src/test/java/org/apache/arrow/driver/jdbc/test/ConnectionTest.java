c29f840acfa5e504fc405600413e53dbdf1168af
Jose Almeida <almeidajcr90@gmail.com>
2021-06-04 17:15:50 -0300

    Add a method to validate the credentials on the Flight Server

--- java/flight/driver/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ConnectionTest.java
+++ java/flight/driver/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ConnectionTest.java
@@ -110,4 +110,23 @@ public class ConnectionTest {
     DriverManager.getConnection(goodUrl, properties);
   }
 
+  /**
+   * Validate the user's credential on a FlightServer.
+   *
+   * @param username flight server username.
+   * @param password flight server password.
+   * @return the result of validation.
+   */
+  private CallHeaderAuthenticator.AuthResult validate(String username, String password) {
+    if (Strings.isNullOrEmpty(username)) {
+      throw CallStatus.UNAUTHENTICATED.withDescription("Credentials not supplied.").toRuntimeException();
+    }
+    final String identity;
+    if (USERNAME_1.equals(username) && PASSWORD_1.equals(password)) {
+      identity = USERNAME_1;
+    } else {
+      throw CallStatus.UNAUTHENTICATED.withDescription("Username or password is invalid.").toRuntimeException();
+    }
+    return () -> identity;
+  }
 }
