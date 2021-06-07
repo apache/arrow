3e3ab3bee8a83bb10033a03f0e05d875aee5bf8c
crystrix <chenxi.li@live.com>
2021-06-07 07:28:55 +0000

    ARROW-12984: [C++][Compute] Passing options parameter of Count/Index aggregation by reference
    The options parameter of `Count` function is passed by value, it's better to be passed by reference like other aggregation functions.
    
    Closes #10459 from Crystrix/arrow-12984
    
    Authored-by: crystrix <chenxi.li@live.com>
    Signed-off-by: Yibo Cai <yibo.cai@arm.com>

--- cpp/src/arrow/compute/api_aggregate.cc
+++ cpp/src/arrow/compute/api_aggregate.cc
@@ -25,7 +25,7 @@ namespace compute {
 // ----------------------------------------------------------------------
 // Scalar aggregates
 
-Result<Datum> Count(const Datum& value, ScalarAggregateOptions options,
+Result<Datum> Count(const Datum& value, const ScalarAggregateOptions& options,
                     ExecContext* ctx) {
   return CallFunction("count", {value}, &options, ctx);
 }
@@ -77,7 +77,7 @@ Result<Datum> TDigest(const Datum& value, const TDigestOptions& options,
   return CallFunction("tdigest", {value}, &options, ctx);
 }
 
-Result<Datum> Index(const Datum& value, IndexOptions options, ExecContext* ctx) {
+Result<Datum> Index(const Datum& value, const IndexOptions& options, ExecContext* ctx) {
   return CallFunction("index", {value}, &options, ctx);
 }
 
