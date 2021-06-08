f6b50799a8f9ba9ea947cc43fc9bcdf219c76121
David Li <li.davidm96@gmail.com>
2021-06-08 12:09:16 -0400

    ARROW-12827: [C++] Improve error message for dataset discovery failure

--- cpp/src/arrow/dataset/file_parquet_test.cc
+++ cpp/src/arrow/dataset/file_parquet_test.cc
@@ -176,7 +176,7 @@ class TestParquetFileFormat : public FileFormatFixtureMixin<ParquetFormatHelper>
 };
 
 TEST_F(TestParquetFileFormat, InspectFailureWithRelevantError) {
-  TestInspectFailureWithRelevantError(StatusCode::IOError);
+  TestInspectFailureWithRelevantError(StatusCode::IOError, "Parquet");
 }
 TEST_F(TestParquetFileFormat, Inspect) { TestInspect(); }
 
