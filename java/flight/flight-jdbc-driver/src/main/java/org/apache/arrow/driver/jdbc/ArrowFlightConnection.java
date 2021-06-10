484f2ad14559d0d7854b8820bdbc631815efb50e
Abner Eduardo Ferreira <abenaru@protonmail.ch>
2021-06-10 16:36:25 -0300

    Change Flight JDBC driver URL parser to accept generic params

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightConnection.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightConnection.java
@@ -17,205 +17,148 @@
 
 package org.apache.arrow.driver.jdbc;
 
-import static org.apache.arrow.driver.jdbc.utils.BaseProperty.HOST;
-import static org.apache.arrow.driver.jdbc.utils.BaseProperty.KEYSTORE_PASS;
-import static org.apache.arrow.driver.jdbc.utils.BaseProperty.KEYSTORE_PATH;
-import static org.apache.arrow.driver.jdbc.utils.BaseProperty.PASSWORD;
-import static org.apache.arrow.driver.jdbc.utils.BaseProperty.PORT;
-import static org.apache.arrow.driver.jdbc.utils.BaseProperty.USERNAME;
-import static org.apache.arrow.driver.jdbc.utils.BaseProperty.USE_TLS;
-import static org.apache.arrow.util.Preconditions.checkNotNull;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.security.GeneralSecurityException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import javax.annotation.Nullable;
 
-import org.apache.arrow.driver.jdbc.client.ArrowFlightClientHandler;
-import org.apache.arrow.driver.jdbc.utils.BaseProperty;
-import org.apache.arrow.flight.CallHeaders;
-import org.apache.arrow.flight.FlightCallHeaders;
-import org.apache.arrow.flight.HeaderCallOption;
+import org.apache.arrow.driver.jdbc.utils.DefaultProperty;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.util.AutoCloseables;
+import org.apache.arrow.util.Preconditions;
 import org.apache.calcite.avatica.AvaticaConnection;
 import org.apache.calcite.avatica.AvaticaFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.netty.util.concurrent.DefaultThreadFactory;
 
 /**
  * Connection to the Arrow Flight server.
  */
-public class ArrowFlightConnection extends AvaticaConnection {
+public final class ArrowFlightConnection extends AvaticaConnection {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(ArrowFlightConnection.class);
-  private final BufferAllocator allocator;
-  private ExecutorService executorService;
+  private BufferAllocator allocator;
 
   // TODO Use this later to run queries.
   @SuppressWarnings("unused")
-  private ArrowFlightClientHandler client;
+  private ArrowFlightClient client;
 
   /**
    * Instantiates a new Arrow Flight Connection.
    *
-   * @param driver  The JDBC driver to use.
+   * @param driver The JDBC driver to use.
    * @param factory The Avatica Factory to use.
-   * @param url     The URL to connect to.
-   * @param info    The properties of this connection.
+   * @param url The URL to connect to.
+   * @param info The properties of this connection.
    * @throws SQLException If the connection cannot be established.
    */
-  protected ArrowFlightConnection(final ArrowFlightJdbcDriver driver,
-                                  final AvaticaFactory factory, final String url, final Properties info)
-      throws SQLException {
+  public ArrowFlightConnection(ArrowFlightJdbcDriver driver,
+      AvaticaFactory factory, String url, Properties info) throws SQLException {
     super(driver, factory, url, info);
-    allocator = new RootAllocator(Integer.MAX_VALUE);
-
+    allocator = new RootAllocator(
+        Integer.MAX_VALUE);
+    
     try {
       loadClient();
-    } catch (final SQLException e) {
-      close();
-      throw new SQLException("Failed to initialize Flight Client.", e);
+    } catch (SQLException e) {
+      allocator.close();
+      throw e;
     }
-  }
-
-  protected final ArrowFlightClientHandler getClient() {
-    return client;
   }
 
   /**
    * Sets {@link #client} based on the properties of this connection.
    *
-   * @throws KeyStoreException        If an error occurs while trying to retrieve KeyStore information.
-   * @throws NoSuchAlgorithmException If a particular cryptographic algorithm is required but does not
-   *                                  exist.
-   * @throws CertificateException     If an error occurs while trying to retrieve certificate
-   *                                  information.
-   * @throws IOException              If an I/O operation fails.
-   * @throws NumberFormatException    If the port number to connect to is invalid.
-   * @throws URISyntaxException       If the URI syntax is invalid.
+   * @throws KeyStoreException
+   *           If an error occurs while trying to retrieve KeyStore information.
+   * @throws NoSuchAlgorithmException
+   *           If a particular cryptographic algorithm is required but does not
+   *           exist.
+   * @throws CertificateException
+   *           If an error occurs while trying to retrieve certificate
+   *           information.
+   * @throws IOException
+   *           If an I/O operation fails.
+   * @throws NumberFormatException
+   *           If the port number to connect to is invalid.
+   * @throws URISyntaxException
+   *           If the URI syntax is invalid.
    */
   private void loadClient() throws SQLException {
 
     if (client != null) {
-      throw new SQLException("Client already loaded.",
-          new IllegalStateException());
+      throw new IllegalStateException("Client already loaded.");
     }
 
-    try {
-      client = ArrowFlightClientHandler.getClient(allocator,
-          getPropertyAsString(HOST), getPropertyAsInteger(PORT),
-          getPropertyAsString(USERNAME), getPropertyAsString(PASSWORD),
-          getHeaders(),
-          getPropertyAsBoolean(USE_TLS), getPropertyAsString(KEYSTORE_PATH), getPropertyAsString(KEYSTORE_PASS));
-    } catch (GeneralSecurityException | IOException e) {
-      throw new SQLException("Failed to connect to the Arrow Flight client.", e);
-    }
-  }
+    String host = (String) info.getOrDefault(DefaultProperty.HOST.toString(),
+        "localhost");
+    Preconditions.checkArgument(!host.trim().isEmpty());
 
-  private boolean getPropertyAsBoolean(BaseProperty property) {
-    return Boolean.parseBoolean(Objects.toString(getPropertyOrDefault(checkNotNull(property))));
-  }
+    int port = Integer.parseInt((String) info.getOrDefault(DefaultProperty.PORT
+        .toString(), "32010"));
+    Preconditions.checkArgument(0 < port && port < 65536);
 
-  @Nullable
-  protected String getPropertyAsString(BaseProperty property) {
-    return (String) getPropertyOrDefault(checkNotNull(property));
-  }
+    @Nullable
+    String username = info.getProperty(DefaultProperty.USER.toString());
 
-  @Nullable
-  protected int getPropertyAsInteger(BaseProperty property) {
-    return Integer.parseInt(Objects.toString(getPropertyOrDefault(checkNotNull(property))));
-  }
+    @Nullable
+    String password = info.getProperty(DefaultProperty.PASS.toString());
 
-  @Nullable
-  private Object getPropertyOrDefault(BaseProperty property) {
-    return info.getOrDefault(property.getName(), property.getDefaultValue());
-  }
+    boolean useTls = ((String) info.getOrDefault(DefaultProperty.USE_TLS
+        .toString(), "false"))
+        .equalsIgnoreCase("true");
+    
+    boolean authenticate = username != null;
 
-  private HeaderCallOption getHeaders() {
+    if (!useTls) {
 
-    final CallHeaders headers = new FlightCallHeaders();
-    final Iterator<Map.Entry<Object, Object>> properties = info.entrySet()
-        .iterator();
+      if (authenticate) {
+        client = ArrowFlightClient.getBasicClientAuthenticated(allocator, host,
+            port, username, password, null);
+        return;
+      }
 
-    while (properties.hasNext()) {
-      final Map.Entry<Object, Object> entry = properties.next();
+      client = ArrowFlightClient.getBasicClientNoAuth(allocator, host, port,
+          null);
+      return;
 
-      headers.insert(Objects.toString(entry.getKey()),
-          Objects.toString(entry.getValue()));
     }
 
-    return new HeaderCallOption(headers);
-  }
+    String keyStorePath = info.getProperty(
+        DefaultProperty.KEYSTORE_PATH.toString());
+    String keyStorePass = info.getProperty(
+        DefaultProperty.KEYSTORE_PATH.toString());
 
-  /**
-   * Gets the {@link ExecutorService} of this connection.
-   *
-   * @return the {@link #executorService}.
-   */
-  public synchronized ExecutorService getExecutorService() {
-    if (executorService == null) {
-      final int threadPoolSize = getPropertyAsInteger(BaseProperty.THREAD_POOL_SIZE);
-      final DefaultThreadFactory threadFactory = new DefaultThreadFactory(this.getClass().getSimpleName());
-      executorService = Executors.newFixedThreadPool(threadPoolSize, threadFactory);
+    if (authenticate) {
+      client = ArrowFlightClient.getEncryptedClientAuthenticated(allocator,
+          host, port, null, username, password, keyStorePath, keyStorePass);
+      return;
     }
 
-    return executorService;
+    client = ArrowFlightClient.getEncryptedClientNoAuth(allocator, host,
+        port, null, keyStorePath, keyStorePass);
   }
 
   @Override
   public void close() throws SQLException {
-    if (executorService != null) {
-      executorService.shutdown();
-    }
-
-    List<Exception> exceptions = new ArrayList<>();
-
     try {
-      AutoCloseables.close(client);
+      client.close();
     } catch (Exception e) {
-      exceptions.add(e);
+      throw new SQLException(
+          "Failed to close the connection " +
+              "to the Arrow Flight client.", e);
     }
 
     try {
-      Collection<BufferAllocator> childAllocators = allocator.getChildAllocators();
-      AutoCloseables.close(childAllocators.toArray(new AutoCloseable[0]));
+      allocator.close();
     } catch (Exception e) {
-      exceptions.add(e);
+      throw new SQLException("Failed to close the resource allocator used " +
+          "by the Arrow Flight client.", e);
     }
 
-    try {
-      AutoCloseables.close(allocator);
-    } catch (final Exception e) {
-      exceptions.add(e);
-    }
-
-    try {
-      super.close();
-    } catch (Exception e) {
-      throw new SQLException(e);
-    }
-
-    exceptions
-        .forEach(exception -> LOGGER.error(
-            exception.getMessage(), exception));
+    super.close();
   }
 
 }
