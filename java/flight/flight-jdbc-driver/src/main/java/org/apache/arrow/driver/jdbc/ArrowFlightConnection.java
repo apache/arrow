d572cfd3f283a13c743e7893a8fc668ce5566c0d
Abner Eduardo Ferreira <abenaru@protonmail.ch>
2021-06-10 16:56:50 -0300

    Fix KeyStore test

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightConnection.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightConnection.java
@@ -129,7 +129,7 @@ public final class ArrowFlightConnection extends AvaticaConnection {
     String keyStorePath = info.getProperty(
         DefaultProperty.KEYSTORE_PATH.toString());
     String keyStorePass = info.getProperty(
-        DefaultProperty.KEYSTORE_PATH.toString());
+        DefaultProperty.KEYSTORE_PASS.toString());
 
     if (authenticate) {
       client = ArrowFlightClient.getEncryptedClientAuthenticated(allocator,
