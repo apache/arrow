952015a4fdc5a4708123393dd09f16e8f935da96
niranda perera <niranda.perera@gmail.com>
2021-06-17 11:11:19 -0400

    adding ifelse bench

---
+++ cpp/src/arrow/compute/kernels/scalar_if_else_benchmark.cc
@@ -0,0 +1,64 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include <arrow/compute/api_scalar.h>
+#include <arrow/testing/gtest_util.h>
+#include <arrow/testing/random.h>
+#include <benchmark/benchmark.h>
+
+namespace arrow {
+namespace compute {
+
+const int64_t elems = 1024 * 1024;
+
+template <typename Type>
+static void IfElseBench(benchmark::State& state) {
+  using CType = typename Type::c_type;
+  auto type = TypeTraits<Type>::type_singleton();
+  using ArrayType = typename TypeTraits<Type>::ArrayType;
+
+  int64_t len = state.range(0);
+
+  random::RandomArrayGenerator rand(/*seed=*/0);
+
+  auto cond = std::static_pointer_cast<BooleanArray>(
+      rand.ArrayOf(boolean(), len, /*null_probability=*/0.01));
+  auto left = std::static_pointer_cast<ArrayType>(
+      rand.ArrayOf(type, len, /*null_probability=*/0.01));
+  auto right = std::static_pointer_cast<ArrayType>(
+      rand.ArrayOf(type, len, /*null_probability=*/0.01));
+
+  for (auto _ : state) {
+    ABORT_NOT_OK(IfElse(cond, left, right));
+  }
+
+  state.SetBytesProcessed(state.iterations() * (len / 8 + 2 * len * sizeof(CType)));
+}
+
+static void IfElseBench64Wide(benchmark::State& state) {
+  return IfElseBench<UInt64Type>(state);
+}
+
+static void IfElseBench32Wide(benchmark::State& state) {
+  return IfElseBench<UInt32Type>(state);
+}
+
+BENCHMARK(IfElseBench32Wide)->Arg(elems);
+BENCHMARK(IfElseBench64Wide)->Arg(elems);
+
+}  // namespace compute
+}  // namespace arrow
