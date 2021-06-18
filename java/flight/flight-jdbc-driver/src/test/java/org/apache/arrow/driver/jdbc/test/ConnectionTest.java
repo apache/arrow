d04f08f5b60152a7eba9c24ff76032f1944ba2b9
Abner Eduardo Ferreira <abenaru@protonmail.ch>
2021-06-18 02:00:51 -0300

    Increase code coverage to match 80% threshold

--- java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ConnectionTest.java
+++ java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ConnectionTest.java
@@ -17,20 +17,25 @@
 
 package org.apache.arrow.driver.jdbc.test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import org.apache.arrow.driver.jdbc.ArrowFlightClient;
+import org.apache.arrow.driver.jdbc.ArrowFlightConnection;
 import org.apache.arrow.driver.jdbc.ArrowFlightJdbcDriver;
+import org.apache.arrow.driver.jdbc.client.ArrowFlightClientHandler;
 import org.apache.arrow.driver.jdbc.test.utils.FlightTestUtils;
 import org.apache.arrow.flight.CallStatus;
 import org.apache.arrow.flight.FlightProducer;
 import org.apache.arrow.flight.FlightServer;
+import org.apache.arrow.flight.HeaderCallOption;
 import org.apache.arrow.flight.auth2.BasicCallHeaderAuthenticator;
 import org.apache.arrow.flight.auth2.CallHeaderAuthenticator;
 import org.apache.arrow.flight.auth2.GeneratedBearerTokenAuthenticator;
@@ -43,6 +48,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.base.Strings;
+
+import io.grpc.Metadata;
 
 /**
  * Tests for {@link Connection}.
@@ -64,15 +71,16 @@ public class ConnectionTest {
   public void setUp() throws Exception {
     allocator = new RootAllocator(Long.MAX_VALUE);
 
-    flightTestUtils = new FlightTestUtils("localhost", "flight1",
-        "woho1", "invalid", "wrong");
+    flightTestUtils = new FlightTestUtils("localhost", "flight1", "woho1",
+        "invalid", "wrong");
 
-    final FlightProducer flightProducer = flightTestUtils.getFlightProducer(allocator);
-    this.server = flightTestUtils.getStartedServer((location -> FlightServer
-        .builder(allocator, location, flightProducer)
-        .headerAuthenticator(new GeneratedBearerTokenAuthenticator(
-            new BasicCallHeaderAuthenticator(this::validate)))
-        .build()));
+    final FlightProducer flightProducer = flightTestUtils
+        .getFlightProducer(allocator);
+    this.server = flightTestUtils.getStartedServer(
+        location -> FlightServer.builder(allocator, location, flightProducer)
+            .headerAuthenticator(new GeneratedBearerTokenAuthenticator(
+                new BasicCallHeaderAuthenticator(this::validate)))
+            .build());
     serverUrl = flightTestUtils.getConnectionPrefix() +
         flightTestUtils.getLocalhost() + ":" + this.server.getPort();
 
@@ -98,7 +106,7 @@ public class ConnectionTest {
       final String password) {
     if (Strings.isNullOrEmpty(username)) {
       throw CallStatus.UNAUTHENTICATED
-      .withDescription("Credentials not supplied.").toRuntimeException();
+          .withDescription("Credentials not supplied.").toRuntimeException();
     }
     final String identity;
     if (flightTestUtils.getUsername1().equals(username) &&
@@ -106,8 +114,8 @@ public class ConnectionTest {
       identity = flightTestUtils.getUsername1();
     } else {
       throw CallStatus.UNAUTHENTICATED
-      .withDescription("Username or password is invalid.")
-      .toRuntimeException();
+          .withDescription("Username or password is invalid.")
+          .toRuntimeException();
     }
     return () -> identity;
   }
@@ -127,8 +135,8 @@ public class ConnectionTest {
     properties.put("user", flightTestUtils.getUsername1());
     properties.put("password", flightTestUtils.getPassword1());
 
-    try (Connection connection = DriverManager
-        .getConnection(serverUrl, properties)) {
+    try (Connection connection = DriverManager.getConnection(serverUrl,
+        properties)) {
       assert connection.isValid(300);
     }
   }
@@ -161,12 +169,12 @@ public class ConnectionTest {
    *           on error.
    */
   @Test
-  public void testGetBasicClientAuthenticatedShouldOpenConnection() throws Exception {
+  public void testGetBasicClientAuthenticatedShouldOpenConnection()
+      throws Exception {
 
-    try (ArrowFlightClient client = ArrowFlightClient.getBasicClientAuthenticated(
+    try (ArrowFlightClientHandler client = ArrowFlightClientHandler.getClient(
         allocator, flightTestUtils.getLocalhost(), this.server.getPort(),
-        flightTestUtils.getUsername1(), flightTestUtils.getPassword1(),
-        null)) {
+        flightTestUtils.getUsername1(), flightTestUtils.getPassword1())) {
       assertNotNull(client);
     }
   }
@@ -193,6 +201,61 @@ public class ConnectionTest {
     }
   }
 
+  @Test(expected = SQLException.class)
+  public void testReloadClientShouldThrowException()
+      throws Exception {
+    try (Connection connection = DriverManager.getConnection(serverUrl, new Properties())) {
+      Method loadClient = ((ArrowFlightConnection) connection)
+          .getClass().getDeclaredMethod("loadClient");
+      loadClient.setAccessible(true);
+      try {
+        loadClient.invoke(connection);
+      } catch (InvocationTargetException e) {
+        Throwable throwable = e.getCause();
+        if (throwable instanceof SQLException) {
+          throw (SQLException) throwable;
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testGetHeadersShouldReturnPropertiesAsHeaders()
+      throws Exception {
+
+    Properties properties = new Properties();
+    properties.put("TEST", "PROPERTY");
+    properties.put("ONCE", "MORE");
+    properties.put("SHOULD", "SAVE");
+
+    try (Connection connection = DriverManager.getConnection(serverUrl, properties)) {
+      Method getHeaders = ((ArrowFlightConnection) connection)
+          .getClass().getDeclaredMethod("getHeaders");
+      getHeaders.setAccessible(true);
+
+      HeaderCallOption headers =
+          (HeaderCallOption) getHeaders.invoke(connection);
+
+      Field propertiesMetadata =
+          headers.getClass().getDeclaredField("propertiesMetadata");
+      propertiesMetadata.setAccessible(true);
+      Metadata metadata = (Metadata) propertiesMetadata.get(headers);
+
+      assertEquals(
+          metadata.get(Metadata.Key.of("TEST", Metadata.ASCII_STRING_MARSHALLER)),
+          "PROPERTY"
+      );
+      assertEquals(
+          metadata.get(Metadata.Key.of("ONCE", Metadata.ASCII_STRING_MARSHALLER)),
+          "MORE"
+      );
+      assertEquals(
+          metadata.get(Metadata.Key.of("SHOULD", Metadata.ASCII_STRING_MARSHALLER)),
+          "SAVE"
+      );
+    }
+  }
+
   /**
    * Try to instantiate a basic FlightClient.
    *
@@ -202,9 +265,8 @@ public class ConnectionTest {
   @Test
   public void testGetBasicClientNoAuthShouldOpenConnection() throws Exception {
 
-    try (ArrowFlightClient client = ArrowFlightClient.getBasicClientNoAuth(
-        allocator, flightTestUtils.getLocalhost(), this.server.getPort(),
-        null)) {
+    try (ArrowFlightClientHandler client = ArrowFlightClientHandler.getClient(
+        allocator, flightTestUtils.getLocalhost(), this.server.getPort())) {
       assertNotNull(client);
     }
   }
