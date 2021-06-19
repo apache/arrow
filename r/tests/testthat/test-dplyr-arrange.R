2440d1f4060eae4d1ad52a04be140cd97539f807
Ian Cook <ianmcook@gmail.com>
2021-06-18 22:51:25 -0400

    Add test

--- r/tests/testthat/test-dplyr-arrange.R
+++ r/tests/testthat/test-dplyr-arrange.R
@@ -210,4 +210,11 @@ test_that("arrange() with bad inputs", {
     "not found",
     fixed = TRUE
   )
+  expect_error(
+    tbl %>%
+      Table$create() %>%
+      arrange(desc(int, chr)),
+    "expects only one argument",
+    fixed = TRUE
+  )
 })
