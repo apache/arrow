72e521c711969e7aa487e9d6149de24528263f3c
Jose Almeida <almeidajcr90@gmail.com>
2021-06-21 14:30:33 -0300

    Create base for test of resultSetMetadata

--- java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ResultSetMetadataTest.java
+++ java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ResultSetMetadataTest.java
@@ -29,28 +29,24 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Types;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Random;
 
+import org.apache.arrow.driver.jdbc.ArrowFlightJdbcDriver;
 import org.apache.arrow.driver.jdbc.utils.BaseProperty;
 import org.hamcrest.CoreMatchers;
-import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
-import me.alexpanov.net.FreePortFinder;
-
 public class ResultSetMetadataTest {
 
   private static final Map<BaseProperty, Object> properties;
   private static ResultSetMetaData metadata;
-
-  private static Connection connection;
 
   @Rule
   public ErrorCollector collector = new ErrorCollector();
@@ -61,7 +57,7 @@ public class ResultSetMetadataTest {
   static {
     properties = new HashMap<>();
     properties.put(HOST, "localhost");
-    properties.put(PORT, FreePortFinder.findFreeLocalPort());
+    properties.put(PORT, (new Random()).nextInt(65536));
     properties.put(USERNAME, "flight-test-user");
     properties.put(PASSWORD, "flight-test-password");
     rule = new FlightServerTestRule(properties);
@@ -69,123 +65,19 @@ public class ResultSetMetadataTest {
 
   @BeforeClass
   public static void setup() throws SQLException {
-    connection = rule.getConnection();
+    Properties properties = new Properties();
+    properties.put(USERNAME.getEntry().getKey(), rule.getProperty(USERNAME));
+    properties.put(PASSWORD.getEntry().getKey(), rule.getProperty(PASSWORD));
 
-    final Statement statement = connection.createStatement();
-    ResultSet resultSet = statement.executeQuery(FlightServerTestRule.METADATA_QUERY_STRING);
+    try (Connection connection = (new ArrowFlightJdbcDriver())
+        .connect("jdbc:arrow-flight://" +
+                rule.getProperty(HOST) + ":" +
+                rule.getProperty(PORT),
+            properties)) {
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM METADATA");
 
-    metadata = resultSet.getMetaData();
-  }
-
-  @AfterClass
-  public static void teardown() throws SQLException {
-    connection.close();
-  }
-
-  /**
-   * Test if {@link ResultSetMetaData} object is not null.
-   */
-  @Test
-  public void testShouldGetResultSetMetadata() {
-    collector.checkThat(metadata, CoreMatchers.is(notNullValue()));
-  }
-
-  /**
-   * Test if {@link ResultSetMetaData#getColumnCount()} returns the correct values.
-   *
-   * @throws SQLException in case of error.
-   */
-  @Test
-  public void testShouldGetColumnCount() throws SQLException {
-    final int columnCount = metadata.getColumnCount();
-
-    assert columnCount == 3;
-  }
-
-  /**
-   * Test if {@link ResultSetMetaData#getColumnTypeName(int)}  returns the correct type name for each
-   * column.
-   *
-   * @throws SQLException in case of error.
-   */
-  @Test
-  public void testShouldGetColumnTypesName() throws SQLException {
-    final String firstColumn = metadata.getColumnTypeName(1);
-    final String secondColumn = metadata.getColumnTypeName(2);
-    final String thirdColumn = metadata.getColumnTypeName(3);
-
-    collector.checkThat(firstColumn, equalTo("Int"));
-    collector.checkThat(secondColumn, equalTo("Utf8"));
-    collector.checkThat(thirdColumn, equalTo("FloatingPoint"));
-  }
-
-  /**
-   * Test if {@link ResultSetMetaData#getColumnTypeName(int)} passing an column index that does not exist.
-   *
-   * @throws SQLException in case of error.
-   */
-  @Test(expected = IndexOutOfBoundsException.class)
-  public void testShouldGetColumnTypesNameFromOutOfBoundIndex() throws SQLException {
-    final String outOfBoundColumn = metadata.getColumnTypeName(4);
-
-    Assert.fail();
-  }
-
-  /**
-   * Test if {@link ResultSetMetaData#getColumnName(int)} returns the correct name for each column.
-   *
-   * @throws SQLException in case of error.
-   */
-  @Test
-  public void testShouldGetColumnNames() throws SQLException {
-    final String firstColumn = metadata.getColumnName(1);
-    final String secondColumn = metadata.getColumnName(2);
-    final String thirdColumn = metadata.getColumnName(3);
-
-    collector.checkThat(firstColumn, equalTo("integer0"));
-    collector.checkThat(secondColumn, equalTo("string1"));
-    collector.checkThat(thirdColumn, equalTo("float2"));
-  }
-
-
-  /**
-   * Test {@link ResultSetMetaData#getColumnTypeName(int)} passing an column index that does not exist.
-   *
-   * @throws SQLException in case of error.
-   */
-  @Test(expected = IndexOutOfBoundsException.class)
-  public void testShouldGetColumnNameFromOutOfBoundIndex() throws SQLException {
-    final String outOfBoundColumn = metadata.getColumnName(4);
-
-    Assert.fail();
-  }
-
-  /**
-   * Test if {@link ResultSetMetaData#getColumnType(int)}returns the correct values.
-   *
-   * @throws SQLException in case of error.
-   */
-  @Test
-  public void testShouldGetColumnType() throws SQLException {
-    final int firstColumn = metadata.getColumnType(1);
-    final int secondColumn = metadata.getColumnType(2);
-    final int thirdColumn = metadata.getColumnType(3);
-
-    collector.checkThat(firstColumn, equalTo(Types.BIGINT));
-    collector.checkThat(secondColumn, equalTo(Types.VARCHAR));
-    collector.checkThat(thirdColumn, equalTo(Types.FLOAT));
-  }
-
-
-  /**
-   * Test if {@link ResultSetMetaData#getColumnTypeName(int)} passing an column index that does not exist.
-   *
-   * @throws SQLException in case of error.
-   */
-  @Test(expected = IndexOutOfBoundsException.class)
-  public void testShouldGetColumnTypesFromOutOfBoundIndex() throws SQLException {
-    final int outOfBoundColumn = metadata.getColumnType(4);
-
-    Assert.fail();
+      metadata = resultSet.getMetaData();
+    }
   }
 }
