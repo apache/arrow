c43fab3d621bedef15470a1be43570be2026af20
Jonathan Keane <jkeane@gmail.com>
2021-06-21 17:08:04 -0500

    ARROW-13127: [R] Valgrind nightly errors
    Closes #10560 from jonkeane/ARROW-13127-valgrind-c-export
    
    Authored-by: Jonathan Keane <jkeane@gmail.com>
    Signed-off-by: Jonathan Keane <jkeane@gmail.com>

--- r/tests/testthat/test-RecordBatch.R
+++ r/tests/testthat/test-RecordBatch.R
@@ -422,22 +422,22 @@ test_that("record_batch() scalar recycling with vectors", {
 })
 
 test_that("record_batch() scalar recycling with Scalars, Arrays, and ChunkedArrays", {
-  
+
   expect_data_frame(
     record_batch(a = Array$create(1:10), b = Scalar$create(5)),
     tibble::tibble(a = 1:10, b = 5)
   )
-  
+
   expect_data_frame(
     record_batch(a = Array$create(1:10), b = Array$create(5)),
     tibble::tibble(a = 1:10, b = 5)
   )
-  
+
   expect_data_frame(
     record_batch(a = Array$create(1:10), b = ChunkedArray$create(5)),
     tibble::tibble(a = 1:10, b = 5)
   )
-  
+
 })
 
 test_that("record_batch() no recycling with tibbles", {
@@ -448,7 +448,7 @@ test_that("record_batch() no recycling with tibbles", {
     ),
     regexp = "All input tibbles or data.frames must have the same number of rows"
   )
-  
+
   expect_error(
     record_batch(
       tibble::tibble(a = 1:10),
@@ -573,7 +573,6 @@ test_that("RecordBatchReader to C-interface", {
 
   # export the RecordBatchReader via the C-interface
   stream_ptr <- allocate_arrow_array_stream()
-  on.exit(delete_arrow_array_stream(stream_ptr))
   scan <- Scanner$create(tab)
   reader <- scan$ToRecordBatchReader()
   reader$export_to_c(stream_ptr)
@@ -583,9 +582,11 @@ test_that("RecordBatchReader to C-interface", {
   tab_from_c_new <- circle$read_table()
   expect_equal(tab, tab_from_c_new)
 
+  # must clean up the pointer or we leak
+  delete_arrow_array_stream(stream_ptr)
+
   # export the RecordBatchStreamReader via the C-interface
   stream_ptr_new <- allocate_arrow_array_stream()
-  on.exit(delete_arrow_array_stream(stream_ptr_new))
   bytes <- write_to_raw(example_data)
   expect_type(bytes, "raw")
   reader_new <- RecordBatchStreamReader$create(bytes)
@@ -595,6 +596,9 @@ test_that("RecordBatchReader to C-interface", {
   circle_new <- RecordBatchStreamReader$import_from_c(stream_ptr_new)
   tab_from_c_new <- circle_new$read_table()
   expect_equal(tab, tab_from_c_new)
+
+  # must clean up the pointer or we leak
+  delete_arrow_array_stream(stream_ptr_new)
 })
 
 test_that("RecordBatch to C-interface", {
@@ -603,13 +607,13 @@ test_that("RecordBatch to C-interface", {
   # export the RecordBatch via the C-interface
   schema_ptr <- allocate_arrow_schema()
   array_ptr <- allocate_arrow_array()
-  on.exit({
-    delete_arrow_schema(schema_ptr)
-    delete_arrow_array(array_ptr)
-  })
   batch$export_to_c(array_ptr, schema_ptr)
 
   # then import it and check that the roundtripped value is the same
   circle <- RecordBatch$import_from_c(array_ptr, schema_ptr)
-  expect_equal(batch, circle)
+  expect_equal
+
+  # must clean up the pointers or we leak
+  delete_arrow_schema(schema_ptr)
+  delete_arrow_array(array_ptr)
 })
