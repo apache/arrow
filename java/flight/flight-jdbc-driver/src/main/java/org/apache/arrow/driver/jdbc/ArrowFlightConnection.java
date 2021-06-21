6c3e77f1cbe354d6e42c82d7978897cbb9ee305c
Abner Eduardo Ferreira <abenaru@protonmail.ch>
2021-06-21 16:43:34 -0300

    Replace Map.Entry<Object, Object> with Entry<String, Object> @ ArrowFlightConnection

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightConnection.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightConnection.java
@@ -124,13 +124,13 @@ public class ArrowFlightConnection extends AvaticaConnection {
     }
 
     // =================== [ LOCATION CONFIG ] ===================
-    final Map.Entry<Object, Object> forHost = HOST.getEntry();
+    final Map.Entry<String, Object> forHost = HOST.getEntry();
 
     final String host = Objects.toString(info.getOrDefault(forHost.getKey(),
         forHost.getValue()));
     Preconditions.checkArgument(!Strings.isNullOrEmpty(host));
 
-    final Map.Entry<Object, Object> forPort = PORT.getEntry();
+    final Map.Entry<String, Object> forPort = PORT.getEntry();
 
     final int port = Integer.parseInt(Objects
             .toString(info.getOrDefault(forPort.getKey(), forPort.getValue())));
@@ -138,27 +138,27 @@ public class ArrowFlightConnection extends AvaticaConnection {
         "Port number must be between exclusive range (0, 65536).");
 
     // =================== [ CREDENTIALS CONFIG ] ===================
-    final Map.Entry<Object, Object> forUsername = USERNAME.getEntry();
-    final String usernameKey = (String) forUsername.getKey();
+    final Map.Entry<String, Object> forUsername = USERNAME.getEntry();
+    final String usernameKey = forUsername.getKey();
     final String usernameValue = (String) forUsername.getValue();
 
     final String username = (String) info.getOrDefault(usernameKey, usernameValue);
 
-    final Map.Entry<Object, Object> forPassword = PASSWORD.getEntry();
-    final String passwordKey = (String) forPassword.getKey();
+    final Map.Entry<String, Object> forPassword = PASSWORD.getEntry();
+    final String passwordKey = forPassword.getKey();
     final String passwordValue = (String) forPassword.getValue();
 
     final String password = (String) info.getOrDefault(passwordKey, passwordValue);
 
     // =================== [ ENCRYPTION CONFIG ] ===================
-    final Map.Entry<Object, Object> forKeyStorePath = KEYSTORE_PATH.getEntry();
-    final String keyStorePathKey = (String) forKeyStorePath.getKey();
+    final Map.Entry<String, Object> forKeyStorePath = KEYSTORE_PATH.getEntry();
+    final String keyStorePathKey = forKeyStorePath.getKey();
     final String keyStorePathValue = (String) forKeyStorePath.getValue();
 
     final String keyStorePath = (String) info.getOrDefault(keyStorePathKey, keyStorePathValue);
 
-    final Map.Entry<Object, Object> forKeyStorePass = KEYSTORE_PASS.getEntry();
-    final String keyStorePassKey = (String) forKeyStorePass.getKey();
+    final Map.Entry<String, Object> forKeyStorePass = KEYSTORE_PASS.getEntry();
+    final String keyStorePassKey = forKeyStorePass.getKey();
     final String keyStorePassValue = (String) forKeyStorePass.getValue();
 
     final String keyStorePassword = (String) info.getOrDefault(keyStorePassKey, keyStorePassValue);
