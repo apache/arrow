4803b8be2abd79dde9051148cf63599601ae9982
Nic Crane <thisisnic@gmail.com>
2021-06-22 03:33:41 +0100

    Entirely refactor wday formulation so it is achievable via Expressions

--- r/R/dplyr-functions.R
+++ r/R/dplyr-functions.R
@@ -447,45 +447,73 @@ nse_funcs$second <- function(x) {
   Expression$create("add", Expression$create("second", x), Expression$create("subsecond", x))
 }
 
+
 nse_funcs$wday <- function(x, label = FALSE, abbr = TRUE, week_start = getOption("lubridate.week.start", 7)) {
   if (label) {
     arrow_not_supported("Label argument")
   }
-  # After ARROW-13054 is completed, we can change the 2 lines below and delete the function get_date_offset
-  offset <- get_date_offset(week_start)
-  Expression$create("add", Expression$create("day_of_week", x), Expression$scalar(offset))
-}
-
-#' Get date offset
-#' 
-#' Arrow's `day_of_week` kernel counts from 0 (Monday) to 6 (Sunday), whereas
-#' `lubridate::wday` counts from 1 to 7, and allows users to specify which day
-#' of the week is first (Sunday by default).  This function converts the returned
-#' day of the week back to the value that would be returned by lubridate by 
-#' providing offset values based on the specified week_start day, and adding 1
-#' so the returned value is 1-indexed instead of 0-indexed.
-#' 
-#' @param week_start Day on which week starts following ISO conventions - 1 means Monday, 7 means Sunday.
-#' @return Offset value, integer
-#' @keywords internal
-get_date_offset <- function(week_start) {
-  if (week_start < 1 || week_start > 7) {
-    abort(c(
-      "The value of `week_start` must be between 1 and 7",
-      x = paste("`week_start` =", week_start)
+  
+  # After ARROW-13054 is completed, we can refactor this for simplicity
+  # 
+  # Arrow's `day_of_week` kernel counts from 0 (Monday) to 6 (Sunday), whereas
+  # `lubridate::wday` counts from 1 to 7, and allows users to specify which day
+  # of the week is first (Sunday by default).  This Expression converts the returned
+  # day of the week back to the value that would be returned by lubridate by
+  # providing offset values based on the specified week_start day, and adding 1
+  # so the returned value is 1-indexed instead of 0-indexed.
+  # 
+  # This looks complex but if you look at the lubridate implementation of wday,
+  # you can see that this is the same maths, but with 1 added at the end to 
+  # account for the fact that R is 1-based and the Arrow implementation is 0-based
+  # 
+  # overall formula to convert from arrow::wday to lubridate::wday is:
+  #  (3 +wday(day) + (5 - start)) %% 7) + 1
+  # 
+  # To calculate e1%%e2, we can rearrange it as:
+  # {e1 - e2 * ( e1 %/% e2 )}
+  # 
+  # use for testing: x <- Expression$field_ref("day")
+  # week_start = 1
+  
+  # e1 = 3 +wday(day) + (5 - start)
+  e1 = Expression$create(
+    "add_checked",
+    # 3 + wday(date)
+    Expression$create("add_checked",
+                      Expression$scalar(3),
+                      Expression$create("day_of_week", x)
+    ),
+    # (5 - start)
+    Expression$create("subtract_checked",
+                      Expression$scalar(5),
+                      Expression$scalar(week_start)
+    )
+  )
+  
+  e2 = Expression$scalar(7)
+  
+  # (e1 - e2 * ( e1 %/% e2 )) + 1
+  Expression$create(
+    "add_checked",
+    Expression$scalar(1),                
+    # e1 - e2 * ( e1 %/% e2 )
+    Expression$create(
+      "subtract_checked",
+      # e1
+      e1,
+      # e2 * ( e1 %/% e2 )
+      Expression$create(
+        "multiply_checked",
+        e2,
+        # e1 %/% e2; because as.integer(x/y) == x%/%y
+        Expression$create(
+          "cast",
+          Expression$create("divide_checked", e1, e2),
+          options = cast_options(to_type = int32(), allow_float_truncate = TRUE,
+                                 allow_decimal_truncate = TRUE
+          )
+        )
       )
     )
-  }
-  offset_vals <- c(0:-4, 2, 1)
-  
-  # offset by 1 due to R being 1-indexed
-  off_val <- offset_vals[week_start] + 1
-  
-  # so that day numbers go from 1 to 7 then repeat
-  if(off_val > 7){
-    off_val <- off_val - floor(off_val/7) * 7
-  }
-  
-  off_val
+  )
 }
-
