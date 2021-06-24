503f52d080649b4ed0621536181d36903b7254ef
Kevin Gurney <kgurney@mathworks.com>
2021-06-24 16:21:01 -0400

    Integrate GoogleTest with the CMake build system for the MATLAB interface.
    Co-authored-by: Fiona La <fionala7@gmail.com>

---
+++ matlab/src/placeholder_test.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+namespace arrow {
+namespace matlab {
+namespace test {
+// TODO: Remove this placeholder test.
+TEST(PlaceholderTestSuite, PlaceholderTestCase) { ASSERT_TRUE(true); }
+}  // namespace test
+}  // namespace matlab
+}  // namespace arrow
