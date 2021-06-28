a26e59d2634a437256271bc1bfba4253d1ae5752
Ian Cook <ianmcook@gmail.com>
2021-06-28 16:03:01 -0400

    Add binding for find_substring_regex options

--- r/src/compute.cpp
+++ r/src/compute.cpp
@@ -233,7 +233,8 @@ std::shared_ptr<arrow::compute::FunctionOptions> make_compute_options(
   }
 
   if (func_name == "match_substring" || func_name == "match_substring_regex" ||
-      func_name == "match_like" || func_name == "find_substring") {
+      func_name == "find_substring" || func_name == "find_substring_regex" ||
+      func_name == "match_like") {
     using Options = arrow::compute::MatchSubstringOptions;
     bool ignore_case = false;
     if (!Rf_isNull(options["ignore_case"])) {
