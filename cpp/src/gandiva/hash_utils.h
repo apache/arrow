e784ac24872d9701e03e1e42f87341c0286a0c70
frank400 <j.victorhuguenin2018@gmail.com>
2021-07-06 16:50:15 -0300

    Rename gdv_hash_using_ssl to gdv_hash_using_openssl

--- cpp/src/gandiva/hash_utils.h
+++ cpp/src/gandiva/hash_utils.h
@@ -33,9 +33,9 @@ const char* gdv_hash_using_sha1(int64_t context, const void* message,
                                 size_t message_length, int32_t* out_length);
 
 GANDIVA_EXPORT
-const char* gdv_hash_using_ssl(int64_t context, const void* message,
-                               size_t message_length, const EVP_MD* hash_type,
-                               uint32_t result_buf_size, int32_t* out_length);
+const char* gdv_hash_using_openssl(int64_t context, const void* message,
+                                   size_t message_length, const EVP_MD* hash_type,
+                                   uint32_t result_buf_size, int32_t* out_length);
 
 GANDIVA_EXPORT
 const char* gdv_hash_using_md5(int64_t context, const void* message,
