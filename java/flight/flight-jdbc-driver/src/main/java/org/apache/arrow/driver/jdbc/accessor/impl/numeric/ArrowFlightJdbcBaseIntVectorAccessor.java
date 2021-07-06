97635283b853eecbb9c3a886003c06e7146bb354
Jose Almeida <almeidajcr90@gmail.com>
2021-07-06 17:45:35 -0300

    Add accessor for the BaseIntVectors

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/accessor/impl/numeric/ArrowFlightJdbcBaseIntVectorAccessor.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/accessor/impl/numeric/ArrowFlightJdbcBaseIntVectorAccessor.java
@@ -17,25 +17,12 @@
 
 package org.apache.arrow.driver.jdbc.accessor.impl.numeric;
 
-import static org.apache.arrow.driver.jdbc.accessor.impl.numeric.ArrowFlightJdbcNumericGetter.Getter;
-import static org.apache.arrow.driver.jdbc.accessor.impl.numeric.ArrowFlightJdbcNumericGetter.createGetter;
-
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.nio.ByteBuffer;
 import java.util.function.IntSupplier;
 
 import org.apache.arrow.driver.jdbc.accessor.ArrowFlightJdbcAccessor;
-import org.apache.arrow.driver.jdbc.accessor.impl.numeric.ArrowFlightJdbcNumericGetter.NumericHolder;
 import org.apache.arrow.vector.BaseIntVector;
-import org.apache.arrow.vector.BigIntVector;
-import org.apache.arrow.vector.IntVector;
-import org.apache.arrow.vector.SmallIntVector;
-import org.apache.arrow.vector.TinyIntVector;
-import org.apache.arrow.vector.UInt1Vector;
-import org.apache.arrow.vector.UInt2Vector;
-import org.apache.arrow.vector.UInt4Vector;
-import org.apache.arrow.vector.UInt8Vector;
 
 /**
  * Accessor for the arrow types: TinyIntVector, SmallIntVector, IntVector, BigIntVector,
@@ -43,88 +30,22 @@ import org.apache.arrow.vector.UInt8Vector;
  */
 public class ArrowFlightJdbcBaseIntVectorAccessor extends ArrowFlightJdbcAccessor {
 
-  private final boolean isUnsigned;
-  private final int bytesToAllocate;
-  private final Getter getter;
-  private final NumericHolder holder;
+  private final BaseIntVector vector;
+  private final IntSupplier currentRowSupplier;
 
-  public ArrowFlightJdbcBaseIntVectorAccessor(UInt1Vector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, true, UInt1Vector.TYPE_WIDTH);
-  }
-
-  public ArrowFlightJdbcBaseIntVectorAccessor(UInt2Vector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, true, UInt2Vector.TYPE_WIDTH);
-  }
-
-  public ArrowFlightJdbcBaseIntVectorAccessor(UInt4Vector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, true, UInt4Vector.TYPE_WIDTH);
-  }
-
-  public ArrowFlightJdbcBaseIntVectorAccessor(UInt8Vector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, true, UInt8Vector.TYPE_WIDTH);
-  }
-
-  public ArrowFlightJdbcBaseIntVectorAccessor(TinyIntVector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, false, TinyIntVector.TYPE_WIDTH);
-  }
-
-  public ArrowFlightJdbcBaseIntVectorAccessor(SmallIntVector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, false, SmallIntVector.TYPE_WIDTH);
-  }
-
-  public ArrowFlightJdbcBaseIntVectorAccessor(IntVector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, false, IntVector.TYPE_WIDTH);
-  }
-
-  public ArrowFlightJdbcBaseIntVectorAccessor(BigIntVector vector,
-                                              IntSupplier currentRowSupplier) {
-    this(vector, currentRowSupplier, false, BigIntVector.TYPE_WIDTH);
-  }
-
-  private ArrowFlightJdbcBaseIntVectorAccessor(BaseIntVector vector,
-                                               IntSupplier currentRowSupplier,
-                                               boolean isUnsigned,
-                                               int bytesToAllocate) {
-    super(currentRowSupplier);
-    this.holder = new NumericHolder();
-    this.getter = createGetter(vector);
-    this.isUnsigned = isUnsigned;
-    this.bytesToAllocate = bytesToAllocate;
+  public ArrowFlightJdbcBaseIntVectorAccessor(BaseIntVector vector, IntSupplier currentRowSupplier) {
+    this.vector = vector;
+    this.currentRowSupplier = currentRowSupplier;
   }
 
   @Override
   public long getLong() {
-    getter.get(getCurrentRow(), holder);
-
-    this.wasNull = holder.isSet == 0;
-    if (this.wasNull) {
-      return 0;
-    }
-
-    return holder.value;
-  }
-
-  @Override
-  public Class<?> getObjectClass() {
-    return Long.class;
+    return vector.getValueAsLong(currentRowSupplier.getAsInt());
   }
 
   @Override
   public String getString() {
-    final long number = getLong();
-
-    if (this.wasNull) {
-      return null;
-    } else {
-      return isUnsigned ? Long.toUnsignedString(number) : Long.toString(number);
-    }
+    return Long.toString(getLong());
   }
 
   @Override
@@ -154,46 +75,11 @@ public class ArrowFlightJdbcBaseIntVectorAccessor extends ArrowFlightJdbcAccesso
 
   @Override
   public byte[] getBytes() {
-    final ByteBuffer buffer = ByteBuffer.allocate(bytesToAllocate);
-    final long value = getLong();
-
-    if (this.wasNull) {
-      return null;
-    } else if (bytesToAllocate == Byte.BYTES) {
-      return buffer.put((byte) value).array();
-    } else if (bytesToAllocate == Short.BYTES) {
-      return buffer.putShort((short) value).array();
-    } else if (bytesToAllocate == Integer.BYTES) {
-      return buffer.putInt((int) value).array();
-    } else if (bytesToAllocate == Long.BYTES) {
-      return buffer.putLong(value).array();
-    }
-
-    throw new UnsupportedOperationException();
+    return ByteBuffer.allocate(Long.BYTES).putLong(getLong()).array();
   }
 
   @Override
   public BigDecimal getBigDecimal() {
-    final BigDecimal value = BigDecimal.valueOf(getLong());
-    return this.wasNull ? null : value;
-  }
-
-  @Override
-  public BigDecimal getBigDecimal(int scale) {
-    final BigDecimal value = BigDecimal.valueOf(this.getDouble()).setScale(scale, RoundingMode.HALF_UP);
-    return this.wasNull ? null : value;
-  }
-
-  @Override
-  public Object getObject() {
-    long value = getLong();
-    return this.wasNull ? null : value;
-  }
-
-  @Override
-  public boolean getBoolean() {
-    final long value = getLong();
-
-    return value != 0;
+    return BigDecimal.valueOf(getLong());
   }
 }
