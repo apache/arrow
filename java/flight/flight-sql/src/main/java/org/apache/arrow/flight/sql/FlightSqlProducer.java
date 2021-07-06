2e9c7f549cc690f305ddea63417be388a1b003b7
Kyle Porter <kporter@dremio.com>
2021-07-06 16:28:54 -0700

    Additional CR changes.
    Note - FlightSqlExample is not functional and needs to be updated.

--- java/flight/flight-sql/src/main/java/org/apache/arrow/flight/sql/FlightSqlProducer.java
+++ java/flight/flight-sql/src/main/java/org/apache/arrow/flight/sql/FlightSqlProducer.java
@@ -289,6 +289,8 @@ public interface FlightSqlProducer extends FlightProducer, AutoCloseable {
     } else {
       throw CallStatus.INVALID_ARGUMENT.withDescription("Invalid action provided.").toRuntimeException();
     }
+
+    throw Status.INVALID_ARGUMENT.asRuntimeException();
   }
 
   /**
