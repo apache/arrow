c6ace067795d9c164082442e08f07d5a7c456bf8
Benjamin Kietzman <bengilgit@gmail.com>
2021-07-08 12:16:35 -0400

    msvc: that's a lambda body not an initializer list

--- cpp/src/arrow/dataset/scanner.cc
+++ cpp/src/arrow/dataset/scanner.cc
@@ -653,8 +653,8 @@ Result<EnumeratedRecordBatchGenerator> AsyncScanner::ScanBatchesUnorderedAsync(
   std::shared_ptr<void> stop_producing{
       nullptr, [plan, exec_context](...) {
         bool not_finished_yet = plan->finished().TryAddCallback([&] {
-          // keep plan alive until it finishes
-          return [plan, exec_context](const Status&) {};
+          auto keepalive_until_finish = [plan, exec_context](const Status&) {};
+          return keepalive_until_finish;
         });
 
         if (not_finished_yet) {
