c41caf3f050024adfedea2afd99f98ededa0f9a6
Rok <rok@mihevc.org>
2021-07-12 01:00:56 +0200

    Adding file_csv.h import.

--- r/src/dataset.cpp
+++ r/src/dataset.cpp
@@ -22,6 +22,7 @@
 #include <arrow/array.h>
 #include <arrow/compute/api.h>
 #include <arrow/dataset/api.h>
+#include <arrow/dataset/file_csv.h>
 #include <arrow/filesystem/filesystem.h>
 #include <arrow/ipc/writer.h>
 #include <arrow/table.h>
