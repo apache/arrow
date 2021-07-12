9a4d419a1d1730fdddf5fad9a3ddd39ee52f9467
Jonathan Keane <jkeane@gmail.com>
2021-07-12 09:02:37 -0500

    3.5 workaround for altrep

--- r/src/altrep.cpp
+++ r/src/altrep.cpp
@@ -23,7 +23,27 @@
 
 #if defined(HAS_ALTREP)
 
+#if R_VERSION < R_Version(3, 6, 0)
+
+// workaround because R's <R_ext/Altrep.h> not so conveniently uses `class`
+// as a variable name, and C++ is not happy about that
+//
+// SEXP R_new_altrep(R_altrep_class_t class, SEXP data1, SEXP data2);
+//
+#define class klass
+
+// Because functions declared in <R_ext/Altrep.h> have C linkage
+extern "C" {
 #include <R_ext/Altrep.h>
+}
+
+// undo the workaround
+#undef class
+
+#else
+#include <R_ext/Altrep.h>
+#endif
+
 #include <arrow/array.h>
 
 namespace arrow {
