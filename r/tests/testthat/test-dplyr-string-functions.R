e2238582e2a2bf20a68a967145fe1a7b2337a997
Nic Crane <thisisnic@gmail.com>
2021-07-14 09:50:31 -0400

    ARROW-13324: [R] Typo in bindings for utf8_reverse and ascii_reverse
    This PR fixes a typo in the binding for stri_reverse
    
    Closes #10715 from thisisnic/ARROW-13324
    
    Authored-by: Nic Crane <thisisnic@gmail.com>
    Signed-off-by: Neal Richardson <neal.p.richardson@gmail.com>

--- r/tests/testthat/test-dplyr-string-functions.R
+++ r/tests/testthat/test-dplyr-string-functions.R
@@ -774,8 +774,7 @@ test_that("arrow_find_substring and arrow_find_substring_regex", {
 })
 
 test_that("stri_reverse and arrow_ascii_reverse functions", {
-  # TODO: these actually aren't implemented (ARROW-12869)
-  # Fix them, then remove the `warning = TRUE` arguments
+  
   df_ascii <- tibble(x = c("Foo\nand bar", "baz\tand qux and quux"))
 
   df_utf8 <- tibble(x = c("Foo\u00A0\u0061nd\u00A0bar", "\u0062az\u00A0and\u00A0qux\u3000and\u00A0quux"))
@@ -784,16 +783,14 @@ test_that("stri_reverse and arrow_ascii_reverse functions", {
     input %>%
       mutate(x = stri_reverse(x)) %>%
       collect(),
-    df_utf8,
-    warning = TRUE # Remove me
+    df_utf8
   )
 
   expect_dplyr_equal(
     input %>%
       mutate(x = stri_reverse(x)) %>%
       collect(),
-    df_ascii,
-    warning = TRUE # Remove me
+    df_ascii
   )
 
   expect_equivalent(
