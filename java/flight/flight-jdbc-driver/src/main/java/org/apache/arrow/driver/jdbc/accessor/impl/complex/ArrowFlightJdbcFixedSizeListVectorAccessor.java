84e89488e71c3842b017addad31b6832732ab2f7
Rafael Telles <rafael@telles.dev>
2021-07-19 15:00:23 -0300

    Fix CheckStyle issues

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/accessor/impl/complex/ArrowFlightJdbcFixedSizeListVectorAccessor.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/accessor/impl/complex/ArrowFlightJdbcFixedSizeListVectorAccessor.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.arrow.driver.jdbc.accessor.impl.complex;
 
 import java.util.function.IntSupplier;
@@ -5,6 +22,9 @@ import java.util.function.IntSupplier;
 import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.complex.FixedSizeListVector;
 
+/**
+ * Accessor for the Arrow type {@link FixedSizeListVector}.
+ */
 public class ArrowFlightJdbcFixedSizeListVectorAccessor extends AbstractArrowFlightJdbcListVectorAccessor {
 
   private final FixedSizeListVector vector;
@@ -15,12 +35,12 @@ public class ArrowFlightJdbcFixedSizeListVectorAccessor extends AbstractArrowFli
   }
 
   @Override
-  protected long getStart(int index) {
+  protected long getStartOffset(int index) {
     return (long) vector.getListSize() * index;
   }
 
   @Override
-  protected long getEnd(int index) {
+  protected long getEndOffset(int index) {
     return (long) vector.getListSize() * (index + 1);
   }
 
