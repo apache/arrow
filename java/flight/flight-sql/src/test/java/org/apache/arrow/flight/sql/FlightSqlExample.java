c4ce4f41e0876d8ec7e5d43276818ffc99675aba
Jose Almeida <almeidajcr90@gmail.com>
2021-07-26 15:12:31 -0300

    Refactor prepareStatement to use Cache Object

--- java/flight/flight-sql/src/test/java/org/apache/arrow/flight/sql/FlightSqlExample.java
+++ java/flight/flight-sql/src/test/java/org/apache/arrow/flight/sql/FlightSqlExample.java
@@ -608,7 +608,7 @@ public class FlightSqlExample implements FlightSqlProducer, AutoCloseable {
                                      StreamListener<Result> listener) {
     try {
       preparedStatementLoadingCache.invalidate(
-          request.getPreparedStatementHandle());
+          request.getPreparedStatementHandle().toStringUtf8();
     } catch (Exception e) {
       listener.onError(e);
     } finally {
