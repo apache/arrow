d61543dbdad972d1d739fcc084c970dbca452249
Rafael Telles <rafael@telles.dev>
2021-07-27 14:44:37 -0300

    Rename to ArrowFlightJdbcFlightStreamResultSet

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcFactory.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcFactory.java
@@ -90,7 +90,7 @@ public class ArrowFlightJdbcFactory extends AbstractFactory {
                                                                final Meta.Frame frame) throws SQLException {
     final ResultSetMetaData metaData = newResultSetMetaData(statement, signature);
 
-    return new ArrowFlightJdbcVectorFlightStreamResultSet(statement, state, signature, metaData, timeZone, frame);
+    return new ArrowFlightJdbcFlightStreamResultSet(statement, state, signature, metaData, timeZone, frame);
   }
 
   @Override
