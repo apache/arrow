41f4cb72a0b45ba649e6338d51eb372087c6336c
Jose Almeida <almeidajcr90@gmail.com>
2021-07-27 11:08:18 -0300

    Add new imports

--- java/flight/flight-sql/src/test/java/org/apache/arrow/flight/sql/FlightSqlExample.java
+++ java/flight/flight-sql/src/test/java/org/apache/arrow/flight/sql/FlightSqlExample.java
@@ -45,6 +45,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
