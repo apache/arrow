e7131c57184f441911be5c6700fd62eb0f818212
Abner Eduardo Ferreira <abenaru@protonmail.ch>
2021-07-30 12:00:01 -0300

    Fix checkstyle and Maven dependency issues

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/utils/FlightStreamQueue.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/utils/FlightStreamQueue.java
@@ -24,7 +24,6 @@ import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import org.apache.arrow.flight.FlightStream;
