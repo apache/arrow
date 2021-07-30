44b1b63c2df860918f2dad91d8b5e6e4f1a07b36
michalursa <michal@ursacomputing.com>
2021-07-29 17:13:29 -0700

    GrouperFastImpl - fixing unaligned memory access
    Temp vectors used in grouper implementation were allocated using stack implementation.
    The buffer addresses returned by the stack were not aligned to the size of requested vector element.
    Changing this to have returned buffers aligned to 8 byte boundaries.

--- cpp/src/arrow/compute/exec/util.h
+++ cpp/src/arrow/compute/exec/util.h
@@ -26,6 +26,7 @@
 #include "arrow/status.h"
 #include "arrow/util/cpu_info.h"
 #include "arrow/util/logging.h"
+#include "arrow/util/bit_util.h"
 
 #if defined(__clang__) || defined(__GNUC__)
 #define BYTESWAP(x) __builtin_bswap64(x)
@@ -66,9 +67,19 @@ class TempVectorStack {
   }
 
  private:
+  int64_t PaddedAllocationSize(int64_t num_bytes) {
+    // Round up allocation size to multiple of 8 bytes
+    // to avoid returning temp vectors with unaligned address.
+    //
+    // Also add padding at the end to facilitate loads and stores
+    // using SIMD when number of vector elements is not divisible
+    // by the number of SIMD lanes.
+    //
+    return ::arrow::BitUtil::RoundUp(num_bytes, sizeof(int64_t)) + padding;
+  }
   void alloc(uint32_t num_bytes, uint8_t** data, int* id) {
     int64_t old_top = top_;
-    top_ += num_bytes + padding;
+    top_ += PaddedAllocationSize(num_bytes);
     // Stack overflow check
     ARROW_DCHECK(top_ <= buffer_size_);
     *data = buffer_->mutable_data() + old_top;
@@ -76,7 +87,7 @@ class TempVectorStack {
   }
   void release(int id, uint32_t num_bytes) {
     ARROW_DCHECK(num_vectors_ == id + 1);
-    int64_t size = num_bytes + padding;
+    int64_t size = PaddedAllocationSize(num_bytes);
     ARROW_DCHECK(top_ >= size);
     top_ -= size;
     --num_vectors_;
