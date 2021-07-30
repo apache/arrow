ba3651da5c72c275b338449686a27f04433bb769
Jose Almeida <almeidajcr90@gmail.com>
2021-07-30 13:23:57 -0300

    Create tests to validate if the limit of a row, set by user, is being respected

--- java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ResultSetTest.java
+++ java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/test/ResultSetTest.java
@@ -152,6 +152,36 @@ public class ResultSetTest {
     }
   }
 
+
+  /**
+   * Tests whether the {@link ArrowFlightJdbcDriver} query only returns only the
+   * amount of value set by {@link org.apache.calcite.avatica.AvaticaStatement#setMaxRows(int)}.
+   *
+   * @throws Exception If the connection fails to be established.
+   */
+  @Test
+  public void testShouldRunSelectQuerySettingMaxRowLimit() throws Exception {
+    try (Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery("SELECT * FROM TEST")) {
+
+      final int maxRowsLimit = 3;
+      statement.setMaxRows(maxRowsLimit);
+
+      collector.checkThat(statement.getMaxRows(), is(maxRowsLimit));
+
+      int count = 0;
+      int columns = 6;
+      for (; resultSet.next(); count++) {
+        for (int column = 1; column <= columns; column++) {
+          resultSet.getObject(column);
+        }
+        collector.checkThat("Test Name #" + count, is(resultSet.getString(2)));
+      }
+
+      collector.checkThat(maxRowsLimit, is(count));
+    }
+  }
+
   /**
    * Tests whether the {@link ArrowFlightJdbcDriver} fails upon attempting
    * to run an invalid query.
