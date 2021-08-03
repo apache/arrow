cdf277119b07458d26d18bd7ec59556e3d2f6723
niranda perera <niranda.perera@gmail.com>
2021-08-03 00:42:11 -0400

    adding serial test case

--- cpp/src/arrow/compute/exec/plan_test.cc
+++ cpp/src/arrow/compute/exec/plan_test.cc
@@ -581,32 +581,57 @@ TEST(ExecPlanExecution, ScalarSourceScalarAggSink) {
       }))));
 }
 
+void GenerateBatchesFromString(const std::shared_ptr<Schema>& schema,
+                               const std::vector<util::string_view>& json_strings,
+                               BatchesWithSchema* out_batches) {
+  std::vector<ValueDescr> descrs;
+  for (auto&& field : schema->fields()) {
+    descrs.emplace_back(field->type());
+  }
+
+  for (auto&& s : json_strings) {
+    out_batches->batches.push_back(ExecBatchFromJSON(descrs, s));
+  }
+
+  out_batches->schema = schema;
+}
+
 TEST(ExecPlanExecution, SourceHashLeftSemiJoin) {
-  // TODO (Niranda) add this!
-  /*  for (bool parallel : {false, true}) {
-      SCOPED_TRACE(parallel ? "parallel/merged" : "serial");
+  BatchesWithSchema l_batches, r_batches;
 
-      auto input = MakeGroupableBatches(*/
-  /*multiplicity=*/                    /*parallel ? 100 : 1);
+  GenerateBatchesFromString(schema({field("l_i32", int32()), field("l_str", utf8())}),
+                            {R"([[0,"d"], [1,"b"]])", R"([[2,"d"], [3,"a"], [4,"a"]])",
+                             R"([[5,"b"], [6,"c"], [7,"e"], [8,"e"]])"},
+                            &l_batches);
 
-ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make());
+  GenerateBatchesFromString(
+      schema({field("r_str", utf8()), field("r_i32", int32())}),
+      {R"([["f", 0], ["b", 1], ["b", 2]])", R"([["c", 3], ["g", 4]])", R"([["e", 5]])"},
+      &r_batches);
 
-ASSERT_OK_AND_ASSIGN(auto source,
-       MakeTestSourceNode(plan.get(), "source", input,
-                          */
-  /*parallel=*//*parallel, */ /*slow=*//*false));
-ASSERT_OK_AND_ASSIGN(
-auto gby, MakeGroupByNode(source, "gby", */
-  /*keys=*//*{"str"}, */ /*targets=*/  /*{"i32"},
-{{"hash_sum", nullptr}}));
-auto sink_gen = MakeSinkNode(gby, "sink");
+  SCOPED_TRACE("serial");
 
-ASSERT_THAT(StartAndCollect(plan.get(), sink_gen),
-Finishes(ResultWith(UnorderedElementsAreArray({ExecBatchFromJSON(
-{int64(), utf8()},
-parallel ? R"([[800, "alfa"], [1000, "beta"], [400, "gama"]])"
-: R"([[8, "alfa"], [10, "beta"], [4, "gama"]])")}))));
-}*/
+  ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make());
+
+  ASSERT_OK_AND_ASSIGN(auto l_source,
+                       MakeTestSourceNode(plan.get(), "l_source", l_batches,
+                                          /*parallel=*/false,
+                                          /*slow=*/false));
+  ASSERT_OK_AND_ASSIGN(auto r_source,
+                       MakeTestSourceNode(plan.get(), "r_source", r_batches,
+                                          /*parallel=*/false,
+                                          /*slow=*/false));
+
+  ASSERT_OK_AND_ASSIGN(
+      auto semi_join,
+      MakeHashLeftSemiJoinNode(l_source, r_source, "l_semi_join",
+                               /*left_keys=*/{"l_str"}, /*right_keys=*/{"r_str"}));
+  auto sink_gen = MakeSinkNode(semi_join, "sink");
+
+  ASSERT_THAT(
+      StartAndCollect(plan.get(), sink_gen),
+      Finishes(ResultWith(UnorderedElementsAreArray({ExecBatchFromJSON(
+          {int64(), utf8()}, R"([[1,"b"], [5,"b"], [6,"c"], [7,"e"], [8,"e"]])")}))));
 }
 
 }  // namespace compute
