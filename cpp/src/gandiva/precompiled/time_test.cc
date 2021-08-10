17a22dbd065b9ae357a5d040ff09967abc5432ea
frank400 <j.victorhuguenin2018@gmail.com>
2021-08-09 21:54:33 -0300

    Updates subtract function test case to check if it's considering leap years

--- cpp/src/gandiva/precompiled/time_test.cc
+++ cpp/src/gandiva/precompiled/time_test.cc
@@ -457,8 +457,8 @@ TEST(TestTime, TimeStampAdd) {
       StringToTimestamp("1999-10-27 00:00:00"));
 
   EXPECT_EQ(
-      subtract_day_time_interval_timestamp(StringToTimestamp("2000-02-27 00:00:00"), 4),
-      StringToTimestamp("2000-02-23 00:00:00"));
+      subtract_day_time_interval_timestamp(StringToTimestamp("2000-03-04 00:00:00"), 4),
+      StringToTimestamp("2000-02-29 00:00:00"));
 
   // 77309411328000001 represents 1 day and 5 hours of interval to be add.
   EXPECT_EQ(subtract_day_time_interval_time32(MILLIS_IN_DAY - 10 * MILLIS_IN_HOUR,
