d376199d5a9873fb702e8170f68cb0cfa0dc80a7
Abner Eduardo Ferreira <abenaru@protonmail.ch>
2021-08-23 17:43:39 -0300

    Rename ArrowFlightConnection#handler to ArrowFlightConnection#clientHandler

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcFlightStreamResultSet.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcFlightStreamResultSet.java
@@ -81,7 +81,7 @@ public class ArrowFlightJdbcFlightStreamResultSet extends ArrowFlightJdbcVectorS
     loadNewQueue();
     getFlightStreamQueue().enqueue(
         ((ArrowFlightConnection) getStatement().getConnection())
-            .getHandler().getStreams(signature.sql));
+            .getClientHandler().getStreams(signature.sql));
     loadNewFlightStream();
 
     // Ownership of the root will be passed onto the cursor.
