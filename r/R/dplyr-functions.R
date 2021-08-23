79cf5009a7b8277721818075ef6d1b47d3b6f986
Nic Crane <thisisnic@gmail.com>
2021-08-23 19:18:24 +0100

    Remove accidental function

--- r/R/dplyr-functions.R
+++ r/R/dplyr-functions.R
@@ -551,15 +551,6 @@ nse_funcs$pmax <- function(..., na.rm = FALSE) {
   )
 }
 
-nse_funcs$mean <- function(..., na.rm = FALSE) {
-  build_expr(
-    "mean",
-    ...,
-    options = list(na.rm = na.rm)
-  )
-}
-
-
 nse_funcs$str_pad <- function(string, width, side = c("left", "right", "both"), pad = " ") {
   assert_that(is_integerish(width))
   side <- match.arg(side)
