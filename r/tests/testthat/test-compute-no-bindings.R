ed2a01e5f0dbc938bac5d6210254effd679d4003
Nic Crane <thisisnic@gmail.com>
2021-09-06 09:58:17 +0100

    Move skip_if_not_avaialble and refer to utf8proc specifically

--- r/tests/testthat/test-compute-no-bindings.R
+++ r/tests/testthat/test-compute-no-bindings.R
@@ -15,9 +15,10 @@
 # specific language governing permissions and limitations
 # under the License.
 
-skip_if_not(arrow_available())
-
-test_that("non-bound compute kernels using TrimOptions", {
+test_that("non-bound string compute kernels using TrimOptions", {
+  
+  skip_if_not_available("utf8proc")
+  
   expect_equal(
     call_function("utf8_trim", Scalar$create("abracadabra"), options = list(characters = "ab")),
     Scalar$create("racadabr")
