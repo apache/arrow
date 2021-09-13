f2cb97747e105caab3c3e6843a826deafbe0cce3
Matthew Topol <mtopol@factset.com>
2021-09-13 10:30:30 -0400

    ARROW-13646: [Go][Parquet] adding the parquet metadata package
    Here's the next chunk of code following the merging of #10716
    
    Thankfully the metadata package is actually relatively small compared to everything else so far.
    
    @emkornfield @sbinet @nickpoorman for visibility
    
    Closes #10951 from zeroshade/parquet-metadata-package
    
    Authored-by: Matthew Topol <mtopol@factset.com>
    Signed-off-by: Matthew Topol <mtopol@factset.com>

---
+++ go/parquet/metadata/metadata_test.go
@@ -0,0 +1,336 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metadata_test
+
+import (
+	"context"
+	"testing"
+	"unsafe"
+
+	"github.com/apache/arrow/go/parquet"
+	"github.com/apache/arrow/go/parquet/metadata"
+	"github.com/apache/arrow/go/parquet/schema"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func generateTableMetaData(schema *schema.Schema, props *parquet.WriterProperties, nrows int64, statsInt, statsFloat metadata.EncodedStatistics) (*metadata.FileMetaData, error) {
+	fbuilder := metadata.NewFileMetadataBuilder(schema, props, nil)
+	rg1Builder := fbuilder.AppendRowGroup()
+	// metadata
+	// row group 1
+	col1Builder := rg1Builder.NextColumnChunk()
+	col2Builder := rg1Builder.NextColumnChunk()
+	// column metadata
+	dictEncodingStats := map[parquet.Encoding]int32{parquet.Encodings.RLEDict: 1}
+	dataEncodingStats := map[parquet.Encoding]int32{parquet.Encodings.Plain: 1, parquet.Encodings.RLE: 1}
+	statsInt.Signed = true
+	col1Builder.SetStats(statsInt)
+	statsFloat.Signed = true
+	col2Builder.SetStats(statsFloat)
+
+	col1Builder.Finish(metadata.ChunkMetaInfo{nrows / 2, 4, 0, 10, 512, 600}, true, false, metadata.EncodingStats{dictEncodingStats, dataEncodingStats}, nil)
+	col2Builder.Finish(metadata.ChunkMetaInfo{nrows / 2, 24, 0, 30, 512, 600}, true, false, metadata.EncodingStats{dictEncodingStats, dataEncodingStats}, nil)
+
+	rg1Builder.SetNumRows(nrows / 2)
+	rg1Builder.Finish(1024, -1)
+
+	// rowgroup2 metadata
+	rg2Builder := fbuilder.AppendRowGroup()
+	col1Builder = rg2Builder.NextColumnChunk()
+	col2Builder = rg2Builder.NextColumnChunk()
+	// column metadata
+	col1Builder.SetStats(statsInt)
+	col2Builder.SetStats(statsFloat)
+	col1Builder.Finish(metadata.ChunkMetaInfo{nrows / 2, 6, 0, 10, 512, 600}, true, false, metadata.EncodingStats{dictEncodingStats, dataEncodingStats}, nil)
+	col2Builder.Finish(metadata.ChunkMetaInfo{nrows / 2, 16, 0, 26, 512, 600}, true, false, metadata.EncodingStats{dictEncodingStats, dataEncodingStats}, nil)
+
+	rg2Builder.SetNumRows(nrows / 2)
+	rg2Builder.Finish(1024, -1)
+
+	return fbuilder.Finish()
+}
+
+func assertStatsSet(t *testing.T, m *metadata.ColumnChunkMetaData) {
+	ok, err := m.StatsSet()
+	assert.NoError(t, err)
+	assert.True(t, ok)
+}
+
+func assertStats(t *testing.T, m *metadata.ColumnChunkMetaData) metadata.TypedStatistics {
+	s, err := m.Statistics()
+	assert.NoError(t, err)
+	assert.NotNil(t, s)
+	return s
+}
+
+func TestBuildAccess(t *testing.T) {
+	props := parquet.NewWriterProperties(parquet.WithVersion(parquet.V2_LATEST))
+
+	fields := schema.FieldList{
+		schema.NewInt32Node("int_col", parquet.Repetitions.Required, -1),
+		schema.NewFloat32Node("float_col", parquet.Repetitions.Required, -1),
+	}
+	root, err := schema.NewGroupNode("schema", parquet.Repetitions.Repeated, fields, -1)
+	require.NoError(t, err)
+	schema := schema.NewSchema(root)
+
+	var (
+		nrows      int64   = 1000
+		intMin     int32   = 100
+		intMax     int32   = 200
+		floatMin   float32 = 100.100
+		floatMax   float32 = 200.200
+		statsInt   metadata.EncodedStatistics
+		statsFloat metadata.EncodedStatistics
+	)
+
+	statsInt.SetNullCount(0).
+		SetDistinctCount(nrows).
+		SetMin((*(*[4]byte)(unsafe.Pointer(&intMin)))[:]).
+		SetMax((*(*[4]byte)(unsafe.Pointer(&intMax)))[:])
+
+	statsFloat.SetNullCount(0).
+		SetDistinctCount(nrows).
+		SetMin((*(*[4]byte)(unsafe.Pointer(&floatMin)))[:]).
+		SetMax((*(*[4]byte)(unsafe.Pointer(&floatMax)))[:])
+
+	faccessor, err := generateTableMetaData(schema, props, nrows, statsInt, statsFloat)
+	require.NoError(t, err)
+	serialized, err := faccessor.SerializeString(context.Background())
+	assert.NoError(t, err)
+	faccessorCopy, err := metadata.NewFileMetaData([]byte(serialized), nil)
+	assert.NoError(t, err)
+
+	for _, accessor := range []*metadata.FileMetaData{faccessor, faccessorCopy} {
+		assert.Equal(t, nrows, accessor.NumRows)
+		assert.Len(t, accessor.RowGroups, 2)
+		assert.EqualValues(t, parquet.V2_LATEST, accessor.Version())
+		assert.Equal(t, parquet.DefaultCreatedBy, accessor.GetCreatedBy())
+		assert.Equal(t, 3, accessor.NumSchemaElements())
+
+		rg1Access := accessor.RowGroup(0)
+		assert.Equal(t, 2, rg1Access.NumColumns())
+		assert.Equal(t, nrows/2, rg1Access.NumRows())
+		assert.Equal(t, int64(1024), rg1Access.TotalByteSize())
+
+		rg1Col1, err := rg1Access.ColumnChunk(0)
+		assert.NoError(t, err)
+		rg1Col2, err := rg1Access.ColumnChunk(1)
+		assert.NoError(t, err)
+		assertStatsSet(t, rg1Col1)
+		assertStatsSet(t, rg1Col2)
+		assert.Equal(t, statsInt.Min, assertStats(t, rg1Col1).EncodeMin())
+		assert.Equal(t, statsInt.Max, assertStats(t, rg1Col1).EncodeMax())
+		assert.Equal(t, statsFloat.Min, assertStats(t, rg1Col2).EncodeMin())
+		assert.Equal(t, statsFloat.Max, assertStats(t, rg1Col2).EncodeMax())
+		assert.Zero(t, assertStats(t, rg1Col1).NullCount())
+		assert.Zero(t, assertStats(t, rg1Col2).NullCount())
+		assert.Equal(t, nrows, assertStats(t, rg1Col1).DistinctCount())
+		assert.Equal(t, nrows, assertStats(t, rg1Col2).DistinctCount())
+		assert.Equal(t, metadata.DefaultCompressionType, rg1Col1.Compression())
+		assert.Equal(t, metadata.DefaultCompressionType, rg1Col2.Compression())
+		assert.Equal(t, nrows/2, rg1Col1.NumValues())
+		assert.Equal(t, nrows/2, rg1Col2.NumValues())
+		assert.Len(t, rg1Col1.Encodings(), 3)
+		assert.Len(t, rg1Col2.Encodings(), 3)
+		assert.EqualValues(t, 512, rg1Col1.TotalCompressedSize())
+		assert.EqualValues(t, 512, rg1Col2.TotalCompressedSize())
+		assert.EqualValues(t, 600, rg1Col1.TotalUncompressedSize())
+		assert.EqualValues(t, 600, rg1Col2.TotalUncompressedSize())
+		assert.EqualValues(t, 4, rg1Col1.DictionaryPageOffset())
+		assert.EqualValues(t, 24, rg1Col2.DictionaryPageOffset())
+		assert.EqualValues(t, 10, rg1Col1.DataPageOffset())
+		assert.EqualValues(t, 30, rg1Col2.DataPageOffset())
+		assert.Len(t, rg1Col1.EncodingStats(), 3)
+		assert.Len(t, rg1Col2.EncodingStats(), 3)
+
+		rg2Access := accessor.RowGroup(1)
+		assert.Equal(t, 2, rg2Access.NumColumns())
+		assert.Equal(t, nrows/2, rg2Access.NumRows())
+		assert.EqualValues(t, 1024, rg2Access.TotalByteSize())
+
+		rg2Col1, err := rg2Access.ColumnChunk(0)
+		assert.NoError(t, err)
+		rg2Col2, err := rg2Access.ColumnChunk(1)
+		assert.NoError(t, err)
+		assertStatsSet(t, rg1Col1)
+		assertStatsSet(t, rg1Col2)
+		assert.Equal(t, statsInt.Min, assertStats(t, rg1Col1).EncodeMin())
+		assert.Equal(t, statsInt.Max, assertStats(t, rg1Col1).EncodeMax())
+		assert.Equal(t, statsFloat.Min, assertStats(t, rg1Col2).EncodeMin())
+		assert.Equal(t, statsFloat.Max, assertStats(t, rg1Col2).EncodeMax())
+		assert.Zero(t, assertStats(t, rg1Col1).NullCount())
+		assert.Zero(t, assertStats(t, rg1Col2).NullCount())
+		assert.Equal(t, nrows, assertStats(t, rg1Col1).DistinctCount())
+		assert.Equal(t, nrows, assertStats(t, rg1Col2).DistinctCount())
+		assert.Equal(t, metadata.DefaultCompressionType, rg2Col1.Compression())
+		assert.Equal(t, metadata.DefaultCompressionType, rg2Col2.Compression())
+		assert.Equal(t, nrows/2, rg2Col1.NumValues())
+		assert.Equal(t, nrows/2, rg2Col2.NumValues())
+		assert.Len(t, rg2Col1.Encodings(), 3)
+		assert.Len(t, rg2Col2.Encodings(), 3)
+		assert.EqualValues(t, 512, rg2Col1.TotalCompressedSize())
+		assert.EqualValues(t, 512, rg2Col2.TotalCompressedSize())
+		assert.EqualValues(t, 600, rg2Col1.TotalUncompressedSize())
+		assert.EqualValues(t, 600, rg2Col2.TotalUncompressedSize())
+		assert.EqualValues(t, 6, rg2Col1.DictionaryPageOffset())
+		assert.EqualValues(t, 16, rg2Col2.DictionaryPageOffset())
+		assert.EqualValues(t, 10, rg2Col1.DataPageOffset())
+		assert.EqualValues(t, 26, rg2Col2.DataPageOffset())
+		assert.Len(t, rg2Col1.EncodingStats(), 3)
+		assert.Len(t, rg2Col2.EncodingStats(), 3)
+
+		assert.Empty(t, rg2Col1.FilePath())
+		accessor.SetFilePath("/foo/bar/bar.parquet")
+		assert.Equal(t, "/foo/bar/bar.parquet", rg2Col1.FilePath())
+	}
+
+	faccessor2, err := generateTableMetaData(schema, props, nrows, statsInt, statsFloat)
+	require.NoError(t, err)
+	faccessor.AppendRowGroups(faccessor2)
+	assert.Len(t, faccessor.RowGroups, 4)
+	assert.Equal(t, nrows*2, faccessor.NumRows)
+	assert.EqualValues(t, parquet.V2_LATEST, faccessor.Version())
+	assert.Equal(t, parquet.DefaultCreatedBy, faccessor.GetCreatedBy())
+	assert.Equal(t, 3, faccessor.NumSchemaElements())
+
+	faccessor1, err := faccessor.Subset([]int{2, 3})
+	require.NoError(t, err)
+	assert.True(t, faccessor1.Equals(faccessor2))
+
+	faccessor1, err = faccessor2.Subset([]int{0})
+	require.NoError(t, err)
+
+	next, err := faccessor.Subset([]int{0})
+	require.NoError(t, err)
+	faccessor1.AppendRowGroups(next)
+
+	sub, err := faccessor.Subset([]int{2, 0})
+	require.NoError(t, err)
+	assert.True(t, faccessor1.Equals(sub))
+}
+
+func TestV1VersionMetadata(t *testing.T) {
+	props := parquet.NewWriterProperties(parquet.WithVersion(parquet.V1_0))
+
+	fields := schema.FieldList{
+		schema.NewInt32Node("int_col", parquet.Repetitions.Required, -1),
+		schema.NewFloat32Node("float_col", parquet.Repetitions.Required, -1),
+	}
+	root, err := schema.NewGroupNode("schema", parquet.Repetitions.Repeated, fields, -1)
+	require.NoError(t, err)
+	schema := schema.NewSchema(root)
+
+	fbuilder := metadata.NewFileMetadataBuilder(schema, props, nil)
+	faccessor, err := fbuilder.Finish()
+	require.NoError(t, err)
+	assert.EqualValues(t, parquet.V1_0, faccessor.Version())
+}
+
+func TestKeyValueMetadata(t *testing.T) {
+	props := parquet.NewWriterProperties(parquet.WithVersion(parquet.V1_0))
+
+	fields := schema.FieldList{
+		schema.NewInt32Node("int_col", parquet.Repetitions.Required, -1),
+		schema.NewFloat32Node("float_col", parquet.Repetitions.Required, -1),
+	}
+	root, err := schema.NewGroupNode("schema", parquet.Repetitions.Repeated, fields, -1)
+	require.NoError(t, err)
+	schema := schema.NewSchema(root)
+	kvmeta := metadata.NewKeyValueMetadata()
+	kvmeta.Append("test_key", "test_value")
+
+	fbuilder := metadata.NewFileMetadataBuilder(schema, props, kvmeta)
+	faccessor, err := fbuilder.Finish()
+	require.NoError(t, err)
+
+	assert.True(t, faccessor.KeyValueMetadata().Equals(kvmeta))
+}
+
+func TestApplicationVersion(t *testing.T) {
+	version := metadata.NewAppVersion("parquet-mr version 1.7.9")
+	version1 := metadata.NewAppVersion("parquet-mr version 1.8.0")
+	version2 := metadata.NewAppVersion("parquet-cpp version 1.0.0")
+	version3 := metadata.NewAppVersion("")
+	version4 := metadata.NewAppVersion("parquet-mr version 1.5.0ab-cdh5.5.0+cd (build abcd)")
+	version5 := metadata.NewAppVersion("parquet-mr")
+
+	assert.Equal(t, "parquet-mr", version.App)
+	assert.Equal(t, 1, version.Version.Major)
+	assert.Equal(t, 7, version.Version.Minor)
+	assert.Equal(t, 9, version.Version.Patch)
+
+	assert.Equal(t, "parquet-cpp", version2.App)
+	assert.Equal(t, 1, version2.Version.Major)
+	assert.Equal(t, 0, version2.Version.Minor)
+	assert.Equal(t, 0, version2.Version.Patch)
+
+	assert.Equal(t, "parquet-mr", version4.App)
+	assert.Equal(t, "abcd", version4.Build)
+	assert.Equal(t, 1, version4.Version.Major)
+	assert.Equal(t, 5, version4.Version.Minor)
+	assert.Equal(t, 0, version4.Version.Patch)
+	assert.Equal(t, "ab", version4.Version.Unknown)
+	assert.Equal(t, "cdh5.5.0", version4.Version.PreRelease)
+	assert.Equal(t, "cd", version4.Version.BuildInfo)
+
+	assert.Equal(t, "parquet-mr", version5.App)
+	assert.Equal(t, 0, version5.Version.Major)
+	assert.Equal(t, 0, version5.Version.Minor)
+	assert.Equal(t, 0, version5.Version.Patch)
+
+	assert.True(t, version.LessThan(version1))
+
+	var stats metadata.EncodedStatistics
+	assert.False(t, version1.HasCorrectStatistics(parquet.Types.Int96, schema.NoLogicalType{}, stats, schema.SortUNKNOWN))
+	assert.True(t, version.HasCorrectStatistics(parquet.Types.Int32, schema.NoLogicalType{}, stats, schema.SortSIGNED))
+	assert.False(t, version.HasCorrectStatistics(parquet.Types.ByteArray, schema.NoLogicalType{}, stats, schema.SortSIGNED))
+	assert.True(t, version1.HasCorrectStatistics(parquet.Types.ByteArray, schema.NoLogicalType{}, stats, schema.SortSIGNED))
+	assert.False(t, version1.HasCorrectStatistics(parquet.Types.ByteArray, schema.NoLogicalType{}, stats, schema.SortUNSIGNED))
+	assert.True(t, version3.HasCorrectStatistics(parquet.Types.FixedLenByteArray, schema.NoLogicalType{}, stats, schema.SortSIGNED))
+
+	// check that the old stats are correct if min and max are the same regardless of sort order
+	var statsStr metadata.EncodedStatistics
+	statsStr.SetMin([]byte("a")).SetMax([]byte("b"))
+	assert.False(t, version1.HasCorrectStatistics(parquet.Types.ByteArray, schema.NoLogicalType{}, statsStr, schema.SortUNSIGNED))
+	statsStr.SetMax([]byte("a"))
+	assert.True(t, version1.HasCorrectStatistics(parquet.Types.ByteArray, schema.NoLogicalType{}, statsStr, schema.SortUNSIGNED))
+
+	// check that the same holds true for ints
+	var (
+		intMin int32 = 100
+		intMax int32 = 200
+	)
+	var statsInt metadata.EncodedStatistics
+	statsInt.SetMin((*(*[4]byte)(unsafe.Pointer(&intMin)))[:])
+	statsInt.SetMax((*(*[4]byte)(unsafe.Pointer(&intMax)))[:])
+	assert.False(t, version1.HasCorrectStatistics(parquet.Types.ByteArray, schema.NoLogicalType{}, statsInt, schema.SortUNSIGNED))
+	statsInt.SetMax((*(*[4]byte)(unsafe.Pointer(&intMin)))[:])
+	assert.True(t, version1.HasCorrectStatistics(parquet.Types.ByteArray, schema.NoLogicalType{}, statsInt, schema.SortUNSIGNED))
+}
+
+func TestCheckBadDecimalStats(t *testing.T) {
+	version1 := metadata.NewAppVersion("parquet-cpp version 3.0.0")
+	version2 := metadata.NewAppVersion("parquet-cpp-arrow version 3.0.0")
+	version3 := metadata.NewAppVersion("parquet-cpp-arrow version 4.0.0")
+
+	var stats metadata.EncodedStatistics
+	assert.False(t, version1.HasCorrectStatistics(parquet.Types.FixedLenByteArray, schema.NewDecimalLogicalType(5, 0), stats, schema.SortSIGNED))
+	assert.False(t, version2.HasCorrectStatistics(parquet.Types.FixedLenByteArray, schema.NewDecimalLogicalType(5, 0), stats, schema.SortSIGNED))
+	assert.True(t, version3.HasCorrectStatistics(parquet.Types.FixedLenByteArray, schema.NewDecimalLogicalType(5, 0), stats, schema.SortSIGNED))
+}
---
+++ go/parquet/metadata/row_group.go
@@ -0,0 +1,173 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metadata
+
+import (
+	"reflect"
+
+	"github.com/apache/arrow/go/parquet"
+	"github.com/apache/arrow/go/parquet/internal/encryption"
+	format "github.com/apache/arrow/go/parquet/internal/gen-go/parquet"
+	"github.com/apache/arrow/go/parquet/schema"
+	"golang.org/x/xerrors"
+)
+
+// RowGroupMetaData is a proxy around the thrift RowGroup meta data object
+type RowGroupMetaData struct {
+	rowGroup      *format.RowGroup
+	Schema        *schema.Schema
+	version       *AppVersion
+	fileDecryptor encryption.FileDecryptor
+}
+
+// NewRowGroupMetaData constructs an object from the underlying thrift objects and schema,
+// decrypting if provided and necessary. This is primarily used internally and consumers
+// should use the RowGroupMetaDataBuilder rather than this directly.
+func NewRowGroupMetaData(rg *format.RowGroup, sc *schema.Schema, version *AppVersion, decryptor encryption.FileDecryptor) *RowGroupMetaData {
+	return &RowGroupMetaData{
+		rowGroup:      rg,
+		Schema:        sc,
+		version:       version,
+		fileDecryptor: decryptor,
+	}
+}
+
+// NumColumns returns the number of column metadata objects in this row group
+func (r *RowGroupMetaData) NumColumns() int {
+	return len(r.rowGroup.GetColumns())
+}
+
+func (r *RowGroupMetaData) Equals(other *RowGroupMetaData) bool {
+	return reflect.DeepEqual(r.rowGroup, other.rowGroup)
+}
+
+// NumRows is just the number of rows in this row group. All columns have the same
+// number of rows for a row group regardless of repetition and definition levels.
+func (r *RowGroupMetaData) NumRows() int64 { return r.rowGroup.NumRows }
+
+// TotalByteSize is the total size of this rowgroup on disk
+func (r *RowGroupMetaData) TotalByteSize() int64 { return r.rowGroup.GetTotalByteSize() }
+
+// FileOffset is the location in the file where the data for this rowgroup begins
+func (r *RowGroupMetaData) FileOffset() int64 { return r.rowGroup.GetFileOffset() }
+
+func (r *RowGroupMetaData) TotalCompressedSize() int64 { return r.rowGroup.GetTotalCompressedSize() }
+
+// Ordinal is the row group number in order for the given file.
+func (r *RowGroupMetaData) Ordinal() int16 { return r.rowGroup.GetOrdinal() }
+
+// ColumnChunk returns the metadata for the requested (0-based) chunk index
+func (r *RowGroupMetaData) ColumnChunk(i int) (*ColumnChunkMetaData, error) {
+	if i >= r.NumColumns() {
+		panic(xerrors.Errorf("parquet: the file only has %d columns, requested metadata for column: %d", r.NumColumns(), i))
+	}
+
+	return NewColumnChunkMetaData(r.rowGroup.Columns[i], r.Schema.Column(i), r.version, r.rowGroup.GetOrdinal(), int16(i), r.fileDecryptor)
+}
+
+// RowGroupMetaDataBuilder is a convenience object for constructing row group
+// metadata information. Primarily used in conjunction with writing new files.
+type RowGroupMetaDataBuilder struct {
+	rg          *format.RowGroup
+	props       *parquet.WriterProperties
+	schema      *schema.Schema
+	colBuilders []*ColumnChunkMetaDataBuilder
+	nextCol     int
+}
+
+// NewRowGroupMetaDataBuilder returns a builder using the given properties and underlying thrift object.
+//
+// This is primarily used internally, consumers should use the file metadatabuilder and call
+// AppendRowGroup on it to get instances of RowGroupMetaDataBuilder
+func NewRowGroupMetaDataBuilder(props *parquet.WriterProperties, schema *schema.Schema, rg *format.RowGroup) *RowGroupMetaDataBuilder {
+	r := &RowGroupMetaDataBuilder{
+		rg:          rg,
+		props:       props,
+		schema:      schema,
+		colBuilders: make([]*ColumnChunkMetaDataBuilder, 0),
+	}
+	r.rg.Columns = make([]*format.ColumnChunk, schema.NumColumns())
+	return r
+}
+
+// NumColumns returns the current number of columns in this metadata
+func (r *RowGroupMetaDataBuilder) NumColumns() int {
+	return int(len(r.rg.GetColumns()))
+}
+
+func (r *RowGroupMetaDataBuilder) NumRows() int64 {
+	return r.rg.GetNumRows()
+}
+
+func (r *RowGroupMetaDataBuilder) SetNumRows(nrows int64) {
+	r.rg.NumRows = nrows
+}
+
+// CurrentColumn returns the current column chunk (0-based) index that is being built.
+//
+// Returns -1 until the first time NextColumnChunk is called.
+func (r *RowGroupMetaDataBuilder) CurrentColumn() int { return r.nextCol - 1 }
+
+// NextColumnChunk appends a new column chunk, updates the column index,
+// and returns a builder for that column chunk's metadata
+func (r *RowGroupMetaDataBuilder) NextColumnChunk() *ColumnChunkMetaDataBuilder {
+	if r.nextCol >= r.NumColumns() {
+		panic(xerrors.Errorf("parquet: the schema only has %d columns, requested metadata for col: %d", r.NumColumns(), r.nextCol))
+	}
+
+	col := r.schema.Column(r.nextCol)
+	if r.rg.Columns[r.nextCol] == nil {
+		r.rg.Columns[r.nextCol] = &format.ColumnChunk{MetaData: format.NewColumnMetaData()}
+	}
+	colBldr := NewColumnChunkMetaDataBuilderWithContents(r.props, col, r.rg.Columns[r.nextCol])
+	r.nextCol++
+	r.colBuilders = append(r.colBuilders, colBldr)
+	return colBldr
+}
+
+// Finish should be called when complete and updates the metadata with the final
+// file offset, and total compressed sizes. totalBytesWritten gets written as the
+// TotalByteSize for the row group and Ordinal should be the index of the row group
+// being written. e.g. first row group should be 0, second is 1, and so on...
+func (r *RowGroupMetaDataBuilder) Finish(totalBytesWritten int64, ordinal int16) error {
+	if r.nextCol != r.NumColumns() {
+		return xerrors.Errorf("parquet: only %d out of %d columns are initialized", r.nextCol-1, r.schema.NumColumns())
+	}
+
+	var (
+		fileOffset      int64 = 0
+		totalCompressed int64 = 0
+	)
+
+	for idx, col := range r.rg.Columns {
+		if col.FileOffset < 0 {
+			return xerrors.Errorf("parquet: Column %d is not complete", idx)
+		}
+		if idx == 0 {
+			fileOffset = col.FileOffset
+		}
+		// sometimes column metadata is encrypted and not available to read
+		// so we must get total compressed size from column builder
+		totalCompressed += r.colBuilders[idx].TotalCompressedSize()
+	}
+
+	r.rg.FileOffset = &fileOffset
+	r.rg.TotalCompressedSize = &totalCompressed
+	r.rg.TotalByteSize = totalBytesWritten
+	r.rg.Ordinal = &ordinal
+	return nil
+}
