323a82391c8bfd1e72949a7e252c1dcabaefdb3b
Alessandro Molina <amol@turbogears.org>
2021-09-24 12:03:19 +0200

    Document types

--- python/pyarrow/types.py
+++ python/pyarrow/types.py
@@ -44,6 +44,11 @@ _NESTED_TYPES = {lib.Type_LIST, lib.Type_LARGE_LIST, lib.Type_STRUCT,
 def is_null(t):
     """
     Return True if value is an instance of a null type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_NA
 
@@ -51,6 +56,11 @@ def is_null(t):
 def is_boolean(t):
     """
     Return True if value is an instance of a boolean type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_BOOL
 
@@ -58,6 +68,11 @@ def is_boolean(t):
 def is_integer(t):
     """
     Return True if value is an instance of any integer type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _INTEGER_TYPES
 
@@ -65,6 +80,11 @@ def is_integer(t):
 def is_signed_integer(t):
     """
     Return True if value is an instance of any signed integer type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _SIGNED_INTEGER_TYPES
 
@@ -72,6 +92,11 @@ def is_signed_integer(t):
 def is_unsigned_integer(t):
     """
     Return True if value is an instance of any unsigned integer type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _UNSIGNED_INTEGER_TYPES
 
@@ -79,6 +104,11 @@ def is_unsigned_integer(t):
 def is_int8(t):
     """
     Return True if value is an instance of an int8 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_INT8
 
@@ -86,6 +116,11 @@ def is_int8(t):
 def is_int16(t):
     """
     Return True if value is an instance of an int16 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_INT16
 
@@ -93,6 +128,11 @@ def is_int16(t):
 def is_int32(t):
     """
     Return True if value is an instance of an int32 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_INT32
 
@@ -100,6 +140,11 @@ def is_int32(t):
 def is_int64(t):
     """
     Return True if value is an instance of an int64 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_INT64
 
@@ -107,6 +152,11 @@ def is_int64(t):
 def is_uint8(t):
     """
     Return True if value is an instance of an uint8 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_UINT8
 
@@ -114,6 +164,11 @@ def is_uint8(t):
 def is_uint16(t):
     """
     Return True if value is an instance of an uint16 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_UINT16
 
@@ -121,6 +176,11 @@ def is_uint16(t):
 def is_uint32(t):
     """
     Return True if value is an instance of an uint32 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_UINT32
 
@@ -128,6 +188,11 @@ def is_uint32(t):
 def is_uint64(t):
     """
     Return True if value is an instance of an uint64 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_UINT64
 
@@ -135,6 +200,11 @@ def is_uint64(t):
 def is_floating(t):
     """
     Return True if value is an instance of a floating point numeric type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _FLOATING_TYPES
 
@@ -142,6 +212,11 @@ def is_floating(t):
 def is_float16(t):
     """
     Return True if value is an instance of a float16 (half-precision) type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_HALF_FLOAT
 
@@ -149,6 +224,11 @@ def is_float16(t):
 def is_float32(t):
     """
     Return True if value is an instance of a float32 (single precision) type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_FLOAT
 
@@ -156,6 +236,11 @@ def is_float32(t):
 def is_float64(t):
     """
     Return True if value is an instance of a float64 (double precision) type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_DOUBLE
 
@@ -163,6 +248,11 @@ def is_float64(t):
 def is_list(t):
     """
     Return True if value is an instance of a list type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_LIST
 
@@ -170,6 +260,11 @@ def is_list(t):
 def is_large_list(t):
     """
     Return True if value is an instance of a large list type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_LARGE_LIST
 
@@ -177,6 +272,11 @@ def is_large_list(t):
 def is_fixed_size_list(t):
     """
     Return True if value is an instance of a fixed size list type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_FIXED_SIZE_LIST
 
@@ -184,6 +284,11 @@ def is_fixed_size_list(t):
 def is_struct(t):
     """
     Return True if value is an instance of a struct type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_STRUCT
 
@@ -191,6 +296,11 @@ def is_struct(t):
 def is_union(t):
     """
     Return True if value is an instance of a union type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _UNION_TYPES
 
@@ -198,6 +308,11 @@ def is_union(t):
 def is_nested(t):
     """
     Return True if value is an instance of a nested type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _NESTED_TYPES
 
@@ -205,6 +320,11 @@ def is_nested(t):
 def is_temporal(t):
     """
     Return True if value is an instance of date, time, timestamp or duration.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _TEMPORAL_TYPES
 
@@ -212,6 +332,11 @@ def is_temporal(t):
 def is_timestamp(t):
     """
     Return True if value is an instance of a timestamp type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_TIMESTAMP
 
@@ -219,6 +344,11 @@ def is_timestamp(t):
 def is_duration(t):
     """
     Return True if value is an instance of a duration type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_DURATION
 
@@ -226,6 +356,11 @@ def is_duration(t):
 def is_time(t):
     """
     Return True if value is an instance of a time type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _TIME_TYPES
 
@@ -233,6 +368,11 @@ def is_time(t):
 def is_time32(t):
     """
     Return True if value is an instance of a time32 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_TIME32
 
@@ -240,6 +380,11 @@ def is_time32(t):
 def is_time64(t):
     """
     Return True if value is an instance of a time64 type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_TIME64
 
@@ -247,6 +392,11 @@ def is_time64(t):
 def is_binary(t):
     """
     Return True if value is an instance of a variable-length binary type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_BINARY
 
@@ -255,6 +405,11 @@ def is_large_binary(t):
     """
     Return True if value is an instance of a large variable-length
     binary type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_LARGE_BINARY
 
@@ -262,6 +417,11 @@ def is_large_binary(t):
 def is_unicode(t):
     """
     Alias for is_string.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return is_string(t)
 
@@ -269,6 +429,11 @@ def is_unicode(t):
 def is_string(t):
     """
     Return True if value is an instance of string (utf8 unicode) type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_STRING
 
@@ -276,6 +441,11 @@ def is_string(t):
 def is_large_unicode(t):
     """
     Alias for is_large_string.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return is_large_string(t)
 
@@ -283,6 +453,11 @@ def is_large_unicode(t):
 def is_large_string(t):
     """
     Return True if value is an instance of large string (utf8 unicode) type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_LARGE_STRING
 
@@ -290,6 +465,11 @@ def is_large_string(t):
 def is_fixed_size_binary(t):
     """
     Return True if value is an instance of a fixed size binary type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_FIXED_SIZE_BINARY
 
@@ -297,6 +477,11 @@ def is_fixed_size_binary(t):
 def is_date(t):
     """
     Return True if value is an instance of a date type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _DATE_TYPES
 
@@ -304,6 +489,11 @@ def is_date(t):
 def is_date32(t):
     """
     Return True if value is an instance of a date32 (days) type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_DATE32
 
@@ -311,6 +501,11 @@ def is_date32(t):
 def is_date64(t):
     """
     Return True if value is an instance of a date64 (milliseconds) type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_DATE64
 
@@ -318,6 +513,11 @@ def is_date64(t):
 def is_map(t):
     """
     Return True if value is an instance of a map logical type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_MAP
 
@@ -325,6 +525,11 @@ def is_map(t):
 def is_decimal(t):
     """
     Return True if value is an instance of a decimal type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id in _DECIMAL_TYPES
 
@@ -332,6 +537,11 @@ def is_decimal(t):
 def is_decimal128(t):
     """
     Return True if value is an instance of a decimal type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_DECIMAL128
 
@@ -339,6 +549,11 @@ def is_decimal128(t):
 def is_decimal256(t):
     """
     Return True if value is an instance of a decimal type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_DECIMAL256
 
@@ -346,6 +561,11 @@ def is_decimal256(t):
 def is_dictionary(t):
     """
     Return True if value is an instance of a dictionary-encoded type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return t.id == lib.Type_DICTIONARY
 
@@ -353,5 +573,10 @@ def is_dictionary(t):
 def is_primitive(t):
     """
     Return True if the value is an instance of a primitive type.
+
+    Parameters
+    ----------
+    t : DataType
+        type to check
     """
     return lib._is_primitive(t.id)
