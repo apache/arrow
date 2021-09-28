70800d0a19d046aefd6b4ced477a9a339846e81d
Jose Almeida <almeidajcr90@gmail.com>
2021-09-28 16:09:27 -0300

    Add the schemas to be used by the GetTabkes

--- cpp/src/arrow/flight/flight-sql/sql_server.cpp
+++ cpp/src/arrow/flight/flight-sql/sql_server.cpp
@@ -266,6 +266,17 @@ std::shared_ptr<Schema> SqlSchema::GetTablesSchemaWithIncludedSchema() {
                         field("table_schema", binary())});
 }
 
+std::shared_ptr<Schema> SqlSchema::GetTablesSchema() {
+  return arrow::schema({field("catalog_name", utf8()), field("schema_name", utf8()),
+                        field("table_name", utf8()), field("table_type", utf8())});
+}
+
+std::shared_ptr<Schema> SqlSchema::GetTablesSchemaWithSchema() {
+  return arrow::schema({field("catalog_name", utf8()), field("schema_name", utf8()),
+                        field("table_name", utf8()), field("table_type", utf8()),
+                        field("table_schema", binary())});
+}
+
 }  // namespace sql
 }  // namespace flight
 }  // namespace arrow
