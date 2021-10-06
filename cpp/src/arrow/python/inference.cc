79192025c2423e019a33eecd5e150688a19d6be0
Alessandro Molina <amol@turbogears.org>
2021-10-06 16:46:24 +0200

    Set without DictValues

--- cpp/src/arrow/python/inference.cc
+++ cpp/src/arrow/python/inference.cc
@@ -360,7 +360,7 @@ class TypeInferrer {
       RETURN_NOT_OK(VisitNdarray(obj, keep_going));
     } else if (PyDict_Check(obj)) {
       RETURN_NOT_OK(VisitDict(obj));
-    } else if ((Py_TYPE(obj) == &PyDictValues_Type)) {
+    } else if (PySet_Check(obj)) {
       RETURN_NOT_OK(VisitSet(obj, keep_going));
     } else if (PyObject_IsInstance(obj, decimal_type_.obj())) {
       RETURN_NOT_OK(max_decimal_metadata_.Update(obj));
