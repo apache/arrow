283d1ec6b97cdade378bdac99f9af44f01c62285
Benson Muite <benson_muite@emailplus.org>
2021-10-10 18:54:59 +0300

    Move betwwen code into compare

--- cpp/src/arrow/compute/kernels/scalar_compare.cc
+++ cpp/src/arrow/compute/kernels/scalar_compare.cc
@@ -65,6 +65,19 @@ struct GreaterEqual {
   }
 };
 
+struct Between {
+  template <typename T, typename Arg0, typename Arg1, typename Arg2>
+  static constexpr T Call(KernelContext*, const Arg0& value, const Arg1& left,
+                             const Arg2& right, Status*) {
+    static_assert(std::is_same<T, bool>::value &&
+                  std::is_same<Arg0, Arg1>::value &&
+                  std::is_same<Arg1, Arg2>::value,
+                  "");
+    return (left < value) && (value < right);
+  }
+};
+
+
 template <typename T>
 using is_unsigned_integer = std::integral_constant<bool, std::is_integral<T>::value &&
                                                              std::is_unsigned<T>::value>;
@@ -466,6 +479,82 @@ std::shared_ptr<ScalarFunction> MakeScalarMinMax(std::string name,
   return func;
 }
 
+template <typename Op>
+void AddIntegerBetween(const std::shared_ptr<DataType>& ty, ScalarFunction* func) {
+  auto exec =
+      GeneratePhysicalInteger<applicator::ScalarTernaryEqualTypes, BooleanType, Op>(*ty);
+  DCHECK_OK(func->AddKernel({ty, ty, ty}, boolean(), std::move(exec)));
+}
+
+template <typename InType, typename Op>
+void AddGenericBetween(const std::shared_ptr<DataType>& ty, ScalarFunction* func) {
+   DCHECK_OK(
+      func->AddKernel({ty, ty, ty}, boolean(),
+               applicator::ScalarTernaryEqualTypes<BooleanType, InType, Op>::Exec));
+}
+
+template <typename Op>
+std::shared_ptr<ScalarFunction> MakeBetweenFunction(std::string name,
+                const FunctionDoc* doc) {
+  auto func = std::make_shared<ScalarFunction>(name, Arity::Ternary(), doc);
+
+  for (const std::shared_ptr<DataType>& ty : IntTypes()) {
+    AddIntegerBetween<Op>(ty, func.get());
+  }
+  AddIntegerBetween<Op>(date32(), func.get());
+  AddIntegerBetween<Op>(date64(), func.get());
+
+  AddGenericBetween<FloatType, Op>(float32(), func.get());
+  AddGenericBetween<DoubleType, Op>(float64(), func.get());
+
+  // Add timestamp kernels
+  for (auto unit : TimeUnit::values()) {
+    InputType in_type(match::TimestampTypeUnit(unit));
+    auto exec =
+        GeneratePhysicalInteger<applicator::ScalarTernaryEqualTypes, BooleanType, Op>(
+            int64());
+    DCHECK_OK(func->AddKernel({in_type, in_type, in_type}, boolean(), std::move(exec)));
+  }
+
+  // Duration
+  for (auto unit : TimeUnit::values()) {
+    InputType in_type(match::DurationTypeUnit(unit));
+    auto exec =
+        GeneratePhysicalInteger<applicator::ScalarTernaryEqualTypes, BooleanType, Op>(
+            int64());
+    DCHECK_OK(func->AddKernel({in_type, in_type, in_type}, boolean(), std::move(exec)));
+  }
+
+  // Time32 and Time64
+  for (auto unit : {TimeUnit::SECOND, TimeUnit::MILLI}) {
+    InputType in_type(match::Time32TypeUnit(unit));
+    auto exec =
+        GeneratePhysicalInteger<applicator::ScalarTernaryEqualTypes, BooleanType, Op>(
+            int32());
+    DCHECK_OK(func->AddKernel({in_type, in_type, in_type}, boolean(), std::move(exec)));
+  }
+  for (auto unit : {TimeUnit::MICRO, TimeUnit::NANO}) {
+    InputType in_type(match::Time64TypeUnit(unit));
+    auto exec =
+        GeneratePhysicalInteger<applicator::ScalarTernaryEqualTypes, BooleanType, Op>(
+            int64());
+    DCHECK_OK(func->AddKernel({in_type, in_type, in_type}, boolean(), std::move(exec)));
+  }
+
+  for (const std::shared_ptr<DataType>& ty : BaseBinaryTypes()) {
+    auto exec =
+        GenerateVarBinaryBase<applicator::ScalarTernaryEqualTypes, BooleanType, Op>(*ty);
+    DCHECK_OK(func->AddKernel(
+        {InputType::Array(ty), InputType::Scalar(ty), InputType::Scalar(ty)}, boolean(),
+        exec));
+    DCHECK_OK(func->AddKernel(
+        {InputType::Array(ty), InputType::Array(ty), InputType::Array(ty)}, boolean(),
+        std::move(exec)));
+  }
+
+  return func;
+}
+
 const FunctionDoc equal_doc{"Compare values for equality (x == y)",
                             ("A null on either side emits a null comparison result."),
                             {"x", "y"}};
@@ -505,6 +594,12 @@ const FunctionDoc max_element_wise_doc{
      "NaN will be taken over null, but not over any valid float."),
     {"*args"},
     "ElementWiseAggregateOptions"};
+
+const FunctionDoc between_doc{
+    "Check if values are in a range x <= y <= z",
+    ("A null on either side emits a null comparison result."),
+    {"x", "y", "z"}};
+
 }  // namespace
 
 void RegisterScalarComparison(FunctionRegistry* registry) {
@@ -533,6 +628,12 @@ void RegisterScalarComparison(FunctionRegistry* registry) {
   auto max_element_wise =
       MakeScalarMinMax<Maximum>("max_element_wise", &max_element_wise_doc);
   DCHECK_OK(registry->AddFunction(std::move(max_element_wise)));
+}
+
+void RegisterScalarBetween(FunctionRegistry* registry) {
+  auto between =
+          MakeBetweenFunction<Between>("between", &between_doc);
+  DCHECK_OK(registry->AddFunction(std::move(between)));
 }
 
 }  // namespace internal
