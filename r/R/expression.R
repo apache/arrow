877a4f49d6c3c81027ed4e0259ac372065a71da5
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2021-10-11 12:40:11 +0100

    Update r/R/expression.R
    Co-authored-by: Neal Richardson <neal.p.richardson@gmail.com>

--- r/R/expression.R
+++ r/R/expression.R
@@ -215,7 +215,7 @@ build_expr <- function(FUN,
     } else if (FUN == "%/%") {
       # In R, integer division works like floor(float division)
       out <- build_expr("/", args = args)
-      return(nse_funcs$floor(out))
+      return(Expression$create("floor", out))
     } else if (FUN == "%%") {
       return(args[[1]] - args[[2]] * (args[[1]] %/% args[[2]]))
     }
