2852380cc097179bcf16fb852caefb6082266256
Vinicius Fraga <62815192+vfraga@users.noreply.github.com>
2021-10-12 14:14:11 -0300

    Fix Flight SQL and Flight JDBC rebase mismatch (#166)

--- java/flight/flight-sql/src/main/java/org/apache/arrow/flight/sql/FlightSqlClient.java
+++ java/flight/flight-sql/src/main/java/org/apache/arrow/flight/sql/FlightSqlClient.java
@@ -35,7 +35,7 @@ import static org.apache.arrow.flight.sql.impl.FlightSql.CommandStatementUpdate;
 import static org.apache.arrow.flight.sql.impl.FlightSql.DoPutUpdateResult;
 import static org.apache.arrow.flight.sql.impl.FlightSql.SqlInfo;
 
-import com.fasterxml.jackson.databind.util.ByteBufferBackedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.channels.Channels;
 import java.sql.SQLException;
@@ -590,7 +590,7 @@ public class FlightSqlClient implements AutoCloseable {
             new Schema(Collections.emptyList()) :
             MessageSerializer.deserializeSchema(
                 new ReadChannel(Channels.newChannel(
-                    new ByteBufferBackedInputStream(bytes.asReadOnlyByteBuffer()))));
+                    new ByteArrayInputStream(bytes.toByteArray()))));
       } catch (final IOException e) {
         throw new RuntimeException("Failed to deserialize schema", e);
       }
