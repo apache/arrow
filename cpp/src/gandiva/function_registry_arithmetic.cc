6cb2380403b15c9447fa05f8939642e0db3b6868
Augusto Silva <augusto.a.silva@hotmail.com>
2021-10-13 21:32:14 -0300

    Add bround to gandiva function registry

--- cpp/src/gandiva/function_registry_arithmetic.cc
+++ cpp/src/gandiva/function_registry_arithmetic.cc
@@ -107,6 +107,12 @@ std::vector<NativeFunction> GetArithmeticFunctionRegistry() {
       BINARY_GENERIC_SAFE_NULL_IF_NULL(round, {}, int32, int32, int32),
       BINARY_GENERIC_SAFE_NULL_IF_NULL(round, {}, int64, int32, int64),
 
+      // bround functions
+      UNARY_SAFE_NULL_IF_NULL(bround, {}, float32, float32),
+      UNARY_SAFE_NULL_IF_NULL(bround, {}, float64, float64),
+      BINARY_GENERIC_SAFE_NULL_IF_NULL(bround, {}, float32, int32, float32),
+      BINARY_GENERIC_SAFE_NULL_IF_NULL(bround, {}, float64, int32, float64),
+
       // compare functions
       BINARY_RELATIONAL_BOOL_FN(equal, ({"eq", "same"})),
       BINARY_RELATIONAL_BOOL_FN(not_equal, {}),
