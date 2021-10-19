d3bcdfcc8afe91c5d7fd74139ec9b9f9658eb8ba
Nic Crane <thisisnic@gmail.com>
2021-10-19 11:01:23 +0100

    Don't duplicate docs

--- r/R/table.R
+++ r/R/table.R
@@ -85,14 +85,6 @@
 #' - `$columns`: Returns a list of `ChunkedArray`s
 #' @rdname Table
 #' @name Table
-#' @examplesIf arrow_available()
-#' tab <- Table$create(name = rownames(mtcars), mtcars)
-#' dim(tab)
-#' dim(head(tab))
-#' names(tab)
-#' tab$mpg
-#' tab[["cyl"]]
-#' as.data.frame(tab[4:8, c("gear", "hp", "wt")])
 #' @export
 Table <- R6Class("Table",
   inherit = ArrowTabular,
