1e5a8838eb5a6b0222c0537f9387fb0173e448b7
Vinicius Roque <hownatios@gmail.com>
2021-10-21 08:34:37 -0300

    remove duplicate function registry

--- cpp/src/gandiva/function_registry_string.cc
+++ cpp/src/gandiva/function_registry_string.cc
@@ -410,10 +410,7 @@ std::vector<NativeFunction> GetStringFunctionRegistry() {
 
       NativeFunction("split_part", {}, DataTypeVector{utf8(), utf8(), int32()}, utf8(),
                      kResultNullIfNull, "split_part",
-                     NativeFunction::kNeedsContext | NativeFunction::kCanReturnErrors),
-
-      NativeFunction("levenshtein", {}, DataTypeVector{utf8(), utf8()}, int32(),
-                     kResultNullIfNull, "levenshtein", NativeFunction::kNeedsContext)};
+                     NativeFunction::kNeedsContext | NativeFunction::kCanReturnErrors)};
 
   return string_fn_registry_;
 }
