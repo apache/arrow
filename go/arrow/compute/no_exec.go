30c8a81f903509a7328d427673ea070b72b6450c
Matthew Topol <mtopol@factset.com>
2021-10-21 18:17:11 -0400

    placeholder functions

---
+++ go/arrow/compute/no_exec.go
@@ -0,0 +1,41 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// +build !ccalloc,!ccexec
+
+// this file is used to provide dummy implementations for exec
+// functions that are called elsewhere in the compute package by
+// the expression handlers so that the logic can stay where it should
+// belong.
+
+package compute
+
+// dummy function which always returns false when not loading the C++ lib
+func isFuncScalar(funcName string) bool {
+	return false
+}
+
+type boundRef uintptr
+
+func (boundRef) release()                         {}
+func (boundRef) gettype() (arrow.DataType, error) { return nil, nil }
+
+// when compiled without the c++ library (the build tags control whether it looks for it)
+// then we do not have pure go implementation of the expression binding currently.
+func bindExprSchema(contet.Context, memory.Allocator, Expression, *arrow.Schema) (boundRef, ValueDescr, int, error) {
+	panic("arrow/compute: bind expression not implemented")
+}
