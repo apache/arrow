3a8de2570172bc7861c7dcf4e0c30bb441788eea
Matthew Topol <mtopol@factset.com>
2021-10-21 17:57:33 -0400

    record to struct array and back

--- go/arrow/array/util.go
+++ go/arrow/array/util.go
@@ -16,6 +16,38 @@
 
 package array
 
+import (
+	"github.com/apache/arrow/go/arrow"
+	"github.com/apache/arrow/go/arrow/memory"
+)
+
+// RecordToStructArray constructs a struct array from the columns of the record batch
+// by referencing them, zero-copy.
+func RecordToStructArray(rec Record) *Struct {
+	cols := make([]*Data, rec.NumCols())
+	for i, c := range rec.Columns() {
+		cols[i] = c.Data()
+	}
+
+	data := NewData(arrow.StructOf(rec.Schema().Fields()...), int(rec.NumRows()), []*memory.Buffer{nil}, cols, 0, 0)
+	defer data.Release()
+
+	return NewStructData(data)
+}
+
+// RecordFromStructArray is a convenience function for converting a struct array into
+// a record batch without copying the data. If the passed in schema is nil, the fields
+// of the struct will be used to define the record batch. Otherwise the passed in
+// schema will be used to create the record batch. If passed in, the schema must match
+// the fields of the struct column.
+func RecordFromStructArray(in *Struct, schema *arrow.Schema) Record {
+	if schema == nil {
+		schema = arrow.NewSchema(in.DataType().(*arrow.StructType).Fields(), nil)
+	}
+
+	return NewRecord(schema, in.fields, int64(in.Len()))
+}
+
 func min(a, b int) int {
 	if a < b {
 		return a
