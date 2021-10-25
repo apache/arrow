b4bc846fcdf189ae0443b8445c3ef69fc4131764
Benson Muite <benson_muite@emailplus.org>
2021-10-25 14:44:14 -0400

    ARROW-14452: [Release][JS] Update JavaScript testing
    Error message:
    
        + yarn run-s clean:all lint build
        yarn run v1.22.17
        $ /tmp/arrow-6.0.0.BDnN3/apache-arrow-6.0.0/js/node_modules/.bin/run-s clean:all lint build
        events.js:377
              throw er; // Unhandled 'error' event
              ^
    
        Error: EBADF: bad file descriptor, read
        Emitted 'error' event on ReadStream instance at:
            at internal/fs/streams.js:173:14
            at FSReqCallback.wrapper [as oncomplete] (fs.js:562:5) {
          errno: -9,
          code: 'EBADF',
          syscall: 'read'
        }
        error Command failed with exit code 1.
    
    Closes #11529 from bkmgit/ARROW-14452
    
    Lead-authored-by: Benson Muite <benson_muite@emailplus.org>
    Co-authored-by: Benson Muite <bkmgit@users.noreply.github.com>
    Signed-off-by: Dominik Moritz <domoritz@gmail.com>

--- dev/release/verify-release-candidate.sh
+++ dev/release/verify-release-candidate.sh
@@ -414,7 +414,7 @@ test_js() {
   if [ "${INSTALL_NODE}" -gt 0 ]; then
     export NVM_DIR="`pwd`/.nvm"
     mkdir -p $NVM_DIR
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | \
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | \
       PROFILE=/dev/null bash
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
@@ -423,7 +423,9 @@ test_js() {
   fi
 
   yarn --frozen-lockfile
-  yarn run-s clean:all lint build
+  yarn clean:all
+  yarn lint
+  yarn build
   yarn test
   popd
 }
