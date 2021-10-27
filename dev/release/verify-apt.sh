523e157bbc79e5e65ee87a6c0c7203d93039a6b0
Sutou Kouhei <kou@clear-code.com>
2021-10-27 13:28:16 +0900

    [Packaging][Linux] Add missing libthrift-dev dependency

--- dev/release/verify-apt.sh
+++ dev/release/verify-apt.sh
@@ -148,6 +148,8 @@ pushd build/minimal_build
 cmake .
 make -j$(nproc)
 ./arrow_example
+c++ -o arrow_example arrow_example.cc $(pkg-config --cflags --libs arrow)
+./arrow_example
 popd
 echo "::endgroup::"
 
