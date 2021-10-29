6b4df6d36b2a5bf7b369d876fc5fc4d03e513d4d
Alenka Frim <frim.alenka@gmail.com>
2021-10-29 11:33:23 +0200

    Typo changes to test_scalars.py

--- python/pyarrow/tests/test_scalars.py
+++ python/pyarrow/tests/test_scalars.py
@@ -323,9 +323,12 @@ def test_timestamp_no_overflow():
         s = pa.scalar(ts, type=pa.timestamp("us", tz="UTC"))
         assert s.as_py() == ts
 
-def test_timestamp_print():
+
+def test_timestamp_fixed_offset_print():
+    # ARROW-13896
     arr = pa.array([0], pa.timestamp('s', tz='+02:00'))
     assert str(arr[0]) == "1970-01-01 02:00:00+02:00"
+
 
 def test_duration():
     arr = np.array([0, 3600000000000], dtype='timedelta64[ns]')
