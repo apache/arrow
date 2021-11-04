e6b98c9f14276e219c2d94ebfd92a8a152fc16ba
Terence Honles <terence@honles.com>
2021-11-04 09:32:20 +0100

    ARROW-14580: [Python] update trove classifiers to include Python 3.10
    Closes #11599 from terencehonles/update-trove-classifiers
    
    Authored-by: Terence Honles <terence@honles.com>
    Signed-off-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -623,6 +623,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     license='Apache License, Version 2.0',
     maintainer='Apache Arrow Developers',
