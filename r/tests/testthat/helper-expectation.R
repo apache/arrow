aa3ebee2d39e1e83a3c7113f4f5536bd99196b27
Dragos Moldovan-Grünfeld <dragos.mold@gmail.com>
2021-11-08 09:14:01 +0000

    add helper function wrapping arround `expect_error()` (to be used mostly with `expect_snapshot()`)

--- r/tests/testthat/helper-expectation.R
+++ r/tests/testthat/helper-expectation.R
@@ -317,3 +317,5 @@ split_vector_as_list <- function(vec) {
   vec2 <- vec[seq(from = min(length(vec), vec_split + 1), to = length(vec), by = 1)]
   list(vec1, vec2)
 }
+
+err <- function(...) (expect_error(...))
