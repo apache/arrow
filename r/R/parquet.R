2169c89ac27f74f5ce8b9cc1160e7cc4d2e9d5c4
Nic Crane <thisisnic@gmail.com>
2021-11-11 18:32:37 +0000

    Rephrase docs on chunk size

--- r/R/parquet.R
+++ r/R/parquet.R
@@ -82,7 +82,7 @@ read_parquet <- function(file,
 #' @param x `data.frame`, [RecordBatch], or [Table]
 #' @param sink A string file path, URI, or [OutputStream], or path in a file
 #' system (`SubTreeFileSystem`)
-#' @param chunk_size chunk size in number of rows. If NULL, the total number of rows is used.
+#' @param chunk_size how many rows of data to write to disk at once. If NULL, the total number of rows is used.
 #' @param version parquet version, "1.0" or "2.0". Default "1.0". Numeric values
 #'   are coerced to character.
 #' @param compression compression algorithm. Default "snappy". See details.
