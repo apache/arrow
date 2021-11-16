1eeec38d6b45da0be363d906ea0bc74117b42350
Augusto Silva <augusto.a.silva@hotmail.com>
2021-11-16 07:53:55 -0300

    Add chr_32 implementation

--- cpp/src/gandiva/precompiled/string_ops.cc
+++ cpp/src/gandiva/precompiled/string_ops.cc
@@ -1366,6 +1366,23 @@ gdv_int32 ascii_utf8(const char* data, gdv_int32 data_len) {
 // Returns the ASCII character having the binary equivalent to A.
 // If A is larger than 256 the result is equivalent to chr(A % 256).
 FORCE_INLINE
+const char* chr_int32(gdv_int64 context, gdv_int32 in, gdv_int32* out_len) {
+  in = in % 256;
+  *out_len = 1;
+
+  char* ret = reinterpret_cast<char*>(gdv_fn_context_arena_malloc(context, *out_len));
+  if (ret == nullptr) {
+    gdv_fn_context_set_error_msg(context, "Could not allocate memory for output string");
+    *out_len = 0;
+    return "";
+  }
+  ret[0] = char(in);
+  return ret;
+}
+
+// Returns the ASCII character having the binary equivalent to A.
+// If A is larger than 256 the result is equivalent to chr(A % 256).
+FORCE_INLINE
 const char* chr_int64(gdv_int64 context, gdv_int64 in, gdv_int32* out_len) {
   in = in % 256;
   *out_len = 1;
--- cpp/src/gandiva/precompiled/string_ops_test.cc
+++ cpp/src/gandiva/precompiled/string_ops_test.cc
@@ -59,50 +59,50 @@ TEST(TestStringOps, TestChrBigInt) {
   uint64_t ctx_ptr = reinterpret_cast<gdv_int64>(&ctx);
   int32_t out_len = 0;
 
-  auto out = chr_int64(ctx_ptr, 88, &out_len);
+  auto out = chr_int32(ctx_ptr, 88, &out_len);
   EXPECT_EQ(std::string(out, out_len), "X");
 
   out = chr_int64(ctx_ptr, 65, &out_len);
   EXPECT_EQ(std::string(out, out_len), "A");
 
-  out = chr_int64(ctx_ptr, 49, &out_len);
+  out = chr_int32(ctx_ptr, 49, &out_len);
   EXPECT_EQ(std::string(out, out_len), "1");
 
   out = chr_int64(ctx_ptr, 84, &out_len);
   EXPECT_EQ(std::string(out, out_len), "T");
 
-  out = chr_int64(ctx_ptr, 340, &out_len);
+  out = chr_int32(ctx_ptr, 340, &out_len);
   EXPECT_EQ(std::string(out, out_len), "T");
 
   out = chr_int64(ctx_ptr, 256, &out_len);
   EXPECT_EQ(std::strcmp(out, "\0"), 0);
 
-  out = chr_int64(ctx_ptr, 33, &out_len);
+  out = chr_int32(ctx_ptr, 33, &out_len);
   EXPECT_EQ(std::string(out, out_len), "!");
 
   out = chr_int64(ctx_ptr, 46, &out_len);
   EXPECT_EQ(std::string(out, out_len), ".");
 
-  out = chr_int64(ctx_ptr, 63, &out_len);
+  out = chr_int32(ctx_ptr, 63, &out_len);
   EXPECT_EQ(std::string(out, out_len), "?");
 
   out = chr_int64(ctx_ptr, 0, &out_len);
   EXPECT_EQ(std::strcmp(out, "\0"), 0);
 
-  out = chr_int64(ctx_ptr, -158, &out_len);
+  out = chr_int32(ctx_ptr, -158, &out_len);
   EXPECT_EQ(std::string(out, out_len), "b");
 
   out = chr_int64(ctx_ptr, -5, &out_len);
   EXPECT_EQ(std::string(out, out_len), "\xFB");
 
-  out = chr_int64(ctx_ptr, -340, &out_len);
+  out = chr_int32(ctx_ptr, -340, &out_len);
   EXPECT_EQ(std::string(out, out_len), "\xAC");
 
   out = chr_int64(ctx_ptr, -66, &out_len);
   EXPECT_EQ(std::string(out, out_len), "\xBE");
 
   //€
-  out = chr_int64(ctx_ptr, 128, &out_len);
+  out = chr_int32(ctx_ptr, 128, &out_len);
   EXPECT_EQ(std::string(out, out_len), "\x80");
 
   //œ
@@ -110,7 +110,7 @@ TEST(TestStringOps, TestChrBigInt) {
   EXPECT_EQ(std::string(out, out_len), "\x9C");
 
   //ÿ
-  out = chr_int64(ctx_ptr, 255, &out_len);
+  out = chr_int32(ctx_ptr, 255, &out_len);
   EXPECT_EQ(std::string(out, out_len), "\xFF");
 
   // BACKSPACE
@@ -118,7 +118,7 @@ TEST(TestStringOps, TestChrBigInt) {
   EXPECT_EQ(std::string(out, out_len), "\b");
 
   // DEVICE CONTROL 3 (DC3)
-  out = chr_int64(ctx_ptr, 19, &out_len);
+  out = chr_int32(ctx_ptr, 19, &out_len);
   EXPECT_EQ(std::string(out, out_len), "\x13");
 
   // ESCAPE (ESC)
