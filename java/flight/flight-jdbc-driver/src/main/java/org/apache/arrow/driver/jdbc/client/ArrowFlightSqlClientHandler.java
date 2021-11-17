498dd073eee0317d6206ed1accdb4aad0c2517af
Juscelino Junior <juscelinojunior@id.uff.br>
2021-11-17 13:36:14 -0300

    Move ArrowFlightSqlClientHanlder to the client folder

--- java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/client/ArrowFlightSqlClientHandler.java
+++ java/flight/flight-jdbc-driver/src/main/java/org/apache/arrow/driver/jdbc/client/ArrowFlightSqlClientHandler.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.arrow.driver.jdbc.client.impl;
+package org.apache.arrow.driver.jdbc.client;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
