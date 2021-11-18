e9036d253ad55e5b65fbac035fd5453f92b9833a
Augusto Silva <augusto.a.silva@hotmail.com>
2021-11-18 08:25:44 -0300

    Fix broken function registry string after resolve conflicts with master

--- cpp/src/gandiva/function_registry_string.cc
+++ cpp/src/gandiva/function_registry_string.cc
@@ -106,11 +106,7 @@ std::vector<NativeFunction> GetStringFunctionRegistry() {
                      utf8(), kResultNullIfNull, "concat_ws_utf8_utf8_utf8_utf8_utf8",
                      NativeFunction::kNeedsContext | NativeFunction::kCanReturnErrors),
 
-      NativeFunction("elt", {}, DataTypeVector{int32(), utf8()}, utf8(), kResultNullIfNull,
-                     "gdv_fn_elt_utf8",
-      NativeFunction("elt", {}, DataTypeVector{int32(), utf8()}, utf8(),
-                     kResultNullIfNull, "gdv_fn_elt_utf8",
-                     NativeFunction::kNeedsContext | NativeFunction::kCanReturnErrors),
+
       NativeFunction("elt", {}, DataTypeVector{int32(), utf8(), utf8()}, utf8(),
                      kResultNullInternal, "elt_int32_utf8_utf8",
                      NativeFunction::kNeedsContext),
