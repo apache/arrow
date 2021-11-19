987d47adb0a2c277ae7fe665cac844a42e6f64c4
Jonathan Keane <jkeane@gmail.com>
2021-11-19 12:58:21 -0600

    ARROW-14746: [CI] Allow (temporary) disabling of constantly failing nightlies
    Closes #11731 from jonkeane/crossbow-group-droplist
    
    Authored-by: Jonathan Keane <jkeane@gmail.com>
    Signed-off-by: Jonathan Keane <jkeane@gmail.com>

--- dev/archery/archery/crossbow/tests/test_core.py
+++ dev/archery/archery/crossbow/tests/test_core.py
@@ -18,8 +18,54 @@
 from archery.utils.source import ArrowSources
 from archery.crossbow import Config
 
+import pathlib
+
 
 def test_config():
     src = ArrowSources.find()
     conf = Config.load_yaml(src.dev / "tasks" / "tasks.yml")
     conf.validate()
+
+
+def test_task_select(request):
+    conf = Config.load_yaml(pathlib.Path(
+        request.node.fspath).parent / "fixtures" / "tasks.yaml")
+    conf.validate()
+
+    test_out = conf.select(tasks=["test-a-test-two"])
+    assert test_out.keys() >= {"test-a-test-two"}
+
+
+def test_group_select(request):
+    conf = Config.load_yaml(pathlib.Path(
+        request.node.fspath).parent / "fixtures" / "tasks.yaml")
+    conf.validate()
+
+    test_out = conf.select(groups=["test"])
+    assert test_out.keys() >= {"test-a-test-two", "test-a-test"}
+
+
+def test_group_select_blocklist(request):
+    conf = Config.load_yaml(pathlib.Path(
+        request.node.fspath).parent / "fixtures" / "tasks.yaml")
+    conf.validate()
+
+    # we respect the nightly blocklist
+    nightly_out = conf.select(groups=["nightly"])
+    assert nightly_out.keys() >= {"test-a-test", "nightly-fine"}
+
+    # but if a task is not blocked in both groups, it shows up at least once
+    test_nightly_out = conf.select(groups=["nightly", "test"])
+    assert test_nightly_out.keys() >= {
+        "test-a-test-two", "test-a-test", "nightly-fine"}
+
+    # but can then over-ride by requesting the task
+    test_nightly_out = conf.select(
+        tasks=["nightly-not-fine", "nightly-fine"], groups=["nightly", "test"])
+    assert test_nightly_out.keys() >= {
+        "test-a-test-two", "test-a-test", "nightly-fine", "nightly-not-fine"}
+
+    # and we can glob with the blocklist too!
+    test_nightly_no_test_out = conf.select(groups=["nightly-no-test"])
+    assert test_nightly_no_test_out.keys(
+    ) >= {"nightly-fine", "nightly-not-fine"}
