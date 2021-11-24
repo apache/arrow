cc61c3bb04425021762474c8cf3f815dd6c3855e
Vinicius Roque <hownatios@gmail.com>
2021-11-24 08:54:38 -0300

    Change aliases

--- cpp/src/gandiva/tests/projector_test.cc
+++ cpp/src/gandiva/tests/projector_test.cc
@@ -2053,7 +2053,7 @@ TEST_F(TestProjector, TestQuarter) {
   auto schema = arrow::schema({field0});
 
   // output fields
-  auto field_result = field("QUARTER", arrow::int64());
+  auto field_result = field("quarter", arrow::int64());
 
   // Build expression
   auto myexpr = TreeExprBuilder::MakeExpression("quarter", {field0}, field_result);
