a660b47d9a753ee3633a001334d057254e95a4d9
Jonathan Keane <jkeane@gmail.com>
2021-11-29 18:05:52 -0600

    Use RSPM binary if it's in the repos

--- ci/scripts/r_deps.sh
+++ ci/scripts/r_deps.sh
@@ -45,7 +45,7 @@ ${R_BIN} -e "options(warn=2); install.packages('remotes'); remotes::install_cran
 # (Temporarily) install DuckDB from source to avoid their Unity builds (though only if we haven't filtered it out of the deps above)
 # Remove when there is a DuckDB release > 0.3.1-1
 if grep -q "duckdb" DESCRIPTION; then
-  ${R_BIN} -e "remotes::install_github('duckdb/duckdb', subdir = '/tools/rpkg', build = FALSE)"
+  ${R_BIN} -e "if (all(!grepl('packagemanager.rstudio', options('repos')))) { remotes::install_github('duckdb/duckdb', subdir = '/tools/rpkg', build = FALSE) }"
 fi
 
 # Separately install the optional/test dependencies but don't error on them,
