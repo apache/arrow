07b30f9518d1e0a981bfa8d0256e40b4b9a44bf5
Dragos Moldovan-Grünfeld <dragos.mold@gmail.com>
2021-11-30 18:24:41 +0000

    cleaned up some of the unit tests: * removed tests that were basic (r errors due to unsupplied arguments)

--- r/tests/testthat/test-data-type.R
+++ r/tests/testthat/test-data-type.R
@@ -387,9 +387,7 @@ test_that("DictionaryType validation", {
 test_that("decimal type and validation", {
   expect_r6_class(decimal(4, 2), "Decimal128Type")
 
-  expect_error(decimal())
   expect_error(decimal("four"), '"precision" must be an integer')
-  expect_error(decimal(4))
   expect_error(decimal(4, "two"), '"scale" must be an integer')
   expect_error(decimal(NA, 2), '"precision" must be an integer')
   expect_error(decimal(0, 2),
@@ -403,9 +401,7 @@ test_that("decimal type and validation", {
   expect_r6_class(decimal128(4, 2), "Decimal128Type")
   expect_identical(class(decimal(2, 4)), class(decimal128(2, 4)))
 
-  expect_error(decimal128())
   expect_error(decimal128("four"), '"precision" must be an integer')
-  expect_error(decimal128(4))
   expect_error(decimal128(4, "two"), '"scale" must be an integer')
   expect_error(decimal128(NA, 2), '"precision" must be an integer')
   expect_error(decimal128(0, 2),
