80b7340b599fb3693998c4d79f83a970e65d1991
jayhomn-bitquill <jay.ho@dremio.com>
2021-11-29 17:42:24 -0800

    Refactored common code into helper methods

--- java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcCursorTest.java
+++ java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcCursorTest.java
@@ -71,125 +71,48 @@ public class ArrowFlightJdbcCursorTest {
 
     @Test
     public void testBinaryVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Binary",
-                        new FieldType(true, new ArrowType.Binary(),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Binary", new ArrowType.Binary(), null);
         ((VarBinaryVector) root.getVector("Binary")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getBytes();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testDateVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Date",
-                        new FieldType(true, new ArrowType.Date(DateUnit.DAY),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Date", new ArrowType.Date(DateUnit.DAY), null);
         ((DateDayVector) root.getVector("Date")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getDate(null);
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
     }
 
     @Test
     public void testDurationVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Duration",
-                        new FieldType(true, new ArrowType.Duration(TimeUnit.MILLISECOND),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Duration",
+                new ArrowType.Duration(TimeUnit.MILLISECOND), null);
         ((DurationVector) root.getVector("Duration")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testDateInternalNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Interval",
-                        new FieldType(true, new ArrowType.Interval(IntervalUnit.DAY_TIME),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Interval",
+                new ArrowType.Interval(IntervalUnit.DAY_TIME), null);
         ((IntervalDayVector) root.getVector("Interval")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testTimeStampVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "TimeStamp",
-                        new FieldType(true, new ArrowType.Timestamp(TimeUnit.MILLISECOND,null),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("TimeStamp",
+                new ArrowType.Timestamp(TimeUnit.MILLISECOND,null), null);
         ((TimeStampMilliVector) root.getVector("TimeStamp")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
     }
 
     @Test
     public void testTimeVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Time",
-                        new FieldType(true, new ArrowType.Time(TimeUnit.MILLISECOND,32),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Time",
+                new ArrowType.Time(TimeUnit.MILLISECOND,32), null);
         ((TimeMilliVector) root.getVector("Time")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
 
     }
 
@@ -198,22 +121,10 @@ public class ArrowFlightJdbcCursorTest {
         List<Field> fieldList = new ArrayList<>();
         fieldList.add(new Field("Null", new FieldType(true,new ArrowType.Null(), null),
                 null));
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "FixedSizeList",
-                        new FieldType(true, new ArrowType.FixedSizeList(10),
-                                null),
-                        fieldList)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("FixedSizeList",
+                new ArrowType.FixedSizeList(10), fieldList);
         ((FixedSizeListVector) root.getVector("FixedSizeList")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
     }
 
     @Test
@@ -221,23 +132,9 @@ public class ArrowFlightJdbcCursorTest {
         List<Field> fieldList = new ArrayList<>();
         fieldList.add(new Field("Null", new FieldType(true,new ArrowType.Null(), null),
                 null));
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "LargeList",
-                        new FieldType(true, new ArrowType.LargeList(),
-                                null),
-                        fieldList
-                        )));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("LargeList", new ArrowType.LargeList(), fieldList);
         ((LargeListVector) root.getVector("LargeList")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
     }
 
     @Test
@@ -245,22 +142,9 @@ public class ArrowFlightJdbcCursorTest {
         List<Field> fieldList = new ArrayList<>();
         fieldList.add(new Field("Null", new FieldType(true,new ArrowType.Null(), null),
                 null));
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "List",
-                        new FieldType(true, new ArrowType.List(),
-                                null),
-                        fieldList)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("List", new ArrowType.List(), fieldList);
         ((ListVector) root.getVector("List")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
     }
 
     @Test
@@ -273,181 +157,84 @@ public class ArrowFlightJdbcCursorTest {
         List<Field> fieldList = new ArrayList<>();
         fieldList.add(new Field("Struct", new FieldType(false,new ArrowType.Struct(), null),
                 structChildren));
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Map",
-                        new FieldType(true, new ArrowType.Map(false),
-                                null),
-                        fieldList)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Map", new ArrowType.Map(false), fieldList);
         ((MapVector) root.getVector("Map")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
     }
 
     @Test
     public void testStructVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Struct",
-                        new FieldType(true, new ArrowType.Struct(),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Struct", new ArrowType.Struct(), null);
         ((StructVector) root.getVector("Struct")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testBaseIntVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "BaseInt",
-                        new FieldType(true, new ArrowType.Int(32,false),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("BaseInt",
+                new ArrowType.Int(32,false), null);
         ((UInt4Vector) root.getVector("BaseInt")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testBitVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Bit",
-                        new FieldType(true, new ArrowType.Bool(),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Bit", new ArrowType.Bool(), null);
         ((BitVector) root.getVector("Bit")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
+        testCursorWasNull(root);
     }
 
     @Test
     public void testDecimalVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Decimal",
-                        new FieldType(true, new ArrowType.Decimal(2, 2),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Decimal",
+                new ArrowType.Decimal(2, 2), null);
         ((DecimalVector) root.getVector("Decimal")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testFloat4VectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Float4",
-                        new FieldType(true, new ArrowType.FloatingPoint(FloatingPointPrecision.SINGLE),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Float4",
+                new ArrowType.FloatingPoint(FloatingPointPrecision.SINGLE), null);
         ((Float4Vector) root.getVector("Float4")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testFloat8VectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "Float8",
-                        new FieldType(true, new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("Float8",
+                new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE), null);
         ((Float8Vector) root.getVector("Float8")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testVarCharVectorNullTrue() throws SQLException {
-        final Schema schema = new Schema(ImmutableList.of(
-                new Field(
-                        "VarChar",
-                        new FieldType(true, new ArrowType.Utf8(),
-                                null),
-                        null)));
-        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-        final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
-        root.allocateNew();
+        final VectorSchemaRoot root = getVectorSchemaRoot("VarChar", new ArrowType.Utf8(), null);
         ((VarCharVector) root.getVector("VarChar")).setNull(0);
-        root.setRowCount(1);
-        cursor = new ArrowFlightJdbcCursor(root);
-        cursor.next();
-        List<Cursor.Accessor> accessorList = cursor.createAccessors(null,null,null);
-        accessorList.get(0).getObject();
-        assertTrue(cursor.wasNull());
-
+        testCursorWasNull(root);
     }
 
     @Test
     public void testNullVectorNullTrue() throws SQLException {
+        final VectorSchemaRoot root = getVectorSchemaRoot("Null", new ArrowType.Null(), null);
+        testCursorWasNull(root);
+    }
+
+    private VectorSchemaRoot getVectorSchemaRoot(String name, ArrowType arrowType, List<Field> children) {
         final Schema schema = new Schema(ImmutableList.of(
                 new Field(
-                        "Null",
-                        new FieldType(true, new ArrowType.Null(),
+                        name,
+                        new FieldType(true, arrowType,
                                 null),
-                        null)));
+                        children)));
         final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
         final VectorSchemaRoot root = VectorSchemaRoot.create(schema, allocator);
         root.allocateNew();
+        return root;
+    }
+
+    private void testCursorWasNull(VectorSchemaRoot root) throws SQLException {
         root.setRowCount(1);
         cursor = new ArrowFlightJdbcCursor(root);
         cursor.next();
