715efbd7f3e8d914050fe10b0eb6cdad1015dde9
Joost Hoozemans <joosthooz@msn.com>
2021-12-02 13:25:18 +0100

    Set a minimum version requirement for the LZ4 library

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -2157,7 +2157,8 @@ macro(build_lz4)
 endmacro()
 
 if(ARROW_WITH_LZ4)
-  resolve_dependency(Lz4 PC_PACKAGE_NAMES liblz4)
+  set(ARROW_LZ4_REQUIRED_VERSION "1.8.0")
+  resolve_dependency(Lz4 PC_PACKAGE_NAMES liblz4 REQUIRED_VERSION ${ARROW_LZ4_REQUIRED_VERSION})
 
   # TODO: Don't use global includes but rather target_include_directories
   get_target_property(LZ4_INCLUDE_DIR LZ4::lz4 INTERFACE_INCLUDE_DIRECTORIES)
