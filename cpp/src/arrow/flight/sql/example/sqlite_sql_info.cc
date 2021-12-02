2b3839aff39b474d77d515e0ce6290e8e1a9f81a
Rafael Telles <rafael@telles.dev>
2021-12-02 13:48:49 -0300

    Remove generated protobuf enum from example application

--- cpp/src/arrow/flight/sql/example/sqlite_sql_info.cc
+++ cpp/src/arrow/flight/sql/example/sqlite_sql_info.cc
@@ -17,7 +17,6 @@
 
 #include "arrow/flight/sql/example/sqlite_sql_info.h"
 
-#include "arrow/flight/sql/FlightSql.pb.h"
 #include "arrow/flight/sql/types.h"
 
 namespace arrow {
@@ -25,36 +24,35 @@ namespace flight {
 namespace sql {
 namespace example {
 
-namespace flight_sql_pb = arrow::flight::protocol::sql;
-
 /// \brief Gets the mapping from SQL info ids to SqlInfoResult instances.
 /// \return the cache.
 SqlInfoResultMap GetSqlInfoResultMap() {
   return {
-      {flight_sql_pb::SqlInfo::FLIGHT_SQL_SERVER_NAME,
+      {SqlInfoOptions::SqlInfo::FLIGHT_SQL_SERVER_NAME,
        SqlInfoResult(std::string("db_name"))},
-      {flight_sql_pb::SqlInfo::FLIGHT_SQL_SERVER_VERSION,
+      {SqlInfoOptions::SqlInfo::FLIGHT_SQL_SERVER_VERSION,
        SqlInfoResult(std::string("sqlite 3"))},
-      {flight_sql_pb::SqlInfo::FLIGHT_SQL_SERVER_ARROW_VERSION,
+      {SqlInfoOptions::SqlInfo::FLIGHT_SQL_SERVER_ARROW_VERSION,
        SqlInfoResult(std::string("7.0.0-SNAPSHOT" /* Only an example */))},
-      {flight_sql_pb::SqlInfo::FLIGHT_SQL_SERVER_READ_ONLY, SqlInfoResult(false)},
-      {flight_sql_pb::SqlInfo::SQL_DDL_CATALOG,
+      {SqlInfoOptions::SqlInfo::FLIGHT_SQL_SERVER_READ_ONLY, SqlInfoResult(false)},
+      {SqlInfoOptions::SqlInfo::SQL_DDL_CATALOG,
        SqlInfoResult(false /* SQLite 3 does not support catalogs */)},
-      {flight_sql_pb::SqlInfo::SQL_DDL_SCHEMA,
+      {SqlInfoOptions::SqlInfo::SQL_DDL_SCHEMA,
        SqlInfoResult(false /* SQLite 3 does not support schemas */)},
-      {flight_sql_pb::SqlInfo::SQL_DDL_TABLE, SqlInfoResult(true)},
-      {flight_sql_pb::SqlInfo::SQL_IDENTIFIER_CASE,
-       SqlInfoResult(int64_t(flight_sql_pb::SqlSupportedCaseSensitivity::
+      {SqlInfoOptions::SqlInfo::SQL_DDL_TABLE, SqlInfoResult(true)},
+      {SqlInfoOptions::SqlInfo::SQL_IDENTIFIER_CASE,
+       SqlInfoResult(int64_t(SqlInfoOptions::SqlSupportedCaseSensitivity::
                                  SQL_CASE_SENSITIVITY_CASE_INSENSITIVE))},
-      {flight_sql_pb::SqlInfo::SQL_IDENTIFIER_QUOTE_CHAR,
+      {SqlInfoOptions::SqlInfo::SQL_IDENTIFIER_QUOTE_CHAR,
        SqlInfoResult(std::string("\""))},
-      {flight_sql_pb::SqlInfo::SQL_QUOTED_IDENTIFIER_CASE,
-       SqlInfoResult(int64_t(flight_sql_pb::SqlSupportedCaseSensitivity::
+      {SqlInfoOptions::SqlInfo::SQL_QUOTED_IDENTIFIER_CASE,
+       SqlInfoResult(int64_t(SqlInfoOptions::SqlSupportedCaseSensitivity::
                                  SQL_CASE_SENSITIVITY_CASE_INSENSITIVE))},
-      {flight_sql_pb::SqlInfo::SQL_ALL_TABLES_ARE_SELECTABLE, SqlInfoResult(true)},
-      {flight_sql_pb::SqlInfo::SQL_NULL_ORDERING,
-       SqlInfoResult(int64_t(flight_sql_pb::SqlNullOrdering::SQL_NULLS_SORTED_AT_START))},
-      {flight_sql_pb::SqlInfo::SQL_KEYWORDS,
+      {SqlInfoOptions::SqlInfo::SQL_ALL_TABLES_ARE_SELECTABLE, SqlInfoResult(true)},
+      {SqlInfoOptions::SqlInfo::SQL_NULL_ORDERING,
+       SqlInfoResult(
+           int64_t(SqlInfoOptions::SqlNullOrdering::SQL_NULLS_SORTED_AT_START))},
+      {SqlInfoOptions::SqlInfo::SQL_KEYWORDS,
        SqlInfoResult(std::vector<std::string>({"ABORT",
                                                "ACTION",
                                                "ADD",
@@ -202,21 +200,21 @@ SqlInfoResultMap GetSqlInfoResultMap() {
                                                "WINDOW",
                                                "WITH",
                                                "WITHOUT"}))},
-      {flight_sql_pb::SqlInfo::SQL_NUMERIC_FUNCTIONS,
+      {SqlInfoOptions::SqlInfo::SQL_NUMERIC_FUNCTIONS,
        SqlInfoResult(std::vector<std::string>(
            {"acos",    "acosh", "asin", "asinh",   "atan", "atan2", "atanh", "ceil",
             "ceiling", "cos",   "cosh", "degrees", "exp",  "floor", "ln",    "log",
             "log",     "log10", "log2", "mod",     "pi",   "pow",   "power", "radians",
             "sin",     "sinh",  "sqrt", "tan",     "tanh", "trunc"}))},
-      {flight_sql_pb::SqlInfo::SQL_STRING_FUNCTIONS,
+      {SqlInfoOptions::SqlInfo::SQL_STRING_FUNCTIONS,
        SqlInfoResult(
            std::vector<std::string>({"SUBSTR", "TRIM", "LTRIM", "RTRIM", "LENGTH",
                                      "REPLACE", "UPPER", "LOWER", "INSTR"}))},
-      {flight_sql_pb::SqlInfo::SQL_SUPPORTS_CONVERT,
+      {SqlInfoOptions::SqlInfo::SQL_SUPPORTS_CONVERT,
        SqlInfoResult(std::unordered_map<int32_t, std::vector<int32_t>>(
-           {{flight_sql_pb::SqlSupportsConvert::SQL_CONVERT_BIGINT,
+           {{SqlInfoOptions::SqlSupportsConvert::SQL_CONVERT_BIGINT,
              std::vector<int32_t>(
-                 {flight_sql_pb::SqlSupportsConvert::SQL_CONVERT_INTEGER})}}))}};
+                 {SqlInfoOptions::SqlSupportsConvert::SQL_CONVERT_INTEGER})}}))}};
 }
 
 }  // namespace example
