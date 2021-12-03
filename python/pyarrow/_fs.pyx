d72dd8c62072d9e796a388981bd05a2d009fcfe1
Jedi18 <akhiljnair.188@gmail.com>
2021-12-03 11:01:36 +0530

    Undeprecate FileSystem::OpenAppendStream

--- python/pyarrow/_fs.pyx
+++ python/pyarrow/_fs.pyx
@@ -30,7 +30,6 @@ from datetime import datetime, timezone
 import os
 import pathlib
 import sys
-import warnings
 
 
 cdef _init_ca_paths():
@@ -681,13 +680,10 @@ cdef class FileSystem(_Weakrefable):
     def open_append_stream(self, path, compression='detect',
                            buffer_size=None, metadata=None):
         """
-        DEPRECATED: Open an output stream for appending.
 
         If the target doesn't exist, a new empty file is created.
-
-        .. deprecated:: 6.0
-            Several filesystems don't support this functionality
-            and it will be later removed.
+        Note: This cannot be supported by all filesystem kinds, it will remain
+              unimplemented for filesystems which don't support it
 
         Parameters
         ----------
@@ -717,11 +713,6 @@ cdef class FileSystem(_Weakrefable):
             NativeFile stream = NativeFile()
             shared_ptr[COutputStream] out_handle
             shared_ptr[const CKeyValueMetadata] c_metadata
-
-        warnings.warn(
-            "`open_append_stream` is deprecated as of 6.0.0; several "
-            "filesystems don't support it and it will be later removed",
-            FutureWarning)
 
         if metadata is not None:
             c_metadata = pyarrow_unwrap_metadata(KeyValueMetadata(metadata))
