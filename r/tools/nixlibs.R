b5db7389e7659d2b5775e2fcfae3b79b92b3ee67
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-07 08:23:10 -0400

    clarify interaction between NOT_CRAN and LIBARROW_BINARY env vars

--- r/tools/nixlibs.R
+++ r/tools/nixlibs.R
@@ -48,8 +48,8 @@ quietly <- !env_is("ARROW_R_DEV", "true")
 # Default is build from source, not download a binary
 build_ok <- !env_is("LIBARROW_BUILD", "false")
 # For LIBARROW_BINARY we support "true" or the name of the OS to use to
-# locate the appropriate binary (e.g., 'ubuntu-18.04'). When NOT_CRAN=true, the
-# configure script sets LIBARROW_BINARY=true.
+# locate the appropriate binary (e.g., 'ubuntu-18.04'). When NOT_CRAN=true, and
+# LIBARROW_BINARY is unset, configure script sets LIBARROW_BINARY=true.
 binary_ok <- !env_is("LIBARROW_BINARY", "false") || env_is("LIBARROW_BINARY", "")
 
 # Check if we're doing an offline build.
