d5b149f9fc06295a5f102024612ed61e2da2729f
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-08 10:36:02 -0400

    move documentation from comments into internal documentation for register_translation()

--- r/R/dplyr-funcs.R
+++ r/R/dplyr-funcs.R
@@ -19,8 +19,69 @@
 #' @include expression.R
 NULL
 
-# This environment contains a cache of nse_funcs as a list()
-.cache <- new.env(parent = emptyenv())
+
+#' Register compute translations
+#'
+#' The [register_translation()] and [register_translation_agg()] functions
+#' are used to populate a list of functions that operate on (and return)
+#' Expressions. These are the basis for the `.data` mask inside dplyr methods.
+#'
+#' @section Writing translations:
+#' When to use `build_expr()` vs. `Expression$create()`?
+#'
+#' Use `build_expr()` if you need to
+#' - map R function names to Arrow C++ functions
+#' - wrap R inputs (vectors) as Array/Scalar
+#'
+#' `Expression$create()` is lower level. Most of the translations use it
+#' because they manage the preparation of the user-provided inputs
+#' and don't need or don't want to the automatic conversion of R objects
+#' to [Scalar].
+#'
+#' @param fun_name A function name in the form `"function"` or
+#'   `"package::function"`. The package name is currently not used but
+#'   may be used in the future to allow these types of function calls.
+#' @param fun A function or `NULL` to un-register a previous function.
+#'   This function must accept [Expression] objects as arguments and return
+#'   [Expression] objects instead of regular R objects.
+#' @param agg_fun An aggregate function or `NULL` to un-register a previous
+#'   aggregate function. This function must accept [Expression] objects as
+#'   arguments and return a [list()] with components:
+#'   - `fun`: string function name
+#'   - `data`: [Expression] (these are all currently a single field)
+#'   - `options`: list of function options, as passed to call_function
+#' @param registry An [environment()] in which the functions should be
+#'   assigned.
+#'
+#' @return The previously registered function or `NULL` if no previously
+#'   registered function existed.
+#' @keywords internal
+register_translation <- function(fun_name, fun, registry = translation_registry()) {
+  name <- gsub("^.*?::", "", fun_name)
+  namespace <- gsub("::.*$", "", fun_name)
+
+  previous_fun <- if (name %in% names(fun)) registry[[name]] else NULL
+
+  if (is.null(fun)) {
+    rm(list = name, envir = registry)
+  } else {
+    registry[[name]] <- fun
+  }
+
+  invisible(previous_fun)
+}
+
+register_translation_agg <- function(fun_name, agg_fun, registry = translation_registry_agg()) {
+  register_translation(fun_name, agg_fun, registry = registry)
+}
+
+translation_registry <- function() {
+  nse_funcs
+}
+
+translation_registry_agg <- function() {
+  agg_funcs
+}
 
 # Called in .onLoad()
 create_translation_cache <- function() {
@@ -51,53 +112,7 @@ create_translation_cache <- function() {
   .cache$functions <- c(as.list(nse_funcs), arrow_funcs)
 }
 
-# nse_funcs is a list of functions that operated on (and return) Expressions
-# These will be the basis for a data_mask inside dplyr methods
-# and will be added to .cache at package load time
+# environments in the arrow namespace used in the above functions
 nse_funcs <- new.env(parent = emptyenv())
-
-# agg_funcs is a list of functions with a different signature than nse_funcs;
-# described below
 agg_funcs <- new.env(parent = emptyenv())
-
-translation_registry <- function() {
-  nse_funcs
-}
-
-translation_registry_agg <- function() {
-  agg_funcs
-}
-
-register_translation <- function(fun_name, fun, registry = translation_registry()) {
-  name <- gsub("^.*?::", "", fun_name)
-  namespace <- gsub("::.*$", "", fun_name)
-
-  previous_fun <- if (name %in% names(fun)) registry[[name]] else NULL
-
-  if (is.null(fun)) {
-    rm(list = name, envir = registry)
-  } else {
-    registry[[name]] <- fun
-  }
-
-  invisible(previous_fun)
-}
-
-register_translation_agg <- function(fun_name, fun, registry = translation_registry_agg()) {
-  register_translation(fun_name, fun, registry = registry)
-}
-
-
-# Now add functions to that list where the mapping from R to Arrow isn't 1:1
-# Each of these functions should have the same signature as the R function
-# they're replacing.
-#
-# When to use `build_expr()` vs. `Expression$create()`?
-#
-# Use `build_expr()` if you need to
-# (1) map R function names to Arrow C++ functions
-# (2) wrap R inputs (vectors) as Array/Scalar
-#
-# `Expression$create()` is lower level. Most of the functions below use it
-# because they manage the preparation of the user-provided inputs
-# and don't need to wrap scalars
+.cache <- new.env(parent = emptyenv())
