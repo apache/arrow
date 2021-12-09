7eb138b154b0794d0a693a4211330766ae08ad5d
Dragos Moldovan-Grünfeld <dragos.mold@gmail.com>
2021-12-09 20:35:39 +0000

    removed the line as the C++ linter doesn't like commented code

--- r/src/array_to_vector.cpp
+++ r/src/array_to_vector.cpp
@@ -920,7 +920,6 @@ class Converter_Decimal : public Converter {
 
   Status Ingest_some_nulls(SEXP data, const std::shared_ptr<arrow::Array>& array,
                            R_xlen_t start, R_xlen_t n, size_t chunk_index) const {
-    //using DecimalArray = typename TypeTraits<Type>::ArrayType;
     auto p_data = REAL(data) + start;
     const auto& decimals_arr = checked_cast<const arrow::DecimalArray&>(*array);
 
