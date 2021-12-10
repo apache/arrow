81b81fb2b5cb42d5fd560828c8dfa63ebe5b8bde
Jonathan Keane <jkeane@gmail.com>
2021-12-10 16:07:46 -0600

    ARROW-15057: [R] [CI] Move where we install DuckDB from in CI
    Closes #11927 from jonkeane/duckdb-building-again
    
    Authored-by: Jonathan Keane <jkeane@gmail.com>
    Signed-off-by: Jonathan Keane <jkeane@gmail.com>

--- ci/scripts/r_deps.sh
+++ ci/scripts/r_deps.sh
@@ -46,7 +46,7 @@ ${R_BIN} -e "options(warn=2); install.packages('remotes'); remotes::install_cran
 # (though only if we haven't filtered it out of the deps above,
 # and if we can't get a binary from RSPM)
 # Remove when there is a DuckDB release > 0.3.1-1
-if grep -q "duckdb," DESCRIPTION; then
+if [ ${R_PRUNE_DEPS} != "true" ]; then
   ${R_BIN} -e "if (all(!grepl('packagemanager.rstudio', options('repos')))) { remotes::install_github('duckdb/duckdb', subdir = '/tools/rpkg', build = FALSE) }"
 fi
 
