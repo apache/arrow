7172ac385ec8e4d68471f7fe86a8b9f2de11e501
Alessandro Molina <amol@turbogears.org>
2021-12-13 15:16:10 +0100

    Solve missing stubs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,6 +95,7 @@ ipython_mplbackend = ''
 
 # numpydoc configuration
 numpydoc_xref_param_type = True
+numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
