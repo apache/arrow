4d384abefe187b1d0070734a4fdcbe4f57be5bd8
Rafael Telles <rafael@telles.dev>
2021-12-20 14:18:00 -0300

    Skip Rust and Go from Flight SQL integration test scenario

--- dev/archery/archery/integration/runner.py
+++ dev/archery/archery/integration/runner.py
@@ -384,7 +384,9 @@ def run_all_tests(with_cpp=True, with_java=True, with_js=True,
         ),
         Scenario(
             "flight_sql",
-            description="Ensure that Flight SQL protocol is working as expected."),
+            description="Ensure that Flight SQL protocol is working as expected.",
+            skip={"Rust", "Go"}
+        ),
     ]
 
     runner = IntegrationRunner(json_files, flight_scenarios, testers, **kwargs)
