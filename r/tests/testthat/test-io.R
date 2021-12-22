e11682e2ff0b0d97109ad9c296b565e52c024680
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-22 16:02:57 -0400

    with better tests

--- r/tests/testthat/test-io.R
+++ r/tests/testthat/test-io.R
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-test_that("reencoding input stream works", {
+test_that("reencoding input stream works for windows-1252", {
   string <- "province_name\nQu\u00e9bec"
   bytes_windows1252 <- iconv(string, to = "windows-1252", toRaw = TRUE)[[1]]
   bytes_utf8 <- iconv(string, to = "UTF-8", toRaw = TRUE)[[1]]
@@ -36,19 +36,38 @@ test_that("reencoding input stream works", {
   unlink(temp_windows1252)
 })
 
-test_that("reencoding input stream works when buffer needs reallocating", {
+test_that("reencoding input stream works for UTF-16", {
   string <- paste0(strrep("a\u00e9\U0001f4a9", 30))
   bytes_utf16 <- iconv(string, to = "UTF-16LE", toRaw = TRUE)[[1]]
+  bytes_utf8 <- iconv(string, to = "UTF-8", toRaw = TRUE)[[1]]
+
+  temp_utf16 <- tempfile()
+  con <- file(temp_utf16, open = "wb")
+  writeBin(bytes_utf16, con)
+  close(con)
+
+  fs <- LocalFileSystem$create()
+
+  stream <- fs$OpenInputStream(temp_utf8)
+  stream_utf8 <- MakeRencodeInputStream(stream, "UTF-8")
+
+  expect_identical(
+    as.raw(stream_utf8$Read(length(bytes_utf8))),
+    bytes_utf8
+  )
+
+  stream_utf8$close()
+  stream$close()
+  unlink(temp_utf16)
+})
+
+test_that("reencoding input stream works with pending characters", {
+  string <- paste0(strrep("a\u00e9\U0001f4a9", 30))
   bytes_utf8 <- iconv(string, to = "UTF-8", toRaw = TRUE)[[1]]
 
   temp_utf8 <- tempfile()
   con <- file(temp_utf8, open = "wb")
   writeBin(bytes_utf8, con)
-  close(con)
-
-  temp_utf16 <- tempfile()
-  con <- file(temp_utf16, open = "wb")
-  writeBin(bytes_utf16, con)
   close(con)
 
   fs <- LocalFileSystem$create()
@@ -67,20 +86,6 @@ test_that("reencoding input stream works when buffer needs reallocating", {
     as.raw(stream_utf8$Read(length(bytes_utf8))),
     bytes_utf8[23:length(bytes_utf8)]
   )
-
-  stream$close()
-  stream_utf8$close()
-
-
-  stream <- fs$OpenInputStream(temp_utf8)
-  stream_utf8 <- MakeRencodeInputStream(stream, "UTF-8")
-
-  for (i in seq_len(14)) as.raw(stream_utf8$Read(5))
-  # this call leaves pending characters
-  as.raw(stream_utf8$Read(5))
-
-  # and this call fails with not implemented
-  as.raw(stream_utf8$Read(5))
 
   stream$close()
   stream_utf8$close()
