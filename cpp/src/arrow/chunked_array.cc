b44ed7d38efd42f31ddc40c271d2d9d2e8c118e6
Eduardo Ponce <edponce00@gmail.com>
2021-12-28 14:09:39 -0500

    change to single constructor with function overloading

--- cpp/src/arrow/chunked_array.cc
+++ cpp/src/arrow/chunked_array.cc
@@ -42,24 +42,6 @@ class MemoryPool;
 // ----------------------------------------------------------------------
 // ChunkedArray methods
 
-ChunkedArray::ChunkedArray(ArrayVector chunks) : chunks_(std::move(chunks)) {
-  length_ = 0;
-  null_count_ = 0;
-
-  ARROW_CHECK_GT(chunks_.size(), 0)
-      << "cannot construct ChunkedArray from empty vector and omitted type";
-  type_ = chunks_[0]->type();
-  for (const auto& chunk : chunks_) {
-    ARROW_CHECK(chunk->type()->Equals(*type_))
-        << Status::Invalid("Array chunks must all be same type") << type_->ToString();
-  }
-
-  for (const std::shared_ptr<Array>& chunk : chunks_) {
-    length_ += chunk->length();
-    null_count_ += chunk->null_count();
-  }
-}
-
 ChunkedArray::ChunkedArray(ArrayVector chunks, std::shared_ptr<DataType> type)
     : chunks_(std::move(chunks)), type_(std::move(type)) {
   length_ = 0;
