9e753faac2769a7bb4cf4fe828e4a1a92b2f5d5b
Alessandro Molina <amol@turbogears.org>
2022-01-03 14:05:13 +0100

    lint

--- cpp/src/arrow/compute/kernels/vector_selection_test.cc
+++ cpp/src/arrow/compute/kernels/vector_selection_test.cc
@@ -2392,8 +2392,8 @@ TEST(TestIndicesNonZero, IndicesNonZero) {
   result = actual.make_array();
   AssertArraysEqual(*result, *ArrayFromJSON(uint64(), "[]"));
 
-  ChunkedArray chunkedarr({ArrayFromJSON(uint32(), "[1, 0, 3]"),
-                           ArrayFromJSON(uint32(), "[4, 0, 6]")});
+  ChunkedArray chunkedarr(
+      {ArrayFromJSON(uint32(), "[1, 0, 3]"), ArrayFromJSON(uint32(), "[4, 0, 6]")});
   ASSERT_OK_AND_ASSIGN(actual,
                        CallFunction("indices_nonzero", {static_cast<Datum>(chunkedarr)}));
   Datum expected = ChunkedArrayFromJSON(uint64(), {R"([0, 2])", R"([0, 2])"});
