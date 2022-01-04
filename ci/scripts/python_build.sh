b79a50a38a5677e9d0ea810ef01829c4cad30d58
Antoine Pitrou <antoine@python.org>
2022-01-04 13:04:02 +0100

    ARROW-15243: [CI][Python] Make PyArrow installation more robust in CI
    On Debian/Ubuntu, the system-provided setuptools is patched to install into
    the system-specific `dist-packages` directory.
    However, the newer setuptools from upstream would install PyArrow in the wrong location (`site-packages`).
    
    Therefore, use "pip install" instead of "python setup.py build" to get the right behaviour.

--- ci/scripts/python_build.sh
+++ ci/scripts/python_build.sh
@@ -27,6 +27,21 @@ python_build_dir=${build_dir}/python
 
 : ${BUILD_DOCS_PYTHON:=OFF}
 
+case "$(uname)" in
+  Linux)
+    n_jobs=$(nproc)
+    ;;
+  Darwin)
+    n_jobs=$(sysctl -n hw.ncpu)
+    ;;
+  MINGW*)
+    n_jobs=${NUMBER_OF_PROCESSORS:-1}
+    ;;
+  *)
+    n_jobs=${NPROC:-1}
+    ;;
+esac
+
 if [ ! -z "${CONDA_PREFIX}" ]; then
   echo -e "===\n=== Conda environment for build\n==="
   conda list
@@ -44,19 +59,17 @@ export PYARROW_WITH_GANDIVA=${ARROW_GANDIVA:-OFF}
 export PYARROW_WITH_PARQUET=${ARROW_PARQUET:-OFF}
 export PYARROW_WITH_DATASET=${ARROW_DATASET:-OFF}
 
+export PYARROW_PARALLEL=${n_jobs}
+
 export LD_LIBRARY_PATH=${ARROW_HOME}/lib:${LD_LIBRARY_PATH}
 
+# Build and install from source distribution to avoid leaving build artifacts
+# in the source directory.
+rm -f /tmp/pyarrow*.tar.gz
 pushd ${source_dir}
-
-relative_build_dir=$(realpath --relative-to=. $python_build_dir)
-
-# not nice, but prevents mutating the mounted the source directory for docker
-${PYTHON:-python} \
-  setup.py build --build-base $python_build_dir \
-           install --single-version-externally-managed \
-                   --record $relative_build_dir/record.txt
-
+${PYTHON:-python} setup.py sdist --dist-dir /tmp
 popd
+${PYTHON:-python} -m pip install -vv /tmp/pyarrow*.tar.gz
 
 if [ "${BUILD_DOCS_PYTHON}" == "ON" ]; then
   ncpus=$(python -c "import os; print(os.cpu_count())")
