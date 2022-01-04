31a07be1d9dc2f7c9720cc0fdcd7f083d947aba1
Will Jones <willjones127@gmail.com>
2022-01-04 16:56:43 +0100

    ARROW-13735: [C++][Python] Creating a Map array with non-default field names segfaults
    The segfault only happened in Debug build (which is why some weren't able to repro) because it was from a failing DCHECK on this line:
    
    https://github.com/apache/arrow/blob/e734856676a00335f3dfe79899a995d87286f5a9/cpp/src/arrow/array/array_nested.cc#L193
    
    The LHS of the DCHECK is missing field names, while the RHS has them.
    
    The basic problem is that MapBuilder doesn't preserve field names when creating the array. This PR fixes that.
    
    Closes #11855 from wjones127/ARROW-13735-map-custom-name-segfault
    
    Lead-authored-by: Will Jones <willjones127@gmail.com>
    Co-authored-by: Will Jones <wjones127@users.noreply.github.com>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/src/arrow/array/builder_nested.h
+++ cpp/src/arrow/array/builder_nested.h
@@ -330,7 +330,14 @@ class ARROW_EXPORT MapBuilder : public ArrayBuilder {
   ArrayBuilder* value_builder() const { return list_builder_->value_builder(); }
 
   std::shared_ptr<DataType> type() const override {
-    return map(key_builder_->type(), item_builder_->type(), keys_sorted_);
+    // Key and Item builder may update types, but they don't contain the field names,
+    // so we need to reconstruct the type. (See ARROW-13735.)
+    return std::make_shared<MapType>(
+        field(entries_name_,
+              struct_({field(key_name_, key_builder_->type(), false),
+                       field(item_name_, item_builder_->type(), item_nullable_)}),
+              false),
+        keys_sorted_);
   }
 
   Status ValidateOverflow(int64_t new_elements) {
@@ -342,6 +349,10 @@ class ARROW_EXPORT MapBuilder : public ArrayBuilder {
 
  protected:
   bool keys_sorted_ = false;
+  bool item_nullable_ = false;
+  std::string entries_name_;
+  std::string key_name_;
+  std::string item_name_;
   std::shared_ptr<ListBuilder> list_builder_;
   std::shared_ptr<ArrayBuilder> key_builder_;
   std::shared_ptr<ArrayBuilder> item_builder_;
