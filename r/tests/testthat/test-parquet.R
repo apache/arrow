a77474c734ce3ead290ee0ba7e6251276db5119e
Will Jones <willjones127@gmail.com>
2022-01-04 13:51:28 -0800

    Add in parquet tests for nested data

--- r/tests/testthat/test-parquet.R
+++ r/tests/testthat/test-parquet.R
@@ -371,12 +371,14 @@ test_that("deprecated int96 timestamp unit can be specified when reading Parquet
   expect_true(result$some_datetime == table$some_datetime)
 })
 
-# Wait on ARROW-13735 for this
-# test_that("Can read parquet with nested lists and maps", {
-#   parquet_test_data <- test_path("../../../cpp/submodules/parquet-testing/data")
-#   skip_if_not(dir.exists(parquet_test_data), "Parquet test data missing")
+test_that("Can read parquet with nested lists and maps", {
+  parquet_test_data <- test_path("../../../cpp/submodules/parquet-testing/data")
+  skip_if_not(dir.exists(parquet_test_data), "Parquet test data missing")
 
-#   # pq <- read_parquet(paste0(parquet_test_data, '/nested_lists.snappy.parquet'))
-#   browser()
-#   pq <- read_parquet(paste0(parquet_test_data, '/nested_maps.snappy.parquet'))
-# })
+  pq <- read_parquet(paste0(parquet_test_data, "/nested_lists.snappy.parquet"), as_data_frame = FALSE)
+  # value name is "element" from parquet reader, but type default is "item"
+  expect_equal(pq$a$type, list_of(field("element", list_of(field("element", list_of(field("element", utf8())))))))
+
+  pq <- read_parquet(paste0(parquet_test_data, "/nested_maps.snappy.parquet"), as_data_frame = FALSE)
+  expect_equal(pq$a$type, map_of(utf8(), map_of(int32(), boolean())))
+})
