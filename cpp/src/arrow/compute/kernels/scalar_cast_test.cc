3b995ce89d65d8469e2e6e02824648a0c3fbefd9
William Ayd <william.ayd@icloud.com>
2022-01-08 15:59:27 -0500

    Added failing test case

--- cpp/src/arrow/compute/kernels/scalar_cast_test.cc
+++ cpp/src/arrow/compute/kernels/scalar_cast_test.cc
@@ -2217,6 +2217,28 @@ TEST(Cast, ListToListOptionsPassthru) {
   }
 }
 
+static void CheckStructToStruct(const std::vector<std::shared_ptr<DataType>>& value_types) {
+  for (const auto& src_value_type : value_types) {
+    for (const auto& dest_value_type : value_types) {
+      std::vector<std::string> field_names = {"a", "b"};      
+      std::shared_ptr<Array> a1, b1, a2, b2;
+      a1 = ArrayFromJSON(src_value_type, "[1, 2]");
+      b1 = ArrayFromJSON(src_value_type, "[3, 4]");
+      a2 = ArrayFromJSON(dest_value_type, "[1, 2]");
+      b2 = ArrayFromJSON(dest_value_type, "[3, 4]");      
+
+      auto src = StructArray::Make({a1, b2}, field_names).ValueOrDie();
+      auto dest = StructArray::Make({a2, b2}, field_names).ValueOrDie();
+
+      CheckCast(src, dest);
+    }
+  }
+}
+
+TEST(Cast, StructToSameSizedAndNamedStruct) {
+  CheckStructToStruct({int32(), float32(), int64()});
+}
+
 TEST(Cast, IdentityCasts) {
   // ARROW-4102
   auto CheckIdentityCast = [](std::shared_ptr<DataType> type, const std::string& json) {
