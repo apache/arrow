8ac0944cde06bdb8ec75d18ae1a77010c605c2de
David Li <li.davidm96@gmail.com>
2022-01-10 14:55:37 +0100

    ARROW-15248: [C++][Docs] Improve docs about linting/formatting
    This improves the docs by clarifying a few things:
    
    * How to lint/format using a CMake build,
    * The linter/formatter version must match exactly,
    * Formatting can be done by commenting on a PR.
    
    Also, the example Archery command is extended to list all the C++ formatters, and we describe where to get a copy of the LLVM tools.
    
    Closes #12085 from lidavidm/arrow-15248
    
    Authored-by: David Li <li.davidm96@gmail.com>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/cmake_modules/FindClangTools.cmake
+++ cpp/cmake_modules/FindClangTools.cmake
@@ -64,6 +64,8 @@ function(FIND_CLANG_TOOL NAME OUTPUT VERSION_CHECK_PATTERN)
                       OUTPUT_VARIABLE CLANG_TOOL_VERSION_MESSAGE
                       OUTPUT_STRIP_TRAILING_WHITESPACE)
       if(NOT (${CLANG_TOOL_VERSION_MESSAGE} MATCHES ${VERSION_CHECK_PATTERN}))
+        message(STATUS "${NAME} found, but version did not match \"${VERSION_CHECK_PATTERN}\""
+        )
         set(CLANG_TOOL_BIN "CLANG_TOOL_BIN-NOTFOUND")
       endif()
     endif()
@@ -89,7 +91,7 @@ if(CLANG_TIDY_BIN)
   message(STATUS "clang-tidy found at ${CLANG_TIDY_BIN}")
 else()
   set(CLANG_TIDY_FOUND 0)
-  message(STATUS "clang-tidy not found")
+  message(STATUS "clang-tidy ${ARROW_CLANG_TOOLS_VERSION} not found")
 endif()
 
 find_clang_tool(clang-format CLANG_FORMAT_BIN
@@ -99,7 +101,7 @@ if(CLANG_FORMAT_BIN)
   message(STATUS "clang-format found at ${CLANG_FORMAT_BIN}")
 else()
   set(CLANG_FORMAT_FOUND 0)
-  message(STATUS "clang-format not found")
+  message(STATUS "clang-format ${ARROW_CLANG_TOOLS_VERSION} not found")
 endif()
 
 find_package_handle_standard_args(ClangTools REQUIRED_VARS CLANG_FORMAT_BIN
