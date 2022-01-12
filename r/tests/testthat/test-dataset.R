4aa6e78963e04e078a89d6bdcf789b9601444027
Neal Richardson <neal.p.richardson@gmail.com>
2022-01-12 17:57:28 -0500

    Fix NOTE about global variables in map_batches

--- r/tests/testthat/test-dataset.R
+++ r/tests/testthat/test-dataset.R
@@ -671,24 +671,22 @@ test_that("Scanner$ScanBatches", {
   table <- Table$create(!!!batches)
   expect_equal(as.data.frame(table), rbind(df1, df2))
 
-  expect_deprecated(ds$NewScan()$UseAsync(TRUE), paste0(
+  expect_deprecated(ds$NewScan()$UseAsync(TRUE), paste(
     "The function",
-    " 'UseAsync' is deprecated and will be removed in a future release."
+    "'UseAsync' is deprecated and will be removed in a future release."
   ))
-  expect_deprecated(ds$NewScan()$UseAsync(FALSE), paste0(
+  expect_deprecated(ds$NewScan()$UseAsync(FALSE), paste(
     "The function",
-    " 'UseAsync' is deprecated and will be removed in a future release."
+    "'UseAsync' is deprecated and will be removed in a future release."
   ))
 
-  expect_deprecated(Scanner$create(ds, use_async = TRUE), paste0(
-    "The",
-    " parameter 'use_async' is deprecated and will be removed in a future",
-    " release."
+  expect_deprecated(Scanner$create(ds, use_async = TRUE), paste(
+    "The parameter 'use_async' is deprecated and will be removed in a future",
+    "release."
   ))
-  expect_deprecated(Scanner$create(ds, use_async = FALSE), paste0(
-    "The",
-    " parameter 'use_async' is deprecated and will be removed in a future",
-    " release."
+  expect_deprecated(Scanner$create(ds, use_async = FALSE), paste(
+    "The parameter 'use_async' is deprecated and will be removed in a future",
+    "release."
   ))
 })
 
