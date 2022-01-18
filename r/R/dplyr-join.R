1e40b8860c7649681b6b9225d3ef508577e521e4
Dragos Moldovan-Grünfeld <dragos.mold@gmail.com>
2022-01-18 15:46:03 +0000

    remove dependency on {glue}

--- r/R/dplyr-join.R
+++ r/R/dplyr-join.R
@@ -120,24 +120,28 @@ handle_join_by <- function(by, x, y) {
 
   missing_x_cols <- setdiff(names(by), names(x))
   if (length(missing_x_cols) > 0) {
-    message <- glue::glue(
-      "Join {ngettext(length(missing_x_cols), 'column', 'columns')} must be \\
-      present in data."
+    message <- paste(
+      "Join",
+      ngettext(length(missing_x_cols), 'column', 'columns'),
+      "must be present in data."
     )
-    message_x <- glue::glue(
-      "{oxford_paste(missing_x_cols, quote_symbol = '`')} not present in x."
+    message_x <- paste(
+      oxford_paste(missing_x_cols, quote_symbol = '`'),
+      "not present in x."
       )
     abort(c(message, x = message_x))
   }
 
   missing_y_cols <- setdiff(by, names(y))
   if (length(missing_y_cols) > 0) {
-    message <- glue::glue(
-      "Join {ngettext(length(missing_y_cols), 'column', 'columns')} must be \\
-      present in data."
+    message <- paste(
+      "Join",
+      ngettext(length(missing_y_cols), 'column', 'columns'),
+      "must be present in data."
     )
-    message_y <- glue::glue(
-      "{oxford_paste(missing_y_cols, quote_symbol = '`')} not present in y."
+    message_y <- paste(
+      oxford_paste(missing_y_cols, quote_symbol = '`'),
+      "not present in y."
     )
     abort(c(message, x = message_y))
   }
