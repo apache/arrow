351e550f5ce7ba3cf62a78e42b74461867cfa3e1
Nic Crane <thisisnic@gmail.com>
2022-01-20 16:42:56 +0000

    Initial script

---
+++ ci/scripts/util_update_docs_versions.py
@@ -0,0 +1,64 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import json
+import sys
+
+dir_path = sys.argv[0]
+version = sys.argv[1] 
+r_version = sys.argv[2] 
+
+main_versions_path = dir_path + "/docs/source/_static/versions.json"
+r_versions_path = dir_path + "/r/pkgdown/assets/versions.json"
+
+## Udpdate main docs version script
+
+with open(main_versions_path) as json_file:
+    old_versions = json.load(json_file)
+    
+split_version = version.split(".")
+major_minor = split_version[0] + "." + split_version[1]
+
+# Update the old stable version to a numbered version
+old_versions[0]["name"] = old_versions[0]["name"].split(" ")[0]
+old_versions[0]["version"] = old_versions[0]["name"] + '/'
+
+# Create row for new version
+new_version = [{'name': major_minor + " (stable)", 'version':''}]
+new_version.extend(old_versions)
+with open(main_versions_path, 'w') as json_file:
+    json.dump(new_version, json_file, indent = 4)
+    
+## Update R package version script
+
+with open(r_versions_path) as json_file:
+    old_r_versions = json.load(json_file)
+    
+# update release to oldrel
+old_r_versions[1]["name"] = old_r_versions[1]["name"].split(" ")[0]
+old_rel_split = old_r_versions[1]["name"].split(".")
+old_r_versions[1]["version"] = old_rel_split[0] + "." + old_rel_split[1] + "/"
+
+# update dev to release
+old_r_versions[0]["name"] = r_version + " (release)"
+old_r_versions[0]["version"] = ""
+
+# add new dev version
+new_r_version = [{'name': r_version + ".9000" + " (dev)", 'version':'dev/'}]
+new_r_version.extend(old_r_versions)
+with open(r_versions_path, 'w') as json_file:
+    json.dump(new_r_version, json_file, indent = 4)
