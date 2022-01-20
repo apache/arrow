eab03791c44fd7abb7dd5a7ef7ebcc19d669c227
Vibhatha Abeykoon <vibhatha@gmail.com>
2022-01-20 15:19:47 +0530

    adding nbytes test cases

--- python/pyarrow/tests/test_array.py
+++ python/pyarrow/tests/test_array.py
@@ -2471,12 +2471,15 @@ def test_buffers_nested():
 
 def test_total_buffer_size():
     a = pa.array(np.array([4, 5, 6], dtype='int64'))
+    assert a.nbytes == 8 * 3
     assert a.get_total_buffer_size() == 8 * 3
     assert sys.getsizeof(a) >= object.__sizeof__(a) + a.nbytes
     a = pa.array([1, None, 3], type='int64')
+    assert a.nbytes == 8*3 + 1
     assert a.get_total_buffer_size() == 8*3 + 1
     assert sys.getsizeof(a) >= object.__sizeof__(a) + a.nbytes
     a = pa.array([[1, 2], None, [3, None, 4, 5]], type=pa.list_(pa.int64()))
+    assert a.nbytes == 62
     assert a.get_total_buffer_size() == 1 + 4 * 4 + 1 + 6 * 8
     assert sys.getsizeof(a) >= object.__sizeof__(a) + a.nbytes
     a = pa.array([[[5, 6, 7]], [[9, 10]]], type=pa.list_(pa.list_(pa.int8())))
