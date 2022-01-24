8bd2e85a00cb90af0a6c0a3035f10ce72052ed0e
Danielle Navarro <djnavarro@protonmail.com>
2022-01-25 10:49:53 +1100

    add bindings and tests

--- r/tests/testthat/test-dplyr-funcs-datetime.R
+++ r/tests/testthat/test-dplyr-funcs-datetime.R
@@ -684,3 +684,23 @@ test_that("leap_year mirror lubridate", {
   )
 
 })
+
+test_that("am/pm mirror lubridate", {
+
+  compare_dplyr_binding(
+    .input %>%
+      mutate(
+        am = am(test_time),
+        pm = pm(test_time)
+      ) %>%
+      collect(),
+    data.frame(
+      test_time = strptime(c(
+        "2022-01-25 11:50:59",
+        "2022-01-25 12:00:00",
+        "2022-01-25 00:00:00"
+      ), format = "%Y-%m-%d %H:%M:%S")
+    )
+  )
+
+})
