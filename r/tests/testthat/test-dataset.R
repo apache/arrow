9c56ed06d2b8f2aaf71d3d6ee31100857024027c
Jonathan Keane <jkeane@gmail.com>
2022-01-26 14:13:02 -0600

    Simplified RBR -> stream test

--- r/tests/testthat/test-dataset.R
+++ r/tests/testthat/test-dataset.R
@@ -849,14 +849,8 @@ test_that("dataset RecordBatchReader to C-interface to arrow_dplyr_query", {
   reader <- scan$ToRecordBatchReader()
   reader$export_to_c(stream_ptr)
 
-  # then import it and check that the roundtripped value is the same
-  circle <- RecordBatchStreamReader$import_from_c(stream_ptr)
-
-  # create an arrow_dplyr_query() from the recordbatch reader
-  reader_adq <- arrow_dplyr_query(circle)
-
   expect_equal(
-    reader_adq %>%
+    RecordBatchStreamReader$import_from_c(stream_ptr) %>%
       filter(int < 8 | int > 55) %>%
       mutate(part_plus = group + 6) %>%
       arrange(dbl) %>%
