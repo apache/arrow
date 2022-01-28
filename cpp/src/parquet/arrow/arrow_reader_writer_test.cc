88298c531d6d3cc323c46b9340f06e7e95bbde56
Will Jones <willjones127@gmail.com>
2022-01-28 14:53:12 -0800

    Create a test for overflow

--- cpp/src/parquet/arrow/arrow_reader_writer_test.cc
+++ cpp/src/parquet/arrow/arrow_reader_writer_test.cc
@@ -3675,6 +3675,116 @@ TEST(TestArrowReaderAdHoc, LARGE_MEMORY_TEST(LargeStringColumn)) {
   AssertTablesEqual(*table, *batched_table, /*same_chunk_layout=*/false);
 }
 
+class NoOverflowMemoryPool : public MemoryPool {
+ public:
+  explicit NoOverflowMemoryPool(MemoryPool* pool, int64_t padding);
+  ~NoOverflowMemoryPool() override = default;
+
+  Status Allocate(int64_t size, uint8_t** out) override;
+  Status Reallocate(int64_t old_size, int64_t new_size, uint8_t** ptr) override;
+
+  void Free(uint8_t* buffer, int64_t size) override;
+
+  int64_t bytes_allocated() const override;
+
+  int64_t max_memory() const override;
+
+  std::string backend_name() const override;
+
+ private:
+  MemoryPool* pool_;
+  int64_t padding_;
+  std::unordered_map<uint8_t**, int64_t> buffers_;
+  const uint8_t check_value_ = '\xff';
+};
+
+NoOverflowMemoryPool::NoOverflowMemoryPool(MemoryPool* pool, int64_t padding)
+    : pool_(pool), padding_(padding) {}
+
+Status NoOverflowMemoryPool::Allocate(int64_t size, uint8_t** out) {
+  Status s = pool_->Allocate(size + padding_, out);
+  if (s.ok()) {
+    buffers_[out] = size;
+
+    for (int i = size; i < size + padding_; ++i) {
+      (*out)[i] = check_value_;
+    }
+  }
+  return s;
+}
+
+Status NoOverflowMemoryPool::Reallocate(int64_t old_size, int64_t new_size,
+                                        uint8_t** ptr) {
+  Status s = pool_->Reallocate(old_size + padding_, new_size + padding_, ptr);
+  if (s.ok()) {
+    buffers_[ptr] = new_size;
+
+    for (int i = new_size; i < new_size + padding_; ++i) {
+      (*ptr)[i] = check_value_;
+    }
+  }
+  return s;
+}
+
+void NoOverflowMemoryPool::Free(uint8_t* buffer, int64_t size) {
+  // DCHECK_EQ(size, buffers_[&buffer]);
+  for (int i = size; i < size + padding_; ++i) {
+    DCHECK_EQ(buffer[i], check_value_);
+  }
+  pool_->Free(buffer, size + padding_);
+}
+
+int64_t NoOverflowMemoryPool::bytes_allocated() const { return pool_->bytes_allocated(); }
+
+int64_t NoOverflowMemoryPool::max_memory() const { return pool_->max_memory(); }
+
+std::string NoOverflowMemoryPool::backend_name() const { return pool_->backend_name(); }
+
+TEST(TestArrowReaderAdHoc, RepeatReadNullableStructColumn) {
+  // ARROW-14047
+
+  // This pool validates that no bytes are modified past the end of the buffer
+  auto pool_obj = NoOverflowMemoryPool(default_memory_pool(), /*padding=*/3);
+  auto pool = &pool_obj;
+  
+  auto gen = ::arrow::random::RandomArrayGenerator(42);
+  auto float_arr = gen.Float64(320 * 8, /*min=*/-10.0, /*max=*/10.0,
+                               /*null_probability=*/0.1, /*nan_probability=*/0.0);
+  ASSERT_OK_AND_ASSIGN(auto struct_arr, ::arrow::StructArray::Make(
+                           {float_arr}, {std::make_shared<::arrow::Field>(
+                                            "x", ::arrow::float64(), true)}));
+  auto list_arr = gen.List(*struct_arr.get(), 800, /*null_probability=*/0.1,
+                           /*force_empty_nulls=*/false);
+  auto fields = {std::make_shared<::arrow::Field>(
+          "records", list_arr->type(), /*nullable=*/true)};
+  auto batch = ::arrow::RecordBatch::Make(
+      std::make_shared<::arrow::Schema>(fields),
+      list_arr->length(), {list_arr});
+  ASSERT_OK_AND_ASSIGN(auto expected, Table::FromRecordBatches({batch}));
+
+  // Write table
+  using ::arrow::io::BufferOutputStream;
+  ASSERT_OK_AND_ASSIGN(auto outs, BufferOutputStream::Create(1 << 10, pool));
+  auto props = default_writer_properties();
+  std::unique_ptr<arrow::FileWriter> writer;
+  ASSERT_OK(
+      arrow::FileWriter::Open(*expected->schema().get(), pool, outs, props, &writer));
+  ASSERT_OK(writer->WriteTable(*expected.get(), 1));
+  ASSERT_OK(writer->Close());
+
+  ASSERT_OK_AND_ASSIGN(auto buffer, outs->Finish());
+
+  // Read from table
+  std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
+  ASSERT_OK(parquet::arrow::OpenFile(std::make_shared<BufferReader>(buffer), pool,
+                                     &arrow_reader));
+
+  std::shared_ptr<::arrow::Table> table_read;
+  ASSERT_OK(arrow_reader->ReadTable(&table_read));
+
+  ::arrow::AssertTablesEqual(*expected.get(), *table_read.get());
+}
+
 TEST(TestArrowReaderAdHoc, HandleDictPageOffsetZero) {
 #ifndef ARROW_WITH_SNAPPY
   GTEST_SKIP() << "Test requires Snappy compression";
