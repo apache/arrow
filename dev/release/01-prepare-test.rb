5f9a84d8b5bdb6e971072d364dc2cedb35715a56
Nic Crane <thisisnic@gmail.com>
2022-02-02 16:58:11 +0000

    Remove extra parens

--- dev/release/01-prepare-test.rb
+++ dev/release/01-prepare-test.rb
@@ -251,8 +251,8 @@ class PrepareTest < Test::Unit::TestCase
             "+    {",
             "+        \"name\": \"#{@previous_version}\",",
             "+        \"version\": \"#{@previous_compatible_version + "/"}\"",
-            "+    },"],
-            ["-]", "+]     "],
+            "+    },",
+            "-]", "+]     ",
           ]
         ]
       }
