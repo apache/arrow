1125c3e0904573f9af0a0fb3cde9d0cea18fd873
Vibhatha Abeykoon <vibhatha@gmail.com>
2022-02-04 08:05:56 +0530

    removing the future from options

--- cpp/src/arrow/compute/exec/options.h
+++ cpp/src/arrow/compute/exec/options.h
@@ -287,15 +287,11 @@ class ARROW_EXPORT SelectKSinkNodeOptions : public SinkNodeOptions {
 class ARROW_EXPORT TableSinkNodeOptions : public ExecNodeOptions {
  public:
   TableSinkNodeOptions(std::shared_ptr<Table>* output_table,
-                       std::shared_ptr<Schema> output_schema,
-                       Future<> finish = Future<>::Make())
-      : output_table(output_table),
-        output_schema(std::move(output_schema)),
-        finish(std::move(finish)) {}
+                       std::shared_ptr<Schema> output_schema)
+      : output_table(output_table), output_schema(std::move(output_schema)) {}
 
   std::shared_ptr<Table>* output_table;
   std::shared_ptr<Schema> output_schema;
-  Future<> finish;
 };
 
 }  // namespace compute
