c4a5cfe7423585e6084622add6a08a3fdffee40a
Alessandro Molina <amol@turbogears.org>
2022-02-11 16:35:47 +0100

    Add options

--- cpp/src/arrow/compute/exec/options.cc
+++ cpp/src/arrow/compute/exec/options.cc
@@ -17,6 +17,11 @@
 
 #include "arrow/compute/exec/options.h"
 #include "arrow/util/logging.h"
+#include "arrow/compute/exec/exec_plan.h"
+#include "arrow/compute/exec/options.h"
+#include "arrow/io/util_internal.h"
+#include "arrow/util/async_generator.h"
+#include "arrow/table.h"
 
 namespace arrow {
 namespace compute {
@@ -42,6 +47,30 @@ std::string ToString(JoinType t) {
   }
   ARROW_LOG(FATAL) << "Invalid variant of arrow::compute::JoinType";
   std::abort();
+}
+
+Result<std::shared_ptr<SourceNodeOptions>> SourceNodeOptions::FromTable(const Table& table) {
+  std::shared_ptr<RecordBatchReader> reader = std::make_shared<TableBatchReader>(table);
+
+  // Map the RecordBatchReader to a SourceNode
+  ARROW_ASSIGN_OR_RAISE(
+    auto batch_gen,
+    MakeReaderGenerator(std::move(reader), arrow::io::internal::GetIOThreadPool()));
+
+  return std::shared_ptr<SourceNodeOptions>(new SourceNodeOptions(table.schema(), batch_gen));
+}
+
+
+std::pair<std::shared_ptr<SinkNodeOptions>, std::shared_ptr<RecordBatchReader>> SinkNodeOptions::MakeForRecordBatchReader(std::shared_ptr<Schema> schema) {
+  // finally, pipe the project node into a sink node
+  AsyncGenerator<util::optional<compute::ExecBatch>> sink_gen;
+  auto node_options = std::shared_ptr<SinkNodeOptions>(new compute::SinkNodeOptions(&sink_gen));
+
+  // translate sink_gen (async) to sink_reader (sync)
+  std::shared_ptr<RecordBatchReader> sink_reader = compute::MakeGeneratorReader(
+      schema, std::move(sink_gen), default_memory_pool());
+
+  return std::make_pair(node_options, sink_reader);
 }
 
 }  // namespace compute
