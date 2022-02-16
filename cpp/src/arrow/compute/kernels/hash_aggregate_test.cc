45b39bf5064b44401f4deb1fcceca717ecfad22c
Dhruv Vats <dhruv25vats@gmail.com>
2022-02-17 01:55:34 +0530

    Modify matcher definition

--- cpp/src/arrow/compute/kernels/hash_aggregate_test.cc
+++ cpp/src/arrow/compute/kernels/hash_aggregate_test.cc
@@ -2722,9 +2722,10 @@ TEST(GroupBy, OneBinaryTypes) {
 TEST(GroupBy, OneScalar) {
   BatchesWithSchema input;
   input.batches = {
-      ExecBatchFromJSON({int32(), int64()}, R"([[-1, 1], [-1, 1], [7, 1], [23, 1]])"),
-      ExecBatchFromJSON({int32(), int64()},
-                        R"([[-9, 1], [null, 1], [null, 2], [null, 2]])"),
+      ExecBatchFromJSON({ValueDescr::Scalar(int32()), int64()},
+                        R"([[-1, 1], [-1, 1], [-1, 1], [-1, 1]])"),
+      ExecBatchFromJSON({ValueDescr::Scalar(int32()), int64()},
+                        R"([[null, 1], [null, 1], [null, 2], [null, 3]])"),
       ExecBatchFromJSON({int32(), int64()}, R"([[22, 1], [3, 2], [4, 3]])"),
   };
   input.schema = schema({field("argument", int32()), field("key", int64())});
@@ -2742,8 +2743,7 @@ TEST(GroupBy, OneScalar) {
                       struct_arr->field(struct_arr->num_fields() - 1));
 
     const auto& col = struct_arr->field(0);
-    EXPECT_THAT(col->GetScalar(0),
-                ResultWith(AnyOfJSON(int32(), R"([7, -1, 23, 22, -9])")));
+    EXPECT_THAT(col->GetScalar(0), ResultWith(AnyOfJSON(int32(), R"([-1])")));
     EXPECT_THAT(col->GetScalar(1), ResultWith(AnyOfJSON(int32(), R"([3])")));
     EXPECT_THAT(col->GetScalar(2), ResultWith(AnyOfJSON(int32(), R"([4])")));
   }
