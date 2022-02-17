5411458f4ae57640137ce1cbcf080d92ba38b8cd
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2022-02-17 14:29:38 +0000

    without casting

--- r/R/dplyr-funcs-datetime.R
+++ r/R/dplyr-funcs-datetime.R
@@ -170,20 +170,20 @@ register_bindings_datetime <- function() {
 }
 
 binding_format_datetime <- function(x, format = "", tz = "", usetz = FALSE) {
-
   if (usetz) {
     format <- paste(format, "%Z")
   }
 
-  if (call_binding("is.POSIXct", x)) {
-    if (tz == "" && x$type()$timezone() != "") {
-      tz <- x$type()$timezone()
-    } else if (tz == "") {
-      tz <- Sys.timezone()
-    }
-    ts <- Expression$create("cast", x, options = list(to_type = timestamp(x$type()$unit(), tz)))
-  } else {
-    ts <- x
-  }
+  # if (call_binding("is.POSIXct", x)) {
+  #   if (tz == "" && x$type()$timezone() != "") {
+  #     tz <- x$type()$timezone()
+  #   } else if (tz == "") {
+  #     tz <- Sys.timezone()
+  #   }
+  #   ts <- Expression$create("cast", x, options = list(to_type = timestamp(x$type()$unit(), tz)))
+  # } else {
+  #   ts <- x
+  # }
+  ts <- x
   Expression$create("strftime", ts, options = list(format = format, locale = Sys.getlocale("LC_TIME")))
 }
