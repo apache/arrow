be1992cf2d147d8f584a0c940e6c6d656bf178ce
Dewey Dunnington <dewey@fishandwhistle.net>
2022-02-18 18:16:32 -0400

    clang-format

--- r/src/extension.cpp
+++ r/src/extension.cpp
@@ -19,41 +19,40 @@
 
 #if defined(ARROW_R_WITH_ARROW)
 
-#include <arrow/type.h>
-#include <arrow/extension_type.h>
 #include <arrow/array.h>
+#include <arrow/extension_type.h>
+#include <arrow/type.h>
 
-
-class RExtensionType: public arrow::ExtensionType {
-public:
+class RExtensionType : public arrow::ExtensionType {
+ public:
   RExtensionType(const std::shared_ptr<arrow::DataType> storage_type,
                  std::string extension_name, std::string extension_metadata,
                  cpp11::environment r6_type_generator,
                  cpp11::environment r6_array_generator)
-    : arrow::ExtensionType(storage_type),
-      extension_name_(extension_name),
-      extension_metadata_(extension_metadata),
-      r6_type_generator_(r6_type_generator),
-      r6_array_generator_(r6_array_generator) {}
+      : arrow::ExtensionType(storage_type),
+        extension_name_(extension_name),
+        extension_metadata_(extension_metadata),
+        r6_type_generator_(r6_type_generator),
+        r6_array_generator_(r6_array_generator) {}
 
   std::string extension_name() const { return extension_name_; }
-  bool ExtensionEquals(const arrow::ExtensionType &other) const;
+  bool ExtensionEquals(const arrow::ExtensionType& other) const;
   std::shared_ptr<arrow::Array> MakeArray(std::shared_ptr<arrow::ArrayData> data) const;
   arrow::Result<std::shared_ptr<arrow::DataType>> Deserialize(
       std::shared_ptr<arrow::DataType> storage_type,
-      const std::string &serialized_data) const;
+      const std::string& serialized_data) const;
   std::string Serialize() const { return extension_metadata_; }
 
   std::shared_ptr<RExtensionType> Clone() const;
 
-private:
+ private:
   std::string extension_name_;
   std::string extension_metadata_;
   cpp11::environment r6_type_generator_;
   cpp11::environment r6_array_generator_;
 };
 
-bool RExtensionType::ExtensionEquals(const arrow::ExtensionType &other) const {
+bool RExtensionType::ExtensionEquals(const arrow::ExtensionType& other) const {
   if (other.extension_name() != extension_name()) {
     return false;
   }
@@ -65,7 +64,8 @@ bool RExtensionType::ExtensionEquals(const arrow::ExtensionType &other) const {
   return true;
 }
 
-std::shared_ptr<arrow::Array> RExtensionType::MakeArray(std::shared_ptr<arrow::ArrayData> data) const {
+std::shared_ptr<arrow::Array> RExtensionType::MakeArray(
+    std::shared_ptr<arrow::ArrayData> data) const {
   std::shared_ptr<arrow::ArrayData> new_data = data->Copy();
   new_data->type = Clone();
   return std::make_shared<arrow::ExtensionArray>(new_data);
@@ -73,60 +73,47 @@ std::shared_ptr<arrow::Array> RExtensionType::MakeArray(std::shared_ptr<arrow::A
 
 arrow::Result<std::shared_ptr<arrow::DataType>> RExtensionType::Deserialize(
     std::shared_ptr<arrow::DataType> storage_type,
-    const std::string &serialized_data) const {
+    const std::string& serialized_data) const {
   try {
     cpp11::function make_extension_type(cpp11::package("arrow")["MakeExtensionType"]);
     cpp11::sexp storage_type_r6 = cpp11::to_r6<arrow::DataType>(storage_type);
     cpp11::writable::raws serialized_data_raw(serialized_data);
 
-    cpp11::sexp result = make_extension_type(
-      storage_type_r6,
-      extension_name(),
-      serialized_data_raw,
-      r6_type_generator_,
-      r6_array_generator_
-    );
+    cpp11::sexp result =
+        make_extension_type(storage_type_r6, extension_name(), serialized_data_raw,
+                            r6_type_generator_, r6_array_generator_);
 
     auto ptr = arrow::r::r6_to_pointer<std::shared_ptr<arrow::DataType>*>(result);
     return *ptr;
-  } catch(std::exception& e) {
+  } catch (std::exception& e) {
     return arrow::Status::UnknownError(e.what());
   }
 }
 
 std::shared_ptr<RExtensionType> RExtensionType::Clone() const {
-  return std::make_shared<RExtensionType>(
-    storage_type(),
-    extension_name_,
-    extension_metadata_,
-    r6_type_generator_,
-    r6_array_generator_
-  );
+  return std::make_shared<RExtensionType>(storage_type(), extension_name_,
+                                          extension_metadata_, r6_type_generator_,
+                                          r6_array_generator_);
 }
 
 // [[arrow::export]]
 cpp11::sexp ExtensionType__initialize(
-    const std::shared_ptr<arrow::DataType>& storage_type,
-    std::string extension_name,
-    cpp11::raws extension_metadata,
-    cpp11::environment r6_type_generator,
-    cpp11::environment r6_array_generator
-) {
+    const std::shared_ptr<arrow::DataType>& storage_type, std::string extension_name,
+    cpp11::raws extension_metadata, cpp11::environment r6_type_generator,
+    cpp11::environment r6_array_generator) {
   cpp11::function constructor(r6_type_generator["new"]);
   std::string metadata_string(extension_metadata.begin(), extension_metadata.end());
   auto shared_ptr_ptr = new std::shared_ptr<RExtensionType>(
-    new RExtensionType(
-        storage_type,
-        extension_name,
-        metadata_string,
-        r6_type_generator,
-        r6_array_generator));
-  auto external_ptr = cpp11::external_pointer<std::shared_ptr<RExtensionType>>(shared_ptr_ptr);
+      new RExtensionType(storage_type, extension_name, metadata_string, r6_type_generator,
+                         r6_array_generator));
+  auto external_ptr =
+      cpp11::external_pointer<std::shared_ptr<RExtensionType>>(shared_ptr_ptr);
   return constructor(external_ptr);
 }
 
 // [[arrow::export]]
-std::string ExtensionType__extension_name(const std::shared_ptr<arrow::ExtensionType>& type) {
+std::string ExtensionType__extension_name(
+    const std::shared_ptr<arrow::ExtensionType>& type) {
   return type->extension_name();
 }
 
@@ -138,18 +125,21 @@ cpp11::raws ExtensionType__Serialize(const std::shared_ptr<arrow::ExtensionType>
 }
 
 // [[arrow::export]]
-std::shared_ptr<arrow::DataType> ExtensionType__storage_type(const std::shared_ptr<arrow::ExtensionType>& type) {
+std::shared_ptr<arrow::DataType> ExtensionType__storage_type(
+    const std::shared_ptr<arrow::ExtensionType>& type) {
   return type->storage_type();
 }
 
 // [[arrow::export]]
-std::shared_ptr<arrow::Array> ExtensionType__MakeArray(const std::shared_ptr<arrow::ExtensionType>& type,
-                                                       const std::shared_ptr<arrow::ArrayData>& data) {
+std::shared_ptr<arrow::Array> ExtensionType__MakeArray(
+    const std::shared_ptr<arrow::ExtensionType>& type,
+    const std::shared_ptr<arrow::ArrayData>& data) {
   return type->MakeArray(data);
 }
 
 // [[arrow::export]]
-std::shared_ptr<arrow::Array> ExtensionArray__storage(const std::shared_ptr<arrow::ExtensionArray>& array) {
+std::shared_ptr<arrow::Array> ExtensionArray__storage(
+    const std::shared_ptr<arrow::ExtensionArray>& array) {
   return array->storage();
 }
 
