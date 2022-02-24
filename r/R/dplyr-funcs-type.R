1d965721aa69ec27ffed3c487dab68b2c1faa575
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2022-02-24 12:46:40 +0000

    typo

--- r/R/dplyr-funcs-type.R
+++ r/R/dplyr-funcs-type.R
@@ -79,7 +79,7 @@ register_bindings_type_cast <- function() {
   register_binding("as.Date", function(x) {
     # base::as.Date() first converts to UTC and then extracts the date, which is
     # why we need to go through timestamp() first - see unit tests for the real
-    # life impact of the difference between lubridate::date() and base::as.Date
+    # life impact of the difference between lubridate::date() and base::as.Date()
     y <- build_expr("cast", x, options = list(to_type = timestamp()))
     build_expr("cast", y, options = list(to_type = date32()))
   })
