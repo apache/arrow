5671a1372a31b54634cf770e2692c3d1d23a4555
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-04 08:09:12 -0400

    remove event loop indirection

--- r/src/safe-call-into-r.h
+++ r/src/safe-call-into-r.h
@@ -5,94 +5,58 @@
 #include "./arrow_types.h"
 
 #include <deque>
-#include <unordered_set>
+#include <functional>
 #include <mutex>
 #include <thread>
-#include <functional>
+#include <unordered_set>
 
 class MainRThreadTasks {
-public:
-    class Task {
-    public:
-        virtual ~Task() {}
-        virtual void run() {}
-    };
+ public:
+  class Task {
+   public:
+    virtual ~Task() {}
+    virtual void run() {}
+  };
 
-    class EventLoop {
-    public:
-        EventLoop();
-        ~EventLoop();
-        void start_looping();
-        void stop_looping();
-    private:
-        std::thread::id thread_;
-        bool keep_looping_;
-    };
+  class EventLoop {
+   public:
+    EventLoop();
+    ~EventLoop();
+    std::thread::id thread() { return thread_; }
 
-    void EvaluatePending() {
-        lock_.lock();
-        while (!tasks_.empty()) {
-            Task* task = tasks_.back();
-            tasks_.pop_back();
-            task->run();
-        }
-        lock_.unlock();
-    }
+   private:
+    std::thread::id thread_;
+  };
 
-    void Add(Task* task) {
-        lock_.lock();
-        tasks_.push_front(task);
-        lock_.unlock();
-    }
+  void Register(EventLoop* loop) { loop_ = loop; }
 
-    bool is_finished(Task* task) {
-        lock_.lock();
-        bool result = results_.find(task) != results_.end();
-        lock_.unlock();
-        return result;
-    }
+  void Unregister() { loop_ = nullptr; }
 
-    void Register(EventLoop* loop) {
-        if (loop_ == nullptr) {
-            throw std::runtime_error("Event loop already registered");
-        }
-        loop_ = loop;
-    }
+  EventLoop* Loop() { return loop_; }
 
-    void Unregister() {
-        loop_ = nullptr;
-    }
-
-private:
-    std::deque<Task*> tasks_;
-    std::unordered_set<Task*> results_;
-    std::mutex lock_;
-    EventLoop* loop_;
+ private:
+  EventLoop* loop_;
 };
-
 
 void SafeCallIntoRBase(MainRThreadTasks::Task* task);
 
 template <typename T>
 T SafeCallIntoR(std::function<cpp11::sexp(void)> fun) {
-    class TypedTask: public MainRThreadTasks::Task {
-    public:
-        TypedTask(std::function<cpp11::sexp(void)> fun): fun_(fun) {};
+  class TypedTask : public MainRThreadTasks::Task {
+   public:
+    TypedTask(std::function<cpp11::sexp(void)> fun) : fun_(fun){};
 
-        void run() {
-            result = cpp11::as_cpp<T>(fun_());
-        }
+    void run() { result = cpp11::as_cpp<T>(fun_()); }
 
-        T result;
+    T result;
 
-    private:
-        std::function<cpp11::sexp(void)> fun_;
-    };
+   private:
+    std::function<cpp11::sexp(void)> fun_;
+  };
 
-    TypedTask task(fun);
-    // SafeCallIntoRBase(&task);
-    task.run();
-    return task.result;
+  TypedTask task(fun);
+  SafeCallIntoRBase(&task);
+  return task.result;
 }
 
 #endif
