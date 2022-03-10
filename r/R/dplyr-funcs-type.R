a797f9bd240428f4335596f166cce9a8a90e79dd
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2022-03-10 11:42:01 +0000

    typo + simplified logic for integer vs double

--- r/R/dplyr-funcs-type.R
+++ r/R/dplyr-funcs-type.R
@@ -141,18 +141,18 @@ register_bindings_type_cast <- function() {
       return(diff_x_y)
     }
 
-    # numeric -> duration not supported in Arrow yet so we use time23() as
+    # numeric -> duration not supported in Arrow yet so we use time32() as
     # intermediate step
     # TODO revisit once https://issues.apache.org/jira/browse/ARROW-15862 done
-    if (call_binding("is.numeric", x)) {
-      if (call_binding("is.integer", x)) {
-        x <- build_expr("cast", x, options = cast_options(to_type = time32(unit = "s")))
-        y <- build_expr("cast", 0L, options = cast_options(to_type = time32(unit = "s")))
-        diff_x_y <- call_binding("difftime", x, y, units = "secs", tz = tz)
-        return(diff_x_y)
-      } else {
+    if (call_binding("is.integer", x)) {
+      x <- build_expr("cast", x, options = cast_options(to_type = time32(unit = "s")))
+      y <- build_expr("cast", 0L, options = cast_options(to_type = time32(unit = "s")))
+      diff_x_y <- call_binding("difftime", x, y, units = "secs", tz = tz)
+      return(diff_x_y)
+    }
+
+    if (call_binding("is.double")) {
       abort("`as.difftime()` with double/float inputs not supported in Arrow ")
-      }
     }
 
     build_expr("cast", x, options = cast_options(to_type = duration(unit = "s")))
