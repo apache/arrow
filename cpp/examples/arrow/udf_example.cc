f1f96871eb14d716fd1d8748370d6196c0883583
Vibhatha Abeykoon <vibhatha@gmail.com>
2022-03-14 12:15:46 +0530

    cleaning up udf C++ example

--- cpp/examples/arrow/udf_example.cc
+++ cpp/examples/arrow/udf_example.cc
@@ -33,10 +33,8 @@
 #include <iostream>
 #include <memory>
 
-#include <arrow/python/api.h>
-#include <arrow/python/udf.h>
-
-// Demonstrate registering an Arrow compute function outside of the Arrow source tree
+// Demonstrate registering an user-defined Arrow compute function outside of the Arrow
+// source tree
 
 namespace cp = ::arrow::compute;
 
@@ -48,21 +46,6 @@ namespace cp = ::arrow::compute;
       abort();                                     \
     }                                              \
   } while (0);
-
-struct BatchesWithSchema {
-  std::vector<cp::ExecBatch> batches;
-  std::shared_ptr<arrow::Schema> schema;
-  // // This method uses internal arrow utilities to
-  // // convert a vector of record batches to an AsyncGenerator of optional batches
-  arrow::AsyncGenerator<arrow::util::optional<cp::ExecBatch>> gen() const {
-    auto opt_batches = ::arrow::internal::MapVector(
-        [](cp::ExecBatch batch) { return arrow::util::make_optional(std::move(batch)); },
-        batches);
-    arrow::AsyncGenerator<arrow::util::optional<cp::ExecBatch>> gen;
-    gen = arrow::MakeVectorGenerator(std::move(opt_batches));
-    return gen;
-  }
-};
 
 template <typename TYPE,
           typename = typename std::enable_if<arrow::is_number_type<TYPE>::value |
@@ -88,133 +71,57 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> GetSampleRecordBatch(
   return record_batch->FromStructArray(struct_result);
 }
 
-arrow::Result<cp::ExecBatch> GetExecBatchFromVectors(
-    const arrow::FieldVector& field_vector, const arrow::ArrayVector& array_vector) {
-  std::shared_ptr<arrow::RecordBatch> record_batch;
-  ARROW_ASSIGN_OR_RAISE(auto res_batch, GetSampleRecordBatch(array_vector, field_vector));
-  cp::ExecBatch batch{*res_batch};
-  return batch;
-}
-
-arrow::Result<BatchesWithSchema> MakeBasicBatches() {
-  BatchesWithSchema out;
-  auto field_vector = {arrow::field("a", arrow::int64()),
-                       arrow::field("b", arrow::boolean())};
-  ARROW_ASSIGN_OR_RAISE(auto b1_int, GetArrayDataSample<arrow::Int64Type>({0, 4}));
-  ARROW_ASSIGN_OR_RAISE(auto b2_int, GetArrayDataSample<arrow::Int64Type>({5, 6, 7}));
-  ARROW_ASSIGN_OR_RAISE(auto b3_int, GetArrayDataSample<arrow::Int64Type>({8, 9, 10}));
-
-  ARROW_ASSIGN_OR_RAISE(auto b1_bool,
-                        GetArrayDataSample<arrow::BooleanType>({false, true}));
-  ARROW_ASSIGN_OR_RAISE(auto b2_bool,
-                        GetArrayDataSample<arrow::BooleanType>({true, false, true}));
-  ARROW_ASSIGN_OR_RAISE(auto b3_bool,
-                        GetArrayDataSample<arrow::BooleanType>({false, true, false}));
-
-  ARROW_ASSIGN_OR_RAISE(auto b1,
-                        GetExecBatchFromVectors(field_vector, {b1_int, b1_bool}));
-  ARROW_ASSIGN_OR_RAISE(auto b2,
-                        GetExecBatchFromVectors(field_vector, {b2_int, b2_bool}));
-  ARROW_ASSIGN_OR_RAISE(auto b3,
-                        GetExecBatchFromVectors(field_vector, {b3_int, b3_bool}));
-
-  out.batches = {b1, b2, b3};
-  out.schema = arrow::schema(field_vector);
-  return out;
-}
-
 arrow::Result<std::shared_ptr<arrow::Table>> GetTable() {
   std::shared_ptr<arrow::Table> table;
 
-  auto field_vector = {arrow::field("a", arrow::int64()),
-                       arrow::field("b", arrow::boolean())};
+  auto field_vector = {
+      arrow::field("a", arrow::int64()), arrow::field("x", arrow::int64()),
+      arrow::field("y", arrow::int64()), arrow::field("z", arrow::int64()),
+      arrow::field("b", arrow::boolean())};
+
   ARROW_ASSIGN_OR_RAISE(auto int_array,
-                        GetArrayDataSample<arrow::Int64Type>({0, 4, 10, 20, 30}));
+                        GetArrayDataSample<arrow::Int64Type>({1, 2, 3, 4, 5, 6}));
+  ARROW_ASSIGN_OR_RAISE(auto x,
+                        GetArrayDataSample<arrow::Int64Type>({21, 22, 23, 24, 25, 26}));
+  ARROW_ASSIGN_OR_RAISE(auto y,
+                        GetArrayDataSample<arrow::Int64Type>({31, 32, 33, 34, 35, 36}));
+  ARROW_ASSIGN_OR_RAISE(auto z,
+                        GetArrayDataSample<arrow::Int64Type>({41, 42, 43, 44, 45, 46}));
   ARROW_ASSIGN_OR_RAISE(auto bool_array, GetArrayDataSample<arrow::BooleanType>(
-                                             {false, true, false, true, true}));
+                                             {false, true, false, true, true, false}));
 
   auto schema = arrow::schema(field_vector);
-  auto data_vector = {int_array, bool_array};
+  auto data_vector = {int_array, x, y, z, bool_array};
 
-  table = arrow::Table::Make(schema, data_vector, 5);
+  table = arrow::Table::Make(schema, data_vector, 6);
 
   return table;
 }
 
-class ExampleFunctionOptionsType : public cp::FunctionOptionsType {
-  const char* type_name() const override { return "ExampleFunctionOptionsType"; }
+class UDFOptionsType : public cp::FunctionOptionsType {
+  const char* type_name() const override { return "UDFOptionsType"; }
   std::string Stringify(const cp::FunctionOptions&) const override {
-    return "ExampleFunctionOptionsType";
+    return "UDFOptionsType";
   }
   bool Compare(const cp::FunctionOptions&, const cp::FunctionOptions&) const override {
     return true;
   }
   std::unique_ptr<cp::FunctionOptions> Copy(const cp::FunctionOptions&) const override;
-  // optional: support for serialization
-  // Result<std::shared_ptr<Buffer>> Serialize(const FunctionOptions&) const override;
-  // Result<std::unique_ptr<FunctionOptions>> Deserialize(const Buffer&) const override;
 };
 
-cp::FunctionOptionsType* GetExampleFunctionOptionsType() {
-  static ExampleFunctionOptionsType options_type;
+cp::FunctionOptionsType* GetUDFOptionsType() {
+  static UDFOptionsType options_type;
   return &options_type;
 }
 
-class ExampleFunctionOptions : public cp::FunctionOptions {
+class UDFOptions : public cp::FunctionOptions {
  public:
-  ExampleFunctionOptions() : cp::FunctionOptions(GetExampleFunctionOptionsType()) {}
+  UDFOptions() : cp::FunctionOptions(GetUDFOptionsType()) {}
 };
 
-std::unique_ptr<cp::FunctionOptions> ExampleFunctionOptionsType::Copy(
+std::unique_ptr<cp::FunctionOptions> UDFOptionsType::Copy(
     const cp::FunctionOptions&) const {
-  return std::unique_ptr<cp::FunctionOptions>(new ExampleFunctionOptions());
-}
-
-PyObject* SimpleFunction() {
-  PyObject* out = Py_BuildValue("s", "hello");
-  std::cout << "HELLO FROM PYTHON FUNCTION IN C++" << std::endl;
-  return std::move(out);
-}
-
-arrow::Status rb_test() {
-  auto datasource = MakeBasicBatches();
-  auto batches = datasource->batches;
-
-  ARROW_ASSIGN_OR_RAISE(auto rb, batches[0].ToRecordBatch(datasource->schema,
-                                                          arrow::default_memory_pool()));
-  ARROW_ASSIGN_OR_RAISE(auto result, cp::CallFunction("add", {rb, rb}));
-  return arrow::Status::OK();
-}
-
-// PyObject* objectsRepresentation = PyObject_Repr(yourObject);
-// const char* s = PyString_AsString(objectsRepresentation);
-
-arrow::Status ExampleFunctionImpl(cp::KernelContext* ctx, const cp::ExecBatch& batch,
-                                  arrow::Datum* out) {
-  std::cout << "calling udf :" << batch.length << std::endl;
-  Py_Initialize();
-  PyObject* res = SimpleFunction();
-  PyObject* objectsRepresentation = PyObject_Repr(res);
-  const char* s = PyUnicode_AsUTF8(objectsRepresentation);
-  std::cout << "Message :: " << s << std::endl;
-  Py_Finalize();
-  auto result = cp::CallFunction("add", {batch[0].array(), batch[0].array()});
-  *out->mutable_array() = *result.ValueOrDie().array();
-  return arrow::Status::OK();
-}
-// cp::KernelContext*, const cp::ExecBatch&, Datum*, PyObject* func
-arrow::Status ExamplePyFunctionImpl(cp::KernelContext* ctx, const cp::ExecBatch& batch,
-                                    arrow::Datum* out, PyObject* func) {
-  std::cout << "H" << std::endl;
-  auto result = cp::CallFunction("add", {batch[0].array(), batch[0].array()});
-  *out->mutable_array() = *result.ValueOrDie().array();
-  // PyObject* res = SimpleFunction();
-  // PyObject* objectsRepresentation = PyObject_Repr(res);
-  // const char* s = PyUnicode_AsUTF8(objectsRepresentation);
-  std::cout << "Message :: "
-            << "s" << std::endl;
-
-  return arrow::Status::OK();
+  return std::unique_ptr<cp::FunctionOptions>(new UDFOptions());
 }
 
 class ExampleNodeOptions : public cp::ExecNodeOptions {};
@@ -257,147 +164,34 @@ arrow::Result<cp::ExecNode*> ExampleExecNodeFactory(cp::ExecPlan* plan,
 }
 
 const cp::FunctionDoc func_doc{
-    "Example function to demonstrate registering an out-of-tree function",
-    "",
-    {"x", "y"},
-    "ExampleFunctionOptions"};
-
-const cp::FunctionDoc func_doc2{
-    "Example function to demonstrate registering an out-of-tree function",
-    "",
-    {"x"},
-    "ExampleFunctionOptions2"};
-
-PyObject* MultiplyFunction(PyObject* scalar) {
-  PyObject* constant = PyLong_FromLong(2);
-  PyObject* res = PyNumber_Multiply(constant, scalar);
-  return std::move(res);
-}
-
-class ScalarUDF {
- public:
-  ScalarUDF();
-  explicit ScalarUDF(cp::Arity arity, std::vector<cp::InputType> input_types,
-                     cp::OutputType output_type, PyObject* (*function)(PyObject*))
-      : arity_(std::move(arity)),
-        input_types_(std::move(input_types)),
-        output_type_(output_type),
-        function_(function) {}
-
-  arrow::Status Make(cp::KernelContext* ctx, const cp::ExecBatch& batch,
-                     arrow::Datum* out) {
-    Py_Initialize();
-    PyObject* args = PyTuple_Pack(1, PyLong_FromLong(2));
-    PyObject* myResult = function_(args);
-    int64_t result = PyLong_AsLong(myResult);
-    Py_Finalize();
-    std::cout << "Value : " << result << std::endl;
-    arrow::Result<arrow::Datum> maybe_result;
-    arrow::Int64Builder builder(arrow::default_memory_pool());
-    std::shared_ptr<arrow::Array> arr;
-    ABORT_ON_FAILURE(builder.Append(result));
-    ABORT_ON_FAILURE(builder.Finish(&arr));
-    maybe_result = cp::CallFunction("add", {batch[0].array(), arr});
-    *out->mutable_array() = *maybe_result.ValueOrDie().array();
-    return arrow::Status::OK();
-  }
-
- private:
-  cp::Arity arity_;
-  std::vector<cp::InputType> input_types_;
-  cp::OutputType output_type_;
-  PyObject* (*function_)(PyObject*);
-};
+    "User-defined-function usage to demonstrate registering an out-of-tree function",
+    "returns x + y + z",
+    {"x", "y", "z"},
+    "UDFOptions"};
 
 arrow::Status Execute() {
   const std::string name = "x+x";
-  auto func = std::make_shared<cp::ScalarFunction>(name, cp::Arity::Unary(), &func_doc2);
-  cp::ScalarKernel kernel({cp::InputType::Array(arrow::int64())}, arrow::int64(),
-                          ExampleFunctionImpl);
+  auto func = std::make_shared<cp::ScalarFunction>(name, cp::Arity::Ternary(), &func_doc);
 
-  kernel.mem_allocation = cp::MemAllocation::NO_PREALLOCATE;
-
-  ABORT_ON_FAILURE(func->AddKernel(std::move(kernel)));
-
-  auto registry = cp::GetFunctionRegistry();
-  ABORT_ON_FAILURE(registry->AddFunction(std::move(func)));
-
-  arrow::Int64Builder builder(arrow::default_memory_pool());
-  std::shared_ptr<arrow::Array> arr1, arr2;
-  ABORT_ON_FAILURE(builder.Append(42));
-  ABORT_ON_FAILURE(builder.Finish(&arr1));
-  ABORT_ON_FAILURE(builder.Append(58));
-  ABORT_ON_FAILURE(builder.Finish(&arr2));
-  auto options = std::make_shared<ExampleFunctionOptions>();
-  auto maybe_result = cp::CallFunction(name, {arr1}, options.get());
-  ABORT_ON_FAILURE(maybe_result.status());
-
-  std::cout << "Result 1: " << maybe_result->make_array()->ToString() << std::endl;
-
-  // Expression serialization will raise NotImplemented if an expression includes
-  // FunctionOptions for which serialization is not supported.
-  // auto expr = cp::call(name, {}, options);
-  // auto maybe_serialized = cp::Serialize(expr);
-  // std::cerr << maybe_serialized.status().ToString() << std::endl;
-
-  auto exec_registry = cp::default_exec_factory_registry();
-  ABORT_ON_FAILURE(
-      exec_registry->AddFactory("compute_register_example", ExampleExecNodeFactory));
-
-  auto maybe_plan = cp::ExecPlan::Make();
-  ABORT_ON_FAILURE(maybe_plan.status());
-  auto plan = maybe_plan.ValueOrDie();
-  cp::ExecContext exec_context(arrow::default_memory_pool(),
-                               ::arrow::internal::GetCpuThreadPool());
-  arrow::AsyncGenerator<arrow::util::optional<cp::ExecBatch>> source_gen, sink_gen;
-  ARROW_ASSIGN_OR_RAISE(auto basic_data, MakeBasicBatches());
-
-  cp::Expression a_times_10 = cp::call("multiply", {cp::field_ref("a"), cp::literal(10)});
-  cp::Expression custom_exp = cp::call(name, {cp::field_ref("a")}, options);
-
-  auto source_node_options = cp::SourceNodeOptions{basic_data.schema, basic_data.gen()};
-  auto project_node_options = cp::ProjectNodeOptions{{
-      cp::field_ref("a"),
-      custom_exp,
-      cp::field_ref("b"),
-  }};
-  auto output_schema = arrow::schema({arrow::field("a", arrow::int64()),
-                                      arrow::field("a + a", arrow::int64()),
-                                      arrow::field("b", arrow::boolean())});
-  std::shared_ptr<arrow::Table> out;
-  ABORT_ON_FAILURE(cp::Declaration::Sequence(
-                       {
-                           {"source", source_node_options},
-                           {"project", project_node_options},
-                           {"table_sink", cp::TableSinkNodeOptions{&out, output_schema}},
-                       })
-                       .AddToPlan(plan.get())
-                       .status());
-
-  ARROW_RETURN_NOT_OK(plan->StartProducing());
-
-  std::cout << "Output Table Data : " << std::endl;
-  std::cout << out->ToString() << std::endl;
-
-  auto future = plan->finished();
-
-  return future.status();
-}
-
-arrow::Status ExecuteVar() {
-  const std::string name = "x+x";
-  auto func = std::make_shared<cp::ScalarFunction>(name, cp::Arity::Unary(), &func_doc2);
   auto exec_func = [](cp::KernelContext* ctx, const cp::ExecBatch& batch,
                       arrow::Datum* out) -> arrow::Status {
-    std::cout << "Batch as Table " << std::endl;
-    *out->mutable_array() = *batch[0].array();
+    auto in_res = cp::CallFunction("add", {batch[0].array(), batch[1].array()});
+    auto in_arr = in_res.ValueOrDie().make_array();
+    auto final_res = cp::CallFunction("add", {in_arr, batch[2].array()});
+    auto final_arr = final_res.ValueOrDie().array();
+    auto datum = new arrow::Datum(final_arr);
+    *out = *datum;
     return arrow::Status::OK();
   };
-  auto options = std::make_shared<ExampleFunctionOptions>();
-  cp::ScalarKernel kernel({cp::InputType::Array(arrow::int64())}, arrow::int64(),
-                          exec_func);
+
+  auto options = std::make_shared<UDFOptions>();
+  cp::ScalarKernel kernel(
+      {cp::InputType::Array(arrow::int64()), cp::InputType::Array(arrow::int64()),
+       cp::InputType::Array(arrow::int64())},
+      arrow::int64(), exec_func);
 
   kernel.mem_allocation = cp::MemAllocation::NO_PREALLOCATE;
+  kernel.null_handling = cp::NullHandling::COMPUTED_NO_PREALLOCATE;
 
   ABORT_ON_FAILURE(func->AddKernel(std::move(kernel)));
 
@@ -406,7 +200,7 @@ arrow::Status ExecuteVar() {
 
   auto exec_registry = cp::default_exec_factory_registry();
   ABORT_ON_FAILURE(
-      exec_registry->AddFactory("compute_register_example", ExampleExecNodeFactory));
+      exec_registry->AddFactory("udf_register_example", ExampleExecNodeFactory));
 
   auto maybe_plan = cp::ExecPlan::Make();
   ABORT_ON_FAILURE(maybe_plan.status());
@@ -414,12 +208,10 @@ arrow::Status ExecuteVar() {
   cp::ExecContext exec_context(arrow::default_memory_pool(),
                                ::arrow::internal::GetCpuThreadPool());
   arrow::AsyncGenerator<arrow::util::optional<cp::ExecBatch>> source_gen, sink_gen;
-  ARROW_ASSIGN_OR_RAISE(auto basic_data, MakeBasicBatches());
 
-  cp::Expression a_times_10 = cp::call("multiply", {cp::field_ref("a"), cp::literal(10)});
-  cp::Expression custom_exp = cp::call(name, {cp::field_ref("a")}, options);
+  cp::Expression custom_exp = cp::call(
+      name, {cp::field_ref("x"), cp::field_ref("y"), cp::field_ref("z")}, options);
 
-  auto source_node_options = cp::SourceNodeOptions{basic_data.schema, basic_data.gen()};
   ARROW_ASSIGN_OR_RAISE(auto table, GetTable());
   auto table_source_node_options = cp::TableSourceNodeOptions{table, 2};
   auto project_node_options = cp::ProjectNodeOptions{{
@@ -428,21 +220,30 @@ arrow::Status ExecuteVar() {
       cp::field_ref("b"),
   }};
   auto output_schema = arrow::schema({arrow::field("a", arrow::int64()),
-                                      arrow::field("a + a", arrow::int64()),
+                                      arrow::field("x + y + z", arrow::int64()),
                                       arrow::field("b", arrow::boolean())});
   std::shared_ptr<arrow::Table> out;
-  ABORT_ON_FAILURE(cp::Declaration::Sequence(
-                       {
-                           {"table_source", table_source_node_options},
-                           {"project", project_node_options},
-                           {"table_sink", cp::TableSinkNodeOptions{&out, output_schema}},
-                       })
-                       .AddToPlan(plan.get())
-                       .status());
+  auto table_sink_node_options = cp::TableSinkNodeOptions{&out, output_schema};
+  ABORT_ON_FAILURE(
+      cp::Declaration::Sequence({
+                                    {"table_source", table_source_node_options},
+                                    {"project", project_node_options},
+                                    {"table_sink", table_sink_node_options},
+                                })
+          .AddToPlan(plan.get())
+          .status());
 
   ARROW_RETURN_NOT_OK(plan->StartProducing());
+  constexpr int print_len = 25;
+  std::cout << std::string(print_len, '#') << std::endl;
+  std::cout << "Input Table Data : " << std::endl;
+  std::cout << std::string(print_len, '#') << std::endl;
 
+  std::cout << table->ToString() << std::endl;
+
+  std::cout << std::string(print_len, '#') << std::endl;
   std::cout << "Output Table Data : " << std::endl;
+  std::cout << std::string(print_len, '#') << std::endl;
   std::cout << out->ToString() << std::endl;
 
   auto future = plan->finished();
@@ -451,7 +252,7 @@ arrow::Status ExecuteVar() {
 }
 
 int main(int argc, char** argv) {
-  auto status = ExecuteVar();
+  auto status = Execute();
   if (!status.ok()) {
     std::cerr << "Error occurred : " << status.message() << std::endl;
     return EXIT_FAILURE;
