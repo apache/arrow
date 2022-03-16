1121200b17deadf61e58398cd2c8c067aa62167b
Antoine Pitrou <antoine@python.org>
2022-03-16 15:31:25 +0100

    ARROW-15949: [Python] Do not require Parquet encryption when Parquet is disabled
    Fix the Java C Data Interface Integration build failure.

--- python/setup.py
+++ python/setup.py
@@ -193,6 +193,9 @@ class build_ext(_build_ext):
         self.bundle_plasma_executable = strtobool(
             os.environ.get('PYARROW_BUNDLE_PLASMA_EXECUTABLE', '1'))
 
+        self.with_parquet_encryption = (self.with_parquet_encryption and
+                                        self.with_parquet)
+
     CYTHON_MODULE_NAMES = [
         'lib',
         '_fs',
