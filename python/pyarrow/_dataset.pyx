4e00f5e937162b16535e17bd41d119dbfa3550d1
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2022-03-28 14:22:55 +0530

    docs: docs update for augmented fields

--- python/pyarrow/_dataset.pyx
+++ python/pyarrow/_dataset.pyx
@@ -2043,17 +2043,23 @@ cdef class Scanner(_Weakrefable):
         Dataset to scan.
     columns : list of str or dict, default None
         The columns to project. This can be a list of column names to
-        include (order and duplicates will be preserved) which may contain the
-        augmented fields such as `batch_index`, `fragment_index`, 
-        `last_in_fragment` and `filename`, or a dictionary
-        with {new_column_name: expression} values for more advanced
+        include (order and duplicates will be preserved), or a dictionary
+        with {{new_column_name: expression}} values for more advanced
         projections.
+        
+        The list of columns or expressions may use the special fields
+        like  `__batch_index` (the index of the batch within the fragment), 
+        `__fragment_index` (the index of the fragment within the dataset), 
+        `__last_in_fragment` (whether the batch is last in fragment) and
+        `__filename` (the name of the source file or a description of the 
+        source fragment).
+
         The columns will be passed down to Datasets and corresponding data
         fragments to avoid loading, copying, and deserializing columns
         that will not be required further down the compute chain.
-        By default all of the available columns are projected. Raises
-        an exception if any of the referenced column names does not exist
-        in the dataset's Schema.
+        By default all of the available columns are projected. 
+        Raises an exception if any of the referenced column names 
+        does not exist in the dataset's Schema.
     filter : Expression, default None
         Scan will return only the rows matching the filter.
         If possible the predicate will be pushed down to exploit the
