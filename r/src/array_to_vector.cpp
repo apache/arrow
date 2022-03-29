1d71a0314a711fa728a0c873847c48499058c36e
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-29 09:21:07 -0300

    move extension type logic to the extension type converter

--- r/src/array_to_vector.cpp
+++ r/src/array_to_vector.cpp
@@ -66,14 +66,7 @@ class Converter {
 
   // converter is passed as self to outlive the scope of Converter::Convert()
   SEXP ScheduleConvertTasks(RTasks& tasks, std::shared_ptr<Converter> self) {
-    // ExtensionType has to go through the ExensionType R6 instance
-    auto extension_type =
-        dynamic_cast<const RExtensionType*>(chunked_array_->type().get());
-    if (extension_type != nullptr) {
-      return extension_type->Convert(chunked_array_);
-    }
-
-    // try altrep before the Converter api:
+    // try altrep first
     SEXP alt = altrep::MakeAltrepVector(chunked_array_);
     if (!Rf_isNull(alt)) {
       return alt;
@@ -1162,22 +1155,32 @@ class Converter_Null : public Converter {
   }
 };
 
+// Unlike other types, conversion of ExtensionType (chunked) arrays occurs at
+// R level via the ExtensionType (or subclass) R6 instance. We do this via Allocate,
+// since it is called once per ChunkedArray.
 class Converter_Extension : public Converter {
  public:
   explicit Converter_Extension(const std::shared_ptr<ChunkedArray>& chunked_array)
       : Converter(chunked_array) {}
 
   SEXP Allocate(R_xlen_t n) const {
-    Rf_error("Can't use Converter() API for ExtensionType directly");
+    auto extension_type =
+        dynamic_cast<const RExtensionType*>(chunked_array_->type().get());
+    if (extension_type == nullptr) {
+      Rf_error("Converter_Extension can't be used with a non-R extension type");
+    }
+
+    return extension_type->Convert(chunked_array_);
   }
 
+  // At this point we have already done the conversion
   Status Ingest_all_nulls(SEXP data, R_xlen_t start, R_xlen_t n) const {
-    return Status::NotImplemented("Converter API for ExtensionType");
+    return Status::OK();
   }
 
   Status Ingest_some_nulls(SEXP data, const std::shared_ptr<arrow::Array>& array,
                            R_xlen_t start, R_xlen_t n, size_t chunk_index) const {
-    return Status::NotImplemented("Converter API for ExtensionType");
+    return Status::OK();
   }
 };
 
