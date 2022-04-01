ac3be08e4923be914b1b89c2062d0886d23141b7
Dhruv Vats <dhruv25vats@gmail.com>
2022-04-01 12:21:33 -0400

    ARROW-15643: [C++] Allow selecting subset of fields of a StructArray via cast
    Provide the ability to select a subset of the fields of a StructArray array by casting it to the required fields, provided the fields exist in the StructArray.
    
    Closes #12724 from dhruv9vats/ARROW-15643-subset-struct-cast
    
    Authored-by: Dhruv Vats <dhruv25vats@gmail.com>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- cpp/src/arrow/dataset/scanner_test.cc
+++ cpp/src/arrow/dataset/scanner_test.cc
@@ -1669,7 +1669,9 @@ TEST(ScanNode, MaterializationOfNestedVirtualColumn) {
 
   // TODO(ARROW-1888): allow scanner to "patch up" structs with casts
   EXPECT_FINISHES_AND_RAISES_WITH_MESSAGE_THAT(
-      TypeError, ::testing::HasSubstr("struct field sizes do not match"), plan.Run());
+      TypeError,
+      ::testing::HasSubstr("struct fields don't match or are in the wrong order"),
+      plan.Run());
 }
 
 TEST(ScanNode, MinimalEndToEnd) {
