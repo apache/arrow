07eb8fbabc6573dbe9d2066ec026863793d9046d
assignUser <jacob@wujciak.de>
2022-04-04 19:32:36 +0200

    add skips

--- r/tests/testthat/test-style.R
+++ r/tests/testthat/test-style.R
@@ -15,6 +15,8 @@
 # specific language governing permissions and limitations
 # under the License.
 
-test_that("package is lint free", {
+test_that("Package is lint free", {
+    skip_on_cran()
+    skip_if_not_installed("lintr")
     lintr::expect_lint_free()
 })
