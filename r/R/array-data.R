6ce0b985623f6bc44d90b2061f9227e1a933188e
assignUser <jacob@wujciak.de>
2022-04-04 16:44:09 +0200

    add lint

--- r/R/array-data.R
+++ r/R/array-data.R
@@ -44,7 +44,7 @@
 ArrayData <- R6Class("ArrayData",
   inherit = ArrowObject,
   active = list(
-    type = function() ArrayData__get_type(self),
+    type =function() ArrayData__get_type(self),
     length = function() ArrayData__get_length(self),
     null_count = function() ArrayData__get_null_count(self),
     offset = function() ArrayData__get_offset(self),
