681ede6fcb81c102786261da1b193e0388387e77
Weston Pace <weston.pace@gmail.com>
2022-04-13 10:17:16 -1000

    ARROW-15604: [C++][CI] Sporadic ThreadSanitizer failure with OpenTracing
    The particular cause of the failure was:
    
    ```
      void InputReceived(ExecNode* input, ExecBatch batch) override {
        EVENT(span_, "InputReceived", {{"batch.length", batch.length}});
        util::tracing::Span span;
        START_SPAN_WITH_PARENT(span, span_, "InputReceived",
                               {{"node.label", label()}, {"batch.length", batch.length}});
    
        DCHECK_EQ(input, inputs_[0]);
    
        bool did_push = producer_.Push(std::move(batch));
        if (!did_push) return;  // producer_ was Closed already
    
        if (input_counter_.Increment()) {
          // This will mark the exec plan finished
          Finish();
          // At this point the main thread is generally free to exit but span hasn't been destructed yet
        }
      }
    ```
    
    It could be fixed with scoping:
    
    ```
      void InputReceived(ExecNode* input, ExecBatch batch) override {
        EVENT(span_, "InputReceived", {{"batch.length", batch.length}});
        {
          util::tracing::Span span;
          START_SPAN_WITH_PARENT(span, span_, "InputReceived",
                                 {{"node.label", label()}, {"batch.length", batch.length}});
    
          DCHECK_EQ(input, inputs_[0]);
    
          bool did_push = producer_.Push(std::move(batch));
          if (!did_push) return;  // producer_ was Closed already
        }
        if (input_counter_.Increment()) {
          // This will mark the exec plan finished
          Finish();
        }
      }
    ```
    However, I suspect this would quickly get tedious.  Instead it seems we can control the lifetime of the OT runtime storage and bind it to our worker threads.  In the future we may want to consider doing similar tricks to keep alive the memory pool, global thread pools, etc.
    
    Closes #12408 from westonpace/bugfix/ARROW-15604--sporadic-test-failure-with-ot
    
    Authored-by: Weston Pace <weston.pace@gmail.com>
    Signed-off-by: Weston Pace <weston.pace@gmail.com>

--- cpp/src/arrow/util/thread_pool.cc
+++ cpp/src/arrow/util/thread_pool.cc
@@ -37,6 +37,11 @@ namespace internal {
 
 Executor::~Executor() = default;
 
+// By default we do nothing here.  Subclasses that expect to be allocated
+// with static storage duration should override this and ensure any threads respect the
+// lifetime of these resources.
+void Executor::KeepAlive(std::shared_ptr<Resource> resource) {}
+
 namespace {
 
 struct Task {
@@ -200,6 +205,8 @@ struct ThreadPool::State {
   // Are we shutting down?
   bool please_shutdown_ = false;
   bool quick_shutdown_ = false;
+
+  std::vector<std::shared_ptr<Resource>> kept_alive_resources_;
 };
 
 // The worker loop is an independent function so that it can keep running
@@ -417,19 +424,11 @@ Status ThreadPool::SpawnReal(TaskHints hints, FnOnce<void()> task, StopToken sto
                              StopCallback&& stop_callback) {
   {
     ProtectAgainstFork();
-    std::lock_guard<std::mutex> lock(state_->mutex_);
-    if (state_->please_shutdown_) {
-      return Status::Invalid("operation forbidden during or after shutdown");
-    }
-    CollectFinishedWorkersUnlocked();
-    state_->tasks_queued_or_running_++;
-    if (static_cast<int>(state_->workers_.size()) < state_->tasks_queued_or_running_ &&
-        state_->desired_capacity_ > static_cast<int>(state_->workers_.size())) {
-      // We can still spin up more workers so spin up a new worker
-      LaunchWorkersUnlocked(/*threads=*/1);
-    }
 #ifdef ARROW_WITH_OPENTELEMETRY
     // Wrap the task to propagate a parent tracing span to it
+    // This task-wrapping needs to be done before we grab the mutex because the
+    // first call to OT (whatever that happens to be) will attempt to grab this mutex
+    // when calling KeepAlive to keep the OT infrastructure alive.
     struct {
       void operator()() {
         auto scope = ::arrow::internal::tracing::GetTracer()->WithActiveSpan(activeSpan);
@@ -441,11 +440,28 @@ Status ThreadPool::SpawnReal(TaskHints hints, FnOnce<void()> task, StopToken sto
               ::arrow::internal::tracing::GetTracer()->GetCurrentSpan()};
     task = std::move(wrapper);
 #endif
+    std::lock_guard<std::mutex> lock(state_->mutex_);
+    if (state_->please_shutdown_) {
+      return Status::Invalid("operation forbidden during or after shutdown");
+    }
+    CollectFinishedWorkersUnlocked();
+    state_->tasks_queued_or_running_++;
+    if (static_cast<int>(state_->workers_.size()) < state_->tasks_queued_or_running_ &&
+        state_->desired_capacity_ > static_cast<int>(state_->workers_.size())) {
+      // We can still spin up more workers so spin up a new worker
+      LaunchWorkersUnlocked(/*threads=*/1);
+    }
     state_->pending_tasks_.push_back(
         {std::move(task), std::move(stop_token), std::move(stop_callback)});
   }
   state_->cv_.notify_one();
   return Status::OK();
+}
+
+void ThreadPool::KeepAlive(std::shared_ptr<Executor::Resource> resource) {
+  // Seems unlikely but we might as well guard against concurrent calls to KeepAlive
+  std::lock_guard<std::mutex> lk(state_->mutex_);
+  state_->kept_alive_resources_.push_back(std::move(resource));
 }
 
 Result<std::shared_ptr<ThreadPool>> ThreadPool::Make(int threads) {
