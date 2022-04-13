ffca8c285d4058e4a351e74854ff3fa554fd4711
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2022-04-13 17:32:41 +0300

    short comment on what the purrr::imap + reduce bit does

--- r/R/dplyr-funcs-datetime.R
+++ r/R/dplyr-funcs-datetime.R
@@ -419,6 +419,7 @@ duration_from_chunks <- function(chunks) {
     "day" = 86400L,
     "week" = 604800L
   )
+  # transform the duration of each chunk in seconds and add everything together
   chunks_total <- purrr::imap(chunks, ~.x * chunk_duration[[.y]]) %>%
     purrr::reduce(`+`)
   chunks_total
