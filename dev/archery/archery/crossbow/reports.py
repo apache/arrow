c39aa0fd605647c8b57ff4dec0a7cb53c9ae4f4f
Raúl Cumplido <raulcumplido@gmail.com>
2022-04-14 15:52:23 +0200

    Fix repo_url parsing as requested on review

--- dev/archery/archery/crossbow/reports.py
+++ dev/archery/archery/crossbow/reports.py
@@ -169,13 +169,16 @@ class EmailReport(Report):
         self.recipient_email = recipient_email
         super().__init__(job)
 
+    @property
+    def repo_url(self):
+        url = self.job.queue.remote_url
+        return url[:-4] if url.endswith('.git') else url
+
     def url(self, query):
-        repo_url = self.job.queue.remote_url.strip('.git')
-        return '{}/branches/all?query={}'.format(repo_url, query)
+        return '{}/branches/all?query={}'.format(self.repo_url, query)
 
     def branch_url(self, branch):
-        repo_url = self.job.queue.remote_url.strip('.git')
-        return '{}/tree/{}'.format(repo_url, branch)
+        return '{}/tree/{}'.format(self.repo_url, branch)
 
     def listing(self, tasks):
         return '\n'.join(
