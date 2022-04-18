27444060384a7d8a5189b609e352d19c753aae10
Yibo Cai <yibo.cai@arm.com>
2022-04-18 06:58:52 +0000

    use target_include_directories, target_link_libraries

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -4626,10 +4626,8 @@ endmacro()
 if(ARROW_WITH_UCX)
   resolve_dependency(ucx PC_PACKAGE_NAMES ucx)
   add_library(ucx::ucx INTERFACE IMPORTED)
-  set_property(TARGET ucx::ucx PROPERTY INTERFACE_INCLUDE_DIRECTORIES
-                                        "${UCX_INCLUDE_DIRS}")
-  set_property(TARGET ucx::ucx PROPERTY INTERFACE_LINK_LIBRARIES ucx::ucp ucx::uct
-                                        ucx::ucs)
+  target_include_directories(ucx::ucx INTERFACE "${UCX_INCLUDE_DIRS}")
+  target_link_libraries(ucx::ucx INTERFACE ucx::ucp ucx::uct ucx::ucs)
 endif()
 
 message(STATUS "All bundled static libraries: ${ARROW_BUNDLED_STATIC_LIBS}")
