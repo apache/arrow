36ff017eb1047a957489a5629788afb52fa3a579
Jose Almeida <almeidajcr90@gmail.com>
2022-04-19 13:08:21 -0300

    Fix test that map arrows vector to jdbc types

--- java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/utils/SqlTypesTest.java
+++ java/flight/flight-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/utils/SqlTypesTest.java
@@ -65,8 +65,7 @@ public class SqlTypesTest {
     assertEquals(Types.ARRAY, getSqlTypeIdFromArrowType(new ArrowType.LargeList()));
     assertEquals(Types.ARRAY, getSqlTypeIdFromArrowType(new ArrowType.FixedSizeList(10)));
 
-    assertEquals(Types.STRUCT, getSqlTypeIdFromArrowType(new ArrowType.Struct()));
-
+    assertEquals(Types.JAVA_OBJECT, getSqlTypeIdFromArrowType(new ArrowType.Struct()));
     assertEquals(Types.JAVA_OBJECT,
         getSqlTypeIdFromArrowType(new ArrowType.Duration(TimeUnit.MILLISECOND)));
     assertEquals(Types.JAVA_OBJECT,
@@ -109,7 +108,7 @@ public class SqlTypesTest {
     assertEquals("ARRAY", getSqlTypeNameFromArrowType(new ArrowType.LargeList()));
     assertEquals("ARRAY", getSqlTypeNameFromArrowType(new ArrowType.FixedSizeList(10)));
 
-    assertEquals("STRUCT", getSqlTypeNameFromArrowType(new ArrowType.Struct()));
+    assertEquals("JAVA_OBJECT", getSqlTypeNameFromArrowType(new ArrowType.Struct()));
 
     assertEquals("JAVA_OBJECT",
         getSqlTypeNameFromArrowType(new ArrowType.Duration(TimeUnit.MILLISECOND)));
