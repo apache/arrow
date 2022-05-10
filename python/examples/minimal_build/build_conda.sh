335a3c7fc95b5cc9ff92a46c740b22977a653d94
Raúl Cumplido <raulcumplido@gmail.com>
2022-05-10 14:42:37 +0200

    Add submodule as git safe.directory

--- python/examples/minimal_build/build_conda.sh
+++ python/examples/minimal_build/build_conda.sh
@@ -29,6 +29,12 @@ ARROW_ROOT=/arrow
 PYTHON=3.10
 
 git config --global --add safe.directory $ARROW_ROOT
+# git submodules are required for unit tests
+git submodule update --init
+git config --global --add safe.directory $ARROW_ROOT/cpp/submodules/parquet-testing
+export PARQUET_TEST_DATA="$ARROW_ROOT/cpp/submodules/parquet-testing/data"
+export ARROW_TEST_DATA="$ARROW_ROOT/testing/data"
+
 #----------------------------------------------------------------------
 # Run these only once
 
@@ -109,10 +115,5 @@ export PYARROW_WITH_PARQUET=1
 
 # python setup.py build_ext --inplace
 python setup.py develop
-
-# git submodules are required for unit tests
-git submodule update --init
-export PARQUET_TEST_DATA="$ARROW_ROOT/cpp/submodules/parquet-testing/data"
-export ARROW_TEST_DATA="$ARROW_ROOT/testing/data"
 
 py.test pyarrow
--- python/examples/minimal_build/build_venv.sh
+++ python/examples/minimal_build/build_venv.sh
@@ -34,6 +34,12 @@ source $WORKDIR/venv/bin/activate
 
 git config --global --add safe.directory $ARROW_ROOT
 
+# git submodules are required for unit tests
+git submodule update --init
+git config --global --add safe.directory $ARROW_ROOT/cpp/submodules/parquet-testing
+export PARQUET_TEST_DATA="$ARROW_ROOT/cpp/submodules/parquet-testing/data"
+export ARROW_TEST_DATA="$ARROW_ROOT/testing/data"
+
 pip install -r $ARROW_ROOT/python/requirements-build.txt
 
 #----------------------------------------------------------------------
@@ -75,11 +81,6 @@ export PYARROW_WITH_PARQUET=1
 
 # python setup.py build_ext --inplace
 python setup.py develop
-
-# git submodules are required for unit tests
-git submodule update --init
-export PARQUET_TEST_DATA="$ARROW_ROOT/cpp/submodules/parquet-testing/data"
-export ARROW_TEST_DATA="$ARROW_ROOT/testing/data"
 
 pip install -r $ARROW_ROOT/python/requirements-test.txt
 
