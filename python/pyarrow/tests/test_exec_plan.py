65e9ba59e89f606992680dfe68e8d7285138637c
Alessandro Molina <amol@turbogears.org>
2022-05-10 11:24:19 +0200

    Test errors

--- python/pyarrow/tests/test_exec_plan.py
+++ python/pyarrow/tests/test_exec_plan.py
@@ -193,6 +193,25 @@ def test_table_join_keys_order():
     })
 
 
+def test_filter_table_errors():
+    t = pa.table({
+        "a": [1, 2, 3, 4, 5],
+        "b": [10, 20, 30, 40, 50]
+    })
+
+    with pytest.raises(pa.ArrowTypeError):
+        ep._filter_table(
+            t, pc.divide(pc.field("a"), pc.scalar(2)),
+            output_type=pa.Table
+        )
+
+    with pytest.raises(pa.ArrowInvalid):
+        ep._filter_table(
+            t, (pc.field("Z") <= pc.scalar(2)),
+            output_type=pa.Table
+        )
+
+
 @pytest.mark.parametrize("use_datasets", [False, True])
 def test_filter_table(use_datasets):
     t = pa.table({
