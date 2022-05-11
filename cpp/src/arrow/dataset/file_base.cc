f0f4d2f83f07c150755ddea07d65d8cbf5047799
Weston Pace <weston.pace@gmail.com>
2022-05-10 18:51:14 -1000

    ARROW-16525: Added tests for the tee node and write node.  Fix bug in tee node where it was not properly marking itself finished

--- cpp/src/arrow/dataset/file_base.cc
+++ cpp/src/arrow/dataset/file_base.cc
@@ -460,6 +460,23 @@ class TeeNode : public compute::MapNode {
 
   const char* kind_name() const override { return "TeeNode"; }
 
+  void Finish(Status finish_st) override {
+    dataset_writer_->Finish().AddCallback([this, finish_st](const Status& dw_status) {
+      // Need to wait for the task group to complete regardless of dw_status
+      task_group_.End().AddCallback(
+          [this, dw_status, finish_st](const Status& tg_status) {
+            // Prefer dw_status then finish_st and then tg_status
+            if (!dw_status.ok()) {
+              finished_.MarkFinished(dw_status);
+            }
+            if (!finish_st.ok()) {
+              finished_.MarkFinished(finish_st);
+            }
+            finished_.MarkFinished(tg_status);
+          });
+    });
+  }
+
   Result<compute::ExecBatch> DoTee(const compute::ExecBatch& batch) {
     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<RecordBatch> record_batch,
                           batch.ToRecordBatch(output_schema()));
