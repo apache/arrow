e26b858609065768474c0ef20fd856fcfc4bdc84
Sam Albers <sam.albers@gmail.com>
2022-05-17 20:49:07 -0700

    use a tibble and make both simpler

--- r/tests/testthat/test-s3-minio.R
+++ r/tests/testthat/test-s3-minio.R
@@ -55,15 +55,16 @@ if (arrow_with_s3() && process_is_running("minio server")) {
   })
 
   test_that("read/write compressed csv by filesystem", {
-    write_csv_arrow(example_data, fs$path(minio_path("test.csv.gz")))
+    dat <- tibble(x = seq(1,10, by = 0.2))
+    write_csv_arrow(dat, fs$path(minio_path("test.csv.gz")))
     expect_identical(
       read_csv_arrow(fs$path(minio_path("test.csv.gz"))),
-      example_data
+      dat
     )
   })
 
   test_that("read/write csv by filesystem", {
-    dat <- data.frame(x = seq(1,10, by = 0.2))
+    dat <- tibble(x = seq(1,10, by = 0.2))
     write_csv_arrow(dat, fs$path(minio_path("test.csv")))
     expect_identical(
       read_csv_arrow(fs$path(minio_path("test.csv"))),
