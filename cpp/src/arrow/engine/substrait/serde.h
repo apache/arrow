75834b8ee00b6f5f3951943030e1df17d609a650
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2022-05-19 20:19:32 +0530

    refactor: rename ext_set to ext_set_out in DeserialisePlan in serde header file

--- cpp/src/arrow/engine/substrait/serde.h
+++ cpp/src/arrow/engine/substrait/serde.h
@@ -44,16 +44,16 @@ using ConsumerFactory = std::function<std::shared_ptr<compute::SinkNodeConsumer>
 /// message
 /// \param[in] consumer_factory factory function for generating the node that consumes
 /// the batches produced by each toplevel Substrait relation
-/// \param[out] ext_set if non-null, the extension mapping used by the Substrait Plan is
+/// \param[out] ext_set_out if non-null, the extension mapping used by the Substrait Plan is
 /// returned here.
 /// \return a vector of ExecNode declarations, one for each toplevel relation in the
 /// Substrait Plan
 ARROW_ENGINE_EXPORT Result<std::vector<compute::Declaration>> DeserializePlans(
-    const Buffer& buf, const ConsumerFactory& consumer_factory, ExtensionSet* ext_set = NULLPTR);
+    const Buffer& buf, const ConsumerFactory& consumer_factory, ExtensionSet* ext_set_out = NULLPTR);
 
 Result<compute::ExecPlan> DeserializePlan(const Buffer& buf,
                                           const ConsumerFactory& consumer_factory,
-                                          ExtensionSet* ext_set = NULLPTR);
+                                          ExtensionSet* ext_set_out = NULLPTR);
 
 /// \brief Deserializes a Substrait Type message to the corresponding Arrow type
 ///
