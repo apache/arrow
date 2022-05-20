9eecd8c35068572ae2d5458d07debafa7172b3fd
Vibhatha Abeykoon <vibhatha@gmail.com>
2022-05-20 15:36:39 +0530

    fixing tpch benchmarks for agg op refactor

--- cpp/src/arrow/compute/exec/tpch_benchmark.cc
+++ cpp/src/arrow/compute/exec/tpch_benchmark.cc
@@ -78,21 +78,17 @@ std::shared_ptr<ExecPlan> Plan_Q1(AsyncGenerator<util::optional<ExecBatch>>* sin
       std::make_shared<ScalarAggregateOptions>(ScalarAggregateOptions::Defaults());
   auto count_opts = std::make_shared<CountOptions>(CountOptions::CountMode::ALL);
   std::vector<arrow::compute::internal::Aggregate> aggs = {
-      {"hash_sum", sum_opts, "sum_qty"},
-      {"hash_sum", sum_opts, "sum_base_price"},
-      {"hash_sum", sum_opts, "sum_disc_price"},
-      {"hash_sum", sum_opts, "sum_charge"},
-      {"hash_mean", sum_opts, "avg_qty"},
-      {"hash_mean", sum_opts, "avg_price"},
-      {"hash_mean", sum_opts, "avg_disc"},
-      {"hash_count", count_opts, "sum_qty"}};
-
-  std::vector<std::string> names = {"sum_qty",    "sum_base_price", "sum_disc_price",
-                                    "sum_charge", "avg_qty",        "avg_price",
-                                    "avg_disc",   "count_order"};
+      {"hash_sum", sum_opts, "sum_qty", "sum_qty"},
+      {"hash_sum", sum_opts, "sum_base_price", "sum_base_price"},
+      {"hash_sum", sum_opts, "sum_disc_price", "sum_disc_price"},
+      {"hash_sum", sum_opts, "sum_charge", "sum_charge"},
+      {"hash_mean", sum_opts, "avg_qty", "avg_qty"},
+      {"hash_mean", sum_opts, "avg_price", "avg_price"},
+      {"hash_mean", sum_opts, "avg_disc", "avg_disc"},
+      {"hash_count", count_opts, "sum_qty", "count_order"}};
 
   std::vector<FieldRef> keys = {"l_returnflag", "l_linestatus"};
-  AggregateNodeOptions agg_opts(aggs, names, keys);
+  AggregateNodeOptions agg_opts(aggs, keys);
 
   SortKey l_returnflag_key("l_returnflag");
   SortKey l_linestatus_key("l_linestatus");
