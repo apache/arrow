0c75d84384c34c522e7c352916526d2266ac98fb
Antoine Pitrou <antoine@python.org>
2022-05-31 14:50:45 +0200

    Simplify C++ guards

--- java/c/src/main/cpp/abi.h
+++ java/c/src/main/cpp/abi.h
@@ -65,14 +65,6 @@ struct ArrowArray {
 
 #endif  // ARROW_C_DATA_INTERFACE
 
-#ifdef __cplusplus
-}
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 // EXPERIMENTAL: C stream interface
 
 #ifndef ARROW_C_STREAM_INTERFACE
