6f64e1c55c5b17aca61572e0da36967193b049c3
Johnnathan <johnnathanalmeida@gmail.com>
2022-05-31 20:09:54 -0300

    Added new projector test to reproduce the error

--- cpp/src/gandiva/tests/utf8_test.cc
+++ cpp/src/gandiva/tests/utf8_test.cc
@@ -748,4 +748,47 @@ TEST_F(TestUtf8, TestSpace) {
   EXPECT_ARROW_ARRAY_EQUALS(exp_space, outputs.at(0));
 }
 
+TEST_F(TestUtf8, TestReplace) {
+
+
+//  auto literal0 = TreeExprBuilder::MakeLiteral(d0);
+//  auto expr0 = TreeExprBuilder::MakeExpression(literal0, res);
+
+  // schema for input fields
+  auto field_a = field("a", arrow::utf8());
+  auto field_b = field("b", arrow::utf8());
+  auto field_c = field("c", arrow::utf8());
+  auto schema = arrow::schema({field_a, field_b, field_c});
+
+  // output fields
+  auto res = field("replace", arrow::utf8());
+
+  // Build expression
+  auto replace_expr = TreeExprBuilder::MakeExpression("regexp_replace", {field_a, field_b, field_c}, res);
+
+  std::shared_ptr<Projector> projector;
+  auto status = Projector::Make(schema, {replace_expr}, TestConfiguration(), &projector);
+  EXPECT_TRUE(status.ok()) << status.message();
+
+  // Create a row-batch with some sample data
+  int num_records = 2;
+  auto array0 = MakeArrowArrayUtf8({"123456789", "asdf1asdf"}, {true, true});
+  auto array1 = MakeArrowArrayUtf8({"3", "1"}, {true, true});
+  auto array2 = MakeArrowArrayUtf8({"9", "0"}, {true, true});
+
+  // expected output
+  auto exp_replace = MakeArrowArrayUtf8({"129456789", "asdf0asdf"}, {true, true});
+
+  // prepare input record batch
+  auto in_batch = arrow::RecordBatch::Make(schema, num_records, {array0, array1, array2});
+
+  // Evaluate expression
+  arrow::ArrayVector outputs;
+  status = projector->Evaluate(*in_batch, pool_, &outputs);
+  EXPECT_TRUE(status.ok()) << status.message();
+
+  // Validate results
+  EXPECT_ARROW_ARRAY_EQUALS(exp_replace, outputs.at(0));
+}
+
 }  // namespace gandiva
