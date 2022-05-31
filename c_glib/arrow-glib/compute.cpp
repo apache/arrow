1227a133cd793617c78ee6de9e2b250d128c4928
Vibhatha Abeykoon <vibhatha@gmail.com>
2022-05-31 18:18:54 +0530

    adding patch

--- c_glib/arrow-glib/compute.cpp
+++ c_glib/arrow-glib/compute.cpp
@@ -1255,8 +1255,6 @@ garrow_aggregate_node_options_new(GList *aggregations,
                                   GError **error)
 {
   std::vector<arrow::compute::internal::Aggregate> arrow_aggregates;
-  std::vector<arrow::FieldRef> arrow_targets;
-  std::vector<std::string> arrow_names;
   std::vector<arrow::FieldRef> arrow_keys;
   for (auto node = aggregations; node; node = node->next) {
     auto aggregation_priv = GARROW_AGGREGATION_GET_PRIVATE(node->data);
@@ -1279,7 +1277,12 @@ garrow_aggregate_node_options_new(GList *aggregations,
                                "[aggregate-node-options][new][input]")) {
       return NULL;
     }
-    arrow_names.emplace_back(aggregation_priv->output);
+    arrow_aggregates.push_back({
+                                  aggregation_priv->function,
+                                  function_options,
+                                  arrow_targets[0],
+                                  aggregation_priv->output
+                               });
   }
   for (gsize i = 0; i < n_keys; ++i) {
     if (!garrow_field_refs_add(arrow_keys,
@@ -1291,8 +1294,6 @@ garrow_aggregate_node_options_new(GList *aggregations,
   }
   auto arrow_options =
     new arrow::compute::AggregateNodeOptions(std::move(arrow_aggregates),
-                                             std::move(arrow_targets),
-                                             std::move(arrow_names),
                                              std::move(arrow_keys));
   auto options = g_object_new(GARROW_TYPE_AGGREGATE_NODE_OPTIONS,
                               "options", arrow_options,
