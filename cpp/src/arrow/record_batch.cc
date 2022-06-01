774c86230842ed86dcff23c452c81780991273b9
Vibhatha Abeykoon <vibhatha@gmail.com>
2022-06-01 09:48:04 +0530

    fix format

--- cpp/src/arrow/record_batch.cc
+++ cpp/src/arrow/record_batch.cc
@@ -393,7 +393,8 @@ Result<std::shared_ptr<RecordBatchReader>> RecordBatchReader::Make(
 RecordBatchReader::~RecordBatchReader() {
   auto st = this->Close();
   if (!st.ok()) {
-    ARROW_LOG(WARNING) << "Implicitly called RecordBatchReader::Close failed: " << st.ToString();
+    ARROW_LOG(WARNING) << "Implicitly called RecordBatchReader::Close failed: "
+                       << st.ToString();
   }
 }
 
