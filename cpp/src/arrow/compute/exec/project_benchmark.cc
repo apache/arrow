6289e91159b9abf74a10299abd5b1f734e83cba8
Ivan Chau <ivan.m.chau@gmail.com>
2022-06-07 10:09:44 -0400

    rename, remove gbenchmark thread control, refactoring, buggy isolation tests

--- cpp/src/arrow/compute/exec/project_benchmark.cc
+++ cpp/src/arrow/compute/exec/project_benchmark.cc
@@ -31,37 +31,26 @@
 namespace arrow {
 namespace compute {
 
-static constexpr int64_t total_batch_size = 1e5;
+static constexpr int64_t total_batch_size = 1e6;
 
-void SetArgs(benchmark::internal::Benchmark* bench) {
-  bench->ArgNames({"batch_size"})
-      ->RangeMultiplier(10)
-      ->Range(1, total_batch_size)
-      ->DenseThreadRange(1, std::thread::hardware_concurrency(),
-                         std::thread::hardware_concurrency())
-      ->UseRealTime();
-}
-
-static void ExecuteScalarProjectionOverhead(benchmark::State& state, Expression expr) {
+static void ProjectionOverhead(benchmark::State& state, Expression expr) {
   const auto batch_size = static_cast<int32_t>(state.range(0));
   const auto num_batches = total_batch_size / batch_size;
 
   auto data = MakeRandomBatches(schema({field("i64", int64()), field("bool", boolean())}),
                                 num_batches, batch_size);
-  ExecContext* ctx = default_exec_context();
-  *ctx = ExecContext(default_memory_pool(), arrow::internal::GetCpuThreadPool());
+  ExecContext ctx(default_memory_pool(), arrow::internal::GetCpuThreadPool());
   for (auto _ : state) {
     state.PauseTiming();
-    ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make(ctx));
+    ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make(&ctx));
     AsyncGenerator<util::optional<ExecBatch>> sink_gen;
     ASSERT_OK(Declaration::Sequence(
                   {
                       {"source",
                        SourceNodeOptions{data.schema,
-                                         data.gen(/*parallel=*/false, /*slow=*/false)},
+                                         data.gen(/*parallel=*/true, /*slow=*/false)},
                        "custom_source_label"},
                       {"project", ProjectNodeOptions{{
-                                      field_ref("bool"),
                                       expr,
                                   }}},
                       {"sink", SinkNodeOptions{&sink_gen}, "custom_sink_label"},
@@ -78,6 +67,39 @@ static void ExecuteScalarProjectionOverhead(benchmark::State& state, Expression 
       static_cast<double>(state.iterations() * num_batches), benchmark::Counter::kIsRate);
 }
 
+static void ProjectionOverheadIsolated(benchmark::State& state, Expression expr) {
+  const auto batch_size = static_cast<int32_t>(state.range(0));
+  const auto num_batches = total_batch_size / batch_size;
+
+  auto data = MakeRandomBatches(schema({field("i64", int64()), field("bool", boolean())}),
+                                num_batches, batch_size);
+  ExecContext ctx(default_memory_pool(), arrow::internal::GetCpuThreadPool());
+  for (auto _ : state) {
+    state.PauseTiming();
+    ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make(&ctx));
+    
+    ASSERT_OK_AND_ASSIGN(auto sn, MakeExecNode("source", plan.get(), {}, SourceNodeOptions{data.schema,
+                                         data.gen(/*parallel=*/true, /*slow=*/false)}));                          
+    ASSERT_OK_AND_ASSIGN(auto pn, MakeExecNode("project", plan.get(), {sn}, ProjectNodeOptions{{
+                                      expr,
+                                  }}));
+    // sn->PrepareToProduce();
+    // pn->PrepareToProduce();
+    state.ResumeTiming();
+    pn->InputFinished(sn, num_batches);
+    for (auto b : data.batches) {
+        pn->InputReceived(sn, b);
+    }
+    pn->finished();
+  }
+  state.counters["rows_per_second"] = benchmark::Counter(
+      static_cast<double>(state.iterations() * num_batches * batch_size),
+      benchmark::Counter::kIsRate);
+
+  state.counters["batches_per_second"] = benchmark::Counter(
+      static_cast<double>(state.iterations() * num_batches), benchmark::Counter::kIsRate);
+}
+
 auto complex_expression =
     and_(less(field_ref("i64"), literal(20)), greater(field_ref("i64"), literal(0)));
 auto simple_expression = call("negate", {field_ref("i64")});
@@ -85,16 +107,28 @@ auto zero_copy_expression = call("cast", {field_ref("i64")},
                                  compute::CastOptions::Safe(timestamp(TimeUnit::NANO)));
 auto ref_only_expression = field_ref("i64");
 
-BENCHMARK_CAPTURE(ExecuteScalarProjectionOverhead, complex_expression, complex_expression)
+void SetArgs(benchmark::internal::Benchmark* bench) {
+  bench->ArgNames({"batch_size"})
+      ->RangeMultiplier(10)
+      ->Range(1000, total_batch_size)
+      ->UseRealTime();
+}
+
+BENCHMARK_CAPTURE(ProjectionOverheadIsolated, complex_expression, complex_expression)
     ->Apply(SetArgs);
 
-BENCHMARK_CAPTURE(ExecuteScalarProjectionOverhead, simple_expression, simple_expression)
+
+BENCHMARK_CAPTURE(ProjectionOverhead, complex_expression, complex_expression)
     ->Apply(SetArgs);
-BENCHMARK_CAPTURE(ExecuteScalarProjectionOverhead, zero_copy_expression,
+
+BENCHMARK_CAPTURE(ProjectionOverhead, simple_expression, simple_expression)
+    ->Apply(SetArgs);
+BENCHMARK_CAPTURE(ProjectionOverhead, zero_copy_expression,
                   zero_copy_expression)
     ->Apply(SetArgs);
-BENCHMARK_CAPTURE(ExecuteScalarProjectionOverhead, ref_only_expression,
+BENCHMARK_CAPTURE(ProjectionOverhead, ref_only_expression,
                   ref_only_expression)
     ->Apply(SetArgs);
+
 }  // namespace compute
 }  // namespace arrow
