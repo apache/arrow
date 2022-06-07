bf78dab6c1b4317fa7b3b6a07c48343a422876df
Tobias Zagorni <tobias@zagorni.eu>
2022-06-07 19:20:18 +0200

    test decoding in rle tests

--- cpp/src/arrow/compute/kernels/vector_run_length_encode_test.cc
+++ cpp/src/arrow/compute/kernels/vector_run_length_encode_test.cc
@@ -40,20 +40,26 @@ TEST_F(TestRunLengthEncode, EncodeInt32Array) {
   std::array<uint64_t, 3> expected_run_lengths{2, 3, 6};
   std::array<int32_t, 3> expected_values{1, 2, -5};
 
-  ASSERT_OK_AND_ASSIGN(Datum result_datum, RunLengthEncode(input));
+  ASSERT_OK_AND_ASSIGN(Datum encoded_datum, RunLengthEncode(input));
 
-  auto result = result_datum.array();
-  ASSERT_EQ(*(result->GetMutableValues<std::array<uint64_t, 3>>(0)),
+  auto encoded = encoded_datum.array();
+  ASSERT_EQ(*(encoded->GetMutableValues<std::array<uint64_t, 3>>(0)),
             expected_run_lengths);
-  ASSERT_EQ(*(result->child_data[0]->GetMutableValues<std::array<int32_t, 3>>(1)),
+  ASSERT_EQ(*(encoded->child_data[0]->GetMutableValues<std::array<int32_t, 3>>(1)),
             expected_values);
-  ASSERT_EQ(result->child_data[0]->GetMutableValues<uint8_t>(0), nullptr);
-  ASSERT_EQ(result->buffers[0]->size(), 3 * sizeof(uint64_t));
-  ASSERT_EQ(result->child_data[0]->buffers[1]->size(), 3 * sizeof(int32_t));
-  ASSERT_EQ(result->length, 3);
-  ASSERT_EQ(*result->type, RunLengthEncodedType(int32()));
-  ASSERT_EQ(result->null_count, 0);
-  ASSERT_EQ(result->child_data[0]->null_count, 0);
+  ASSERT_EQ(encoded->child_data[0]->GetMutableValues<uint8_t>(0), nullptr);
+  ASSERT_EQ(encoded->buffers[0]->size(), 3 * sizeof(uint64_t));
+  ASSERT_EQ(encoded->child_data[0]->buffers[1]->size(), 3 * sizeof(int32_t));
+  ASSERT_EQ(encoded->length, 3);
+  ASSERT_EQ(*encoded->type, RunLengthEncodedType(int32()));
+  ASSERT_EQ(encoded->null_count, 0);
+  ASSERT_EQ(encoded->child_data[0]->null_count, 0);
+  ASSERT_EQ(encoded->child_data[0]->length, 3);
+
+  ASSERT_OK_AND_ASSIGN(Datum decoded_datum, RunLengthDecode(encoded));
+  auto decoded = decoded_datum.make_array();
+  ASSERT_OK(decoded->ValidateFull());
+  ASSERT_TRUE(decoded->Equals(input));
 }
 
 TEST_F(TestRunLengthEncode, EncodeArrayWithNull) {
@@ -68,21 +74,25 @@ TEST_F(TestRunLengthEncode, EncodeArrayWithNull) {
   std::array<uint64_t, 4> expected_run_lengths{1, 3, 5, 6};
   uint8_t expected_null_bitmap{0b1010};
 
-  ASSERT_OK_AND_ASSIGN(Datum result_datum, RunLengthEncode(input));
+  ASSERT_OK_AND_ASSIGN(Datum encoded_datum, RunLengthEncode(input));
 
-  auto result = result_datum.array();
-
-  ASSERT_EQ(*(result->GetMutableValues<std::array<uint64_t, 4>>(0)),
+  auto encoded = encoded_datum.array();
+  ASSERT_EQ(*(encoded->GetMutableValues<std::array<uint64_t, 4>>(0)),
             expected_run_lengths);
-  ASSERT_EQ(result->child_data[0]->GetMutableValues<int32_t>(1)[1], 1);
-  ASSERT_EQ(result->child_data[0]->GetMutableValues<int32_t>(1)[3], -5);
-  ASSERT_EQ(*(result->child_data[0]->GetMutableValues<uint8_t>(0)), expected_null_bitmap);
-  ASSERT_EQ(result->buffers[0]->size(), 4 * sizeof(uint64_t));
-  ASSERT_EQ(result->child_data[0]->buffers[1]->size(), 4 * sizeof(int32_t));
-  ASSERT_EQ(result->length, 4);
-  ASSERT_EQ(*result->type, RunLengthEncodedType(int32()));
-  ASSERT_EQ(result->null_count, 2);
-  ASSERT_EQ(result->child_data[0]->null_count, 2);
+  ASSERT_EQ(encoded->child_data[0]->GetMutableValues<int32_t>(1)[1], 1);
+  ASSERT_EQ(encoded->child_data[0]->GetMutableValues<int32_t>(1)[3], -5);
+  ASSERT_EQ(*(encoded->child_data[0]->GetMutableValues<uint8_t>(0)), expected_null_bitmap);
+  ASSERT_EQ(encoded->buffers[0]->size(), 4 * sizeof(uint64_t));
+  ASSERT_EQ(encoded->child_data[0]->buffers[1]->size(), 4 * sizeof(int32_t));
+  ASSERT_EQ(encoded->length, 4);
+  ASSERT_EQ(*encoded->type, RunLengthEncodedType(int32()));
+  ASSERT_EQ(encoded->null_count, 3);
+  ASSERT_EQ(encoded->child_data[0]->null_count, 2);
+
+  ASSERT_OK_AND_ASSIGN(Datum decoded_datum, RunLengthDecode(encoded));
+  auto decoded = decoded_datum.make_array();
+  ASSERT_OK(decoded->ValidateFull());
+  ASSERT_TRUE(decoded->Equals(input));
 }
 
 }  // namespace compute
