982960a1845c9c6760b0256970a216e9c5ac34dd
Jacky Lee <lijunqing@baidu.com>
2022-06-08 21:30:07 +0800

    ARROW-16566: [Java] Initialize JNI components on use instead of statically (#13146)
    We are using Arrow in Spark. When speculation is turned on, it is possible for the task to be forcibly killed during the running process. It means, `JniLoader.get().ensureLoaded()` maybe failed with `ClosedByInterruptException` when the task was killed, and it should be accepted with this.
    However, `NativeMemoryPool` use it as a static code, this leads to an init failure every time the NativeMemroyPool is trying to initialized and can never succeed. It would always fail to init `NativeMemoryPool` with `java.lang.NoClassDefFoundError: Could not initialize class org.apache.arrow.dataset.jni.NativeMemoryPool`.
    
    Lead-authored-by: jackylee-ch <lijunqing@baidu.com>
    Co-authored-by: stczwd <qcsd2011@163.com>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- java/dataset/src/main/java/org/apache/arrow/dataset/jni/JniWrapper.java
+++ java/dataset/src/main/java/org/apache/arrow/dataset/jni/JniWrapper.java
@@ -25,11 +25,11 @@ public class JniWrapper {
   private static final JniWrapper INSTANCE = new JniWrapper();
 
   public static JniWrapper get() {
+    JniLoader.get().ensureLoaded();
     return INSTANCE;
   }
 
   private JniWrapper() {
-    JniLoader.get().ensureLoaded();
   }
 
   /**
