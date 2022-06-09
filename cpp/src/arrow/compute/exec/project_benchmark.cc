a0282d02f9c59ae31a503c929668d1a79080de6f
Ivan Chau <ivan.m.chau@gmail.com>
2022-06-09 09:57:31 -0400

    remove auto, change variable names

--- cpp/src/arrow/compute/exec/project_benchmark.cc
+++ cpp/src/arrow/compute/exec/project_benchmark.cc
@@ -35,18 +35,19 @@
 namespace arrow {
 namespace compute {
 
-static constexpr int64_t total_batch_size = 1e6;
+static constexpr int64_t kTotalBatchSize = 1e6;
 
 static void ProjectionOverhead(benchmark::State& state, Expression expr) {
-  const auto batch_size = static_cast<int32_t>(state.range(0));
-  const auto num_batches = total_batch_size / batch_size;
+  const int32_t batch_size = static_cast<int32_t>(state.range(0));
+  const int64_t num_batches = kTotalBatchSize / batch_size;
 
-  auto data = MakeRandomBatches(schema({field("i64", int64()), field("bool", boolean())}),
-                                num_batches, batch_size);
+  arrow::compute::BatchesWithSchema data = MakeRandomBatches(
+      schema({field("i64", int64()), field("bool", boolean())}), num_batches, batch_size);
   ExecContext ctx(default_memory_pool(), arrow::internal::GetCpuThreadPool());
   for (auto _ : state) {
     state.PauseTiming();
-    ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make(&ctx));
+    ASSERT_OK_AND_ASSIGN(std::shared_ptr<arrow::compute::ExecPlan> plan,
+                         ExecPlan::Make(&ctx));
     AsyncGenerator<util::optional<ExecBatch>> sink_gen;
     ASSERT_OK(Declaration::Sequence(
                   {
@@ -63,6 +64,7 @@ static void ProjectionOverhead(benchmark::State& state, Expression expr) {
     state.ResumeTiming();
     ASSERT_FINISHES_OK(StartAndCollect(plan.get(), sink_gen));
   }
+
   state.counters["rows_per_second"] = benchmark::Counter(
       static_cast<double>(state.iterations() * num_batches * batch_size),
       benchmark::Counter::kIsRate);
@@ -72,62 +74,65 @@ static void ProjectionOverhead(benchmark::State& state, Expression expr) {
 }
 
 static void ProjectionOverheadIsolated(benchmark::State& state, Expression expr) {
-  const auto batch_size = static_cast<int32_t>(state.range(0));
-  const auto num_batches = total_batch_size / batch_size;
+  const int32_t batch_size = static_cast<int32_t>(state.range(0));
+  const int64_t num_batches = kTotalBatchSize / batch_size;
 
-  auto data = MakeRandomBatches(schema({field("i64", int64()), field("bool", boolean())}),
-                                num_batches, batch_size);
+  arrow::compute::BatchesWithSchema data = MakeRandomBatches(
+      schema({field("i64", int64()), field("bool", boolean())}), num_batches, batch_size);
   ExecContext ctx(default_memory_pool(), arrow::internal::GetCpuThreadPool());
   for (auto _ : state) {
     state.PauseTiming();
 
     AsyncGenerator<util::optional<ExecBatch>> sink_gen;
-    ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make(&ctx));
+    ASSERT_OK_AND_ASSIGN(std::shared_ptr<arrow::compute::ExecPlan> plan,
+                         ExecPlan::Make(&ctx));
     // Source and sink nodes have no effect on the benchmark.
     // Used for dummy purposes as they are referenced in InputReceived and InputFinished.
     ASSERT_OK_AND_ASSIGN(
-        auto sn, MakeExecNode("source", plan.get(), {},
-                              SourceNodeOptions{data.schema, data.gen(/*parallel=*/true,
-                                                                      /*slow=*/false)}));
-    ASSERT_OK_AND_ASSIGN(auto pn, MakeExecNode("project", plan.get(), {sn},
-                                               ProjectNodeOptions{{
-                                                   expr,
-                                               }}));
-    MakeExecNode("sink", plan.get(), {pn}, SinkNodeOptions{&sink_gen});
+        arrow::compute::ExecNode * source_node,
+        MakeExecNode("source", plan.get(), {},
+                     SourceNodeOptions{data.schema, data.gen(/*parallel=*/true,
+                                                             /*slow=*/false)}));
+    ASSERT_OK_AND_ASSIGN(arrow::compute::ExecNode * project_node,
+                         MakeExecNode("project", plan.get(), {source_node},
+                                      ProjectNodeOptions{{
+                                          expr,
+                                      }}));
+    MakeExecNode("sink", plan.get(), {project_node}, SinkNodeOptions{&sink_gen});
 
-    auto scheduler = TaskScheduler::Make();
-    std::condition_variable cv;
+    std::unique_ptr<arrow::compute::TaskScheduler> scheduler = TaskScheduler::Make();
+    std::condition_variable all_tasks_finished_cv;
     std::mutex mutex;
     int task_group_id = scheduler->RegisterTaskGroup(
         [&](size_t thread_id, int64_t task_id) {
-          pn->InputReceived(sn, data.batches[task_id]);
+          project_node->InputReceived(source_node, data.batches[task_id]);
           return Status::OK();
         },
         [&](size_t thread_id) {
-          pn->InputFinished(sn, data.batches.size());
+          project_node->InputFinished(source_node, data.batches.size());
           std::unique_lock<std::mutex> lk(mutex);
-          cv.notify_one();
+          all_tasks_finished_cv.notify_one();
           return Status::OK();
         });
     scheduler->RegisterEnd();
     ThreadIndexer thread_indexer;
 
     state.ResumeTiming();
-    auto tp = arrow::internal::GetCpuThreadPool();
+    arrow::internal::ThreadPool* thread_pool = arrow::internal::GetCpuThreadPool();
     ASSERT_OK(scheduler->StartScheduling(
         thread_indexer(),
         [&](std::function<Status(size_t)> task) -> Status {
-          return tp->Spawn([&, task]() {
+          return thread_pool->Spawn([&, task]() {
             size_t tid = thread_indexer();
             ARROW_DCHECK_OK(task(tid));
           });
         },
-        tp->GetCapacity(),
+        thread_pool->GetCapacity(),
         /*use_sync_execution=*/false));
     std::unique_lock<std::mutex> lk(mutex);
     ASSERT_OK(scheduler->StartTaskGroup(thread_indexer(), task_group_id, num_batches));
-    cv.wait(lk);
-    ASSERT_TRUE(pn->finished().is_finished());
+    all_tasks_finished_cv.wait(lk);
+    ASSERT_TRUE(project_node->finished().is_finished());
   }
 
   state.counters["rows_per_second"] = benchmark::Counter(
@@ -138,17 +143,17 @@ static void ProjectionOverheadIsolated(benchmark::State& state, Expression expr)
       static_cast<double>(state.iterations() * num_batches), benchmark::Counter::kIsRate);
 }
 
-auto complex_expression =
+arrow::compute::Expression complex_expression =
     and_(less(field_ref("i64"), literal(20)), greater(field_ref("i64"), literal(0)));
-auto simple_expression = call("negate", {field_ref("i64")});
-auto zero_copy_expression = call("cast", {field_ref("i64")},
-                                 compute::CastOptions::Safe(timestamp(TimeUnit::NANO)));
-auto ref_only_expression = field_ref("i64");
+arrow::compute::Expression simple_expression = call("negate", {field_ref("i64")});
+arrow::compute::Expression zero_copy_expression = call(
+    "cast", {field_ref("i64")}, compute::CastOptions::Safe(timestamp(TimeUnit::NANO)));
+arrow::compute::Expression ref_only_expression = field_ref("i64");
 
 void SetArgs(benchmark::internal::Benchmark* bench) {
   bench->ArgNames({"batch_size"})
       ->RangeMultiplier(10)
-      ->Range(1000, total_batch_size)
+      ->Range(1000, kTotalBatchSize)
       ->UseRealTime();
 }
 
