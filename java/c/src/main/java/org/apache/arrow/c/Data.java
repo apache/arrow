83e663dec5a863ba707b264432f5e2c68c695ff5
Will Jones <willjones127@gmail.com>
2022-06-09 08:10:42 -0700

    fix: This EXPERIMENTAL marker referred to the API, not the C interface

--- java/c/src/main/java/org/apache/arrow/c/Data.java
+++ java/c/src/main/java/org/apache/arrow/c/Data.java
@@ -36,6 +36,8 @@ import org.apache.arrow.vector.types.pojo.Schema;
 /**
  * Functions for working with the C data interface.
  * <p>
+ * This API is EXPERIMENTAL. Note that currently only 64bit systems are
+ * supported.
  * Note that currently only 64bit systems are supported.
  */
 public final class Data {
