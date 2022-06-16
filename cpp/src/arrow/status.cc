779ec214827fae5ce31a54a55f811cf00a4a2661
Aldrin M <octalene.dev@pm.me>
2022-06-16 07:42:56 -0700

    [ARROW-16769]: Warn() logs just the status
    reviewer pointed out that empty messages will have a spurious ":".
    Instead of introducing a conditional on whether the string is empty,
    it's better to make Warn() just log the status.

--- cpp/src/arrow/status.cc
+++ cpp/src/arrow/status.cc
@@ -131,7 +131,9 @@ void Status::Abort(const std::string& message) const {
   std::abort();
 }
 
-void Status::Warn() const { Warn(std::string()); }
+void Status::Warn() const {
+  ARROW_LOG(WARNING) << ToString();
+}
 
 void Status::Warn(const std::string& message) const {
   ARROW_LOG(WARNING) << "Arrow Warning -- "
