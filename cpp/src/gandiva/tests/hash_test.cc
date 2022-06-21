7766a8c20d043ef0c91d84a82f364b9af2f54e58
Johnnathan <johnnathanalmeida@gmail.com>
2022-06-21 16:08:47 -0300

    Was ran clang to fix lint error in CI

--- cpp/src/gandiva/tests/hash_test.cc
+++ cpp/src/gandiva/tests/hash_test.cc
@@ -260,8 +260,8 @@ TEST_F(TestHash, TestSha512Varlen) {
       "ði ıntəˈnæʃənəl fəˈnɛtık əsoʊsiˈeın\nY "
       "[ˈʏpsilɔn], Yen [jɛn], Yoga [ˈjoːgɑ] コンニチハ";
 
-  auto array_a =
-      MakeArrowArrayUtf8({"foo", first_string, second_string, " "}, {false, true, true, true});
+  auto array_a = MakeArrowArrayUtf8({"foo", first_string, second_string, " "},
+                                    {false, true, true, true});
 
   // prepare input record batch
   auto in_batch = arrow::RecordBatch::Make(schema, num_records, {array_a});
