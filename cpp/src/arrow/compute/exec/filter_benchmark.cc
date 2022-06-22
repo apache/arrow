42072c62c28133bccc8367d13da301318c5d0483
Ivan Chau <ivan.m.chau@gmail.com>
2022-06-22 10:55:12 -0400

    package benchmarking code in benchmark_util.h

--- cpp/src/arrow/compute/exec/filter_benchmark.cc
+++ cpp/src/arrow/compute/exec/filter_benchmark.cc
@@ -29,6 +29,7 @@
 #include "arrow/testing/future_util.h"
 #include "arrow/testing/generator.h"
 #include "arrow/testing/gtest_util.h"
+#include "arrow/util/benchmark_util.h"
 #include "arrow/type.h"
 
 namespace arrow {
@@ -43,111 +44,45 @@ static void FilterOverhead(benchmark::State& state, Expression expr) {
   arrow::compute::BatchesWithSchema data = MakeRandomBatches(
       schema({field("i64", int64()), field("bool", boolean())}), num_batches, batch_size);
   ExecContext ctx(default_memory_pool(), arrow::internal::GetCpuThreadPool());
-  for (auto _ : state) {
-    state.PauseTiming();
-    ASSERT_OK_AND_ASSIGN(std::shared_ptr<arrow::compute::ExecPlan> plan,
-                         ExecPlan::Make(&ctx));
-    AsyncGenerator<util::optional<ExecBatch>> sink_gen;
-    ASSERT_OK(Declaration::Sequence(
-                  {
-                      {"source",
-                       SourceNodeOptions{data.schema,
-                                         data.gen(/*parallel=*/true, /*slow=*/false)},
-                       "custom_source_label"},
-                      {"filter",
-                       FilterNodeOptions{
-                           expr,
-                       }},
-                      {"sink", SinkNodeOptions{&sink_gen}, "custom_sink_label"},
-                  })
-                  .AddToPlan(plan.get()));
-    state.ResumeTiming();
-    ASSERT_FINISHES_OK(StartAndCollect(plan.get(), sink_gen));
-  }
-  state.counters["rows_per_second"] = benchmark::Counter(
-      static_cast<double>(state.iterations() * num_batches * batch_size),
-      benchmark::Counter::kIsRate);
-
-  state.counters["batches_per_second"] = benchmark::Counter(
-      static_cast<double>(state.iterations() * num_batches), benchmark::Counter::kIsRate);
+  std::vector<arrow::compute::Declaration> filter_node_dec = {{
+      "filter",
+      FilterNodeOptions{expr}
+  }};
+  BenchmarkNodeOverhead(
+      state,
+      ctx,
+      expr,
+      num_batches,
+      batch_size,
+      data,
+      filter_node_dec
+  );
 }
 
 static void FilterOverheadIsolated(benchmark::State& state, Expression expr) {
+  ExecContext ctx(default_memory_pool(), arrow::internal::GetCpuThreadPool());
   const int32_t batch_size = static_cast<int32_t>(state.range(0));
   const int32_t num_batches = kTotalBatchSize / batch_size;
-
   arrow::compute::BatchesWithSchema data = MakeRandomBatches(
       schema({field("i64", int64()), field("bool", boolean())}), num_batches, batch_size);
-  ExecContext ctx(default_memory_pool(), arrow::internal::GetCpuThreadPool());
-  for (auto _ : state) {
-    state.PauseTiming();
-    AsyncGenerator<util::optional<ExecBatch>> sink_gen;
-
-    ASSERT_OK_AND_ASSIGN(std::shared_ptr<arrow::compute::ExecPlan> plan,
-                         ExecPlan::Make(&ctx));
-    // Source and sink nodes have no effect on the benchmark.
-    // Used for dummy purposes as they are referenced in InputReceived and InputFinished.
-    ASSERT_OK_AND_ASSIGN(
-        arrow::compute::ExecNode * source_node,
-        MakeExecNode("source", plan.get(), {},
-                     SourceNodeOptions{data.schema, data.gen(/*parallel=*/true,
-                                                             /*slow=*/false)}));
-    ASSERT_OK_AND_ASSIGN(arrow::compute::ExecNode * project_node,
-                         MakeExecNode("filter", plan.get(), {source_node},
-                                      FilterNodeOptions{
-                                          expr,
-                                      }));
-    MakeExecNode("sink", plan.get(), {project_node}, SinkNodeOptions{&sink_gen});
-
-    std::unique_ptr<arrow::compute::TaskScheduler> scheduler = TaskScheduler::Make();
-    std::condition_variable all_tasks_finished_cv;
-    std::mutex mutex;
-    int task_group_id = scheduler->RegisterTaskGroup(
-        [&](size_t thread_id, int64_t task_id) {
-          project_node->InputReceived(source_node, data.batches[task_id]);
-          return Status::OK();
-        },
-        [&](size_t thread_id) {
-          project_node->InputFinished(source_node, static_cast<int>(data.batches.size()));
-          std::unique_lock<std::mutex> lk(mutex);
-          all_tasks_finished_cv.notify_one();
-          return Status::OK();
-        });
-    scheduler->RegisterEnd();
-    ThreadIndexer thread_indexer;
-
-    state.ResumeTiming();
-    arrow::internal::ThreadPool* thread_pool = arrow::internal::GetCpuThreadPool();
-    ASSERT_OK(scheduler->StartScheduling(
-        thread_indexer(),
-        [&](std::function<Status(size_t)> task) -> Status {
-          return thread_pool->Spawn([&, task]() {
-            size_t tid = thread_indexer();
-            ARROW_DCHECK_OK(task(tid));
-          });
-        },
-        thread_pool->GetCapacity(),
-        /*use_sync_execution=*/false));
-    std::unique_lock<std::mutex> lk(mutex);
-    ASSERT_OK(scheduler->StartTaskGroup(thread_indexer(), task_group_id, num_batches));
-    all_tasks_finished_cv.wait(lk);
-    ASSERT_TRUE(project_node->finished().is_finished());
-  }
-  state.counters["rows_per_second"] = benchmark::Counter(
-      static_cast<double>(state.iterations() * num_batches * batch_size),
-      benchmark::Counter::kIsRate);
-
-  state.counters["batches_per_second"] = benchmark::Counter(
-      static_cast<double>(state.iterations() * num_batches), benchmark::Counter::kIsRate);
+  FilterNodeOptions options = FilterNodeOptions{expr};
+  BenchmarkIsolatedNodeOverhead(
+      state,
+      ctx,
+      expr,
+      num_batches,
+      batch_size,
+      data,
+      "filter",
+      options
+  );
 }
 
 arrow::compute::Expression complex_expression =
     less(less(field_ref("i64"), literal(20)), greater(field_ref("i64"), literal(0)));
 arrow::compute::Expression simple_expression =
     less(call("negate", {field_ref("i64")}), literal(0));
-arrow::compute::Expression zero_copy_expression = is_valid((call(
-    "cast", {field_ref("i64")}, compute::CastOptions::Safe(timestamp(TimeUnit::NANO)))));
-arrow::compute::Expression ref_only_expression = less(field_ref("i64"), literal(0));
+arrow::compute::Expression ref_only_expression = field_ref("bool");
 
 void SetArgs(benchmark::internal::Benchmark* bench) {
   bench->ArgNames({"batch_size"})
@@ -160,15 +95,11 @@ BENCHMARK_CAPTURE(FilterOverheadIsolated, complex_expression, complex_expression
     ->Apply(SetArgs);
 BENCHMARK_CAPTURE(FilterOverheadIsolated, simple_expression, simple_expression)
     ->Apply(SetArgs);
-BENCHMARK_CAPTURE(FilterOverheadIsolated, zero_copy_expression, zero_copy_expression)
-    ->Apply(SetArgs);
 BENCHMARK_CAPTURE(FilterOverheadIsolated, ref_only_expression, ref_only_expression)
     ->Apply(SetArgs);
 
 BENCHMARK_CAPTURE(FilterOverhead, complex_expression, complex_expression)->Apply(SetArgs);
 BENCHMARK_CAPTURE(FilterOverhead, simple_expression, simple_expression)->Apply(SetArgs);
-BENCHMARK_CAPTURE(FilterOverhead, zero_copy_expression, zero_copy_expression)
-    ->Apply(SetArgs);
 BENCHMARK_CAPTURE(FilterOverhead, ref_only_expression, ref_only_expression)
     ->Apply(SetArgs);
 
