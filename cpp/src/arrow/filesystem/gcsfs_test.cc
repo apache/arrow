1b8fddcf3b84b5b407b56ae097f9c60392689bb1
Sutou Kouhei <kou@clear-code.com>
2022-06-27 06:07:46 +0900

    ARROW-16906: [CI][C++] Enable ARROW_GCS on MinGW workflows

--- cpp/src/arrow/filesystem/gcsfs_test.cc
+++ cpp/src/arrow/filesystem/gcsfs_test.cc
@@ -15,6 +15,24 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <algorithm>  // Missing include in boost/process
+
+// This boost/asio/io_context.hpp include is needless for no MinGW
+// build.
+//
+// This is for including boost/asio/detail/socket_types.hpp before any
+// "#include <windows.h>". boost/asio/detail/socket_types.hpp doesn't
+// work if windows.h is already included. boost/process.h ->
+// boost/process/args.hpp -> boost/process/detail/basic_cmd.hpp
+// includes windows.h. boost/process/args.hpp is included before
+// boost/process/async.h that includes
+// boost/asio/detail/socket_types.hpp implicitly is included.
+#include <boost/asio/io_context.hpp>
+// We need BOOST_USE_WINDOWS_H definition with MinGW when we use
+// boost/process.hpp. See BOOST_USE_WINDOWS_H=1 in
+// cpp/cmake_modules/ThirdpartyToolchain.cmake for details.
+#include <boost/process.hpp>
+
 #include "arrow/filesystem/gcsfs.h"
 
 #include <absl/time/time.h>
@@ -26,7 +44,6 @@
 #include <gtest/gtest.h>
 
 #include <array>
-#include <boost/process.hpp>
 #include <random>
 #include <string>
 
@@ -89,8 +106,7 @@ class GcsTestbench : public ::testing::Environment {
         continue;
       }
 
-      server_process_ = bp::child(boost::this_process::environment(), exe_path, "-m",
-                                  "testbench", "--port", port_, group_);
+      server_process_ = bp::child(exe_path, "-m", "testbench", "--port", port_, group_);
       if (server_process_.valid() && server_process_.running()) break;
       error += " (failed to start)";
       server_process_.terminate();
