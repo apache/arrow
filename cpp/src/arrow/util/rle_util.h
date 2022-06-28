66ccfde0f0c979529754d620a51a60cece253ba3
Tobias Zagorni <tobias@zagorni.eu>
2022-06-29 00:41:43 +0200

    more proper FindPhysicalOffset function

--- cpp/src/arrow/util/rle_util.h
+++ cpp/src/arrow/util/rle_util.h
@@ -1,15 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
 namespace arrow {
 namespace rle_util {
 
-#include "arrow/array/data.h"
+int64_t FindPhysicalOffset(const int64_t* accumulated_run_lengths,
+                           int64_t physical_length, int64_t logical_offset);
 
-int64_t FindPhysicalOffset(const int64_t* accumulated_run_lengths, int64_t /*physical_length*/, int64_t logical_offset) {
-  // TODO
-  int64_t physical_offset = 0;
-  while (accumulated_run_lengths[physical_offset] <= logical_offset) {
-    physical_offset++;
-  }
-  return physical_offset;
-}
-}
-}
+}  // namespace rle_util
+}  // namespace arrow
