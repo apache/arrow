be71950970ad9bc9a418077a505adf0b589921d6
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2022-06-30 15:20:40 +0100

    revert `binding_as_date()`

--- r/R/dplyr-datetime-helpers.R
+++ r/R/dplyr-datetime-helpers.R
@@ -102,6 +102,9 @@ duration_from_chunks <- function(chunks) {
 
 binding_as_date <- function(x,
                             ...) {
+  if (is.null(format) && length(tryFormats) > 1) {
+    abort("`as.Date()` with multiple `tryFormats` is not supported in Arrow")
+  }
 
   if (call_binding("is.Date", x)) {
     return(x)
@@ -126,8 +129,7 @@ binding_as_date_character <- function(x,
   build_expr("strptime", x, options = list(format = format, unit = 0L))
 }
 
-binding_as_date_numeric <- function(x,
-                                    origin = "1970-01-01") {
+binding_as_date_numeric <- function(x, origin = "1970-01-01") {
 
   # Arrow does not support direct casting from double to date32(), but for
   # integer-like values we can go via int32()
