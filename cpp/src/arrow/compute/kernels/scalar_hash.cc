945d23737375fee96ece723595afaa7ceace4e3b
Aldrin M <octalene.dev@pm.me>
2022-06-30 14:14:39 -0700

    [ARROW-8991]: updated scalar_hash implementation
    The kernel implementation has been updated to be fully functional, using
    the Hashing32::HashMultiColumn function. HashMultiColumn is used because
    inputs to FastHash32 are expected to be arrays or scalars, and we would
    like to accommodate nested arrays (arrays of list type). Using
    HashBatch is a slightly simpler interface, but requires us to
    potentially wrap the input in an ExecBatch only to be unwrapped and
    extracted into a KeyColumnArray immediately. This implementation is not
    yet benchmarked, and so likely has room for improvement.

--- cpp/src/arrow/compute/kernels/scalar_hash.cc
+++ cpp/src/arrow/compute/kernels/scalar_hash.cc
@@ -20,114 +20,126 @@
  * @brief Element-wise (scalar) kernels for hashing values.
  */
 
-#include "arrow/result.h"
+#include <algorithm>
+#include <iostream>
+
+#include "arrow/array/array_base.h"
+#include "arrow/array/builder_primitive.h"
+#include "arrow/compute/exec/key_hash.h"
+#include "arrow/compute/exec/util.h"
 #include "arrow/compute/kernels/common.h"
 #include "arrow/compute/light_array.h"
-#include "arrow/compute/exec/util.h"
-#include "arrow/compute/exec/key_hash.h"
-
-/*
-#include "arrow/array/util.h"
-#include "arrow/array/array_base.h"
-#include "arrow/array/array_dict.h"
-#include "arrow/array/array_nested.h"
-#include "arrow/array/builder_primitive.h"
-#include "arrow/array/builder_nested.h"
-#include "arrow/array/concatenate.h"
-#include "arrow/array/dict_internal.h"
-#include "arrow/compute/api_scalar.h"
-#include "arrow/util/make_unique.h"
-*/
+#include "arrow/result.h"
 
 
 // NOTES:
 // * `KeyColumnArray` comes from light_array.h
 //   * Should be replaceable with `ExecSpan`
 
-namespace arrow::compute::internal {
+namespace arrow {
+  namespace compute {
+    namespace internal {
 
-  // Define symbols visible within `arrow::compute::internal` in this file;
-  // these symbols are not visible outside of this file.
-  namespace {
+      // Define symbols visible within `arrow::compute::internal` in this file;
+      // these symbols are not visible outside of this file.
+      namespace {
 
-    // Documentation for `FastHash32` function:
-    //  1. Summary
-    //  2. Description
-    //  3. Argument Names
-    const FunctionDoc fast_hash_32_doc {
-       "Construct a hash for every element of the input argument"
-      ,(
-         "`hash_input` may be a scalar value or an array.\n"
-         "A hash result is emitted for each non-null element in `hash_input`.\n"
-         "A null value emits a null; null elements in an array produce null results."
-       )
-      ,{ "hash_input" }
-    };
+        // ------------------------------
+        // Function documentation
+        const FunctionDoc fast_hash_32_doc {
+           "Construct a hash for every element of the input argument"
+          ,(
+             "This function uses an xxHash-like algorithm.\n"
+             "Hash results are 32-bit and emitted for each valid row.\n"
+             "Null (or invalid) rows emit a null in the output."
+           )
+          ,{ "hash_input" }
+        };
 
-    // ------------------------------
-    // For scalar inputs
 
-    struct FastHash32Scalar {
-      template <typename ArrayType>
-      UInt32Array
-      Exec(KernelContext *ctx, const Datum& input_array, ExecResult *out) {
-        ARROW_DCHECK(input_array.is_array());
+        // ------------------------------
+        // Kernel implementations
+        struct FastHash32Scalar {
+          static Status
+          Exec(KernelContext *ctx, const ExecSpan &input_arg, ExecResult *out) {
+            if (input_arg.num_values() != 1 or not input_arg[0].is_array()) {
+              return Status::Invalid("FastHash32 currently supports a single array input");
+            }
 
-        LightContext    light_ctx;
-        TempVectorStack mem_stack;
-        std::vector<KeyColumnArray> col_arrays;
-        std::vector<uint32_t>       hash_results;
+            // Initialize stack-based memory allocator with an allocator and memory size
+            util::TempVectorStack stack_memallocator;
+            ARROW_RETURN_NOT_OK(
+              stack_memallocator.Init(
+                 ctx->exec_context()->memory_pool()
+                ,max_bitwidth * max_batchsize
+              )
+            );
 
-        // Initialize LightContext
-        light_ctx.hardware_flags = hardware_flags;
-        light_ctx.stack          = mem_stack;
+            // Prepare input data structure for propagation to hash function
+            ArraySpan hash_input = input_arg[0].array;
+            ARROW_ASSIGN_OR_RAISE(
+               KeyColumnArray input_keycol
+              ,ColumnArrayFromArrayData( hash_input.ToArrayData()
+                                        ,default_rstart
+                                        ,hash_input.length)
+            );
 
-				// TODO: replace this with correct type inspection and make robust
-				// CalculateTempStackSize<StringType, StringArray>()
-				auto col_bufsize = 16384;
-        auto init_status = mem_stack.Init(ctx->exec_context()->memory_pool(), col_bufsize);
+            // Call hashing function
+            std::vector<uint32_t> hash_results(hash_input.length);
+            LightContext          hash_ctx;
 
-        // TODO: generalize for nested arrays
-        col_arrays.resize(1);
-        ARROW_ASSIGN_OR_RAISE(
-           col_arrays[0]
-          ,ColumnArrayFromArrayData(input_array.array(), start_row, num_rows)
-        );
+            hash_ctx.hardware_flags = ctx->exec_context()->cpu_info()->hardware_flags();
+            hash_ctx.stack          = &stack_memallocator;
+            Hashing32::HashMultiColumn({ input_keycol }, &hash_ctx, hash_results.data());
 
-        // Use HashMultiColumn to compute hash for each col_array and combine them
-        HashMultiColumn(col_arrays, &light_ctx, hash_results.data());
+            // Prepare results of hash function for kernel output argument
+            arrow::UInt32Builder builder;
+            ARROW_RETURN_NOT_OK(builder.Reserve(hash_results.size()));
+            ARROW_RETURN_NOT_OK(builder.AppendValues(hash_results));
+            ARROW_ASSIGN_OR_RAISE(auto result_array, builder.Finish());
 
-        // TODO: convert to an Array
-        return hash_results;
+            out->value = result_array->data();
+            return Status::OK();
+          }
+
+          // This 96 represents the most bits *per row* that the Hashing32 and Hashing64
+          // algorithms allocate from the provided `TempVectorStack`
+          static constexpr uint32_t max_bitwidth   = 96;
+          static constexpr uint32_t max_batchsize  = util::MiniBatch::kMiniBatchLength;
+          static constexpr int64_t  default_rstart = 0;
+        };
+
+        // ------------------------------
+        // Function construction and kernel registration
+        std::shared_ptr<ScalarFunction>
+        RegisterKernelsFastHash32() {
+          // Create function instance
+          auto fn_fast_hash_32 = std::make_shared<ScalarFunction>("fast_hash_32",
+                                                                  Arity::Unary(),
+                                                                  fast_hash_32_doc);
+
+          // Associate kernel with function
+          for (auto &simple_inputtype : PrimitiveTypes()) {
+            DCHECK_OK(
+              fn_fast_hash_32->AddKernel({ InputType(simple_inputtype) },
+                                         OutputType(uint32())  ,
+                                         FastHash32Scalar::Exec)
+            );
+          }
+
+          // Return function to be registered
+          return fn_fast_hash_32;
+        }
+
+
+      } // anonymous namespace (arrow::compute::internal)
+
+      void
+      RegisterScalarHash(FunctionRegistry* registry) {
+        auto fn_scalarhash = RegisterKernelsFastHash32();
+        DCHECK_OK(registry->AddFunction(std::move(fn_scalarhash)));
       }
-    };
 
-  } // anonymous namespace
-
-  void
-  RegisterScalarHash(FunctionRegistry* registry) {
-    // >> Construct instance of compute function
-    auto fn_fast_hash_32 = std::make_shared<ScalarFunction>(
-       "fast_hash_32"   // function name
-      ,Arity::Unary()   // Arity of function (how many parameters)
-      ,fast_hash_32_doc // function documentation
-    );
-
-    // >> Register kernel implementations with compute function instance
-    // TODO: we can probably add a kernel for each type, that also knows how to
-    //       calculate necessary Stack-based memory needed for the type
-    //  |> Input is (scalar)
-    DCHECK_OK(
-      fn_fast_hash_32->AddKernel(
-         { InputType(Type::ARRAY) }    // Input types
-        ,UInt32Array                   // Output type
-        ,FastHash32Scalar<StringArray> // Exec function
-        ,                              // Init function (default: NULLPTR)
-      )
-    );
-
-    // >> Register compute function with FunctionRegistry
-    DCHECK_OK(registry->AddFunction(std::move(fn_fast_hash_32)));
-  }
-
-}  // namespace arrow::compute::internal
+    } // namespace arrow::compute::internal
+  } // namespace arrow::compute
+}  // namespace arrow
