d8cbea38118117d042aa742f44c9ce4e51d461df
Alenka Frim <frim.alenka@gmail.com>
2022-07-06 08:33:51 +0200

    Linter corrections

--- python/setup.py
+++ python/setup.py
@@ -444,13 +444,16 @@ class build_ext(_build_ext):
                 shutil.move(pjoin(build_prefix, 'include'),
                             pjoin(build_lib, 'pyarrow'))
 
-                # We need to add the C PyArrow include folder, that was also deleted
+                # We need to, again, add the C PyArrow include folder
                 build_cpyarrow_include = pjoin(saved_cwd, 'build/dist/include')
-                if not os.path.isdir(pjoin(build_cpyarrow_include, "arrow", "python")):
+                if not os.path.isdir(
+                        pjoin(build_cpyarrow_include, "arrow", "python")):
                     self.mkpath(
                         pjoin(build_cpyarrow_include, "arrow", "python"))
-                shutil.move(pjoin(build_cpyarrow_include, "arrow", "python"), pjoin(
-                    build_lib, "pyarrow", "include", "arrow", "python"))
+                shutil.move(pjoin(build_cpyarrow_include, "arrow", "python"),
+                            pjoin(
+                                build_lib, "pyarrow", "include",
+                                "arrow", "python"))
 
             # Move the built C-extension to the place expected by the Python
             # build
