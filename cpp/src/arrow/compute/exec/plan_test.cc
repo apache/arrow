431609aec1acaf33519846a7a26e569c6f5f5945
Aldrin M <octalene.dev@pm.me>
2022-07-07 14:32:29 -0700

    [ARROW-16904]: added test to plan_test.cc
    This test exercises the bug found in ARROW-16904, by creating a
    ScalarAggregateNode for the "min_max" function. Previously, there was no
    unit test for scalar aggregate nodes.

--- cpp/src/arrow/compute/exec/plan_test.cc
+++ cpp/src/arrow/compute/exec/plan_test.cc
@@ -933,6 +933,57 @@ TEST(ExecPlanExecution, SourceGroupedSum) {
   }
 }
 
+TEST(ExecPlanExecution, SourceMinMaxScalar) {
+  for (bool parallel : { false, true }) {
+    SCOPED_TRACE(parallel ? "parallel/merged" : "serial");
+
+    auto input           = MakeGroupableBatches(/*multiplicity=*/parallel ? 100 : 1);
+    auto minmax_opts     = std::make_shared<ScalarAggregateOptions>();
+    auto expected_result = ExecBatch::Make({
+      *StructScalar::Make(
+          ScalarVector {
+             ScalarFromJSON(int32(), R"(-8)")
+            ,ScalarFromJSON(int32(), R"(12)")
+          }
+         ,{ "min", "max" }
+       )
+    });
+
+    ASSERT_OK_AND_ASSIGN(auto plan, ExecPlan::Make());
+    AsyncGenerator<util::optional<ExecBatch>> sink_gen;
+
+    // NOTE: Test `ScalarAggregateNode` by omitting `keys` attribute
+    ASSERT_OK(
+      Declaration::Sequence({
+         {
+             "source"
+            ,SourceNodeOptions {
+                input.schema
+               ,input.gen(parallel, /*slow=*/false)
+             }
+         }
+        ,{
+            "aggregate"
+           ,AggregateNodeOptions {
+               /*aggregates=*/{
+                 { "min_max", std::move(minmax_opts), "i32", "min_max" }
+               }
+              ,/*keys=*/{}
+            }
+         }
+        ,{ "sink", SinkNodeOptions{ &sink_gen } }
+      }).AddToPlan(plan.get())
+    );
+
+    ASSERT_THAT(
+       StartAndCollect(plan.get(), sink_gen)
+      ,Finishes(ResultWith(
+         UnorderedElementsAreArray({ *expected_result })
+       ))
+    );
+  }
+}
+
 TEST(ExecPlanExecution, NestedSourceFilter) {
   for (bool parallel : {false, true}) {
     SCOPED_TRACE(parallel ? "parallel/merged" : "serial");
