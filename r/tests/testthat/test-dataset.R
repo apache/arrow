f48390ece1c632c9ba905c361e3d8fb41ced829e
Aldrin M <octalene.dev@pm.me>
2022-07-07 14:16:14 -0700

    [ARROW-16904]: added R test
    This R test captures the code from ARROW-16904 that reproduces this bug.
    It uses a scalar aggregate, min, on a dataset (1 column) that produces
    several exec batches.

--- r/tests/testthat/test-dataset.R
+++ r/tests/testthat/test-dataset.R
@@ -618,6 +618,31 @@ test_that("UnionDataset handles InMemoryDatasets", {
   expect_equal(actual, expected)
 })
 
+test_that("scalar aggregates with many batches", {
+  test_data             <- data.frame(val=1:1e7)
+  expected_result_distr <- (
+    sapply(1:100, function (iter_ndx) {
+      test_data                              %>%
+        dplyr::summarise(min_val = min(val)) %>%
+        dplyr::collect()                     %>%
+        dplyr::pull(min_val)
+    }) %>% table()
+  )
+
+  ds_tmpfile <- tempfile('test-aggregate', fileext='.parquet')
+  arrow::write_parquet(test_data, ds_tmpfile)
+  actual_result_distr <- (
+    sapply(1:100, function (iter_ndx) {
+      arrow::open_dataset(ds_tmpfile)        %>%
+        dplyr::summarise(min_val = min(val)) %>%
+        dplyr::collect()                     %>%
+        dplyr::pull(min_val)
+    }) %>% table()
+  )
+
+  expect_equal(actual_result_distr, expected_result_distr)
+})
+
 test_that("map_batches", {
   ds <- open_dataset(dataset_dir, partitioning = "part")
 
