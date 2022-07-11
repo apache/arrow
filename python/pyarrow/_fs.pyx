5f6aecf2e627e0d78ec1c651a40ea0acbc9eb6d6
Alenka Frim <frim.alenka@gmail.com>
2022-07-11 12:14:54 +0200

    Fix doctest CI error part 2

--- python/pyarrow/_fs.pyx
+++ python/pyarrow/_fs.pyx
@@ -385,7 +385,7 @@ cdef class FileSelector(_Weakrefable):
 
     >>> selector_2 = fs.FileSelector('/tmp/alphabet')
     >>> local.get_file_info(selector_2)
-    [<FileInfo for '/tmp/alphabet/aeiou': type=FileType.Directory>, <FileInfo for '/tmp/alphabet/fileinfo.dat'...
+    [<FileInfo for '/tmp/alphabet/fileinfo.dat': type=FileType.File, size=4>, <FileInfo for '/tmp/alphabet/aeiou': ...
 
     Return empty selection if the directory doesn't exist:
 
