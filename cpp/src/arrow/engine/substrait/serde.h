6d61c8580df033e6bff00487848ff91a558b93fe
Jeroen van Straten <jeroen.van.straten@gmail.com>
2022-07-12 21:31:18 +0200

    Update brief descriptions to match conventions

--- cpp/src/arrow/engine/substrait/serde.h
+++ cpp/src/arrow/engine/substrait/serde.h
@@ -39,7 +39,7 @@ namespace engine {
 /// each top-level Substrait relation when deserializing a Substrait Plan.
 using ConsumerFactory = std::function<std::shared_ptr<compute::SinkNodeConsumer>()>;
 
-/// \brief Deserializes a Substrait Plan message to a list of ExecNode declarations.
+/// \brief Deserialize a Substrait Plan message to a list of ExecNode declarations.
 ///
 /// The output of each top-level Substrait relation will be sent to a caller supplied
 /// consumer function provided by consumer_factory.
@@ -57,7 +57,7 @@ ARROW_ENGINE_EXPORT Result<std::vector<compute::Declaration>> DeserializePlans(
     const Buffer& buf, const ConsumerFactory& consumer_factory,
     const ExtensionIdRegistry* registry = NULLPTR, ExtensionSet* ext_set_out = NULLPTR);
 
-/// \brief Deserializes a single-relation Substrait Plan message to an execution plan.
+/// \brief Deserialize a single-relation Substrait Plan message to an execution plan.
 ///
 /// The output of each top-level Substrait relation will be sent to the given consumer.
 ///
@@ -78,7 +78,7 @@ Result<compute::ExecPlan> DeserializePlan(
 /// produced by each top-level Substrait relation when deserializing a Substrait Plan.
 using WriteOptionsFactory = std::function<std::shared_ptr<dataset::WriteNodeOptions>()>;
 
-/// \brief Deserializes a Substrait Plan message to a list of ExecNode declarations.
+/// \brief Deserialize a Substrait Plan message to a list of ExecNode declarations.
 ///
 /// The output of each top-level Substrait relation will be written to a filesystem.
 /// `write_options_factory` can be used to control write behavior.
@@ -96,7 +96,7 @@ ARROW_ENGINE_EXPORT Result<std::vector<compute::Declaration>> DeserializePlans(
     const Buffer& buf, const WriteOptionsFactory& write_options_factory,
     const ExtensionIdRegistry* registry = NULLPTR, ExtensionSet* ext_set_out = NULLPTR);
 
-/// \brief Deserializes a single-relation Substrait Plan message to an execution plan.
+/// \brief Deserialize a single-relation Substrait Plan message to an execution plan.
 ///
 /// The output of the single Substrait relation will be written to a filesystem.
 /// `write_options_factory` can be used to control write behavior.
@@ -114,7 +114,7 @@ ARROW_ENGINE_EXPORT Result<compute::ExecPlan> DeserializePlan(
     const Buffer& buf, const std::shared_ptr<dataset::WriteNodeOptions>& write_options,
     const ExtensionIdRegistry* registry = NULLPTR, ExtensionSet* ext_set_out = NULLPTR);
 
-/// \brief Deserializes a Substrait Type message to the corresponding Arrow type.
+/// \brief Deserialize a Substrait Type message to the corresponding Arrow type.
 ///
 /// \param[in] buf a buffer containing the protobuf serialization of a Substrait Type
 /// message.
@@ -125,7 +125,7 @@ ARROW_ENGINE_EXPORT
 Result<std::shared_ptr<DataType>> DeserializeType(const Buffer& buf,
                                                   const ExtensionSet& ext_set);
 
-/// \brief Serializes an Arrow type to a Substrait Type message.
+/// \brief Serialize an Arrow type to a Substrait Type message.
 ///
 /// \param[in] type the Arrow data type to serialize.
 /// \param[in,out] ext_set the extension mapping to use; may be updated to add a
@@ -136,7 +136,7 @@ ARROW_ENGINE_EXPORT
 Result<std::shared_ptr<Buffer>> SerializeType(const DataType& type,
                                               ExtensionSet* ext_set);
 
-/// \brief Deserializes a Substrait NamedStruct message to an Arrow schema.
+/// \brief Deserialize a Substrait NamedStruct message to an Arrow schema.
 ///
 /// \param[in] buf a buffer containing the protobuf serialization of a Substrait
 /// NamedStruct message.
@@ -147,7 +147,7 @@ ARROW_ENGINE_EXPORT
 Result<std::shared_ptr<Schema>> DeserializeSchema(const Buffer& buf,
                                                   const ExtensionSet& ext_set);
 
-/// \brief Serializes an Arrow schema to a Substrait NamedStruct message.
+/// \brief Serialize an Arrow schema to a Substrait NamedStruct message.
 ///
 /// \param[in] schema the Arrow schema to serialize.
 /// \param[in,out] ext_set the extension mapping to use; may be updated to add
@@ -158,7 +158,7 @@ ARROW_ENGINE_EXPORT
 Result<std::shared_ptr<Buffer>> SerializeSchema(const Schema& schema,
                                                 ExtensionSet* ext_set);
 
-/// \brief Deserializes a Substrait Expression message to a compute expression.
+/// \brief Deserialize a Substrait Expression message to a compute expression.
 ///
 /// \param[in] buf a buffer containing the protobuf serialization of a Substrait
 /// Expression message.
@@ -169,7 +169,7 @@ ARROW_ENGINE_EXPORT
 Result<compute::Expression> DeserializeExpression(const Buffer& buf,
                                                   const ExtensionSet& ext_set);
 
-/// \brief Serializes an Arrow compute expression to a Substrait Expression message.
+/// \brief Serialize an Arrow compute expression to a Substrait Expression message.
 ///
 /// \param[in] expr the Arrow compute expression to serialize.
 /// \param[in,out] ext_set the extension mapping to use; may be updated to add
@@ -180,7 +180,7 @@ ARROW_ENGINE_EXPORT
 Result<std::shared_ptr<Buffer>> SerializeExpression(const compute::Expression& expr,
                                                     ExtensionSet* ext_set);
 
-/// \brief Deserializes a Substrait Rel (relation) message to an ExecNode declaration.
+/// \brief Deserialize a Substrait Rel (relation) message to an ExecNode declaration.
 ///
 /// \param[in] buf a buffer containing the protobuf serialization of a Substrait
 /// Rel message.
@@ -192,7 +192,7 @@ ARROW_ENGINE_EXPORT Result<compute::Declaration> DeserializeRelation(
 
 namespace internal {
 
-/// \brief Checks whether two protobuf serializations of a particular Substrait message
+/// \brief Check whether two protobuf serializations of a particular Substrait message
 /// type are equivalent.
 ///
 /// Note that a binary comparison of the two buffers is insufficient. One reason for this
@@ -206,8 +206,8 @@ ARROW_ENGINE_EXPORT
 Status CheckMessagesEquivalent(util::string_view message_name, const Buffer& l_buf,
                                const Buffer& r_buf);
 
-/// \brief Utility function to convert a JSON serialization of a Substrait message to
-/// its binary serialization.
+/// \brief Convert a JSON serialization of a Substrait message to its binary
+/// serialization.
 ///
 /// \param[in] type_name the name of the Substrait message type to convert.
 /// \param[in] json the JSON string to convert.
@@ -216,8 +216,7 @@ ARROW_ENGINE_EXPORT
 Result<std::shared_ptr<Buffer>> SubstraitFromJSON(util::string_view type_name,
                                                   util::string_view json);
 
-/// \brief Utility function to convert a binary protobuf serialization of a Substrait
-/// message to JSON.
+/// \brief Convert a binary protobuf serialization of a Substrait message to JSON.
 ///
 /// \param[in] type_name the name of the Substrait message type to convert.
 /// \param[in] buf the buffer containing the binary protobuf serialization of the
