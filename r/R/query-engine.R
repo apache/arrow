d887d42227fe6c763bec916b0a7a4f0c83b13b66
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2022-07-18 12:06:27 +0100

    comment

--- r/R/query-engine.R
+++ r/R/query-engine.R
@@ -261,8 +261,8 @@ ExecPlan <- R6Class("ExecPlan",
     },
     ToString = function() ExecPlan_ToString(self),
     # SinkNodes (involved in arrange and/or head/tail operations) are created in
-    # ExecPlan_run, so we take a similar approach to expose them before calling
-    # the print method
+    # ExecPlan_run and are not captured by the regulat print method. We take a
+    # similar approach to expose them before calling the print method.
     ToStringWithSink = function(node) {
       assert_is(node, "ExecNode")
 
