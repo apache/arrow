8b4cf6de6bf3429803e161134951fdb33bf001c3
Dewey Dunnington <dewey@fishandwhistle.net>
2022-07-19 12:22:03 -0300

    fix more map_batches tests

--- r/tests/testthat/test-dataset-write.R
+++ r/tests/testthat/test-dataset-write.R
@@ -703,6 +703,7 @@ test_that("Dataset min_rows_per_group", {
 
   row_group_sizes <- ds %>%
     map_batches(~ record_batch(nrows = .$num_rows)) %>%
+    (function(x) x$read_table()) %>%
     pull(nrows)
   index <- 1
 
@@ -741,6 +742,7 @@ test_that("Dataset write max rows per group", {
   ds <- open_dataset(file_path)
   row_group_sizes <- ds %>%
     map_batches(~ record_batch(nrows = .$num_rows)) %>%
+    (function(x) x$read_table()) %>%
     pull(nrows) %>%
     sort()
 
