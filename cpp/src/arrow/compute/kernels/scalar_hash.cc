3feec6419d165ca44c0ec0d3485645adf89ffa9f
Aldrin M <octalene.dev@pm.me>
2022-07-21 16:42:28 -0700

    [ARROW-8991]: Added naive wrapper around hashing.h
    Added XxHash as a naive scalar hash compute function backed by hash
    functions in util/hashing.h. This was partially to see what it would
    look like and partially to approximate the overhead of invoking these
    functions through the compute function framework.

--- cpp/src/arrow/compute/kernels/scalar_hash.cc
+++ cpp/src/arrow/compute/kernels/scalar_hash.cc
@@ -31,6 +31,8 @@
 #include "arrow/compute/light_array.h"
 #include "arrow/result.h"
 
+#include "arrow/util/hashing.h"
+
 // NOTES:
 // * `KeyColumnArray` comes from light_array.h
 //   * Should be replaceable with `ExecSpan`
@@ -42,6 +44,8 @@ namespace internal {
 // Define symbols visible within `arrow::compute::internal` in this file;
 // these symbols are not visible outside of this file.
 namespace {
+
+using arrow::internal::ScalarHelper;
 
 // ------------------------------
 // Function documentation
@@ -61,18 +65,31 @@ const FunctionDoc fast_hash_64_doc{
      "Null (or invalid) rows emit a null in the output."),
     {"hash_input"}};
 
+const FunctionDoc xx_hash_doc{
+    "Construct a hash for every element of the input argument",
+    ("An element-wise function that uses the xxHash algorithm.\n"
+     "Hash results are 64-bit and emitted for each valid row.\n"
+     "Null (or invalid) rows emit a null in the output."),
+    {"hash_input"}};
+
 // ------------------------------
 // Kernel implementations
 struct FastHash32Scalar {
   static Status Exec(KernelContext* ctx, const ExecSpan& input_arg, ExecResult* out) {
-    if (input_arg.num_values() != 1 or not input_arg[0].is_array()) {
+    if (input_arg.num_values() != 1 or !input_arg[0].is_array()) {
       return Status::Invalid("FastHash32 currently supports a single array input");
+    }
+
+    auto exec_ctx = default_exec_context();
+    if (ctx && ctx->exec_context()) {
+      exec_ctx = ctx->exec_context();
     }
 
     // Initialize stack-based memory allocator with an allocator and memory size
     util::TempVectorStack stack_memallocator;
-    ARROW_RETURN_NOT_OK(stack_memallocator.Init(ctx->exec_context()->memory_pool(),
-                                                max_bitwidth * max_batchsize));
+    ARROW_RETURN_NOT_OK(
+        stack_memallocator.Init(exec_ctx->memory_pool(),
+                                3 * sizeof(int32_t) * util::MiniBatch::kMiniBatchLength));
 
     // Prepare input data structure for propagation to hash function
     ArraySpan hash_input = input_arg[0].array;
@@ -81,40 +98,41 @@ struct FastHash32Scalar {
                                                    default_rstart, hash_input.length));
 
     // Call hashing function
-    std::vector<uint32_t> hash_results(hash_input.length);
     LightContext hash_ctx;
+    ARROW_ASSIGN_OR_RAISE(std::unique_ptr<Buffer> hash_buffer,
+                          AllocateBuffer(hash_input.length * sizeof(uint32_t)));
 
-    hash_ctx.hardware_flags = ctx->exec_context()->cpu_info()->hardware_flags();
+    hash_ctx.hardware_flags = exec_ctx->cpu_info()->hardware_flags();
     hash_ctx.stack = &stack_memallocator;
-    Hashing32::HashMultiColumn({input_keycol}, &hash_ctx, hash_results.data());
+    Hashing32::HashMultiColumn({input_keycol}, &hash_ctx,
+                               reinterpret_cast<uint32_t*>(hash_buffer->mutable_data()));
 
-    // Prepare results of hash function for kernel output argument
-    arrow::UInt32Builder builder;
-    ARROW_RETURN_NOT_OK(builder.Reserve(hash_results.size()));
-    ARROW_RETURN_NOT_OK(builder.AppendValues(hash_results));
-    ARROW_ASSIGN_OR_RAISE(auto result_array, builder.Finish());
-
-    out->value = ArraySpan{*(result_array->data())};
+    auto result_buffer = std::shared_ptr<Buffer>(std::move(hash_buffer));
+    ArraySpan hash_result{uint64().get(), hash_input.length};
+    hash_result.SetBuffer(1, result_buffer);
+    out->value = hash_result;
     return Status::OK();
   }
 
-  // This 96 represents the most bits *per row* that the Hashing32 and Hashing64
-  // algorithms allocate from the provided `TempVectorStack`
-  static constexpr uint32_t max_bitwidth = 96;
-  static constexpr uint32_t max_batchsize = util::MiniBatch::kMiniBatchLength;
   static constexpr int64_t default_rstart = 0;
 };
 
 struct FastHash64Scalar {
   static Status Exec(KernelContext* ctx, const ExecSpan& input_arg, ExecResult* out) {
-    if (input_arg.num_values() != 1 or not input_arg[0].is_array()) {
+    if (input_arg.num_values() != 1 or !input_arg[0].is_array()) {
       return Status::Invalid("FastHash64 currently supports a single array input");
+    }
+
+    auto exec_ctx = default_exec_context();
+    if (ctx && ctx->exec_context()) {
+      exec_ctx = ctx->exec_context();
     }
 
     // Initialize stack-based memory allocator with an allocator and memory size
     util::TempVectorStack stack_memallocator;
-    ARROW_RETURN_NOT_OK(stack_memallocator.Init(ctx->exec_context()->memory_pool(),
-                                                max_bitwidth * max_batchsize));
+    ARROW_RETURN_NOT_OK(
+        stack_memallocator.Init(exec_ctx->memory_pool(),
+                                3 * sizeof(int32_t) * util::MiniBatch::kMiniBatchLength));
 
     // Prepare input data structure for propagation to hash function
     ArraySpan hash_input = input_arg[0].array;
@@ -123,28 +141,51 @@ struct FastHash64Scalar {
                                                    default_rstart, hash_input.length));
 
     // Call hashing function
-    std::vector<uint64_t> hash_results(hash_input.length);
     LightContext hash_ctx;
+    ARROW_ASSIGN_OR_RAISE(std::unique_ptr<Buffer> hash_buffer,
+                          AllocateBuffer(hash_input.length * sizeof(uint64_t)));
 
-    hash_ctx.hardware_flags = ctx->exec_context()->cpu_info()->hardware_flags();
+    hash_ctx.hardware_flags = exec_ctx->cpu_info()->hardware_flags();
     hash_ctx.stack = &stack_memallocator;
-    Hashing64::HashMultiColumn({input_keycol}, &hash_ctx, hash_results.data());
+    Hashing64::HashMultiColumn({input_keycol}, &hash_ctx,
+                               reinterpret_cast<uint64_t*>(hash_buffer->mutable_data()));
 
-    // Prepare results of hash function for kernel output argument
-    arrow::UInt64Builder builder;
-    ARROW_RETURN_NOT_OK(builder.Reserve(hash_results.size()));
-    ARROW_RETURN_NOT_OK(builder.AppendValues(hash_results));
-    ARROW_ASSIGN_OR_RAISE(auto result_array, builder.Finish());
-
-    out->value = ArraySpan{*(result_array->data())};
+    auto result_buffer = std::shared_ptr<Buffer>(std::move(hash_buffer));
+    ArraySpan hash_result{uint64().get(), hash_input.length};
+    hash_result.SetBuffer(1, result_buffer);
+    out->value = hash_result;
     return Status::OK();
   }
 
-  // This 96 represents the most bits *per row* that the Hashing32 and Hashing64
-  // algorithms allocate from the provided `TempVectorStack`
-  static constexpr uint64_t max_bitwidth = 96;
-  static constexpr uint32_t max_batchsize = util::MiniBatch::kMiniBatchLength;
   static constexpr int64_t default_rstart = 0;
+};
+
+struct XxHashScalar {
+  static Status Exec(KernelContext* ctx, const ExecSpan& input_arg, ExecResult* out) {
+    if (input_arg.num_values() != 1 or !input_arg[0].is_array()) {
+      return Status::Invalid("FastHash64 currently supports a single array input");
+    }
+
+    // Prepare input data structure for propagation to hash function
+    const uint64_t* hash_inputs = input_arg[0].array.GetValues<uint64_t>(1, 0);
+    const auto input_len = input_arg[0].array.length;
+
+    ARROW_ASSIGN_OR_RAISE(std::unique_ptr<Buffer> hash_buffer,
+                          AllocateBuffer(input_len * sizeof(uint64_t)));
+
+    // Call hashing function
+    uint64_t* hash_results = reinterpret_cast<uint64_t*>(hash_buffer->mutable_data());
+    for (int val_ndx = 0; val_ndx < input_len; ++val_ndx) {
+      hash_results[val_ndx] = ScalarHelper<int64_t, 0>::ComputeHash(hash_inputs[val_ndx]);
+      // total += ScalarHelper<int64_t, 1>::ComputeHash(v);
+    }
+
+    auto result_buffer = std::shared_ptr<Buffer>(std::move(hash_buffer));
+    ArraySpan hash_result{uint64().get(), input_len};
+    hash_result.SetBuffer(1, result_buffer);
+    out->value = hash_result;
+    return Status::OK();
+  }
 };
 
 // ------------------------------
@@ -179,6 +220,21 @@ std::shared_ptr<ScalarFunction> RegisterKernelsFastHash64() {
   return fn_fast_hash_64;
 }
 
+std::shared_ptr<ScalarFunction> RegisterKernelsXxHash() {
+  // Create function instance
+  auto fn_xx_hash =
+      std::make_shared<ScalarFunction>("xx_hash", Arity::Unary(), xx_hash_doc);
+
+  // Associate kernel with function
+  for (auto& simple_inputtype : PrimitiveTypes()) {
+    DCHECK_OK(fn_xx_hash->AddKernel({InputType(simple_inputtype)}, OutputType(uint64()),
+                                    FastHash64Scalar::Exec));
+  }
+
+  // Return function to be registered
+  return fn_xx_hash;
+}
+
 }  // namespace
 
 void RegisterScalarHash(FunctionRegistry* registry) {
@@ -187,6 +243,9 @@ void RegisterScalarHash(FunctionRegistry* registry) {
 
   auto fn_scalarhash64 = RegisterKernelsFastHash64();
   DCHECK_OK(registry->AddFunction(std::move(fn_scalarhash64)));
+
+  auto fn_scalarhashxx = RegisterKernelsXxHash();
+  DCHECK_OK(registry->AddFunction(std::move(fn_scalarhashxx)));
 }
 
 }  // namespace internal
