57cd0d6b7a3a64a495ee2c03ad10aaece9ecc0a9
Alenka Frim <frim.alenka@gmail.com>
2022-07-21 13:53:48 +0200

    Add a fix for manylinux wheels - thank you Raul

--- ci/scripts/python_wheel_manylinux_build.sh
+++ ci/scripts/python_wheel_manylinux_build.sh
@@ -151,6 +151,7 @@ export PYARROW_WITH_PARQUET_ENCRYPTION=${PARQUET_REQUIRE_ENCRYPTION}
 export PYARROW_WITH_PLASMA=${ARROW_PLASMA}
 export PYARROW_WITH_SUBSTRAIT=${ARROW_SUBSTRAIT}
 export PYARROW_WITH_S3=${ARROW_S3}
+export ARROW_HOME=/tmp/arrow-dist
 # PyArrow build configuration
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/tmp/arrow-dist/lib/pkgconfig
 
