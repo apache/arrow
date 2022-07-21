f746fb799c8d91bfb3461472e195977459c6befd
Tobias Zagorni <tobias@zagorni.eu>
2022-07-21 23:22:54 +0200

    rle_util: add AddArtificialOffsetInChildArray for testing

--- cpp/src/arrow/util/rle_util.h
+++ cpp/src/arrow/util/rle_util.h
@@ -2,11 +2,18 @@
 
 #include <cstdint>
 
+#include "arrow/util/logging.h"
+
+#include "arrow/array/data.h"
+
 namespace arrow {
 namespace rle_util {
 
 int64_t FindPhysicalOffset(const int32_t* accumulated_run_lengths,
                            int64_t physical_length, int64_t logical_offset);
+
+// TODO: this may fit better into some testing header
+void AddArtificialOffsetInChildArray(ArrayData* array, int64_t offset);
 
 }  // namespace rle_util
 }  // namespace arrow
