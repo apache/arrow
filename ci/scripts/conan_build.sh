517b017e47dc86afed368d77b733e1ad0e82bfd2
Sutou Kouhei <kou@clear-code.com>
2022-07-25 14:59:54 +0900

    ARROW-17194: [CI][Conan] Enable glog

--- ci/scripts/conan_build.sh
+++ ci/scripts/conan_build.sh
@@ -34,6 +34,9 @@ fi
 if [ -n "${ARROW_CONAN_WITH_BROTLI:-}" ]; then
   conan_args+=(--options arrow:with_brotli=${ARROW_CONAN_WITH_BROTLI})
 fi
+if [ -n "${ARROW_CONAN_WITH_GLOG:-}" ]; then
+  conan_args+=(--options arrow:with_glog=${ARROW_CONAN_WITH_GLOG})
+fi
 if [ -n "${ARROW_CONAN_WITH_JEMALLOC:-}" ]; then
   conan_args+=(--options arrow:with_jemalloc=${ARROW_CONAN_WITH_JEMALLOC})
 fi
