e24d760a34a157cad7010bd90a300473f56a4600
Hongze Zhang <hongze.zhang@intel.com>
2022-07-25 16:11:48 +0800

    Fixup

--- cpp/src/arrow/dataset/discovery.cc
+++ cpp/src/arrow/dataset/discovery.cc
@@ -283,7 +283,7 @@ Result<std::shared_ptr<Dataset>> FileSystemDatasetFactory::Finish(FinishOptions 
 
   std::vector<std::shared_ptr<FileFragment>> fragments;
   for (const auto& src : files_) {
-    auto fixed_path = StripPrefixAndFilename(src.path(), options_.partition_base_dir);
+    auto fixed_path = StripPrefix(src.path(), options_.partition_base_dir);
     ARROW_ASSIGN_OR_RAISE(auto partition, partitioning->Parse(fixed_path));
     ARROW_ASSIGN_OR_RAISE(auto fragment, format_->MakeFragment(src, partition));
     fragments.push_back(fragment);
