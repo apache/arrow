9cbce4f5fd89b085b5a7efd340f82fd19a7744b7
3dbrows <duncan.macquarrie@wayflyer.com>
2022-07-29 10:15:19 +0100

    Fixing build problems

--- cpp/src/arrow/filesystem/s3fs.cc
+++ cpp/src/arrow/filesystem/s3fs.cc
@@ -235,8 +235,8 @@ class AwsRetryStrategy : S3RetryStrategy {
     auto exception_name = new Aws::String(detail.exception_name.c_str());
     auto message = new Aws::String(detail.message.c_str());
     return new Aws::Client::AWSError<Aws::Client::CoreErrors>(
-        static_cast<Aws::Client::CoreErrors>(detail.error_type), exception_name, message,
-        detail.should_retry);
+        static_cast<Aws::Client::CoreErrors>(detail.error_type), *exception_name,
+        *message, detail.should_retry);
   }
 };
 
