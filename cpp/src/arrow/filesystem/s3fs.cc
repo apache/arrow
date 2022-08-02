d67bd200b4755baede829347d6560b1b09c5c4fd
Duncan MacQuarrie <5956299+3dbrows@users.noreply.github.com>
2022-08-02 09:44:52 +0100

    Update cpp/src/arrow/filesystem/s3fs.cc
    Co-authored-by: David Li <li.davidm96@gmail.com>

--- cpp/src/arrow/filesystem/s3fs.cc
+++ cpp/src/arrow/filesystem/s3fs.cc
@@ -231,10 +231,10 @@ class AwsRetryStrategy : public S3RetryStrategy {
       const S3RetryStrategy::AWSErrorDetail& detail) {
     auto exception_name = ToAwsString(detail.exception_name);
     auto message = ToAwsString(detail.message);
-    auto errors = new Aws::Client::AWSError<Aws::Client::CoreErrors>(
-        static_cast<Aws::Client::CoreErrors>(detail.error_type), *exception_name,
-        *message, detail.should_retry);
-    return *errors;
+    auto errors = Aws::Client::AWSError<Aws::Client::CoreErrors>(
+        static_cast<Aws::Client::CoreErrors>(detail.error_type), exception_name,
+        message, detail.should_retry);
+    return errors;
   }
 };
 
