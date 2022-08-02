6878b5bcd995e96e2c4130b45d4b49f68986c129
Tobias Zagorni <tobias@zagorni.eu>
2022-08-02 23:07:57 +0200

    array_encoded: add methods for working with RunLengthEncodedArray

--- cpp/src/arrow/array/array_encoded.cc
+++ cpp/src/arrow/array/array_encoded.cc
@@ -16,6 +16,7 @@
 // under the License.
 
 #include "arrow/array/array_encoded.h"
+#include "arrow/array/util.h"
 #include "arrow/util/logging.h"
 
 namespace arrow {
@@ -36,6 +37,37 @@ RunLengthEncodedArray::RunLengthEncodedArray(const std::shared_ptr<DataType>& ty
   ARROW_CHECK_EQ(type->id(), Type::RUN_LENGTH_ENCODED);
   SetData(ArrayData::Make(type, length, {run_ends_buffer}, null_count, offset));
   data_->child_data.push_back(std::move(values_array->data()));
+}
+
+Result<std::shared_ptr<RunLengthEncodedArray>> RunLengthEncodedArray::Make(
+    std::shared_ptr<Array>& values_array, std::shared_ptr<Array>& run_ends_array,
+    int64_t logical_length, int64_t null_count, int64_t offset) {
+  if (run_ends_array->type_id() != Type::INT32) {
+    return Status::Invalid("Run ends array must be int32 type");
+  }
+  int64_t physical_length = values_array->length();
+  if (run_ends_array->null_count() != 0) {
+    return Status::Invalid("Run ends array cannot contain null values");
+  }
+
+  std::shared_ptr<Buffer> run_ends_buffer = SliceBuffer(
+      run_ends_array->data()->buffers[1], run_ends_array->offset(), physical_length);
+
+  return std::make_shared<RunLengthEncodedArray>(
+      run_length_encoded(values_array->type()), logical_length, values_array,
+      std::move(run_ends_buffer), null_count, offset);
+}
+
+std::shared_ptr<Array> RunLengthEncodedArray::values_array() const {
+  return MakeArray(data()->child_data[0]);
+}
+
+std::shared_ptr<Buffer> RunLengthEncodedArray::run_ends_buffer() const {
+  return data()->buffers[0];
+}
+
+const int32_t* RunLengthEncodedArray::run_ends() const {
+  return data()->GetValuesSafe<int32_t>(0, 0);
 }
 
 }  // namespace arrow
