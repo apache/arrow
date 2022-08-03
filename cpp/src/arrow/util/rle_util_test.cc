c94e1dd4d6d9a9b207cef417308cd2664bf2c7af
Tobias Zagorni <tobias@zagorni.eu>
2022-08-03 18:27:57 +0200

    wip: test for VisitMergedRuns

--- cpp/src/arrow/util/rle_util_test.cc
+++ cpp/src/arrow/util/rle_util_test.cc
@@ -40,17 +40,42 @@ TEST(TestRleUtil, FindPhysicalOffsetTest) {
   ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 3, 4, 5}, 3*4+3, 100), 2);
 }
 
+
 TEST(TestRleUtil, VisitMergedRuns) {
-  /*std::vector<int32_t> left = {5, 10, 5, 5};
-  std::vector<int32_t> right = {5, 4, 16};*/
-  std::vector<int32_t> merged = {5, 4, 6, 5, 5};
+  const auto left_run_ends = ArrayFromJSON(int32(), "[1, 2, 3, 4, 5, 6, 7, 8, 9, 1000, 1005, 1015, 1020, 1025, 1050]");
+  const auto right_run_ends = ArrayFromJSON(int32(), "[1, 2, 3, 4, 5, 2005, 2009, 2025, 2050]");
+  const std::vector<int32_t> expected_run_lengths = {5, 4, 6, 5, 5, 25};
+  const std::vector<int32_t> expected_left_visits = {5, 4, 6, 5, 5};
+  const std::vector<int32_t> expected_right_visits = {5, 4, 6, 5, 5};
+  const int32_t left_parent_offset = 1000;
+  const int32_t left_child_offset = 100;
+  const int32_t right_parent_offset = 200;
+  const int32_t right_child_offset = 100;
 
-  size_t merged_position = 0;
+  std::shared_ptr<Array> left_child = std::make_shared<NullArray>(left_child_offset + left_run_ends->length());
+  std::shared_ptr<Array> right_child = std::make_shared<NullArray>(right_child_offset + right_run_ends->length());
 
-  rle_util::VisitMergedRuns()
+  left_child = left_child->Slice(left_child_offset);
+  right_child = right_child->Slice(right_child_offset);
+
+  ASSERT_OK_AND_ASSIGN(std::shared_ptr<Array> left_array, RunLengthEncodedArray::Make(left_run_ends, left_child, 1026));
+  ASSERT_OK_AND_ASSIGN(std::shared_ptr<Array> right_array, RunLengthEncodedArray::Make(right_run_ends, right_child, 2026));
+
+  left_array = left_array->Slice(left_parent_offset);
+  left_array = left_array->Slice(left_parent_offset);
+
+  size_t position = 0;
+
+  rle_util::VisitMergedRuns(ArraySpan(*left_array->data()), ArraySpan(*left_array->data()),
+                            [&position, &expected_run_lengths, &expected_left_visits, expected_right_visits](int64_t run_length, int64_t left_index, int64_t right_index) {
+    ASSERT_EQ(run_length, expected_run_lengths[position]);
+    ASSERT_EQ(left_index, expected_left_visits[position]);
+    ASSERT_EQ(run_length, expected_right_visits[position]);
+    position++;
+  });
 
 
-  ASSERT_EQ(merged_position, merged.size());
+  ASSERT_EQ(position, expected_run_lengths.size());
 
 
 }
