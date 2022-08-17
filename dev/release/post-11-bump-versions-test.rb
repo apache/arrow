e075e676734a5c69cb974c4505821b49903c59df
Matt Topol <zotthewizard@gmail.com>
2022-08-17 19:28:33 -0400

    fix for now

--- dev/release/post-11-bump-versions-test.rb
+++ dev/release/post-11-bump-versions-test.rb
@@ -204,8 +204,8 @@ class PostBumpVersionsTest < Test::Unit::TestCase
             "+module github.com/apache/arrow/go/v#{@next_major_version}/arrow/compute",
             "-replace github.com/apache/arrow/go/v#{@snapshot_major_version} => ../../",
             "+replace github.com/apache/arrow/go/v#{@next_major_version} => ../../",
-            "-\tgithub.com/apache/arrow/go/v#{@snapshot_major_version} v#{@snapshot_version}-00010101000000-000000000000",
-            "+\tgithub.com/apache/arrow/go/v#{@next_major_version} v#{@next_snapshot_version}-00010101000000-000000000000",
+            "-\tgithub.com/apache/arrow/go/v#{@snapshot_major_version} v#{@snapshot_major_version}.0.0-00010101000000-000000000000",
+            "+\tgithub.com/apache/arrow/go/v#{@next_major_version} v#{@next_major_version}.0.0-00010101000000-000000000000",
           ],
         ]}
         next
