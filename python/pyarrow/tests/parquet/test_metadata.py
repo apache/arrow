f9e36d8ad4716d13d5adc4b00e46091d8c3f2e5f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-08-17 10:19:44 +0200

    use our own tempdir instead of tmpdir

--- python/pyarrow/tests/parquet/test_metadata.py
+++ python/pyarrow/tests/parquet/test_metadata.py
@@ -18,14 +18,13 @@
 import datetime
 import decimal
 from collections import OrderedDict
-import os
 
 import numpy as np
 import pytest
 
 import pyarrow as pa
 from pyarrow.tests.parquet.common import _check_roundtrip, make_sample_file
-from pyarrow.fs import LocalFileSystem, FileSystem
+from pyarrow.fs import LocalFileSystem
 from pyarrow.tests import util
 
 try:
@@ -536,37 +535,39 @@ def test_metadata_exceeds_message_size():
     metadata = pq.read_metadata(pa.BufferReader(buf))
 
 
-def test_metadata_schema_filesystem(tmpdir):
+def test_metadata_schema_filesystem(tempdir):
     table = pa.table({"a": [1, 2, 3]})
 
     # URI writing to local file.
     fname = "data.parquet"
-    file_path = 'file:///' + os.path.join(str(tmpdir), fname)
+    file_path = str(tempdir / fname)
+    file_uri = 'file:///' + file_path
 
     pq.write_table(table, file_path)
 
     # Get expected `metadata` from path.
-    metadata = pq.read_metadata(tmpdir / fname)
+    metadata = pq.read_metadata(tempdir / fname)
     schema = table.schema
 
-    assert pq.read_metadata(file_path).equals(metadata)
+    assert pq.read_metadata(file_uri).equals(metadata)
     assert pq.read_metadata(
-        fname, filesystem=f'file:///{tmpdir}').equals(metadata)
+        file_path, filesystem=LocalFileSystem()).equals(metadata)
+    assert pq.read_metadata(
+        fname, filesystem=f'file:///{tempdir}').equals(metadata)
 
-    assert pq.read_schema(file_path).equals(schema)
-    assert pq.read_schema(fname, filesystem=f'file:///{tmpdir}').equals(schema)
+    assert pq.read_schema(file_uri).equals(schema)
+    assert pq.read_schema(
+        file_path, filesystem=LocalFileSystem()).equals(schema)
+    assert pq.read_schema(
+        fname, filesystem=f'file:///{tempdir}').equals(schema)
 
-    with util.change_cwd(tmpdir):
+    with util.change_cwd(tempdir):
         # Pass `filesystem` arg
         assert pq.read_metadata(
             fname, filesystem=LocalFileSystem()).equals(metadata)
 
         assert pq.read_schema(
             fname, filesystem=LocalFileSystem()).equals(schema)
-
-        err_msg = ('FileSystem is an abstract class')
-        with pytest.raises(TypeError, match=err_msg):
-            pq.read_metadata(fname, filesystem=FileSystem())
 
 
 def test_metadata_equals():
