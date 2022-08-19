c2f2cf6e79526fda7a34ee1a28e844e6039dba7c
Ziheng Wang <zihengw@stanford.edu>
2022-08-19 16:06:20 -0700

    Update cpp/src/arrow/dataset/scanner.h
    Co-authored-by: Weston Pace <weston.pace@gmail.com>

--- cpp/src/arrow/dataset/scanner.h
+++ cpp/src/arrow/dataset/scanner.h
@@ -383,12 +383,11 @@ class ARROW_DS_EXPORT ScannerBuilder {
 
   /// \brief Set the number of batches to read ahead within a fragment.
   ///
-  /// \param[in] batch_readahead How many batches to read ahead within a fragment,
-  ///  might not work for all formats.
-  /// \returns An error if this number is less than 0.
+  /// \param[in] batch_readahead How many batches to read ahead within a fragment
+  /// \returns an error if this number is less than 0.
   ///
-  /// This option provides a control on RAM vs I/O tradeoff.
-  /// It might not be support by all file formats, in which case it will
+  /// This option provides a control on the RAM vs I/O tradeoff.
+  /// It might not be supported by all file formats, in which case it will
   /// simply be ignored.
   Status BatchReadahead(int32_t batch_readahead);
 
