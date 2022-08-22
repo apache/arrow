0600bb27d6b287d91c2073763e785232be67bb30
Matt Topol <zotthewizard@gmail.com>
2022-08-22 19:11:38 -0400

    ARROW-17496: [Go] Fix Nightly Build (#13943)
    Turns out that the `pragma_table_info` function in modernc.org/sqlite's package doesn't work correctly in go1.17 either, only in go1.18. As this is only used for testing and the example sqlite flightsql server, rather than anything needed in the flightsql package itself, the bulid failure is easily solved by marking the example and its tests to be only built in go1.18.
    
    As we already have a git workflow that runs with go1.18, the CI will still continue to test the example code, but mamba builds using go1.17 won't break anymore.
    
    Authored-by: Matt Topol <zotthewizard@gmail.com>
    Signed-off-by: Matt Topol <zotthewizard@gmail.com>

--- go/arrow/flight/flightsql/example/sql_batch_reader.go
+++ go/arrow/flight/flightsql/example/sql_batch_reader.go
@@ -14,8 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.17
-// +build go1.17
+//go:build go1.18
+// +build go1.18
 
 package example
 
