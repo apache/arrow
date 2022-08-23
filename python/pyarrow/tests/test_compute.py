b848e48b20208b663ebf2f5b002522199ca2c8de
Miles Granger <miles59923@gmail.com>
2022-08-23 13:42:00 +0200

    Fixup tests, error types

--- python/pyarrow/tests/test_compute.py
+++ python/pyarrow/tests/test_compute.py
@@ -395,9 +395,8 @@ def test_mode_chunked_array():
 
 
 def test_empty_chunked_array():
-    msg = ("When passing an empty collection of arrays you "
-           "must also pass the data type")
-    with pytest.raises(ValueError, match=msg):
+    msg = "cannot construct ChunkedArray from empty vector and omitted type"
+    with pytest.raises(pa.ArrowInvalid, match=msg):
         pa.chunked_array([])
 
     pa.chunked_array([], type=pa.int8())
