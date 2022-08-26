1b9c57e20802fb061c90837c39e99d8fa69cc212
Matt Topol <zotthewizard@gmail.com>
2022-08-26 14:13:20 -0400

    ARROW-17453: [Go][C++][Parquet] Inconsistent Data with Repetition Levels (#13982)
    Both the C++ and Go parquet implementations assumed that if the max repetition level was 0, that there were no bytes to be skipped when initializing decoders for `DataPageV2` but the Parquet files produced by Athena in this case had repetition bytes to be skipped before getting the definition level bytes. Since the byte wasn't skipped, the wrong values were decoded for Definition levels.
    
    In the case of the Go implementation, it made additional assumptions that proved to be incorrect on top of the same bug.
    
    This fixes both of them to properly respect the repetition level byte length reported in the DataPageV2 header.
    
    Authored-by: Matt Topol <zotthewizard@gmail.com>
    Signed-off-by: Matt Topol <zotthewizard@gmail.com>

--- go/parquet/file/column_reader_test.go
+++ go/parquet/file/column_reader_test.go
@@ -531,6 +531,40 @@ func (p *PrimitiveReaderSuite) TestInt32FlatRequiredSkip() {
 	})
 }
 
+func (p *PrimitiveReaderSuite) TestRepetitionLvlBytesWithMaxRepZero() {
+	const batchSize = 4
+	p.maxDefLvl = 1
+	p.maxRepLvl = 0
+	typ := schema.NewInt32Node("a", parquet.Repetitions.Optional, -1)
+	descr := schema.NewColumn(typ, p.maxDefLvl, p.maxRepLvl)
+	// Bytes here came from the example parquet file in ARROW-17453's int32
+	// column which was delta bit-packed. The key part is the first three
+	// bytes: the page header reports 1 byte for repetition levels even
+	// though the max rep level is 0. If that byte isn't skipped then
+	// we get def levels of [1, 1, 0, 0] instead of the correct [1, 1, 1, 0].
+	pageData := [...]byte{0x3, 0x3, 0x7, 0x80, 0x1, 0x4, 0x3,
+		0x18, 0x1, 0x2, 0x0, 0x0, 0x0, 0xc,
+		0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
+
+	p.pages = append(p.pages, file.NewDataPageV2(memory.NewBufferBytes(pageData[:]), batchSize, 1, batchSize,
+		parquet.Encodings.DeltaBinaryPacked, 2, 1, int32(len(pageData)), false))
+
+	p.initReader(descr)
+	p.NotPanics(func() { p.reader.HasNext() })
+
+	var (
+		values  [4]int32
+		defLvls [4]int16
+	)
+	i32Rdr := p.reader.(*file.Int32ColumnChunkReader)
+	total, read, err := i32Rdr.ReadBatch(batchSize, values[:], defLvls[:], nil)
+	p.NoError(err)
+	p.EqualValues(batchSize, total)
+	p.EqualValues(3, read)
+	p.Equal([]int16{1, 1, 1, 0}, defLvls[:])
+	p.Equal([]int32{12, 11, 13, 0}, values[:])
+}
+
 func (p *PrimitiveReaderSuite) TestDictionaryEncodedPages() {
 	p.maxDefLvl = 0
 	p.maxRepLvl = 0
