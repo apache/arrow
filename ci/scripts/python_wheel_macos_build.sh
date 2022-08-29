307638d3596314c7e9ab10ec427ad9b895693e64
Antoine Pitrou <antoine@python.org>
2022-08-29 23:07:15 +0200

    ARROW-17554: [Python][Packaging] Stop producing macOS Mavericks wheels (#13996)
    Also remove an unused script (fixing ARROW-17555).
    
    Authored-by: Antoine Pitrou <antoine@python.org>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- ci/scripts/python_wheel_macos_build.sh
+++ ci/scripts/python_wheel_macos_build.sh
@@ -34,7 +34,7 @@ rm -rf ${source_dir}/python/pyarrow/*.so.*
 
 echo "=== (${PYTHON_VERSION}) Set SDK, C++ and Wheel flags ==="
 export _PYTHON_HOST_PLATFORM="macosx-${MACOSX_DEPLOYMENT_TARGET}-${arch}"
-export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-10.9}
+export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-10.13}
 export SDKROOT=${SDKROOT:-$(xcrun --sdk macosx --show-sdk-path)}
 
 if [ $arch = "arm64" ]; then
