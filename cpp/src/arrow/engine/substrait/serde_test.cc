b9d6f073a6cad65b7cd316115e14c24459fd97c2
Vibhatha Abeykoon <vibhatha@gmail.com>
2022-08-29 13:49:54 +0530

    fix(native): updated the file_path method to check CI failure

--- cpp/src/arrow/engine/substrait/serde_test.cc
+++ cpp/src/arrow/engine/substrait/serde_test.cc
@@ -2006,7 +2006,7 @@ TEST(Substrait, BasicPlanRoundTrippingEndToEnd) {
   ASSERT_OK_AND_ASSIGN(auto tempdir,
                        arrow::internal::TemporaryDir::Make("substrait_tempdir"));
   ASSERT_OK_AND_ASSIGN(auto file_path, tempdir->path().Join(file_name));
-  std::string file_path_str = file_path.ToNative();
+  std::string file_path_str = file_path.ToString();
 
   // Note: there is an additional forward slash introduced by the tempdir
   // it must be replaced to properly load into reading files
