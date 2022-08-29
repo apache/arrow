26f4a9ef54430f6c1aef923b16348e0e882af905
Egill Fridgeirsson <egillax@gmail.com>
2022-08-29 11:38:41 +0200

    add tests that fail because of bug

--- r/tests/testthat/test-Array.R
+++ r/tests/testthat/test-Array.R
@@ -180,6 +180,11 @@ test_that("Array support null type (ARROW-7064)", {
   expect_array_roundtrip(vctrs::unspecified(10), null())
 })
 
+test_that("Array support 0-length NULL vectors (Arrow-17543)", {
+  expect_equal(Array$create(c())$type, null())
+  expect_equal(Array$create(NULL)$type, null())
+})
+
 test_that("Array supports logical vectors (ARROW-3341)", {
   # with NA
   x <- sample(c(TRUE, FALSE, NA), 1000, replace = TRUE)
