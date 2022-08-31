3ad17b285a2ee4b75c2099c9953fcc201c993b2a
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-08-31 14:14:48 +0900

    ARROW-17089: [Python] Use `.arrow` as extension for IPC file dataset (#13677)
    Authored-by: SHIMA Tatsuya <ts1s1andn@gmail.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- python/pyarrow/dataset.py
+++ python/pyarrow/dataset.py
@@ -26,6 +26,7 @@ from pyarrow._dataset import (  # noqa
     Dataset,
     DatasetFactory,
     DirectoryPartitioning,
+    FeatherFileFormat,
     FilenamePartitioning,
     FileFormat,
     FileFragment,
@@ -284,8 +285,10 @@ def _ensure_format(obj):
         if not _parquet_available:
             raise ValueError(_parquet_msg)
         return ParquetFileFormat()
-    elif obj in {"ipc", "arrow", "feather"}:
+    elif obj in {"ipc", "arrow"}:
         return IpcFileFormat()
+    elif obj == "feather":
+        return FeatherFileFormat()
     elif obj == "csv":
         return CsvFileFormat()
     elif obj == "orc":
