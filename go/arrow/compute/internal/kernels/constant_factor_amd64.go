93445ce9c92d2a19c26e24650fa31380db54a8b2
Matt Topol <zotthewizard@gmail.com>
2022-09-01 11:46:10 -0400

    add simd impls for constant_factor handling

--- go/arrow/compute/internal/kernels/constant_factor_amd64.go
+++ go/arrow/compute/internal/kernels/constant_factor_amd64.go
@@ -14,4 +14,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !noasm
+
 package kernels
+
+import (
+	"golang.org/x/sys/cpu"
+)
+
+func init() {
+	if cpu.X86.HasAVX2 {
+		multiplyConstantInt32Int32 = multiplyConstantInt32Int32Avx2
+		multiplyConstantInt32Int64 = multiplyConstantInt32Int64Avx2
+		multiplyConstantInt64Int32 = multiplyConstantInt64Int32Avx2
+		multiplyConstantInt64Int64 = multiplyConstantInt64Int64Avx2
+
+		divideConstantInt32Int32 = divideConstantInt32Int32Avx2
+		divideConstantInt32Int64 = divideConstantInt32Int64Avx2
+		divideConstantInt64Int32 = divideConstantInt64Int32Avx2
+		divideConstantInt64Int64 = divideConstantInt64Int64Avx2
+	} else if cpu.X86.HasSSE42 {
+		multiplyConstantInt32Int32 = multiplyConstantInt32Int32SSE4
+		multiplyConstantInt32Int64 = multiplyConstantInt32Int64SSE4
+		multiplyConstantInt64Int32 = multiplyConstantInt64Int32SSE4
+		multiplyConstantInt64Int64 = multiplyConstantInt64Int64SSE4
+
+		divideConstantInt32Int32 = divideConstantInt32Int32SSE4
+		divideConstantInt32Int64 = divideConstantInt32Int64SSE4
+		divideConstantInt64Int32 = divideConstantInt64Int32SSE4
+		divideConstantInt64Int64 = divideConstantInt64Int64SSE4
+	} else {
+		multiplyConstantInt32Int32 = multiplyConstantGo[int32, int32]
+		multiplyConstantInt32Int64 = multiplyConstantGo[int32, int64]
+		multiplyConstantInt64Int32 = multiplyConstantGo[int64, int32]
+		multiplyConstantInt64Int64 = multiplyConstantGo[int64, int64]
+
+		divideConstantInt32Int32 = divideConstantGo[int32, int32]
+		divideConstantInt32Int64 = divideConstantGo[int32, int64]
+		divideConstantInt64Int32 = divideConstantGo[int64, int32]
+		divideConstantInt64Int64 = divideConstantGo[int64, int64]
+	}
+}
