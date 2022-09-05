fbce2310806a8ff975247aa9ce8a2db205bc3aed
Philipp Moritz <pcmoritz@gmail.com>
2022-09-05 11:40:57 -0700

    fix

--- cpp/src/arrow/filesystem/s3_internal.h
+++ cpp/src/arrow/filesystem/s3_internal.h
@@ -152,8 +152,9 @@ Status ErrorToStatus(const std::string& prefix, const std::string& operation,
   // XXX Handle fine-grained error types
   // See
   // https://sdk.amazonaws.com/cpp/api/LATEST/namespace_aws_1_1_s3.html#ae3f82f8132b619b6e91c88a9f1bde371
-  auto status = S3ErrorToString(static_cast<Aws::S3::S3Errors>(error.GetErrorType()));
-  if (error.GetErrorType() == Aws::S3::S3Errors::UNKNOWN) {
+  auto error_type = static_cast<Aws::S3::S3Errors>(error.GetErrorType());
+  auto status = S3ErrorToString(error_type);
+  if (error_type == Aws::S3::S3Errors::UNKNOWN) {
     status += " (http status " + std::to_string(error.GetResponseCode()) + ")";
   }
   return Status::IOError(
