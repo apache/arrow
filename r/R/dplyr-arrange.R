6651b75f711a1fef3ccf3a263849e8225c10cb9a
SHIMA Tatsuya <ts1s1andn@gmail.com>
2022-09-10 14:02:26 +0000

    Implement dplyr::across() inside arrange()
    Signed-off-by: SHIMA Tatsuya <ts1s1andn@gmail.com>

--- r/R/dplyr-arrange.R
+++ r/R/dplyr-arrange.R
@@ -20,7 +20,10 @@
 
 arrange.arrow_dplyr_query <- function(.data, ..., .by_group = FALSE) {
   call <- match.call()
-  exprs <- quos(...)
+
+  expression_list <- expand_across(.data, quos(...))
+  exprs <- ensure_named_exprs(expression_list)
+
   if (.by_group) {
     # when the data is is grouped and .by_group is TRUE, order the result by
     # the grouping columns first
