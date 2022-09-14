2851d13f2b9092fb1fc85fa6aa81876aef5e1634
Aldrin M <octalene.dev@pm.me>
2022-09-14 14:39:03 -0700

    ARROW-8991: moved hash_64 benchmarks
    Moved benchmarks of the compute function out of
    `hashing_benchmark.cc` and into `scalar_hash_benchmark.cc`. This is so that
    if `ARROW_COMPUTE` is set to `OFF`, hashing_benchmark.cc can still be
    run.

--- cpp/src/arrow/util/hashing_benchmark.cc
+++ cpp/src/arrow/util/hashing_benchmark.cc
@@ -30,7 +30,7 @@
 
 #include "arrow/array/builder_primitive.h"
 #include "arrow/compute/exec/key_hash.h"
-#include "arrow/compute/light_array.h"
+
 
 namespace arrow {
 namespace internal {
@@ -38,7 +38,6 @@ namespace internal {
 namespace {
 // copied from scalar_string_benchmark
 constexpr auto kSeed = 0x94378165;
-constexpr double null_prob = 0.2;
 
 static random::RandomArrayGenerator hashing_rng(kSeed);
 }  // namespace
@@ -73,20 +72,6 @@ static std::vector<std::string> MakeStrings(int32_t n_values, int32_t min_length
     return s;
   });
   return values;
-}
-
-static Result<std::shared_ptr<StructArray>> MakeStructArray(int64_t n_values,
-                                                            int32_t min_strlen,
-                                                            int32_t max_strlen) {
-  auto vals_first = hashing_rng.Int64(n_values, 0, std::numeric_limits<int64_t>::max());
-  auto vals_second = hashing_rng.String(n_values, min_strlen, max_strlen, null_prob);
-  auto vals_third = hashing_rng.Int64(n_values, 0, std::numeric_limits<int64_t>::max());
-
-  return arrow::StructArray::Make(
-      arrow::ArrayVector{vals_first, vals_second, vals_third},
-      arrow::FieldVector{arrow::field("first", arrow::int64()),
-                         arrow::field("second", arrow::utf8()),
-                         arrow::field("third", arrow::int64())});
 }
 
 static void BenchmarkStringHashing(benchmark::State& state,  // NOLINT non-const reference
@@ -225,113 +210,6 @@ static void KeyHashIntegers64(benchmark::State& state) {  // NOLINT non-const re
   state.SetItemsProcessed(state.iterations() * test_vals->length());
 }
 
-static void FastHash64Int64(benchmark::State& state) {  // NOLINT non-const reference
-  auto test_vals = hashing_rng.Int64(10000, 0, std::numeric_limits<int64_t>::max());
-
-  while (state.KeepRunning()) {
-    ASSERT_OK_AND_ASSIGN(Datum hash_result,
-                         compute::CallFunction("fast_hash_64", {test_vals}));
-    benchmark::DoNotOptimize(hash_result);
-  }
-
-  state.SetBytesProcessed(state.iterations() * test_vals->length() * sizeof(int64_t));
-  state.SetItemsProcessed(state.iterations() * test_vals->length());
-}
-
-static void FastHash64StructSmallStrings(
-    benchmark::State& state) {  // NOLINT non-const reference
-  ASSERT_OK_AND_ASSIGN(std::shared_ptr<StructArray> values_array,
-                       MakeStructArray(10000, 2, 20));
-
-  // 2nd column (index 1) is a string column, which has offset type of int32_t
-  ASSERT_OK_AND_ASSIGN(std::shared_ptr<Array> values_second,
-                       values_array->GetFlattenedField(1));
-  std::shared_ptr<StringArray> str_vals =
-      std::static_pointer_cast<StringArray>(values_second);
-  int32_t total_string_size = str_vals->total_values_length();
-
-  while (state.KeepRunning()) {
-    ASSERT_OK_AND_ASSIGN(Datum hash_result,
-                         compute::CallFunction("fast_hash_64", {values_array}));
-    benchmark::DoNotOptimize(hash_result);
-  }
-
-  state.SetBytesProcessed(state.iterations() *
-                          ((values_array->length() * sizeof(int64_t)) +
-                           (total_string_size) +
-                           (values_array->length() * sizeof(int64_t))));
-  state.SetItemsProcessed(state.iterations() * 3 * values_array->length());
-}
-
-static void FastHash64StructMediumStrings(
-    benchmark::State& state) {  // NOLINT non-const reference
-  ASSERT_OK_AND_ASSIGN(std::shared_ptr<StructArray> values_array,
-                       MakeStructArray(10000, 20, 120));
-
-  // 2nd column (index 1) is a string column, which has offset type of int32_t
-  ASSERT_OK_AND_ASSIGN(std::shared_ptr<Array> values_second,
-                       values_array->GetFlattenedField(1));
-  std::shared_ptr<StringArray> str_vals =
-      std::static_pointer_cast<StringArray>(values_second);
-  int32_t total_string_size = str_vals->total_values_length();
-
-  while (state.KeepRunning()) {
-    ASSERT_OK_AND_ASSIGN(Datum hash_result,
-                         compute::CallFunction("fast_hash_64", {values_array}));
-    benchmark::DoNotOptimize(hash_result);
-  }
-
-  state.SetBytesProcessed(state.iterations() *
-                          ((values_array->length() * sizeof(int64_t)) +
-                           (total_string_size) +
-                           (values_array->length() * sizeof(int64_t))));
-  state.SetItemsProcessed(state.iterations() * 3 * values_array->length());
-}
-
-static void FastHash64StructLargeStrings(
-    benchmark::State& state) {  // NOLINT non-const reference
-  ASSERT_OK_AND_ASSIGN(std::shared_ptr<StructArray> values_array,
-                       MakeStructArray(10000, 120, 2000));
-
-  // 2nd column (index 1) is a string column, which has offset type of int32_t
-  ASSERT_OK_AND_ASSIGN(std::shared_ptr<Array> values_second,
-                       values_array->GetFlattenedField(1));
-  std::shared_ptr<StringArray> str_vals =
-      std::static_pointer_cast<StringArray>(values_second);
-  int32_t total_string_size = str_vals->total_values_length();
-
-  while (state.KeepRunning()) {
-    ASSERT_OK_AND_ASSIGN(Datum hash_result,
-                         compute::CallFunction("fast_hash_64", {values_array}));
-    benchmark::DoNotOptimize(hash_result);
-  }
-
-  state.SetBytesProcessed(state.iterations() *
-                          ((values_array->length() * sizeof(int64_t)) +
-                           (total_string_size) +
-                           (values_array->length() * sizeof(int64_t))));
-  state.SetItemsProcessed(state.iterations() * 3 * values_array->length());
-}
-
-static void FastHash64Map(benchmark::State& state) {  // NOLINT non-const reference
-  constexpr int64_t test_size = 10000;
-  auto test_keys = hashing_rng.String(test_size, 2, 20, /*null_probability=*/0);
-  auto test_vals = hashing_rng.Int64(test_size, 0, std::numeric_limits<int64_t>::max());
-  auto test_keyvals = hashing_rng.Map(test_keys, test_vals, test_size);
-
-  auto key_arr = std::static_pointer_cast<StringArray>(test_keys);
-  int32_t total_key_size = key_arr->total_values_length();
-  int32_t total_val_size = test_size * sizeof(int64_t);
-
-  while (state.KeepRunning()) {
-    ASSERT_OK_AND_ASSIGN(Datum hash_result,
-                         compute::CallFunction("fast_hash_64", {test_keyvals}));
-    benchmark::DoNotOptimize(hash_result);
-  }
-
-  state.SetBytesProcessed(state.iterations() * (total_key_size + total_val_size));
-  state.SetItemsProcessed(state.iterations() * 2 * test_size);
-}
 
 // ----------------------------------------------------------------------
 // Benchmark declarations
@@ -346,15 +224,6 @@ BENCHMARK(HashIntegers64);
 // Directly uses "KeyHash" hash functions from key_hash.h (xxHash-like)
 BENCHMARK(KeyHashIntegers32);
 BENCHMARK(KeyHashIntegers64);
-
-// Uses "FastHash" compute functions (wraps KeyHash functions)
-BENCHMARK(FastHash64Int64);
-
-BENCHMARK(FastHash64StructSmallStrings);
-BENCHMARK(FastHash64StructMediumStrings);
-BENCHMARK(FastHash64StructLargeStrings);
-
-BENCHMARK(FastHash64Map);
 
 }  // namespace internal
 }  // namespace arrow
