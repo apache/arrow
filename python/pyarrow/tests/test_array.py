833f02f858d19352128f8c18b0a7883d64f2335e
Miles Granger <miles59923@gmail.com>
2022-09-14 11:38:15 +0200

    Keep variable sized empty list and diff sizes

--- python/pyarrow/tests/test_array.py
+++ python/pyarrow/tests/test_array.py
@@ -2706,14 +2706,28 @@ def test_list_value_parent_indices(list_type):
                           (pa.int32(), pa.list_(pa.int32(), list_size=2)),
                           (pa.int64(), pa.large_list(pa.int32()))])
 def test_list_value_lengths(offset_type, list_type):
-    arr = pa.array(
-        [
-            [0, 1],
-            None,
-            [None, None],
-            [3, 4]
-        ], type=list_type)
-    expected = pa.array([2, None, 2, 2], type=offset_type)
+
+    # FixedSizeListArray needs fixed list sizes
+    if getattr(list_type, "list_size", None):
+        arr = pa.array(
+            [
+                [0, 1],
+                None,
+                [None, None],
+                [3, 4]
+            ], type=list_type)
+        expected = pa.array([2, None, 2, 2], type=offset_type)
+
+    # Otherwise create variable list sizes
+    else:
+        arr = pa.array(
+            [
+                [0, 1, 2],
+                None,
+                [],
+                [3, 4]
+            ], type=list_type)
+        expected = pa.array([3, None, 0, 2], type=offset_type)
     assert arr.value_lengths().equals(expected)
 
 
