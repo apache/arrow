b97497aadcb06f03305f534dc5aee8a759b5442f
Vibhatha Lakmal Abeykoon <vibhatha@users.noreply.github.com>
2022-09-20 04:20:55 +0530

    ARROW-17647: [C++] Using better namespace style when using protobuf with Substrait (#14121)
    This PR includes minor changes to the namespace usage in Substrait integation.
    
    Authored-by: Vibhatha Abeykoon <vibhatha@gmail.com>
    Signed-off-by: Weston Pace <weston.pace@gmail.com>

--- cpp/src/arrow/engine/substrait/relation_internal.cc
+++ cpp/src/arrow/engine/substrait/relation_internal.cc
@@ -165,23 +165,23 @@ Result<DeclarationInfo> FromProto(const substrait::Rel& rel, const ExtensionSet&
       for (const auto& item : read.local_files().items()) {
         std::string path;
         if (item.path_type_case() ==
-            substrait::ReadRel_LocalFiles_FileOrFiles::kUriPath) {
+            substrait::ReadRel::LocalFiles::FileOrFiles::kUriPath) {
           path = item.uri_path();
         } else if (item.path_type_case() ==
-                   substrait::ReadRel_LocalFiles_FileOrFiles::kUriFile) {
+                   substrait::ReadRel::LocalFiles::FileOrFiles::kUriFile) {
           path = item.uri_file();
         } else if (item.path_type_case() ==
-                   substrait::ReadRel_LocalFiles_FileOrFiles::kUriFolder) {
+                   substrait::ReadRel::LocalFiles::FileOrFiles::kUriFolder) {
           path = item.uri_folder();
         } else {
           path = item.uri_path_glob();
         }
 
         switch (item.file_format_case()) {
-          case substrait::ReadRel_LocalFiles_FileOrFiles::kParquet:
+          case substrait::ReadRel::LocalFiles::FileOrFiles::kParquet:
             format = std::make_shared<dataset::ParquetFileFormat>();
             break;
-          case substrait::ReadRel_LocalFiles_FileOrFiles::kArrow:
+          case substrait::ReadRel::LocalFiles::FileOrFiles::kArrow:
             format = std::make_shared<dataset::IpcFileFormat>();
             break;
           default:
@@ -212,7 +212,7 @@ Result<DeclarationInfo> FromProto(const substrait::Rel& rel, const ExtensionSet&
 
         path = path.substr(7);
         switch (item.path_type_case()) {
-          case substrait::ReadRel_LocalFiles_FileOrFiles::kUriPath: {
+          case substrait::ReadRel::LocalFiles::FileOrFiles::kUriPath: {
             ARROW_ASSIGN_OR_RAISE(auto file, filesystem->GetFileInfo(path));
             if (file.type() == fs::FileType::File) {
               files.push_back(std::move(file));
@@ -226,11 +226,11 @@ Result<DeclarationInfo> FromProto(const substrait::Rel& rel, const ExtensionSet&
             }
             break;
           }
-          case substrait::ReadRel_LocalFiles_FileOrFiles::kUriFile: {
+          case substrait::ReadRel::LocalFiles::FileOrFiles::kUriFile: {
             files.emplace_back(path, fs::FileType::File);
             break;
           }
-          case substrait::ReadRel_LocalFiles_FileOrFiles::kUriFolder: {
+          case substrait::ReadRel::LocalFiles::FileOrFiles::kUriFolder: {
             fs::FileSelector selector;
             selector.base_dir = path;
             selector.recursive = true;
@@ -240,7 +240,7 @@ Result<DeclarationInfo> FromProto(const substrait::Rel& rel, const ExtensionSet&
                       std::back_inserter(files));
             break;
           }
-          case substrait::ReadRel_LocalFiles_FileOrFiles::kUriPathGlob: {
+          case substrait::ReadRel::LocalFiles::FileOrFiles::kUriPathGlob: {
             ARROW_ASSIGN_OR_RAISE(auto discovered_files,
                                   fs::internal::GlobFiles(filesystem, path));
             std::move(discovered_files.begin(), discovered_files.end(),
@@ -582,15 +582,16 @@ Result<std::unique_ptr<substrait::ReadRel>> ScanRelationConverter(
     return Status::Invalid(
         "Can only convert scan node with FileSystemDataset to a Substrait plan.");
   }
+
   // set schema
   ARROW_ASSIGN_OR_RAISE(auto named_struct,
                         ToProto(*dataset->schema(), ext_set, conversion_options));
   read_rel->set_allocated_base_schema(named_struct.release());
 
   // set local files
-  auto read_rel_lfs = make_unique<substrait::ReadRel_LocalFiles>();
+  auto read_rel_lfs = make_unique<substrait::ReadRel::LocalFiles>();
   for (const auto& file : dataset->files()) {
-    auto read_rel_lfs_ffs = make_unique<substrait::ReadRel_LocalFiles_FileOrFiles>();
+    auto read_rel_lfs_ffs = make_unique<substrait::ReadRel::LocalFiles::FileOrFiles>();
     read_rel_lfs_ffs->set_uri_path(UriFromAbsolutePath(file));
     // set file format
     auto format_type_name = dataset->format()->type_name();
