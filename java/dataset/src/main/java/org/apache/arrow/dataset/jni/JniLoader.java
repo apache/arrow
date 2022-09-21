50eeec7f92862e1a48e1ab0b518e447dfe76f400
jackylee-ch <lijunqing@baidu.com>
2022-09-21 20:24:40 +0800

    change the way delete file

--- java/dataset/src/main/java/org/apache/arrow/dataset/jni/JniLoader.java
+++ java/dataset/src/main/java/org/apache/arrow/dataset/jni/JniLoader.java
@@ -29,8 +29,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.io.FileUtils;
-
 /**
  * The JniLoader for Dataset API's native implementation.
  */
@@ -81,7 +79,7 @@ public final class JniLoader {
     final String libraryToLoad = System.mapLibraryName(name);
     try {
       File temp = File.createTempFile("jnilib-", ".tmp", new File(System.getProperty("java.io.tmpdir")));
-      FileUtils.forceDeleteOnExit(temp);
+      temp.deleteOnExit();
       try (final InputStream is
                = JniWrapper.class.getClassLoader().getResourceAsStream(libraryToLoad)) {
         if (is == null) {
