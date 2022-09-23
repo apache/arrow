3f6d5869db13190b4de100f3c03cfa404f2b76c7
Yibo Cai <yibo.cai@arm.com>
2022-09-23 02:47:56 +0000

    skip install sccache on unsupported arch

--- ci/scripts/install_sccache.sh
+++ ci/scripts/install_sccache.sh
@@ -30,6 +30,11 @@ PREFIX=$2
 VERSION=${3:-0.3.0}
 ARCH=$(uname -m)
 
+if [ "${ARCH}" != x86_64 ] && [ "${ARCH}" != aarch64 ]; then
+    echo "Skipped sccache installation on unsupported arch: ${ARCH}"
+    exit 0
+fi
+
 SCCACHE_URL="https://github.com/mozilla/sccache/releases/download/v$VERSION/sccache-v$VERSION-$ARCH-$BUILD.tar.gz"
 SCCACHE_ARCHIVE=sccache.tar.gz
 
