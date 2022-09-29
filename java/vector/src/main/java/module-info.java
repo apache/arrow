4dcfb04b7f2da2b17e446ec6ac4f6ec208002613
david dali susanibar arce <davi.sarces@gmail.com>
2022-09-29 10:47:48 -0500

    Adding vector deps module

--- java/vector/src/main/java/module-info.java
+++ java/vector/src/main/java/module-info.java
@@ -10,6 +10,7 @@ module arrow.vector {
     requires arrow.memory.core;
     requires org.apache.arrow.flatbuf;
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
     requires io.netty.common;
     requires java.sql;
     requires com.fasterxml.jackson.datatype.jsr310;
