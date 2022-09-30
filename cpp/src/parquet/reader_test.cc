4ee95216b760289d7b6c5c4469201d8714fef354
sfc-gh-nthimmegowda <nishanth.thimmegowda@snowflake.com>
2022-09-30 01:15:56 +0000

    Restructure BooleanDecoder and add tests with update rle_boolean encoding file

--- cpp/src/parquet/reader_test.cc
+++ cpp/src/parquet/reader_test.cc
@@ -139,7 +139,11 @@ class TestBooleanRLE : public ::testing::Test {
 };
 
 TEST_F(TestBooleanRLE, TestBooleanScanner) {
-  auto group = reader_->RowGroup(0);
+
+int nvalues = 68;
+int validation_values = 16;
+
+auto group = reader_->RowGroup(0);
 
   // column 0, id
   auto scanner = std::make_shared<BoolScanner>(group->Column(0));
@@ -147,40 +151,55 @@ TEST_F(TestBooleanRLE, TestBooleanScanner) {
   bool val = false;
   bool is_null = false;
 
-  // For this file, 3rd index value is null
-  int expectedNull[8] = {false, false, true, false, false, false, false, false};
-  bool expectedValue[8] = {true, false, false, true, true, false, false, true};
+  // For this file, 3rd and 16th index value is null
+  std::vector<bool> expected_null = {false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, true};
+  std::vector<bool> expected_value  = {true, false, false, true, true, false, false, true,  true, true, false, false, true, true, false, false};
 
-  for (int i = 0; i < 8; i++) {
+  // Assert sizes are same
+  ASSERT_EQ(validation_values, expected_null.size());
+  ASSERT_EQ(validation_values, expected_value.size());
+
+  for (int i = 0; i < validation_values; i++) {
     ASSERT_TRUE(scanner->HasNext());
     ASSERT_TRUE(scanner->NextValue(&val, &is_null));
 
-    ASSERT_EQ(expectedNull[i], is_null);
+    ASSERT_EQ(expected_null[i], is_null);
 
     // Only validate val if not null
     if (!is_null) {
-      ASSERT_EQ(expectedValue[i], val);
+      ASSERT_EQ(expected_value[i], val);
     }
   }
 
+  // Loop through rest of the values to assert data exists
+  for (int i = validation_values; i < nvalues;i++) {
+    ASSERT_TRUE(scanner->HasNext());
+    ASSERT_TRUE(scanner->NextValue(&val, &is_null));
+}
+
+  // Attempt to read past end of column
   ASSERT_FALSE(scanner->HasNext());
   ASSERT_FALSE(scanner->NextValue(&val, &is_null));
 }
 
 TEST_F(TestBooleanRLE, TestBatchRead) {
+  int nvalues = 68;
+  int num_row_groups = 1;
+  int metadata_size = 111;
+
   auto group = reader_->RowGroup(0);
 
   // column 0, id
   auto col = std::dynamic_pointer_cast<BoolReader>(group->Column(0));
 
-  // This file only has 8 rows
-  ASSERT_EQ(8, reader_->metadata()->num_rows());
+  // This file only has 68 rows
+  ASSERT_EQ(nvalues, reader_->metadata()->num_rows());
   // This file only has 1 row group
-  ASSERT_EQ(1, reader_->metadata()->num_row_groups());
-  // Size of the metadata is 106 bytes
-  ASSERT_EQ(106, reader_->metadata()->size());
-  // This row group must have 8 rows
-  ASSERT_EQ(8, group->metadata()->num_rows());
+  ASSERT_EQ(num_row_groups, reader_->metadata()->num_row_groups());
+  // Size of the metadata is 111 bytes
+  ASSERT_EQ(metadata_size, reader_->metadata()->size());
+  // This row group must have 68 rows
+  ASSERT_EQ(nvalues, group->metadata()->num_rows());
 
   // Check if the column is encoded with RLE
   auto col_chunk = group->metadata()->ColumnChunk(0);
@@ -191,24 +210,31 @@ TEST_F(TestBooleanRLE, TestBatchRead) {
   ASSERT_TRUE(col->HasNext());
   int64_t curr_batch_read = 0;
 
-  const int16_t batch_size = 8;
+  const int16_t batch_size = 17;
+  const int16_t num_nulls = 2;
   int16_t def_levels[batch_size];
   int16_t rep_levels[batch_size];
-  bool values[batch_size - 1];
+  bool values[batch_size - num_nulls];
 
   auto levels_read =
       col->ReadBatch(batch_size, def_levels, rep_levels, values, &curr_batch_read);
   ASSERT_EQ(batch_size, levels_read);
 
-  // Since one value is a null value, expect batches read to be one less than indicated
+  // Since two value's are null value, expect batches read to be num_nulls less than indicated
   // batch_size
-  ASSERT_EQ(batch_size - 1, curr_batch_read);
+  ASSERT_EQ(batch_size - num_nulls, curr_batch_read);
 
   // 3rd index is null value
-  ASSERT_THAT(def_levels, testing::ElementsAre(1, 1, 0, 1, 1, 1, 1, 1));
+  ASSERT_THAT(def_levels, testing::ElementsAre(1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1));
 
   // Validate inserted data is as expected
-  ASSERT_THAT(values, testing::ElementsAre(1, 0, 1, 1, 0, 0, 1));
+  ASSERT_THAT(values, testing::ElementsAre(1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1));
+
+  // Loop through rest of the values and assert batch_size read
+  for (int i = batch_size; i< nvalues ;i = i + batch_size) {
+    levels_read = col->ReadBatch(batch_size, def_levels, rep_levels, values, &curr_batch_read);
+    ASSERT_EQ(batch_size, levels_read);
+  }
 
   // Now read past the end of the file
   ASSERT_FALSE(col->HasNext());
