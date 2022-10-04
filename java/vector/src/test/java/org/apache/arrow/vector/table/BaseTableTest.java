27a9bb6755f4672fe56711d8a729c6d7ffe7374c
Larry White <lwhite1@users.noreply.github.com>
2022-10-03 20:40:21 -0400

    remove unused import

--- java/vector/src/test/java/org/apache/arrow/vector/table/BaseTableTest.java
+++ java/vector/src/test/java/org/apache/arrow/vector/table/BaseTableTest.java
@@ -25,7 +25,6 @@ import static org.apache.arrow.vector.table.TestUtils.twoIntColumns;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
