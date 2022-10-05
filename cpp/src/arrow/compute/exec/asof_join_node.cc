8e075f15f3b9073456e7777f3a6f61c1e21b48d1
Weston Pace <weston.pace@gmail.com>
2022-10-05 12:25:07 -0700

    Simplified logic for better readability

--- cpp/src/arrow/compute/exec/asof_join_node.cc
+++ cpp/src/arrow/compute/exec/asof_join_node.cc
@@ -781,8 +781,9 @@ class AsofJoinNode : public ExecNode {
     // Prune memo entries that have expired (to bound memory consumption)
     if (!lhs.Empty()) {
       for (size_t i = 1; i < state_.size(); ++i) {
-        OnType min_value = std::min<OnType>(0, lhs.GetLatestTime() - tolerance_);
-        state_[i]->RemoveMemoEntriesWithLesserTime(min_value);
+        if (lhs.GetLatestTime() > tolerance_) {
+          state_[i]->RemoveMemoEntriesWithLesserTime(lhs.GetLatestTime() - tolerance_);
+        }
       }
     }
 
