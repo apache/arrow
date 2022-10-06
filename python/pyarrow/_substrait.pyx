a9b086690dabfaab475f3bf1fb6f59d557c59a7b
Miles Granger <miles59923@gmail.com>
2022-10-06 10:39:41 +0200

    substrait.run_query accept bytes/Buffer and not segfault

--- python/pyarrow/_substrait.pyx
+++ python/pyarrow/_substrait.pyx
@@ -19,7 +19,7 @@
 from cython.operator cimport dereference as deref
 from libcpp.vector cimport vector as std_vector
 
-from pyarrow import Buffer
+from pyarrow import Buffer, py_buffer
 from pyarrow.lib import frombytes, tobytes
 from pyarrow.lib cimport *
 from pyarrow.includes.libarrow cimport *
@@ -48,13 +48,18 @@ cdef CDeclaration _create_named_table_provider(dict named_args, const std_vector
                         no_c_inputs, c_input_node_opts)
 
 
-def run_query(plan, table_provider=None):
+ctypedef fused plan_type:
+    Buffer
+    bytes
+
+
+def run_query(plan_type plan, table_provider=None):
     """
     Execute a Substrait plan and read the results as a RecordBatchReader.
 
     Parameters
     ----------
-    plan : Buffer
+    plan : Union[Buffer, bytes]
         The serialized Substrait plan to execute.
     table_provider : object (optional)
         A function to resolve any NamedTable relation to a table.
@@ -124,7 +129,10 @@ def run_query(plan, table_provider=None):
         function[CNamedTableProvider] c_named_table_provider
         CConversionOptions c_conversion_options
 
-    c_buf_plan = pyarrow_unwrap_buffer(plan)
+    if isinstance(plan, bytes):
+        c_buf_plan = pyarrow_unwrap_buffer(py_buffer(plan))
+    else:
+        c_buf_plan = pyarrow_unwrap_buffer(plan)
 
     if table_provider is not None:
         named_table_args = {
--- python/pyarrow/tests/test_substrait.py
+++ python/pyarrow/tests/test_substrait.py
@@ -40,10 +40,12 @@ def _write_dummy_data_to_disk(tmpdir, file_name, table):
     return path
 
 
-@pytest.mark.skipif(sys.platform == 'win32',
-                    reason="ARROW-16392: file based URI is" +
-                    " not fully supported for Windows")
-def test_run_serialized_query(tmpdir):
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="ARROW-16392: file based URI is not fully supported for Windows",
+)
+@pytest.mark.parametrize("input_type", ("buffer", "bytes", "unsupported"))
+def test_run_serialized_query(tmpdir, input_type):
     substrait_query = """
     {
         "relations": [
@@ -74,11 +76,21 @@ def test_run_serialized_query(tmpdir):
     """
 
     file_name = "read_data.arrow"
-    table = pa.table([[1, 2, 3, 4, 5]], names=['foo'])
+    table = pa.table([[1, 2, 3, 4, 5]], names=["foo"])
     path = _write_dummy_data_to_disk(tmpdir, file_name, table)
     query = tobytes(substrait_query.replace("FILENAME_PLACEHOLDER", path))
 
     buf = pa._substrait._parse_json_plan(query)
+
+    # Passing unsupported type, like int, will not segfault.
+    if input_type == "unsupported":
+        with pytest.raises(TypeError, match="No matching signature found"):
+            substrait.run_query(1)
+        return
+
+    # Ensure user can pass bytes directly as well.
+    if input_type == "bytes":
+        buf = buf.to_pybytes()
 
     reader = substrait.run_query(buf)
     res_tb = reader.read_all()
