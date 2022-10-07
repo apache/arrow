8a1f7cdb7996fa5ca49b474138ca6f87e67f88df
SHIMA Tatsuya <ts1s1andn@gmail.com>
2022-10-07 22:22:42 +0000

    fix rebasing
    Signed-off-by: SHIMA Tatsuya <ts1s1andn@gmail.com>

--- r/tests/testthat/test-dplyr-query.R
+++ r/tests/testthat/test-dplyr-query.R
@@ -597,33 +597,6 @@ test_that("compute() on a grouped query returns a Table with groups in metadata"
     tbl %>%
       group_by(int)
   )
-  expect_equal(
-    collect(tab1),
-    tbl %>%
-      group_by(int)
-  )
-})
-
-test_that("collect() is identical to compute() %>% collect()", {
-  tab1 <- tbl %>%
-    arrow_table()
-  adq1 <- tab1 %>%
-    group_by(int)
-
-  expect_equal(
-    tab1 %>%
-      compute() %>%
-      collect(),
-    tab1 %>%
-      collect()
-  )
-  expect_equal(
-    adq1 %>%
-      compute() %>%
-      collect(),
-    adq1 %>%
-      collect()
-  )
 })
 
 test_that("collect() is identical to compute() %>% collect()", {
