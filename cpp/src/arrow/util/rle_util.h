90b15aa29a61d4bd6a88b800ad5af9b6dd825a2c
Tobias Zagorni <tobias@zagorni.eu>
2022-10-07 20:05:59 +0200

    rle_util: support different types for run ends array

--- cpp/src/arrow/util/rle_util.h
+++ cpp/src/arrow/util/rle_util.h
@@ -31,7 +31,8 @@ namespace rle_util {
 /// \brief Get the physical offset from a logical offset given run end values using binary
 /// search. Returns num_run_ends if the physical offset is not within the first
 /// num_run_ends elements.
-int64_t FindPhysicalOffset(const int32_t* run_ends, int64_t num_run_ends,
+template <typename RunEndsType>
+int64_t FindPhysicalOffset(const RunEndsType* run_ends, int64_t num_run_ends,
                            int64_t logical_offset);
 
 /// \brief Get the physical offset of an RLE ArraySpan. Warning: calling this may result
@@ -49,8 +50,9 @@ static inline const ArraySpan& RunEndsArray(const ArraySpan& span) {
 }
 
 /// \brief Get a pointer to run ends values of an RLE array
-static inline const int32_t* RunEnds(const ArraySpan& span) {
-  return RunEndsArray(span).GetValues<int32_t>(1);
+template <typename RunEndsType>
+static inline const RunEndsType* RunEnds(const ArraySpan& span) {
+  return RunEndsArray(span).GetValues<RunEndsType>(1);
 }
 
 /// \brief Get the child array holding the data values from an RLE array
@@ -60,63 +62,60 @@ static inline const ArraySpan& ValuesArray(const ArraySpan& span) {
 
 /// \brief Iterate over two run-length encoded arrays in segments of runs that are inside
 /// run boundaries in each input
-template <size_t NUM_INPUTS>
+template <typename... RunEndsTypes>
 class MergedRunsIterator {
  public:
-  // end iterator
-  MergedRunsIterator() {}
-
-  // TODO: genereric constructor
-  MergedRunsIterator(const ArraySpan& a) {
-    static_assert(NUM_INPUTS == 1, "incorrect number of inputs");
-
-    inputs[0] = &a;
-
-    logical_length = a.length;
-
-    for (size_t input_id = 0; input_id < NUM_INPUTS; input_id++) {
-      const ArraySpan* input = inputs[input_id];
-      run_index[input_id] = rle_util::FindPhysicalOffset(
-          RunEnds(*input), RunEndsArray(*input).length, input->offset);
-    }
-    if (!isEnd()) {
-      FindMergedRun();
+  static constexpr size_t NUM_INPUTS = sizeof...(RunEndsTypes);
+  template <typename... InputTypes>
+  explicit MergedRunsIterator(InputTypes&... array_spans) : inputs(array_spans...) {
+    static_assert(sizeof...(InputTypes) == sizeof...(RunEndsTypes),
+                  "number of run ends types and input ArraySpans must be the same");
+    if constexpr (NUM_INPUTS == 0) {
+      // end interator
+      logical_length_ = 0;
+    } else {
+      logical_length_ = FindCommonLength();
+      if (!isEnd()) {
+        FindMergedRun();
+      }
     }
   }
 
-  MergedRunsIterator(const ArraySpan& a, const ArraySpan& b) {
-    static_assert(NUM_INPUTS == 2, "incorrect number of inputs");
-
-    inputs[0] = &a;
-    inputs[1] = &b;
-
-    ARROW_DCHECK_EQ(a.length, b.length);
-    logical_length = a.length;
-
-    for (size_t input_id = 0; input_id < NUM_INPUTS; input_id++) {
-      const ArraySpan* input = inputs[input_id];
-      run_index[input_id] = rle_util::FindPhysicalOffset(
-          RunEnds(*input), RunEndsArray(*input).length, input->offset);
-    }
-    if (!isEnd()) {
-      FindMergedRun();
+  /*explicit MergedRunsIterator(ArraySpan array_span) : inputs(Input<int32_t>(array_span))
+  {
+    //static_assert(sizeof...(InputTypes) == sizeof...(RunEndsTypes), "number of run ends
+  types and input ArraySpans must be the same"); if constexpr (NUM_INPUTS == 0) {
+      // end interator
+      logical_length_ = 0;
+    } else {
+      logical_length_ = FindCommonLength();
+      if (!isEnd()) {
+        FindMergedRun();
+      }
     }
   }
+  explicit MergedRunsIterator(ArraySpan array_span_a, ArraySpan array_span_b) :
+  inputs(Input<int32_t>(array_span_a), Input<int32_t>(array_span_b)) {
+    //static_assert(sizeof...(InputTypes) == sizeof...(RunEndsTypes), "number of run ends
+  types and input ArraySpans must be the same"); if constexpr (NUM_INPUTS == 0) {
+      // end interator
+      logical_length_ = 0;
+    } else {
+      logical_length_ = FindCommonLength();
+      if (!isEnd()) {
+        FindMergedRun();
+      }
+    }
+  }*/
 
   MergedRunsIterator(const MergedRunsIterator& other) = default;
 
   MergedRunsIterator& operator++() {
-    logical_position = merged_run_end;
-
-    for (size_t input_id = 0; input_id < NUM_INPUTS; input_id++) {
-      if (logical_position == run_end[input_id]) {
-        run_index[input_id]++;
-      }
-    }
+    logical_position_ = merged_run_end_;
+    IncrementInputs();
     if (!isEnd()) {
       FindMergedRun();
     }
-
     return *this;
   }
 
@@ -126,52 +125,100 @@ class MergedRunsIterator {
     return result;
   }
 
-  bool operator==(const MergedRunsIterator& other) const {
+  template <typename... OthersInputs>
+  bool operator==(const MergedRunsIterator<OthersInputs...>& other) const {
     return (isEnd() && other.isEnd()) ||
-           (!isEnd() && !other.isEnd() && logical_position == other.logical_position);
+           (!isEnd() && !other.isEnd() && logical_position_ == other.logical_position());
   }
 
-  bool operator!=(const MergedRunsIterator& other) const { return !(*this == other); }
+  template <typename... OthersInputs>
+  bool operator!=(const MergedRunsIterator<OthersInputs...>& other) const {
+    return !(*this == other);
+  }
 
   /// \brief returns a physical index into the values array buffers of a given input,
   /// pointing to the value of the current run. The index includes the array offset, so it
   /// can be used to access a buffer directly
-  int64_t index_into_buffer(int64_t input_id) const {
-    return run_index[input_id] + ValuesArray(*inputs[input_id]).offset;
+  template <size_t input_id>
+  int64_t index_into_buffer() const {
+    auto& input = std::get<input_id>(inputs);
+    return input.run_index + ValuesArray(input.array_span).offset;
   }
   /// \brief returns a physical index into the values array of a given input, pointing to
   /// the value of the current run
-  int64_t index_into_array(int64_t input_id) const { return run_index[input_id]; }
+  template <size_t input_id>
+  int64_t index_into_array() const {
+    return std::get<input_id>(inputs).run_index;
+  }
   /// \brief returns the logical length of the current run
-  int64_t run_length() const { return merged_run_end - logical_position; }
+  int64_t run_length() const { return merged_run_end_ - logical_position_; }
   /// \brief returns the accumulated length of all runs from the beginning of the array
   /// including the current one
-  int64_t accumulated_run_length() const { return merged_run_end; }
+  int64_t accumulated_run_length() const { return merged_run_end_; }
+
+  bool isEnd() const { return logical_position_ == logical_length_; }
+  int64_t logical_position() const { return logical_position_; }
 
  private:
-  void FindMergedRun() {
-    merged_run_end = std::numeric_limits<int64_t>::max();
-    for (size_t input_id = 0; input_id < NUM_INPUTS; input_id++) {
-      // logical indices of the end of the run we are currently in each input
-      run_end[input_id] =
-          RunEnds(*inputs[input_id])[run_index[input_id]] - inputs[input_id]->offset;
-      // the logical length may end in the middle of a run, in case the array was sliced
-      run_end[input_id] = std::min(run_end[input_id], logical_length);
-      ARROW_DCHECK_GT(run_end[input_id], logical_position);
+  template <typename RunEndsType>
+  struct Input {
+    Input(const ArraySpan& array_span) : array_span{array_span} {
+      run_ends = RunEnds<RunEndsType>(array_span);
+      run_index = rle_util::FindPhysicalOffset(run_ends, RunEndsArray(array_span).length,
+                                               array_span.offset);
+      // actual value found later by FindMergedRun:
+      current_run_end = 0;
+    }
 
-      merged_run_end = std::min(merged_run_end, run_end[input_id]);
+    const ArraySpan& array_span;
+    const RunEndsType* run_ends;
+    int64_t run_index;
+    int64_t current_run_end;
+  };
+
+  template <size_t input_id = 0>
+  void FindMergedRun() {
+    if constexpr (input_id == 0) {
+      merged_run_end_ = std::numeric_limits<int64_t>::max();
+    }
+    auto& input = std::get<input_id>(inputs);
+    // logical indices of the end of the run we are currently in each input
+    input.current_run_end = input.run_ends[input.run_index] - input.array_span.offset;
+    // the logical length may end in the middle of a run, in case the array was sliced
+    input.current_run_end = std::min(input.current_run_end, logical_length_);
+    ARROW_DCHECK_GT(input.current_run_end, logical_position_);
+    merged_run_end_ = std::min(merged_run_end_, input.current_run_end);
+    if constexpr (input_id < NUM_INPUTS - 1) {
+      FindMergedRun<input_id + 1>();
     }
   }
 
-  bool isEnd() const { return logical_position == logical_length; }
+  template <size_t input_id = 0>
+  int64_t FindCommonLength() {
+    int64_t our_length = std::get<input_id>(inputs).array_span.length;
+    if constexpr (input_id < NUM_INPUTS - 1) {
+      int64_t other_length = FindCommonLength<input_id + 1>();
+      ARROW_CHECK_EQ(our_length, other_length)
+          << "MergedRunsIteratror can only be used on arrays of the same length";
+    }
+    return our_length;
+  }
 
-  std::array<const ArraySpan*, NUM_INPUTS> inputs;
-  std::array<int64_t, NUM_INPUTS> run_index;
-  // logical indices of the end of the run we are currently in each input
-  std::array<int64_t, NUM_INPUTS> run_end;
-  int64_t logical_position = 0;
-  int64_t logical_length = 0;
-  int64_t merged_run_end;
+  template <size_t input_id = 0>
+  void IncrementInputs() {
+    auto& input = std::get<input_id>(inputs);
+    if (logical_position_ == input.current_run_end) {
+      input.run_index++;
+    }
+    if constexpr (input_id < NUM_INPUTS - 1) {
+      IncrementInputs<input_id + 1>();
+    }
+  }
+
+  std::tuple<Input<RunEndsTypes>...> inputs;
+  int64_t logical_position_ = 0;
+  int64_t logical_length_ = 0;
+  int64_t merged_run_end_;
 };
 
 // TODO: this may fit better into some testing header
--- cpp/src/arrow/util/rle_util_test.cc
+++ cpp/src/arrow/util/rle_util_test.cc
@@ -21,42 +21,50 @@
 #include "arrow/builder.h"
 #include "arrow/compute/api_vector.h"
 #include "arrow/testing/gtest_util.h"
+#include "arrow/type_traits.h"
 #include "arrow/util/rle_util.h"
 
 namespace arrow {
 namespace rle_util {
 
-TEST(TestRleUtil, FindPhysicalOffsetTest) {
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){1}, 1, 0), 0);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){1, 2, 3}, 3, 0), 0);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){1, 2, 3}, 3, 1), 1);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){1, 2, 3}, 3, 2), 2);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 3, 4}, 3, 0), 0);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 3, 4}, 3, 1), 0);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 3, 4}, 3, 2), 1);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 3, 4}, 3, 3), 2);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 4, 6}, 3, 3), 1);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){1, 2, 3, 4, 5, 6, 7, 8, 9, 1000, 1005,
-                                                 1015, 1020, 1025, 1050},
+template <typename RunEndsType>
+struct RleUtilTest : public ::testing::Test {};
+TYPED_TEST_SUITE_P(RleUtilTest);
+
+TYPED_TEST_P(RleUtilTest, PhysicalOffset) {
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){1}, 1, 0), 0);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){1, 2, 3}, 3, 0), 0);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){1, 2, 3}, 3, 1), 1);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){1, 2, 3}, 3, 2), 2);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 3, 4}, 3, 0), 0);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 3, 4}, 3, 1), 0);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 3, 4}, 3, 2), 1);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 3, 4}, 3, 3), 2);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 4, 6}, 3, 3), 1);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){1, 2, 3, 4, 5, 6, 7, 8, 9, 1000, 1005,
+                                                   1015, 1020, 1025, 1050},
                                15, 1000),
             10);
 
   // out-of-range logical offset should return num_run_ends
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 4, 6}, 3, 6), 3);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 4, 6}, 3, 10000), 3);
-  ASSERT_EQ(FindPhysicalOffset((const int32_t[]){2, 4, 6}, 0, 5), 0);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 4, 6}, 3, 6), 3);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 4, 6}, 3, 10000), 3);
+  ASSERT_EQ(FindPhysicalOffset((const TypeParam[]){2, 4, 6}, 0, 5), 0);
 }
 
-TEST(TestRleUtil, ArtificalOffset) {
+TYPED_TEST_P(RleUtilTest, ArtificalOffset) {
+  const std::shared_ptr<DataType> run_ends_type =
+      std::make_shared<typename CTypeTraits<TypeParam>::ArrowType>();
+
   const auto values = ArrayFromJSON(int32(), "[1, 2, 3]");
-  const auto run_ends = ArrayFromJSON(int32(), "[10, 20, 30]");
+  const auto run_ends = ArrayFromJSON(run_ends_type, "[10, 20, 30]");
   ASSERT_OK_AND_ASSIGN(auto array, RunLengthEncodedArray::Make(run_ends, values, 30));
   AddArtificialOffsetInChildArray(array->data().get(), 100);
   ASSERT_ARRAYS_EQUAL(*values, *array->values_array());
   ASSERT_EQ(array->values_array()->offset(), 100);
 }
 
-TEST(TestRleUtil, MergedRunsInterator) {
+TYPED_TEST_P(RleUtilTest, MergedRunsInterator) {
   /* Construct the following two test arrays with a lot of different offsets to test the
    * RLE iterator: left:
    *
@@ -93,10 +101,13 @@ TEST(TestRleUtil, MergedRunsInterator) {
    *                                 logical offset: 2000
    *                                  physical offset: 5
    */
+  const std::shared_ptr<DataType> run_ends_type =
+      std::make_shared<typename CTypeTraits<TypeParam>::ArrowType>();
+
   const auto left_run_ends = ArrayFromJSON(
-      int32(), "[1, 2, 3, 4, 5, 6, 7, 8, 9, 1000, 1005, 1015, 1020, 1025, 30000]");
+      run_ends_type, "[1, 2, 3, 4, 5, 6, 7, 8, 9, 1000, 1005, 1015, 1020, 1025, 30000]");
   const auto right_run_ends =
-      ArrayFromJSON(int32(), "[1, 2, 3, 4, 5, 2005, 2009, 2025, 2050]");
+      ArrayFromJSON(run_ends_type, "[1, 2, 3, 4, 5, 2005, 2009, 2025, 2050]");
   const std::vector<int32_t> expected_run_lengths = {5, 4, 6, 5, 5, 25};
   const std::vector<int32_t> expected_left_visits = {110, 111, 111, 112, 113, 114};
   const std::vector<int32_t> expected_right_visits = {205, 206, 207, 207, 207, 208};
@@ -124,13 +135,14 @@ TEST(TestRleUtil, MergedRunsInterator) {
 
   size_t position = 0;
   size_t logical_position = 0;
-  for (auto it = MergedRunsIterator<2>(left_span, right_span);
-       it != MergedRunsIterator<2>(); ++it) {
+  for (auto it = MergedRunsIterator<TypeParam, TypeParam>(left_span, right_span);
+       it != MergedRunsIterator(); ++it) {
     ASSERT_EQ(it.run_length(), expected_run_lengths[position]);
-    ASSERT_EQ(it.index_into_buffer(0), expected_left_visits[position]);
-    ASSERT_EQ(it.index_into_buffer(1), expected_right_visits[position]);
-    ASSERT_EQ(it.index_into_array(0), expected_left_visits[position] - left_child_offset);
-    ASSERT_EQ(it.index_into_array(1),
+    ASSERT_EQ(it.template index_into_buffer<0>(), expected_left_visits[position]);
+    ASSERT_EQ(it.template index_into_buffer<1>(), expected_right_visits[position]);
+    ASSERT_EQ(it.template index_into_array<0>(),
+              expected_left_visits[position] - left_child_offset);
+    ASSERT_EQ(it.template index_into_array<1>(),
               expected_right_visits[position] - right_child_offset);
     position++;
     logical_position += it.run_length();
@@ -143,13 +155,13 @@ TEST(TestRleUtil, MergedRunsInterator) {
 
   position = 0;
   logical_position = 0;
-  for (auto it = MergedRunsIterator<2>(left_span, left_span);
-       it != MergedRunsIterator<2>(); ++it) {
+  for (auto it = MergedRunsIterator<TypeParam, TypeParam>(left_span, left_span);
+       it != MergedRunsIterator(); ++it) {
     ASSERT_EQ(it.run_length(), left_only_run_lengths[position]);
-    ASSERT_EQ(it.index_into_buffer(0), 110 + position);
-    ASSERT_EQ(it.index_into_buffer(1), 110 + position);
-    ASSERT_EQ(it.index_into_array(0), 10 + position);
-    ASSERT_EQ(it.index_into_array(1), 10 + position);
+    ASSERT_EQ(it.template index_into_buffer<0>(), 110 + position);
+    ASSERT_EQ(it.template index_into_buffer<1>(), 110 + position);
+    ASSERT_EQ(it.template index_into_array<0>(), 10 + position);
+    ASSERT_EQ(it.template index_into_array<1>(), 10 + position);
     position++;
     logical_position += it.run_length();
     ASSERT_EQ(it.accumulated_run_length(), logical_position);
@@ -158,10 +170,11 @@ TEST(TestRleUtil, MergedRunsInterator) {
 
   position = 0;
   logical_position = 0;
-  for (auto it = MergedRunsIterator<1>(left_span); it != MergedRunsIterator<1>(); ++it) {
+  for (auto it = MergedRunsIterator<TypeParam>(left_span); it != MergedRunsIterator();
+       ++it) {
     ASSERT_EQ(it.run_length(), left_only_run_lengths[position]);
-    ASSERT_EQ(it.index_into_buffer(0), 110 + position);
-    ASSERT_EQ(it.index_into_array(0), 10 + position);
+    ASSERT_EQ(it.template index_into_buffer<0>(), 110 + position);
+    ASSERT_EQ(it.template index_into_array<0>(), 10 + position);
     position++;
     logical_position += it.run_length();
     ASSERT_EQ(it.accumulated_run_length(), logical_position);
@@ -173,13 +186,13 @@ TEST(TestRleUtil, MergedRunsInterator) {
 
   position = 0;
   logical_position = 0;
-  for (auto it = MergedRunsIterator<2>(right_span, right_span);
-       it != MergedRunsIterator<2>(); ++it) {
+  for (auto it = MergedRunsIterator<TypeParam, TypeParam>(right_span, right_span);
+       it != MergedRunsIterator(); ++it) {
     ASSERT_EQ(it.run_length(), right_only_run_lengths[position]);
-    ASSERT_EQ(it.index_into_buffer(0), 205 + position);
-    ASSERT_EQ(it.index_into_buffer(1), 205 + position);
-    ASSERT_EQ(it.index_into_array(0), 5 + position);
-    ASSERT_EQ(it.index_into_array(1), 5 + position);
+    ASSERT_EQ(it.template index_into_buffer<0>(), 205 + position);
+    ASSERT_EQ(it.template index_into_buffer<1>(), 205 + position);
+    ASSERT_EQ(it.template index_into_array<0>(), 5 + position);
+    ASSERT_EQ(it.template index_into_array<1>(), 5 + position);
     position++;
     logical_position += it.run_length();
     ASSERT_EQ(it.accumulated_run_length(), logical_position);
@@ -188,16 +201,22 @@ TEST(TestRleUtil, MergedRunsInterator) {
 
   position = 0;
   logical_position = 0;
-  for (auto it = MergedRunsIterator<1>(right_span); it != MergedRunsIterator<1>(); ++it) {
+  for (auto it = MergedRunsIterator<TypeParam>(right_span); it != MergedRunsIterator();
+       ++it) {
     ASSERT_EQ(it.run_length(), right_only_run_lengths[position]);
-    ASSERT_EQ(it.index_into_buffer(0), 205 + position);
-    ASSERT_EQ(it.index_into_array(0), 5 + position);
+    ASSERT_EQ(it.template index_into_buffer<0>(), 205 + position);
+    ASSERT_EQ(it.template index_into_array<0>(), 5 + position);
     position++;
     logical_position += it.run_length();
     ASSERT_EQ(it.accumulated_run_length(), logical_position);
   }
   ASSERT_EQ(position, right_only_run_lengths.size());
 }
+
+REGISTER_TYPED_TEST_SUITE_P(RleUtilTest, PhysicalOffset, ArtificalOffset,
+                            MergedRunsInterator);
+using RunEndsTypes = testing::Types<int16_t, int32_t, int64_t>;
+INSTANTIATE_TYPED_TEST_SUITE_P(RleUtilTestt, RleUtilTest, RunEndsTypes);
 
 }  // namespace rle_util
 }  // namespace arrow
