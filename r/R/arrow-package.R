7f8b8cf655bd2540f291b743044cc1c1bc659d3b
Nic Crane <thisisnic@gmail.com>
2022-10-08 10:01:39 +0100

    Import expr_text

--- r/R/arrow-package.R
+++ r/R/arrow-package.R
@@ -26,7 +26,7 @@
 #' @importFrom rlang expr caller_env is_character quo_name is_quosure enexpr enexprs as_quosure
 #' @importFrom rlang is_list call2 is_empty as_function as_label arg_match is_symbol is_call call_args
 #' @importFrom rlang quo_set_env quo_get_env is_formula quo_is_call f_rhs parse_expr f_env new_quosure
-#' @importFrom rlang new_quosures
+#' @importFrom rlang new_quosures expr_text
 #' @importFrom tidyselect vars_pull vars_rename vars_select eval_select
 #' @importFrom glue glue
 #' @useDynLib arrow, .registration = TRUE
