bf6e7a7b80272cc8768794958ce2e0e8c53c00c8
Neal Richardson <neal.p.richardson@gmail.com>
2022-10-10 09:31:30 -0400

    ARROW-13766: [R] Add slice_*() methods

--- r/R/arrow-package.R
+++ r/R/arrow-package.R
@@ -64,6 +64,31 @@ supported_dplyr_methods <- list(
   rename_with = NULL,
   union = NULL,
   union_all = NULL,
+  slice_head = c(
+    "slicing within groups not supported;",
+    "Arrow datasets do not have row order, so head is non-deterministic;",
+    "`prop` only supported on queries where `nrow()` is knowable without evaluating"
+  ),
+  slice_tail = c(
+    "slicing within groups not supported;",
+    "Arrow datasets do not have row order, so tail is non-deterministic;",
+    "`prop` only supported on queries where `nrow()` is knowable without evaluating"
+  ),
+  slice_min = c(
+    "slicing within groups not supported;",
+    "`with_ties = TRUE` (dplyr default) is not supported;",
+    "`prop` only supported on queries where `nrow()` is knowable without evaluating"
+  ),
+  slice_max = c(
+    "slicing within groups not supported;",
+    "`with_ties = TRUE` (dplyr default) is not supported;",
+    "`prop` only supported on queries where `nrow()` is knowable without evaluating"
+  ),
+  slice_sample = c(
+    "slicing within groups not supported;",
+    "`replace = TRUE` and the `weight_by` argument not supported;",
+    "`n` only supported on queries where `nrow()` is knowable without evaluating"
+  ),
   glimpse = NULL,
   show_query = NULL,
   explain = NULL
---
+++ r/tests/testthat/test-dplyr-slice.R
@@ -0,0 +1,168 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+library(dplyr, warn.conflicts = FALSE)
+
+tbl <- example_data
+
+test_that("slice_head/tail, ungrouped", {
+  # head/tail are not deterministic in Arrow because data is unordered
+  # so we can't assert identical to dplyr, just assert right number of rows
+  tab <- arrow_table(tbl)
+  expect_equal(
+    tab %>%
+      slice_head(n = 5) %>%
+      nrow(),
+    5
+  )
+  expect_equal(
+    tab %>%
+      slice_tail(n = 5) %>%
+      nrow(),
+    5
+  )
+
+  expect_equal(
+    tab %>%
+      slice_head(prop = .2) %>%
+      nrow(),
+    2
+  )
+  expect_equal(
+    tab %>%
+      slice_tail(prop = .2) %>%
+      nrow(),
+    2
+  )
+})
+
+test_that("slice_min/max, ungrouped", {
+  # with_ties must be FALSE
+  tab <- arrow_table(tbl)
+  expect_error(
+    tab %>% slice_max(int, n = 5),
+    "with_ties = TRUE"
+  )
+  expect_error(
+    tab %>% slice_min(int, n = 5),
+    "with_ties = TRUE"
+  )
+  compare_dplyr_binding(
+    .input %>%
+      slice_max(int, n = 4, with_ties = FALSE) %>%
+      collect(),
+    tbl
+  )
+  compare_dplyr_binding(
+    .input %>%
+      slice_min(int, n = 4, with_ties = FALSE) %>%
+      collect(),
+    tbl
+  )
+
+  compare_dplyr_binding(
+    .input %>%
+      slice_max(int, prop = .2, with_ties = FALSE) %>%
+      collect(),
+    tbl
+  )
+  compare_dplyr_binding(
+    .input %>%
+      slice_min(int, prop = .2, with_ties = FALSE) %>%
+      collect(),
+    tbl
+  )
+})
+
+test_that("slice_sample, ungrouped", {
+  tab <- arrow_table(tbl)
+  expect_error(
+    tab %>% slice_sample(replace = TRUE),
+    "Sampling with replacement"
+  )
+  expect_error(
+    tab %>% slice_sample(weight_by = dbl),
+    "weight_by"
+  )
+
+  skip("random() doesn't actually work")
+  # ! Invalid: ExecuteScalarExpression cannot Execute non-scalar expression Array[double]
+  tab %>%
+    slice_sample(prop = .25) %>%
+    collect()
+
+  # TODO: test that slice_sample(n) returns n rows (might need larger dataset)
+})
+
+test_that("slice_* not supported with groups", {
+  grouped <- tbl %>%
+    arrow_table() %>%
+    group_by(lgl)
+  expect_error(
+    slice_head(grouped, n = 5),
+    "Slicing Arrow data with groups"
+  )
+  expect_error(
+    slice_tail(grouped, n = 5),
+    "Slicing Arrow data with groups"
+  )
+  expect_error(
+    slice_min(grouped, n = 5),
+    "Slicing Arrow data with groups"
+  )
+  expect_error(
+    slice_max(grouped, n = 5),
+    "Slicing Arrow data with groups"
+  )
+  expect_error(
+    slice_sample(grouped, n = 5),
+    "Slicing Arrow data with groups"
+  )
+})
+
+test_that("input validation: prop", {
+  tab <- arrow_table(tbl)
+  for (p in list("a", -1, 2, c(.01, .02), NA_real_)) {
+    expect_error(
+      slice_head(tab, prop = !!p),
+      "`prop` must be a single numeric value in [0, 1]",
+      fixed = TRUE
+    )
+  }
+})
+
+test_that("n <-> prop conversion when nrow is not known", {
+  joined <- tbl %>%
+    arrow_table() %>%
+    full_join(tbl, by = "int")
+  expect_true(is.na(nrow(joined)))
+
+  expect_error(
+    joined %>%
+      slice_min(int, prop = .2, with_ties = FALSE),
+    "Slicing with `prop` when"
+  )
+
+  expect_error(
+    joined %>%
+      slice_sample(n = 5),
+    "slice_sample() with `n` when",
+    fixed = TRUE
+  )
+})
+
+# TODO: handle edge case where prop = 1, do nothing?
