b58aac10b6434c3607b8c0a355d179632c7d8a18
Miles Granger <miles59923@gmail.com>
2022-10-10 14:58:57 +0200

    Allow scalars when creating expression from compute kernels

--- python/pyarrow/_compute.pyx
+++ python/pyarrow/_compute.pyx
@@ -24,7 +24,7 @@ from cython.operator cimport dereference as deref
 
 from collections import namedtuple
 
-from pyarrow.lib import frombytes, tobytes, ordered_dict
+from pyarrow.lib import frombytes, tobytes, ordered_dict, ArrowInvalid
 from pyarrow.lib cimport *
 from pyarrow.includes.common cimport *
 from pyarrow.includes.libarrow cimport *
@@ -2234,7 +2234,13 @@ cdef class Expression(_Weakrefable):
 
         for argument in arguments:
             if not isinstance(argument, Expression):
-                raise TypeError("only other expressions allowed as arguments")
+                # Attempt to help convert this to an expression
+                try:
+                    import pyarrow.compute as pc
+                    argument = pc.scalar(argument)
+                except (ImportError, ArrowInvalid):
+                    raise TypeError(
+                        "only other expressions allowed as arguments")
             c_arguments.push_back((<Expression> argument).expr)
 
         if options is not None:
