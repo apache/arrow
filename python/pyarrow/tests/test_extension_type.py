21ba22bfbb3123a224a320832a4bca0a7237309f
Miles Granger <miles59923@gmail.com>
2022-10-12 15:27:23 +0200

    ARROW-17813: [Python] Nested ExtensionArray conversion to/from pandas/numpy (#14238)
    [ARROW-17813](https://issues.apache.org/jira/browse/ARROW-17813)
    
    Lead-authored-by: Miles Granger <miles59923@gmail.com>
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>
    Signed-off-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- python/pyarrow/tests/test_extension_type.py
+++ python/pyarrow/tests/test_extension_type.py
@@ -78,6 +78,15 @@ class UuidType2(pa.PyExtensionType):
         return UuidType2, ()
 
 
+class LabelType(pa.PyExtensionType):
+
+    def __init__(self):
+        pa.PyExtensionType.__init__(self, pa.string())
+
+    def __reduce__(self):
+        return LabelType, ()
+
+
 class ParamExtType(pa.PyExtensionType):
 
     def __init__(self, width):
@@ -1018,6 +1027,26 @@ def test_empty_take():
     result = empty_arr.take(pa.array([], pa.int32()))
     assert len(result) == 0
     assert result.equals(empty_arr)
+
+
+@pytest.mark.parametrize("data,ty", (
+    ([1, 2, 3], IntegerType),
+    (["cat", "dog", "horse"], LabelType)
+))
+@pytest.mark.parametrize("into", ("to_numpy", "to_pandas"))
+def test_extension_array_to_numpy_pandas(data, ty, into):
+    storage = pa.array(data)
+    ext_arr = pa.ExtensionArray.from_storage(ty(), storage)
+    offsets = pa.array([0, 1, 2, 3])
+    list_arr = pa.ListArray.from_arrays(offsets, ext_arr)
+    result = getattr(list_arr, into)(zero_copy_only=False)
+
+    list_arr_storage_type = list_arr.cast(pa.list_(ext_arr.type.storage_type))
+    expected = getattr(list_arr_storage_type, into)(zero_copy_only=False)
+    if into == "to_pandas":
+        assert result.equals(expected)
+    else:
+        assert np.array_equal(result, expected)
 
 
 def test_array_constructor():
