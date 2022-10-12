3b8d54e3e53ae363274c787517b535e392af121a
Sutou Kouhei <kou@clear-code.com>
2022-10-12 16:24:28 +0900

    ARROW-17997: [Ruby] Add support for building Arrow::Tensor from raw nested Ruby array

--- ruby/red-arrow/test/test-tensor.rb
+++ ruby/red-arrow/test/test-tensor.rb
@@ -16,6 +16,103 @@
 # under the License.
 
 class TensorTest < Test::Unit::TestCase
+  sub_test_case("class methods") do
+    sub_test_case(".new") do
+      def setup
+        @raw_tensor = [
+          [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+          ],
+          [
+            [9, 10, 11, 12],
+            [13, 14, 15, 16],
+          ],
+          [
+            [17, 18, 19, 20],
+            [21, 22, 23, 24],
+          ],
+        ]
+        @shape = [3, 2, 4]
+        @strides = [8, 4, 1]
+      end
+
+      test("Array") do
+        tensor = Arrow::Tensor.new(@raw_tensor)
+        assert_equal({
+                       value_data_type: Arrow::UInt8DataType.new,
+                       buffer: @raw_tensor.flatten.pack("C*"),
+                       shape: @shape,
+                       strides: @strides,
+                       dimension_names: ["", "", ""],
+                     },
+                     {
+                       value_data_type: tensor.value_data_type,
+                       buffer: tensor.buffer.data.to_s,
+                       shape: tensor.shape,
+                       strides: tensor.strides,
+                       dimension_names: tensor.dimension_names,
+                     })
+      end
+
+      test("Array, data_type: Symbol") do
+        tensor = Arrow::Tensor.new(@raw_tensor, data_type: :int32)
+        assert_equal({
+                       value_data_type: Arrow::Int32DataType.new,
+                       buffer: @raw_tensor.flatten.pack("l*"),
+                       shape: @shape,
+                       strides: @strides.collect {|x| x * 4},
+                       dimension_names: ["", "", ""],
+                     },
+                     {
+                       value_data_type: tensor.value_data_type,
+                       buffer: tensor.buffer.data.to_s,
+                       shape: tensor.shape,
+                       strides: tensor.strides,
+                       dimension_names: tensor.dimension_names,
+                     })
+      end
+
+      test("Array, dimension_names: Array<String>") do
+        tensor = Arrow::Tensor.new(@raw_tensor,
+                                   dimension_names: ["a", "b", "c"])
+        assert_equal({
+                       value_data_type: Arrow::UInt8DataType.new,
+                       buffer: @raw_tensor.flatten.pack("C*"),
+                       shape: @shape,
+                       strides: @strides,
+                       dimension_names: ["a", "b", "c"],
+                     },
+                     {
+                       value_data_type: tensor.value_data_type,
+                       buffer: tensor.buffer.data.to_s,
+                       shape: tensor.shape,
+                       strides: tensor.strides,
+                       dimension_names: tensor.dimension_names,
+                     })
+      end
+
+      test("Array, dimension_names: Array<Symbol>") do
+        tensor = Arrow::Tensor.new(@raw_tensor,
+                                   dimension_names: [:a, :b, :c])
+        assert_equal({
+                       value_data_type: Arrow::UInt8DataType.new,
+                       buffer: @raw_tensor.flatten.pack("C*"),
+                       shape: @shape,
+                       strides: @strides,
+                       dimension_names: ["a", "b", "c"],
+                     },
+                     {
+                       value_data_type: tensor.value_data_type,
+                       buffer: tensor.buffer.data.to_s,
+                       shape: tensor.shape,
+                       strides: tensor.strides,
+                       dimension_names: tensor.dimension_names,
+                     })
+      end
+    end
+  end
+
   sub_test_case("instance methods") do
     def setup
       raw_data = [
@@ -28,11 +125,11 @@ class TensorTest < Test::Unit::TestCase
         9, 10,
         11, 12,
       ]
-      data = Arrow::Buffer.new(raw_data.pack("c*"))
+      data = Arrow::Buffer.new(raw_data.pack("c*").freeze)
       shape = [3, 2, 2]
       strides = []
       names = ["a", "b", "c"]
-      @tensor = Arrow::Tensor.new(Arrow::Int8DataType.new,
+      @tensor = Arrow::Tensor.new(:int8,
                                   data,
                                   shape,
                                   strides,
