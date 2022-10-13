761781b3a4dd1240832151851d49c1a19286ac6c
Weston Pace <weston.pace@gmail.com>
2022-10-13 11:03:38 -0700

    ARROW-18041: pylint

--- python/pyarrow/tests/test_substrait.py
+++ python/pyarrow/tests/test_substrait.py
@@ -73,7 +73,8 @@ def test_run_serialized_query(tmpdir):
     file_name = "read_data.arrow"
     table = pa.table([[1, 2, 3, 4, 5]], names=['foo'])
     path = _write_dummy_data_to_disk(tmpdir, file_name, table)
-    query = tobytes(substrait_query.replace("FILENAME_PLACEHOLDER", pathlib.Path(path).as_uri()))
+    query = tobytes(substrait_query.replace(
+        "FILENAME_PLACEHOLDER", pathlib.Path(path).as_uri()))
 
     buf = pa._substrait._parse_json_plan(query)
 
@@ -148,7 +149,8 @@ def test_binary_conversion_with_json_options(tmpdir):
     file_name = "binary_json_data.arrow"
     table = pa.table([[1, 2, 3, 4, 5]], names=['bar'])
     path = _write_dummy_data_to_disk(tmpdir, file_name, table)
-    query = tobytes(substrait_query.replace("FILENAME_PLACEHOLDER", pathlib.Path(path).as_uri()))
+    query = tobytes(substrait_query.replace(
+        "FILENAME_PLACEHOLDER", pathlib.Path(path).as_uri()))
     buf = pa._substrait._parse_json_plan(tobytes(query))
 
     reader = substrait.run_query(buf)
