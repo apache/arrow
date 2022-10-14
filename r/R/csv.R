8b68607249eac2cb07e5270726257fa63d2b9ec4
Nic Crane <thisisnic@gmail.com>
2022-10-14 12:43:44 +0100

    Update CSV reader to use eval_select

--- r/R/csv.R
+++ r/R/csv.R
@@ -215,7 +215,8 @@ read_delim_arrow <- function(file,
   # TODO: move this into convert_options using include_columns
   col_select <- enquo(col_select)
   if (!quo_is_null(col_select)) {
-    tab <- tab[vars_select(names(tab), !!col_select)]
+    sim_df <- as.data.frame(tab$schema)
+    tab <- tab[eval_select(col_select, sim_df)]
   }
 
   if (isTRUE(as_data_frame)) {
