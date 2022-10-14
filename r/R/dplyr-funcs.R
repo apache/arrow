033a220a5180a4296a7f79916c61f3862c852d19
Neal Richardson <neal.p.richardson@gmail.com>
2022-10-14 12:06:35 -0400

    Move random UDF definition to inside of slice_sample

--- r/R/dplyr-funcs.R
+++ r/R/dplyr-funcs.R
@@ -139,7 +139,6 @@ call_binding_agg <- function(fun_name, ...) {
   agg_funcs[[fun_name]](...)
 }
 
-#' @importFrom stats runif
 create_binding_cache <- function() {
   # Called in .onLoad()
   .cache$docs <- list()
@@ -163,17 +162,6 @@ create_binding_cache <- function() {
   register_bindings_string()
   register_bindings_type()
   register_bindings_augmented()
-
-  # HACK because random() doesn't work (ARROW-17974)
-  register_scalar_function(
-    "_random_along",
-    function(context, x) {
-      Array$create(runif(length(x)))
-    },
-    in_type = schema(x = boolean()),
-    out_type = float64(),
-    auto_convert = FALSE
-  )
 
   # We only create the cache for nse_funcs and not agg_funcs
   .cache$functions <- c(as.list(nse_funcs), arrow_funcs)
