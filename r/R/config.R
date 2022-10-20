cc860961f3e1b9ecc01f9e935e1b3ce1aeeffa1a
Dewey Dunnington <dewey@voltrondata.com>
2022-10-20 14:11:37 -0300

    export for now

--- r/R/config.R
+++ r/R/config.R
@@ -47,10 +47,17 @@ set_io_thread_count <- function(num_threads) {
   invisible(current_io_thread_count)
 }
 
+#' Manage experimental R code evaluation from within Arrow
+#'
+#' @export
 use_run_with_captured_r <- function() {
   UseRunWithCapturedR()
 }
 
+#' @rdname use_run_with_captured_r
+#' @param run_with_captured_r Use TRUE to enable R code evaluation from within
+#'   arrow
+#' @export
 set_use_run_with_captured_r <- function(run_with_captured_r = TRUE) {
   current <- UseRunWithCapturedR()
   SetUseRunWithCapturedR(run_with_captured_r)
