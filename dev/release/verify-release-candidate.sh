33f2c0ec8e281fc4fe8c03b07ed2d32e343d9b0e
David Li <li.davidm96@gmail.com>
2022-10-22 16:57:27 -0400

    MINOR: [Release] Fix loop in universal2 wheel verification (#14479)
    Authored-by: David Li <li.davidm96@gmail.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- dev/release/verify-release-candidate.sh
+++ dev/release/verify-release-candidate.sh
@@ -1068,11 +1068,12 @@ test_macos_wheels() {
   # the interpreter should be installed from python.org:
   #   https://www.python.org/ftp/python/3.9.6/python-3.9.6-macosx10.9.pkg
   if [ "$(uname -m)" = "arm64" ]; then
-    for pyver in "3.9 3.10"; do
+    for pyver in 3.9 3.10; do
       local python="/Library/Frameworks/Python.framework/Versions/${pyver}/bin/python${pyver}"
 
       # create and activate a virtualenv for testing as arm64
       for arch in "arm64" "x86_64"; do
+        show_header "Testing Python ${pyver} universal2 wheel on ${arch}"
         VENV_ENV=wheel-${pyver}-universal2-${arch} PYTHON=${python} maybe_setup_virtualenv || continue
         # install pyarrow's universal2 wheel
         pip install pyarrow-${VERSION}-cp${pyver/.}-cp${pyver/.}-macosx_11_0_universal2.whl
