4463e9ba648a306eed9b677602889d17e05ad54b
Raúl Cumplido <raulcumplido@gmail.com>
2022-10-25 15:59:17 +0200

    Fix python package to install on MacOS

--- ci/scripts/install_python.sh
+++ ci/scripts/install_python.sh
@@ -46,7 +46,7 @@ full_version=${versions[$2]}
 if [ $platform = "MacOSX" ]; then
     echo "Downloading Python installer..."
 
-    if [ "$(uname -m)" = "arm64" ] || [ "$version" = "3.10" ]; then
+    if [ "$(uname -m)" = "arm64" ] || [ "$version" = "3.10" ] || [ "$version" = "3.11" ]; then
         fname="python-${full_version}-macos11.pkg"
     else
         fname="python-${full_version}-macosx10.9.pkg"
