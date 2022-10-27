7a33ba9aa684a7a3af6f3c6130fcccda15700fd0
Alenka Frim <frim.alenka@gmail.com>
2022-10-27 13:08:31 +0200

    Add a note about ordering to ORCFile.read

--- python/pyarrow/orc.py
+++ python/pyarrow/orc.py
@@ -175,7 +175,8 @@ class ORCFile:
         columns : list
             If not None, only these columns will be read from the file. A
             column name may be a prefix of a nested field, e.g. 'a' will select
-            'a.b', 'a.c', and 'a.d.e'
+            'a.b', 'a.c', and 'a.d.e'. Output always follows the
+            ordering of the file and not the `columns` list.
 
         Returns
         -------
