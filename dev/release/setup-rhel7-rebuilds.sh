4bbccd9478e582bf84dd588b264968f5723f7c13
Benson Muite <bkmgit@users.noreply.github.com>
2022-10-30 14:54:26 +0300

    Use devtoolset11 and set OS variables
    Set $releasever and $basearch following
    https://unix.stackexchange.com/questions/19701/yum-how-can-i-view-variables-like-releasever-basearch-yum0

--- dev/release/setup-rhel7-rebuilds.sh
+++ dev/release/setup-rhel7-rebuilds.sh
@@ -26,8 +26,10 @@ set -exu
 yum -y update
 yum -y groupinstall "Development Tools"
 yum -y install centos-release-scl curl
+releasever="$(rpm --query --file --queryformat '%{VERSION}\n' /etc/system-release)";
+basearch="$(rpm --query --queryformat '%{ARCH}\n' "kernel-$(uname --kernel-release)")";
 cd /etc/pki/rpm-gpg
-curl -O http://springdale.princeton.edu/data/springdale/7/x86_64/os/RPM-GPG-KEY-springdale
+curl -O http://springdale.princeton.edu/data/springdale/$releasever/$basearch/os/RPM-GPG-KEY-springdale
 cd /etc/yum.repos.d
 cat << EOF > Springdale-SCL.repo
 [Springdale-SCL]
@@ -41,7 +43,7 @@ cd $HOME
 yum -y install epel-release
 yum -y install \
   cmake3 \
-  devtoolset-9-* \
+  devtoolset-11-* \
   git \
   gobject-introspection-devel \
   java-11-openjdk-devel \
@@ -67,7 +69,7 @@ alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake3 20 \
   --slave /usr/local/bin/cpack cpack /usr/bin/cpack3 \
   --slave /usr/local/bin/ccmake ccmake /usr/bin/ccmake3 \
   --family cmake
-scl enable rh-python38 devtoolset-9 llvm-toolset-13.0 rh-ruby30 bash
+scl enable rh-python38 devtoolset-11 llvm-toolset-13.0 rh-ruby30 bash
 python -m pip install -U pip
 wget https://www.openssl.org/source/openssl-3.0.5.tar.gz
 tar -xf openssl-3.0.5.tar.gz
