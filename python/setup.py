e21d5aab7690ac3e86c22345a284588a70b892eb
Raúl Cumplido <raulcumplido@gmail.com>
2022-11-04 21:32:09 +0100

    ARROW-17487: [Python][Packaging][CI] Add support for Python 3.11 (#14499)
    This PR adds jobs to build pyarrow wheels for Python 3.11.
    
    Authored-by: Raúl Cumplido <raulcumplido@gmail.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- python/setup.py
+++ python/setup.py
@@ -764,6 +764,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     license='Apache License, Version 2.0',
     maintainer='Apache Arrow Developers',
