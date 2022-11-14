ecf7cfb7181defc646dd408b0d8781450b69e2eb
Vibhatha Lakmal Abeykoon <vibhatha@gmail.com>
2022-11-14 17:43:09 +0530

    fix(test): update test case

--- python/pyarrow/tests/parquet/test_dataset.py
+++ python/pyarrow/tests/parquet/test_dataset.py
@@ -1912,15 +1912,9 @@ def test_write_to_dataset_kwargs_passed(tempdir, write_dataset_kwarg):
 
 @pytest.mark.dataset
 def test_parquet_write_partioning_parser(tempdir):
-    from datetime import datetime
     import pyarrow.dataset as ds
-    df = pd.DataFrame({'a': [datetime(2019, 1, 1, 0),
-                      datetime(2019, 2, 1, 0),
-                      datetime(2019, 1, 1, 0),
-                      datetime(2019, 2, 1, 0),
-                      datetime(2019, 1, 1, 0),
-                      datetime(2019, 2, 1, 0)],
-                       'b': [20, 30, 40, 50, 60, 10]})
+    df = pd.DataFrame({'a' : [1, 2, 1, 2, 3, 4, 5, 1, 2, 4, 7, 8],
+                       'b' : [10, 30, 20, 40, 50, 60, 30, 50, 60, 10, 11, 12]})
     table = pa.Table.from_pandas(df)
     path = tempdir / 'partitioning'
 
@@ -1940,8 +1934,28 @@ def test_parquet_write_partioning_parser(tempdir):
     dataset = ds.dataset(source=path, partitioning=partitioning)
 
     filter_expressions = [dataset.partitioning.parse(path) for path in paths]
-
-    new_table = dataset.to_table(filter=filter_expressions[0])
-    print(table)
+    
+    import operator
+    def reduce(op1, op2, reduction_operator=operator.or_):
+        reduction_operator(op1, op2)
+    
+    def parse_filters(dataset, paths):
+        filter_exprs = [dataset.partitioning.parse(path) for path in paths]
+        print("filter expressions: ")
+        print(filter_exprs)
+        resultant_filter = filter_exprs[0]
+        for exp in filter_exprs[:1]:
+            resultant_filter = reduce(resultant_filter, exp)
+            
+    res_filter = parse_filters(dataset, paths)
+    
+    f1 = ds.field("a") > pc.scalar(3)
+    f2 = ds.field("a") < pc.scalar(8)
+    f11 = ds.field("a") > pc.scalar(3)
+    f22 = ds.field("a") < pc.scalar(6)
+    f3 = f11 & f22
+    print(f3)
+    new_table = dataset.to_table(filter=f3)
+    print(table.to_pandas())
     print("-" * 80)
-    print(new_table)
+    print(new_table.to_pandas())
