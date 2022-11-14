1b0d6ec818580355aac94f1d0a09ba65a5c08b81
Rok Mihevc <rok@mihevc.org>
2022-11-14 17:37:51 +0100

    Update cpp/src/arrow/table.h
    Co-authored-by: Will Jones <willjones127@gmail.com>

--- cpp/src/arrow/table.h
+++ cpp/src/arrow/table.h
@@ -289,7 +289,8 @@ struct ARROW_EXPORT ConcatenateTablesOptions {
 /// \brief Construct a new table from multiple input tables.
 ///
 /// The new table is assembled from existing column chunks without copying,
-/// if schemas are identical. If schemas do not match exactly, an attempt is
+/// if schemas are identical. If schemas do not match exactly and 
+/// unifySchemas is enabled in options (off by default), an attempt is
 /// made to unify them, and then column chunks are converted to their
 /// respective unified datatype, which will probably incur a copy.
 /// :func:`arrow::PromoteTableToSchema` is used to unify schemas.
