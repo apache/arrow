9fdcf2322ae548dcd5d5adbf7432dc7c5d1edce9
Miles Granger <miles59923@gmail.com>
2022-11-15 13:47:13 +0100

    Use s3_example_s3fs fixture and refactor test

--- python/pyarrow/tests/parquet/test_metadata.py
+++ python/pyarrow/tests/parquet/test_metadata.py
@@ -615,64 +615,34 @@ def test_metadata_append_row_groups_diff(t1, t2, expected_error):
         meta1.append_row_groups(meta2)
 
 
-def test_write_metadata_with_without_filesystem(tempdir):
-
-    class MockLocalFileSystem(LocalFileSystem):
-        def open_output_stream(self, *args, **kwargs):
-            nonlocal called
-            called += 1
-            return super().open_output_stream(*args, **kwargs)
+@pytest.mark.s3
+def test_write_metadata_fs_file_combinations(tempdir, s3_example_s3fs):
+    s3_fs, s3_path = s3_example_s3fs
 
     meta1 = tempdir / "meta1"
     meta2 = tempdir / "meta2"
     meta3 = tempdir / "meta3"
     meta4 = tempdir / "meta4"
-    meta5 = tempdir / "meta5"
+    meta5 = f"{s3_path}/meta5"
 
     table = pa.table({"col": range(5)})
-    filesystem = MockLocalFileSystem()
-    called = 0
 
     # plain local path
     pq.write_metadata(table.schema, meta1, [])
 
-    # Used the mock filesystem to resolve opening an output stream
-    pq.write_metadata(table.schema, meta2, [], filesystem=filesystem)
-    assert called == 2
+    # Used the localfilesystem to resolve opening an output stream
+    pq.write_metadata(table.schema, meta2, [], filesystem=LocalFileSystem())
 
-    # Can resolve URI
+    # Can resolve local file URI
     pq.write_metadata(table.schema, meta3.as_uri(), [])
 
     # Take a file-like obj all the way thru?
     with meta4.open('wb+') as meta4_stream:
         pq.write_metadata(table.schema, meta4_stream, [])
 
+    # S3FileSystem
+    pq.write_metadata(table.schema, meta5, [], filesystem=s3_fs)
+
     assert meta1.read_bytes() == meta2.read_bytes() \
-        == meta3.read_bytes() == meta4.read_bytes()
-
-    try:
-        from pyarrow.fs import S3FileSystem
-    except ImportError:
-        return
-
-    class MockS3FileSystem(S3FileSystem):
-        """Not going to actually write to S3"""
-
-        def open_input_file(self, path, *args, **kwargs):
-            assert path == s3_uri
-            return meta5.open('rb')
-
-        def open_output_stream(self, path, *args, **kwargs):
-            nonlocal called
-            assert path == s3_uri
-            called += 1
-            return meta5.open('wb')
-
-    s3_uri = "some-bucket/path.parquet"
-    filesystem = MockS3FileSystem()
-
-    # URI with filesystem
-    pq.write_metadata(table.schema, s3_uri, [], filesystem=filesystem)
-    assert called == 4
-
-    assert meta1.read_bytes() == meta5.read_bytes()
+        == meta3.read_bytes() == meta4.read_bytes() \
+        == s3_fs.open(meta5).read()
