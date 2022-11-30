d4ddb6329135c58974e13a7fae608e5b82520ae9
Miles Granger <miles59923@gmail.com>
2022-11-30 13:27:28 +0100

    Extra comments in MaxSliceLength

--- cpp/src/arrow/compute/kernels/scalar_nested.cc
+++ cpp/src/arrow/compute/kernels/scalar_nested.cc
@@ -102,11 +102,17 @@ int64_t MaxSliceLength(const int64_t start, const int64_t stop, const int64_t st
   const auto stopf = static_cast<float>(stop);
   const auto stepf = static_cast<float>(step);
 
+  // length not considering step, is less than step then
+  // the length will be 1; the index of start.
   if (stopf - startf <= stepf) {
     return 1;
   }
 
+  // Get the raw length with any remainder after dividing by step
   auto length = std::floor((stopf - startf) / stepf);
+
+  // Any remainder from the step into length then it is not evenly split by step
+  // and will need one extra in length.
   if (fmod(length, stepf) > 0.0) {
     ++length;
   }
