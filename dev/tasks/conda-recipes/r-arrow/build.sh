dae66c1f1e5d80bd940ba53264b0434d876b11e6
H. Vetinari <h.vetinari@gmx.com>
2022-12-05 16:31:05 +1100

    sync R recipe with feedstock

--- dev/tasks/conda-recipes/r-arrow/build.sh
+++ dev/tasks/conda-recipes/r-arrow/build.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 set -ex
 
 export DISABLE_AUTOBREW=1
-# See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
-$R CMD INSTALL --build r/.
+# set C++17 due to abseil
+export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -std=c++17"
+
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+${R} CMD INSTALL --build r/. ${R_ARGS}
--- dev/tasks/conda-recipes/r-arrow/build_win.sh
+++ dev/tasks/conda-recipes/r-arrow/build_win.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -exuo pipefail
-
 
 # Rename arrow.dll to lib_arrow.dll to avoid conflicts with the arrow-cpp arrow.dll
 sed -i -e 's/void R_init_arrow/__declspec(dllexport) void R_init_lib_arrow/g' r/src/arrowExports.cpp
