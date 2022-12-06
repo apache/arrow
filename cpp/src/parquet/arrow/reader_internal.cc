1a3cf43d8f73c609516c84c80e86dcc08503913f
zhixingheyi-tian <xiangxiang.shen@intel.com>
2022-12-06 17:22:59 +0800

    Fix LargeString/LargeBinary UTs

--- cpp/src/parquet/arrow/reader_internal.cc
+++ cpp/src/parquet/arrow/reader_internal.cc
@@ -57,6 +57,20 @@
 #include "parquet/types.h"
 #include "parquet/windows_fixup.h"  // for OPTIONAL
 
+ #include <typeinfo>
+ #include <cxxabi.h>
+#include <iostream>
+
+std::string getClearName(const char* name)
+{
+    int status = -1;
+    char* clear_name = abi::__cxa_demangle(name, NULL, NULL, &status);
+    const char* demangle_name = (status==0) ? clear_name : name;
+    std::string ret_val(demangle_name);
+    free(clear_name);
+    return ret_val;
+}
+
 using arrow::Array;
 using arrow::BooleanArray;
 using arrow::ChunkedArray;
@@ -366,6 +380,29 @@ std::shared_ptr<Array> TransferBinaryZeroCopy(RecordReader* reader,
                                                    buffers, reader->null_count());
   // std::cout << "::arrow::MakeArray(data)->ToString():" << ::arrow::MakeArray(data)->ToString() << std::endl;
   return ::arrow::MakeArray(data);
+}
+
+Status TransferLargeBinaryZeroCopy(RecordReader* reader, MemoryPool* pool,
+                                        const std::shared_ptr<DataType>& type,
+                                        std::shared_ptr<ChunkedArray>* out) {
+  std::vector<std::shared_ptr<Buffer>> buffers = {reader->ReleaseIsValid(),
+                                                  reader->ReleaseOffsets(),
+                                                  reader->ReleaseValues()};
+  auto data = std::make_shared<::arrow::ArrayData>(::arrow::binary(), reader->values_written(),
+                                                   buffers, reader->null_count());
+  // std::cout << "::arrow::MakeArray(data)->ToString():" << ::arrow::MakeArray(data)->ToString() << std::endl;
+  auto array = ::arrow::MakeArray(data);
+  ::arrow::compute::ExecContext ctx(pool);
+  ::arrow::compute::CastOptions cast_options;
+  cast_options.allow_invalid_utf8 = true;  // avoid spending time validating UTF8 data
+
+  ARROW_ASSIGN_OR_RAISE(
+	  array,
+	  ::arrow::compute::Cast(*array, type, cast_options, &ctx));
+
+
+  *out = std::make_shared<ChunkedArray>(std::move(::arrow::ArrayVector({array})), type);
+  return Status::OK();
 }
 
 Status TransferBool(RecordReader* reader, bool nullable, MemoryPool* pool, Datum* out) {
@@ -781,12 +818,16 @@ Status TransferColumnData(RecordReader* reader, const std::shared_ptr<Field>& va
       result = chunked_result;
     } break;
     case ::arrow::Type::BINARY:
-    case ::arrow::Type::STRING:
-    case ::arrow::Type::LARGE_BINARY:
-    case ::arrow::Type::LARGE_STRING: {
+    case ::arrow::Type::STRING: {
       result = TransferBinaryZeroCopy(reader, value_field->type());
       // RETURN_NOT_OK(TransferBinary(reader, pool, value_field, &chunked_result));
       // result = chunked_result;
+    } break;
+    case ::arrow::Type::LARGE_BINARY:
+    case ::arrow::Type::LARGE_STRING: {
+      // result = TransferLargeBinaryZeroCopy(reader, pool, value_field->type());
+      RETURN_NOT_OK(TransferLargeBinaryZeroCopy(reader, pool, value_field->type(), &chunked_result));
+      result = chunked_result;
     } break;
     case ::arrow::Type::DECIMAL128: {
       switch (descr->physical_type()) {
