000b4ffb85bc6e2583c57a464a5279964c7fd16f
Miles Granger <miles59923@gmail.com>
2022-12-07 13:08:04 +0100

    Use s3_example_fs and s3 path and uri test cases

--- python/pyarrow/tests/parquet/test_parquet_file.py
+++ python/pyarrow/tests/parquet/test_parquet_file.py
@@ -337,19 +337,23 @@ def test_parquet_file_explicitly_closed(tempdir):
 
 
 @pytest.mark.s3
-def test_parquet_file_with_filesystem(tempdir, s3_example_s3fs):
-    s3_fs, s3_path = s3_example_s3fs
+@pytest.mark.parametrize("use_uri", (True, False))
+def test_parquet_file_with_filesystem(tempdir, s3_example_fs, use_uri):
+    s3_fs, s3_uri, s3_path = s3_example_fs
+
+    args = (s3_uri if use_uri else s3_path,)
+    kwargs = {} if use_uri else dict(filesystem=s3_fs)
 
     table = pa.table({"a": range(10)})
-    pq.write_table(table, s3_path, filesystem=s3_fs)
+    pq.write_table(table, *args, **kwargs)
 
-    parquet_file = pq.ParquetFile(s3_path, filesystem=s3_fs)
+    parquet_file = pq.ParquetFile(*args, **kwargs)
     assert parquet_file.read() == table
     assert not parquet_file.closed
     parquet_file.close()
     assert parquet_file.closed
 
-    with pq.ParquetFile(s3_path, filesystem=s3_fs) as f:
+    with pq.ParquetFile(*args, **kwargs) as f:
         assert f.read() == table
         assert not f.closed
     assert f.closed
