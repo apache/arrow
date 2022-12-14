3883edbfba9dbf53cab3bcbc2ed27fd3aca25e2e
Alessandro Molina <amol@turbogears.org>
2022-12-14 14:31:40 +0100

    Add test for StructArray.flattened_field

--- python/pyarrow/tests/test_array.py
+++ python/pyarrow/tests/test_array.py
@@ -2916,6 +2916,41 @@ def test_struct_array_field():
             a.field(invalid_name)
 
 
+def test_struct_array_flattened_field():
+    ty = pa.struct([pa.field('x', pa.int16()),
+                    pa.field('y', pa.float32())])
+    a = pa.array([(1, 2.5), (3, 4.5), (5, 6.5)], type=ty,
+                 mask=pa.array([False, True, False]))
+
+    x0 = a.flattened_field(0)
+    y0 = a.flattened_field(1)
+    x1 = a.flattened_field(-2)
+    y1 = a.flattened_field(-1)
+    x2 = a.flattened_field('x')
+    y2 = a.flattened_field('y')
+
+    assert isinstance(x0, pa.lib.Int16Array)
+    assert isinstance(y1, pa.lib.FloatArray)
+    assert x0.equals(pa.array([1, None, 5], type=pa.int16()))
+    assert y0.equals(pa.array([2.5, None, 6.5], type=pa.float32()))
+    assert x0.equals(x1)
+    assert x0.equals(x2)
+    assert y0.equals(y1)
+    assert y0.equals(y2)
+
+    for invalid_index in [None, pa.int16()]:
+        with pytest.raises(TypeError):
+            a.flattened_field(invalid_index)
+
+    for invalid_index in [3, -3]:
+        with pytest.raises(IndexError):
+            a.flattened_field(invalid_index)
+
+    for invalid_name in ['z', '']:
+        with pytest.raises(KeyError):
+            a.flattened_field(invalid_name)
+
+
 def test_empty_cast():
     types = [
         pa.null(),
