dca8c07de6089bde7a9c2e10b42b72541a616cb6
Nic Crane <thisisnic@gmail.com>
2022-12-17 21:30:37 +0000

    ARROW-17692: [R] Add support for building with system AWS SDK C++ (#14235)
    This PR uses "pkg-config --static ... arrow" to collect build flags. "pkg-config --static ... arrow" reports suitable build flags that depend on build options and used libraries for Apache Arrow C++. This works with the system AWS SDK C++.
    
    Lead-authored-by: Sutou Kouhei <kou@clear-code.com>
    Co-authored-by: Nic Crane <thisisnic@gmail.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- dev/tasks/conda-recipes/r-arrow/build.sh
+++ dev/tasks/conda-recipes/r-arrow/build.sh
@@ -11,5 +11,7 @@ if [[ "${target_platform}" == osx-* ]]; then
     export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
+
 # ${R_ARGS} necessary to support cross-compilation
 ${R} CMD INSTALL --build r/. ${R_ARGS}
