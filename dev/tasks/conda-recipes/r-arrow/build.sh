4e9b65a4a33f57005472eaf9f2654ae61ff83101
h-vetinari <h.vetinari@gmx.com>
2022-12-20 14:06:13 +1100

    GH-14993: [CI][Conda] Fix missing RECIPE_ROOT variable now expected by conda build (#15014)
    Fixes #14993
    
    
    * Closes: #14993
    
    Lead-authored-by: H. Vetinari <h.vetinari@gmx.com>
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>
    Co-authored-by: Uwe L. Korn <uwe.korn@quantco.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- dev/tasks/conda-recipes/r-arrow/build.sh
+++ dev/tasks/conda-recipes/r-arrow/build.sh
@@ -5,13 +5,12 @@ export DISABLE_AUTOBREW=1
 
 # arrow uses C++17
 export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -std=c++17"
+export LIBARROW_BUILD=false
 
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
-
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
 
 # ${R_ARGS} necessary to support cross-compilation
 ${R} CMD INSTALL --build r/. ${R_ARGS}
