2a29e4df21e5b8a6805cabbce513f3fca34a0692
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2022-12-21 10:09:36 +0100

    tests: use pytest.mark.orc instead of pytest.skip
    Rely on test suite machinery to only run orc-marked
    tests when ORC support is generally available,
    so that the ImportError handler is not needed anymore.

--- python/pyarrow/tests/test_dataset.py
+++ python/pyarrow/tests/test_dataset.py
@@ -3074,12 +3074,8 @@ def test_orc_format_not_supported():
             ds.dataset(".", format="orc")
 
 
+@pytest.mark.orc
 def test_orc_writer_not_implemented_for_dataset():
-    try:
-        from pyarrow.dataset import OrcFileFormat  # noqa
-    except ImportError:
-        pytest.skip('this case is covered by test_orc_format_not_supported')
-
     with pytest.raises(
         NotImplementedError,
         match="Writing datasets not yet implemented for this file format"
