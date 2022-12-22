0a1ef90e9f188385fdceccfb7a2b41aacdf398f7
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2022-12-22 13:43:11 +0100

    archery

--- python/pyarrow/_dataset.pyx
+++ python/pyarrow/_dataset.pyx
@@ -889,13 +889,12 @@ cdef class FileFormat(_Weakrefable):
     def make_write_options(self):
         sp_write_options = self.format.DefaultWriteOptions()
         if sp_write_options.get() == nullptr:
-           # DefaultWriteOptions() may return `nullptr` which means that
-           # the format does not yet support writing datasets.
-           raise NotImplementedError(
-               "Writing datasets not yet implemented for this file format."
+            # DefaultWriteOptions() may return `nullptr` which means that
+            # the format does not yet support writing datasets.
+            raise NotImplementedError(
+                "Writing datasets not yet implemented for this file format."
             )
         return FileWriteOptions.wrap(sp_write_options)
-
 
     @property
     def default_extname(self):
