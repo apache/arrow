da615130f65c32964c146aa158545a84a3ede165
Will Jones <willjones127@gmail.com>
2022-12-27 14:59:25 -0800

    fix: pull down argument checking and check for na

--- r/R/csv.R
+++ r/R/csv.R
@@ -460,15 +460,9 @@ CsvReadOptions$create <- function(use_threads = option_use_threads(),
 readr_to_csv_write_options <- function(include_header = TRUE,
                                        batch_size = 1024L,
                                        na = "") {
-  assert_that(is_integerish(batch_size, n = 1, finite = TRUE), batch_size > 0)
-  assert_that(is.logical(include_header))
-  assert_that(is.character(na))
-  assert_that(length(na) == 1)
-  assert_that(!grepl('"', na), msg = "na argument must not contain quote characters.")
-
   CsvWriteOptions$create(
     include_header = include_header,
-    batch_size = as.integer(batch_size),
+    batch_size = batch_size,
     null_string = na
   )
 }
@@ -478,6 +472,12 @@ readr_to_csv_write_options <- function(include_header = TRUE,
 CsvWriteOptions <- R6Class("CsvWriteOptions", inherit = ArrowObject)
 CsvWriteOptions$create <- function(include_header = TRUE, batch_size = 1024L, null_string = "") {
   assert_that(is_integerish(batch_size, n = 1, finite = TRUE), batch_size > 0)
+  assert_that(is.logical(include_header))
+  assert_that(is.character(null_string))
+  assert_that(!is.na(null_string))
+  assert_that(length(null_string) == 1)
+  assert_that(!grepl('"', null_string), msg = "na argument must not contain quote characters.")
+
   csv___WriteOptions__initialize(
     list(
       include_header = include_header,
