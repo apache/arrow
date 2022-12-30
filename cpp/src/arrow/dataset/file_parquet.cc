7be757d84b0a8452e05b8cbebb7a5e3d662a5309
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2022-12-30 15:13:35 +0530

    fix: parquet predicate push-down handling with NaNs

--- cpp/src/arrow/dataset/file_parquet.cc
+++ cpp/src/arrow/dataset/file_parquet.cc
@@ -135,6 +135,10 @@ std::optional<compute::Expression> ColumnChunkStatisticsAsExpression(
     min = maybe_min.MoveValueUnsafe();
     max = maybe_max.MoveValueUnsafe();
 
+    if(!(min->is_valid) && !(max->is_valid)){
+      return std::nullopt;
+    }
+    
     if (min->Equals(max)) {
       auto single_value = compute::equal(field_expr, compute::literal(std::move(min)));
 
@@ -143,18 +147,25 @@ std::optional<compute::Expression> ColumnChunkStatisticsAsExpression(
       }
       return compute::or_(std::move(single_value), is_null(std::move(field_expr)));
     }
-
+    
     auto lower_bound =
-        compute::greater_equal(field_expr, compute::literal(std::move(min)));
-    auto upper_bound = compute::less_equal(field_expr, compute::literal(std::move(max)));
+        compute::greater_equal(field_expr, compute::literal(min));
+    auto upper_bound = compute::less_equal(field_expr, compute::literal(max));
+    auto in_range = compute::Expression();
 
-    auto in_range = compute::and_(std::move(lower_bound), std::move(upper_bound));
+    if(!(min->is_valid)){
+      in_range = std::move(upper_bound);
+    } else if(!(max->is_valid)){
+      in_range = std::move(lower_bound);
+    } else {
+      in_range = compute::and_(std::move(lower_bound), std::move(upper_bound));
+    }
+
     if (statistics->null_count() != 0) {
       return compute::or_(std::move(in_range), compute::is_null(field_expr));
     }
     return in_range;
   }
-
   return std::nullopt;
 }
 
