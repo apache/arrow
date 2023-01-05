fd8bf30fdef46a36bc3e2b209d07b39b3afc64af
Sutou Kouhei <kou@clear-code.com>
2023-01-05 23:51:26 +0900

    ARROW-18086: [Ruby] Add support for HalfFloat

--- ruby/red-arrow/ext/arrow/raw-records.cpp
+++ ruby/red-arrow/ext/arrow/raw-records.cpp
@@ -84,8 +84,7 @@ namespace red_arrow {
       VISIT(UInt16)
       VISIT(UInt32)
       VISIT(UInt64)
-      // TODO
-      // VISIT(HalfFloat)
+      VISIT(HalfFloat)
       VISIT(Float)
       VISIT(Double)
       VISIT(Binary)
