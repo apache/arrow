555ab807a34ff736eb7aabcc056ee7ac6c875637
vibhatha <vibhatha@gmail.com>
2023-01-13 20:35:04 +0530

    fix(docs)

--- cpp/src/arrow/compute/exec/exec_plan.h
+++ cpp/src/arrow/compute/exec/exec_plan.h
@@ -425,6 +425,7 @@ struct ARROW_EXPORT Declaration {
 };
 
 /// \brief Utility method to run a declaration and collect the results into a table
+///
 /// \param declaration The Declaration which is executed.
 /// \param use_threads If `use_threads` is false then all CPU work will be done on the
 ///                    calling thread.  I/O tasks will still happen on the I/O executor
@@ -444,6 +445,7 @@ ARROW_EXPORT Result<std::shared_ptr<Table>> DeclarationToTable(
 
 /// \brief Asynchronous version of \see DeclarationToTable
 ///
+/// \param declaration The Declaration which is executed.
 /// \param use_threads The behavior of use_threads is slightly different than the
 ///                    synchronous version since we cannot run synchronously on the
 ///                    calling thread. Instead, if use_threads=false then a new thread
