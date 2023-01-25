6bf33d2d213af89f62be1d1bc5d40629f0f72015
Mike Hancock <mhancock34@bloomberg.net>
2023-01-25 17:35:13 -0500

    Use ChunkedArrayFromJSON() in rows_to_batches unit tests.

--- cpp/src/arrow/util/rows_to_batches_test.cc
+++ cpp/src/arrow/util/rows_to_batches_test.cc
@@ -19,81 +19,46 @@
 
 #include <gtest/gtest.h>
 
-#include "arrow/array/builder_primitive.h"
 #include "arrow/array/builder_binary.h"
+#include "arrow/array/builder_primitive.h"
 #include "arrow/scalar.h"
 #include "arrow/table.h"
+#include "arrow/testing/gtest_util.h"
 #include "arrow/util/rows_to_batches.h"
 
 namespace arrow::util {
 
-// clang-format off
 const auto kTestSchema = schema(
-    {field("field_1", int64()),
-     field("field_2", int64()),
-     field("field_3", int64())} );
-// clang-format on
+    {field("field_1", int64()), field("field_2", int64()), field("field_3", int64())});
 
 auto IntConvertor = [](ArrayBuilder& array_builder, int value) {
   return static_cast<Int64Builder&>(array_builder).Append(value);
 };
 
+bool CompareJson(const arrow::Table& arrow_table, const std::string& json,
+                 const std::string& field_name) {
+  const auto col = arrow_table.GetColumnByName(field_name);
+  return arrow::ChunkedArrayFromJSON(col->type(), {json})->Equals(col);
+};
+
 TEST(RowsToBatches, BasicUsage) {
   std::vector<std::vector<int>> data = {{1, 2, 4}, {5, 6, 7}};
-
   auto batches = RowsToBatches(kTestSchema, std::ref(data), IntConvertor).ValueOrDie();
-
   auto table = batches->ToTable().ValueOrDie();
 
-  std::shared_ptr<ChunkedArray> col = table->column(0);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            1);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            5);
-
-  col = table->column(1);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            6);
-
-  col = table->column(2);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            4);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            7);
+  EXPECT_TRUE(CompareJson(*table, R"([1, 5])", "field_1"));
+  EXPECT_TRUE(CompareJson(*table, R"([2, 6])", "field_2"));
+  EXPECT_TRUE(CompareJson(*table, R"([4, 7])", "field_3"));
 }
 
 TEST(RowsToBatches, ConstRange) {
   const std::vector<std::vector<int>> data = {{1, 2, 4}, {5, 6, 7}};
-
   auto batches = RowsToBatches(kTestSchema, std::cref(data), IntConvertor).ValueOrDie();
-
   auto table = batches->ToTable().ValueOrDie();
 
-  std::shared_ptr<ChunkedArray> col = table->column(0);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            1);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            5);
-
-  col = table->column(1);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            6);
-
-  col = table->column(2);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            4);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            7);
+  EXPECT_TRUE(CompareJson(*table, R"([1, 5])", "field_1"));
+  EXPECT_TRUE(CompareJson(*table, R"([2, 6])", "field_2"));
+  EXPECT_TRUE(CompareJson(*table, R"([4, 7])", "field_3"));
 }
 
 TEST(RowsToBatches, StructAccessor) {
@@ -112,26 +77,9 @@ TEST(RowsToBatches, StructAccessor) {
 
   auto table = batches->ToTable().ValueOrDie();
 
-  std::shared_ptr<ChunkedArray> col = table->column(0);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            1);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            5);
-
-  col = table->column(1);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            6);
-
-  col = table->column(2);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            4);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            7);
+  EXPECT_TRUE(CompareJson(*table, R"([1, 5])", "field_1"));
+  EXPECT_TRUE(CompareJson(*table, R"([2, 6])", "field_2"));
+  EXPECT_TRUE(CompareJson(*table, R"([4, 7])", "field_3"));
 }
 
 TEST(RowsToBatches, Variant) {
@@ -149,24 +97,12 @@ TEST(RowsToBatches, Variant) {
   std::vector<std::vector<std::variant<int, std::string>>> data = {{1, std::string("2")},
                                                                    {4, std::string("5")}};
 
-  auto batches = RowsToBatches(test_schema, std::ref(data), VariantConvertor).ValueOrDie();
+  auto batches =
+      RowsToBatches(test_schema, std::ref(data), VariantConvertor).ValueOrDie();
   auto table = batches->ToTable().ValueOrDie();
 
-  std::shared_ptr<ChunkedArray> col = table->column(0);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
-            1);
-  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
-            4);
-
-  col = table->column(1);
-  EXPECT_EQ(col->length(), 2);
-  EXPECT_EQ(
-      std::dynamic_pointer_cast<StringScalar>(col->GetScalar(0).ValueOrDie())->view(),
-      "2");
-  EXPECT_EQ(
-      std::dynamic_pointer_cast<StringScalar>(col->GetScalar(1).ValueOrDie())->view(),
-      "5");
+  EXPECT_TRUE(CompareJson(*table, R"([1, 4])", "x"));
+  EXPECT_TRUE(CompareJson(*table, R"(["2", "5"])", "y"));
 }
 
 }  // namespace arrow::util
