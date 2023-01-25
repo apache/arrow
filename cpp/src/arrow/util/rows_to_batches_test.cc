f96cc4ec2703ecb35cd323d684f6c8d7a5b8a799
Mike Hancock <mhancock34@bloomberg.net>
2023-01-25 16:46:25 -0500

    Add more unit tests for rows_to_batches

--- cpp/src/arrow/util/rows_to_batches_test.cc
+++ cpp/src/arrow/util/rows_to_batches_test.cc
@@ -20,6 +20,7 @@
 #include <gtest/gtest.h>
 
 #include "arrow/array/builder_primitive.h"
+#include "arrow/array/builder_binary.h"
 #include "arrow/scalar.h"
 #include "arrow/table.h"
 #include "arrow/util/rows_to_batches.h"
@@ -64,6 +65,108 @@ TEST(RowsToBatches, BasicUsage) {
             4);
   EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
             7);
+}
+
+TEST(RowsToBatches, ConstRange) {
+  const std::vector<std::vector<int>> data = {{1, 2, 4}, {5, 6, 7}};
+
+  auto batches = RowsToBatches(kTestSchema, std::cref(data), IntConvertor).ValueOrDie();
+
+  auto table = batches->ToTable().ValueOrDie();
+
+  std::shared_ptr<ChunkedArray> col = table->column(0);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
+            1);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
+            5);
+
+  col = table->column(1);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
+            2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
+            6);
+
+  col = table->column(2);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
+            4);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
+            7);
+}
+
+TEST(RowsToBatches, StructAccessor) {
+  struct TestStruct {
+    std::vector<int> values;
+  };
+  std::vector<TestStruct> data = {TestStruct{{1, 2, 4}}, TestStruct{{5, 6, 7}}};
+
+  auto accessor =
+      [](const TestStruct& s) -> Result<std::reference_wrapper<const std::vector<int>>> {
+    return std::cref(s.values);
+  };
+
+  auto batches =
+      RowsToBatches(kTestSchema, std::ref(data), IntConvertor, accessor).ValueOrDie();
+
+  auto table = batches->ToTable().ValueOrDie();
+
+  std::shared_ptr<ChunkedArray> col = table->column(0);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
+            1);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
+            5);
+
+  col = table->column(1);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
+            2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
+            6);
+
+  col = table->column(2);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
+            4);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
+            7);
+}
+
+TEST(RowsToBatches, Variant) {
+  auto VariantConvertor = [](ArrayBuilder& array_builder,
+                             const std::variant<int, std::string>& value) {
+    if (std::holds_alternative<int>(value))
+      return dynamic_cast<Int64Builder&>(array_builder).Append(std::get<int>(value));
+    else
+      return dynamic_cast<arrow::StringBuilder&>(array_builder)
+          .Append(std::get<std::string>(value).c_str(),
+                  std::get<std::string>(value).length());
+  };
+
+  const auto test_schema = schema({field("x", int64()), field("y", utf8())});
+  std::vector<std::vector<std::variant<int, std::string>>> data = {{1, std::string("2")},
+                                                                   {4, std::string("5")}};
+
+  auto batches = RowsToBatches(test_schema, std::ref(data), VariantConvertor).ValueOrDie();
+  auto table = batches->ToTable().ValueOrDie();
+
+  std::shared_ptr<ChunkedArray> col = table->column(0);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(0).ValueOrDie())->value,
+            1);
+  EXPECT_EQ(std::dynamic_pointer_cast<Int64Scalar>(col->GetScalar(1).ValueOrDie())->value,
+            4);
+
+  col = table->column(1);
+  EXPECT_EQ(col->length(), 2);
+  EXPECT_EQ(
+      std::dynamic_pointer_cast<StringScalar>(col->GetScalar(0).ValueOrDie())->view(),
+      "2");
+  EXPECT_EQ(
+      std::dynamic_pointer_cast<StringScalar>(col->GetScalar(1).ValueOrDie())->view(),
+      "5");
 }
 
 }  // namespace arrow::util
