f66eafb062418933a98d3698b1e13cfcc9d1984f
Ian Cook <ianmcook@gmail.com>
2023-01-29 16:19:34 -0500

    Try only projecting when needed (half-baked experiment)

--- r/R/query-engine.R
+++ r/R/query-engine.R
@@ -97,15 +97,14 @@ ExecPlan <- R6Class("ExecPlan",
       }
 
       if (!is.null(.data$aggregations)) {
-        # Project to include just the data required for each aggregation,
-        # plus group_by_vars (last)
+        # When necessary, project to include the data required for each
+        # aggregation, plus group_by_vars (last)
         # TODO: validate that none of names(aggregations) are the same as names(group_by_vars)
         # dplyr does not error on this but the result it gives isn't great
-        # TODO: detect whether we actually need to project before aggregating
-        # by checking whether the targets contain literals or expressions. If
-        # they are all just column references, then we don't need to project.
         projection <- summarize_projection(.data)
-        if (length(projection)) {
+        projection_required <- length(projection) &&
+          !all_targets_are_field_refs(.data$aggregations)
+        if (projection_required) {
           node <- node$Project(projection)
         }
 
@@ -121,8 +120,13 @@ ExecPlan <- R6Class("ExecPlan",
           # Embed `name` and `targets` inside the aggregation objects
           x[["name"]] <- name
           if (length(x$data)) {
-            # name the targets to match how summarize_projection() named them
-            x[["targets"]] <- paste0(name, "..", seq_along(x$data))
+            if (projection_required) {
+              # name the targets to match how summarize_projection() named them
+              x[["targets"]] <- paste0(name, "..", seq_along(x$data))
+            } else {
+              # name the targets to match the field names they refer to
+              x[["targets"]] <- map(x$data, ~.$field_name)
+            }
           } else {
             x[["targets"]] <- character(0)
           }
