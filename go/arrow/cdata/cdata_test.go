5765aa29b5ca93dae1a5fe8c7efce05dcd9f73c8
David Li <li.davidm96@gmail.com>
2023-01-31 15:03:10 -0500

    GH-33936: [Go] C Data Interface: export dummy buffer for nil buffers (#33951)
    ### Rationale for this change
    
    While #14805 clarified that NULL buffers are valid in the C Data Interface, not all implementations handle this correctly (or were fixed, but earlier versions still exist).
    
    ### What changes are included in this PR?
    
    Export a non-NULL dummy buffer for nil/empty buffers to ensure compatibility.
    
    ### Are these changes tested?
    
    A regression test is included.
    * Closes: #33936
    
    Authored-by: David Li <li.davidm96@gmail.com>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- go/arrow/cdata/cdata_test.go
+++ go/arrow/cdata/cdata_test.go
@@ -795,6 +795,51 @@ func TestEmptyDictExport(t *testing.T) {
 	assert.Nil(t, out.dictionary.dictionary)
 }
 
+func TestEmptyStringExport(t *testing.T) {
+	// apache/arrow#33936: regression test
+	bldr := array.NewBuilder(memory.DefaultAllocator, &arrow.StringType{})
+	defer bldr.Release()
+
+	arr := bldr.NewArray()
+	defer arr.Release()
+
+	var out CArrowArray
+	var sc CArrowSchema
+	ExportArrowArray(arr, &out, &sc)
+
+	assert.EqualValues(t, 'u', *sc.format)
+	assert.Zero(t, sc.n_children)
+	assert.Nil(t, sc.dictionary)
+
+	assert.EqualValues(t, 3, out.n_buffers)
+	buffers := (*[3]unsafe.Pointer)(unsafe.Pointer(out.buffers))
+	assert.EqualValues(t, unsafe.Pointer(nil), buffers[0])
+	assert.NotEqualValues(t, unsafe.Pointer(nil), buffers[1])
+	assert.NotEqualValues(t, unsafe.Pointer(nil), buffers[2])
+}
+
+func TestEmptyUnionExport(t *testing.T) {
+	// apache/arrow#33936: regression test
+	bldr := array.NewBuilder(memory.DefaultAllocator, arrow.SparseUnionOf([]arrow.Field{
+		{Name: "child", Type: &arrow.Int64Type{}},
+	}, []arrow.UnionTypeCode{0}))
+	defer bldr.Release()
+
+	arr := bldr.NewArray()
+	defer arr.Release()
+
+	var out CArrowArray
+	var sc CArrowSchema
+	ExportArrowArray(arr, &out, &sc)
+
+	assert.EqualValues(t, 1, sc.n_children)
+	assert.Nil(t, sc.dictionary)
+
+	assert.EqualValues(t, 1, out.n_buffers)
+	buffers := (*[1]unsafe.Pointer)(unsafe.Pointer(out.buffers))
+	assert.NotEqualValues(t, unsafe.Pointer(nil), buffers[0])
+}
+
 func TestRecordReaderExport(t *testing.T) {
 	// Regression test for apache/arrow#33767
 	reclist := arrdata.Records["primitives"]
