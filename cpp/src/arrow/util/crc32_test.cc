17e13bd7e22b278f39d12ec45ba03d22dc3e1dfc
mwish <maplewish117@gmail.com>
2023-02-05 22:36:37 +0800

    change to use random u32

--- cpp/src/arrow/util/crc32_test.cc
+++ cpp/src/arrow/util/crc32_test.cc
@@ -44,7 +44,7 @@ TEST(Crc32Test, Basic) {
 }
 
 TEST(Crc32Test, matchesBoost32Type) {
-  const size_t BUFFER_SIZE = 512 * 1024 * sizeof(uint64_t);
+  const size_t BUFFER_SIZE = 512 * 1024;
   std::array<uint8_t, BUFFER_SIZE> buffer{};
 
   // Populate a buffer with a deterministic pattern
@@ -52,10 +52,12 @@ TEST(Crc32Test, matchesBoost32Type) {
   std::random_device r;
   std::seed_seq seed{r(), r(), r(), r(), r(), r(), r(), r()};
   std::mt19937 gen(seed);
-  std::uniform_int_distribution<uint8_t> dist;
+  // N4659 29.6.1.1 [rand.req.genl]/1e requires one of short, int, long, long long,
+  // unsigned short, unsigned int, unsigned long, or unsigned long long
+  std::uniform_int_distribution<uint32_t> dist;
 
   for (size_t i = 0; i < BUFFER_SIZE; ++i) {
-    buffer[i] = dist(gen);
+    buffer[i] = static_cast<uint8_t>(dist(gen));
   }
 
   struct TestCrcGroup {
