43b61e4f0b5fba3e14097f2f58cc5ceac9404f21
Nic Crane <thisisnic@gmail.com>
2023-02-06 14:34:01 +0000

    Add failure behavior handling

--- cpp/src/arrow/engine/substrait/expression_internal.cc
+++ cpp/src/arrow/engine/substrait/expression_internal.cc
@@ -333,14 +333,15 @@ Result<compute::Expression> FromProto(const substrait::Expression& expr,
       ARROW_ASSIGN_OR_RAISE(auto type,
                             FromProto(cast_exp.type(), ext_set, conversion_options));
 
+      if(cast_exp.failure_behavior() == substrait::Expression_Cast_FailureBehavior::Expression_Cast_FailureBehavior_FAILURE_BEHAVIOR_THROW_EXCEPTION){
+        return compute::call("cast", std::move(input), compute::CastOptions::Safe(type));
+      } else if(cast_exp.failure_behavior() == substrait::Expression_Cast_FailureBehavior::Expression_Cast_FailureBehavior_FAILURE_BEHAVIOR_RETURN_NULL){
 
-      // do we want safe or unsafe?  
-      // e.g. compute::CastOptions::Safe(int64())
-      // e.g. compute::CastOptions::Unsafe(int32())
-      // e.g. compute::CastOptions::Safe(timestamp(TimeUnit::NANO))
-
-      compute::CastOptions cast_options;
-      return compute::call("cast", std::move(input), compute::CastOptions::Safe(type));
+      // e.g. if unspecified  
+      } else {
+        return compute::call("cast", std::move(input), compute::CastOptions::Unsafe(type));
+      }
+      
     }
 
     default:
