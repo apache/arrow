36e3b7fe3ad0434360182137d2073973504f19f2
Brent Gardner <brent.gardner@spaceandtime.io>
2023-02-07 11:04:27 -0700

    Failing subquery test

--- java/flight/flight-sql-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcDriverTest.java
+++ java/flight/flight-sql-jdbc-driver/src/test/java/org/apache/arrow/driver/jdbc/ArrowFlightJdbcDriverTest.java
@@ -320,6 +320,41 @@ public class ArrowFlightJdbcDriverTest {
   }
 
   @Test
+  public void testSubqyeryParams() throws Exception {
+    final Driver driver = new ArrowFlightJdbcDriver();
+    Properties props = new Properties();
+    props.setProperty("user", "admin");
+    props.setProperty("password", "password");
+    props.setProperty("useEncryption", "false");
+    String conString = "jdbc:arrow-flight://127.0.0.1:50060";
+    try (Connection con = driver.connect(conString, props); Statement stmt = con.createStatement()) {
+      try {
+        stmt.execute("create table orders (\n" +
+                "o_id int, \n" +
+                "o_d_id int, \n" +
+                "o_w_id int,\n" +
+                "o_c_id int,\n" +
+                "o_entry_d timestamp,\n" +
+                "o_carrier_id int,\n" +
+                "o_ol_cnt int, \n" +
+                "o_all_local int,\n" +
+                "PRIMARY KEY(o_w_id, o_d_id, o_id) \n" +
+                ")");
+      } catch (Exception ignored) {}
+      String sql = "SELECT o_id, o_entry_d, COALESCE(o_carrier_id,0) FROM orders WHERE o_w_id = $1 AND o_d_id = $2 AND o_c_id = $3 AND o_id = (SELECT MAX(o_id) FROM orders WHERE o_w_id = $4 AND o_d_id = $5 AND o_c_id = $6)";
+      try(PreparedStatement ps = con.prepareStatement(sql)) {
+        ps.setInt(1, 1);
+        ps.setInt(2, 1);
+        ps.setInt(3, 1);
+        ps.setInt(4, 1);
+        ps.setInt(5, 1);
+        ps.setInt(6, 1);
+        ps.executeQuery();
+      }
+    }
+  }
+
+  @Test
   public void testCustomer() throws Exception {
     final Driver driver = new ArrowFlightJdbcDriver();
     Properties props = new Properties();
