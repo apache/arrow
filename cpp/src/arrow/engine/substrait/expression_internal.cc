10fa8ad370c561bc76cfc2d90a2bfbc1860af5a0
Nic Crane <thisisnic@gmail.com>
2023-02-08 10:37:40 +0000

    Add an error message when trying to use Expression_Cast_FailureBehavior_FAILURE_BEHAVIOR_RETURN_NULL

--- cpp/src/arrow/engine/substrait/expression_internal.cc
+++ cpp/src/arrow/engine/substrait/expression_internal.cc
@@ -336,7 +336,7 @@ Result<compute::Expression> FromProto(const substrait::Expression& expr,
       if(cast_exp.failure_behavior() == substrait::Expression_Cast_FailureBehavior::Expression_Cast_FailureBehavior_FAILURE_BEHAVIOR_THROW_EXCEPTION){
         return compute::call("cast", std::move(input), compute::CastOptions::Safe(type));
       } else if(cast_exp.failure_behavior() == substrait::Expression_Cast_FailureBehavior::Expression_Cast_FailureBehavior_FAILURE_BEHAVIOR_RETURN_NULL){
-
+        return Status::NotImplemented("Unsupported cast failure behavior: Expression_Cast_FailureBehavior_FAILURE_BEHAVIOR_RETURN_NULL");
       // e.g. if unspecified  
       } else {
         return compute::call("cast", std::move(input), compute::CastOptions::Unsafe(type));
