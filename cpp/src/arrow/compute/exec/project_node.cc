56b8535ba28f7cc0b125488cad72522d1ff8dd94
Joost Hoozemans <joosthooz@msn.com>
2023-02-16 12:37:59 +0100

    Creating a single span for a projection instead of 1 for each expression

--- cpp/src/arrow/compute/exec/project_node.cc
+++ cpp/src/arrow/compute/exec/project_node.cc
@@ -79,12 +79,16 @@ class ProjectNode : public MapNode {
 
   Result<ExecBatch> ProcessBatch(ExecBatch batch) override {
     std::vector<Datum> values{exprs_.size()};
+    util::tracing::Span span;
+    START_COMPUTE_SPAN(span, "Project", {{"project.length", batch.length }});
     for (size_t i = 0; i < exprs_.size(); ++i) {
-      util::tracing::Span span;
-      START_COMPUTE_SPAN(span, "Project",
-                         {{"project.type", exprs_[i].type()->ToString()},
-                          {"project.length", batch.length},
-                          {"project.expression", exprs_[i].ToString()}});
+#ifdef ARROW_WITH_OPENTELEMETRY
+    opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> raw_span =
+            ::arrow::internal::tracing::UnwrapSpan(span.details.get());
+    std::string project_name = "project[" + std::to_string(i) + "]";
+    raw_span->SetAttribute(project_name + ".type", exprs_[i].type()->ToString());
+    raw_span->SetAttribute(project_name + ".expression", exprs_[i].ToString());
+#endif
       ARROW_ASSIGN_OR_RAISE(Expression simplified_expr,
                             SimplifyWithGuarantee(exprs_[i], batch.guarantee));
 
