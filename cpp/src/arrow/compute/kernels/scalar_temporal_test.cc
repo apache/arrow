76c8acda14a85d09e002141adacfc8fb3a1d092f
Rok Mihevc <rok@mihevc.org>
2023-02-20 17:37:08 +0100

    Review feedback

--- cpp/src/arrow/compute/kernels/scalar_temporal_test.cc
+++ cpp/src/arrow/compute/kernels/scalar_temporal_test.cc
@@ -1833,13 +1833,13 @@ TEST_F(ScalarTemporalTest, TestLocalTime) {
           "2008-12-27 14:30:00", "2008-12-28 14:30:00", "2011-12-31 15:32:03", null])";
 
   for (auto u : TimeUnit::values()) {
-    CheckScalarUnary("local_time", timestamp(u), times_seconds_precision, timestamp(u),
-                     times_seconds_precision);
-    CheckScalarUnary("local_time", timestamp(u, "UTC"), times_seconds_precision,
+    CheckScalarUnary("local_timestamp", timestamp(u), times_seconds_precision,
                      timestamp(u), times_seconds_precision);
-    CheckScalarUnary("local_time", timestamp(u, "Asia/Kolkata"), times_seconds_precision,
-                     timestamp(u), expected_local_kolkata);
-    CheckScalarUnary("local_time", timestamp(u, "Pacific/Marquesas"),
+    CheckScalarUnary("local_timestamp", timestamp(u, "UTC"), times_seconds_precision,
+                     timestamp(u), times_seconds_precision);
+    CheckScalarUnary("local_timestamp", timestamp(u, "Asia/Kolkata"),
+                     times_seconds_precision, timestamp(u), expected_local_kolkata);
+    CheckScalarUnary("local_timestamp", timestamp(u, "Pacific/Marquesas"),
                      times_seconds_precision, timestamp(u), expected_local_marquesas);
   }
 }
