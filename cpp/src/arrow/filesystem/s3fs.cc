08ce652c551f11a54af43bb342c14a6b0adb8c84
Weston Pace <weston.pace@gmail.com>
2023-02-21 13:43:15 -0800

    Addressing PR review

--- cpp/src/arrow/filesystem/s3fs.cc
+++ cpp/src/arrow/filesystem/s3fs.cc
@@ -2597,16 +2597,16 @@ Status DoInitializeS3(const S3GlobalOptions& options) {
 #undef LOG_LEVEL_CASE
 
 #ifdef ARROW_S3_HAS_CRT
-  aws_options.ioOptions.clientBootstrap_create_fn =
+  aws_options.ioOptions.client_bootstrap_create_fn =
       [ev_threads = options.num_event_loop_threads]() {
         // https://github.com/aws/aws-sdk-cpp/blob/1.11.15/src/aws-cpp-sdk-core/source/Aws.cpp#L65
-        Aws::Crt::Io::EventLoopGroup eventLoopGroup(ev_threads);
-        Aws::Crt::Io::DefaultHostResolver defaultHostResolver(
-            eventLoopGroup, /*maxHosts=*/8, /*maxTTL=*/30);
-        auto clientBootstrap = Aws::MakeShared<Aws::Crt::Io::ClientBootstrap>(
-            "Aws_Init_Cleanup", eventLoopGroup, defaultHostResolver);
-        clientBootstrap->EnableBlockingShutdown();
-        return clientBootstrap;
+        Aws::Crt::Io::EventLoopGroup event_loop_group(ev_threads);
+        Aws::Crt::Io::DefaultHostResolver default_host_resolver(
+            event_loop_group, /*maxHosts=*/8, /*maxTTL=*/30);
+        auto client_bootstrap = Aws::MakeShared<Aws::Crt::Io::ClientBootstrap>(
+            "Aws_Init_Cleanup", event_loop_group, default_host_resolver);
+        client_bootstrap->EnableBlockingShutdown();
+        return client_bootstrap;
       };
 #endif
 
