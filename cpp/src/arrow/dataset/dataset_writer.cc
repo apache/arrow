1163ef1896962d9988536f51dae95cbec8b9a11f
Joost Hoozemans <joosthooz@msn.com>
2023-03-02 16:11:46 +0100

    Added push and pop spans to the datasetwriter that allow tracking staging area

--- cpp/src/arrow/dataset/dataset_writer.cc
+++ cpp/src/arrow/dataset/dataset_writer.cc
@@ -156,8 +156,6 @@ class DatasetWriterFileQueue {
   Result<std::shared_ptr<RecordBatch>> PopStagedBatch() {
     std::vector<std::shared_ptr<RecordBatch>> batches_to_write;
     uint64_t num_rows = 0;
-    util::tracing::Span span;
-    START_SPAN(span, "DatasetWriter::PopStagedBatch");
     while (!staged_batches_.empty()) {
       std::shared_ptr<RecordBatch> next = std::move(staged_batches_.front());
       staged_batches_.pop_front();
@@ -193,18 +191,35 @@ class DatasetWriterFileQueue {
   }
 
   Result<int64_t> PopAndDeliverStagedBatch() {
+    util::tracing::Span span;
+    START_SPAN(span, "DatasetWriter::Pop");
     ARROW_ASSIGN_OR_RAISE(std::shared_ptr<RecordBatch> next_batch, PopStagedBatch());
     int64_t rows_popped = next_batch->num_rows();
     rows_currently_staged_ -= next_batch->num_rows();
+#ifdef ARROW_WITH_OPENTELEMETRY
+    ::arrow::internal::tracing::UnwrapSpan(span.details.get())->SetAttribute("batch.size_rows", next_batch->num_rows());
+    ::arrow::internal::tracing::UnwrapSpan(span.details.get())->SetAttribute("rows_currently_staged", rows_currently_staged_);
+#endif
     ScheduleBatch(std::move(next_batch));
     return rows_popped;
   }
 
   // Stage batches, popping and delivering batches if enough data has arrived
   Status Push(std::shared_ptr<RecordBatch> batch) {
-    uint64_t delta_staged = batch->num_rows();
-    rows_currently_staged_ += delta_staged;
-    staged_batches_.push_back(std::move(batch));
+      uint64_t delta_staged = batch->num_rows();
+      rows_currently_staged_ += delta_staged;
+    {
+      util::tracing::Span span;
+      START_SPAN(span, "DatasetWriter::Push", {
+        {"batch.size_rows", batch->num_rows()},
+        {"rows_currently_staged", rows_currently_staged_},
+        // staged_rows_count is updated at the end, after this push and possibly multiple pops
+//        {"staged_rows_count", writer_state_->staged_rows_count},
+        {"options_.min_rows_per_group", options_.min_rows_per_group},
+        {"max_rows_staged", writer_state_->max_rows_staged}
+      });
+      staged_batches_.push_back(std::move(batch));
+    }
     while (!staged_batches_.empty() &&
            (writer_state_->StagingFull() ||
             rows_currently_staged_ >= options_.min_rows_per_group)) {
@@ -354,6 +369,8 @@ class DatasetWriterDirectoryQueue {
     latest_open_file_tasks_ = util::MakeThrottledAsyncTaskGroup(
         scheduler_, 1, /*queue=*/nullptr, std::move(file_finish_task));
     if (init_future_.is_valid()) {
+      util::tracing::Span span;
+      START_SPAN(span, "arrow::dataset::WaitForDirectoryInit");
       latest_open_file_tasks_->AddSimpleTask(
           [init_future = init_future_]() { return init_future; },
           "DatasetWriter::WaitForDirectoryInit"sv);
@@ -365,6 +382,8 @@ class DatasetWriterDirectoryQueue {
   uint64_t rows_written() const { return rows_written_; }
 
   void PrepareDirectory() {
+    util::tracing::Span span;
+    START_SPAN(span, "arrow::dataset::SubmitPrepareDirectoryTask");
     if (directory_.empty() || !write_options_.create_dir) {
       return;
     }
@@ -386,6 +405,8 @@ class DatasetWriterDirectoryQueue {
     if (write_options_.existing_data_behavior ==
         ExistingDataBehavior::kDeleteMatchingPartitions) {
       init_task = [this, create_dir_cb, notify_waiters_cb, notify_waiters_on_err_cb] {
+        util::tracing::Span span;
+        START_SPAN(span, "arrow::dataset::PrepareDirectory");
         return write_options_.filesystem
             ->DeleteDirContentsAsync(directory_,
                                      /*missing_dir_ok=*/true)
@@ -612,21 +633,15 @@ class DatasetWriter::DatasetWriterImpl {
       backpressure =
           writer_state_.rows_in_flight_throttle.Acquire(next_chunk->num_rows());
       if (!backpressure.is_finished()) {
-#ifdef ARROW_WITH_OPENTELEMETRY
-        opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> scheduler_span =
-                ::arrow::internal::tracing::UnwrapSpan(scheduler_->span().details.get());
-        scheduler_span->AddEvent("DatasetWriter::Backpressure::TooManyRowsQueued");
-#endif
+        EVENT(scheduler_->span(), "DatasetWriter::Backpressure::TooManyRowsQueued");
+        EVENT_ON_CURRENT_SPAN("DatasetWriter::Backpressure::TooManyRowsQueued");
         break;
       }
       if (will_open_file) {
         backpressure = writer_state_.open_files_throttle.Acquire(1);
         if (!backpressure.is_finished()) {
-#ifdef ARROW_WITH_OPENTELEMETRY
-          opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> scheduler_span =
-                  ::arrow::internal::tracing::UnwrapSpan(scheduler_->span().details.get());
-          scheduler_span->AddEvent("DatasetWriter::Backpressure::TooManyOpenFiles");
-#endif
+          EVENT(scheduler_->span(), "DatasetWriter::Backpressure::TooManyOpenFiles");
+          EVENT_ON_CURRENT_SPAN("DatasetWriter::Backpressure::TooManyOpenFiles");
           RETURN_NOT_OK(CloseLargestFile());
           break;
         }
