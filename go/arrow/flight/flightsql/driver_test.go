195fa81a81422eb87423225611c0e155f2753c28
Sven Rebhan <srebhan@influxdata.com>
2023-03-03 11:17:03 +0100

    Constraint test to go1.18 as we need the example SQLite server.

--- go/arrow/flight/flightsql/driver_test.go
+++ go/arrow/flight/flightsql/driver_test.go
@@ -14,6 +14,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build go1.18
+// +build go1.18
+
 package flightsql_test
 
 import (
