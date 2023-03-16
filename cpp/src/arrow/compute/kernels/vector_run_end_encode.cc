c2af07a77aea4ad765a81afee2d9c07f8e3a9d09
Felipe Oliveira Carvalho <felipekde@gmail.com>
2023-03-15 23:42:50 -0300

    [C++] Add RunEndEncodeOptions::Defaults()

--- cpp/src/arrow/compute/kernels/vector_run_end_encode.cc
+++ cpp/src/arrow/compute/kernels/vector_run_end_encode.cc
@@ -408,8 +408,10 @@ struct RunEndEncodeExec {
 
 Result<std::unique_ptr<KernelState>> RunEndEncodeInit(KernelContext*,
                                                       const KernelInitArgs& args) {
-  auto options = checked_cast<const RunEndEncodeOptions*>(args.options);
-  return std::make_unique<RunEndEncondingState>(options->run_end_type);
+  auto* options = checked_cast<const RunEndEncodeOptions*>(args.options);
+  auto run_end_type =
+      options ? options->run_end_type : RunEndEncodeOptions::Defaults().run_end_type;
+  return std::make_unique<RunEndEncondingState>(std::move(run_end_type));
 }
 
 template <typename RunEndType, typename ValueType, bool has_validity_buffer>
