e5124155b835acdab5176de8090634b06923ca2f
Matt Topol <zotthewizard@gmail.com>
2023-03-20 16:11:29 -0400

    GH-34629: [Go] Fix transpose_ints to work on riscv64-freebsd

--- go/internal/utils/transpose_ints_noasm.go
+++ go/internal/utils/transpose_ints_noasm.go
@@ -16,8 +16,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build noasm
-// +build noasm
+//go:build noasm || (!amd64 && !arm64 && !s390x && !ppc64le)
+// +build noasm || (!amd64 && !arm64 && !s390x && !ppc64le)
 
 package utils
 
