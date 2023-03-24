99d56fd07c95533710bf32c8dccca5253492d1c8
Herman Schaaf <hermanschaaf@gmail.com>
2023-03-24 11:12:38 +0000

    Implement exec

--- go/arrow/compute/internal/kernels/vector_sort.go
+++ go/arrow/compute/internal/kernels/vector_sort.go
@@ -1,10 +1,13 @@
 package kernels
 
 import (
-	"fmt"
+	"bytes"
+	"sort"
 
 	"github.com/apache/arrow/go/v12/arrow"
+	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/apache/arrow/go/v12/arrow/compute/internal/exec"
+	"github.com/apache/arrow/go/v12/arrow/memory"
 )
 
 type SortIndicesOptions struct {
@@ -53,22 +56,41 @@ func GetVectorSortingKernels() []exec.VectorKernel {
 }
 
 func sortExec(ctx *exec.KernelCtx, batch *exec.ExecSpan, out *exec.ExecResult) error {
-	if !batch.Values[0].IsArray() {
-		return fmt.Errorf("input to sortExec must be an array")
-	}
-	ar := batch.Values[0].Array
-	// sz := int64(ar.Type.(arrow.FixedWidthDataType).Bytes())
+	// Get the input array from the batch
+	inExecVal := batch.Values[0]
+	inArr := inExecVal.Array
 
-	out.SetMembers(ar.MakeData())
+	// Create a slice of indices, initialized to [0, 1, 2, ..., n-1]
+	indices := make([]int, inArr.Len)
+	for i := range indices {
+		indices[i] = i
+	}
+
+	sz := inArr.Type.(arrow.FixedWidthDataType).Bytes()
+
+	// Sort the indices slice based on the values in the input array
+	sort.Slice(indices, func(i, j int) bool {
+		// TODO: not sure what to do here?
+		// compare using scalar comparison?
+		a := inArr.Buffers[1].Buf[indices[i]*sz : (indices[i]+1)*sz]
+		b := inArr.Buffers[1].Buf[indices[j]*sz : (indices[j]+1)*sz]
+		return bytes.Compare(a, b) < 0
+	})
+
+	// Create a new array builder to build the output array
+	builder := array.NewInt64Builder(memory.DefaultAllocator)
+
+	// Add the sorted indices to the output array builder
+	for _, index := range indices {
+		builder.Append(int64(index))
+	}
+
+	// Build the output array and set it in the output ExecResult
+	outArr := builder.NewArray()
+	defer outArr.Release()
+
+	out.SetMembers(outArr.Data())
+	out.Release()
+
 	return nil
-	//impl, ok := ctx.State.(HashState)
-	//if !ok {
-	//	return fmt.Errorf("%w: bad initialization of hash state", arrow.ErrInvalid)
-	//}
-	//
-	//if err := impl.Append(ctx, &batch.Values[0].Array); err != nil {
-	//	return err
-	//}
-	//
-	//return impl.Flush(out)
 }
