905910ca9f144cccab226a16e84bc6a188b69067
Rok Mihevc <rok@mihevc.org>
2023-03-30 13:29:20 +0200

    Removing FromTensor/ToTensor

--- cpp/src/arrow/extension/fixed_shape_tensor.h
+++ cpp/src/arrow/extension/fixed_shape_tensor.h
@@ -26,25 +26,6 @@ namespace extension {
 class ARROW_EXPORT FixedShapeTensorArray : public ExtensionArray {
  public:
   using ExtensionArray::ExtensionArray;
-
-  /// \brief Create a FixedShapeTensorArray from a Tensor
-  ///
-  /// This method will create a FixedShapeTensorArray from a Tensor, taking its first
-  /// dimension as the number of elements in the resulting array and the remaining
-  /// dimensions as the shape of the individual tensors. If Tensor provides strides,
-  /// they will be used to determine dimension permutation. Otherwise, row-major layout
-  /// (i.e. no permutation) will be assumed.
-  ///
-  /// \param[in] tensor The Tensor to convert to a FixedShapeTensorArray
-  static Result<std::shared_ptr<FixedShapeTensorArray>> FromTensor(
-      const std::shared_ptr<Tensor>& tensor);
-
-  /// \brief Create a Tensor from FixedShapeTensorArray
-  ///
-  /// This method will create a Tensor from a FixedShapeTensorArray, setting its
-  /// first dimension as length equal to the FixedShapeTensorArray's length and the
-  /// remaining dimensions as the FixedShapeTensorType's shape.
-  const Result<std::shared_ptr<Tensor>> ToTensor() const;
 };
 
 /// \brief Concrete type class for constant-size Tensor data.
@@ -66,12 +47,7 @@ class ARROW_EXPORT FixedShapeTensorType : public ExtensionType {
   size_t ndim() { return shape_.size(); }
 
   /// Shape of tensor elements
-  const std::vector<int64_t> shape() const;
-
-  /// Strides of tensor elements. Strides state offset in bytes between adjacent
-  /// elements along each dimension. In case permutation is non-empty strides are
-  /// computed from permuted tensor element's shape.
-  const std::vector<int64_t>& strides();
+  const std::vector<int64_t> shape() const { return shape_; }
 
   /// Permutation mapping from logical to physical memory layout of tensor elements
   const std::vector<int64_t>& permutation() const { return permutation_; }
@@ -96,17 +72,10 @@ class ARROW_EXPORT FixedShapeTensorType : public ExtensionType {
       const std::vector<int64_t>& permutation = {},
       const std::vector<std::string>& dim_names = {});
 
-  /// \brief Compute strides of FixedShapeTensorType
-  static Status ComputeStrides(const FixedWidthType& type,
-                               const std::vector<int64_t>& shape,
-                               const std::vector<int64_t>& permutation,
-                               std::vector<int64_t>* strides);
-
  private:
   std::shared_ptr<DataType> storage_type_;
   std::shared_ptr<DataType> value_type_;
   std::vector<int64_t> shape_;
-  std::vector<int64_t> strides_;
   std::vector<int64_t> permutation_;
   std::vector<std::string> dim_names_;
 };
