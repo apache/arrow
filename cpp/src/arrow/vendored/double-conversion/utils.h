786cf2576c1a0520104cfdad65a8da52f4003511
liujiacheng777 <1042188676@qq.com>
2023-03-30 17:30:59 +0800

    Add support for LoongArch

--- cpp/src/arrow/vendored/double-conversion/utils.h
+++ cpp/src/arrow/vendored/double-conversion/utils.h
@@ -101,6 +101,7 @@ int main(int argc, char** argv) {
     defined(_MIPS_ARCH_MIPS32R2) || defined(__ARMEB__) ||\
     defined(__AARCH64EL__) || defined(__aarch64__) || defined(__AARCH64EB__) || \
     defined(__riscv) || \
+    defined(__loongarch__) || \
     defined(__or1k__) || defined(__arc__) || \
     defined(__EMSCRIPTEN__)
 #define DOUBLE_CONVERSION_CORRECT_DOUBLE_OPERATIONS 1
