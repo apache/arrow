f7be2d2aee73b4e9ec4e67e11511091d018943b8
Dane Pitkin <dane@voltrondata.com>
2023-04-04 13:03:09 -0400

    GH-34868: [Python] Share docstrings between classes

--- python/pyarrow/tests/test_util.py
+++ python/pyarrow/tests/test_util.py
@@ -18,12 +18,133 @@
 import gc
 import signal
 import sys
+import textwrap
 import weakref
 
 import pytest
 
-from pyarrow import util
+from pyarrow.util import _break_traceback_cycle_from_frame, doc
 from pyarrow.tests.util import disabled_gc
+
+
+@doc(method="cumsum", operation="sum")
+def cumsum(whatever):
+    """
+    This is the {method} method.
+
+    It computes the cumulative {operation}.
+    """
+
+
+@doc(
+    cumsum,
+    textwrap.dedent(
+        """
+        Examples
+        --------
+
+        >>> cumavg([1, 2, 3])
+        2
+        """
+    ),
+    method="cumavg",
+    operation="average",
+)
+def cumavg(whatever):
+    pass
+
+
+@doc(
+    cumsum,
+    method="cumprod",
+    operation="product",
+)
+def cumprod(whatever):
+    """
+    Examples
+    --------
+
+    >>> cumprod([1, 2, 3])
+    2
+    """
+    pass
+
+
+@doc(cumsum, method="cummax", operation="maximum")
+def cummax(whatever):
+    pass
+
+
+@doc(cummax, method="cummin", operation="minimum")
+def cummin(whatever):
+    pass
+
+
+def test_docstring_formatting():
+    docstr = textwrap.dedent(
+        """
+        This is the cumsum method.
+
+        It computes the cumulative sum.
+        """
+    )
+    assert cumsum.__doc__ == docstr
+
+
+def test_docstrings():
+    docstr = textwrap.dedent(
+        """
+        This is the cumavg method.
+
+        It computes the cumulative average.
+
+        Examples
+        --------
+
+        >>> cumavg([1, 2, 3])
+        2
+        """
+    )
+    assert cumavg.__doc__ == docstr
+
+
+def test_docstring_append():
+    docstr = textwrap.dedent(
+        """
+        This is the cumprod method.
+
+        It computes the cumulative product.
+
+        Examples
+        --------
+
+        >>> cumprod([1, 2, 3])
+        2
+        """
+    )
+    assert cumprod.__doc__ == docstr
+
+
+def test_doc_template_from_func():
+    docstr = textwrap.dedent(
+        """
+        This is the cummax method.
+
+        It computes the cumulative maximum.
+        """
+    )
+    assert cummax.__doc__ == docstr
+
+
+def test_inherit_doc_template():
+    docstr = textwrap.dedent(
+        """
+        This is the cummin method.
+
+        It computes the cumulative minimum.
+        """
+    )
+    assert cummin.__doc__ == docstr
 
 
 def exhibit_signal_refcycle():
@@ -48,5 +169,5 @@ def test_signal_refcycle():
     with disabled_gc():
         wr = exhibit_signal_refcycle()
         assert wr() is not None
-        util._break_traceback_cycle_from_frame(sys._getframe(0))
+        _break_traceback_cycle_from_frame(sys._getframe(0))
         assert wr() is None
