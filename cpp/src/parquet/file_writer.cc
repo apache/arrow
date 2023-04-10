0cf4ffaa90de6f036c363ac95d83474d6a1dfcc2
Gang Wu <ustcwg@gmail.com>
2023-04-11 07:48:31 +0800

    GH-34053: [C++][Parquet] Write parquet page index (#34054)
    ### Rationale for this change
    
    Parquet C++ reader supports reading page index from file, but the writer does not yet support writing it.
    
    ### What changes are included in this PR?
    
    Parquet file writer collects page index from all data pages and serializes page index into the file.
    
    ### Are these changes tested?
    
    Not yet, will be added later.
    
    ### Are there any user-facing changes?
    
    `WriterProperties::enable_write_page_index()` and `WriterProperties::disable_write_page_index()` have been added to toggle it on and off.
    * Closes: #34053
    
    Authored-by: Gang Wu <ustcwg@gmail.com>
    Signed-off-by: Will Jones <willjones127@gmail.com>

--- cpp/src/parquet/file_writer.cc
+++ cpp/src/parquet/file_writer.cc
@@ -28,9 +28,9 @@
 #include "parquet/encryption/encryption_internal.h"
 #include "parquet/encryption/internal_file_encryptor.h"
 #include "parquet/exception.h"
+#include "parquet/page_index.h"
 #include "parquet/platform.h"
 #include "parquet/schema.h"
-#include "parquet/types.h"
 
 using arrow::MemoryPool;
 
@@ -89,7 +89,8 @@ class RowGroupSerializer : public RowGroupWriter::Contents {
   RowGroupSerializer(std::shared_ptr<ArrowOutputStream> sink,
                      RowGroupMetaDataBuilder* metadata, int16_t row_group_ordinal,
                      const WriterProperties* properties, bool buffered_row_group = false,
-                     InternalFileEncryptor* file_encryptor = nullptr)
+                     InternalFileEncryptor* file_encryptor = nullptr,
+                     PageIndexBuilder* page_index_builder = nullptr)
       : sink_(std::move(sink)),
         metadata_(metadata),
         properties_(properties),
@@ -100,7 +101,8 @@ class RowGroupSerializer : public RowGroupWriter::Contents {
         next_column_index_(0),
         num_rows_(0),
         buffered_row_group_(buffered_row_group),
-        file_encryptor_(file_encryptor) {
+        file_encryptor_(file_encryptor),
+        page_index_builder_(page_index_builder) {
     if (buffered_row_group) {
       InitColumns();
     } else {
@@ -135,8 +137,7 @@ class RowGroupSerializer : public RowGroupWriter::Contents {
           column_writers_[0]->total_compressed_bytes_written();
     }
 
-    ++next_column_index_;
-
+    const int32_t column_ordinal = next_column_index_++;
     const auto& path = col_meta->descr()->path();
     auto meta_encryptor =
         file_encryptor_ ? file_encryptor_->GetColumnMetaEncryptor(path->ToDotString())
@@ -144,11 +145,17 @@ class RowGroupSerializer : public RowGroupWriter::Contents {
     auto data_encryptor =
         file_encryptor_ ? file_encryptor_->GetColumnDataEncryptor(path->ToDotString())
                         : nullptr;
+    auto ci_builder = page_index_builder_
+                          ? page_index_builder_->GetColumnIndexBuilder(column_ordinal)
+                          : nullptr;
+    auto oi_builder = page_index_builder_
+                          ? page_index_builder_->GetOffsetIndexBuilder(column_ordinal)
+                          : nullptr;
     std::unique_ptr<PageWriter> pager = PageWriter::Open(
         sink_, properties_->compression(path), properties_->compression_level(path),
-        col_meta, row_group_ordinal_, static_cast<int16_t>(next_column_index_ - 1),
+        col_meta, row_group_ordinal_, static_cast<int16_t>(column_ordinal),
         properties_->memory_pool(), false, meta_encryptor, data_encryptor,
-        properties_->page_checksum_enabled());
+        properties_->page_checksum_enabled(), ci_builder, oi_builder);
     column_writers_[0] = ColumnWriter::Make(col_meta, std::move(pager), properties_);
     return column_writers_[0].get();
   }
@@ -240,6 +247,7 @@ class RowGroupSerializer : public RowGroupWriter::Contents {
   mutable int64_t num_rows_;
   bool buffered_row_group_;
   InternalFileEncryptor* file_encryptor_;
+  PageIndexBuilder* page_index_builder_;
 
   void CheckRowsWritten() const {
     // verify when only one column is written at a time
@@ -267,18 +275,25 @@ class RowGroupSerializer : public RowGroupWriter::Contents {
     for (int i = 0; i < num_columns(); i++) {
       auto col_meta = metadata_->NextColumnChunk();
       const auto& path = col_meta->descr()->path();
+      const int32_t column_ordinal = next_column_index_++;
       auto meta_encryptor =
           file_encryptor_ ? file_encryptor_->GetColumnMetaEncryptor(path->ToDotString())
                           : nullptr;
       auto data_encryptor =
           file_encryptor_ ? file_encryptor_->GetColumnDataEncryptor(path->ToDotString())
                           : nullptr;
+      auto ci_builder = page_index_builder_
+                            ? page_index_builder_->GetColumnIndexBuilder(column_ordinal)
+                            : nullptr;
+      auto oi_builder = page_index_builder_
+                            ? page_index_builder_->GetOffsetIndexBuilder(column_ordinal)
+                            : nullptr;
       std::unique_ptr<PageWriter> pager = PageWriter::Open(
           sink_, properties_->compression(path), properties_->compression_level(path),
           col_meta, static_cast<int16_t>(row_group_ordinal_),
-          static_cast<int16_t>(next_column_index_++), properties_->memory_pool(),
+          static_cast<int16_t>(column_ordinal), properties_->memory_pool(),
           buffered_row_group_, meta_encryptor, data_encryptor,
-          properties_->page_checksum_enabled());
+          properties_->page_checksum_enabled(), ci_builder, oi_builder);
       column_writers_.push_back(
           ColumnWriter::Make(col_meta, std::move(pager), properties_));
     }
@@ -317,6 +332,8 @@ class FileSerializer : public ParquetFileWriter::Contents {
       }
       row_group_writer_.reset();
 
+      WritePageIndex();
+
       // Write magic bytes and metadata
       auto file_encryption_properties = properties_->file_encryption_properties();
 
@@ -345,9 +362,12 @@ class FileSerializer : public ParquetFileWriter::Contents {
     }
     num_row_groups_++;
     auto rg_metadata = metadata_->AppendRowGroup();
+    if (page_index_builder_) {
+      page_index_builder_->AppendRowGroup();
+    }
     std::unique_ptr<RowGroupWriter::Contents> contents(new RowGroupSerializer(
         sink_, rg_metadata, static_cast<int16_t>(num_row_groups_ - 1), properties_.get(),
-        buffered_row_group, file_encryptor_.get()));
+        buffered_row_group, file_encryptor_.get(), page_index_builder_.get()));
     row_group_writer_ = std::make_unique<RowGroupWriter>(std::move(contents));
     return row_group_writer_.get();
   }
@@ -412,6 +432,21 @@ class FileSerializer : public ParquetFileWriter::Contents {
     }
   }
 
+  void WritePageIndex() {
+    if (page_index_builder_ != nullptr) {
+      if (properties_->file_encryption_properties()) {
+        throw ParquetException("Encryption is not supported with page index");
+      }
+
+      // Serialize page index after all row groups have been written and report
+      // location to the file metadata.
+      PageIndexLocation page_index_location;
+      page_index_builder_->Finish();
+      page_index_builder_->WriteTo(sink_.get(), &page_index_location);
+      metadata_->SetPageIndexLocation(page_index_location);
+    }
+  }
+
   std::shared_ptr<ArrowOutputStream> sink_;
   bool is_open_;
   const std::shared_ptr<WriterProperties> properties_;
@@ -420,7 +455,7 @@ class FileSerializer : public ParquetFileWriter::Contents {
   std::unique_ptr<FileMetaDataBuilder> metadata_;
   // Only one of the row group writers is active at a time
   std::unique_ptr<RowGroupWriter> row_group_writer_;
-
+  std::unique_ptr<PageIndexBuilder> page_index_builder_;
   std::unique_ptr<InternalFileEncryptor> file_encryptor_;
 
   void StartFile() {
@@ -458,6 +493,10 @@ class FileSerializer : public ParquetFileWriter::Contents {
         // Encrypted file with plaintext footer mode.
         PARQUET_THROW_NOT_OK(sink_->Write(kParquetMagic, 4));
       }
+    }
+
+    if (properties_->write_page_index()) {
+      page_index_builder_ = PageIndexBuilder::Make(&schema_);
     }
   }
 };
