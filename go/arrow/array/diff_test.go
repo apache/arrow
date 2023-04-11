397f826b416bfd8e1319026ffbb4ee949f54f011
Kemal Hadimli <disq@users.noreply.github.com>
2023-04-11 18:20:55 +0100

    diff_test: Update types import path

--- go/arrow/array/diff_test.go
+++ go/arrow/array/diff_test.go
@@ -26,8 +26,8 @@ import (
 
 	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
-	"github.com/apache/arrow/go/v12/arrow/internal/testing/types"
 	"github.com/apache/arrow/go/v12/arrow/memory"
+	"github.com/apache/arrow/go/v12/internal/types"
 )
 
 type diffTestCase struct {
