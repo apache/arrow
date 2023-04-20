ead72605ce7627e56d6472a8e8042688c5361bef
Sutou Kouhei <kou@clear-code.com>
2023-04-20 14:33:42 +0900

    Add a TODO comment

--- cpp/src/arrow/flight/sql/example/sqlite_server.cc
+++ cpp/src/arrow/flight/sql/example/sqlite_server.cc
@@ -304,8 +304,11 @@ class SQLiteFlightSqlServer::Impl {
     ARROW_ASSIGN_OR_RAISE(auto ticket,
                           EncodeTransactionQuery(query, command.transaction_id));
     std::vector<FlightEndpoint> endpoints{FlightEndpoint{std::move(ticket), {}}};
+    // TODO: Set true only when "ORDER BY" is used in a main "SELECT"
+    // in the given query.
+    const bool ordered = false;
     ARROW_ASSIGN_OR_RAISE(auto result,
-                          FlightInfo::Make(*schema, descriptor, endpoints, -1, -1, false))
+                          FlightInfo::Make(*schema, descriptor, endpoints, -1, -1, ordered));
 
     return std::make_unique<FlightInfo>(result);
   }
