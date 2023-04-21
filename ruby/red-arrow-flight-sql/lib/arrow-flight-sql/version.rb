9736dde84bb2e6996d1d12f6a044c33398e3c3a3
Raúl Cumplido <raulcumplido@gmail.com>
2023-04-21 10:37:41 +0200

    MINOR: [Release] Update versions for 12.0.0

--- ruby/red-arrow-flight-sql/lib/arrow-flight-sql/version.rb
+++ ruby/red-arrow-flight-sql/lib/arrow-flight-sql/version.rb
@@ -16,7 +16,7 @@
 # under the License.
 
 module ArrowFlightSQL
-  VERSION = "12.0.0-SNAPSHOT"
+  VERSION = "12.0.0"
 
   module Version
     numbers, TAG = VERSION.split("-")
--- ruby/red-gandiva/lib/gandiva/version.rb
+++ ruby/red-gandiva/lib/gandiva/version.rb
@@ -16,7 +16,7 @@
 # under the License.
 
 module Gandiva
-  VERSION = "12.0.0-SNAPSHOT"
+  VERSION = "12.0.0"
 
   module Version
     numbers, TAG = VERSION.split("-")
