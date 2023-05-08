13a95dbd4762e178ef503509a32263383ed6e1d1
Sarah Gilmore <sgilmore@mathworks.com>
2023-05-08 16:09:26 -0400

    Add arrow.array.Float32Array MATLAB class
    Co-authored-by: Kevin Gurney <kgurney@mathworks.com>

--- matlab/src/cpp/arrow/matlab/proxy/factory.cc
+++ matlab/src/cpp/arrow/matlab/proxy/factory.cc
@@ -26,8 +26,8 @@ namespace arrow::matlab::proxy {
 std::shared_ptr<Proxy> Factory::make_proxy(const ClassName& class_name, const FunctionArguments& constructor_arguments) {
 
     // Register MATLAB Proxy classes with corresponding C++ Proxy classes.
+    REGISTER_PROXY(arrow.array.proxy.Float32Array, arrow::matlab::array::proxy::NumericArray<float>);
     REGISTER_PROXY(arrow.array.proxy.Float64Array, arrow::matlab::array::proxy::NumericArray<double>);
-
     // TODO: Decide what to do in the case that there isn't a Proxy match.
     std::cout << "Did not find a matching C++ proxy for: " + class_name << std::endl;
     return nullptr;
