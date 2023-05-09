840bac98728a1909d5a63f16a030a80971779836
Alenka Frim <frim.alenka@gmail.com>
2023-05-09 20:41:49 +0200

    Correct the test

--- python/pyarrow/tests/interchange/test_conversion.py
+++ python/pyarrow/tests/interchange/test_conversion.py
@@ -195,20 +195,32 @@ def test_roundtrip_pandas_string():
         pytest.skip(" Column.size() called as a method in pandas 2.0.0")
 
     arr = ["a", "", "c"]
-    table = pa.table({"a": pa.array(arr),
-                      "a_large": pa.array(arr, type=pa.large_string())})
+    table = pa.table({"a": pa.array(arr)})
 
     from pandas.api.interchange import (
         from_dataframe as pandas_from_dataframe
     )
 
+    pandas_df = pandas_from_dataframe(table)
+    result = pi.from_dataframe(pandas_df)
+
+    assert result["a"].to_pylist() == table["a"].to_pylist()
+    assert pa.types.is_string(table["a"].type)
+    assert pa.types.is_large_string(result["a"].type)
+
+    table_protocol = table.__dataframe__()
+    result_protocol = result.__dataframe__()
+
+    assert table_protocol.num_columns() == result_protocol.num_columns()
+    assert table_protocol.num_rows() == result_protocol.num_rows()
+    assert table_protocol.num_chunks() == result_protocol.num_chunks()
+    assert table_protocol.column_names() == result_protocol.column_names()
+
+    table = pa.table({"a_large": pa.array(arr, type=pa.large_string())})
+
     if Version(pd.__version__) >= Version("2.0.1"):
         pandas_df = pandas_from_dataframe(table)
         result = pi.from_dataframe(pandas_df)
-
-        assert result["a"].to_pylist() == table["a"].to_pylist()
-        assert pa.types.is_string(table["a"].type)
-        assert pa.types.is_large_string(result["a"].type)
 
         assert result["a_large"].to_pylist() == table["a_large"].to_pylist()
         assert pa.types.is_large_string(table["a_large"].type)
@@ -221,6 +233,7 @@ def test_roundtrip_pandas_string():
         assert table_protocol.num_rows() == result_protocol.num_rows()
         assert table_protocol.num_chunks() == result_protocol.num_chunks()
         assert table_protocol.column_names() == result_protocol.column_names()
+
     else:
         # large string not supported by pandas implementation for
         # older versions of pandas
