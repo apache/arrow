688dcec5642ab92c40bd09ea838ac04739b5d23c
Alenka Frim <frim.alenka@gmail.com>
2023-05-11 12:04:14 +0200

    Add the test

--- python/pyarrow/tests/test_pandas.py
+++ python/pyarrow/tests/test_pandas.py
@@ -4816,3 +4816,23 @@ def test_unhashable_map_keys_with_pydicts():
         for tup1, tup2 in zip(row1, row2):
             assert np.array_equal(tup1[0], tup2[0])
             assert tup1[1] == tup2[1]
+
+
+def test_column_conversion_for_datetime():
+    # GH-35235
+    # pandas implemented __from_arrow__ for DatetimeTZDtype
+    # https://github.com/pandas-dev/pandas/pull/52201
+    if Version(pd.__version__) <= Version("2.0.1"):
+        pytest.skip("__from_arrow__ added to DatetimeTZDtype in pandas version 2.1.0")
+
+    arr = pd.Series(pd.date_range("2012", periods=2, tz="Europe/Brussels"),
+                    name="datetime_column")
+    table = pa.table({"datetime_column": pa.array(arr)})
+    table_col = table.column("datetime_column")
+
+    pandas_dtype = table_col.type.to_pandas_dtype()
+    assert hasattr(pandas_dtype, '__from_arrow__')
+
+    result = table_col.to_pandas()
+    assert result.name == "datetime_column"
+    tm.assert_series_equal(result, arr)
