4776c088bf97e4526b52e46b725799c31e069d5c
Bryce Mecum <petridish@gmail.com>
2023-05-16 20:11:10 -0800

    Whitespace

--- r/R/filesystem.R
+++ r/R/filesystem.R
@@ -396,7 +396,7 @@ S3FileSystem$create <- function(
   if (length(log_level) == 1) {
     if (IsS3Initialized()) {
       warning(
-        "S3 is already initialized. Argument 'log_level' ignored.", 
+        "S3 is already initialized. Argument 'log_level' ignored.",
         call. = FALSE
       )
     } else {
