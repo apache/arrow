5002535ec7c66615ba946fd8e75bb4528bf6fea9
sgilmore10 <74676073+sgilmore10@users.noreply.github.com>
2023-05-18 20:50:48 -0400

    GH-35537: [MATLAB] Create shared test class utility for numeric arrays (#35556)
    ### Rationale for this change
    To avoid writing duplicate tests, we created an an Abstract Test Class called `hNumericArray` which defines tests that can be shared by all numeric array types. Concrete test classes can inherit from this class to automatically get the shared test points. 
    
    ### What changes are included in this PR?
    
    1. Defined new abstract test class `hNumericArray`
    2. Modified `tFloat32Array` to inherit from `hNumericArray`
    3. Modified `tFloat64Array` to inherit from `hNumericArray`
    
    ### Are these changes tested?
    
    1. Qualified the tests still pass on macOS.
    
    ### Are there any user-facing changes?
    There are no user-facing changes in this PR. This PR only has test-related changes.
    
    * Closes: #35537
    
    Lead-authored-by: Sarah Gilmore <sgilmore@mathworks.com>
    Co-authored-by: sgilmore10 <74676073+sgilmore10@users.noreply.github.com>
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>
    Co-authored-by: Kevin Gurney <kgurney@mathworks.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- matlab/test/arrow/array/tFloat32Array.m
+++ matlab/test/arrow/array/tFloat32Array.m
@@ -1,4 +1,4 @@
-classdef tFloat32Array < matlab.unittest.TestCase
+classdef tFloat32Array < hNumericArray
     % Tests for arrow.array.Float32rray
 
     % Licensed to the Apache Software Foundation (ASF) under one or more
@@ -16,90 +16,20 @@ classdef tFloat32Array < matlab.unittest.TestCase
     % implied.  See the License for the specific language governing
     % permissions and limitations under the License.
     
-    properties (TestParameter)
-        MakeDeepCopy = {true false}
+    properties
+        ArrowArrayClassName = "arrow.array.Float32Array"
+        ArrowArrayConstructor = @arrow.array.Float32Array
+        MatlabConversionFcn = @single % single method on class
+        MatlabArrayFcn = @single % single function
+        MaxValue = realmax("single")
+        MinValue = realmin("single")
     end
 
-    methods(TestClassSetup)
-        function verifyOnMatlabPath(testCase)
-            % arrow.array.Float32Array must be on the MATLAB path.
-            testCase.assertTrue(~isempty(which('arrow.array.Float32Array')), ...
-                '''arrow.array.Float32Array'' must be on the MATLAB path. Use ''addpath'' to add folders to the MATLAB path.');
-        end
-    end
-    
     methods(Test)
-        function Basic(testCase, MakeDeepCopy)
-            A = arrow.array.Float32Array(single([1, 2, 3]), DeepCopy=MakeDeepCopy);
-            className = string(class(A));
-            testCase.verifyEqual(className, "arrow.array.Float32Array");
-        end
-
-        function ShallowCopy(testCase)
-            % By default, Float32Array does not create a deep copy on
-            % construction when constructed from a MATLAB array. Instead,
-            % it stores a shallow copy of the array keep the memory alive.
-            A = arrow.array.Float32Array(single([1, 2, 3]));
-            testCase.verifyEqual(A.MatlabArray, single([1 2 3]));
-            testCase.verifyEqual(single(A), single([1 2 3]'));
-
-            A = arrow.array.Float32Array(single([1, 2, 3]), DeepCopy=false);
-            testCase.verifyEqual(A.MatlabArray, single([1 2 3]));
-            testCase.verifyEqual(single(A), single([1 2 3]'));
-        end
-
-        function DeepCopy(testCase)
-          % Verify Float32Array does not store shallow copy of the MATLAB
-          % array if DeepCopy=true was supplied.
-            A = arrow.array.Float32Array(single([1, 2, 3]), DeepCopy=true);
-            testCase.verifyEqual(A.MatlabArray, single([]));
-            testCase.verifyEqual(single(A), single([1 2 3]'));
-        end
-
-        function Single(testCase, MakeDeepCopy)
-            % Create a Float32Array from a scalar double
-            A1 = arrow.array.Float32Array(single(100), DeepCopy=MakeDeepCopy);
-            data = single(A1);
-            testCase.verifyEqual(data, single(100));
-
-            % Create a Float32Array from a double vector 
-            A2 = arrow.array.Float32Array(single([1 2 3]), DeepCopy=MakeDeepCopy);
-            data = single(A2);
-            testCase.verifyEqual(data, single([1 2 3]'));
-        
-            % Create a Float32Array from an empty double vector
-            A3 = arrow.array.Float32Array(single([]), DeepCopy=MakeDeepCopy);
-            data = single(A3);
-            testCase.verifyEqual(data, single.empty(0, 1));
-        end
-
-        function MinValue(testCase, MakeDeepCopy)
-            A1 = arrow.array.Float32Array(realmin("single"), DeepCopy=MakeDeepCopy);
-            data = single(A1);
-            testCase.verifyEqual(data, realmin("single"));
-        end
-
-        function MaxValue(testCase, MakeDeepCopy)
-            A1 = arrow.array.Float32Array(realmax('single'), DeepCopy=MakeDeepCopy);
-            data = single(A1);
-            testCase.verifyEqual(data, realmax('single'));
-        end
-
         function InfValues(testCase, MakeDeepCopy)
             A1 = arrow.array.Float32Array(single([Inf -Inf]), DeepCopy=MakeDeepCopy);
             data = single(A1);
             testCase.verifyEqual(data, single([Inf -Inf]'));
-        end
-
-        function ErrorIfComplex(testCase, MakeDeepCopy)
-            fcn = @() arrow.array.Float32Array(single([10 + 1i, 4]), DeepCopy=MakeDeepCopy);
-            testCase.verifyError(fcn, "MATLAB:expectedReal");
-        end
-
-        function toMATLAB(testCase, MakeDeepCopy)
-            A1 = arrow.array.Float32Array(single(100), DeepCopy=MakeDeepCopy);
-            data = toMATLAB(A1);
-            testCase.verifyEqual(data, single(100));
         end
     end
 end
