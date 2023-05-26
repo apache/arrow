c434bb254f04450c25a502662af22cd9e771ae8f
Weston Pace <weston.pace@gmail.com>
2023-05-26 11:08:05 -0700

    Cleaning up comment

--- cpp/src/arrow/util/align_util.h
+++ cpp/src/arrow/util/align_util.h
@@ -75,10 +75,9 @@ namespace util {
 ///
 /// If this is passed as the `alignment` in one of the CheckAlignment or EnsureAlignment
 /// functions, then the function will ensure ensure each buffer is suitably aligned
-/// for the data type of the
-/// array.  For example, given an int32 buffer the values buffer's address must be a
-/// multiple of 4.  Given a large_string buffer the offsets buffer's address must be a
-/// multiple of 8.
+/// for the data type of the array.  For example, given an int32 buffer the values
+/// buffer's address must be a multiple of 4.  Given a large_string buffer the offsets
+/// buffer's address must be a multiple of 8.
 constexpr int64_t kValueAlignment = -3;
 
 /// \brief Calculate if the buffer's address is a multiple of `alignment`
