26952930e0a1525814715190a5ef399ec0c9e383
mwish <maplewish117@gmail.com>
2023-05-31 01:15:01 +0800

    GH-35729: [C++][Parquet] Implement batch interface for BloomFilter in Parquet (#35731)
    ### Rationale for this change
    
    For optimizing https://github.com/apache/arrow/pull/35691 . We need batch execution for BloomFilter
    
    ### What changes are included in this PR?
    
    * Add `InsertHashes` and `Hashes` for BloomFilter
    * Make `BloomFilter::Find` faster by inlining the bitmask computation inside the loop
    
    * Closes: #35729
    
    Lead-authored-by: mwish <maplewish117@gmail.com>
    Co-authored-by: Antoine Pitrou <antoine@python.org>
    Co-authored-by: mwish <1506118561@qq.com>
    Co-authored-by: Gang Wu <ustcwg@gmail.com>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/src/parquet/test_util.h
+++ cpp/src/parquet/test_util.h
@@ -744,5 +744,53 @@ inline void PrimitiveTypedTest<BooleanType>::GenerateData(int64_t num_values,
   std::fill(def_levels_.begin(), def_levels_.end(), 1);
 }
 
+// ----------------------------------------------------------------------
+// test data generation
+
+template <typename T>
+inline void GenerateData(int num_values, T* out, std::vector<uint8_t>* heap) {
+  // seed the prng so failure is deterministic
+  random_numbers(num_values, 0, std::numeric_limits<T>::min(),
+                 std::numeric_limits<T>::max(), out);
+}
+
+template <typename T>
+inline void GenerateBoundData(int num_values, T* out, T min, T max,
+                              std::vector<uint8_t>* heap) {
+  // seed the prng so failure is deterministic
+  random_numbers(num_values, 0, min, max, out);
+}
+
+template <>
+inline void GenerateData<bool>(int num_values, bool* out, std::vector<uint8_t>* heap) {
+  // seed the prng so failure is deterministic
+  random_bools(num_values, 0.5, 0, out);
+}
+
+template <>
+inline void GenerateData<Int96>(int num_values, Int96* out, std::vector<uint8_t>* heap) {
+  // seed the prng so failure is deterministic
+  random_Int96_numbers(num_values, 0, std::numeric_limits<int32_t>::min(),
+                       std::numeric_limits<int32_t>::max(), out);
+}
+
+template <>
+inline void GenerateData<ByteArray>(int num_values, ByteArray* out,
+                                    std::vector<uint8_t>* heap) {
+  // seed the prng so failure is deterministic
+  int max_byte_array_len = 12;
+  heap->resize(num_values * max_byte_array_len);
+  random_byte_array(num_values, 0, heap->data(), out, 2, max_byte_array_len);
+}
+
+static constexpr int kGenerateDataFLBALength = 8;
+
+template <>
+inline void GenerateData<FLBA>(int num_values, FLBA* out, std::vector<uint8_t>* heap) {
+  // seed the prng so failure is deterministic
+  heap->resize(num_values * kGenerateDataFLBALength);
+  random_fixed_byte_array(num_values, 0, heap->data(), kGenerateDataFLBALength, out);
+}
+
 }  // namespace test
 }  // namespace parquet
