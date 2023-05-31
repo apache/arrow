27a91154cdafc4ea8bd784abcfdd93695b98bea7
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-05-31 11:51:45 +0200

    actually check if tz database is available or not on Windows

--- python/pyarrow/tests/test_scalars.py
+++ python/pyarrow/tests/test_scalars.py
@@ -26,6 +26,7 @@ import numpy as np
 
 import pyarrow as pa
 import pyarrow.compute as pc
+from pyarrow.tests import util
 
 
 @pytest.mark.parametrize(['value', 'ty', 'klass'], [
@@ -296,9 +297,8 @@ def test_cast():
         pa.scalar('foo').cast('int32')
 
 
-# TODO: We should test on windows once ARROW-13168 is resolved.
-@pytest.mark.skipif(sys.platform == 'win32',
-                    reason="Timezone database is not available on Windows yet")
+@pytest.mark.skipif(sys.platform == "win32" and not util.windows_has_tzdata(),
+                    reason="Timezone database is not installed on Windows")
 def test_cast_timestamp_to_string():
     # GH-35370
     pytest.importorskip("pytz")
