0e8dfcc1f5d352cf15eda09634c583b68f5500c9
Junming Chen <junming.chen.r@outlook.com>
2023-06-06 23:26:32 +0800

    OrderedSpillingAccumulationQueue Done

--- cpp/src/arrow/acero/external_order_by_node.cc
+++ cpp/src/arrow/acero/external_order_by_node.cc
@@ -31,6 +31,11 @@
 #include "arrow/util/checked_cast.h"
 #include "arrow/util/tracing_internal.h"
 #include "arrow/util/type_fwd.h"
+#include "arrow/util/async_generator.h"
+#include "arrow/util/async_util.h"
+#include "arrow/util/thread_pool.h"
+#include "arrow/util/future.h"
+#include "arrow/io/util_internal.h"
 #include "parquet/arrow/writer.h"
 #include "parquet/arrow/reader.h"
 
@@ -65,7 +70,7 @@ class ExternalOrderByNode : public ExecNode, public TracedNode {
     if (order_options.ordering.is_implicit() || order_options.ordering.is_unordered()) {
       return Status::Invalid("`ordering` must be an explicit non-empty ordering");
     }
-
+    
     //todo check buffer_size && path_to_folder
 
     std::shared_ptr<Schema> output_schema = inputs[0]->output_schema();
@@ -163,7 +168,6 @@ class ExternalOrderByNode : public ExecNode, public TracedNode {
   int64_t buffer_size_;
   std::string path_to_folder_;
   Ordering ordering_;
-  std::mutex mutex_;
   OrderedSpillingAccumulationQueue accumulation_queue_;
 };
 
@@ -178,7 +182,9 @@ class OrderedSpillingAccumulationQueue {
         path_to_folder_(path_to_folder),
         ordering_(new_ordering),
         accumulation_queue_size_(0),
-        spill_count_(0) {}
+        spill_count_(0),
+        exec_batch_in_memory_(nullptr),
+        has_only_memory_spill_count_(false) {}
 
   // Inserts a batch into the queue.  This may trigger a write to disk if enough data is
   // accumulated If it does, then SpillCount should be incremented before this method
@@ -190,7 +196,7 @@ class OrderedSpillingAccumulationQueue {
 
     if (accumulation_queue_size_ >= buffer_size_) {
       spill_count_++;
-      int64_t spill_index=spill_count_-1;
+      int64_t spill_index = spill_count_ - 1;
       ARROW_ASSIGN_OR_RAISE(
           auto table,
           Table::FromRecordBatches(
@@ -208,56 +214,64 @@ class OrderedSpillingAccumulationQueue {
   }
 
   Status push_finshed() {
-    mutex_.lock();
+    bool  = false;
+    if(accumulation_queue_size_>0){
+      spill_count_++;
+    }
     batch_size_ = buffer_size_ / spill_count_;
+
     if (accumulation_queue_size_ > 0) {
-      if (accumulation_queue_size_ > batch_size_) {
-        spill_count_++;
+      ARROW_ASSIGN_OR_RAISE(
+          auto table,
+          Table::FromRecordBatches(
+              output_schema_, std::move(accumulation_queue_)));  // todo check batches_
+
+      if (accumulation_queue_size_ > batch_size_) { // output oversize part of data
         int64_t spill_index = spill_count_ - 1;
         int64_t output_size = accumulation_queue_size_ - batch_size_;
-        ARROW_ASSIGN_OR_RAISE(
-            auto table,
-            Table::FromRecordBatches(
-                output_schema_, std::move(accumulation_queue_)));  // todo check batches_
-        accumulation_queue_size_ = 0;
-        mutex_.unlock();
 
-        //output oversize part of data
         TableBatchReader reader(table);
         reader.set_chunksize(std::move(output_size));
         ARROW_ASSIGN_OR_RAISE(std::shared_ptr<RecordBatch> next, reader.Next());
         std::vector<std::shared_ptr<RecordBatch>> batches;
         batches.push_back(std::move(next));
-        ARROW_ASSIGN_OR_RAISE(
-            auto output_table, Table::FromRecordBatches(output_schema_, std::move(batches)));
+        ARROW_ASSIGN_OR_RAISE(auto output_table, Table::FromRecordBatches(
+                                                     output_schema_, std::move(batches)));
         ARROW_ASSIGN_OR_RAISE(auto sorted_table, sort_table(output_table));
         ARROW_RETURN_NOT_OK(schedule_write_task(sorted_table, spill_index));
 
-        //todo buffer the remanent data
         reader.set_chunksize(batch_size_);
         ARROW_ASSIGN_OR_RAISE(std::shared_ptr<RecordBatch> record_batch, reader.Next());
+        exec_batch_in_memory_ = std::make_shared<ExecBatch>(*record_batch);
+      } else {//in memory
+        has_only_memory_spill_count_ = true;
 
-
-    plan_->query_context()->async_scheduler()->
-      }else{
-        //todo buffer the remanent data
-
-
+        TableBatchReader reader(table);
+        reader.set_chunksize(batch_size_);
+        ARROW_ASSIGN_OR_RAISE(std::shared_ptr<RecordBatch> record_batch, reader.Next());
+        exec_batch_in_memory_ = std::make_shared<ExecBatch>(*record_batch);
       }
+      accumulation_queue_size_ = 0;
+    }
 
-    } else {
-      mutex_.unlock();
+    //init AsyncGenerators
+    int64_t count=spill_count_;
+    if(has_only_memory_spill_count_){
+      count--;
+    }
+    for (int i = 0; i <count ; i++) {
+      std::string path_to_file = get_path_to_file(i);
+      ARROW_ASSIGN_OR_RAISE(auto generator, MakeGenerator(path_to_file, batch_size_));
+      asyncGenerators_.push_back(generator);
     }
     return Status::OK();
   }
 
-  // The number of files that have been written to disk.  This should also include any data in memory
-  // so it will be the number of files written to disk + 1 if there is in-memory data.
+  // The number of files that have been written to disk.  This should also include any
+  // data in memory so it will be the number of files written to disk + 1 if there is
+  // in-memory data.
   int SpillCount() {
-    {// to do check whether the lock is reentried
-      std::lock_guard lk(mutex_);
-      return spill_count_;
-    }
+      return spill_count_;    
   }
 
   // This should only be called after all calls to InsertBatch have been completed.  This
@@ -265,52 +279,37 @@ class OrderedSpillingAccumulationQueue {
   // the given spilled file.  If spill_index
   // == SpillCount() - 1 then this might be data that is already in-memory.
   Future<std::optional<ExecBatch>> FetchNextBatch(int spill_index) {
-    std::string path_to_file = get_path_to_file(spill_index);
-    Future<std::optional<ExecBatch>> future = Future<std::optional<ExecBatch>>::Make();
-    arrow::MemoryPool* pool = arrow::default_memory_pool();
-
-    // Configure general Parquet reader settings
-    auto reader_properties = parquet::ReaderProperties(pool);
-    reader_properties.set_buffer_size(4096 * 4);
-    reader_properties.enable_buffered_stream();
-
-    // Configure Arrow-specific Parquet reader settings
-    auto arrow_reader_props = parquet::ArrowReaderProperties();
-    arrow_reader_props.set_batch_size(batch_size_);
-
-    parquet::arrow::FileReaderBuilder reader_builder;
-    ARROW_RETURN_NOT_OK(
-        reader_builder.OpenFile(path_to_file, /*memory_map=*/false, reader_properties));
-    reader_builder.memory_pool(pool);
-    reader_builder.properties(arrow_reader_props);
-
-    std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
-    ARROW_ASSIGN_OR_RAISE(arrow_reader, reader_builder.Build());
-
-    std::shared_ptr<::arrow::RecordBatchReader> rb_reader;
-    ARROW_RETURN_NOT_OK(arrow_reader->GetRecordBatchReader(&rb_reader));
-    for (arrow::Result<std::shared_ptr<arrow::RecordBatch>> maybe_batch : *rb_reader) {
-      // Operate on each batch...
+    if (spill_index == spill_count_ - 1 && exec_batch_in_memory_ != nullptr) {
+      auto future = Future<std::optional<ExecBatch>>::MakeFinished(
+          *(std::move(exec_batch_in_memory_)));
+      exec_batch_in_memory_ = nullptr;
+      return future;
     }
-    return future;
+    if (spill_index >= 0 &&
+        (spill_index < spill_count_ - 1 ||
+         (spill_index == spill_count_ - 1 && !has_only_memory_spill_count_))) {
+      return asyncGenerators_.at(spill_index)();
+    }
+
+    return Future<std::optional<ExecBatch>>::MakeFinished(std::nullopt);
   }
 
-  protected:
-   inline std::string get_path_to_file(int spill_index) {
-      return path_to_folder_ + "/sort_" + std::to_string(spill_index) + ".parquet";
-   }
+ protected:
+  inline std::string get_path_to_file(int spill_index) {
+    return path_to_folder_ + "/sort_" + std::to_string(spill_index) + ".parquet";
+  }
 
-   Result<std::shared_ptr<arrow::Table>> sort_table(std::shared_ptr<arrow::Table> table) {
-      SortOptions sort_options(ordering_.sort_keys(), ordering_.null_placement());
-      ExecContext* ctx = plan_->query_context()->exec_context();
-      ARROW_ASSIGN_OR_RAISE(auto indices, SortIndices(table, sort_options, ctx));
-      ARROW_ASSIGN_OR_RAISE(auto sorted_table,
-                            Take(table, indices, TakeOptions::NoBoundsCheck(), ctx));
-      return sorted_table.table();
-   }
+  Result<std::shared_ptr<arrow::Table>> sort_table(std::shared_ptr<arrow::Table> table) {
+    SortOptions sort_options(ordering_.sort_keys(), ordering_.null_placement());
+    ExecContext* ctx = plan_->query_context()->exec_context();
+    ARROW_ASSIGN_OR_RAISE(auto indices, SortIndices(table, sort_options, ctx));
+    ARROW_ASSIGN_OR_RAISE(auto sorted_table,
+                          Take(table, indices, TakeOptions::NoBoundsCheck(), ctx));
+    return sorted_table.table();
+  }
 
-   Status schedule_write_task(std::shared_ptr<arrow::Table> table, int spill_index) {
-    std::string path_to_file =get_path_to_file(spill_index);
+  Status schedule_write_task(std::shared_ptr<arrow::Table> table, int spill_index) {
+    std::string path_to_file = get_path_to_file(spill_index);
     std::shared_ptr<WriterProperties> props =
         WriterProperties::Builder().compression(arrow::Compression::SNAPPY)->build();
     std::shared_ptr<ArrowWriterProperties> arrow_props =
@@ -324,11 +323,59 @@ class OrderedSpillingAccumulationQueue {
               /*chunk_size=*/3, props, arrow_props));
         },
         "OrderByNode::OrderedSpillingAccumulationQueue::Spillover");
-   }
+  }
+
+  static Result<arrow::AsyncGenerator<std::optional<ExecBatch>>> MakeGenerator(
+      const std::string& path_to_file, int64_t batch_size) {
+    arrow::MemoryPool* pool = arrow::default_memory_pool();
+    // Configure general Parquet reader settings
+    auto reader_properties = parquet::ReaderProperties(pool);
+    reader_properties.set_buffer_size(4096 * 4);
+    reader_properties.enable_buffered_stream();
+
+    // Configure Arrow-specific Parquet reader settings
+    auto arrow_reader_props = parquet::ArrowReaderProperties();
+    arrow_reader_props.set_batch_size(batch_size);
+
+    parquet::arrow::FileReaderBuilder reader_builder;
+    ARROW_RETURN_NOT_OK(
+        reader_builder.OpenFile(path_to_file, /*memory_map=*/false, reader_properties));
+    reader_builder.memory_pool(pool);
+    reader_builder.properties(arrow_reader_props);
+
+    std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
+    ARROW_ASSIGN_OR_RAISE(arrow_reader, reader_builder.Build());
+
+    std::shared_ptr<::arrow::RecordBatchReader> rb_reader;
+    ARROW_RETURN_NOT_OK(arrow_reader->GetRecordBatchReader(&rb_reader));
+
+    return MakeGenerator(rb_reader, io::internal::GetIOThreadPool());
+  }
+
+  static Result<arrow::AsyncGenerator<std::optional<ExecBatch>>> MakeGenerator(
+      const std::shared_ptr<RecordBatchReader>& reader,
+      arrow::internal::Executor* io_executor) {
+    auto to_exec_batch =
+        [](const std::shared_ptr<RecordBatch>& batch) -> std::optional<ExecBatch> {
+      if (batch == NULLPTR) {
+        return std::nullopt;
+      }
+      return std::optional<ExecBatch>(ExecBatch(*batch));
+    };
+    Iterator<std::shared_ptr<RecordBatch>> batch_it = MakeIteratorFromReader(reader);
+    auto exec_batch_it = MakeMapIterator(to_exec_batch, std::move(batch_it));
+    if (io_executor == nullptr) {
+      return MakeBlockingGenerator(std::move(exec_batch_it));
+    }
+    return MakeBackgroundGenerator(std::move(exec_batch_it), io_executor);
+  }
 
  private:
   std::mutex mutex_;
   std::vector<std::shared_ptr<RecordBatch>> accumulation_queue_;
+  std::vector<arrow::AsyncGenerator<std::optional<ExecBatch>>> asyncGenerators_;
+  std::shared_ptr<ExecBatch> exec_batch_in_memory_;
+  bool has_only_memory_spill_count_;
   int64_t spill_count_;
   int64_t accumulation_queue_size_;
   int64_t buffer_size_;
@@ -336,9 +383,8 @@ class OrderedSpillingAccumulationQueue {
   std::shared_ptr<Schema> output_schema_;
   std::string path_to_folder_;
   Ordering ordering_;
-  ExecPlan* plan_ ;
+  ExecPlan* plan_;
 };
-
 }  // namespace
 
 namespace internal {
