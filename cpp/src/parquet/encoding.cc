9e9dff9b416a6b03c687b161cea5e7711a9382a1
Arthur Passos <arthur.ti@outlook.com>
2023-06-09 13:19:20 -0300

    add overflow check in dict

--- cpp/src/parquet/encoding.cc
+++ cpp/src/parquet/encoding.cc
@@ -1531,7 +1531,9 @@ class DictDecoderImpl : public DecoderImpl, virtual public DictDecoder<Type> {
 
     int total_size = 0;
     for (int i = 0; i < dictionary_length_; ++i) {
-      total_size += dict_values[i].len;
+      if (AddWithOverflow(total_size, dict_values[i].len, &total_size)) {
+        throw ParquetException("String/Binary Length to large");
+      }
     }
     PARQUET_THROW_NOT_OK(byte_array_data_->Resize(total_size,
                                                   /*shrink_to_fit=*/false));
