a52d4566eaf3576ee6dffcbcd6920944a95a6a5a
Jin Shang <shangjin1997@gmail.com>
2023-06-21 08:35:32 +0800

    adjust doc

--- cpp/src/arrow/compute/kernels/scalar_arithmetic.cc
+++ cpp/src/arrow/compute/kernels/scalar_arithmetic.cc
@@ -644,8 +644,9 @@ struct ArithmeticFunction : ScalarFunction {
       }
 
       if (name_ == "multiply_checked") {
-        // Multiply_checked requires the same type for both operands, so we can't easily
-        // make kernels for duration * integers, so we just cast the integers to int64
+        // MultiplyChecked requires the same type for both operands, therefore we can't
+        // easily make kernels for integers other than int64, so we just cast integers to
+        // int64
         if ((*types)[0].id() == Type::DURATION && is_integer((*types)[1].id())) {
           ReplaceTypes(int64(), &(*types)[1], 1);
         } else if ((*types)[1].id() == Type::DURATION && is_integer((*types)[0].id())) {
