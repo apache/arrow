427f5507c596fe75b31888447d9f001a5678d829
Dane Pitkin <48041712+danepitkin@users.noreply.github.com>
2023-06-28 13:08:11 -0400

    GH-36129: [Python] Consolidate common APIs in Table and RecordBatch (#36130)
    ### Rationale for this change
    
    Deduplicate python code in subclasses. The following functions are moved to the shared parent class:
    ```
    __init__
    __len__
    column_names
    from_pylist
    from_pydict
    itercolumns
    to_pylist
    to_pydict
    ```
    ### What changes are included in this PR?
    
    Refactoring and minor test updates.
    
    ### Are these changes tested?
    
    Yes
    
    ### Are there any user-facing changes?
    
    No
    * Closes: #36129
    
    Authored-by: Dane Pitkin <dane@voltrondata.com>
    Signed-off-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- python/pyarrow/tests/test_table.py
+++ python/pyarrow/tests/test_table.py
@@ -549,6 +549,28 @@ foo: 'bar'"""
     assert wr() is None
 
 
+def test_recordbatch_dunder_init():
+    with pytest.raises(TypeError, match='RecordBatch'):
+        pa.RecordBatch()
+
+
+def test_recordbatch_itercolumns():
+    data = [
+        pa.array(range(5), type='int16'),
+        pa.array([-10, -5, 0, None, 10], type='int32')
+    ]
+    batch = pa.record_batch(data, ['c0', 'c1'])
+
+    columns = []
+    for col in batch.itercolumns():
+        columns.append(col)
+
+    assert batch.columns == columns
+    assert batch == pa.record_batch(columns, names=batch.column_names)
+    assert batch != pa.record_batch(columns[1:], names=batch.column_names[1:])
+    assert batch != columns
+
+
 def test_recordbatch_equals():
     data1 = [
         pa.array(range(5), type='int16'),
@@ -950,6 +972,11 @@ def test_table_basics():
     assert wr() is not None
     del table
     assert wr() is None
+
+
+def test_table_dunder_init():
+    with pytest.raises(TypeError, match='Table'):
+        pa.Table()
 
 
 def test_table_from_arrays_preserves_column_metadata():
