1eb100d707be8203bdf8f8c9e09ef0e79d6253ac
Benjamin Kietzman <bengilgit@gmail.com>
2023-06-29 10:30:20 -0400

    GH-36367: [C++] Add a zipped range utility

--- cpp/src/arrow/util/range_test.cc
+++ cpp/src/arrow/util/range_test.cc
@@ -20,18 +20,21 @@
 #include <cstdint>
 #include <vector>
 
+#include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
 #include "arrow/testing/random.h"
 #include "arrow/testing/util.h"
 #include "arrow/util/range.h"
 
-namespace arrow {
+using testing::ElementsAre;
+
+namespace arrow::internal {
 
 class TestLazyIter : public ::testing::Test {
  public:
   int64_t kSize = 1000;
-  void SetUp() {
+  void SetUp() override {
     randint(kSize, 0, 1000000, &source_);
     target_.resize(kSize);
   }
@@ -43,7 +46,7 @@ class TestLazyIter : public ::testing::Test {
 
 TEST_F(TestLazyIter, TestIncrementCopy) {
   auto add_one = [this](int64_t index) { return source_[index] + 1; };
-  auto lazy_range = internal::MakeLazyRange(add_one, kSize);
+  auto lazy_range = MakeLazyRange(add_one, kSize);
   std::copy(lazy_range.begin(), lazy_range.end(), target_.begin());
 
   for (int64_t index = 0; index < kSize; ++index) {
@@ -53,7 +56,7 @@ TEST_F(TestLazyIter, TestIncrementCopy) {
 
 TEST_F(TestLazyIter, TestPostIncrementCopy) {
   auto add_one = [this](int64_t index) { return source_[index] + 1; };
-  auto lazy_range = internal::MakeLazyRange(add_one, kSize);
+  auto lazy_range = MakeLazyRange(add_one, kSize);
   auto iter = lazy_range.begin();
   auto end = lazy_range.end();
   auto target_iter = target_.begin();
@@ -66,4 +69,79 @@ TEST_F(TestLazyIter, TestPostIncrementCopy) {
     ASSERT_EQ(source_[index] + 1, target_[index]);
   }
 }
-}  // namespace arrow
+
+TEST(Zip, TupleTypes) {
+  static size_t count = 0;
+  struct Counted {
+    Counted() { ++count; }
+    ~Counted() { --count; }
+
+    Counted(const Counted&) { ++count; }
+    Counted& operator=(const Counted&) = delete;
+  };
+
+  {
+    const Counted const_arr[] = {{}, {}, {}};
+    EXPECT_EQ(count, std::size(const_arr));
+
+    for (auto [e] : Zip(const_arr)) {
+      // Putting a const reference to range into Zip results in no copies and the
+      // corresponding tuple element will also be a const reference
+      EXPECT_EQ(count, std::size(const_arr));
+      static_assert(std::is_same_v<decltype(e), const Counted&>);
+    }
+  }
+
+  {
+    Counted arr[] = {{}, {}, {}};
+    EXPECT_EQ(count, std::size(arr));
+
+    for (auto [e] : Zip(arr)) {
+      // Putting a mutable reference to range into Zip results in no copies and the
+      // corresponding tuple element will also be a mutable reference
+      EXPECT_EQ(count, std::size(arr));
+      static_assert(std::is_same_v<decltype(e), Counted&>);
+    }
+  }
+
+  for (auto [e] : Zip(std::vector<Counted>{{}, {}, {}})) {
+    // Putting a prvalue vector into Zip results in no copies and keeps the temporary
+    // alive as a mutable vector so that we can move out of it if we might reuse the
+    // elements:
+    EXPECT_EQ(count, 3);
+    static_assert(std::is_same_v<decltype(e), Counted&>);
+  }
+
+  std::vector<bool> v{false, false, false, false};
+  for (auto [i, e] : Zip(Enumerate<int>, v)) {
+    // Testing with a range whose references aren't actually references
+    static_assert(std::is_same_v<decltype(e), decltype(v)::reference>);
+    static_assert(std::is_same_v<decltype(e), decltype(v[0])>);
+    static_assert(!std::is_reference_v<decltype(e)>);
+    e = (i % 2 == 0);
+  }
+
+  EXPECT_THAT(v, ElementsAre(true, false, true, false));
+}
+
+TEST(Zip, EndAfterShortestEnds) {
+  std::vector<int> shorter{0, 0, 0}, longer{9, 9, 9, 9, 9, 9};
+
+  for (auto [s, l] : Zip(shorter, longer)) {
+    std::swap(s, l);
+  }
+
+  EXPECT_THAT(longer, ElementsAre(0, 0, 0, 9, 9, 9));
+}
+
+TEST(Zip, Enumerate) {
+  std::vector<std::string> vec(3);
+
+  for (auto [i, s] : Zip(Enumerate<>, vec)) {
+    static_assert(std::is_same_v<decltype(s), std::string&>);
+    s = std::to_string(i + 7);
+  }
+
+  EXPECT_THAT(vec, ElementsAre("7", "8", "9"));
+}
+}  // namespace arrow::internal
