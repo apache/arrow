a485bb7a7e948b2089b935fe7d5ab098a0e615de
Julien Jerphanion <git@jjerphan.xyz>
2023-07-10 10:36:08 +0200

    GH-34787: [Python] Accept zero_copy_only=False for ChunkedArray.to_numpy (#35582)
    ### Rationale for this change
    
    Addresses GH-34787.
    
    ### What changes are included in this PR?
    
    I think this proposes the minimal viable changes to have the same signature as proposed in GH-34787.
    
    ### Are these changes tested?
    
    Yes, for now, a single simple test has been introduced to test the correct behavior of `ChunkedArray.to_numpy` with respect to the addition of `zero_copy_only`.
    
    * Closes: #34787
    
    Lead-authored-by: Julien Jerphanion <git@jjerphan.xyz>
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>
    Signed-off-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- python/pyarrow/tests/test_array.py
+++ python/pyarrow/tests/test_array.py
@@ -179,6 +179,21 @@ def test_to_numpy_zero_copy():
     np.testing.assert_array_equal(np_arr, expected)
 
 
+def test_chunked_array_to_numpy_zero_copy():
+    elements = [[2, 2, 4], [4, 5, 100]]
+
+    chunked_arr = pa.chunked_array(elements)
+
+    msg = "zero_copy_only must be False for pyarrow.ChunkedArray.to_numpy"
+
+    with pytest.raises(ValueError, match=msg):
+        chunked_arr.to_numpy(zero_copy_only=True)
+
+    np_arr = chunked_arr.to_numpy()
+    expected = [2, 2, 4, 4, 5, 100]
+    np.testing.assert_array_equal(np_arr, expected)
+
+
 def test_to_numpy_unsupported_types():
     # ARROW-2871: Some primitive types are not yet supported in to_numpy
     bool_arr = pa.array([True, False, True])
