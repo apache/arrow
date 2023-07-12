27789935e588866acc22a92afdf7fd2da561f8d5
Dewey Dunnington <dewey@voltrondata.com>
2023-07-12 09:31:36 -0300

    don't modify formula

--- dev/tasks/homebrew-formulae/autobrew/apache-arrow.rb
+++ dev/tasks/homebrew-formulae/autobrew/apache-arrow.rb
@@ -35,7 +35,6 @@ class ApacheArrow < Formula
   depends_on "aws-sdk-cpp"
   depends_on "brotli"
   depends_on "lz4"
-  depends_on "openssl@1.1"
   depends_on "snappy"
   depends_on "thrift"
   depends_on "zstd"
