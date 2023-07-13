5f6c9c3e648b4961a44cd6fa1edd9e4d596d36ab
Junming Chen <junming.chen.r@outlook.com>
2023-07-13 14:55:26 +0800

    Update vector_hash.cc

--- cpp/src/arrow/compute/kernels/vector_hash.cc
+++ cpp/src/arrow/compute/kernels/vector_hash.cc
@@ -764,7 +764,8 @@ const FunctionDoc dictionary_encode_doc(
     "DictionaryEncodeOptions");
 
 // ----------------------------------------------------------------------
-// This function does not use any hashing utilities but is kept in this file to be near dictionary_encode
+// This function does not use any hashing utilities
+// but is kept in this file to be near dictionary_encode
 // Dictionary decode implementation
 
 const FunctionDoc dictionary_decode_doc{
