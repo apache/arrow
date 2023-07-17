07268224fd6e1e8c9031d8a2f5d0e9617009ca1d
mwish <maplewish117@gmail.com>
2023-07-17 13:13:01 +0800

    tiny update

--- cpp/src/parquet/benchmark_util.h
+++ cpp/src/parquet/benchmark_util.h
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #pragma once
 
 #include <random>
@@ -24,7 +41,7 @@ constexpr uint32_t kDefaultDataStringLength = 8;
 
 template <typename T>
 void GenerateBenchmarkData(uint32_t size, uint32_t seed, T* data,
-                           [[maybe_unused]] std::vector<uint8_t>* heap = nullptr,
+                           [[maybe_unused]] std::vector<uint8_t>* heap,
                            uint32_t data_string_length = kDefaultDataStringLength) {
   if constexpr (std::is_integral_v<T>) {
     std::default_random_engine gen(seed);
