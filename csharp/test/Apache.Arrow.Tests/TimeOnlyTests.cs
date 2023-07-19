7e09e1af1c781e8cd992560d211ae3df7992a525
Curt Hagenlocher <curt@hagenlocher.org>
2023-07-19 11:45:18 -0700

    GH-34620: [C#] Support DateOnly and TimeOnly on .NET 6.0+ (#36125)
    ### What changes are included in this PR?
    
    Date32Array and Date64Array now support DateOnly values for construction and reading on .NET 6.0 and later.
    Time32Array and Time64Array now support TimeOnly values for construction and reading on .NET 6.0 and later.
    A new TimeArrayBuilder type is used to share logic between Time32Array.Builder and Time64Array.Builder just as the DateArrayBuilder does for the date array types.
    
    ### Are these changes tested?
    
    Yes
    * Closes: #34620
    
    Authored-by: Curt Hagenlocher <curt@hagenlocher.org>
    Signed-off-by: Weston Pace <weston.pace@gmail.com>

---
+++ csharp/test/Apache.Arrow.Tests/TimeOnlyTests.cs
@@ -0,0 +1,111 @@
+﻿// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Apache.Arrow.Types;
+using Xunit;
+
+namespace Apache.Arrow.Tests
+{
+    public class TimeOnlyTests
+    {
+        private static IEnumerable<object[]> GetTimeOnlyData(params TimeUnit[] units) =>
+            from time in TestDateAndTimeData.ExampleTimes
+            from unit in units
+            select new object[] { TimeOnly.FromTimeSpan(time), unit };
+
+        public class Time32
+        {
+            public static IEnumerable<object[]> GetTestData => GetTimeOnlyData(TimeUnit.Second, TimeUnit.Millisecond);
+
+            [Fact]
+            public void AppendThenGetGivesNull()
+            {
+                // Arrange
+                var builder = new Time32Array.Builder();
+
+                // Act
+                builder = builder.AppendNull();
+
+                // Assert
+                var array = builder.Build();
+                Assert.Equal(1, array.Length);
+                Assert.Null(array.GetTime(0));
+                Assert.Null(array.GetValue(0));
+            }
+
+            [Theory]
+            [MemberData(nameof(GetTestData))]
+            public void AppendTimeGivesSameTime(TimeOnly time, TimeUnit timeUnit)
+            {
+                // Arrange
+                var builder = new Time32Array.Builder(timeUnit);
+                var expectedTime = time;
+                int expectedMilliseconds = (int)(time.Ticks / TimeSpan.TicksPerMillisecond);
+
+                // Act
+                builder = builder.Append(time);
+
+                // Assert
+                var array = builder.Build();
+                Assert.Equal(1, array.Length);
+                Assert.Equal(expectedTime, array.GetTime(0));
+                Assert.Equal(expectedMilliseconds, array.GetMilliSeconds(0));
+            }
+        }
+
+        public class Time64
+        {
+            public static IEnumerable<object[]> GetTestData => GetTimeOnlyData(TimeUnit.Microsecond, TimeUnit.Nanosecond);
+
+            [Fact]
+            public void AppendThenGetGivesNull()
+            {
+                // Arrange
+                var builder = new Time64Array.Builder();
+
+                // Act
+                builder = builder.AppendNull();
+
+                // Assert
+                var array = builder.Build();
+                Assert.Equal(1, array.Length);
+                Assert.Null(array.GetTime(0));
+                Assert.Null(array.GetValue(0));
+            }
+
+            [Theory]
+            [MemberData(nameof(GetTestData))]
+            public void AppendTimeGivesSameTime(TimeOnly time, TimeUnit timeUnit)
+            {
+                // Arrange
+                var builder = new Time64Array.Builder(timeUnit);
+                var expectedTime = time;
+                long expectedNanoseconds = time.Ticks * TimeSpan.NanosecondsPerTick;
+
+                // Act
+                builder = builder.Append(time);
+
+                // Assert
+                var array = builder.Build();
+                Assert.Equal(1, array.Length);
+                Assert.Equal(expectedTime, array.GetTime(0));
+                Assert.Equal(expectedNanoseconds, array.GetNanoSeconds(0));
+            }
+        }
+    }
+}
