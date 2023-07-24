b8b8697cb990ce69c3e5ce131ea00ac75817b7d4
thorfour <me@thor-hansen.com>
2023-07-24 16:59:48 -0500

    panic binary builder unit test
    Ensures the indicies are released upon panic

--- go/arrow/array/dictionary_test.go
+++ go/arrow/array/dictionary_test.go
@@ -1800,3 +1800,49 @@ func TestDictionaryAppendIndices(t *testing.T) {
 		})
 	}
 }
+
+type panicAllocator struct {
+	n       int
+	paniced bool
+	memory.Allocator
+}
+
+func (p *panicAllocator) Allocate(size int) []byte {
+	if size > p.n {
+		p.paniced = true
+		panic("panic allocator")
+	}
+	return p.Allocator.Allocate(size)
+}
+
+func (p *panicAllocator) Reallocate(size int, b []byte) []byte {
+	return p.Allocator.Reallocate(size, b)
+}
+
+func (p *panicAllocator) Free(b []byte) {
+	p.Allocator.Free(b)
+}
+
+func TestBinaryDictionaryPanic(t *testing.T) {
+	mem := memory.NewCheckedAllocator(memory.DefaultAllocator)
+	defer mem.AssertSize(t, 0)
+
+	allocator := &panicAllocator{
+		n:         400,
+		Allocator: mem,
+	}
+
+	expectedType := &arrow.DictionaryType{IndexType: &arrow.Int8Type{}, ValueType: arrow.BinaryTypes.String}
+	bldr := array.NewDictionaryBuilder(allocator, expectedType)
+	defer bldr.Release()
+
+	bldr.AppendNull()
+	allocator.n = 0 // force panic
+	func() {
+		defer func() {
+			recover()
+		}()
+		bldr.NewArray()
+	}()
+	assert.True(t, allocator.paniced)
+}
