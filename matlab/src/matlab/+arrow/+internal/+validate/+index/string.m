d84a9654c5e3f7142ec2271f2761d6263450076a
Sarah Gilmore <sgilmore@mathworks.com>
2023-08-14 11:33:58 -0400

    Fix help text in arrow.internal.valdiate.index.string

--- matlab/src/matlab/+arrow/+internal/+validate/+index/string.m
+++ matlab/src/matlab/+arrow/+internal/+validate/+index/string.m
@@ -1,4 +1,4 @@
-%NUMERIC Validates the numeric index value.
+%STRING Validates string index values.
 
 % Licensed to the Apache Software Foundation (ASF) under one or more
 % contributor license agreements.  See the NOTICE file distributed with
