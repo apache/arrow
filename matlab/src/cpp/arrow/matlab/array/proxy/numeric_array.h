e4cfd88d812a71c029c0258a3b98d1d0394aa484
sgilmore10 <74676073+sgilmore10@users.noreply.github.com>
2023-08-17 16:27:24 -0400

    GH-37228: [MATLAB] Add C++ `ARROW_MATLAB_EXPORT` symbol export macro (#37233)
    ### Rationale for this change
    
    Currently, the main shared library used by the MATLAB interface - `libarrowproxy` - does not explicitly export any symbols.
    
    This hasn't been a major issue so far based on our usage of these symbols - except for in one case - which was being able to define a template specialization of the static make function for `NumericArray<T>` in a C++ source file. To work around this, we decided to inline these template specializations.
    
    However, this isn't an ideal solution because these functions shouldn't need to be inlined (and, in fact, this could have undesirable performance implications).
    
    In addition, to enable the ability to write C++ unit tests for code in the "Proxy layer", we would need to export symbols from `libarrowproxy`.
    
    This is the first in a series of pull requests in which we will export symbols from `libarrowproxy` so we can write C++ unit tests via the GoogleTest framework.
    
    ### What changes are included in this PR?
    
    1. Added a C++ `ARROW_MATLAB_EXPORT` macro for exporting symbols in `visibility.h`
    2. Added a new header `timestamp_array.h` which declares the template method specialization of the static `make` function for `NumericArray<arrow::TimestampType>`
    3. Moved the definition of `NumericArray<arrow::TimestampType>::make` to `timestamp_array.cc`
    
    ### Are these changes tested?
    
    New tests points are not needed. These changes are covered by our existing  test suite.
    
    ### Are there any user-facing changes?
    
    No.
    
    ### Future Directions
    
    1. As mentioned above, this is the first in a series of pull requests. In future PRs we will export more symbols to enable C++ unit testing.
    * Closes: #37228
    
    Lead-authored-by: Sarah Gilmore <sgilmore@mathworks.com>
    Co-authored-by: Kevin Gurney <kgurney@mathworks.com>
    Signed-off-by: Kevin Gurney <kgurney@mathworks.com>

--- matlab/src/cpp/arrow/matlab/array/proxy/numeric_array.h
+++ matlab/src/cpp/arrow/matlab/array/proxy/numeric_array.h
@@ -33,9 +33,6 @@
 
 #include "libmexclass/proxy/Proxy.h"
 
-#include "arrow/matlab/type/time_unit.h"
-#include "arrow/util/utf8.h"
-
 namespace arrow::matlab::array::proxy {
 
 template<typename ArrowType>
@@ -86,50 +83,4 @@ class NumericArray : public arrow::matlab::array::proxy::Array {
             context.outputs[0] = result;
         }
 };
-
-     // Specialization of NumericArray::Make for arrow::TimestampType.
-    template <>
-    inline libmexclass::proxy::MakeResult NumericArray<arrow::TimestampType>::make(const libmexclass::proxy::FunctionArguments& constructor_arguments) {
-        namespace mda = ::matlab::data;
-        using MatlabBuffer = arrow::matlab::buffer::MatlabBuffer;
-        using TimestampArray = arrow::TimestampArray;
-        using TimestampArrayProxy = arrow::matlab::array::proxy::NumericArray<arrow::TimestampType>;
-
-        mda::StructArray opts = constructor_arguments[0];
-
-        // Get the mxArray from constructor arguments
-        const mda::TypedArray<int64_t> timestamp_mda = opts[0]["MatlabArray"];
-        const mda::TypedArray<bool> validity_bitmap_mda = opts[0]["Valid"];
-        
-        const mda::TypedArray<mda::MATLABString> timezone_mda = opts[0]["TimeZone"];
-        const mda::TypedArray<mda::MATLABString> units_mda = opts[0]["TimeUnit"];
-
-        // extract the time zone string
-        const std::u16string& u16_timezone = timezone_mda[0];
-        MATLAB_ASSIGN_OR_ERROR(const auto timezone, 
-                               arrow::util::UTF16StringToUTF8(u16_timezone),
-                               error::UNICODE_CONVERSION_ERROR_ID);
-
-        // extract the time unit
-        const std::u16string& u16_timeunit = units_mda[0];
-        MATLAB_ASSIGN_OR_ERROR(const auto time_unit, 
-                               arrow::matlab::type::timeUnitFromString(u16_timeunit),
-                               error::UKNOWN_TIME_UNIT_ERROR_ID)
-
-        // create the timestamp_type
-        auto data_type = arrow::timestamp(time_unit, timezone);
-        auto array_length = static_cast<int64_t>(timestamp_mda.getNumberOfElements()); // cast size_t to int64_t
-
-        auto data_buffer = std::make_shared<MatlabBuffer>(timestamp_mda);
-
-        // Pack the validity bitmap values.
-        MATLAB_ASSIGN_OR_ERROR(auto packed_validity_bitmap, 
-                               bit::packValid(validity_bitmap_mda), 
-                               error::BITPACK_VALIDITY_BITMAP_ERROR_ID);
-
-        auto array_data = arrow::ArrayData::Make(data_type, array_length, {packed_validity_bitmap, data_buffer});
-        auto timestamp_array = std::static_pointer_cast<TimestampArray>(arrow::MakeArray(array_data));
-        return std::make_shared<TimestampArrayProxy>(std::move(timestamp_array));
-    }
-
 }
