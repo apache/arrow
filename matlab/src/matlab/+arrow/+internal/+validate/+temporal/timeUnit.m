12885d26fabc295f2a73c9d59bba97ebf95c54cc
Kevin Gurney <kgurney@mathworks.com>
2023-08-18 14:59:18 -0400

    Add missing Apache V2 license header.

--- matlab/src/matlab/+arrow/+internal/+validate/+temporal/timeUnit.m
+++ matlab/src/matlab/+arrow/+internal/+validate/+temporal/timeUnit.m
@@ -1,6 +1,21 @@
-function timeUnit(temporalType, timeUnit)
 %TIMEUNIT Validates whether the given arrow.type.TimeUnit enumeration
 % value is supported for the specified temporal type.
+
+% Licensed to the Apache Software Foundation (ASF) under one or more
+% contributor license agreements.  See the NOTICE file distributed with
+% this work for additional information regarding copyright ownership.
+% The ASF licenses this file to you under the Apache License, Version
+% 2.0 (the "License"); you may not use this file except in compliance
+% with the License.  You may obtain a copy of the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+% implied.  See the License for the specific language governing
+% permissions and limitations under the License.
+function timeUnit(temporalType, timeUnit)
     arguments
         temporalType (1,1) string {mustBeMember(temporalType, ["Time32", "Time64"])}
         timeUnit (1,1) arrow.type.TimeUnit
