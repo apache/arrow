49599366f367561a06be66b768ecfa1965895313
sgilmore10 <74676073+sgilmore10@users.noreply.github.com>
2023-08-21 12:08:25 -0400

    GH-37262: [MATLAB] Add an abstract class called `arrow.type.TimeType` (#37279)
    ### Rationale for this change
    
    To reduce code duplication within `Time32Type` and `Time64Type`, we should add an abstract class called `arrow.type.TimeType` that implements  the getter method for the `TimeUnit` property. This class hierarchy will mirror the class hierarchy in the C++ arrow implementation.
    
    ### What changes are included in this PR?
    
    1. Defined a new C++ proxy class for called `arrow::matlab::type::proxy::TimeType`. This class defines a `getTimeUnit` method.
    2. Modified the C++ proxy class `Time32Type` to inherit from `arrow::matlab::type::proxy::TimeType`. Removed the `getTimeUnit` method from this class because it's now defined on `TimeType`.
    3. Added a new MATLAB class called `arrow.type.TimeType`. It has one method: `get.TimeUnit`.
    4. Modified `arrow.type.Time32Type` to inherit from `arrow.type.TimeType` and removed its `get.TimeUnit` method.
    
    ### Are these changes tested?
    
    Yes, the existing tests cover these changes.
    
    ### Are there any user-facing changes?
    
    No.
    
    ### Future Directions
    
    1. #37232
    2. #37229
    3. #37230   
    * Closes: #37262
    
    Authored-by: Sarah Gilmore <sgilmore@mathworks.com>
    Signed-off-by: Kevin Gurney <kgurney@mathworks.com>

--- matlab/src/cpp/arrow/matlab/type/proxy/time32_type.cc
+++ matlab/src/cpp/arrow/matlab/type/proxy/time32_type.cc
@@ -22,9 +22,7 @@
 
 namespace arrow::matlab::type::proxy {
 
-    Time32Type::Time32Type(std::shared_ptr<arrow::Time32Type> time32_type) : FixedWidthType(std::move(time32_type)) {
-        REGISTER_METHOD(Time32Type, getTimeUnit);
-    }
+    Time32Type::Time32Type(std::shared_ptr<arrow::Time32Type> time32_type) : TimeType(std::move(time32_type)) {}
 
     libmexclass::proxy::MakeResult Time32Type::make(const libmexclass::proxy::FunctionArguments& constructor_arguments) {
         namespace mda = ::matlab::data;
@@ -44,17 +42,5 @@ namespace arrow::matlab::type::proxy {
         auto type = arrow::time32(timeunit);
         auto time_type = std::static_pointer_cast<arrow::Time32Type>(type);
         return std::make_shared<Time32TypeProxy>(std::move(time_type));
-    }
-
-    void Time32Type::getTimeUnit(libmexclass::proxy::method::Context& context) {
-        namespace mda = ::matlab::data;
-        mda::ArrayFactory factory;
-
-        auto time32_type = std::static_pointer_cast<arrow::Time32Type>(data_type);
-        const auto timeunit = time32_type->unit();
-        // Cast to uint8_t since there are only four supported TimeUnit enumeration values:
-        // Nanosecond, Microsecond, Millisecond, Second
-        auto timeunit_mda = factory.createScalar(static_cast<uint8_t>(timeunit));
-        context.outputs[0] = timeunit_mda;
     }
 }
