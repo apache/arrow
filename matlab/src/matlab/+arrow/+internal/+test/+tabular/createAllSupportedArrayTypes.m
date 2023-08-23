dea8eb8435d6f988a0f788f639d33d856b62e074
Sarah Gilmore <sgilmore@mathworks.com>
2023-08-23 14:38:37 -0400

    Rename createSupportedArrays to createAllSupportedArrayTypes

--- matlab/src/matlab/+arrow/+internal/+test/+tabular/createAllSupportedArrayTypes.m
+++ matlab/src/matlab/+arrow/+internal/+test/+tabular/createAllSupportedArrayTypes.m
@@ -16,7 +16,7 @@
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
 
-function [arrowArrays, matlabData] = createSupportedArrays(opts)
+function [arrowArrays, matlabData] = createAllSupportedArrayTypes(opts)
     arguments
         opts.NumRows(1, 1) {mustBeFinite, mustBeNonnegative} = 3;  
     end
