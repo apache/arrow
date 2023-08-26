0e5397637ab41af30585f772ecf31966e1ebd48d
Sutou Kouhei <kou@clear-code.com>
2023-08-26 16:05:31 +0900

    GH-37352: [C++] Don't put all dependencies to ArrowConfig.cmake/arrow.pc
    Split Flight related dependencies such as gRPC to
    ArrowFlightConfig.cmake/arrow-flight.pc from ArrowConfig.cmake/arrow.pc.
    
    Split Parquet related dependencies such as Thrift to
    ParquetConfig.cmake/parquet.pc from ArrowConfig.cmake/arrow.pc.

--- dev/release/verify-yum.sh
+++ dev/release/verify-yum.sh
@@ -211,7 +211,7 @@ pushd build/minimal_build
 ${cmake_command} .
 make -j$(nproc)
 ./arrow-example
-c++ -std=c++17 -o arrow-example example.cc $(pkg-config --cflags --libs arrow)
+c++ -o arrow-example example.cc $(pkg-config --cflags --libs arrow) -std=c++17
 ./arrow-example
 popd
 echo "::endgroup::"
