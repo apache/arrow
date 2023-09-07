8f4727e3f15ecc0add7f9c37ba9c65ca83be70c3
Sarah Gilmore <sgilmore@mathworks.com>
2023-09-07 10:15:53 -0400

    Add base class NumericType that implements preallocateMATLABArray for all Numeric Types

---
+++ matlab/src/matlab/+arrow/+type/NumericType.m
@@ -0,0 +1,19 @@
+classdef NumericType < arrow.type.FixedWidthType
+
+    methods
+        function obj = NumericType(proxy)
+            arguments
+                proxy(1, 1) libmexclass.proxy.Proxy
+            end
+
+            obj@arrow.type.FixedWidthType(proxy);
+        end
+    end
+
+    methods(Hidden)
+        function data = preallocateMATLABArray(obj, length)
+            traits = arrow.type.traits.traits(obj.Type.ID);
+            data = zeros([length 1], traits.MatlabClassName);
+        end
+    end
+end
