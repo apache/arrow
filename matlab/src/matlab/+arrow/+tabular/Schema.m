164a671f77720c94965c45510175d324fb81cdfe
Sarah Gilmore <sgilmore@mathworks.com>
2023-09-07 14:26:34 -0400

    Make Schema scalar

--- matlab/src/matlab/+arrow/+tabular/Schema.m
+++ matlab/src/matlab/+arrow/+tabular/Schema.m
@@ -1,3 +1,7 @@
+%SCHEMA A tabular schema which semantically describes
+% the names and types of the columns of an associated tabular
+% Arrow data type.
+
 % Licensed to the Apache Software Foundation (ASF) under one or more
 % contributor license agreements.  See the NOTICE file distributed with
 % this work for additional information regarding copyright ownership.
@@ -13,10 +17,8 @@
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
 
-classdef Schema < matlab.mixin.CustomDisplay
-%SCHEMA A tabular schema which semantically describes
-% the names and types of the columns of an associated tabular
-% Arrow data type.
+classdef Schema < matlab.mixin.CustomDisplay & ...
+                  matlab.mixin.Scalar
 
     properties (GetAccess=public, SetAccess=private, Hidden)
         Proxy
