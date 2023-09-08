b85c19ad42f39e9691dc88279f13b621028b9758
Jin Shang <shangjin1997@gmail.com>
2023-09-08 21:57:12 +0800

    revert delete empty line

--- cpp/src/arrow/compute/kernels/aggregate_internal.h
+++ cpp/src/arrow/compute/kernels/aggregate_internal.h
@@ -24,9 +24,8 @@
 #include "arrow/util/bit_run_reader.h"
 #include "arrow/util/int128_internal.h"
 #include "arrow/util/logging.h"
-namespace arrow {
-namespace compute {
-namespace internal {
+
+namespace arrow::compute::internal {
 
 // Find the largest compatible primitive type for a primitive type.
 template <typename I, typename Enable = void>
@@ -257,6 +256,4 @@ SumType SumArray(const ArraySpan& data, Status* status) {
       data, [](ValueType v) { return static_cast<SumType>(v); }, status);
 }
 
-}  // namespace internal
-}  // namespace compute
-}  // namespace arrow
+}  // namespace arrow::compute::internal
