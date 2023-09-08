a19b8a7093067e38dd3918226e8acdea28eebc6d
H. Vetinari <h.vetinari@gmx.com>
2023-09-08 16:15:20 +1100

    update arrow-cpp recipe

--- dev/tasks/conda-recipes/arrow-cpp/build-arrow.sh
+++ dev/tasks/conda-recipes/arrow-cpp/build-arrow.sh
@@ -43,8 +43,8 @@ if [[ "${build_platform}" != "${target_platform}" ]]; then
     fi
     EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DCLANG_EXECUTABLE=${BUILD_PREFIX}/bin/${CONDA_TOOLCHAIN_HOST}-clang"
     EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DLLVM_LINK_EXECUTABLE=${BUILD_PREFIX}/bin/llvm-link"
+    EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DARROW_JEMALLOC_LG_PAGE=16"
     sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=${BUILD_PREFIX}/bin/grpc_cpp_plugin\";g" ../src/arrow/flight/CMakeLists.txt
-    sed -ie 's;"--with-jemalloc-prefix\=je_arrow_";"--with-jemalloc-prefix\=je_arrow_" "--with-lg-page\=16";g' ../cmake_modules/ThirdpartyToolchain.cmake
 fi
 
 # disable -fno-plt, which causes problems with GCC on PPC
