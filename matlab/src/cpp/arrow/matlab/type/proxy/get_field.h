6ef195e986ce58d965e9b3834a65f8adf73b6a6d
Sarah Gilmore <sgilmore@mathworks.com>
2023-09-11 12:03:20 -0400

    fix indentation in get_field

--- matlab/src/cpp/arrow/matlab/type/proxy/get_field.h
+++ matlab/src/cpp/arrow/matlab/type/proxy/get_field.h
@@ -30,35 +30,35 @@
 
 namespace arrow::matlab::type::proxy {
 
-        namespace {
+    namespace {
     
-            libmexclass::error::Error makeUnknownFieldNameError(const std::string& name) {
-                using namespace libmexclass::error;
-                std::stringstream error_message_stream;
-                error_message_stream << "Unknown field name: '";
-                error_message_stream << name;
-                error_message_stream << "'.";
-                return Error{error::ARROW_UNKNOWN_FIELD_NAME, error_message_stream.str()};
-            }
+        libmexclass::error::Error makeUnknownFieldNameError(const std::string& name) {
+            using namespace libmexclass::error;
+            std::stringstream error_message_stream;
+            error_message_stream << "Unknown field name: '";
+            error_message_stream << name;
+            error_message_stream << "'.";
+            return Error{error::ARROW_UNKNOWN_FIELD_NAME, error_message_stream.str()};
+        }
     
-            libmexclass::error::Error makeZeroFieldsError() {
-                using namespace libmexclass::error;
-                return Error{error::ARROW_ZERO_FIELDS_NUMERIC_INDEX,
-                             "Numeric indexing using the field method is not supported when the number of fields is 0"};
-            }
+        libmexclass::error::Error makeZeroFieldsError() {
+            using namespace libmexclass::error;
+            return Error{error::ARROW_ZERO_FIELDS_NUMERIC_INDEX,
+                         "Numeric indexing using the field method is not supported when the number of fields is 0"};
+        }
 
-            libmexclass::error::Error makeInvalidNumericIndexError(const int32_t matlab_index, const int32_t num_fields) {
-                using namespace libmexclass::error;
-                const std::string& error_message_id = std::string{error::ARROW_INVALID_NUMERIC_FIELD_INDEX};
-                std::stringstream error_message_stream;
-                error_message_stream << "Invalid field index: ";
-                error_message_stream << matlab_index;
-                error_message_stream << ". Field index must be between 1 and the number of fields (";
-                error_message_stream << num_fields;
-                error_message_stream << ").";
-                const std::string& error_message = error_message_stream.str();
-               return Error{error_message_id, error_message}; 
-            }
+        libmexclass::error::Error makeInvalidNumericIndexError(const int32_t matlab_index, const int32_t num_fields) {
+            using namespace libmexclass::error;
+            const std::string& error_message_id = std::string{error::ARROW_INVALID_NUMERIC_FIELD_INDEX};
+            std::stringstream error_message_stream;
+            error_message_stream << "Invalid field index: ";
+            error_message_stream << matlab_index;
+            error_message_stream << ". Field index must be between 1 and the number of fields (";
+            error_message_stream << num_fields;
+            error_message_stream << ").";
+            const std::string& error_message = error_message_stream.str();
+           return Error{error_message_id, error_message}; 
+        }
     }
 
 
@@ -80,10 +80,9 @@ namespace arrow::matlab::type::proxy {
         if (matlab_index < 1 || matlab_index > num_fields) {
             return makeInvalidNumericIndexError(matlab_index, num_fields);
         }
+
         auto field = obj->field(index);
         auto field_proxy = std::make_shared<proxy::Field>(std::move(field));
-
-        auto field_proxy_id = ProxyManager::manageProxy(field_proxy);
-        return field_proxy_id;
+        return ProxyManager::manageProxy(field_proxy);
     }
 }
