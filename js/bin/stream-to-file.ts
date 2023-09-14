e8f3eb8083e41b2bba0988cf82633935ed9d7f0b
ptaylor <paul.e.taylor@me.com>
2023-09-14 14:00:13 -0700

    cleanup

--- js/bin/stream-to-file.ts
+++ js/bin/stream-to-file.ts
@@ -17,8 +17,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// @ts-check
-
 import * as fs from 'fs';
 import * as path from 'path';
 import { finished as eos } from 'stream/promises';
