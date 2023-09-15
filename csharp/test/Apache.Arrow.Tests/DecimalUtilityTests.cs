8d3237f0427a9d157e68ec2f3e7b6ea11affbfdd
David Coe <coedavid@umich.edu>
2023-09-14 21:19:45 -0400

    update values

--- csharp/test/Apache.Arrow.Tests/DecimalUtilityTests.cs
+++ csharp/test/Apache.Arrow.Tests/DecimalUtilityTests.cs
@@ -49,7 +49,7 @@ namespace Apache.Arrow.Tests
 
             [Theory]
             [InlineData(4.56, 38, 9, false)]
-            [InlineData(7.56, 76, 38, true)]
+            [InlineData(7.89, 76, 38, true)]
             public void Decimal256HasExpectedResultOrThrows(decimal d, int precision, int scale, bool shouldThrow)
             {
                 var builder = new Decimal256Array.Builder(new Decimal256Type(precision, scale));
