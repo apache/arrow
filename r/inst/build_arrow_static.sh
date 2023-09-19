d06c73ddbfbfac036a786c388edba5f757aed601
Jacob Wujciak-Jens <jacob@wujciak.de>
2023-09-19 04:09:08 +0200

    specify comment

--- r/inst/build_arrow_static.sh
+++ r/inst/build_arrow_static.sh
@@ -55,7 +55,7 @@ else
   ARROW_DEFAULT_PARAM="OFF"
 fi
 
-# Disable mimalloc on IntelLLVM
+# Disable mimalloc on IntelLLVM because the bundled version (2.0.x) does not support it
 case "$CXX" in
   *icpx*)
     ARROW_MIMALLOC="OFF"
