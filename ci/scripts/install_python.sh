907f66d4b5f9cde99d23be11834f961658d3c683
Raúl Cumplido <raulcumplido@gmail.com>
2023-09-27 13:26:22 +0200

    GH-37880: [CI][Python][Packaging] Add support for Python 3.12

--- ci/scripts/install_python.sh
+++ ci/scripts/install_python.sh
@@ -27,8 +27,9 @@ platforms=([windows]=Windows
 declare -A versions
 versions=([3.8]=3.8.10
           [3.9]=3.9.13
-          [3.10]=3.10.8
-          [3.11]=3.11.0)
+          [3.10]=3.10.13
+          [3.11]=3.11.5
+          [3.12]=3.12.0)
 
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <platform> <version>"
@@ -47,6 +48,8 @@ if [ $platform = "MacOSX" ]; then
 
     if [ "$(uname -m)" = "arm64" ] || [ "$version" = "3.10" ] || [ "$version" = "3.11" ]; then
         fname="python-${full_version}-macos11.pkg"
+    else if [ "$version" = "3.12" ]; then
+        fname="python-3.12.0rc3-macos11.pkg"
     else
         fname="python-${full_version}-macosx10.9.pkg"
     fi
