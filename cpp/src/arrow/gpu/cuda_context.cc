14b7cfcff89983a2c86f34a0aa7cf948c086aaa6
Dane Pitkin <dane@voltrondata.com>
2023-10-06 17:50:52 -0400

    Revert "Use cuda enum"
    This reverts commit 834b29e835d62dbeae8fd1b41224347372f867e7.

--- cpp/src/arrow/gpu/cuda_context.cc
+++ cpp/src/arrow/gpu/cuda_context.cc
@@ -322,8 +322,9 @@ Status CudaDevice::Stream::WaitEvent(const Device::SyncEvent& event) {
   }
 
   ContextSaver set_temporary(reinterpret_cast<CUcontext>(context_.get()->handle()));
-  CU_RETURN_NOT_OK("cuStreamWaitEvent",
-                   cuStreamWaitEvent(value(), cu_event, CU_EVENT_WAIT_DEFAULT));
+  // we are currently building with CUDA toolkit 11.0.3 which doesn't have enum
+  // values for the flags yet. The "flags" param *must* be 0 for now.
+  CU_RETURN_NOT_OK("cuStreamWaitEvent", cuStreamWaitEvent(value(), cu_event, 0));
   return Status::OK();
 }
 
