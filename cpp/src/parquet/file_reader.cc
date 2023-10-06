08cd4ced63f99c95873fee75ce492095d9abb229
mwish <maplewish117@gmail.com>
2023-10-06 20:21:44 +0800

    Fix Overlap column chunk ranges for pre-buffer

--- cpp/src/parquet/file_reader.cc
+++ cpp/src/parquet/file_reader.cc
@@ -139,8 +139,10 @@ const RowGroupMetaData* RowGroupReader::metadata() const { return contents_->met
 ::arrow::io::ReadRange ComputeColumnChunkRange(FileMetaData* file_metadata,
                                                int64_t source_size, int row_group_index,
                                                int column_index) {
-  auto row_group_metadata = file_metadata->RowGroup(row_group_index);
-  auto column_metadata = row_group_metadata->ColumnChunk(column_index);
+  std::unique_ptr<RowGroupMetaData> row_group_metadata =
+      file_metadata->RowGroup(row_group_index);
+  std::unique_ptr<ColumnChunkMetaData> column_metadata =
+      row_group_metadata->ColumnChunk(column_index);
 
   int64_t col_start = column_metadata->data_page_offset();
   if (column_metadata->has_dictionary_page() &&
@@ -365,6 +367,20 @@ class SerializedFile : public ParquetFileReader::Contents {
             ComputeColumnChunkRange(file_metadata_.get(), source_size_, row, col));
       }
     }
+#ifndef NDEBUG
+    if (!ranges.empty()) {
+      auto copied_ranges = ranges;
+      std::sort(copied_ranges.begin(), copied_ranges.end(),
+                [](const ::arrow::io::ReadRange& a, const ::arrow::io::ReadRange& b) {
+                  return a.offset < b.offset;
+                });
+      for (size_t i = 1; i < copied_ranges.size(); ++i) {
+        if (ranges[i].offset < ranges[i - 1].offset + ranges[i - 1].length) {
+          throw ParquetException("Overlapping column chunk ranges for prebuffer");
+        }
+      }
+    }
+#endif
     PARQUET_THROW_NOT_OK(cached_source_->Cache(ranges));
   }
 
