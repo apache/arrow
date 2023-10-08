aadb69c2ca0cbde97f3583511ca84177f9d9b7f6
mwish <maplewish117@gmail.com>
2023-10-09 05:36:44 +0800

    GH-38071: [C++][CI] Fix Overlap column chunk ranges for pre-buffer (#38073)
    ### Rationale for this change
    
    The C++ Parquet Arrow fuzz will generate bad Parquet file with bad row-range, this patch change the `CoalesceReadRanges` to return `Result<>`.
    
    ### What changes are included in this PR?
    
    Just a checking, change `CoalesceReadRanges` to return `Result<>`.
    
    ### Are these changes tested?
    
    No.
    
    ### Are there any user-facing changes?
    
    No.
    
    * Closes: #38071
    
    Authored-by: mwish <maplewish117@gmail.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- cpp/src/parquet/file_reader.cc
+++ cpp/src/parquet/file_reader.cc
@@ -139,8 +139,10 @@ const RowGroupMetaData* RowGroupReader::metadata() const { return contents_->met
 ::arrow::io::ReadRange ComputeColumnChunkRange(FileMetaData* file_metadata,
                                                int64_t source_size, int row_group_index,
                                                int column_index) {
-  auto row_group_metadata = file_metadata->RowGroup(row_group_index);
-  auto column_metadata = row_group_metadata->ColumnChunk(column_index);
+  std::unique_ptr<RowGroupMetaData> row_group_metadata =
+      file_metadata->RowGroup(row_group_index);
+  std::unique_ptr<ColumnChunkMetaData> column_metadata =
+      row_group_metadata->ColumnChunk(column_index);
 
   int64_t col_start = column_metadata->data_page_offset();
   if (column_metadata->has_dictionary_page() &&
