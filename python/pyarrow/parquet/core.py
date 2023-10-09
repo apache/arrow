ecec908f407bc568040b44842b6d5f30a35848eb
mwish <maplewish117@gmail.com>
2023-10-09 23:09:17 +0800

    add RLE as encoding type

--- python/pyarrow/parquet/core.py
+++ python/pyarrow/parquet/core.py
@@ -771,7 +771,8 @@ use_dictionary : bool or list, default True
     Specify if we should use dictionary encoding in general or only for
     some columns.
     When encoding the column, if the dictionary size is too large, the
-    column will fallback to 'PLAIN' encoding.
+    column will fallback to fallback encoding. Specially, ``BOOLEAN`` type
+    doesn't support dictionary encoding.
 compression : str or dict, default 'snappy'
     Specify the compression codec, either on a general basis or per-column.
     Valid values: {'NONE', 'SNAPPY', 'GZIP', 'BROTLI', 'LZ4', 'ZSTD'}.
@@ -823,10 +824,11 @@ use_byte_stream_split : bool or list, default False
     and should be combined with a compression codec.
 column_encoding : string or dict, default None
     Specify the encoding scheme on a per column basis.
-    Can only be used when when ``use_dictionary`` is set to False, and cannot
-    be used in combination with ``use_byte_stream_split``.
+    Can only be used when when ``use_dictionary`` is set to False, and
+    cannot be used in combination with ``use_byte_stream_split``.
     Currently supported values: {'PLAIN', 'BYTE_STREAM_SPLIT',
-    'DELTA_BINARY_PACKED', 'DELTA_LENGTH_BYTE_ARRAY', 'DELTA_BYTE_ARRAY'}.
+    'DELTA_BINARY_PACKED', 'DELTA_LENGTH_BYTE_ARRAY', 'DELTA_BYTE_ARRAY',
+    'RLE}.
     Certain encodings are only compatible with certain data types.
     Please refer to the encodings section of `Reading and writing Parquet
     files <https://arrow.apache.org/docs/cpp/parquet.html#encodings>`_.
