8a33abe12ff90946d490afde2cada63634a4e24a
Raúl Cumplido <raulcumplido@gmail.com>
2023-10-11 16:33:18 +0200

    GH-37880: [CI][Python][Packaging] Add support for Python 3.12 (#37901)
    ### Rationale for this change
    
    Python 3.12 will be released in the next couple of weeks. We should add the wheels for pyarrow on our 14.0.0 release.
    
    ### What changes are included in this PR?
    
    This PR adds jobs to build pyarrow wheels for Python 3.12.
    
    ### Are these changes tested?
    
    They will be tested via archery tasks
    
    ### Are there any user-facing changes?
    
    No but users will be able to use pyarrow with Python 3.12
    
    * Closes: #37880
    
    Authored-by: Raúl Cumplido <raulcumplido@gmail.com>
    Signed-off-by: Raúl Cumplido <raulcumplido@gmail.com>

--- ci/scripts/install_python.sh
+++ ci/scripts/install_python.sh
@@ -27,8 +27,9 @@ platforms=([windows]=Windows
 declare -A versions
 versions=([3.8]=3.8.10
           [3.9]=3.9.13
-          [3.10]=3.10.8
-          [3.11]=3.11.0)
+          [3.10]=3.10.11
+          [3.11]=3.11.5
+          [3.12]=3.12.0)
 
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <platform> <version>"
@@ -45,7 +46,7 @@ full_version=${versions[$2]}
 if [ $platform = "MacOSX" ]; then
     echo "Downloading Python installer..."
 
-    if [ "$(uname -m)" = "arm64" ] || [ "$version" = "3.10" ] || [ "$version" = "3.11" ]; then
+    if [ "$(uname -m)" = "arm64" ] || [ "$version" = "3.10" ] || [ "$version" = "3.11" ] || [ "$version" = "3.12" ]; then
         fname="python-${full_version}-macos11.pkg"
     else
         fname="python-${full_version}-macosx10.9.pkg"
