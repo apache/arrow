95e33faa6bc0aa8b9920ce8d9f3af2b33ca5dced
Sutou Kouhei <kou@clear-code.com>
2023-10-16 18:50:15 +0900

    GH-38206: [CI] Remove more pre-installed files (#38233)
    ### Rationale for this change
    
    We need more disk space...
    
    ### What changes are included in this PR?
    
    Remove more pre-installed files.
    
    ### Are these changes tested?
    
    Yes.
    
    ### Are there any user-facing changes?
    
    No.
    * Closes: #38206
    
    Authored-by: Sutou Kouhei <kou@clear-code.com>
    Signed-off-by: Raúl Cumplido <raulcumplido@gmail.com>

--- ci/scripts/util_free_space.sh
+++ ci/scripts/util_free_space.sh
@@ -24,12 +24,19 @@ if [ "${GITHUB_ACTIONS}" = "true" ]; then
   echo "::group::/usr/local/*"
   du -hsc /usr/local/*
   echo "::endgroup::"
+  # ~1GB
+  sudo rm -rf \
+    /usr/local/aws-cli \
+    /usr/local/aws-sam-cil \
+    /usr/local/julia* || :
   echo "::group::/usr/local/bin/*"
   du -hsc /usr/local/bin/*
   echo "::endgroup::"
   # ~1GB (From 1.2GB to 214MB)
   sudo rm -rf \
     /usr/local/bin/aliyun \
+    /usr/local/bin/aws \
+    /usr/local/bin/aws_completer \
     /usr/local/bin/azcopy \
     /usr/local/bin/bicep \
     /usr/local/bin/cmake-gui \
@@ -41,16 +48,37 @@ if [ "${GITHUB_ACTIONS}" = "true" ]; then
     /usr/local/bin/node \
     /usr/local/bin/packer \
     /usr/local/bin/pulumi* \
+    /usr/local/bin/sam \
     /usr/local/bin/stack \
     /usr/local/bin/terraform || :
+  # 142M
+  sudo rm -rf /usr/local/bin/oc || : \
   echo "::group::/usr/local/share/*"
   du -hsc /usr/local/share/*
   echo "::endgroup::"
+  # 506MB
+  sudo rm -rf /usr/local/share/chromium || :
   # 1.3GB
   sudo rm -rf /usr/local/share/powershell || :
+  echo "::group::/usr/local/lib/*"
+  du -hsc /usr/local/lib/*
+  echo "::endgroup::"
+  # 15GB
+  sudo rm -rf /usr/local/lib/android || :
+  # 341MB
+  sudo rm -rf /usr/local/lib/heroku || :
+  # 1.2GB
+  sudo rm -rf /usr/local/lib/node_modules || :
   echo "::group::/opt/*"
   du -hsc /opt/*
   echo "::endgroup::"
+  # 679MB
+  sudo rm -rf /opt/az || :
+  echo "::group::/opt/microsoft/*"
+  du -hsc /opt/microsoft/*
+  echo "::endgroup::"
+  # 197MB
+  sudo rm -rf /opt/microsoft/powershell || :
   echo "::group::/opt/hostedtoolcache/*"
   du -hsc /opt/hostedtoolcache/*
   echo "::endgroup::"
