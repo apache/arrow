0db1842e8bc2d218b49d1ac431a11e61c7ac9a29
Kevin Gurney <kgurney@mathworks.com>
2023-10-23 11:37:33 -0400

    GH-37815: [MATLAB] Add `arrow.array.ListArray` MATLAB class (#38357)
    ### Rationale for this change
    
    Now that many of the commonly-used "primitive" array types have been added to the MATLAB interface, we can implement an `arrow.array.ListArray` class.
    
    This pull request adds a new `arrow.array.ListArray` class which can be converted to a MATLAB `cell` array by calling the static `toMATLAB` method.
    
    ### What changes are included in this PR?
    
    1. Added a new `arrow.array.ListArray` MATLAB class.
    
    *Methods*
    
    `cellArray = arrow.array.ListArray.toMATLAB()`
    `listArray = arrow.array.ListArray.fromArrays(offsets, values)`
    
    *Properties*
    
    `Offsets` - `Int32Array` list offsets (uses zero-based indexing)
    `Values` - Array of values in the list (supports nesting)
    
    2. Added a new `arrow.type.traits.ListTraits` MATLAB class.
    
    **Example**
    ```matlab
    >> offsets = arrow.array(int32([0, 2, 3, 7]))
    
    offsets = 
    
    [
      0,
      2,
      3,
      7
    ]
    
    >> values = arrow.array(["A", "B", "C", "D", "E", "F", "G"])
    
    values = 
    
    [
      "A",
      "B",
      "C",
      "D",
      "E",
      "F",
      "G"
    ]
    
    >> arrowArray = arrow.array.ListArray.fromArrays(offsets, values)
    
    arrowArray = 
    
    [
      [
        "A",
        "B"
      ],
      [
        "C"
      ],
      [
        "D",
        "E",
        "F",
        "G"
      ]
    ]
    
    >> matlabArray = arrowArray.toMATLAB()
    
    matlabArray =
    
      3x1 cell array
    
        {2x1 string}
        {["C"     ]}
        {4x1 string}
    
    >> matlabArray{:}
    
    ans = 
    
      2x1 string array
    
        "A"
        "B"
    
    ans = 
    
        "C"
    
    ans = 
    
      4x1 string array
    
        "D"
        "E"
        "F"
        "G"
    
    ```
    
    ### Are these changes tested?
    
    Yes.
    
    1. Added a new `tListArray.m` test class.
    2. Added a new `tListTraits.m` test class.
    3. Updated `arrow.internal.test.tabular.createAllSupportedArrayTypes` to include `ListArray`.
    
    ### Are there any user-facing changes?
    
    Yes.
    
    1. Users can now create an `arrow.array.ListArray` from an `offsets` and `values` array by calling the static `arrow.array.ListArray.fromArrays(offsets, values)` method. `ListArray`s can be converted into MATLAB `cell` arrays by calling the static `arrow.array.ListArray.toMATLAB` method.
    
    ### Notes
    
    1. We chose to use the "missing-class" `missing` value as the `NullSubstitutionValue` for the time being for `ListArray`. However, we eventually want to add `arrow.array.NullArray`, and will most likely want to use the "missing-class" `missing` value to represent `NullArray` values in MATLAB. So, this could cause some ambiguity in the future. We have been thinking about whether we should consider introducing some sort of special "sentinel value" to represent null values when converting to MATLAB `cell` arrays. Perhaps, something like `arrow.Null`, or something to that effect, in order to avoid this ambiguity. If we think it makes sense to do that, we may want to retroactively change the `NullSubstitutionValue` to be `arrow.Null` and break compatibility. Since we are still in pre-`0.1`, we don't think the impact of such a behavior change would be very large.
    2. Implementing `ListArray` is fairly involved. So, in the spirit of incremental delivery, we chose not to include an implementation of `arrow.array.ListArray.fromMATLAB` in this initial pull request. We plan on following up with some more changes to `arrow.array.ListArray`. See #38353, #38354, and #38361.
    3. Thank you @ sgilmore10 for your help with this pull request!
    
    ### Future Directions
    
    1. #38353
    2. #38354
    3. #38361
    4. Consider adding a null sentinel value like `arrow.Null` for conversion to MATLAB `cell` arrays.
    * Closes: #37815 
    
    Lead-authored-by: Kevin Gurney <kgurney@mathworks.com>
    Co-authored-by: Sarah Gilmore <sgilmore@mathworks.com>
    Signed-off-by: Kevin Gurney <kgurney@mathworks.com>

--- matlab/src/cpp/arrow/matlab/proxy/factory.cc
+++ matlab/src/cpp/arrow/matlab/proxy/factory.cc
@@ -22,6 +22,7 @@
 #include "arrow/matlab/array/proxy/time32_array.h"
 #include "arrow/matlab/array/proxy/time64_array.h"
 #include "arrow/matlab/array/proxy/struct_array.h"
+#include "arrow/matlab/array/proxy/list_array.h"
 #include "arrow/matlab/array/proxy/chunked_array.h"
 #include "arrow/matlab/tabular/proxy/record_batch.h"
 #include "arrow/matlab/tabular/proxy/table.h"
@@ -61,6 +62,7 @@ libmexclass::proxy::MakeResult Factory::make_proxy(const ClassName& class_name, 
     REGISTER_PROXY(arrow.array.proxy.BooleanArray  , arrow::matlab::array::proxy::BooleanArray);
     REGISTER_PROXY(arrow.array.proxy.StringArray   , arrow::matlab::array::proxy::StringArray);
     REGISTER_PROXY(arrow.array.proxy.StructArray   , arrow::matlab::array::proxy::StructArray);
+    REGISTER_PROXY(arrow.array.proxy.ListArray     , arrow::matlab::array::proxy::ListArray);
     REGISTER_PROXY(arrow.array.proxy.TimestampArray, arrow::matlab::array::proxy::NumericArray<arrow::TimestampType>);
     REGISTER_PROXY(arrow.array.proxy.Time32Array   , arrow::matlab::array::proxy::NumericArray<arrow::Time32Type>);
     REGISTER_PROXY(arrow.array.proxy.Time64Array   , arrow::matlab::array::proxy::NumericArray<arrow::Time64Type>);
