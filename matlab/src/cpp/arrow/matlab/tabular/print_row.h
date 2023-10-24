7b92cfd81dba060d282d372cc5d892f4a2b59947
Sarah Gilmore <sgilmore@mathworks.com>
2023-10-24 16:25:17 -0400

    Update print_row to slice arrays to use the same formatting as array display

--- matlab/src/cpp/arrow/matlab/tabular/print_row.h
+++ matlab/src/cpp/arrow/matlab/tabular/print_row.h
@@ -18,46 +18,49 @@
 #include "arrow/result.h"
 #include "arrow/type_fwd.h"
 #include "arrow/scalar.h"
+#include "arrow/type_traits.h"
+#include "arrow/util/formatting.h"
+#include "arrow/pretty_print.h"
 
 #include <sstream>
 
 namespace arrow::matlab::tabular {
 
-    namespace {
-        arrow::Result<std::string> scalar_to_string(const std::shared_ptr<arrow::Scalar>& scalar) {
-            using ID = arrow::Type::type;
-
-            const auto type_id = scalar->type->id();
-            if (arrow::is_primitive(type_id) || arrow::is_string(type_id)) {
-                return scalar->ToString();
-            } else if (type_id == ID::STRUCT) {
-                return "<Struct>";
-            } else if (type_id == ID::LIST) {
-                return "<List>";
-            } else {
-                return arrow::Status::NotImplemented("Unsupported DataType: " + scalar->type->ToString());
-            }
-        }
-    }  // namespace
+    arrow::PrettyPrintOptions make_pretty_print_options() {
+        auto opts = arrow::PrettyPrintOptions::Defaults();
+        opts.skip_new_lines = true;
+        opts.array_delimiters.open = "";
+        opts.array_delimiters.close = "";
+        opts.chunked_array_delimiters.open = "";
+        opts.chunked_array_delimiters.close = "";
+        return opts;
+    }
 
     template <typename TabularType>
     arrow::Result<std::string> print_row(const std::shared_ptr<TabularType>& tabular_object, const int64_t matlab_row_index) {
         std::stringstream ss;
         const int64_t row_index = matlab_row_index - 1;
-        
         if (row_index >= tabular_object->num_rows() || row_index < 0) {
             ss << "Invalid Row Index: " << matlab_row_index;
             return arrow::Status::Invalid(ss.str());
         }
 
+        const auto opts = make_pretty_print_options();
         const auto num_columns = tabular_object->num_columns();
         const auto& columns = tabular_object->columns();
 
         for (int32_t i = 0; i < num_columns; ++i) {
             const auto& column = columns[i];
-            ARROW_ASSIGN_OR_RAISE(auto scalar, column->GetScalar(row_index));
-            ARROW_ASSIGN_OR_RAISE(auto str, scalar_to_string(scalar));
-            ss << str;
+            const auto type_id = column->type()->id();
+            if (arrow::is_primitive(type_id) || arrow::is_string(type_id)) {
+                auto slice = column->Slice(row_index, 1);
+                ARROW_RETURN_NOT_OK(arrow::PrettyPrint(*slice, opts, &ss));
+            } else if (type_id == arrow::Type::type::STRUCT) {
+                ss << "<Struct>";
+            } else if (type_id == arrow::Type::type::LIST) {
+                ss << "<List>";
+            }
+
             if (i + 1 < num_columns) {
                 ss << " | ";
             }
