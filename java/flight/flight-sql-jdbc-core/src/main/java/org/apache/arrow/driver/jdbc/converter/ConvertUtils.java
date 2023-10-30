cc3ea42ba8fea43842bb3de5de7d89782dd30ab5
Diego Fernandez <aiguo.fernandez@gmail.com>
2023-10-30 17:34:32 +0000

    Simplify more

--- java/flight/flight-sql-jdbc-core/src/main/java/org/apache/arrow/driver/jdbc/converter/ConvertUtils.java
+++ java/flight/flight-sql-jdbc-core/src/main/java/org/apache/arrow/driver/jdbc/converter/ConvertUtils.java
@@ -22,6 +22,21 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.arrow.driver.jdbc.converter.impl.BinaryAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.BoolAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.DateAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.DecimalAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.DurationAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.FixedSizeBinaryAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.FloatingPointAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.IntAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.IntervalAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.LargeBinaryAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.LargeUtf8AvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.NullAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.TimeAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.TimestampAvaticaParameterConverter;
+import org.apache.arrow.driver.jdbc.converter.impl.Utf8AvaticaParameterConverter;
 import org.apache.arrow.driver.jdbc.utils.SqlTypes;
 import org.apache.arrow.flight.sql.FlightSqlColumnMetadata;
 import org.apache.arrow.vector.types.pojo.ArrowType;
@@ -123,6 +138,120 @@ public final class ConvertUtils {
    * @return list of {@link AvaticaParameter}.
    */
   public static List<AvaticaParameter> convertArrowFieldsToAvaticaParameters(final List<Field> fields) {
-    return fields.stream().map(ArrowFieldToAvaticaParameter::convert).collect(Collectors.toList());
+    return fields.stream().map( field -> field.getType().accept(new ConverterVisitor(field))).collect(Collectors.toList());
   }
+
+  private static class ConverterVisitor implements ArrowType.ArrowTypeVisitor<AvaticaParameter> {
+    private final Field field;
+
+    private ConverterVisitor(Field field) {
+      this.field = field;
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Null type) {
+      return new NullAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Struct type) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.List type) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.LargeList type) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.FixedSizeList type) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Union type) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Map type) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Int type) {
+      return new IntAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.FloatingPoint type) {
+      return new FloatingPointAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Utf8 type) {
+      return new Utf8AvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.LargeUtf8 type) {
+      return new LargeUtf8AvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Binary type) {
+      return new BinaryAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.LargeBinary type) {
+      return new LargeBinaryAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.FixedSizeBinary type) {
+      return new FixedSizeBinaryAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Bool type) {
+      return new BoolAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Decimal type) {
+      return new DecimalAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Date type) {
+      return new DateAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Time type) {
+      return new TimeAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Timestamp type) {
+      return new TimestampAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Interval type) {
+      return new IntervalAvaticaParameterConverter(type).createParameter(field);
+    }
+
+    @Override
+    public AvaticaParameter visit(ArrowType.Duration type) {
+      return new DurationAvaticaParameterConverter(type).createParameter(field);
+    }
+  }
+
 }
