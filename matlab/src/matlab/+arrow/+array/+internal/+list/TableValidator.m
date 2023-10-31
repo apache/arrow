eeb6f146aaea5229129ade866162939b391a6715
Sarah Gilmore <sgilmore@mathworks.com>
2023-10-31 16:36:05 -0400

    Implement TableValidator's validateElement method

--- matlab/src/matlab/+arrow/+array/+internal/+list/TableValidator.m
+++ matlab/src/matlab/+arrow/+array/+internal/+list/TableValidator.m
@@ -13,7 +13,7 @@
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
 
-classdef TableValidator < arrow.array.internal.list.Validator
+classdef TableValidator < arrow.array.internal.list.ClassTypeValidator
 
     properties (GetAccess=public, SetAccess=private)
         VariableNames string = string.empty(1, 0)
@@ -33,6 +33,7 @@ classdef TableValidator < arrow.array.internal.list.Validator
                     "Require tables to have at least one variable.");
             end
 
+            obj@arrow.array.internal.list.ClassTypeValidator(table);
             obj.VariableNames = string(T.Properties.VariableNames);
             validators = cell([1 numVars]);
             for ii = 1:numVars
@@ -43,7 +44,36 @@ classdef TableValidator < arrow.array.internal.list.Validator
         end
 
         function validateElement(obj, element)
-            error("Not Implemented");
+            % Verify element is a table
+            validateElement@arrow.array.internal.list.ClassTypeValidator(obj, element);
+            
+            % Validate element has the expected number of variables
+            numVars = numel(obj.VariableNames);
+            if width(element) ~= numVars
+                id = "arrow:array:list:NumVariablesMismatch";
+                fmt = "Expected all tables in the cell array to have %d variables";
+                msg = compose(fmt, numVars);
+                error(id, msg);
+            end
+
+            % Validate element has the expected variable names
+            if ~all(obj.VariableNames == string(element.Properties.VariableNames))
+                id = "arrow:array:list:VariableNamesMismatch";
+                msg = "Expected table names to match";
+                error(id, msg);
+            end
+
+            for ii=1:numVars
+                var = element.(ii);
+
+                if ~istable(var) || ~iscolumn(var)
+                    id = "arrow:array:list:NonTabularVariablesMustBeColumnar";
+                    msg = "Table variables must be columnar";
+                    error(id, msg);
+                end
+
+                obj.VariableValidators(ii).validateElement(var);
+            end
         end
 
         function length = getElementLength(~, element)
