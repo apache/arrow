ce01208d11a8f258e8f8c5fb95ae7e88b56c9c60
Sarah Gilmore <sgilmore@mathworks.com>
2023-11-01 16:55:53 -0400

    Set the MatlabConstructor of ListTraits to @arrow.array.ListArray.fromMATLAB

--- matlab/src/matlab/+arrow/+type/+traits/ListTraits.m
+++ matlab/src/matlab/+arrow/+type/+traits/ListTraits.m
@@ -23,9 +23,7 @@ classdef ListTraits < arrow.type.traits.TypeTraits
         TypeConstructor = @arrow.type.ListType
         TypeClassName = "arrow.type.ListType"
         TypeProxyClassName = "arrow.type.proxy.ListType"
-        % The cell function works differently than other
-        % "type construction functions" in MATLAB.
-        MatlabConstructor = missing
+        MatlabConstructor = @arrow.array.ListArray.fromMATLAB
         MatlabClassName = "cell"
     end
 
