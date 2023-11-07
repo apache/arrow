5540c70235d58e80887f2861dd3e285905b12aa7
Thomas Newton <thomas.w.newton@gmail.com>
2023-11-07 12:47:42 +0000

    Auto-formatting

--- cpp/src/arrow/filesystem/azurefs_test.cc
+++ cpp/src/arrow/filesystem/azurefs_test.cc
@@ -485,7 +485,8 @@ std::shared_ptr<const KeyValueMetadata> NormalizerKeyValueMetadata(
         value = "2023-10-31T08:15:20Z";
       }
     } else if (key == "ETag") {
-      if (arrow::internal::StartsWith(value, "\"") && arrow::internal::EndsWith(value, "\"")) {
+      if (arrow::internal::StartsWith(value, "\"") &&
+          arrow::internal::EndsWith(value, "\"")) {
         // Valid value
         value = "\"ETagValue\"";
       }
