647f2002e280411ba170f2823ddee378f55132a6
Sarah Gilmore <sgilmore@mathworks.com>
2023-11-08 16:59:42 -0500

    Configure ToolboxOptions for packaging

--- matlab/tools/packageMatlabInterface.m
+++ matlab/tools/packageMatlabInterface.m
@@ -1,6 +1,24 @@
+toolboxFolder = string(getenv("ARROW_MATLAB_TOOLBOX_FOLDER"));
+outputFolder = string(getenv("ARROW_MATLAB_TOOLBOX_OUTPUT_FOLDER"));
+toolboxVersion = string(getenv("ARROW_MATLAB_TOOLBOX_VERSION"));
 
-toolboxFolder = getenv("ARROW_MATLAB_TOOLBOX_FOLDER");
 identifier = "ARROW-MATLAB-TOOLBOX";
+
 opts = matlab.addons.toolbox.ToolboxOptions(toolboxFolder, identifier);
 
+opts.ToolboxName = "MATLAB Interface to Arrow";
+opts.ToolboxVersion = toolboxVersion;
+opts.ToolboxMatlabPath = toolboxFolder;
 
+% Set the SupportedPlatforms
+opts.SupportedPlatforms.Win64 = true;
+opts.SupportedPlatforms.Maci64 = true;
+opts.SupportedPlatforms.Glnxa64 = true;
+opts.SupportedPlatforms.MatlabOnline = true;
+
+% TODO: Determine what to set the min/max release to
+opts.MinimumMatlabRelease = "R2023a";
+opts.MaximumMatlabRelease = "";
+
+opts.OutputFile = fullfile(outputFolder, "arrow_matlab.mltbx");
+matlab.addons.toolbox.packageToolbox(opts);
