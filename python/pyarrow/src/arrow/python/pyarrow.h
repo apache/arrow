dc706b38d2d58fa6b004a81457e253bbf8ce154a
Jeremy Aguilon <jeraguilon@gmail.com>
2023-11-08 17:43:46 -0500

    Change unwrap macro to not assume a shared ptr

--- python/pyarrow/src/arrow/python/pyarrow.h
+++ python/pyarrow/src/arrow/python/pyarrow.h
@@ -45,31 +45,30 @@ namespace py {
 // Returns 0 on success, -1 on error.
 ARROW_PYTHON_EXPORT int import_pyarrow();
 
-#define DECLARE_WRAP_FUNCTIONS(FUNC_SUFFIX, TYPE_NAME)                         \
-  ARROW_PYTHON_EXPORT bool is_##FUNC_SUFFIX(PyObject*);                        \
-  ARROW_PYTHON_EXPORT Result<std::shared_ptr<TYPE_NAME>> unwrap_##FUNC_SUFFIX( \
-      PyObject*);                                                              \
-  ARROW_PYTHON_EXPORT PyObject* wrap_##FUNC_SUFFIX(const std::shared_ptr<TYPE_NAME>&);
+#define DECLARE_WRAP_FUNCTIONS(FUNC_SUFFIX, TYPE_NAME)                   \
+  ARROW_PYTHON_EXPORT bool is_##FUNC_SUFFIX(PyObject*);                  \
+  ARROW_PYTHON_EXPORT Result<TYPE_NAME> unwrap_##FUNC_SUFFIX(PyObject*); \
+  ARROW_PYTHON_EXPORT PyObject* wrap_##FUNC_SUFFIX(const TYPE_NAME&);
 
-DECLARE_WRAP_FUNCTIONS(buffer, Buffer)
+DECLARE_WRAP_FUNCTIONS(buffer, std::shared_ptr<Buffer>)
 
-DECLARE_WRAP_FUNCTIONS(data_type, DataType)
-DECLARE_WRAP_FUNCTIONS(field, Field)
-DECLARE_WRAP_FUNCTIONS(schema, Schema)
+DECLARE_WRAP_FUNCTIONS(data_type, std::shared_ptr<DataType>)
+DECLARE_WRAP_FUNCTIONS(field, std::shared_ptr<Field>)
+DECLARE_WRAP_FUNCTIONS(schema, std::shared_ptr<Schema>)
 
-DECLARE_WRAP_FUNCTIONS(scalar, Scalar)
+DECLARE_WRAP_FUNCTIONS(scalar, std::shared_ptr<Scalar>)
 
-DECLARE_WRAP_FUNCTIONS(array, Array)
-DECLARE_WRAP_FUNCTIONS(chunked_array, ChunkedArray)
+DECLARE_WRAP_FUNCTIONS(array, std::shared_ptr<Array>)
+DECLARE_WRAP_FUNCTIONS(chunked_array, std::shared_ptr<ChunkedArray>)
 
-DECLARE_WRAP_FUNCTIONS(sparse_coo_tensor, SparseCOOTensor)
-DECLARE_WRAP_FUNCTIONS(sparse_csc_matrix, SparseCSCMatrix)
-DECLARE_WRAP_FUNCTIONS(sparse_csf_tensor, SparseCSFTensor)
-DECLARE_WRAP_FUNCTIONS(sparse_csr_matrix, SparseCSRMatrix)
-DECLARE_WRAP_FUNCTIONS(tensor, Tensor)
+DECLARE_WRAP_FUNCTIONS(sparse_coo_tensor, std::shared_ptr<SparseCOOTensor>)
+DECLARE_WRAP_FUNCTIONS(sparse_csc_matrix, std::shared_ptr<SparseCSCMatrix>)
+DECLARE_WRAP_FUNCTIONS(sparse_csf_tensor, std::shared_ptr<SparseCSFTensor>)
+DECLARE_WRAP_FUNCTIONS(sparse_csr_matrix, std::shared_ptr<SparseCSRMatrix>)
+DECLARE_WRAP_FUNCTIONS(tensor, std::shared_ptr<Tensor>)
 
-DECLARE_WRAP_FUNCTIONS(batch, RecordBatch)
-DECLARE_WRAP_FUNCTIONS(table, Table)
+DECLARE_WRAP_FUNCTIONS(batch, std::shared_ptr<RecordBatch>)
+DECLARE_WRAP_FUNCTIONS(table, std::shared_ptr<Table>)
 
 #undef DECLARE_WRAP_FUNCTIONS
 
