ee58ae4da94d42c736b5ae416876f3517e71fdee
Sarah Gilmore <sgilmore@mathworks.com>
2023-11-09 10:09:13 -0500

    Update test case comments

--- matlab/test/arrow/array/tSlice.m
+++ matlab/test/arrow/array/tSlice.m
@@ -19,6 +19,8 @@ classdef tSlice < matlab.unittest.TestCase
     
     methods(Test)
         function BooleanArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a Boolean Array.
             boolArray = arrow.array([true true false true true false], Valid=[1 2 3 6]);
             slice = boolArray.slice(int64(2), int64(4));
             testCase.verifyEqual(slice.NumElements, int64(4));
@@ -27,6 +29,8 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function NumericArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a Numeric Array.
             float64Array = arrow.array(1:10, Valid=[2 3 4 5 8 10]);
             slice = float64Array.slice(int64(4), int64(5));
             testCase.verifyEqual(slice.NumElements, int64(5));
@@ -35,6 +39,8 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function DateArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a Date Array.
             import arrow.array.Date32Array
             dates = datetime(2023, 11, 8:16);
             date32Array = Date32Array.fromMATLAB(dates, Valid=[4 5 6 9]);
@@ -46,6 +52,8 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function TimeArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a Time Array.
             times = seconds(10:20);
             time64Array = arrow.array(times, Valid=[2 4 6 7 8 10]);
             slice = time64Array.slice(int64(5), int64(6));
@@ -56,6 +64,8 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function TimestampArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a TimestampArray.
             dates = datetime(2023, 11, 8:16);
             date32Array = arrow.array(dates, Valid=[1 2 4 5 6 8]);
             slice = date32Array.slice(int64(5), int64(3));
@@ -66,6 +76,8 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function StringArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a StringArray.
             stringArray = arrow.array(["a" "b" "c" "d" "e" "f" "g"], Valid=[1 3 4 5 6]);
             slice = stringArray.slice(int64(2), int64(3));
             testCase.verifyEqual(slice.NumElements, int64(3));
@@ -75,6 +87,8 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function ListArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a ListArray.
             cellArray = {missing, [1, 2, 3], missing, [4, NaN], [6, 7, 8], missing};
             listArray = arrow.array(cellArray);
             slice = listArray.slice(int64(2), int64(4));
@@ -85,6 +99,8 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function StructArray(testCase)
+            % Verify the slice method returns the expected array when
+            % called on a StructArray.
             numbers = [NaN; 2; 3; 4; 5; 6; 7; NaN; 9; 10];
             text = ["a"; missing; "c"; "d"; "e"; missing; "g"; "h"; "i"; "j"];
             t = table(numbers, text);
@@ -99,6 +115,9 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function NonPositiveOffsetError(testCase)
+            % Verify the slice method throws an error whose identifier is
+            % "arrow:array:slice:NonPositiveOffset" if given a non-positive
+            % value as the offset.
             array = arrow.array(1:10);
             fcn = @() array.slice(int64(0), int64(2));
             testCase.verifyError(fcn, "arrow:array:slice:NonPositiveOffset");
@@ -107,6 +126,9 @@ classdef tSlice < matlab.unittest.TestCase
         end
 
         function NegativeLengthError(testCase)
+            % Verify the slice method throws an error whose identifier is
+            % "arrow:array:slice:NegativeLength" if given a negative value
+            % as the length.
             array = arrow.array(1:10);
             fcn = @() array.slice(int64(1), int64(-1));
             testCase.verifyError(fcn, "arrow:array:slice:NegativeLength");
