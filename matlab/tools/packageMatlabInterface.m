35598744b784da8aa014bedd83446beccb47b51e
Kevin Gurney <kgurney@mathworks.com>
2023-11-09 16:33:39 -0500

    1. Add missing license header to packageMatlabInterface. 2. Extract Major.Minor.Patch version from toolbox version string.

--- matlab/tools/packageMatlabInterface.m
+++ matlab/tools/packageMatlabInterface.m
@@ -1,6 +1,33 @@
+% Licensed to the Apache Software Foundation (ASF) under one
+% or more contributor license agreements.  See the NOTICE file
+% distributed with this work for additional information
+% regarding copyright ownership.  The ASF licenses this file
+% to you under the Apache License, Version 2.0 (the
+% "License"); you may not use this file except in compliance
+% with the License.  You may obtain a copy of the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing,
+% software distributed under the License is distributed on an
+% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+% KIND, either express or implied.  See the License for the
+% specific language governing permissions and limitations
+% under the License.
+
 toolboxFolder = string(getenv("ARROW_MATLAB_TOOLBOX_FOLDER"));
 outputFolder = string(getenv("ARROW_MATLAB_TOOLBOX_OUTPUT_FOLDER"));
-toolboxVersion = string(getenv("ARROW_MATLAB_TOOLBOX_VERSION"));
+toolboxVersionRaw = string(getenv("ARROW_MATLAB_TOOLBOX_VERSION"));
+
+% Note: This string processing heuristic may not be robust to future
+% changes in the Arrow versioning scheme.
+dotIdx = strfind(toolboxVersionRaw, ".");
+numDots = numel(dotIdx);
+if numDots >= 3
+    toolboxVersion = extractBefore(toolboxVersionRaw, dotIdx(3));
+else
+    toolboxVersion = toolboxVersionRaw;
+end
 
 identifier = "ARROW-MATLAB-TOOLBOX";
 
