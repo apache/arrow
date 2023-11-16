1c3ac8f7ca9cedc1386a6f56e789f1e3838cf6bd
Jonathan Keane <jkeane@gmail.com>
2023-11-16 17:04:26 -0600

    Wrap rosetta detection in tryCatch

--- r/R/install-arrow.R
+++ r/R/install-arrow.R
@@ -271,6 +271,13 @@ wslify_path <- function(path) {
 
 on_rosetta <- function() {
   # make sure to suppress warnings and ignore the stderr so that this is silent where proc_translated doesn't exist
+  sysctl_out <- tryCatch(
+    suppressWarnings(system("sysctl -n sysctl.proc_translated", intern = TRUE, ignore.stderr = TRUE)),
+    error = function(e) {
+      # If this has errored, we assume that this is not on rosetta
+      return("0")
+    }
+  )
   identical(tolower(Sys.info()[["sysname"]]), "darwin") &&
-    identical(suppressWarnings(system("sysctl -n sysctl.proc_translated", intern = TRUE, ignore.stderr = TRUE)), "1")
+    identical(sysctl_out, "1")
 }
