082298d8819e6f58a5eae08c83fd9518bd4a5c13
Christian Holm Christensen <Christian.Holm.Christensen@cern.ch>
2023-11-17 16:49:07 +0100

    My bad - used `and` instead of `&&`

--- cpp/src/arrow/compute/kernel.h
+++ cpp/src/arrow/compute/kernel.h
@@ -39,7 +39,7 @@
 #include "arrow/util/visibility.h"
 // MacOSX defines PREALLOCATE as a preprocessor define in the header sys/vnode.h
 // No other BSD seems to do so.  The name is used as an identifier in MemAllocation enum
-#if defined(__APPLE__) and defined(PREALLOCATE)
+#if defined(__APPLE__) && defined(PREALLOCATE)
 # undef PREALLOCATE 
 #endif  
 
