05f6f38ba67117266b1288c26f0125821b019734
Felipe Oliveira Carvalho <felipekde@gmail.com>
2023-11-17 00:26:17 -0300

    fixup! list_util.h: Add RangeOfValuesUsed() function

--- cpp/src/arrow/compute/kernels/scalar_cast_nested.cc
+++ cpp/src/arrow/compute/kernels/scalar_cast_nested.cc
@@ -29,7 +29,6 @@
 #include "arrow/compute/kernels/scalar_cast_internal.h"
 #include "arrow/util/bitmap_ops.h"
 #include "arrow/util/int_util.h"
-#include "arrow/util/list_util.h"
 
 namespace arrow {
 
