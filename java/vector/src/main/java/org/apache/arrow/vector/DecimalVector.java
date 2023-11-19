37dd3bd1791a6c8b66059ba9adcfe9ede15afc27
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-19 04:00:33 -0500

    spelling: than
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- java/vector/src/main/java/org/apache/arrow/vector/DecimalVector.java
+++ java/vector/src/main/java/org/apache/arrow/vector/DecimalVector.java
@@ -211,7 +211,7 @@ public final class DecimalVector extends BaseFixedWidthVector {
    * ArrowBuf of decimal vector.
    *
    * <p>This method takes care of adding the necessary padding if the length
-   * of byte array is less then 16 (length of decimal type).
+   * of byte array is less than 16 (length of decimal type).
    *
    * @param index position of element
    * @param value array of bytes containing decimal in big endian byte order.
