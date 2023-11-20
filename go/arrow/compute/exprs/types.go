97dc3c44d95aa675d5524df0c135dfc5d4209f52
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-20 15:07:46 -0500

    spelling: arithmetic
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- go/arrow/compute/exprs/types.go
+++ go/arrow/compute/exprs/types.go
@@ -35,7 +35,7 @@ const (
 	// URI for official Arrow Substrait Extension Types
 	ArrowExtTypesUri          = "https://github.com/apache/arrow/blob/main/format/substrait/extension_types.yaml"
 	SubstraitDefaultURIPrefix = extensions.SubstraitDefaultURIPrefix
-	// URI for official Substrait Arithemetic funcs extensions
+	// URI for official Substrait Arithmetic funcs extensions
 	SubstraitArithmeticFuncsURI = SubstraitDefaultURIPrefix + "functions_arithmetic.yaml"
 	// URI for official Substrait Comparison funcs extensions
 	SubstraitComparisonFuncsURI = SubstraitDefaultURIPrefix + "functions_comparison.yaml"
