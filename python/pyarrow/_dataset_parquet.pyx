6ffe48422efb576f45161d358779da98486e728c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-20 21:32:28 -0500

    spelling: coalescing
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- python/pyarrow/_dataset_parquet.pyx
+++ python/pyarrow/_dataset_parquet.pyx
@@ -688,7 +688,7 @@ cdef class ParquetFragmentScanOptions(FragmentScanOptions):
     pre_buffer : bool, default True
         If enabled, pre-buffer the raw Parquet data instead of issuing one
         read per column chunk. This can improve performance on high-latency
-        filesystems (e.g. S3, GCS) by coalesing and issuing file reads in
+        filesystems (e.g. S3, GCS) by coalescing and issuing file reads in
         parallel using a background I/O thread pool.
         Set to False if you want to prioritize minimal memory usage
         over maximum speed.
