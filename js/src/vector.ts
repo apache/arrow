b310ea9435f69ddf2f037b42197b12fe339f50df
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-20 23:40:56 -0500

    spelling: therefore
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- js/src/vector.ts
+++ js/src/vector.ts
@@ -303,7 +303,7 @@ export class Vector<T extends DataType = any> {
      *
      * Memoization is very useful when decoding a value is expensive such as
      * Uft8. The memoization creates a cache of the size of the Vector and
-     * therfore increases memory usage.
+     * therefore increases memory usage.
      *
      * @returns A new vector that memoizes calls to {@link get}.
      */
