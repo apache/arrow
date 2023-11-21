0016cc6a31d83db6c00514fcadb6e6ff92ef6e8c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-20 21:58:50 -0500

    spelling: enforced
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- go/arrow/array/union.go
+++ go/arrow/array/union.go
@@ -980,7 +980,7 @@ func (b *SparseUnionBuilder) AppendEmptyValues(n int) {
 //
 // After appending to the corresponding child builder, all other child
 // builders should have a null or empty value appended to them (although
-// this is not enfoced and any value is theoretically allowed and will be
+// this is not enforced and any value is theoretically allowed and will be
 // ignored).
 func (b *SparseUnionBuilder) Append(nextType arrow.UnionTypeCode) {
 	b.typesBuilder.AppendValue(nextType)
