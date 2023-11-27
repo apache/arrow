c6ee1bb3f654a9bd00eac6a114d8ac35b7ce4aae
AlenkaF <frim.alenka@gmail.com>
2023-11-27 18:45:30 +0100

    Skip the tests for real

--- python/pyarrow/tests/test_array.py
+++ python/pyarrow/tests/test_array.py
@@ -3570,7 +3570,7 @@ def PyCapsule_IsValid(capsule, name):
     ]
 )
 def test_dlpack(value_type, np_type):
-    if Version(np.__version__) < Version("1.22.0"):
+    if Version(np.__version__) < Version("1.24.0"):
         pytest.skip("No dlpack support in numpy versions older than 1.22.0, "
                     "strict keyward in assert_array_equal added in numpy version "
                     "1.24.0")
@@ -3598,7 +3598,7 @@ def test_dlpack(value_type, np_type):
 
 
 def test_dlpack_float_16():
-    if Version(np.__version__) < Version("1.22.0"):
+    if Version(np.__version__) < Version("1.24.0"):
         pytest.skip("No dlpack support in numpy versions older than 1.22.0, "
                     "strict keyward in assert_array_equal added in numpy version "
                     "1.24.0")
