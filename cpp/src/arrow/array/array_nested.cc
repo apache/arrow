5f365adfe2bdfe8e838967fe65f0974fb5871f53
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-27 08:39:07 -0500

    spelling: through
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- cpp/src/arrow/array/array_nested.cc
+++ cpp/src/arrow/array/array_nested.cc
@@ -393,7 +393,7 @@ Result<std::shared_ptr<ArrayData>> ListViewFromListImpl(
   const auto* offsets = list_data->template GetValues<offset_type>(1, 0);
   auto* sizes = sizes_buffer->mutable_data_as<offset_type>();
   // Zero the initial padding area to avoid leaking any data when buffers are
-  // sent over IPC or throught the C Data interface.
+  // sent over IPC or through the C Data interface.
   memset(sizes, 0, list_data->offset * sizeof(offset_type));
   for (int64_t i = list_data->offset; i < buffer_length; i++) {
     sizes[i] = offsets[i + 1] - offsets[i];
