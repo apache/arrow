01c594b2a2a2b10a0f61e2238d8c317f2efaaa5d
James Duong <james.duong@improving.com>
2023-11-28 12:41:04 -0800

    Export packages for arrow-vector module
    Export public package for arrow-vector module.
    Allow jackson to use arrow-vector pojo classes
    reflectively for serialization.

--- java/vector/src/main/java/module-info.java
+++ java/vector/src/main/java/module-info.java
@@ -16,6 +16,26 @@
  */
 
 module org.apache.arrow.vector {
+  exports org.apache.arrow.vector;
+  exports org.apache.arrow.vector.compare;
+  exports org.apache.arrow.vector.compare.util;
+  exports org.apache.arrow.vector.complex;
+  exports org.apache.arrow.vector.complex.impl;
+  exports org.apache.arrow.vector.complex.reader;
+  exports org.apache.arrow.vector.complex.writer;
+  exports org.apache.arrow.vector.compression;
+  exports org.apache.arrow.vector.dictionary;
+  exports org.apache.arrow.vector.holders;
+  exports org.apache.arrow.vector.ipc;
+  exports org.apache.arrow.vector.ipc.message;
+  exports org.apache.arrow.vector.table;
+  exports org.apache.arrow.vector.types;
+  exports org.apache.arrow.vector.types.pojo;
+  exports org.apache.arrow.vector.util;
+  exports org.apache.arrow.vector.validate;
+
+  opens org.apache.arrow.vector.types.pojo to com.fasterxml.jackson.databind;
+
   requires com.fasterxml.jackson.annotation;
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
