1b9fe98be6338d4fae917d271c40261b25118f45
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-01 03:25:59 -0500

    GH-38968: [C++] Fix spelling (dataset) (#38969)
    ### Rationale for this change
    
    ### What changes are included in this PR?
    
    Spelling fixes to cpp/src/arrow/dataset/
    
    ### Are these changes tested?
    
    ### Are there any user-facing changes?
    
    * Closes: #38968
    
    Authored-by: Josh Soref <2119212+jsoref@users.noreply.github.com>
    Signed-off-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- cpp/src/arrow/dataset/scan_node.cc
+++ cpp/src/arrow/dataset/scan_node.cc
@@ -94,7 +94,7 @@ Future<AsyncGenerator<std::shared_ptr<Fragment>>> GetFragments(
 /// fragment on disk actually had a column x, and the value was not 7, then we will prefer
 /// the guarantee in this invalid case.
 ///
-/// Ths next step is to fetch the metadata for the fragment.  For some formats (e.g.
+/// The next step is to fetch the metadata for the fragment.  For some formats (e.g.
 /// CSV) this may be quite simple (get the size of the file).  For other formats (e.g.
 /// parquet) this is more involved and requires reading data.  There is one metadata
 /// io-task per fragment.  The metadata io-task creates an AsyncGenerator<RecordBatch>
@@ -150,7 +150,7 @@ class ScanNode : public acero::ExecNode, public acero::TracedNode {
     }
 
     if (normalized.filter.call() && normalized.filter.IsBound()) {
-      // There is no easy way to make sure a filter was bound agaisnt the same
+      // There is no easy way to make sure a filter was bound against the same
       // function registry as the one in ctx so we just require it to be unbound
       // FIXME - Do we care if it was bound to a different function registry?
       return Status::Invalid("Scan filter must be unbound");
