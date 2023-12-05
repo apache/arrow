c6c7b6494f724c636aeada1f13dd7df8e9936d61
Jacob Wujciak-Jens <jacob@wujciak.de>
2023-12-05 22:39:32 +0100

    MINOR: [R] Increase timeout for binary download (#39080)
    ### Rationale for this change
    
    Download of mac binaries failed on CRAN and would likely fail on my local ancient bandwidth DSL (~300-500kbs) as well so a bit more buffer in the timeour value should help.
    
    ### What changes are included in this PR?
    
    Increase timeout.
    
    ### Are these changes tested?
    
    Not really testable.
    
    ### Are there any user-facing changes?
    
    No.
    
    Authored-by: Jacob Wujciak-Jens <jacob@wujciak.de>
    Signed-off-by: Jacob Wujciak-Jens <jacob@wujciak.de>

--- r/tools/nixlibs.R
+++ r/tools/nixlibs.R
@@ -81,8 +81,9 @@ find_latest_nightly <- function(description_version,
 try_download <- function(from_url, to_file, hush = quietly) {
   # We download some fairly large files, so ensure the timeout is set appropriately.
   # This assumes a static library size of 100 MB (generous) and a download speed
-  # of 1 MB/s (slow).
-  opts <- options(timeout = max(100, getOption("timeout")))
+  # of .3 MB/s (slow). This is to anticipate slower user connections or load on
+  # artifactory servers.
+  opts <- options(timeout = max(300, getOption("timeout")))
   on.exit(options(opts))
 
   status <- try(
