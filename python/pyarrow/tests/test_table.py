2c0ab6716175b50ce39cdf78a02ee2e78c36b52c
Judah Rand <17158624+judahrand@users.noreply.github.com>
2023-12-11 15:40:59 +0000

    Test with `max_chunksize`

--- python/pyarrow/tests/test_table.py
+++ python/pyarrow/tests/test_table.py
@@ -938,10 +938,22 @@ def test_table_to_struct_array():
     )
     result = table.to_struct_array()
     assert result.equals(pa.chunked_array(
-        pa.array(
-            [{"ints": 1}, {"floats": 1.0}],
-            type=pa.struct([("ints", pa.int32()), ("floats", pa.float32())]),
-        ),
+        [[{"ints": 1}, {"floats": 1.0}]],
+        type=pa.struct([("ints", pa.int32()), ("floats", pa.float32())]),
+    ))
+
+
+def test_table_to_struct_array_with_max_chunksize():
+    table = pa.Table.from_arrays(
+        [
+            pa.array([1, None], type=pa.int32()),
+            pa.array([None, 1.0], type=pa.float32()),
+        ], ["ints", "floats"]
+    )
+    result = table.to_struct_array(max_chunksize=1)
+    assert result.equals(pa.chunked_array(
+        [[{"ints": 1}], [{"floats": 1.0}]],
+        type=pa.struct([("ints", pa.int32()), ("floats", pa.float32())]),
     ))
 
 
