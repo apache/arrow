94b9c204268412b2c5a39f5aaebae489d740b8f8
James Duong <james.duong@improving.com>
2023-12-11 12:23:27 -0800

    Use correct JPMS name for SLF4J in arrow-vector

--- java/vector/src/main/java/module-info.java
+++ java/vector/src/main/java/module-info.java
@@ -46,5 +46,5 @@ module org.apache.arrow.vector {
   requires org.apache.arrow.memory.core;
   requires org.apache.commons.codec;
   requires org.eclipse.collections.impl;
-  requires slf4j.api;
+  requires org.slf4j;
 }
