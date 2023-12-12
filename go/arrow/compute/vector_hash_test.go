250597137c471f48594e3c2fe66aa562f0ea4316
Benjamin Kietzman <bengilgit@gmail.com>
2023-12-12 12:18:54 -0500

    unused import

--- go/arrow/compute/vector_hash_test.go
+++ go/arrow/compute/vector_hash_test.go
@@ -26,7 +26,6 @@ import (
 	"github.com/apache/arrow/go/v15/arrow"
 	"github.com/apache/arrow/go/v15/arrow/array"
 	"github.com/apache/arrow/go/v15/arrow/compute"
-	"github.com/apache/arrow/go/v15/arrow/compute/exec"
 	"github.com/apache/arrow/go/v15/arrow/decimal128"
 	"github.com/apache/arrow/go/v15/arrow/decimal256"
 	"github.com/apache/arrow/go/v15/arrow/memory"
