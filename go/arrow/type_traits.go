70058686bafd99a8a96cf08523a4c9a8d34282e2
Benjamin Kietzman <bengilgit@gmail.com>
2023-12-13 12:21:35 -0500

    GetData/GetBytes with empty slices

--- go/arrow/type_traits.go
+++ go/arrow/type_traits.go
@@ -78,27 +78,30 @@ type TemporalType interface {
 		MonthInterval | MonthDayNanoInterval
 }
 
+func getSlice[Out, T interface{}](b []T, l int) []Out {
+	h := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+  return unsafe.Slice((*Out)(unsafe.Pointer(h.Data)), l)
+}
+
 func GetValues[T FixedWidthType](data ArrayData, i int) []T {
 	if data.Buffers()[i] == nil || data.Buffers()[i].Len() == 0 {
 		return nil
 	}
-	ret := unsafe.Slice((*T)(unsafe.Pointer(&data.Buffers()[i].Bytes()[0])), data.Offset()+data.Len())
-	return ret[data.Offset():]
+  return getSlice[T](data.Buffers()[i].Bytes(), data.Offset()+data.Len())[data.Offset():]
 }
 
 func GetOffsets[T int32 | int64](data ArrayData, i int) []T {
-	ret := unsafe.Slice((*T)(unsafe.Pointer(&data.Buffers()[i].Bytes()[0])), data.Offset()+data.Len()+1)
-	return ret[data.Offset():]
+  return getSlice[T](data.Buffers()[i].Bytes(), data.Offset()+data.Len()+1)[data.Offset():]
 }
 
 func GetBytes[T FixedWidthType | ViewHeader](in []T) []byte {
 	var z T
-	return unsafe.Slice((*byte)(unsafe.Pointer(&in[0])), len(in)*int(unsafe.Sizeof(z)))
+  return getSlice[byte](in, len(in)*int(unsafe.Sizeof(z)))
 }
 
 func GetData[T FixedWidthType | ViewHeader](in []byte) []T {
 	var z T
-	return unsafe.Slice((*T)(unsafe.Pointer(&in[0])), len(in)/int(unsafe.Sizeof(z)))
+  return getSlice[T](in, len(in)/int(unsafe.Sizeof(z)))
 }
 
 var typMap = map[reflect.Type]DataType{
