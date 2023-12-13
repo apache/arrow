9a68818ba47505c4880df15d73205c7fc13c2fb5
Matt Topol <zotthewizard@gmail.com>
2023-12-13 17:10:21 -0500

    add go client example

---
+++ http/examples/get/client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/apache/arrow/go/v15/arrow"
+	"github.com/apache/arrow/go/v15/arrow/ipc"
+	"github.com/apache/arrow/go/v15/arrow/memory"
+)
+
+func main() {
+	start := time.Now()
+	resp, err := http.Get("http://localhost:8000")
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("got non-200 status: %d", resp.StatusCode))
+	}
+	defer resp.Body.Close()
+
+	rdr, err := ipc.NewReader(resp.Body, ipc.WithAllocator(memory.DefaultAllocator))
+	if err != nil {
+		panic(err)
+	}
+	defer rdr.Release()
+
+	batches := make([]arrow.Record, 0)
+	defer func() {
+		for _, b := range batches {
+			b.Release()
+		}
+	}()
+
+	for rdr.Next() {
+		rec := rdr.Record()
+		rec.Retain()
+		batches = append(batches, rec)
+	}
+
+	if rdr.Err() != nil {
+		panic(rdr.Err())
+	}
+
+	execTime := time.Since(start)
+
+	fmt.Println(resp.ContentLength, " bytes recieved")
+	fmt.Println(len(batches), " record batches received")
+	fmt.Println(execTime.Seconds(), " seconds elapsed")
+}
