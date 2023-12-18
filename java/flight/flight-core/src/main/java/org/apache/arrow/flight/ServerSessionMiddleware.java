5b587db6b580dfb046ef827cff222bff0fcea113
Paul Nienaber <paul.nienaber@dremio.com>
2023-12-18 12:38:49 -0800

    [Java builds] Add ServerSessionMiddleware.java

---
+++ java/flight/flight-core/src/main/java/org/apache/arrow/flight/ServerSessionMiddleware.java
@@ -0,0 +1,188 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.arrow.flight;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Middleware for handling Flight SQL Sessions including session cookie handling.
+ *
+ * Currently experimental.
+ */
+public class ServerSessionMiddleware implements FlightServerMiddleware {
+  Factory factory;
+  boolean existingSession;
+  private Session session;
+
+  public static final String sessionCookieName = "arrow_flight_session_id";
+
+  /**
+   * Factory for managing and accessing ServerSessionMiddleware.
+   */
+  public static class Factory implements FlightServerMiddleware.Factory<ServerSessionMiddleware> {
+    private final Map<String, Session> sessionStore =
+        new ConcurrentHashMap<>();
+    private final Callable<String> idGenerator;
+
+    /**
+     * Construct a factory for ServerSessionMiddleware.
+     *
+     * Factory manages and accesses persistent sessions based on HTTP cookies.
+     *
+     * @param idGenerator A Callable returning unique session id Strings.
+     */
+    public Factory(Callable<String> idGenerator) {
+      this.idGenerator = idGenerator;
+    }
+
+    private synchronized Session createNewSession() {
+      String id;
+      try {
+        id = idGenerator.call();
+      } catch (Exception ignored) {
+        // Most impls aren't going to throw so making caller handle a nonexistent checked is a bit silly
+        return null;
+      }
+      if (sessionStore.containsKey(id)) {
+        // Collision, should also never happen
+        return null;
+      }
+
+      Session newSession = new Session(id);
+      sessionStore.put(id, newSession);
+
+      return newSession;
+    }
+
+    @Override
+    public ServerSessionMiddleware onCallStarted(CallInfo callInfo, CallHeaders incomingHeaders,
+                                                RequestContext context) {
+      String sessionId = null;
+
+      findIdCookie:
+      for (final String headerValue : incomingHeaders.getAll("cookie")) {
+        for (final String cookie : headerValue.split(" ;")) {
+          final String[] cookiePair = cookie.split("=");
+          if (cookiePair.length != 2) {
+            // Soft failure:  Ignore invalid cookie list field
+            break;
+          }
+
+          if (cookiePair[0] == sessionCookieName && cookiePair[1].length() > 0) {
+            sessionId = cookiePair[1];
+            break findIdCookie;
+          }
+        }
+      }
+
+      if (sessionId == null) {
+        // No session cookie, create middleware instance without session.
+        return new ServerSessionMiddleware(this, incomingHeaders, null);
+      }
+
+      Session session = sessionStore.get(sessionId);
+      // Cookie provided by caller, but invalid
+      if (session == null) {
+        // Can't soft-fail/proceed here, clients will get unexpected behaviour without options they thought were set.
+        throw new CallStatus(
+            FlightStatusCode.NOT_FOUND, null,
+            "Invalid or expired " + sessionCookieName + " cookie.", null)
+          .toRuntimeException();
+      }
+
+      return new ServerSessionMiddleware(this, incomingHeaders, session);
+    }
+  }
+
+  /**
+   * A thread-safe container for named SessionOptionValues.
+   */
+  public static class Session {
+    public final String id;
+    private Map<String, SessionOptionValue> session_data =
+        new ConcurrentHashMap<String, SessionOptionValue>();
+
+    /**
+     * Construct a new Session with the given id.
+     *
+     * @param id The Session's id string, which is used as the session cookie value.
+     */
+    private Session(String id) {
+      this.id = id;
+    }
+
+    /** Get session option by name, or null if it does not exist. */
+    public SessionOptionValue getSessionOption(String name) {
+      return session_data.get(name);
+    }
+
+    /** Set session option by name to given value. */
+    public void setSessionOption(String name, SessionOptionValue value) {
+      session_data.put(name, value);
+    }
+
+    /** Idempotently  remove name from this session. */
+    public void eraseSessionOption(String name) {
+      session_data.remove(name);
+    }
+  }
+
+  private final CallHeaders headers;
+
+  private ServerSessionMiddleware(ServerSessionMiddleware.Factory factory,
+                                  CallHeaders incomingHeaders, Session session) {
+    this.factory = factory;
+    headers = incomingHeaders;
+    this.session = session;
+    existingSession = (session != null);
+  }
+
+  /**
+   * Get the existing or new session value map for this call.
+   *
+   * @return The session option value map, or null in case of an id generation collision.
+   */
+  public synchronized Session getSession() {
+    if (session == null) {
+      session = factory.createNewSession();
+    }
+
+    return session;
+  }
+
+  public CallHeaders getCallHeaders() {
+    return headers;
+  }
+
+  @Override
+  public void onBeforeSendingHeaders(CallHeaders outgoingHeaders) {
+    if (!existingSession && session != null) {
+      outgoingHeaders.insert("set-cookie", sessionCookieName + "=" + session.id);
+    }
+  }
+
+  @Override
+  public void onCallCompleted(CallStatus status) {
+  }
+
+  @Override
+  public void onCallErrored(Throwable err) {
+  }
+}
