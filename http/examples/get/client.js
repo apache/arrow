e8e50bb6699b927106357cb6de29f1e63012db7e
Ian Cook <ianmcook@gmail.com>
2023-12-20 17:16:13 -0500

    Simplify JS client example
    Co-authored-by: Dominik Moritz <domoritz@gmail.com>

--- http/examples/get/client.js
+++ http/examples/get/client.js
@@ -2,28 +2,14 @@ const Arrow = require('apache-arrow');
 
 const url = 'http://localhost:8000';
 
-async function getArrowData(url) {
-  try {
-    const response = await fetch(url);
-    const table = await Arrow.tableFromIPC(response);
-    return table;
-  } catch (error) {
-    console.error('Error:', error.message);
-    throw error;
-  }
-}
-
 async function runExample(url) {
   const startTime = new Date();
-  try {
-    const table = await getArrowData(url);
-    const endTime = new Date();
-    const duration = (endTime - startTime) / 1000;
-    console.log(`${table.batches.length} record batches received`);
-    console.log(`${duration.toFixed(2)} seconds elapsed`);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+  
+  const table = await Arrow.tableFromIPC(fetch(url));
+  
+  const duration = (new Date() - startTime) / 1000;
+  console.log(`${table.batches.length} record batches received`);
+  console.log(`${duration.toFixed(2)} seconds elapsed`);
 }
 
 runExample(url);
