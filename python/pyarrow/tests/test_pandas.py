7080d616d6e888d77211f4783b41ed4afe96b7db
AlenkaF <frim.alenka@gmail.com>
2024-01-08 09:48:11 +0100

    Update version check for test_roundtrip_with_bytes_unicode

--- python/pyarrow/tests/test_pandas.py
+++ python/pyarrow/tests/test_pandas.py
@@ -3095,7 +3095,7 @@ def _fully_loaded_dataframe_example():
 
 @pytest.mark.parametrize('columns', ([b'foo'], ['foo']))
 def test_roundtrip_with_bytes_unicode(columns):
-    if Version("2.0.0") <= Version(pd.__version__) < Version("2.2.0"):
+    if Version("2.0.0") <= Version(pd.__version__) < Version("2.3.0"):
         # TODO: regression in pandas, hopefully fixed in next version
         # https://issues.apache.org/jira/browse/ARROW-18394
         # https://github.com/pandas-dev/pandas/issues/50127
