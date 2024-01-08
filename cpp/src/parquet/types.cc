dea3fe67eae6dc886d4e19fa16b5eda9e2f0231e
mwish <maplewish117@gmail.com>
2024-01-09 02:09:48 +0800

    resolve comment

--- cpp/src/parquet/types.cc
+++ cpp/src/parquet/types.cc
@@ -345,12 +345,18 @@ std::shared_ptr<const LogicalType> LogicalType::FromConvertedType(
     case ConvertedType::DATE:
       return DateLogicalType::Make();
     case ConvertedType::TIME_MILLIS:
+      // ConvertedType::TIME_{*} are deprecated in favor of LogicalType::Time, the
+      // compatibility for ConvertedType::TIME_{*} are listed in
+      // https://github.com/apache/parquet-format/blob/master/LogicalTypes.md#deprecated-time-convertedtype
       return TimeLogicalType::Make(/*is_adjusted_to_utc=*/true,
                                    LogicalType::TimeUnit::MILLIS);
     case ConvertedType::TIME_MICROS:
       return TimeLogicalType::Make(/*is_adjusted_to_utc=*/true,
                                    LogicalType::TimeUnit::MICROS);
     case ConvertedType::TIMESTAMP_MILLIS:
+      // ConvertedType::TIMESTAMP_{*} are deprecated in favor of LogicalType::Timestamp,
+      // the compatibility for ConvertedType::TIMESTAMP_{*} are listed in
+      // https://github.com/apache/parquet-format/blob/master/LogicalTypes.md#deprecated-timestamp-convertedtype
       return TimestampLogicalType::Make(/*is_adjusted_to_utc=*/true,
                                         LogicalType::TimeUnit::MILLIS,
                                         /*is_from_converted_type=*/true,
