5acf77251c84a05bb7c8c669cba7ecdbb6e333a3
Jonathan Keane <jkeane@gmail.com>
2024-01-15 08:20:22 -0600

    testing output of checksums to diagnose the binary build

--- r/tools/nixlibs.R
+++ r/tools/nixlibs.R
@@ -115,16 +115,16 @@ validate_checksum <- function(binary_url, libfile, hush = quietly) {
       system2(
         "shasum",
         args = c("--status", "-a", "512", "-c", checksum_file),
-        stdout = FALSE,
-        stderr = FALSE
+        stdout = TRUE,
+        stderr = TRUE
       ) == 0
     },
     error = function(e) {
       system2(
         "sha512sum",
         args = c("--status", "-c", checksum_file),
-        stdout = FALSE,
-        stderr = FALSE
+        stdout = TRUE,
+        stderr = TRUE
       ) == 0
     }
     )
