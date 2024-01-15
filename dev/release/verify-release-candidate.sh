4efa83de9303fbcbba7d6c1d96850a5408682038
Curt Hagenlocher <curt@hagenlocher.org>
2024-01-14 17:17:00 -0800

    Fix verification script

--- dev/release/verify-release-candidate.sh
+++ dev/release/verify-release-candidate.sh
@@ -880,8 +880,8 @@ test_csharp() {
   if [ "${SOURCE_KIND}" = "local" ]; then
     echo "Skipping sourelink verification on local build"
   else
-    dotnet tool run sourcelink test artifacts/Apache.Arrow/Release/netstandard1.3/Apache.Arrow.pdb
-    dotnet tool run sourcelink test artifacts/Apache.Arrow/Release/netcoreapp3.1/Apache.Arrow.pdb
+    dotnet tool run sourcelink test artifacts/Apache.Arrow/Release/netstandard2.0/Apache.Arrow.pdb
+    dotnet tool run sourcelink test artifacts/Apache.Arrow/Release/net6.0/Apache.Arrow.pdb
   fi
 
   popd
