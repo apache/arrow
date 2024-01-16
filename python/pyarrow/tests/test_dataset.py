98e1a14e700069e29fbf22bea7ecaa7cade9475a
Alenka Frim <AlenkaF@users.noreply.github.com>
2024-01-16 09:20:38 +0100

    Apply suggestions from code review
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- python/pyarrow/tests/test_dataset.py
+++ python/pyarrow/tests/test_dataset.py
@@ -179,13 +179,13 @@ def multisourcefs(request):
     # simply split the dataframe into four chunks to construct a data source
     # from each chunk into its own directory
     n = len(df)
-    df_a, df_b, df_c, df_d = [df[i:i+n//4] for i in range(0, n, n//4)]
+    df_a, df_b, df_c, df_d = [df.iloc[i:i+n//4] for i in range(0, n, n//4)]
 
     # create a directory containing a flat sequence of parquet files without
     # any partitioning involved
     mockfs.create_dir('plain')
     n = len(df_a)
-    for i, chunk in enumerate([df_a[i:i+n//10] for i in range(0, n, n//10)]):
+    for i, chunk in enumerate([df_a.iloc[i:i+n//10] for i in range(0, n, n//10)]):
         path = 'plain/chunk-{}.parquet'.format(i)
         with mockfs.open_output_stream(path) as out:
             pq.write_table(_table_from_pandas(chunk), out)
