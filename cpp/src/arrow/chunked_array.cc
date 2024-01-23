10d44b46bd7d5d4b56fa0be394d1d33412e6a8c0
Alessandro Molina <alessandro@molina.fyi>
2024-01-23 18:03:29 +0100

    Fix detection of out of bounds indices

--- cpp/src/arrow/chunked_array.cc
+++ cpp/src/arrow/chunked_array.cc
@@ -169,7 +169,7 @@ bool ChunkedArray::ApproxEquals(const ChunkedArray& other,
 
 Result<std::shared_ptr<Scalar>> ChunkedArray::GetScalar(int64_t index) const {
   const auto loc = chunk_resolver_.Resolve(index);
-  if (loc.chunk_index >= static_cast<int64_t>(chunks_.size())) {
+  if (loc.chunk_index == -1) {
     return Status::IndexError("index with value of ", index,
                               " is out-of-bounds for chunked array of length ", length_);
   }
