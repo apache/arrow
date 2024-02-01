9874670bb0500e6dbb0a7ed85feb4d6def3dc396
Adam Reeve <adreeve@gmail.com>
2024-02-01 13:06:15 +1300

    Reuse zstd compressor

--- csharp/src/Apache.Arrow.Compression/ZstdCompressionCodec.cs
+++ csharp/src/Apache.Arrow.Compression/ZstdCompressionCodec.cs
@@ -23,19 +23,21 @@ namespace Apache.Arrow.Compression
     internal sealed class ZstdCompressionCodec : ICompressionCodec
     {
         private readonly Decompressor _decompressor;
-        private readonly int _compressionLevel;
+        private readonly Compressor _compressor;
 
         public ZstdCompressionCodec(int? compressionLevel = null)
         {
-            _decompressor = new Decompressor();
-            _compressionLevel = compressionLevel ?? Compressor.DefaultCompressionLevel;
-            if (_compressionLevel < Compressor.MinCompressionLevel ||
-                _compressionLevel > Compressor.MaxCompressionLevel)
+            if (compressionLevel.HasValue &&
+                (compressionLevel.Value < Compressor.MinCompressionLevel ||
+                 compressionLevel.Value > Compressor.MaxCompressionLevel))
             {
                 throw new ArgumentException(
                     $"Zstd compression level must be between {Compressor.MinCompressionLevel} and {Compressor.MaxCompressionLevel}",
                     nameof(compressionLevel));
             }
+
+            _decompressor = new Decompressor();
+            _compressor = new Compressor(compressionLevel ?? Compressor.DefaultCompressionLevel);
         }
 
         public int Decompress(ReadOnlyMemory<byte> source, Memory<byte> destination)
@@ -45,13 +47,15 @@ namespace Apache.Arrow.Compression
 
         public void Compress(ReadOnlyMemory<byte> source, Stream destination)
         {
-            using var compressor = new CompressionStream(destination, level: _compressionLevel, leaveOpen: true);
+            using var compressor = new CompressionStream(
+                destination, _compressor, preserveCompressor: true, leaveOpen: true);
             compressor.Write(source.Span);
         }
 
         public void Dispose()
         {
             _decompressor.Dispose();
+            _compressor.Dispose();
         }
     }
 }
