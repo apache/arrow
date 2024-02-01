83259308a7a8d64416e35e4b0c8151bdec187dfb
Divyansh200102 <divyanshkhatri200102@gmail.com>
2024-02-01 23:16:30 +0530

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -287,9 +287,6 @@ intersphinx_mapping = {
 # a list of builtin themes.
 #
 html_theme = 'pydata_sphinx_theme'
-html_theme_options = {
-    "version": "0.15.2",
-}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
