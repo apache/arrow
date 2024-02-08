e56d3d0e3df674d68662c755ffc97b7ccbacdc49
Dewey Dunnington <dewey@fishandwhistle.net>
2024-02-08 16:54:35 -0400

    add tests

--- python/pyarrow/tests/test_cffi.py
+++ python/pyarrow/tests/test_cffi.py
@@ -601,3 +601,25 @@ def test_roundtrip_batch_reader_capsule():
     assert imported_reader.read_next_batch().equals(batch)
     with pytest.raises(StopIteration):
         imported_reader.read_next_batch()
+
+
+def test_roundtrip_chunked_array_capsule():
+    chunked = pa.chunked_array([pa.array(['a', 'b', 'c'])])
+
+    capsule = chunked.__arrow_c_stream__()
+    assert PyCapsule_IsValid(capsule, b"arrow_array_stream") == 1
+    imported_chunked = pa.ChunkedArray._import_from_c_capsule(capsule)
+    assert imported_chunked.type == chunked.type
+    assert imported_chunked == chunked
+
+
+def test_roundtrip_chunked_array_capsule_requested_schema():
+    chunked = pa.chunked_array([pa.array(['a', 'b', 'c'])])
+    requested_type = pa.binary()
+
+    requested_capsule = requested_type.__arrow_c_schema__()
+    capsule = chunked.__arrow_c_stream__(requested_capsule)
+    assert PyCapsule_IsValid(capsule, b"arrow_array_stream") == 1
+    imported_chunked = pa.ChunkedArray._import_from_c_capsule(capsule)
+    assert imported_chunked.type == requested_type
+    assert imported_chunked == chunked.cast(requested_type)
