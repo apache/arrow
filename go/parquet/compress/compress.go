db2fb3260b2e668bb6cb57bbde23bb6a693badbd
Yan Zhou <zhouyan@me.com>
2024-02-20 23:45:16 +0800

    GH-40113 add doc comments for RegisterCodec

--- go/parquet/compress/compress.go
+++ go/parquet/compress/compress.go
@@ -92,6 +92,22 @@ type Codec interface {
 
 var codecs = map[Compression]Codec{}
 
+// RegisterCodec add new or override existing codec implementation for a given compression algorithm.
+// The intended use case is within the init() section of a package. For example,
+//
+//	    // inside a custom codec package, say czstd
+//
+//		func init() {
+//		    RegisterCodec(compress.Codecs.Zstd, czstdCodec{})
+//		}
+//
+//		type czstdCodec struct{} // implementing Codec interface using CGO based ZSTD wrapper
+//
+// And user of the custom codec can import the above package like below,
+//
+//	    package main
+//
+//		import _ "package/path/to/czstd"
 func RegisterCodec(compression Compression, codec Codec) {
 	codecs[compression] = codec
 }
