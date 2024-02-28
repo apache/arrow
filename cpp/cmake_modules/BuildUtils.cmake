a98893a474fec433877e2026ce31452756666ef7
Jonathan Keane <jkeane@gmail.com>
2024-02-28 07:56:28 -0600

    lint

--- cpp/cmake_modules/BuildUtils.cmake
+++ cpp/cmake_modules/BuildUtils.cmake
@@ -105,15 +105,16 @@ function(arrow_create_merged_static_lib output_target)
     # HINTS is used before system paths and before PATHS, so we use that
     # even though hard coded paths should go in PATHS
     # TODO: use a VALIDATOR when we require cmake >= 3.25
-    find_program(LIBTOOL_MACOS libtool
-                HINTS /usr/bin /Library/Developer/CommandLineTools/usr/bin)
+    find_program(LIBTOOL_MACOS libtool HINTS /usr/bin
+                                             /Library/Developer/CommandLineTools/usr/bin)
 
     # confirm that the libtool we found is not GNU libtool
     execute_process(COMMAND ${LIBTOOL_MACOS} -V
                     OUTPUT_VARIABLE LIBTOOL_V_OUTPUT
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(NOT "${LIBTOOL_V_OUTPUT}" MATCHES ".*cctools-([0-9.]+).*")
-      message(FATAL_ERROR "libtool found appears to be the incompatible GNU libtool: ${LIBTOOL_MACOS}")
+      message(FATAL_ERROR "libtool found appears to be the incompatible GNU libtool: ${LIBTOOL_MACOS}"
+      )
     endif()
 
     set(BUNDLE_COMMAND ${LIBTOOL_MACOS} "-no_warning_for_no_symbols" "-static" "-o"
