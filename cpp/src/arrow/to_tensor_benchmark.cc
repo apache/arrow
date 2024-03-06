e75703d457b8ef78cafc37446024814e12848253
AlenkaF <frim.alenka@gmail.com>
2024-03-06 11:27:03 +0100

    Use templates and bigger data

--- cpp/src/arrow/to_tensor_benchmark.cc
+++ cpp/src/arrow/to_tensor_benchmark.cc
@@ -19,15 +19,14 @@
 
 #include "arrow/record_batch.h"
 #include "arrow/testing/gtest_util.h"
+#include "arrow/testing/random.h"
 #include "arrow/type.h"
 
 namespace arrow {
 
-static void RecordBatchUniformTypesSimple(
-    benchmark::State& state) {  // NOLINT non-const reference
-  const int length = 9;
-  auto ty = int32();
-
+template <typename ValueType>
+static void BatchToTensorSimple(benchmark::State& state) {
+  std::shared_ptr<DataType> ty = TypeTraits<ValueType>::type_singleton();
   auto f0 = field("f0", ty);
   auto f1 = field("f1", ty);
   auto f2 = field("f2", ty);
@@ -35,11 +34,13 @@ static void RecordBatchUniformTypesSimple(
   std::vector<std::shared_ptr<Field>> fields = {f0, f1, f2};
   auto schema = ::arrow::schema(fields);
 
-  auto a0 = ArrayFromJSON(ty, "[1, 2, 3, 4, 5, 6, 7, 8, 9]");
-  auto a1 = ArrayFromJSON(ty, "[10, 20, 30, 40, 50, 60, 70, 80, 90]");
-  auto a2 = ArrayFromJSON(ty, "[100, 100, 100, 100, 100, 100, 100, 100, 100]");
+  constexpr int kNumRows = 500;
+  arrow::random::RandomArrayGenerator gen_{42};
+  auto a0 = gen_.ArrayOf(ty, kNumRows);
+  auto a1 = gen_.ArrayOf(ty, kNumRows);
+  auto a2 = gen_.ArrayOf(ty, kNumRows);
 
-  auto batch = RecordBatch::Make(schema, length, {a0, a1, a2});
+  auto batch = RecordBatch::Make(schema, kNumRows, {a0, a1, a2});
 
   ASSERT_OK_AND_ASSIGN(auto tensor, batch->ToTensor());
 
@@ -52,6 +53,16 @@ static void RecordBatchUniformTypesSimple(
                           batch->num_rows());
 }
 
-BENCHMARK(RecordBatchUniformTypesSimple);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt8Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt16Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt32Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt64Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int8Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int16Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int32Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int64Type);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, HalfFloatType);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, FloatType);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, DoubleType);
 
 }  // namespace arrow
