c24db80c5de59fbf7ffa46a6c78c980b35df6449
Sutou Kouhei <kou@clear-code.com>
2024-03-07 22:31:26 +0900

    Don't enable mold by default
    mold doesn't work with conda.

--- cpp/cmake_modules/DefineOptions.cmake
+++ cpp/cmake_modules/DefineOptions.cmake
@@ -159,7 +159,7 @@ takes precedence over ccache if a storage backend is configured" ON)
 
   define_option(ARROW_USE_LD_GOLD "Use ld.gold for linking on Linux (if available)" OFF)
 
-  define_option(ARROW_USE_MOLD "Use mold for linking on Linux (if available)" ON)
+  define_option(ARROW_USE_MOLD "Use mold for linking on Linux (if available)" OFF)
 
   define_option(ARROW_USE_PRECOMPILED_HEADERS "Use precompiled headers when compiling"
                 OFF)
