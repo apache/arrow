ed5b43c2d459ce6dc07e5222fb6106068da90bb4
AlenkaF <frim.alenka@gmail.com>
2024-03-11 15:16:00 +0100

    Fix linter error

--- cpp/src/arrow/record_batch.cc
+++ cpp/src/arrow/record_batch.cc
@@ -248,15 +248,15 @@ Result<std::shared_ptr<StructArray>> RecordBatch::ToStructArray() const {
                                        /*offset=*/0);
 }
 
-#define TYPE_CASE(type)                                                    \
-  case Type::type: {                                                       \
-    using T = typename TypeIdTraits<Type::type>::Type;                     \
-    using CType_in = typename TypeTraits<T>::CType;                        \
-    auto* in_values = batch.column(i) -> data() -> GetValues<CType_in>(1); \
-    for (int64_t i = 0; i < arr.length(); ++i) {                           \
-      *out_values++ = static_cast<CType>(*in_values++);                    \
-    }                                                                      \
-    break;                                                                 \
+#define TYPE_CASE(type)                                                \
+  case Type::type: {                                                   \
+    using T = typename TypeIdTraits<Type::type>::Type;                 \
+    using CType_in = typename TypeTraits<T>::CType;                    \
+    auto* in_values = batch.column(i)->data()->GetValues<CType_in>(1); \
+    for (int64_t i = 0; i < arr.length(); ++i) {                       \
+      *out_values++ = static_cast<CType>(*in_values++);                \
+    }                                                                  \
+    break;                                                             \
   }
 
 template <typename DataType>
