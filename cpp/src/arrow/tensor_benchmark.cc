391547c2613e4473b06fcb5c98f0392b18fe4352
AlenkaF <frim.alenka@gmail.com>
2024-03-13 14:19:20 +0100

    Clean up and add SetArgs

--- cpp/src/arrow/tensor_benchmark.cc
+++ cpp/src/arrow/tensor_benchmark.cc
@@ -21,48 +21,49 @@
 #include "arrow/testing/gtest_util.h"
 #include "arrow/testing/random.h"
 #include "arrow/type.h"
+#include "arrow/util/benchmark_util.h"
 
 namespace arrow {
 
 template <typename ValueType>
 static void BatchToTensorSimple(benchmark::State& state) {
+  RegressionArgs args(state);
   std::shared_ptr<DataType> ty = TypeTraits<ValueType>::type_singleton();
-  auto f0 = field("f0", ty);
-  auto f1 = field("f1", ty);
-  auto f2 = field("f2", ty);
 
-  std::vector<std::shared_ptr<Field>> fields = {f0, f1, f2};
-  auto schema = ::arrow::schema(fields);
-
-  constexpr int kNumRows = 100000;
+  const int64_t kNumRows = args.size * 8;
   arrow::random::RandomArrayGenerator gen_{42};
+
+  std::vector<std::shared_ptr<Field>> fields = {};
   std::vector<std::shared_ptr<Array>> columns = {};
+
   for (int i = 0; i < 100; ++i) {
+    fields.push_back(field("f" + std::to_string(i), ty));
     columns.push_back(gen_.ArrayOf(ty, kNumRows));
   }
+  auto schema = std::make_shared<Schema>(std::move(fields));
   auto batch = RecordBatch::Make(schema, kNumRows, columns);
-
-  ASSERT_OK_AND_ASSIGN(auto tensor, batch->ToTensor());
 
   for (auto _ : state) {
     ASSERT_OK_AND_ASSIGN(auto tensor, batch->ToTensor());
   }
-  benchmark::DoNotOptimize(tensor);
-  state.SetItemsProcessed(state.iterations() * batch->num_rows() * batch->num_columns());
-  state.SetBytesProcessed(state.iterations() * ty->bit_width() * batch->num_columns() *
-                          batch->num_rows());
+  state.SetItemsProcessed(state.iterations() * kNumRows * batch->num_columns());
+  state.SetBytesProcessed(state.iterations() * ty->bit_width() * kNumRows * batch->num_columns());
 }
 
-BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt8Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt16Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt32Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt64Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, Int8Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, Int16Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, Int32Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, Int64Type);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, HalfFloatType);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, FloatType);
-BENCHMARK_TEMPLATE(BatchToTensorSimple, DoubleType);
+void SetArgs(benchmark::internal::Benchmark* bench) {
+  BenchmarkSetArgsWithSizes(bench, {kL1Size, kL2Size});
+}
+
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt8Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt16Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt32Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, UInt64Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int8Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int16Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int32Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, Int64Type)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, HalfFloatType)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, FloatType)->Apply(SetArgs);
+BENCHMARK_TEMPLATE(BatchToTensorSimple, DoubleType)->Apply(SetArgs);
 
 }  // namespace arrow
