8f589dab77e03cf2cc13ff03acbdb0cfe384f371
AlenkaF <frim.alenka@gmail.com>
2024-03-20 09:43:10 +0100

    Remove helper function and use pa.array to construct ree array

--- python/pyarrow/tests/test_array.py
+++ python/pyarrow/tests/test_array.py
@@ -3619,24 +3619,18 @@ def test_run_end_encoded_from_array_with_type():
     assert result.equals(expected)
 
 
-def _generate_ree_array():
-    run_ends = [1, 3, 6]
-    values = [1, 2, 3]
-    ree_type = pa.run_end_encoded(pa.int32(), pa.int64())
-    return pa.RunEndEncodedArray.from_arrays(run_ends, values,
-                                             ree_type)
-
-
 def test_run_end_encoded_to_numpy():
-    ree_array = _generate_ree_array()
-    expected = np.array([1, 2, 2, 3, 3, 3])
+    arr = [1, 2, 2, 3, 3, 3]
+    ree_array = pa.array(arr, pa.run_end_encoded(pa.int32(), pa.int64()))
+    expected = np.array(arr)
     np.testing.assert_array_equal(ree_array.to_numpy(), expected)
 
 
 @pytest.mark.pandas
 def test_run_end_encoded_to_pandas():
-    ree_array = _generate_ree_array()
-    assert ree_array.to_pandas().tolist() == [1, 2, 2, 3, 3, 3]
+    arr = [1, 2, 2, 3, 3, 3]
+    ree_array = pa.array(arr, pa.run_end_encoded(pa.int32(), pa.int64()))
+    assert ree_array.to_pandas().tolist() == arr
 
 
 @pytest.mark.parametrize(('list_array_type', 'list_type_factory'),
