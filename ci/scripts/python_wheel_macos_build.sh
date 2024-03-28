9673f991b29261ad5b48a37b44d8df751091c1df
Gang Wu <ustcwg@gmail.com>
2024-03-29 01:17:33 +0800

    GH-40507: [C++][ORC] Upgrade ORC to 2.0.0 (#40508)
    ### Rationale for this change
    
    This PR aims to upgrade to a new major version of Apache ORC: https://orc.apache.org/news/2024/03/08/ORC-2.0.0/
    
    ### What changes are included in this PR?
    
    This PR upgrades ORC dependency from 1.9.2 to 2.0.0.
    
    ### Are these changes tested?
    
    Pass the CIs.
    
    ### Are there any user-facing changes?
    
    No.
    * GitHub Issue: #40507
    
    Lead-authored-by: Antoine Pitrou <antoine@python.org>
    Co-authored-by: Gang Wu <ustcwg@gmail.com>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- ci/scripts/python_wheel_macos_build.sh
+++ ci/scripts/python_wheel_macos_build.sh
@@ -91,6 +91,13 @@ echo "=== (${PYTHON_VERSION}) Building Arrow C++ libraries ==="
 : ${VCPKG_FEATURE_FLAGS:=-manifests}
 : ${VCPKG_TARGET_TRIPLET:=${VCPKG_DEFAULT_TRIPLET:-x64-osx-static-${CMAKE_BUILD_TYPE}}}
 
+echo "=== Protobuf compiler versions on PATH ==="
+which -a protoc || echo "no protoc on PATH!"
+
+echo "=== Protobuf compiler version from vcpkg ==="
+_pbc=${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/tools/protobuf/protoc
+echo "$_pbc: `$_pbc --version`"
+
 mkdir -p ${build_dir}/build
 pushd ${build_dir}/build
 
@@ -122,6 +129,7 @@ cmake \
     -DARROW_SUBSTRAIT=${ARROW_SUBSTRAIT} \
     -DARROW_TENSORFLOW=${ARROW_TENSORFLOW} \
     -DARROW_USE_CCACHE=ON \
+    -DARROW_VERBOSE_THIRDPARTY_BUILD=ON \
     -DARROW_WITH_BROTLI=${ARROW_WITH_BROTLI} \
     -DARROW_WITH_BZ2=${ARROW_WITH_BZ2} \
     -DARROW_WITH_LZ4=${ARROW_WITH_LZ4} \
@@ -134,7 +142,6 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=${build_dir}/install \
     -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES} \
     -DCMAKE_UNITY_BUILD=${CMAKE_UNITY_BUILD} \
-    -DORC_PROTOBUF_EXECUTABLE=${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/tools/protobuf/protoc \
     -DORC_SOURCE=BUNDLED \
     -DPARQUET_REQUIRE_ENCRYPTION=${PARQUET_REQUIRE_ENCRYPTION} \
     -DVCPKG_MANIFEST_MODE=OFF \
