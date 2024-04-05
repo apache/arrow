a35aaca0b7c06377fa60d31639c23ac38d6b3f54
Bryce Mecum <petridish@gmail.com>
2024-04-05 10:35:23 -0800

    Fix includes

--- cpp/src/arrow/ipc/message_internal_test.cc
+++ cpp/src/arrow/ipc/message_internal_test.cc
@@ -21,6 +21,8 @@
 
 #include "arrow/buffer.h"
 #include "arrow/ipc/dictionary.h"
+#include "arrow/ipc/metadata_internal.h"
+#include "arrow/ipc/options.h"
 #include "arrow/memory_pool.h"
 #include "arrow/testing/gtest_util.h"
 #include "arrow/util/key_value_metadata.h"
