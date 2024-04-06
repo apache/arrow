431171afdf98075a1813c6d9e21e15ca6972b38d
Dewey Dunnington <dewey@fishandwhistle.net>
2024-04-06 19:30:39 -0300

    remove unused import

--- csharp/src/Apache.Arrow/C/CArrowArrayImporter.cs
+++ csharp/src/Apache.Arrow/C/CArrowArrayImporter.cs
@@ -18,7 +18,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using Apache.Arrow.Memory;
 using Apache.Arrow.Types;
