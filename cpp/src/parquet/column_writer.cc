e39a9e4b5731cfc1d06018aa0f75bc012a97f664
Clif Houck <me@clifhouck.com>
2024-04-11 09:46:08 -0500

    RLE_DICTIONARY is also dictionary encoding

--- cpp/src/parquet/column_writer.cc
+++ cpp/src/parquet/column_writer.cc
@@ -1206,7 +1206,7 @@ Status ConvertDictionaryToDense(const ::arrow::Array& array, MemoryPool* pool,
 }
 
 static inline bool IsDictionaryEncoding(Encoding::type encoding) {
-  return encoding == Encoding::PLAIN_DICTIONARY;
+  return encoding == Encoding::PLAIN_DICTIONARY || encoding == Encoding::RLE_DICTIONARY;
 }
 
 template <typename DType>
