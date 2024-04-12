ec2d7cbfb426854ccb57be1d2abd9e2f88b268b9
andyfan <52736754+DuanWeiFan@users.noreply.github.com>
2024-04-12 13:11:32 -0500

    GH-41159: [Go][Parquet] Improvement Parquet BitWriter WriteVlqInt Performance  (#41160)
    [GH-41159](https://github.com/apache/arrow/issues/41159)
    ### Rationale for this change
    
    This change improves Parquet FileWriter performance while writing parquets from arrow Records.
    We saw a speed improvement from writing 320k rows/sec -> 650 rows/sec after making this change.
    
    ### What changes are included in this PR?
    This PR reuses the `buf` variable being used by the bitWriter when writing parquet files.
    
    ### Are these changes tested?
    Yes
    
    ### Are there any user-facing changes?
    No
    
    Authored-by: @ hhoughgg 
    
    * GitHub Issue: #41159
    
    Lead-authored-by: Andy Fan <duan-wei@cloudflare.com>
    Co-authored-by: andyfan <52736754+DuanWeiFan@users.noreply.github.com>
    Signed-off-by: Matt Topol <zotthewizard@gmail.com>

--- go/parquet/internal/utils/bit_reader_test.go
+++ go/parquet/internal/utils/bit_reader_test.go
@@ -59,6 +59,23 @@ func TestBitWriter(t *testing.T) {
 
 	assert.Equal(t, byte(0xAA), buf[0])
 	assert.Equal(t, byte(0xCC), buf[1])
+
+	for i := 0; i < 3; i++ {
+		assert.True(t, bw.WriteVlqInt(uint64(i)))
+	}
+	assert.Equal(t, byte(0xAA), buf[0])
+	assert.Equal(t, byte(0xCC), buf[1])
+	assert.Equal(t, byte(0), buf[2])
+	assert.Equal(t, byte(1), buf[3])
+	assert.Equal(t, byte(2), buf[4])
+}
+
+func BenchmarkBitWriter(b *testing.B) {
+	buf := make([]byte, b.N)
+	bw := utils.NewBitWriter(utils.NewWriterAtBuffer(buf))
+	for i := 0; i < b.N; i++ {
+		assert.True(b, bw.WriteVlqInt(uint64(1)))
+	}
 }
 
 func TestBitReader(t *testing.T) {
