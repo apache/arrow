7937dc48aca96103403507d682ad4a5a97a4c32d
Sutou Kouhei <kou@clear-code.com>
2024-04-17 10:45:52 +0900

    Use more C.UTF-8

--- dev/release/05-binary-upload.sh
+++ dev/release/05-binary-upload.sh
@@ -21,8 +21,17 @@ set -e
 set -u
 set -o pipefail
 
-export LANG=C
-export LC_CTYPE=C
+export LANG=C.UTF-8
+export LC_ADDRESS=C.UTF-8
+export LC_CTYPE=C.UTF-8
+export LC_IDENTIFICATION=C.UTF-8
+export LC_MEASUREMENT=C.UTF-8
+export LC_MONETARY=C.UTF-8
+export LC_NAME=C.UTF-8
+export LC_NUMERIC=C.UTF-8
+export LC_PAPER=C.UTF-8
+export LC_TELEPHONE=C.UTF-8
+export LC_TIME=C.UTF-8
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
