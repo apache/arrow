8348c538a5354127e95bf37157040138254b351c
Dominik Moritz <domoritz@apache.org>
2024-04-17 23:21:42 -0400

    GH-39722: [JS] Clean up packaging (#39723)
    * Remove .mts since it's the same as .ts
    * Use .js consistently for all packages
    * Closes: #39722
    
    ---------
    
    Signed-off-by: Matt Topol <zotthewizard@gmail.com>
    Co-authored-by: ptaylor <paul.e.taylor@me.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- js/gulp/typescript-task.js
+++ js/gulp/typescript-task.js
@@ -61,10 +61,9 @@ function compileTypescript(out, tsconfigPath, tsconfigOverrides, writeSourcemaps
     );
     const writeSources = observableFromStreams(tsProject.src(), gulp.dest(path.join(out, 'src')));
     const writeDTypes = observableFromStreams(dts, sourcemaps.write('./', { includeContent: false, sourceRoot: './src' }), gulp.dest(out));
-    const mapFile = tsProject.options.module === tsc.ModuleKind.ES2015 ? esmMapFile : cjsMapFile;
     const writeJSArgs = writeSourcemaps ? [
         js,
-        sourcemaps.write('./', { mapFile, includeContent: false, sourceRoot: './src' }),
+        sourcemaps.write('./', { includeContent: false, sourceRoot: './src' }),
         gulp.dest(out)
       ] : [
         js,
@@ -73,6 +72,3 @@ function compileTypescript(out, tsconfigPath, tsconfigOverrides, writeSourcemaps
     const writeJS = observableFromStreams(...writeJSArgs);
     return ObservableForkJoin([writeSources, writeDTypes, writeJS]);
 }
-
-const cjsMapFile = (mapFilePath) => mapFilePath;
-const esmMapFile = (mapFilePath) => mapFilePath.replace('.js.map', '.mjs.map');
