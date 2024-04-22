af98b11efc66b0a6f5b12185804d3bf7d45445d8
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-04-22 12:46:31 +0100

    lint

--- python/pyarrow/__init__.py
+++ python/pyarrow/__init__.py
@@ -428,7 +428,7 @@ def get_library_dirs():
 
     return library_dirs
 
-# On emscripten in browser we need to load the timezones from tzdata 
+# On emscripten in browser we need to load the timezones from tzdata
 # package into where arrow expects them to be, otherwise all time
 # handling will break.
 if _sys.platform == 'emscripten':
