3ef83516e8d51f274b5b4153bc1d73f6f0c687a9
Vibhatha Abeykoon <vibhatha@gmail.com>
2024-04-24 14:41:14 +0530

    fix: addressing reviews s4 v3.3: format

--- java/vector/src/test/java/org/apache/arrow/vector/TestValueVector.java
+++ java/vector/src/test/java/org/apache/arrow/vector/TestValueVector.java
@@ -1377,14 +1377,14 @@ public class TestValueVector {
       ReusableByteArray reusableByteArray = new ReusableByteArray();
       vector.read(0, reusableByteArray);
       assertArrayEquals(str.getBytes(StandardCharsets.UTF_8),
-          Arrays.copyOfRange(
-              reusableByteArray.getBuffer(), 0, (int) reusableByteArray.getLength()));
+          Arrays.copyOfRange(reusableByteArray.getBuffer(),
+              0, (int) reusableByteArray.getLength()));
       byte[] oldBuffer = reusableByteArray.getBuffer();
 
       vector.read(1, reusableByteArray);
       assertArrayEquals(str2.getBytes(StandardCharsets.UTF_8),
-          Arrays.copyOfRange(
-              reusableByteArray.getBuffer(), 0, (int) reusableByteArray.getLength()));
+          Arrays.copyOfRange(reusableByteArray.getBuffer(),
+              0, (int) reusableByteArray.getLength()));
 
       // There should not have been any reallocation since the newer value is smaller in length.
       assertSame(oldBuffer, reusableByteArray.getBuffer());
