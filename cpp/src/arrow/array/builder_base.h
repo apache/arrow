2b1dadddf7ad4bc0d170b8360b8b89ee59f8e681
Will Ayd <william.ayd@icloud.com>
2024-05-01 13:48:50 -0400

    GH41478: [C++] Clean up more redundant move warnings

--- cpp/src/arrow/array/builder_base.h
+++ cpp/src/arrow/array/builder_base.h
@@ -332,7 +332,7 @@ inline Result<std::unique_ptr<ArrayBuilder>> MakeBuilder(
     const std::shared_ptr<DataType>& type, MemoryPool* pool = default_memory_pool()) {
   std::unique_ptr<ArrayBuilder> out;
   ARROW_RETURN_NOT_OK(MakeBuilder(pool, type, &out));
-  return std::move(out);
+  return out;
 }
 
 /// \brief Construct an empty ArrayBuilder corresponding to the data
@@ -346,7 +346,7 @@ inline Result<std::unique_ptr<ArrayBuilder>> MakeBuilderExactIndex(
     const std::shared_ptr<DataType>& type, MemoryPool* pool = default_memory_pool()) {
   std::unique_ptr<ArrayBuilder> out;
   ARROW_RETURN_NOT_OK(MakeBuilderExactIndex(pool, type, &out));
-  return std::move(out);
+  return out;
 }
 
 /// \brief Construct an empty DictionaryBuilder initialized optionally
@@ -365,7 +365,7 @@ inline Result<std::unique_ptr<ArrayBuilder>> MakeDictionaryBuilder(
     MemoryPool* pool = default_memory_pool()) {
   std::unique_ptr<ArrayBuilder> out;
   ARROW_RETURN_NOT_OK(MakeDictionaryBuilder(pool, type, dictionary, &out));
-  return std::move(out);
+  return out;
 }
 
 }  // namespace arrow
