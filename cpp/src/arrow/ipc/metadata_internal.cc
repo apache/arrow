7ccb3da2ddd463a76f042ad3b3a1215ab766d48e
Bryce Mecum <petridish@gmail.com>
2024-05-01 11:55:58 -0800

    Remove _

--- cpp/src/arrow/ipc/metadata_internal.cc
+++ cpp/src/arrow/ipc/metadata_internal.cc
@@ -478,9 +478,9 @@ static Status GetDictionaryEncoding(FBB& fbb, const std::shared_ptr<Field>& fiel
 
 static KeyValueOffset AppendKeyValue(FBB& fbb, const std::string& key,
                                      const std::string& value) {
-  auto fbb_key_ = fbb.CreateString(key);
-  auto fbb_value_ = fbb.CreateString(value);
-  return flatbuf::CreateKeyValue(fbb, fbb_key_, fbb_value_);
+  auto fbb_key = fbb.CreateString(key);
+  auto fbb_value = fbb.CreateString(value);
+  return flatbuf::CreateKeyValue(fbb, fbb_key, fbb_value);
 }
 
 static void AppendKeyValueMetadata(FBB& fbb, const KeyValueMetadata& metadata,
