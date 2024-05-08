5252c6ce13694fa31dbcb2623d1629cd8fe53a47
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2024-05-08 22:46:45 +0300

    GH-41594: [Go] Support reading `date64` type & properly validate list-like types (#41595)
    This PR includes 2 fixes:
    1. support reading `date64` columns (as write is supported)
    2. properly validate list-like data types (list of unsupported is unsupported)
    
    ### Rationale for this change
    
    See #41594
    
    ### What changes are included in this PR?
    
    1. Added `date64` reading & conversion funcs similar to `date32`
    2. Refactored date type validation
    
    ### Are these changes tested?
    
    a55cd5324d2c47932410b0c7a9c46075386645d2
    
    ### Are there any user-facing changes?
    
    No.
    
    * GitHub Issue: #41594
    
    Authored-by: candiduslynx <candiduslynx@gmail.com>
    Signed-off-by: Matt Topol <zotthewizard@gmail.com>

--- go/arrow/csv/common.go
+++ go/arrow/csv/common.go
@@ -239,21 +239,31 @@ func WithStringsReplacer(replacer *strings.Replacer) Option {
 
 func validate(schema *arrow.Schema) {
 	for i, f := range schema.Fields() {
-		switch ft := f.Type.(type) {
-		case *arrow.BooleanType:
-		case *arrow.Int8Type, *arrow.Int16Type, *arrow.Int32Type, *arrow.Int64Type:
-		case *arrow.Uint8Type, *arrow.Uint16Type, *arrow.Uint32Type, *arrow.Uint64Type:
-		case *arrow.Float16Type, *arrow.Float32Type, *arrow.Float64Type:
-		case *arrow.StringType, *arrow.LargeStringType:
-		case *arrow.TimestampType:
-		case *arrow.Date32Type, *arrow.Date64Type:
-		case *arrow.Decimal128Type, *arrow.Decimal256Type:
-		case *arrow.ListType, *arrow.LargeListType, *arrow.FixedSizeListType:
-		case *arrow.BinaryType, *arrow.LargeBinaryType, *arrow.FixedSizeBinaryType:
-		case arrow.ExtensionType:
-		case *arrow.NullType:
-		default:
-			panic(fmt.Errorf("arrow/csv: field %d (%s) has invalid data type %T", i, f.Name, ft))
+		if !typeSupported(f.Type) {
+			panic(fmt.Errorf("arrow/csv: field %d (%s) has invalid data type %T", i, f.Name, f.Type))
 		}
 	}
+}
+
+func typeSupported(dt arrow.DataType) bool {
+	switch dt := dt.(type) {
+	case *arrow.BooleanType:
+	case *arrow.Int8Type, *arrow.Int16Type, *arrow.Int32Type, *arrow.Int64Type:
+	case *arrow.Uint8Type, *arrow.Uint16Type, *arrow.Uint32Type, *arrow.Uint64Type:
+	case *arrow.Float16Type, *arrow.Float32Type, *arrow.Float64Type:
+	case *arrow.StringType, *arrow.LargeStringType:
+	case *arrow.TimestampType:
+	case *arrow.Date32Type, *arrow.Date64Type:
+	case *arrow.Decimal128Type, *arrow.Decimal256Type:
+	case *arrow.MapType:
+		return false
+	case arrow.ListLikeType:
+		return typeSupported(dt.Elem())
+	case *arrow.BinaryType, *arrow.LargeBinaryType, *arrow.FixedSizeBinaryType:
+	case arrow.ExtensionType:
+	case *arrow.NullType:
+	default:
+		return false
+	}
+	return true
 }
