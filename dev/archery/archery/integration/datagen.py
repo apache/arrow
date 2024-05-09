e9bfe06e9a5f96cab177ef7322bb6a7de0b968eb
Dewey Dunnington <dewey@voltrondata.com>
2024-05-09 10:05:56 -0300

    whitespace

--- dev/archery/archery/integration/datagen.py
+++ dev/archery/archery/integration/datagen.py
@@ -1934,8 +1934,7 @@ def get_generated_json_files(tempdir=None):
         generate_binary_view_case()
         .skip_tester('Java')
         .skip_tester('JS')
-        .skip_tester('Rust')
-        ,
+        .skip_tester('Rust'),
 
         generate_list_view_case()
         .skip_tester('C#')     # Doesn't support large list views
