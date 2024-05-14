07345ab4a9f0d8b596ac7de37f320832d5afff0b
Rossi Sun <zanmato1984@gmail.com>
2024-05-15 01:10:22 +0800

    GH-41334: [C++][Acero] Use per-node basis temp vector stack to mitigate overflow (#41335)
    ### Rationale for this change
    
    The risk of temp vector stack overflow still exists as described in #41334 . Many people have agreed on a per-node basis approach:
    > 1) it doesn't introduce more performance penalty than shared stack; 2) it can mitigate the overflow in a natural way, i.e., expanding the stack size linear to the number of nodes; 3) it requires no more complexity to the existing stack implementation.
    
    The full (but long) story is also revealed in the subsequent discussion of this PR. Feel free to scroll down.
    
    ### What changes are included in this PR?
    
    1. Change the current shared (per-thread) temp vector stack usage to per-node basis.
    2. Make the stack size required by each stack user more explicit.
    
    ### Are these changes tested?
    
    UT included.
    
    ### Are there any user-facing changes?
    
    None.
    
    * GitHub Issue: #41334
    
    Authored-by: Ruoxi Sun <zanmato1984@gmail.com>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/src/arrow/compute/row/compare_test.cc
+++ cpp/src/arrow/compute/row/compare_test.cc
@@ -19,23 +19,26 @@
 
 #include "arrow/compute/row/compare_internal.h"
 #include "arrow/testing/gtest_util.h"
+#include "arrow/testing/random.h"
 
 namespace arrow {
 namespace compute {
 
 using arrow::bit_util::BytesForBits;
 using arrow::internal::CpuInfo;
+using arrow::random::RandomArrayGenerator;
 using arrow::util::MiniBatch;
 using arrow::util::TempVectorStack;
 
 // Specialized case for GH-39577.
 TEST(KeyCompare, CompareColumnsToRowsCuriousFSB) {
   int fsb_length = 9;
+  int num_rows = 7;
+
   MemoryPool* pool = default_memory_pool();
   TempVectorStack stack;
-  ASSERT_OK(stack.Init(pool, 8 * MiniBatch::kMiniBatchLength * sizeof(uint64_t)));
+  ASSERT_OK(stack.Init(pool, KeyCompare::CompareColumnsToRowsTempStackUsage(num_rows)));
 
-  int num_rows = 7;
   auto column_right = ArrayFromJSON(fixed_size_binary(fsb_length), R"([
       "000000000",
       "111111111",
@@ -103,6 +106,61 @@ TEST(KeyCompare, CompareColumnsToRowsCuriousFSB) {
       SCOPED_TRACE(i);
       ASSERT_EQ(arrow::bit_util::GetBit(match_bitvector.data(), i), i != 6);
     }
+  }
+}
+
+// Make sure that KeyCompare::CompareColumnsToRows uses no more stack space than declared
+// in KeyCompare::CompareColumnsToRowsTempStackUsage().
+TEST(KeyCompare, CompareColumnsToRowsTempStackUsage) {
+  for (auto num_rows :
+       {0, 1, MiniBatch::kMiniBatchLength, MiniBatch::kMiniBatchLength * 64}) {
+    SCOPED_TRACE("num_rows = " + std::to_string(num_rows));
+
+    MemoryPool* pool = default_memory_pool();
+    TempVectorStack stack;
+    ASSERT_OK(stack.Init(pool, KeyCompare::CompareColumnsToRowsTempStackUsage(num_rows)));
+
+    RandomArrayGenerator gen(42);
+
+    auto column_right = gen.Int8(num_rows, 0, 127);
+    ExecBatch batch_right({column_right}, num_rows);
+
+    std::vector<KeyColumnMetadata> column_metadatas_right;
+    ASSERT_OK(ColumnMetadatasFromExecBatch(batch_right, &column_metadatas_right));
+
+    RowTableMetadata table_metadata_right;
+    table_metadata_right.FromColumnMetadataVector(column_metadatas_right,
+                                                  sizeof(uint64_t), sizeof(uint64_t));
+
+    std::vector<KeyColumnArray> column_arrays_right;
+    ASSERT_OK(ColumnArraysFromExecBatch(batch_right, &column_arrays_right));
+
+    RowTableImpl row_table;
+    ASSERT_OK(row_table.Init(pool, table_metadata_right));
+
+    RowTableEncoder row_encoder;
+    row_encoder.Init(column_metadatas_right, sizeof(uint64_t), sizeof(uint64_t));
+    row_encoder.PrepareEncodeSelected(0, num_rows, column_arrays_right);
+
+    std::vector<uint16_t> row_ids_right(num_rows);
+    std::iota(row_ids_right.begin(), row_ids_right.end(), 0);
+    ASSERT_OK(row_encoder.EncodeSelected(&row_table, num_rows, row_ids_right.data()));
+
+    auto column_left = gen.Int8(num_rows, 0, 127);
+    ExecBatch batch_left({column_left}, num_rows);
+    std::vector<KeyColumnArray> column_arrays_left;
+    ASSERT_OK(ColumnArraysFromExecBatch(batch_left, &column_arrays_left));
+
+    std::vector<uint32_t> row_ids_left(num_rows);
+    std::iota(row_ids_left.begin(), row_ids_left.end(), 0);
+
+    LightContext ctx{CpuInfo::GetInstance()->hardware_flags(), &stack};
+
+    uint32_t num_rows_no_match;
+    std::vector<uint16_t> row_ids_out(num_rows);
+    KeyCompare::CompareColumnsToRows(num_rows, NULLPTR, row_ids_left.data(), &ctx,
+                                     &num_rows_no_match, row_ids_out.data(),
+                                     column_arrays_left, row_table, true, NULLPTR);
   }
 }
 
