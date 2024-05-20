5e5814460df6626d22d0413e81b34d1923a952fe
Kevin Gurney <kgurney@mathworks.com>
2024-05-20 11:29:50 -0400

    Make ArrayImporter import from matlab.mixin.Scalar.

--- matlab/src/matlab/+arrow/+c/+internal/ArrayImporter.m
+++ matlab/src/matlab/+arrow/+c/+internal/ArrayImporter.m
@@ -14,7 +14,7 @@
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
-classdef ArrayImporter
+classdef ArrayImporter < matlab.mixin.Scalar
 
     properties (Hidden, SetAccess=private, GetAccess=public)
         Proxy
