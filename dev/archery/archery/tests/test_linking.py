704095f1dd3ce34b0aacefc80272b4d8c5c84ef3
Vibhatha Abeykoon <vibhatha@gmail.com>
2024-05-29 06:14:19 +0530

    fix: test build

--- dev/archery/archery/tests/test_linking.py
+++ dev/archery/archery/tests/test_linking.py
@@ -15,18 +15,24 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from linking import _remove_weak_symbols, _remove_symbol_versions
+import pytest
+from archery.linking import DynamicLibrary
 
 
-def test_remove_weak_symbols(self):
+@pytest.fixture(autouse=True)
+def dynamic_library():
+    return DynamicLibrary("")
+
+
+def test_remove_weak_symbols(dynamic_library):
     symbol_info = ["symbol1 v", "symbol2 V",
                    "symbol3", "symbol4 w", "symbol5 W", "symbol6"]
     expected_result = ["symbol3", "symbol6"]
-    self.assertEqual(_remove_weak_symbols(self, symbol_info), expected_result)
+    assert dynamic_library._remove_weak_symbols(symbol_info) == expected_result
 
 
-def test_remove_symbol_versions(self):
+def test_remove_symbol_versions(dynamic_library):
     symbol_info = ["symbol1@version1",
                    "symbol2@version2", "symbol3", "symbol4@version4"]
     expected_result = ["symbol1", "symbol2", "symbol3", "symbol4"]
-    self.assertEqual(_remove_symbol_versions(self, symbol_info), expected_result)
+    assert dynamic_library._remove_symbol_versions(symbol_info) == expected_result
