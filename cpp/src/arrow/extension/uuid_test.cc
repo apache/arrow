f4cb7feddb7f5a1cd920ee8d7c457948c4349112
Rok Mihevc <rok@mihevc.org>
2024-06-06 15:36:31 +0200

    Post rebase changes

--- cpp/src/arrow/extension/uuid_test.cc
+++ cpp/src/arrow/extension/uuid_test.cc
@@ -29,8 +29,6 @@
 
 namespace arrow {
 
-using arrow::ipc::test::RoundtripBatch;
-
 TEST(TestUuuidExtensionType, ExtensionTypeTest) {
   auto type = uuid();
   ASSERT_EQ(type->id(), Type::EXTENSION);
