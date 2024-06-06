1067c035a3a287f7d3da572802cb7a95dc33679a
AlenkaF <frim.alenka@gmail.com>
2024-06-06 12:27:03 +0200

    Change -1 to kUnknownNullCount

--- cpp/src/arrow/array/array_nested.cc
+++ cpp/src/arrow/array/array_nested.cc
@@ -851,7 +851,7 @@ Result<std::shared_ptr<Array>> MapArray::FromArraysInternal(
   int64_t null_count = 0;
   if (null_bitmap) {
     buffers[0] = std::move(null_bitmap);
-    null_count = -1;
+    null_count = kUnknownNullCount;
   }
   buffers[1] = typed_offsets.values();
   return std::make_shared<MapArray>(type, offsets->length() - 1, std::move(buffers), keys,
