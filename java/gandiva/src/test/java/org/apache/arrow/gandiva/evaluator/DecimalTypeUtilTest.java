ee6fcf3c46c3a7a2816dc0fbc4e3d5f1a38fd1b3
Laurent Goujon <laurentgo@users.noreply.github.com>
2024-06-10 18:17:10 -0700

    GH-40827: [Java] Adding Spotless to Gandiva module (#42055)
    ### Rationale for this change
    
    Applying Java code style and formatting options to Gandiva module.
    
    ### What changes are included in this PR?
    
    Java code formatting via spotless plugin has been enabled.
    
    ### Are these changes tested?
    
    Yes, but doesn't involve test cases, the plugin itself corrects.
    
    ### Are there any user-facing changes?
    
    No
    
    * GitHub Issue: #40827
    
    Authored-by: Laurent Goujon <laurent@apache.org>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- java/gandiva/src/test/java/org/apache/arrow/gandiva/evaluator/DecimalTypeUtilTest.java
+++ java/gandiva/src/test/java/org/apache/arrow/gandiva/evaluator/DecimalTypeUtilTest.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.arrow.gandiva.evaluator;
 
 import org.apache.arrow.vector.types.pojo.ArrowType;
@@ -27,63 +26,68 @@ public class DecimalTypeUtilTest {
   public void testOutputTypesForAdd() {
     ArrowType.Decimal operand1 = getDecimal(30, 10);
     ArrowType.Decimal operand2 = getDecimal(30, 10);
-    ArrowType.Decimal resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-            .OperationType.ADD, operand1, operand2);
+    ArrowType.Decimal resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.ADD, operand1, operand2);
     Assert.assertTrue(getDecimal(31, 10).equals(resultType));
 
     operand1 = getDecimal(30, 6);
     operand2 = getDecimal(30, 5);
-    resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-            .OperationType.ADD, operand1, operand2);
+    resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.ADD, operand1, operand2);
     Assert.assertTrue(getDecimal(32, 6).equals(resultType));
 
     operand1 = getDecimal(30, 10);
     operand2 = getDecimal(38, 10);
-    resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-            .OperationType.ADD, operand1, operand2);
+    resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.ADD, operand1, operand2);
     Assert.assertTrue(getDecimal(38, 9).equals(resultType));
 
     operand1 = getDecimal(38, 10);
     operand2 = getDecimal(38, 38);
-    resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-            .OperationType.ADD, operand1, operand2);
+    resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.ADD, operand1, operand2);
     Assert.assertTrue(getDecimal(38, 9).equals(resultType));
 
     operand1 = getDecimal(38, 10);
     operand2 = getDecimal(38, 2);
-    resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-            .OperationType.ADD, operand1, operand2);
+    resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.ADD, operand1, operand2);
     Assert.assertTrue(getDecimal(38, 6).equals(resultType));
-
   }
 
   @Test
   public void testOutputTypesForMultiply() {
     ArrowType.Decimal operand1 = getDecimal(30, 10);
     ArrowType.Decimal operand2 = getDecimal(30, 10);
-    ArrowType.Decimal resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-                    .OperationType.MULTIPLY, operand1, operand2);
+    ArrowType.Decimal resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.MULTIPLY, operand1, operand2);
     Assert.assertTrue(getDecimal(38, 6).equals(resultType));
 
     operand1 = getDecimal(38, 10);
     operand2 = getDecimal(9, 2);
-    resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-            .OperationType.MULTIPLY, operand1, operand2);
+    resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.MULTIPLY, operand1, operand2);
     Assert.assertTrue(getDecimal(38, 6).equals(resultType));
-
   }
 
   @Test
   public void testOutputTypesForMod() {
     ArrowType.Decimal operand1 = getDecimal(30, 10);
     ArrowType.Decimal operand2 = getDecimal(28, 7);
-    ArrowType.Decimal resultType = DecimalTypeUtil.getResultTypeForOperation(DecimalTypeUtil
-                    .OperationType.MOD, operand1, operand2);
+    ArrowType.Decimal resultType =
+        DecimalTypeUtil.getResultTypeForOperation(
+            DecimalTypeUtil.OperationType.MOD, operand1, operand2);
     Assert.assertTrue(getDecimal(30, 10).equals(resultType));
   }
 
   private ArrowType.Decimal getDecimal(int precision, int scale) {
     return new ArrowType.Decimal(precision, scale, 128);
   }
-
 }
