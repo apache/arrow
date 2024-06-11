641ab7ef61122289bb0b6382391402ab4f4b32d2
AlenkaF <frim.alenka@gmail.com>
2024-06-11 09:16:48 +0200

    Remove shape and strides from ToTensor docstrings

--- cpp/src/arrow/table.h
+++ cpp/src/arrow/table.h
@@ -103,8 +103,7 @@ class ARROW_EXPORT Table {
 
   /// \brief Convert Table to Tensor
   ///
-  /// Create a Tensor object with shape (number of rows, number of columns) and
-  /// strides (type size in bytes, type size in bytes * number of rows).
+  /// Create a Tensor object.
   ///
   /// \param[in] null_to_nan if true, convert nulls to NaN
   /// \param[in] row_major if true, create row-major Tensor else column-major Tensor
