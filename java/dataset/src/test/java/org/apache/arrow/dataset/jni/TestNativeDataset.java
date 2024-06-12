832880a6ed61976f756450fdeae0c6fe0923fbff
Vibhatha Lakmal Abeykoon <vibhatha@users.noreply.github.com>
2024-06-12 12:24:38 +0530

    GH-40824: [Java] Adding Spotless to Dataset module (#42062)
    ### Rationale for this change
    
    Adding code style and formatting options for Dataset module. 
    
    ### What changes are included in this PR?
    
    Code formatting spotless plugin has been added. 
    
    ### Are these changes tested?
    
    Yes, but doesn't involve test cases, the plugin itself corrects. 
    * GitHub Issue: #40824
    
    Authored-by: Vibhatha Abeykoon <vibhatha@gmail.com>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- java/dataset/src/test/java/org/apache/arrow/dataset/jni/TestNativeDataset.java
+++ java/dataset/src/test/java/org/apache/arrow/dataset/jni/TestNativeDataset.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.arrow.dataset.jni;
 
 import org.apache.arrow.dataset.TestDataset;
