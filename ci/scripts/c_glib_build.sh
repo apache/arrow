4769a8d42eef86741bf09f6278b198bd2472b0df
Adam Reeve <adreeve@gmail.com>
2024-06-13 14:49:23 +1200

    Set PATH to fix dll load error when running gobject-introspection binary

--- ci/scripts/c_glib_build.sh
+++ ci/scripts/c_glib_build.sh
@@ -34,15 +34,21 @@ if [ -n "${MSYSTEM:-}" ]; then
   export ARROW_HOME="$(cygpath --unix "${ARROW_HOME}")"
 fi
 
+export PATH="${ARROW_HOME}/bin:${PATH}"
+
 meson_pkg_config_path="${ARROW_HOME}/lib/pkgconfig"
 
 mkdir -p ${build_dir}
 
 if [ -n "${VCPKG_ROOT:-}" ]; then
   vcpkg_install_root="${build_root}/vcpkg_installed"
+  vcpkg_triplet="x64-windows"
   $VCPKG_ROOT/vcpkg install --x-manifest-root=${source_dir} --x-install-root=${vcpkg_install_root}
-  export PKG_CONFIG="${vcpkg_install_root}/x64-windows/tools/pkgconf/pkgconf.exe"
-  meson_pkg_config_path="${vcpkg_install_root}/x64-windows/lib/pkgconfig:${meson_pkg_config_path}"
+  export PKG_CONFIG="${vcpkg_install_root}/${vcpkg_triplet}/tools/pkgconf/pkgconf.exe"
+  meson_pkg_config_path="${vcpkg_install_root}/${vcpkg_triplet}/lib/pkgconfig:${meson_pkg_config_path}"
+  # Configure PATH for libraries required by the gobject-introspection generated binary
+  cpp_vcpkg_install_root="${build_root}/cpp/vcpkg_installed"
+  export PATH="${vcpkg_install_root}/${vcpkg_triplet}/bin:${cpp_vcpkg_install_root}/${vcpkg_triplet}/bin:${cpp_vcpkg_install_root}/${vcpkg_triplet}/debug/bin:${PATH}"
 fi
 
 if [ -n "${VCToolsInstallDir:-}" -a -n "${MSYSTEM:-}" ]; then
