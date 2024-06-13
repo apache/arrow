fdb12d041889ec52513519e0b3189895d25cfd6b
Sarah Gilmore <sgilmore@mathworks.com>
2024-06-13 15:54:11 -0400

    Implement RecordBatchFileWriter::make(...)

--- matlab/src/cpp/arrow/matlab/io/ipc/proxy/record_batch_file_writer.cc
+++ matlab/src/cpp/arrow/matlab/io/ipc/proxy/record_batch_file_writer.cc
@@ -15,7 +15,15 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include "arrow/io/file.h"
+#include "arrow/matlab/error/error.h"
 #include "arrow/matlab/io/ipc/proxy/record_batch_file_writer.h"
+#include "arrow/matlab/tabular/proxy/record_batch.h"
+#include "arrow/matlab/tabular/proxy/schema.h"
+#include "arrow/util/utf8.h"
+
+#include "libmexclass/proxy/ProxyManager.h"
+
 
 namespace arrow::matlab::io::ipc::proxy {
 
@@ -23,6 +31,32 @@ RecordBatchFileWriter::RecordBatchFileWriter(const std::shared_ptr<arrow::ipc::R
   : writer{std::move(writer)} {}
 
 libmexclass::proxy::MakeResult RecordBatchFileWriter::make(const libmexclass::proxy::FunctionArguments& constructor_arguments) {
-  return libmexclass::error::Error{"arrow:matlab:NotImplemented", "Not Implemented"};
+  namespace mda = ::matlab::data;
+  using RecordBatchFileWriterProxy = arrow::matlab::io::ipc::proxy::RecordBatchFileWriter;
+  using SchemaProxy = arrow::matlab::tabular::proxy::Schema;
+
+  const mda::StructArray opts = constructor_arguments[0];
+
+  const mda::StringArray filename_mda = opts[0]["Filename"];
+  const auto filename_utf16 = std::u16string(filename_mda[0]);
+  MATLAB_ASSIGN_OR_ERROR(const auto filename_utf8,
+                        arrow::util::UTF16StringToUTF8(filename_utf16),
+                        error::UNICODE_CONVERSION_ERROR_ID);
+
+  const mda::TypedArray<uint64_t> arrow_schema_proxy_id_mda = opts[0]["SchemaProxyID"];
+  auto proxy = libmexclass::proxy::ProxyManager::getProxy(arrow_schema_proxy_id_mda[0]);
+  auto arrow_schema_proxy = std::static_pointer_cast<SchemaProxy>(proxy);
+  auto arrow_schema = arrow_schema_proxy->unwrap();
+
+  MATLAB_ASSIGN_OR_ERROR(auto output_stream,
+                        arrow::io::FileOutputStream::Open(filename_utf8),
+                        error::FAILED_TO_OPEN_FILE_FOR_WRITE);
+
+  MATLAB_ASSIGN_OR_ERROR(auto writer, 
+                        arrow::ipc::MakeFileWriter(output_stream, arrow_schema),
+                        "arrow:matlab:MakeFailed");
+
+  return std::make_shared<RecordBatchFileWriterProxy>(std::move(writer));
 }
+
 } // namespace arrow::matlab::io::ipc::proxy 
