8203675ec855a301e2d10e1cf9864aeea5e0e82c
Dane Pitkin <dpitkin@apache.org>
2024-06-17 14:28:44 -0700

    Use double brackets

--- ci/scripts/install_pandas.sh
+++ ci/scripts/install_pandas.sh
@@ -31,7 +31,7 @@ if [ "${numpy}" = "nightly" ]; then
   pip install --extra-index-url https://pypi.anaconda.org/scientific-python-nightly-wheels/simple --pre numpy
 elif [ "${numpy}" = "latest" ]; then
   pip install numpy
-elif [ "${numpy}" = [0-9]* ]; then
+elif [[ ${numpy} = [0-9]* ]]; then
   pip install numpy==${numpy}
 else
   pip install numpy${numpy}
@@ -43,7 +43,7 @@ elif [ "${pandas}" = "nightly" ]; then
   pip install --extra-index-url https://pypi.anaconda.org/scientific-python-nightly-wheels/simple --pre pandas
 elif [ "${pandas}" = "latest" ]; then
   pip install pandas
-elif [ "${pandas}" = [0-9]* ]; then
+elif [[ "${pandas}" = [0-9]* ]]; then
   pip install pandas==${pandas}
 else
   pip install pandas${pandas}
