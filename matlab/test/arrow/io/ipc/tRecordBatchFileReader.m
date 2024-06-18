7e57597867a625bac849379cb7fc73bbce2d282f
Sarah Gilmore <sgilmore@mathworks.com>
2024-06-18 15:59:27 -0400

    Add methods to setup test data files

--- matlab/test/arrow/io/ipc/tRecordBatchFileReader.m
+++ matlab/test/arrow/io/ipc/tRecordBatchFileReader.m
@@ -16,6 +16,54 @@
 % permissions and limitations under the License.
 classdef tRecordBatchFileReader < matlab.unittest.TestCase
 
+    properties
+        DataFolder
+        ZeroBatchFile
+        OneBatchFile
+        MultipleBatchFile
+    end
+
+    methods(TestClassSetup)
+        function setupDataFolder(testCase)
+            import matlab.unittest.fixtures.TemporaryFolderFixture
+            fixture = testCase.applyFixture(TemporaryFolderFixture);
+            testCase.DataFolder = string(fixture.Folder);
+        end
+
+        function setupZeroBatchFile(testCase)
+            fieldA = arrow.field("A", arrow.string());
+            fieldB = arrow.field("B", arrow.float32());
+            schema = arrow.schema([fieldA, fieldB]);
+            fname = fullfile(testCase.DataFolder, "ZeroBatchFile.arrow");
+            writer = arrow.io.ipc.RecordBatchFileWriter(fname, schema);
+            writer.close();
+            testCase.ZeroBatchFile = fname;
+        end
+
+        function setupOneBatchFile(testCase)
+            t = table(["Row1"; "Row2"], single([1; 2]), VariableNames=["A", "B"]);
+            recordBatch = arrow.recordBatch(t);
+            fname = fullfile(testCase.DataFolder, "OneBatchFile.arrow");
+            writer = arrow.io.ipc.RecordBatchFileWriter(fname, recordBatch.Schema);
+            writer.writeRecordBatch(recordBatch);
+            writer.close();
+            testCase.OneBatchFile = fname;
+        end
+
+        function setupMultipleBatchFile(testCase)
+            t1 = table(["Row1"; "Row2"], single([1; 2]), VariableNames=["A", "B"]);
+            t2 = table(["Row3"; "Row4"], single([3; 4]), VariableNames=["A", "B"]);
+            recordBatch1 = arrow.recordBatch(t1);
+            recordBatch2 = arrow.recordBatch(t2);
+            fname = fullfile(testCase.DataFolder, "MultipleBatchFile.arrow");
+            writer = arrow.io.ipc.RecordBatchFileWriter(fname, recordBatch1.Schema);
+            writer.writeRecordBatch(recordBatch1);
+            writer.writeRecordBatch(recordBatch2);
+            writer.close();
+            testCase.OneBatchFile = fname;
+        end
+    end
+
     methods (Test)
         function ZeroLengthFilenameError(testCase)
             fcn = @() arrow.io.ipc.RecordBatchFileReader("");
