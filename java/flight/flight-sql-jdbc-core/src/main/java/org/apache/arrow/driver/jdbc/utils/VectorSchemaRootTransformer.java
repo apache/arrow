0d9036f39ef8d9f878f97f2e6f4ef74f910d60ff
Dane Pitkin <dpitkin@apache.org>
2024-07-26 13:10:56 -0400

    Remove accidental import

--- java/flight/flight-sql-jdbc-core/src/main/java/org/apache/arrow/driver/jdbc/utils/VectorSchemaRootTransformer.java
+++ java/flight/flight-sql-jdbc-core/src/main/java/org/apache/arrow/driver/jdbc/utils/VectorSchemaRootTransformer.java
@@ -17,7 +17,6 @@
 package org.apache.arrow.driver.jdbc.utils;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.List;
 import org.apache.arrow.memory.BufferAllocator;
