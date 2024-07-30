a04a412d85099091e8569c6ffb9bd812de9dd708
Vibhatha Abeykoon <vibhatha@gmail.com>
2024-07-30 07:53:01 +0530

    fix: adding other test skeletons with todos

--- java/vector/src/test/java/org/apache/arrow/vector/testing/ValueVectorDataPopulator.java
+++ java/vector/src/test/java/org/apache/arrow/vector/testing/ValueVectorDataPopulator.java
@@ -60,6 +60,7 @@ import org.apache.arrow.vector.UInt8Vector;
 import org.apache.arrow.vector.VarBinaryVector;
 import org.apache.arrow.vector.VarCharVector;
 import org.apache.arrow.vector.VariableWidthFieldVector;
+import org.apache.arrow.vector.ViewVarCharVector;
 import org.apache.arrow.vector.complex.BaseRepeatedValueVector;
 import org.apache.arrow.vector.complex.BaseRepeatedValueViewVector;
 import org.apache.arrow.vector.complex.FixedSizeListVector;
@@ -567,6 +568,18 @@ public class ValueVectorDataPopulator {
     vector.setValueCount(length);
   }
 
+  /** Populate values for ViewVarCharVector. */
+  public static void setVector(ViewVarCharVector vector, byte[]... values) {
+    final int length = values.length;
+    vector.allocateNewSafe();
+    for (int i = 0; i < length; i++) {
+      if (values[i] != null) {
+        vector.set(i, values[i]);
+      }
+    }
+    vector.setValueCount(length);
+  }
+
   public static void setVector(VariableWidthFieldVector vector, byte[]... values) {
     final int length = values.length;
     vector.allocateNewSafe();
@@ -592,6 +605,18 @@ public class ValueVectorDataPopulator {
 
   /** Populate values for VarCharVector. */
   public static void setVector(VarCharVector vector, String... values) {
+    final int length = values.length;
+    vector.allocateNewSafe();
+    for (int i = 0; i < length; i++) {
+      if (values[i] != null) {
+        vector.setSafe(i, values[i].getBytes(StandardCharsets.UTF_8));
+      }
+    }
+    vector.setValueCount(length);
+  }
+
+  /** Populate values for VarCharVector. */
+  public static void setVector(ViewVarCharVector vector, String... values) {
     final int length = values.length;
     vector.allocateNewSafe();
     for (int i = 0; i < length; i++) {
