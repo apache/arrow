1f45c541d9b4a3fa21ad47f43b8a683f566a8b5e
Antoine Pitrou <antoine@python.org>
2024-08-06 16:20:33 +0200

    DRAFT: [CI] Use `docker compose` instead of `docker-compose`

--- dev/archery/archery/docker/core.py
+++ dev/archery/archery/docker/core.py
@@ -135,9 +135,9 @@ class ComposeConfig:
             compose = Docker()
             args = ['compose']
         else:
-            compose = Command('docker-compose')
+            compose = Command(compose_bin)
             args = []
-        args += ['--file', str(config_path), 'config']
+        args += [f'--file={config_path}', 'config']
         result = compose.run(*args, env=self.env, check=False,
                              stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
@@ -193,7 +193,7 @@ class DockerCompose(Command):
     def _execute_compose(self, *args, **kwargs):
         # execute as a docker compose command
         try:
-            result = super().run('--file', str(self.config.path), *args,
+            result = super().run(f'--file={self.config.path}', *args,
                                  env=self.config.env, **kwargs)
             result.check_returncode()
         except subprocess.CalledProcessError as e:
