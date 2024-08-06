2c1dfeff8375eae00a9f7f8967587eeb79741d7c
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-08-06 14:56:55 +0000

    Add pythoncapi_compat and use strong reference APIs

--- python/pyarrow/src/arrow/python/platform.h
+++ python/pyarrow/src/arrow/python/platform.h
@@ -24,7 +24,7 @@
 // to mean Py_ssize_t (defining this to suppress deprecation warning)
 #define PY_SSIZE_T_CLEAN
 
-#include <Python.h>  // IWYU pragma: export
+#include "arrow/python/pythoncapi_compat.h"  // IWYU pragma: export
 #include <datetime.h>
 
 // Work around C2528 error
--- python/pyarrow/src/arrow/python/python_to_arrow.cc
+++ python/pyarrow/src/arrow/python/python_to_arrow.cc
@@ -1107,19 +1107,13 @@ class PyStructConverter : public StructConverter<PyConverter, PyConverterTrait> 
   Status AppendDict(PyObject* dict, PyObject* field_names) {
     // NOTE we're ignoring any extraneous dict items
     for (int i = 0; i < num_fields_; i++) {
-      PyObject *name, *value;
-#ifdef Py_GIL_DISABLED
-      name = PyList_GetItemRef(field_names, i);
-      RETURN_IF_PYERROR();
-      PyDict_GetItemRef(dict, name, &value);
+      PyObject* name = PyList_GetItemRef(field_names, i);
       RETURN_IF_PYERROR();
       OwnedRef nameref(name);
-      OwnedRef valuered(value);
-#else
-      name = PyList_GET_ITEM(field_names, i);  // borrowed
-      value = PyDict_GetItem(dict, name);      // borrowed
+      PyObject* value;
+      PyDict_GetItemRef(dict, name, &value);
       RETURN_IF_PYERROR();
-#endif
+      OwnedRef valueref(value);
       RETURN_NOT_OK(this->children_[i]->Append(value ? value : Py_None));
     }
     return Status::OK();
@@ -1149,14 +1143,9 @@ class PyStructConverter : public StructConverter<PyConverter, PyConverterTrait> 
       ARROW_ASSIGN_OR_RAISE(auto pair, GetKeyValuePair(items, i));
 
       // validate that the key and the field name are equal
-      PyObject *name;
-#ifdef Py_GIL_DISABLED
-      name = PyList_GetItemRef(field_names, i);
+      PyObject* name = PyList_GetItemRef(field_names, i);
       RETURN_IF_PYERROR();
       OwnedRef nameref(name);
-#else
-      name = PyList_GET_ITEM(field_names, i);
-#endif
       bool are_equal = PyObject_RichCompareBool(pair.first, name, Py_EQ);
       RETURN_IF_PYERROR();
 
