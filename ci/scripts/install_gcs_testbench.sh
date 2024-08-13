00efbd9a9888ee10f191b0a3eb2f02d11d2555fc
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-08-13 07:48:53 +0000

    Debug python version

--- ci/scripts/install_gcs_testbench.sh
+++ ci/scripts/install_gcs_testbench.sh
@@ -47,6 +47,7 @@ fi
 # This script is run with PYTHON undefined in some places,
 # but those only use older pythons.
 if [[ -z "${PYTHON_VERSION}" || "${PYTHON_VERSION}" != "3.13" ]]; then
+  echo "Python version: ${PYTHON_VERSION:-notset}"
   python3 -m pip install \
     "https://github.com/googleapis/storage-testbench/archive/${version}.tar.gz"
 fi
