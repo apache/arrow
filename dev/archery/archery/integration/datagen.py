2328b6ee39b497d9f48e6d342db9f7d0c34d9791
Rok Mihevc <rok@mihevc.org>
2024-08-26 16:34:18 +0200

    GH-15058: [C++][Python] Native support for UUID (#37298)
    ### Rationale for this change
    
    See #15058.
    UUID datatype is common in throughout the ecosystem and Arrow as supporting it as a native type would reduce friction.
    
    ### What changes are included in this PR?
    
    This PR implements logic for Arrow canonical extension type in C++ and a Python wrapper.
    
    ### Are these changes tested?
    
    Yes.
    
    ### Are there any user-facing changes?
    
    Yes, new extension type is added.
    * Closes: #15058
    
    Authored-by: Rok Mihevc <rok@mihevc.org>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- dev/archery/archery/integration/datagen.py
+++ dev/archery/archery/integration/datagen.py
@@ -1845,7 +1845,7 @@ def generate_nested_dictionary_case():
 def generate_extension_case():
     dict0 = Dictionary(0, StringField('dictionary0'), size=5, name='DICT0')
 
-    uuid_type = ExtensionType('uuid', 'uuid-serialized',
+    uuid_type = ExtensionType('arrow.uuid', '',
                               FixedSizeBinaryField('', 16))
     dict_ext_type = ExtensionType(
         'dict-extension', 'dict-extension-serialized',
