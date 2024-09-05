3c1b2399dcdd09785163e50b16de47226f9d4763
Curt Hagenlocher <curt@hagenlocher.org>
2024-09-04 17:26:46 -0700

    Add support for decimal32 and decimal64 to C# implementation

--- csharp/test/Apache.Arrow.Tests/CDataInterfacePythonTests.cs
+++ csharp/test/Apache.Arrow.Tests/CDataInterfacePythonTests.cs
@@ -754,7 +754,7 @@ namespace Apache.Arrow.Tests
         public unsafe void RoundTripTestBatch()
         {
             // TODO: Enable these once this the version of pyarrow referenced during testing supports them
-            HashSet<ArrowTypeId> unsupported = new HashSet<ArrowTypeId> { ArrowTypeId.ListView, ArrowTypeId.BinaryView, ArrowTypeId.StringView };
+            HashSet<ArrowTypeId> unsupported = new HashSet<ArrowTypeId> { ArrowTypeId.ListView, ArrowTypeId.BinaryView, ArrowTypeId.StringView, ArrowTypeId.Decimal32, ArrowTypeId.Decimal64 };
             RecordBatch batch1 = TestData.CreateSampleRecordBatch(4, excludedTypes: unsupported);
             RecordBatch batch2 = batch1.Clone();
 
