e3977eefe9d514544ce431a162f6614b26157d68
Sutou Kouhei <kou@clear-code.com>
2024-09-09 09:09:08 +0900

    Don't use temporary file

--- ruby/red-parquet/test/test-arrow-file-writer.rb
+++ ruby/red-parquet/test/test-arrow-file-writer.rb
@@ -16,53 +16,61 @@
 # under the License.
 
 class TestArrowFileWriter < Test::Unit::TestCase
+  def open_buffer_output_stream
+    buffer = Arrow::ResizableBuffer.new(4096)
+    Arrow::BufferOutputStream.open(buffer) do |output|
+      yield(output)
+    end
+    buffer
+  end
+
   sub_test_case("#write") do
     test("RecordBatch") do
       schema = Arrow::Schema.new(visible: :boolean)
       record_batch = Arrow::RecordBatch.new(schema, [[true], [false]])
-      Tempfile.create(["red-parquet", ".parquet"]) do |file|
-        Parquet::ArrowFileWriter.open(record_batch.schema, file.path) do |writer|
+      buffer = open_buffer_output_stream do |output|
+        Parquet::ArrowFileWriter.open(record_batch.schema, output) do |writer|
           writer.write(record_batch)
         end
-        assert_equal(record_batch.to_table,
-                     Arrow::Table.load(file.path))
       end
+      assert_equal(record_batch.to_table,
+                   Arrow::Table.load(buffer, format: :parquet))
     end
 
     test("Table") do
       schema = Arrow::Schema.new(visible: :boolean)
       table = Arrow::Table.new(schema, [[true], [false]])
-      Tempfile.create(["red-parquet", ".parquet"]) do |file|
-        Parquet::ArrowFileWriter.open(table.schema, file.path) do |writer|
+      buffer = open_buffer_output_stream do |output|
+        Parquet::ArrowFileWriter.open(table.schema, output) do |writer|
           writer.write(table)
         end
-        assert_equal(table,
-                     Arrow::Table.load(file.path))
       end
+      assert_equal(table,
+                   Arrow::Table.load(buffer, format: :parquet))
     end
 
     test("[[]]") do
       schema = Arrow::Schema.new(visible: :boolean)
       raw_records = [[true], [false]]
-      Tempfile.create(["red-parquet", ".parquet"]) do |file|
-        Parquet::ArrowFileWriter.open(schema, file.path) do |writer|
+      buffer = open_buffer_output_stream do |output|
+        Parquet::ArrowFileWriter.open(schema, output) do |writer|
           writer.write(raw_records)
         end
-        assert_equal(Arrow::RecordBatch.new(schema, raw_records).to_table,
-                     Arrow::Table.load(file.path))
       end
+      assert_equal(Arrow::RecordBatch.new(schema, raw_records).to_table,
+                   Arrow::Table.load(buffer, format: :parquet))
     end
 
     test("[{}]") do
       schema = Arrow::Schema.new(visible: :boolean)
       raw_columns = [visible: [true, false]]
-      Tempfile.create(["red-parquet", ".parquet"]) do |file|
-        Parquet::ArrowFileWriter.open(schema, file.path) do |writer|
+      buffer = open_buffer_output_stream do |output|
+        Parquet::ArrowFileWriter.open(schema, output) do |writer|
           writer.write(raw_columns)
         end
-        assert_equal(Arrow::RecordBatch.new(schema, raw_columns).to_table,
-                     Arrow::Table.load(file.path))
       end
+      assert_equal(Arrow::RecordBatch.new(schema, raw_columns).to_table,
+                   Arrow::Table.load(buffer, format: :parquet))
     end
   end
 end
