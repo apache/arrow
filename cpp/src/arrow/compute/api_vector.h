797bd94251970ae93bc9d7537c1302e7e1146d06
Ruoxi Sun <zanmato1984@gmail.com>
2024-10-11 16:39:20 +0800

    Refine docs

--- cpp/src/arrow/compute/api_vector.h
+++ cpp/src/arrow/compute/api_vector.h
@@ -270,7 +270,7 @@ class ARROW_EXPORT ReverseIndicesOptions : public FunctionOptions {
   /// length will be ignored.
   int64_t output_length = -1;
   /// \brief The type of the output reverse indices. If null, the output will be of the
-  /// same type as the input indices, otherwise must be integer types. An overflow error
+  /// same type as the input indices, otherwise must be integer types. An invalid error
   /// will be reported if this type is not able to store the length of the input indices.
   std::shared_ptr<DataType> output_type = nullptr;
 };
