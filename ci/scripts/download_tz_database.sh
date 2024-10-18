a753b3b3d4b444080230322563adf528ee6a9544
Sutou Kouhei <kou@clear-code.com>
2024-10-18 11:43:03 +0900

    Debug

--- ci/scripts/download_tz_database.sh
+++ ci/scripts/download_tz_database.sh
@@ -24,7 +24,8 @@ curl https://data.iana.org/time-zones/releases/tzdata2024b.tar.gz --output ~/Dow
 
 # Extract
 mkdir -p ~/Downloads/tzdata
-tar --extract --file ~/Downloads/tzdata2024b.tar.gz --directory ~/Downloads/tzdata
+ls -lah ~/Downloads/tzdata
+tar --extract --file ~/Downloads/tzdata2024b.tar.gz --directory ~/Downloads/tzdata --verbose
 
 # Download Windows timezone mapping
 curl https://raw.githubusercontent.com/unicode-org/cldr/master/common/supplemental/windowsZones.xml --output ~/Downloads/tzdata/windowsZones.xml
