7a715659622a6582c15b09756f391de989a729aa
Taylan O. Toygarlar <taylan.toygarlar@imc.com>
2024-11-18 09:17:24 +0100

    [Python][Parquet] ParquetDataset should expose partition_base_dir (#44765)

--- python/pyarrow/parquet/core.py
+++ python/pyarrow/parquet/core.py
@@ -1169,7 +1169,13 @@ default "hive"
     assumes directory names with key=value pairs like "/year=2009/month=11".
     In addition, a scheme like "/2009/11" is also supported, in which case
     you need to specify the field names or a full schema. See the
-    ``pyarrow.dataset.partitioning()`` function for more details."""
+    ``pyarrow.dataset.partitioning()`` function for more details.
+partition_base_dir : str, optional
+        For the purposes of applying the partitioning, paths will be
+        stripped of the partition_base_dir. Files not matching the
+        partition_base_dir prefix will be skipped for partitioning discovery.
+        The ignored files will still be part of the Dataset, but will not
+        have partition information."""
 
 
 _parquet_dataset_example = """\
