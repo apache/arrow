b8f57532dcfbec89057e62554cf518f3b4bc9bb6
Antoine Pitrou <antoine@python.org>
2024-12-04 17:41:15 +0100

    Revert mirror removal

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -697,7 +697,9 @@ if(DEFINED ENV{ARROW_GTEST_URL})
   set(GTEST_SOURCE_URL "$ENV{ARROW_GTEST_URL}")
 else()
   set_urls(GTEST_SOURCE_URL
-           "https://github.com/google/googletest/archive/v${ARROW_GTEST_BUILD_VERSION}.tar.gz")
+           "https://github.com/google/googletest/archive/v${ARROW_GTEST_BUILD_VERSION}.tar.gz"
+           "https://chromium.googlesource.com/external/github.com/google/googletest/+archive/release-${ARROW_GTEST_BUILD_VERSION}.tar.gz"
+           "${THIRDPARTY_MIRROR_URL}/gtest-${ARROW_GTEST_BUILD_VERSION}.tar.gz")
 endif()
 
 if(DEFINED ENV{ARROW_JEMALLOC_URL})
