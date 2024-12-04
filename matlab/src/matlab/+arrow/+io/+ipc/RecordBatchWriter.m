ac2c18aec5eb714b6c606106c6b62da5eb1ad01c
Sarah Gilmore <74676073+sgilmore10@users.noreply.github.com>
2024-12-04 10:38:56 -0500

    Update matlab/src/matlab/+arrow/+io/+ipc/RecordBatchWriter.m
    Co-authored-by: Kevin Gurney <kevin.p.gurney@gmail.com>

--- matlab/src/matlab/+arrow/+io/+ipc/RecordBatchWriter.m
+++ matlab/src/matlab/+arrow/+io/+ipc/RecordBatchWriter.m
@@ -16,7 +16,6 @@
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
 
-
 classdef (Abstract) RecordBatchWriter < matlab.mixin.Scalar
 
     properties(SetAccess=private, GetAccess=public, Hidden)
