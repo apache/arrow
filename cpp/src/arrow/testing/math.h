e747af65e1c217371a355ea3117e7b16a36afb2d
Antoine Pitrou <antoine@python.org>
2024-12-09 14:45:04 +0100

    MINOR: [C++] Make argument name consistent (#44973)
    Followup to https://github.com/apache/arrow/pull/44955
    
    Authored-by: Antoine Pitrou <antoine@python.org>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/src/arrow/testing/math.h
+++ cpp/src/arrow/testing/math.h
@@ -22,9 +22,9 @@
 namespace arrow {
 
 ARROW_TESTING_EXPORT
-bool WithinUlp(float left, float right, int n_ulp);
+bool WithinUlp(float left, float right, int n_ulps);
 ARROW_TESTING_EXPORT
-bool WithinUlp(double left, double right, int n_ulp);
+bool WithinUlp(double left, double right, int n_ulps);
 
 ARROW_TESTING_EXPORT
 void AssertWithinUlp(float left, float right, int n_ulps);
