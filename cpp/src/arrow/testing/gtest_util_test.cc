0d8c8d9581cc5c2a21881bb8665a634fef07433a
Antoine Pitrou <antoine@python.org>
2024-12-09 12:08:29 +0100

    GH-44915: [C++] Add WithinUlp testing functions (#44906)
    ### Rationale for this change
    
    When testing math-related functions, we might want to check that some results are very close to an expected value, but not necessarily exactly equal.
    
    ### What changes are included in this PR?
    
    Add functions that test whether two floating-point values are within N ulps.
    
    ("ulp" stands for "unit in the last place": https://en.wikipedia.org/wiki/Unit_in_the_last_place)
    
    ### Are these changes tested?
    
    Yes.
    
    ### Are there any user-facing changes?
    
    Potentially more useful error messages.
    
    * GitHub Issue: #44915
    
    Authored-by: Antoine Pitrou <antoine@python.org>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/src/arrow/testing/gtest_util_test.cc
+++ cpp/src/arrow/testing/gtest_util_test.cc
@@ -15,6 +15,9 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <cmath>
+
+#include <gtest/gtest-spi.h>
 #include <gtest/gtest.h>
 
 #include "arrow/array.h"
@@ -23,6 +26,7 @@
 #include "arrow/record_batch.h"
 #include "arrow/tensor.h"
 #include "arrow/testing/gtest_util.h"
+#include "arrow/testing/math.h"
 #include "arrow/testing/random.h"
 #include "arrow/type.h"
 #include "arrow/type_traits.h"
@@ -169,6 +173,112 @@ TEST_F(TestTensorFromJSON, FromJSON) {
       "[9, 2]");
 
   EXPECT_TRUE(tensor_expected->Equals(*result));
+}
+
+template <typename Float>
+void CheckWithinUlpSingle(Float x, Float y, int n_ulp) {
+  ARROW_SCOPED_TRACE("x = ", x, ", y = ", y, ", n_ulp = ", n_ulp);
+  ASSERT_TRUE(WithinUlp(x, y, n_ulp));
+}
+
+template <typename Float>
+void CheckNotWithinUlpSingle(Float x, Float y, int n_ulp) {
+  ARROW_SCOPED_TRACE("x = ", x, ", y = ", y, ", n_ulp = ", n_ulp);
+  ASSERT_FALSE(WithinUlp(x, y, n_ulp));
+}
+
+template <typename Float>
+void CheckWithinUlp(Float x, Float y, int n_ulp) {
+  CheckWithinUlpSingle(x, y, n_ulp);
+  CheckWithinUlpSingle(y, x, n_ulp);
+  CheckWithinUlpSingle(x, y, n_ulp + 1);
+  CheckWithinUlpSingle(y, x, n_ulp + 1);
+  CheckWithinUlpSingle(-x, -y, n_ulp);
+  CheckWithinUlpSingle(-y, -x, n_ulp);
+
+  for (int exp : {1, -1, 10, -10}) {
+    Float x_scaled = std::ldexp(x, exp);
+    Float y_scaled = std::ldexp(y, exp);
+    CheckWithinUlpSingle(x_scaled, y_scaled, n_ulp);
+    CheckWithinUlpSingle(y_scaled, x_scaled, n_ulp);
+  }
+}
+
+template <typename Float>
+void CheckNotWithinUlp(Float x, Float y, int n_ulp) {
+  CheckNotWithinUlpSingle(x, y, n_ulp);
+  CheckNotWithinUlpSingle(y, x, n_ulp);
+  CheckNotWithinUlpSingle(-x, -y, n_ulp);
+  CheckNotWithinUlpSingle(-y, -x, n_ulp);
+  if (n_ulp > 1) {
+    CheckNotWithinUlpSingle(x, y, n_ulp - 1);
+    CheckNotWithinUlpSingle(y, x, n_ulp - 1);
+    CheckNotWithinUlpSingle(-x, -y, n_ulp - 1);
+    CheckNotWithinUlpSingle(-y, -x, n_ulp - 1);
+  }
+
+  for (int exp : {1, -1, 10, -10}) {
+    Float x_scaled = std::ldexp(x, exp);
+    Float y_scaled = std::ldexp(y, exp);
+    CheckNotWithinUlpSingle(x_scaled, y_scaled, n_ulp);
+    CheckNotWithinUlpSingle(y_scaled, x_scaled, n_ulp);
+  }
+}
+
+TEST(TestWithinUlp, Double) {
+  for (double f : {0.0, 1e-20, 1.0, 2345678.9}) {
+    CheckWithinUlp(f, f, 1);
+    CheckWithinUlp(f, f, 42);
+  }
+  CheckWithinUlp(-0.0, 0.0, 1);
+  CheckWithinUlp(1.0, 1.0000000000000002, 1);
+  CheckWithinUlp(1.0, 1.0000000000000007, 3);
+  CheckNotWithinUlp(1.0, 1.0000000000000007, 2);
+  CheckNotWithinUlp(1.0, 1.0000000000000007, 1);
+  // left and right have a different exponent but are still very close
+  CheckWithinUlp(1.0, 0.9999999999999999, 1);
+  CheckWithinUlp(1.0, 0.9999999999999988, 11);
+  CheckNotWithinUlp(1.0, 0.9999999999999988, 10);
+
+  CheckWithinUlp(123.4567, 123.45670000000015, 11);
+  CheckNotWithinUlp(123.4567, 123.45670000000015, 10);
+
+  CheckNotWithinUlp(HUGE_VAL, -HUGE_VAL, 10);
+  CheckNotWithinUlp(12.34, -HUGE_VAL, 10);
+  CheckNotWithinUlp(12.34, std::nan(""), 10);
+  CheckNotWithinUlp(12.34, -12.34, 10);
+  CheckNotWithinUlp(0.0, 1e-20, 10);
+}
+
+TEST(TestWithinUlp, Float) {
+  for (float f : {0.0f, 1e-8f, 1.0f, 123.456f}) {
+    CheckWithinUlp(f, f, 1);
+    CheckWithinUlp(f, f, 42);
+  }
+  CheckWithinUlp(-0.0f, 0.0f, 1);
+  CheckWithinUlp(1.0f, 1.0000001f, 1);
+  CheckWithinUlp(1.0f, 1.0000013f, 11);
+  CheckNotWithinUlp(1.0f, 1.0000013f, 10);
+  // left and right have a different exponent but are still very close
+  CheckWithinUlp(1.0f, 0.99999994f, 1);
+  CheckWithinUlp(1.0f, 0.99999934f, 11);
+  CheckNotWithinUlp(1.0f, 0.99999934f, 10);
+
+  CheckWithinUlp(123.456f, 123.456085f, 11);
+  CheckNotWithinUlp(123.456f, 123.456085f, 10);
+
+  CheckNotWithinUlp(HUGE_VALF, -HUGE_VALF, 10);
+  CheckNotWithinUlp(12.34f, -HUGE_VALF, 10);
+  CheckNotWithinUlp(12.34f, std::nanf(""), 10);
+  CheckNotWithinUlp(12.34f, -12.34f, 10);
+}
+
+TEST(AssertTestWithinUlp, Basics) {
+  AssertWithinUlp(123.4567, 123.45670000000015, 11);
+  AssertWithinUlp(123.456f, 123.456085f, 11);
+  EXPECT_FATAL_FAILURE(AssertWithinUlp(123.4567, 123.45670000000015, 10),
+                       "not within 10 ulps");
+  EXPECT_FATAL_FAILURE(AssertWithinUlp(123.456f, 123.456085f, 10), "not within 10 ulps");
 }
 
 }  // namespace arrow
