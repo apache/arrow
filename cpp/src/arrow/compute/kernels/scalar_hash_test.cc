73361e120ec868df5461dbfaedb743fe8652b190
Krisztian Szucs <szucs.krisztian@gmail.com>
2024-12-16 12:09:45 +0100

    add tests for hash32, dictionary encoded type, extension type

--- cpp/src/arrow/compute/kernels/scalar_hash_test.cc
+++ cpp/src/arrow/compute/kernels/scalar_hash_test.cc
@@ -24,6 +24,7 @@
 #include "arrow/compute/util.h"
 #include "arrow/result.h"
 #include "arrow/status.h"
+#include "arrow/testing/extension_type.h"
 #include "arrow/testing/gtest_util.h"
 #include "arrow/testing/matchers.h"
 #include "arrow/testing/random.h"
@@ -35,7 +36,7 @@ namespace arrow {
 namespace compute {
 
 constexpr auto kSeed = 0x94378165;
-constexpr auto array_lengths = {0, 10, 100};
+constexpr auto array_lengths = {0, 50, 100};
 constexpr auto null_probabilities = {0.0, 0.5, 1.0};
 
 class TestScalarHash : public ::testing::Test {
@@ -45,7 +46,7 @@ class TestScalarHash : public ::testing::Test {
                          std::vector<c_type> exp) {
     auto res_array = res.array();
     for (int64_t val_ndx = 0; val_ndx < arr->length(); ++val_ndx) {
-      uint64_t actual_hash = res_array->GetValues<uint64_t>(1)[val_ndx];
+      c_type actual_hash = res_array->GetValues<c_type>(1)[val_ndx];
       if (arr->IsNull(val_ndx)) {
         ASSERT_EQ(0, actual_hash);
       } else {
@@ -54,104 +55,181 @@ class TestScalarHash : public ::testing::Test {
     }
   }
 
-  std::vector<uint64_t> HashPrimitive(const std::shared_ptr<Array>& arr) {
-    std::vector<uint64_t> hashes(arr->length());
-    Hashing64::HashFixed(false, static_cast<uint32_t>(arr->length()),
-                         arr->type()->bit_width() / 8, arr->data()->GetValues<uint8_t>(1),
-                         hashes.data());
+  template <typename c_type>
+  std::vector<c_type> HashPrimitive(const std::shared_ptr<Array>& arr) {
+    std::vector<c_type> hashes(arr->length());
+    // Choose the Hasher type conditionally based on c_type
+
+    if constexpr (std::is_same<c_type, uint64_t>::value) {
+      Hashing64::HashFixed(false, static_cast<uint32_t>(arr->length()),
+                           arr->type()->bit_width() / 8,
+                           arr->data()->GetValues<uint8_t>(1), hashes.data());
+    } else {
+      Hashing32::HashFixed(::arrow::internal::CpuInfo::GetInstance()->hardware_flags(),
+                           false, static_cast<uint32_t>(arr->length()),
+                           arr->type()->bit_width() / 8,
+                           arr->data()->GetValues<uint8_t>(1), hashes.data(), nullptr);
+    }
+
     return hashes;
   }
 
-  std::vector<uint64_t> HashBinaryLike(const std::shared_ptr<Array>& arr) {
-    std::vector<uint64_t> hashes(arr->length());
-    if (arr->type_id() == Type::LARGE_BINARY || arr->type_id() == Type::LARGE_STRING) {
-      Hashing64::HashVarLen(false, static_cast<uint32_t>(arr->length()),
-                            arr->data()->GetValues<uint64_t>(1),
-                            arr->data()->GetValues<uint8_t>(2), hashes.data());
+  template <typename c_type>
+  std::vector<c_type> HashBinaryLike(const std::shared_ptr<Array>& arr) {
+    std::vector<c_type> hashes(arr->length());
+    auto length = static_cast<uint32_t>(arr->length());
+    auto values = arr->data()->GetValues<uint8_t>(2);
+    if constexpr (std::is_same<c_type, uint64_t>::value) {
+      if (arr->type_id() == Type::LARGE_BINARY || arr->type_id() == Type::LARGE_STRING) {
+        Hashing64::HashVarLen(false, length, arr->data()->GetValues<uint64_t>(1), values,
+                              hashes.data());
+      } else {
+        Hashing64::HashVarLen(false, length, arr->data()->GetValues<uint32_t>(1), values,
+                              hashes.data());
+      }
     } else {
-      Hashing64::HashVarLen(false, static_cast<uint32_t>(arr->length()),
-                            arr->data()->GetValues<uint32_t>(1),
-                            arr->data()->GetValues<uint8_t>(2), hashes.data());
+      auto hw_flags = ::arrow::internal::CpuInfo::GetInstance()->hardware_flags();
+      if (arr->type_id() == Type::LARGE_BINARY || arr->type_id() == Type::LARGE_STRING) {
+        Hashing32::HashVarLen(hw_flags, false, length,
+                              arr->data()->GetValues<uint64_t>(1), values, hashes.data(),
+                              nullptr);
+      } else {
+        Hashing32::HashVarLen(hw_flags, false, length,
+                              arr->data()->GetValues<uint32_t>(1), values, hashes.data(),
+                              nullptr);
+      }
     }
     return hashes;
   }
 
-  void CheckDeterminisic(const std::shared_ptr<Array>& arr) {
+  void CheckDeterminisic(const std::string& func, const std::shared_ptr<Array>& arr) {
     // Check that the hash is deterministic
-    ASSERT_OK_AND_ASSIGN(Datum res1, CallFunction("hash64", {arr}));
-    ASSERT_OK_AND_ASSIGN(Datum res2, CallFunction("hash64", {arr}));
+    ASSERT_OK_AND_ASSIGN(Datum res1, CallFunction(func, {arr}));
+    ASSERT_OK_AND_ASSIGN(Datum res2, CallFunction(func, {arr}));
     ASSERT_EQ(res1.length(), arr->length());
-    ASSERT_EQ(res2.type()->id(), Type::UINT64);
-    AssertDatumsEqual(res1, res2);
+    if (func == "hash64") {
+      ASSERT_EQ(res1.type()->id(), Type::UINT64);
+    } else if (func == "hash32") {
+      ASSERT_EQ(res1.type()->id(), Type::UINT32);
+    } else {
+      FAIL() << "Unknown function: " << func;
+    }
+    AssertDatumsEqual(res1, res2, true);
   }
 
-  void CheckPrimitive(const std::shared_ptr<Array>& arr) {
-    ASSERT_OK_AND_ASSIGN(Datum hash_result, CallFunction("hash64", {arr}));
-    AssertHashesEqual<uint64_t>(arr, hash_result, HashPrimitive(arr));
-    CheckDeterminisic(arr);
+  void CheckPrimitive(const std::string& func, const std::shared_ptr<Array>& arr) {
+    ASSERT_OK_AND_ASSIGN(Datum hash_result, CallFunction(func, {arr}));
+    CheckDeterminisic(func, arr);
+    if (func == "hash64") {
+      AssertHashesEqual<uint64_t>(arr, hash_result, HashPrimitive<uint64_t>(arr));
+    } else if (func == "hash32") {
+      AssertHashesEqual<uint32_t>(arr, hash_result, HashPrimitive<uint32_t>(arr));
+    } else {
+      FAIL() << "Unknown function: " << func;
+    }
   }
 
-  void CheckBinary(const std::shared_ptr<Array>& arr) {
-    ASSERT_OK_AND_ASSIGN(Datum hash_result, CallFunction("hash64", {arr}));
-    AssertHashesEqual<uint64_t>(arr, hash_result, HashBinaryLike(arr));
-    CheckDeterminisic(arr);
+  void CheckBinary(const std::string& func, const std::shared_ptr<Array>& arr) {
+    ASSERT_OK_AND_ASSIGN(Datum hash_result, CallFunction(func, {arr}));
+    CheckDeterminisic(func, arr);
+    if (func == "hash64") {
+      AssertHashesEqual<uint64_t>(arr, hash_result, HashBinaryLike<uint64_t>(arr));
+    } else if (func == "hash32") {
+      AssertHashesEqual<uint32_t>(arr, hash_result, HashBinaryLike<uint32_t>(arr));
+    } else {
+      FAIL() << "Unknown function: " << func;
+    }
   }
 };
 
-// TODO(kszucs): test bool, fixed size binary, dictionary encoded
-
 TEST_F(TestScalarHash, Null) {
   Datum res;
+  std::shared_ptr<Array> arr;
+  std::shared_ptr<Array> exp;
 
-  auto arr = ArrayFromJSON(null(), R"([])");
-  auto exp = ArrayFromJSON(uint64(), "[]");
+  arr = ArrayFromJSON(null(), R"([])");
+  exp = ArrayFromJSON(uint32(), "[]");
+  ASSERT_OK_AND_ASSIGN(res, CallFunction("hash32", {arr}));
+  AssertArraysEqual(*res.make_array(), *exp);
+  CheckDeterminisic("hash32", arr);
+
+  arr = ArrayFromJSON(null(), R"([])");
+  exp = ArrayFromJSON(uint64(), "[]");
   ASSERT_OK_AND_ASSIGN(res, CallFunction("hash64", {arr}));
   AssertArraysEqual(*res.make_array(), *exp);
-  CheckDeterminisic(arr);
+  CheckDeterminisic("hash64", arr);
+
+  arr = ArrayFromJSON(null(), R"([null, null, null])");
+  exp = ArrayFromJSON(uint32(), "[0, 0, 0]");
+  ASSERT_OK_AND_ASSIGN(res, CallFunction("hash32", {arr}));
+  AssertArraysEqual(*res.make_array(), *exp);
+  CheckDeterminisic("hash32", arr);
 
   arr = ArrayFromJSON(null(), R"([null, null, null])");
   exp = ArrayFromJSON(uint64(), "[0, 0, 0]");
   ASSERT_OK_AND_ASSIGN(res, CallFunction("hash64", {arr}));
   AssertArraysEqual(*res.make_array(), *exp);
-  CheckDeterminisic(arr);
+  CheckDeterminisic("hash64", arr);
 }
 
 TEST_F(TestScalarHash, Boolean) {
   auto arr = ArrayFromJSON(boolean(), R"([true, false, null, true, null])");
-  CheckDeterminisic(arr);
+  CheckDeterminisic("hash32", arr);
+  CheckDeterminisic("hash64", arr);
 }
 
 TEST_F(TestScalarHash, NumericLike) {
   auto types = {int8(),   int16(),  int32(),  int64(),   uint8(),
                 uint16(), uint32(), uint64(), float32(), float64()};
-  for (auto type : types) {
-    CheckPrimitive(ArrayFromJSON(type, R"([])"));
-    CheckPrimitive(ArrayFromJSON(type, R"([null])"));
-    CheckPrimitive(ArrayFromJSON(type, R"([1])"));
-    CheckPrimitive(ArrayFromJSON(type, R"([1, 2, null])"));
-    CheckPrimitive(ArrayFromJSON(type, R"([null, 2, 3])"));
-    CheckPrimitive(ArrayFromJSON(type, R"([1, 2, 3, 4])"));
+  for (auto func : {"hash32", "hash64"}) {
+    for (auto type : types) {
+      CheckPrimitive(func, ArrayFromJSON(type, R"([])"));
+      CheckPrimitive(func, ArrayFromJSON(type, R"([null])"));
+      CheckPrimitive(func, ArrayFromJSON(type, R"([1])"));
+      CheckPrimitive(func, ArrayFromJSON(type, R"([1, 2, null])"));
+      CheckPrimitive(func, ArrayFromJSON(type, R"([null, 2, 3])"));
+      CheckPrimitive(func, ArrayFromJSON(type, R"([1, 2, 3, 4])"));
+    }
   }
 }
 
 TEST_F(TestScalarHash, BinaryLike) {
   auto types = {binary(), utf8(), large_binary(), large_utf8()};
-  for (auto type : types) {
-    CheckBinary(ArrayFromJSON(type, R"([])"));
-    CheckBinary(ArrayFromJSON(type, R"([null])"));
-    CheckBinary(ArrayFromJSON(type, R"([""])"));
-    CheckBinary(ArrayFromJSON(type, R"(["first", "second", null])"));
-    CheckBinary(ArrayFromJSON(type, R"(["first", "second", "third"])"));
+  for (auto func : {"hash32", "hash64"}) {
+    for (auto type : types) {
+      CheckBinary(func, ArrayFromJSON(type, R"([])"));
+      CheckBinary(func, ArrayFromJSON(type, R"([null])"));
+      CheckBinary(func, ArrayFromJSON(type, R"([""])"));
+      CheckBinary(func, ArrayFromJSON(type, R"(["first", "second", null])"));
+      CheckBinary(func, ArrayFromJSON(type, R"(["first", "second", "third"])"));
+    }
   }
+}
+
+TEST_F(TestScalarHash, ExtensionType) {
+  auto storage = ArrayFromJSON(int16(), R"([1, 2, 3, 4, null])");
+  auto extension = ExtensionType::WrapArray(smallint(), storage);
+  CheckPrimitive("hash32", extension);
+  CheckPrimitive("hash64", extension);
+}
+
+TEST_F(TestScalarHash, DictionaryType) {
+  auto dict_type = dictionary(int8(), utf8());
+  auto dict = DictArrayFromJSON(dict_type, "[1, 2, null, 3, 0]",
+                                "[\"A0\", \"A1\", \"C2\", \"C3\"]");
+  CheckPrimitive("hash32", dict);
+  CheckPrimitive("hash64", dict);
 }
 
 TEST_F(TestScalarHash, RandomBinaryLike) {
   auto rand = random::RandomArrayGenerator(kSeed);
   auto types = {binary(), utf8(), large_binary(), large_utf8()};
-  for (auto type : types) {
-    for (auto length : array_lengths) {
-      for (auto null_probability : null_probabilities) {
-        CheckBinary(rand.ArrayOf(type, length, null_probability));
+  for (auto func : {"hash32", "hash64"}) {
+    for (auto type : types) {
+      for (auto length : array_lengths) {
+        for (auto null_probability : null_probabilities) {
+          CheckBinary(func, rand.ArrayOf(type, length, null_probability));
+        }
       }
     }
   }
@@ -161,10 +239,12 @@ TEST_F(TestScalarHash, RandomNumericLike) {
   auto rand = random::RandomArrayGenerator(kSeed);
   auto types = {int8(),   int16(),  int32(),  int64(),   uint8(),
                 uint16(), uint32(), uint64(), float32(), float64()};
-  for (auto type : types) {
-    for (auto length : array_lengths) {
-      for (auto null_probability : null_probabilities) {
-        CheckPrimitive(rand.ArrayOf(type, length, null_probability));
+  for (auto func : {"hash32", "hash64"}) {
+    for (auto type : types) {
+      for (auto length : array_lengths) {
+        for (auto null_probability : null_probabilities) {
+          CheckPrimitive(func, rand.ArrayOf(type, length, null_probability));
+        }
       }
     }
   }
@@ -180,6 +260,7 @@ TEST_F(TestScalarHash, RandomNested) {
       large_list(int64()),
       large_list(utf8()),
       large_list(large_binary()),
+      list(boolean()),
       list(list(int16())),
       list(list(list(uint8()))),
       fixed_size_list(int32(), 3),
@@ -187,13 +268,15 @@ TEST_F(TestScalarHash, RandomNested) {
       map(int32(), utf8()),
       map(utf8(), list(int16())),
       struct_({field("f0", int32()), field("f1", utf8())}),
-      struct_({field("f0", list(int32())), field("f1", large_binary())}),
+      struct_({field("f0", list(int32()))}),
       struct_({field("f0", struct_({field("f0", int32()), field("f1", utf8())}))}),
   };
   for (auto type : types) {
     for (auto length : array_lengths) {
       for (auto null_probability : null_probabilities) {
-        CheckDeterminisic(rand.ArrayOf(type, length, null_probability));
+        auto arr = rand.ArrayOf(type, length, null_probability);
+        CheckDeterminisic("hash32", arr);
+        CheckDeterminisic("hash64", arr);
       }
     }
   }
