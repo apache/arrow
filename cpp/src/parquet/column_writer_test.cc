1c3ae5c9cd443f771cf9abd6c1b106cb590d9d45
mwish <maplewish117@gmail.com>
2025-01-14 21:10:20 +0800

    test all compressions

--- cpp/src/parquet/column_writer_test.cc
+++ cpp/src/parquet/column_writer_test.cc
@@ -1774,28 +1774,34 @@ TEST_F(TestInt32Writer, WriteKeyValueMetadataEndToEnd) {
   ASSERT_EQ("bar", value);
 }
 
-TEST_F(TestValuesWriterInt32Type, AllNullsInPageV2) {
-#ifndef ARROW_WITH_SNAPPY
-  GTEST_SKIP() << "Test requires snappy compression";
-#endif
-  // Optional and non-repeated, with definition levels
-  // but no repetition levels
-  this->SetUpSchema(Repetition::OPTIONAL);
-  this->GenerateData(SMALL_SIZE);
-  std::fill(this->def_levels_.begin(), this->def_levels_.end(), 0);
-  ColumnProperties column_properties;
-  column_properties.set_compression(Compression::SNAPPY);
+TEST_F(TestValuesWriterInt32Type, AllNullsCompressionInPageV2) {
+  std::vector<Compression::type> compressions = {Compression::SNAPPY, Compression::GZIP,
+                                                 Compression::ZSTD, Compression::BROTLI,
+                                                 Compression::LZ4};
+  for (auto compression : compressions) {
+    if (!Codec::IsAvailable(compression)) {
+      continue;
+    }
+    SCOPED_TRACE(compression);
+    // Optional and non-repeated, with definition levels
+    // but no repetition levels
+    this->SetUpSchema(Repetition::OPTIONAL);
+    this->GenerateData(SMALL_SIZE);
+    std::fill(this->def_levels_.begin(), this->def_levels_.end(), 0);
+    ColumnProperties column_properties;
+    column_properties.set_compression(compression);
 
-  auto writer =
-      this->BuildWriter(SMALL_SIZE, column_properties, ParquetVersion::PARQUET_2_LATEST,
-                        ParquetDataPageVersion::V2);
-  writer->WriteBatch(this->values_.size(), this->def_levels_.data(), nullptr,
-                     this->values_ptr_);
-  writer->Close();
+    auto writer =
+        this->BuildWriter(SMALL_SIZE, column_properties, ParquetVersion::PARQUET_2_LATEST,
+                          ParquetDataPageVersion::V2);
+    writer->WriteBatch(this->values_.size(), this->def_levels_.data(), nullptr,
+                       this->values_ptr_);
+    writer->Close();
 
-  ASSERT_EQ(100, this->metadata_num_values());
-  this->ReadColumn(Compression::SNAPPY);
-  ASSERT_EQ(0, this->values_read_);
+    ASSERT_EQ(100, this->metadata_num_values());
+    this->ReadColumn(compression);
+    ASSERT_EQ(0, this->values_read_);
+  }
 }
 
 }  // namespace test
