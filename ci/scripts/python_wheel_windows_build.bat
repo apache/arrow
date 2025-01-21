9308397ee5b1b6b8975d12b3e2983c9fe6b31416
anubhav <anubhavkoul01@gmail.com>
2025-01-21 21:10:20 +0530

    Updated delvewheel install command and updated flags used with delvewheel repair

--- ci/scripts/python_wheel_windows_build.bat
+++ ci/scripts/python_wheel_windows_build.bat
@@ -144,16 +144,13 @@ cp C:\Windows\System32\msvcp140.dll pyarrow\
 @REM Since we bundled the Arrow C++ libraries ourselves, we only need to
 @REM mangle msvcp140.dll so as to avoid ABI issues when msvcp140.dll is
 @REM required by multiple Python libraries in the same process.
-@REM
-@REM For now this requires a custom version of delvewheel:
-@REM https://github.com/adang1345/delvewheel/pull/59
-%PYTHON_CMD% -m pip install https://github.com/pitrou/delvewheel/archive/refs/heads/fixes-for-arrow.zip || exit /B 1
+%PYTHON_CMD% -m pip install delvewheel || exit /B 1
 
 for /f %%i in ('dir dist\pyarrow-*.whl /B') do (set WHEEL_NAME=%cd%\dist\%%i) || exit /B 1
 echo "Wheel name: %WHEEL_NAME%"
 
 %PYTHON_CMD% -m delvewheel repair -vv ^
-    --mangle-only=msvcp140.dll --no-patch ^
+    --ignore-existing --with-mangle=msvcp140.dll ^
     -w repaired_wheels %WHEEL_NAME% || exit /B 1
 
 popd
