b41b6630ea1f26691c8926886844cde61329ad21
Sutou Kouhei <kou@clear-code.com>
2025-01-22 09:53:38 +0900

    Use aws-lc to intern symbols in s2n-tls

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -1028,6 +1028,8 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.28)
 endif()
 
 macro(prepare_fetchcontent)
+  # CACHE is for products that still use OLD as CMP0077 (option()
+  # honors normal variables).
   set(BUILD_SHARED_LIBS
       OFF
       CACHE BOOL "" FORCE)
@@ -5041,10 +5043,10 @@ function(build_awssdk)
   message(STATUS "Building AWS SDK for C++ from source")
 
   set(AWSSDK_PRODUCTS aws-c-common aws-checksums)
-  # s2n-tls only needed on Linux.
+  # aws-lc and s2n-tls only needed on Linux.
   # We can use LINUX with CMake 3.25 or later.
   if(UNIX AND NOT APPLE)
-    list(APPEND AWSSDK_PRODUCTS s2n-tls)
+    list(APPEND AWSSDK_PRODUCTS aws-lc s2n-tls)
   endif()
   list(APPEND
        AWSSDK_PRODUCTS
@@ -5107,6 +5109,14 @@ function(build_awssdk)
       ON
       CACHE BOOL "" FORCE)
   set(USE_OPENSSL
+      ON
+      CACHE BOOL "" FORCE)
+
+  # For s2n-tls
+  set(crypto_STATIC_LIBRARY
+      "$<TARGET_FILE:crypto>"
+      CACHE STRING "" FORCE)
+  set(S2N_INTERN_LIBCRYPTO
       ON
       CACHE BOOL "" FORCE)
 
