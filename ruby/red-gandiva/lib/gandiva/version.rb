cbb438ed45debe91e7839ac194456b16c0a9d2be
Bryce Mecum <petridish@gmail.com>
2025-02-02 18:02:39 -0800

    MINOR: [Release] Update versions for 19.0.1

--- ruby/red-gandiva/lib/gandiva/version.rb
+++ ruby/red-gandiva/lib/gandiva/version.rb
@@ -16,7 +16,7 @@
 # under the License.
 
 module Gandiva
-  VERSION = "19.0.0"
+  VERSION = "19.0.1"
 
   module Version
     numbers, TAG = VERSION.split("-")
--- ruby/red-parquet/lib/parquet/version.rb
+++ ruby/red-parquet/lib/parquet/version.rb
@@ -16,7 +16,7 @@
 # under the License.
 
 module Parquet
-  VERSION = "19.0.0"
+  VERSION = "19.0.1"
 
   module Version
     numbers, TAG = VERSION.split("-")
