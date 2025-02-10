7598af62326a3b4cf39cc33de9e7ecac1ad31840
Rossi Sun <zanmato1984@gmail.com>
2025-02-11 00:36:16 +0800

    Sync aborted_ flag using atomic variable

--- cpp/src/arrow/acero/task_util.cc
+++ cpp/src/arrow/acero/task_util.cc
@@ -91,11 +91,11 @@ class TaskSchedulerImpl : public TaskScheduler {
   AbortContinuationImpl abort_cont_impl_;
 
   std::vector<TaskGroup> task_groups_;
-  bool aborted_;
   bool register_finished_;
   std::mutex mutex_;  // Mutex protecting task_groups_ (state_ and num_tasks_present_
-                      // fields), aborted_ flag and register_finished_ flag
+                      // fields) and register_finished_ flag
 
+  AtomicWithPadding<bool> aborted_;
   AtomicWithPadding<int> num_tasks_to_schedule_;
   // If a task group adds tasks it's possible for a thread inside
   // ScheduleMore to miss this fact.  This serves as a flag to
@@ -105,10 +105,8 @@ class TaskSchedulerImpl : public TaskScheduler {
 };
 
 TaskSchedulerImpl::TaskSchedulerImpl()
-    : use_sync_execution_(false),
-      num_concurrent_tasks_(0),
-      aborted_(false),
-      register_finished_(false) {
+    : use_sync_execution_(false), num_concurrent_tasks_(0), register_finished_(false) {
+  aborted_.value.store(false);
   num_tasks_to_schedule_.value.store(0);
   tasks_added_recently_.value.store(false);
 }
@@ -131,12 +129,10 @@ Status TaskSchedulerImpl::StartTaskGroup(size_t thread_id, int group_id,
   ARROW_DCHECK(group_id >= 0 && group_id < static_cast<int>(task_groups_.size()));
   TaskGroup& task_group = task_groups_[group_id];
 
-  bool aborted = false;
+  bool aborted = aborted_.value.load();
   bool all_tasks_finished = false;
   {
     std::lock_guard<std::mutex> lock(mutex_);
-
-    aborted = aborted_;
 
     if (task_group.state_ == TaskGroupState::NOT_READY) {
       task_group.num_tasks_present_ = total_num_tasks;
@@ -212,7 +208,7 @@ std::vector<std::pair<int, int64_t>> TaskSchedulerImpl::PickTasks(int num_tasks,
 
 Status TaskSchedulerImpl::ExecuteTask(size_t thread_id, int group_id, int64_t task_id,
                                       bool* task_group_finished) {
-  if (!aborted_) {
+  if (!aborted_.value.load()) {
     RETURN_NOT_OK(task_groups_[group_id].task_impl_(thread_id, task_id));
   }
   *task_group_finished = PostExecuteTask(thread_id, group_id);
@@ -228,11 +224,10 @@ bool TaskSchedulerImpl::PostExecuteTask(size_t thread_id, int group_id) {
 
 Status TaskSchedulerImpl::OnTaskGroupFinished(size_t thread_id, int group_id,
                                               bool* all_task_groups_finished) {
-  bool aborted = false;
+  bool aborted = aborted_.value.load();
   {
     std::lock_guard<std::mutex> lock(mutex_);
 
-    aborted = aborted_;
     TaskGroup& task_group = task_groups_[group_id];
     task_group.state_ = TaskGroupState::ALL_TASKS_FINISHED;
     *all_task_groups_finished = true;
@@ -260,7 +255,7 @@ Status TaskSchedulerImpl::ExecuteMore(size_t thread_id, int num_tasks_to_execute
 
   int last_id = 0;
   for (;;) {
-    if (aborted_) {
+    if (aborted_.value.load()) {
       return Status::Cancelled("Scheduler cancelled");
     }
 
@@ -328,7 +323,7 @@ Status TaskSchedulerImpl::StartScheduling(size_t thread_id, ScheduleImpl schedul
 }
 
 Status TaskSchedulerImpl::ScheduleMore(size_t thread_id, int num_tasks_finished) {
-  if (aborted_) {
+  if (aborted_.value.load()) {
     return Status::Cancelled("Scheduler cancelled");
   }
 
@@ -396,9 +391,10 @@ Status TaskSchedulerImpl::ScheduleMore(size_t thread_id, int num_tasks_finished)
 
 void TaskSchedulerImpl::Abort(AbortContinuationImpl impl) {
   bool all_finished = true;
+  DCHECK_EQ(aborted_.value.load(), false);
+  aborted_.value.store(true);
   {
     std::lock_guard<std::mutex> lock(mutex_);
-    aborted_ = true;
     abort_cont_impl_ = std::move(impl);
     if (register_finished_) {
       for (size_t i = 0; i < task_groups_.size(); ++i) {
