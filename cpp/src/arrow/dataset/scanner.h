6e01111f02400e61b301c17996f5b6e9d2af2cef
Enrico Minack <github@enrico.minack.dev>
2025-02-11 17:23:49 +0100

    Fix linting

--- cpp/src/arrow/dataset/scanner.h
+++ cpp/src/arrow/dataset/scanner.h
@@ -607,8 +607,7 @@ class ARROW_DS_EXPORT ScanNodeOptions : public acero::ExecNodeOptions {
  public:
   explicit ScanNodeOptions(std::shared_ptr<Dataset> dataset,
                            std::shared_ptr<ScanOptions> scan_options,
-                           bool require_sequenced_output,
-                           bool implicit_ordering)
+                           bool require_sequenced_output, bool implicit_ordering)
       : dataset(std::move(dataset)),
         scan_options(std::move(scan_options)),
         require_sequenced_output(require_sequenced_output),
