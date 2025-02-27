87b6ef4755bab4410bcbb1f3f9264a5805e05dbf
Sutou Kouhei <kou@clear-code.com>
2025-02-27 16:59:44 +0900

    Remove needless configuration

--- cpp/cmake_modules/Usevcpkg.cmake
+++ cpp/cmake_modules/Usevcpkg.cmake
@@ -116,52 +116,9 @@ if(NOT DEFINED VCPKG_MANIFEST_MODE AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg
       CACHE BOOL "Use vcpkg.json manifest")
   message(STATUS "vcpkg.json manifest found. Using VCPKG_MANIFEST_MODE: ON")
 endif()
-# vcpkg can install packages in three different places
-set(_INST_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/vcpkg_installed") # try here first
-set(_INST_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg_installed") # try here second
-set(_INST_VCPKG_ROOT "${VCPKG_ROOT}/installed")
-# Iterate over the places
-foreach(_INST_DIR ${_INST_BUILD_DIR} ${_INST_SOURCE_DIR} ${_INST_VCPKG_ROOT} "notfound")
-  if(_INST_DIR STREQUAL "notfound")
-    message(FATAL_ERROR "vcpkg installed libraries directory not found. "
-                        "Install packages with vcpkg before executing cmake.")
-  elseif(NOT EXISTS "${_INST_DIR}")
-    continue()
-  elseif((_INST_DIR STREQUAL _INST_BUILD_DIR OR _INST_DIR STREQUAL _INST_SOURCE_DIR)
-         AND NOT VCPKG_MANIFEST_MODE)
-    # Do not look for packages in the build or source dirs if manifest mode is off
-    message(STATUS "Skipped looking for installed packages in ${_INST_DIR} "
-                   "because -DVCPKG_MANIFEST_MODE=OFF")
-    continue()
-  else()
-    message(STATUS "Looking for installed packages in ${_INST_DIR}")
-  endif()
-  if(DEFINED VCPKG_TARGET_TRIPLET)
-    # Check if a subdirectory named VCPKG_TARGET_TRIPLET
-    # exists in the vcpkg installed directory
-    if(EXISTS "${_INST_DIR}/${VCPKG_TARGET_TRIPLET}")
-      set(_VCPKG_INSTALLED_DIR "${_INST_DIR}")
-      break()
-    endif()
-  else()
-    # Infer VCPKG_TARGET_TRIPLET from the name of the
-    # subdirectory in the vcpkg installed directory
-    list_subdirs(_VCPKG_TRIPLET_SUBDIRS "${_INST_DIR}")
-    list(REMOVE_ITEM _VCPKG_TRIPLET_SUBDIRS "vcpkg")
-    list(LENGTH _VCPKG_TRIPLET_SUBDIRS _NUM_VCPKG_TRIPLET_SUBDIRS)
-    if(_NUM_VCPKG_TRIPLET_SUBDIRS EQUAL 1)
-      list(GET _VCPKG_TRIPLET_SUBDIRS 0 VCPKG_TARGET_TRIPLET)
-      set(_VCPKG_INSTALLED_DIR "${_INST_DIR}")
-      break()
-    endif()
-  endif()
-endforeach()
 if(NOT DEFINED VCPKG_TARGET_TRIPLET)
   message(FATAL_ERROR "Could not infer VCPKG_TARGET_TRIPLET. "
                       "Specify triplet with -DVCPKG_TARGET_TRIPLET.")
-elseif(NOT DEFINED _VCPKG_INSTALLED_DIR)
-  message(FATAL_ERROR "Could not find installed vcpkg packages for triplet ${VCPKG_TARGET_TRIPLET}. "
-                      "Install packages with vcpkg before executing cmake.")
 endif()
 
 set(VCPKG_TARGET_TRIPLET
@@ -201,52 +158,3 @@ set(ARROW_VCPKG
 set(ARROW_DEPENDENCY_SOURCE
     "SYSTEM"
     CACHE STRING "The specified value VCPKG is implemented internally as SYSTEM" FORCE)
-
-set(BOOST_ROOT
-    "${ARROW_VCPKG_PREFIX}"
-    CACHE STRING "")
-set(BOOST_INCLUDEDIR
-    "${ARROW_VCPKG_PREFIX}/include/boost"
-    CACHE STRING "")
-set(BOOST_LIBRARYDIR
-    "${ARROW_VCPKG_PREFIX}/lib"
-    CACHE STRING "")
-set(OPENSSL_INCLUDE_DIR
-    "${ARROW_VCPKG_PREFIX}/include"
-    CACHE STRING "")
-set(OPENSSL_LIBRARIES
-    "${ARROW_VCPKG_PREFIX}/lib"
-    CACHE STRING "")
-set(OPENSSL_ROOT_DIR
-    "${ARROW_VCPKG_PREFIX}"
-    CACHE STRING "")
-set(Thrift_ROOT
-    "${ARROW_VCPKG_PREFIX}/lib"
-    CACHE STRING "")
-set(ZSTD_INCLUDE_DIR
-    "${ARROW_VCPKG_PREFIX}/include"
-    CACHE STRING "")
-set(ZSTD_ROOT
-    "${ARROW_VCPKG_PREFIX}"
-    CACHE STRING "")
-set(BROTLI_ROOT
-    "${ARROW_VCPKG_PREFIX}"
-    CACHE STRING "")
-set(LZ4_ROOT
-    "${ARROW_VCPKG_PREFIX}"
-    CACHE STRING "")
-
-if(CMAKE_HOST_WIN32)
-  set(LZ4_MSVC_LIB_PREFIX
-      ""
-      CACHE STRING "")
-  set(LZ4_MSVC_STATIC_LIB_SUFFIX
-      ""
-      CACHE STRING "")
-  set(ZSTD_MSVC_LIB_PREFIX
-      ""
-      CACHE STRING "")
-  set(ZSTD_MSVC_STATIC_LIB_SUFFIX
-      ""
-      CACHE STRING "")
-endif()
