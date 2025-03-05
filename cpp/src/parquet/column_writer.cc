614f5df4dda6f331e98acac64ebf63a11cdaef7d
Krisztian Szucs <szucs.krisztian@gmail.com>
2025-03-05 13:13:07 +0100

    prefer to throw parquet exception rather than returning arrow status

--- cpp/src/parquet/column_writer.cc
+++ cpp/src/parquet/column_writer.cc
@@ -1363,9 +1363,8 @@ class TypedColumnWriterImpl : public ColumnWriterImpl,
     }
 
     if (properties_->cdc_enabled()) {
-      ARROW_ASSIGN_OR_RAISE(auto boundaries,
-                            content_defined_chunker_.GetBoundaries(
-                                def_levels, rep_levels, num_levels, leaf_array));
+      auto boundaries = content_defined_chunker_.GetBoundaries(def_levels, rep_levels,
+                                                               num_levels, leaf_array);
       for (auto chunk : boundaries) {
         auto chunk_array = leaf_array.Slice(chunk.value_offset);
         auto chunk_def_levels = AddIfNotNull(def_levels, chunk.level_offset);
