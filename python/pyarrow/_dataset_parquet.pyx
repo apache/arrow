f7b41cf1f2f1f228599744d84a287bf9fb3037ef
Tim Nguyen <6283718+curioustien@users.noreply.github.com>
2025-03-08 18:45:21 -0500

    Revert autoformat

--- python/pyarrow/_dataset_parquet.pyx
+++ python/pyarrow/_dataset_parquet.pyx
@@ -703,7 +703,7 @@ cdef class ParquetFragmentScanOptions(FragmentScanOptions):
     cache_options : pyarrow.CacheOptions, default None
         Cache options used when pre_buffer is enabled. The default values should
         be good for most use cases. You may want to adjust these for example if
-        you have exceptionally high latency to the file system.
+        you have exceptionally high latency to the file system. 
     thrift_string_size_limit : int, default None
         If not None, override the maximum total string size allocated
         when decoding Thrift structures. The default limit should be
