9125826d7911e47c4c93a7f9ff8fe651c4d00875
arash andishgar <arashandishgar1@gmail.com>
2025-03-13 00:40:19 +0330

    fix casting,include,equal method with relevant tests

--- cpp/src/arrow/array/statistics.cc
+++ cpp/src/arrow/array/statistics.cc
@@ -15,12 +15,14 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// This empty .cc file is for embedding not inlined symbols in
-// arrow::ArrayStatistics into libarrow.
-
 #include "arrow/array/statistics.h"
 
+#include <memory>
+#include <type_traits>
+#include <variant>
+
 #include "arrow/scalar.h"
+#include "arrow/type.h"
 
 namespace arrow {
 
@@ -56,4 +58,38 @@ const std::shared_ptr<DataType>& ArrayStatistics::ValueToArrowType(
   } visitor{array_type};
   return std::visit(visitor, value.value());
 }
+namespace {
+bool ValueTypeEquality(const std::optional<ArrayStatistics::ValueType>& first,
+                       const std::optional<ArrayStatistics::ValueType>& second) {
+  if (first == second) {
+    return true;
+  }
+  if (!first || !second) {
+    return false;
+  }
+
+  return std::visit(
+      [](auto& v1, auto& v2) {
+        if constexpr (std::is_same_v<std::decay_t<decltype(v1)>,
+                                     std::shared_ptr<Scalar>> &&
+                      std::is_same_v<std::decay_t<decltype(v2)>,
+                                     std::shared_ptr<Scalar>>) {
+          if (!v1 || !v2) {
+            // both null case is handled in std::optional and return true
+            return false;
+          }
+          return v1->Equals(*v2);
+        }
+        return false;
+      },
+      first.value(), second.value());
+}
+}  // namespace
+bool ArrayStatistics::Equals(const ArrayStatistics& other) const {
+  return null_count == other.null_count && distinct_count == other.distinct_count &&
+         is_min_exact == other.is_min_exact && is_max_exact == other.is_max_exact &&
+         ValueTypeEquality(this->max, other.max) &&
+         ValueTypeEquality(this->min, other.min);
+}
+
 }  // namespace arrow
