2c54cfb40760bf247442be369ab509f44ec682b4
Will Ayd <william.ayd@icloud.com>
2025-03-18 19:22:44 -0400

    GH-45853: [C++] Fix Meson compilation issues in Archery

--- ci/scripts/cpp_build.sh
+++ ci/scripts/cpp_build.sh
@@ -118,6 +118,12 @@ if [ "${ARROW_USE_MESON:-OFF}" = "ON" ]; then
     fi
   }
 
+  ORIGINAL_CXX=${CXX}
+  if [ "${ARROW_USE_CCACHE}" = "ON" ]; then
+      export CXX="ccache ${CXX}"
+  else
+      export CXX="sccache ${CXX}"
+  fi
   meson setup \
     --prefix=${MESON_PREFIX:-${ARROW_HOME}} \
     --buildtype=${ARROW_BUILD_TYPE:-debug} \
@@ -126,6 +132,8 @@ if [ "${ARROW_USE_MESON:-OFF}" = "ON" ]; then
     -Ds3=disabled \
     . \
     ${source_dir}
+
+  export CXX=${ORIGINAL_CXX}
 elif [ "${ARROW_EMSCRIPTEN:-OFF}" = "ON" ]; then
   if [ "${UBUNTU}" = "20.04" ]; then
     echo "arrow emscripten build is not supported on Ubuntu 20.04, run with UBUNTU=22.04"
