2a80461dc4a5bd2b37ed5e88eca68418d2a339a6
Dewey Dunnington <dewey@wherobots.com>
2025-03-20 12:02:01 -0500

    format

--- cpp/src/parquet/column_writer.cc
+++ cpp/src/parquet/column_writer.cc
@@ -1246,7 +1246,7 @@ class TypedColumnWriterImpl : public ColumnWriterImpl,
         (SortOrder::UNKNOWN != descr_->sort_order())) {
       page_statistics_ = MakeStatistics<ParquetType>(descr_, allocator_);
       chunk_statistics_ = MakeStatistics<ParquetType>(descr_, allocator_);
-      
+
       if (descr_->logical_type() != nullptr && descr_->logical_type()->is_geometry()) {
         chunk_geospatial_statistics_ = std::make_shared<GeoStatistics>();
       }
