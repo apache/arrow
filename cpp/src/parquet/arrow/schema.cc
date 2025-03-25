b0e8017a4c520c71576f16d8017e579f36494b62
neilechao <neil.chao@lacework.net>
2025-03-25 10:56:02 -0700

    Update cpp/src/parquet/arrow/schema.cc
    Co-authored-by: Gang Wu <ustcwg@gmail.com>

--- cpp/src/parquet/arrow/schema.cc
+++ cpp/src/parquet/arrow/schema.cc
@@ -1087,7 +1087,7 @@ Result<bool> ApplyOriginalMetadata(const Field& origin_field, SchemaField* infer
       // Arrow extensions are ENABLED in Parquet.
       // origin_type is parquet::arrow::variant(...)
       // inferred_type is
-      // arrow::extension::variant(struct(arrow::binary(),arrow::binary()))
+      // parquet::arrow::variant(struct(arrow::binary(),arrow::binary()))
       auto origin_storage_field = origin_field.WithType(ex_type.storage_type());
 
       // Apply metadata recursively to storage type
