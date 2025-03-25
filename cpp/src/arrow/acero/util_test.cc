f9daf5204a3cec646e0a2a5614e81594a2255c69
Rossi Sun <zanmato1984@gmail.com>
2025-03-25 16:39:50 +0800

    Fix the timing issue in ConcurrentQueueTest

--- cpp/src/arrow/acero/util_test.cc
+++ cpp/src/arrow/acero/util_test.cc
@@ -201,10 +201,10 @@ void ConcurrentQueueBasicTest(Queue& queue) {
   queue.Push(2);
   queue.Push(3);
   queue.Push(4);
-  ASSERT_EQ(fut_pop.wait_for(std::chrono::milliseconds(10)), std::future_status::ready);
+  fut_pop.wait();
   ASSERT_EQ(fut_pop.get(), 2);
   fut_pop = std::async(std::launch::async, [&]() { return queue.WaitAndPop(); });
-  ASSERT_EQ(fut_pop.wait_for(std::chrono::milliseconds(10)), std::future_status::ready);
+  fut_pop.wait();
   ASSERT_EQ(fut_pop.get(), 3);
   ASSERT_FALSE(queue.Empty());
   ASSERT_EQ(queue.TryPop(), std::make_optional(4));
