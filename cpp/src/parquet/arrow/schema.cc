31911187bead3ba1b4f81d76885af124ea4c0dd4
neilechao <neil.chao@lacework.net>
2025-03-25 10:56:12 -0700

    Update cpp/src/parquet/arrow/schema.cc
    Co-authored-by: Gang Wu <ustcwg@gmail.com>

--- cpp/src/parquet/arrow/schema.cc
+++ cpp/src/parquet/arrow/schema.cc
@@ -128,7 +128,7 @@ Status VariantToNode(const std::shared_ptr<VariantExtensionType>& type,
                             &value_node));
 
   NodePtr variant_node = *out =
-      GroupNode::Make("variant", Repetition::OPTIONAL, {metadata_node, value_node});
+      GroupNode::Make("variant", RepetitionFromNullable(nullable), {metadata_node, value_node});
 
   return Status::OK();
 }
