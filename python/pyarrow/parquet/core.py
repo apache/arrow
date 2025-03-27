0014cf9912caecb39a6540fb8b8c466a786e1845
Dewey Dunnington <dewey@fishandwhistle.net>
2025-03-27 14:39:50 -0500

    Update python/pyarrow/parquet/core.py
    Co-authored-by: Antoine Pitrou <pitrou@free.fr>

--- python/pyarrow/parquet/core.py
+++ python/pyarrow/parquet/core.py
@@ -255,8 +255,8 @@ class ParquetFile:
     page_checksum_verification : bool, default False
         If True, verify the checksum for each page read from the file.
     arrow_extensions_enabled : bool, default False
-        If True, read Parquet logical types as Arrow Extension Types where possible,
-        (e.g., JSON arrow.json).
+        If True, read Parquet logical types as Arrow extension types where possible,
+        (e.g., read JSON as the canonical `arrow.json` extension type).
 
     Examples
     --------
