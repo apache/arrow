cc10aa5da9738144d0c591ebca1fc2ac8ec6679b
Sutou Kouhei <kou@clear-code.com>
2025-03-28 12:03:11 +0900

    GH-40760: [Release] Use repository.apache.org (#45903)
    ### Rationale for this change
    
    We're using apache.jfrog.io for binary artifacts mainly for APT/Yum repositories. But apache.jfrog.io isn't stable as we expected. For example, it was down annually. See also: #40744
    
    FYI: Based on https://issues.apache.org/jira/browse/INFRA-26324 , it will not be down this year.
    
    ### What changes are included in this PR?
    
    Use repository.apache.org instead of apache.jfrog.io.
    
    Existing binary artifacts are already copied to repository.apache.org from apache.jfrog.io. For example: https://repo1.maven.org/maven2/org/apache/arrow/debian/
    
    Migration plan:
    
    1. We use repo1.maven.org (it's a CDN of repository.apache.org) in apache-arrow-apt-source (for APT repositories) and apache-arrow-release (for Yum repositories)
    2. We keep uploading only apache-arrow-apt-source (for APT repositories) and apache-arrow-release (for Yum repositories) to apache.jfrog.io
       * Existing users can migrate to repository.apache.org automatically by this
    3. We'll stop uploading apache-arrow-apt-source (for APT repositories) and apache-arrow-release (for Yum repositories) to apache.jfrog.io after we use reproducible builds based auto signing
       * We'll not change signing key after this
    
    ### Are these changes tested?
    
    Yes.
    
    ### Are there any user-facing changes?
    
    Yes.
    * GitHub Issue: #40760
    
    Authored-by: Sutou Kouhei <kou@clear-code.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- dev/release/05-binary-upload.sh
+++ dev/release/05-binary-upload.sh
@@ -80,19 +80,19 @@ rake_tasks=()
 apt_targets=()
 yum_targets=()
 if [ ${UPLOAD_ALMALINUX} -gt 0 ]; then
-  rake_tasks+=(yum:rc)
+  rake_tasks+=(yum:rc:artifactory yum:rc)
   yum_targets+=(almalinux)
 fi
 if [ ${UPLOAD_AMAZON_LINUX} -gt 0 ]; then
-  rake_tasks+=(yum:rc)
+  rake_tasks+=(yum:rc:artifactory yum:rc)
   yum_targets+=(amazon-linux)
 fi
 if [ ${UPLOAD_CENTOS} -gt 0 ]; then
-  rake_tasks+=(yum:rc)
+  rake_tasks+=(yum:rc:artifactory yum:rc)
   yum_targets+=(centos)
 fi
 if [ ${UPLOAD_DEBIAN} -gt 0 ]; then
-  rake_tasks+=(apt:rc)
+  rake_tasks+=(apt:rc:artifactory apt:rc)
   apt_targets+=(debian)
 fi
 if [ ${UPLOAD_DOCS} -gt 0 ]; then
@@ -105,7 +105,7 @@ if [ ${UPLOAD_R} -gt 0 ]; then
   rake_tasks+=(r:rc)
 fi
 if [ ${UPLOAD_UBUNTU} -gt 0 ]; then
-  rake_tasks+=(apt:rc)
+  rake_tasks+=(apt:rc:artifactory apt:rc)
   apt_targets+=(ubuntu)
 fi
 rake_tasks+=(summary:rc)
@@ -123,6 +123,8 @@ docker_run \
     APT_TARGETS=$(IFS=,; echo "${apt_targets[*]}") \
     ARTIFACTORY_API_KEY="${ARTIFACTORY_API_KEY}" \
     ARTIFACTS_DIR="${tmp_dir}/artifacts" \
+    ASF_PASSWORD="${ASF_PASSWORD}" \
+    ASF_USER="${ASF_USER}" \
     DEB_PACKAGE_NAME=${DEB_PACKAGE_NAME:-} \
     DRY_RUN=${DRY_RUN:-no} \
     GPG_KEY_ID="${GPG_KEY_ID}" \
