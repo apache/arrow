80f9e81680236e7463a58656feb6cfc1c9c76f61
Dewey Dunnington <dewey@fishandwhistle.net>
2025-04-11 10:29:50 -0500

    Update cpp/src/parquet/geospatial/statistics.h
    Co-authored-by: Gang Wu <ustcwg@gmail.com>

--- cpp/src/parquet/geospatial/statistics.h
+++ cpp/src/parquet/geospatial/statistics.h
@@ -50,7 +50,7 @@ struct PARQUET_EXPORT EncodedGeoStatistics {
   bool has_m() const { return !std::isinf(mmin - mmax); }
 
   bool is_empty() const {
-    return !geospatial_types.empty() || has_x() || has_y() || has_z() || has_m();
+    return !(!geospatial_types.empty() || has_x() || has_y() || has_z() || has_m());
   }
 };
 
