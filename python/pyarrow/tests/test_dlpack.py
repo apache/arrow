adda307e593a78aa3c7db185c8f0bff425304137
AlenkaF <frim.alenka@gmail.com>
2025-04-15 11:22:14 +0200

    Update numpy import in the test

--- python/pyarrow/tests/test_dlpack.py
+++ python/pyarrow/tests/test_dlpack.py
@@ -23,7 +23,7 @@ import pytest
 try:
     import numpy as np
 except ImportError:
-    np = None
+    pytestmark = pytest.mark.numpy
 
 import pyarrow as pa
 from pyarrow.vendored.version import Version
