a7facc3112054b9cc1d9d8d3979ec849eddc9922
Nic Crane <thisisnic@gmail.com>
2025-05-07 10:09:55 +0000

    ignore WillNeed errors in ReadRangeCache::Cache

--- cpp/src/arrow/io/caching.cc
+++ cpp/src/arrow/io/caching.cc
@@ -188,7 +188,8 @@ struct ReadRangeCache::Impl {
       entries = std::move(new_entries);
     }
     // Prefetch immediately, regardless of executor availability, if possible
-    return file->WillNeed(ranges);
+    ARROW_UNUSED(file->WillNeed(ranges)); // As this is optimisation only, failures should not be treated as fatal
+    return Status::OK();
   }
 
   // Read the given range from the cache, blocking if needed. Cannot read a range
