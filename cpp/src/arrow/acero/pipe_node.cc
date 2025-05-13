eb7d3056f1ff918ce2d4df3805e67b275aa3ffcc
Rafał Hibner <rafal.hibner@secom.com.pl>
2025-05-13 09:01:49 +0000

    Rebase fix

--- cpp/src/arrow/acero/pipe_node.cc
+++ cpp/src/arrow/acero/pipe_node.cc
@@ -30,6 +30,7 @@
 #include "arrow/util/checked_cast.h"
 #include "arrow/util/future.h"
 #include "arrow/util/logging.h"
+#include "arrow/util/logging_internal.h"
 #include "arrow/util/tracing_internal.h"
 #include "arrow/util/unreachable.h"
 namespace arrow {
