f50c489d02e5dadf01cd8819800ac31d0bd45b50
Dmitriy Ignatov <djjanks@gmail.com>
2025-05-18 17:03:47 +0300

    [JS] Define commpression registry

---
+++ js/src/ipc/compression.ts
@@ -0,0 +1,48 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import { CompressionType } from '../fb/compression-type.js';
+
+export const LENGTH_NO_COMPRESSED_DATA: number = -1;
+export const LENGTH_OF_PREFIX_DATA: number = 8;
+
+export interface Codec {
+    encode?(data: Uint8Array): Uint8Array;
+    decode?(data: Uint8Array): Uint8Array;
+}
+
+class _CompressionRegistry {
+    protected declare registry: { [key in CompressionType]?: Codec };
+
+    constructor() {
+        this.registry = {};
+    }
+
+    set(compression: CompressionType, codec: Codec) {
+        this.registry[compression] = codec;
+    }
+
+    get(compression?: CompressionType): Codec | null {
+        if (compression !== undefined) {
+            return this.registry?.[compression] || null;
+        }
+        return null;
+    }
+
+}
+
+export const compressionRegistry = new _CompressionRegistry();
