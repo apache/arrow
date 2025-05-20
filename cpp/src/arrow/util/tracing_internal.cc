74fa97d45c905328149e755cf6b12ca64120e79d
Rossi Sun <zanmato1984@gmail.com>
2025-05-20 10:02:30 -0700

    Comment useless argument name

--- cpp/src/arrow/util/tracing_internal.cc
+++ cpp/src/arrow/util/tracing_internal.cc
@@ -93,12 +93,11 @@ class OtlpOStreamExporter final : public sdktrace::SpanExporter {
 
     return otel::sdk::common::ExportResult::kSuccess;
   }
-  bool ForceFlush(std::chrono::microseconds timeout) noexcept override {
+  bool ForceFlush(std::chrono::microseconds /*timeout*/) noexcept override {
     (*out_).flush();
     return true;
   }
-  bool Shutdown(std::chrono::microseconds timeout =
-                    std::chrono::microseconds(0)) noexcept override {
+  bool Shutdown(std::chrono::microseconds timeout) noexcept override {
     return exporter_.Shutdown(timeout);
   }
   // XXX: OTel 1.19 silent breaking change: this must be overridden
