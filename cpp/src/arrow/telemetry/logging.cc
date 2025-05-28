1ad8b543cf7cf06b602b38ee5cb627c3bf4794d6
Rossi Sun <zanmato1984@gmail.com>
2025-05-28 15:23:29 -0700

    Revert previous hack

--- cpp/src/arrow/telemetry/logging.cc
+++ cpp/src/arrow/telemetry/logging.cc
@@ -71,13 +71,12 @@ class OtlpOStreamLogRecordExporter final : public otel::sdk::logs::LogRecordExpo
     otel::exporter::otlp::OtlpRecordableUtils::PopulateRequest(records, &request);
 
     for (const auto& logs : request.resource_logs()) {
-      std::string out = logs.DebugString();
-      // std::string out;
-      // auto status =
-      //     google::protobuf::util::MessageToJsonString(logs, &out, pb_json_options_);
-      // if (ARROW_PREDICT_FALSE(!status.ok())) {
-      //   return otel::sdk::common::ExportResult::kFailure;
-      // }
+      std::string out;
+      auto status =
+          google::protobuf::util::MessageToJsonString(logs, &out, pb_json_options_);
+      if (ARROW_PREDICT_FALSE(!status.ok())) {
+        return otel::sdk::common::ExportResult::kFailure;
+      }
       (*sink_) << out << std::endl;
     }
 
