92a7980e4ca1b0311bfef44a3f5a9e5bd84e5dd1
Enrico Minack <github@enrico.minack.dev>
2025-05-28 14:39:41 +0200

    Add SecureString implementation to arrow/util/

---
+++ cpp/src/arrow/util/secure_string.cc
@@ -0,0 +1,147 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#if defined(ARROW_USE_OPENSSL)
+#  include <openssl/crypto.h>
+#  include <openssl/opensslv.h>
+#endif
+#include <utility>
+#if defined(_WIN32)
+#  include <windows.h>
+#endif
+
+#include "arrow/util/secure_string.h"
+#include "arrow/util/span.h"
+
+namespace arrow::util {
+SecureString::SecureString(SecureString&& secret) noexcept
+    : secret_(std::move(secret.secret_)) {
+  secret.Dispose();
+}
+SecureString::SecureString(std::string&& secret) noexcept : secret_(std::move(secret)) {
+  SecureClear(&secret);
+}
+SecureString::SecureString(size_t n, char c) noexcept : secret_(n, c) {}
+
+SecureString& SecureString::operator=(SecureString&& secret) noexcept {
+  if (this == &secret) {
+    // self-assignment
+    return *this;
+  }
+  Dispose();
+  secret_ = std::move(secret.secret_);
+  secret.Dispose();
+  return *this;
+}
+SecureString& SecureString::operator=(const SecureString& secret) {
+  if (this == &secret) {
+    // self-assignment
+    return *this;
+  }
+  Dispose();
+  secret_ = secret.secret_;
+  return *this;
+}
+SecureString& SecureString::operator=(std::string&& secret) noexcept {
+  Dispose();
+  // if secret is local string (length <= 15 characters), copies local buffer, resets to 0
+  // - requires secure cleaning the local buffer
+  // if secret is longer, moves the pointer to secret_, resets to 0 and uses local buffer
+  // - does not require cleaning anything
+  secret_ = std::move(secret);
+  // cleans only the local buffer of secret as this always is a local string by now
+  SecureClear(&secret);
+  return *this;
+}
+
+bool SecureString::operator==(const SecureString& other) const {
+  return secret_ == other.secret_;
+}
+
+bool SecureString::operator!=(const SecureString& other) const {
+  return secret_ != other.secret_;
+}
+
+bool SecureString::empty() const { return secret_.empty(); }
+std::size_t SecureString::size() const { return secret_.size(); }
+std::size_t SecureString::length() const { return secret_.length(); }
+
+::arrow::util::span<uint8_t> SecureString::as_span() {
+  return {reinterpret_cast<uint8_t*>(secret_.data()), secret_.size()};
+}
+::arrow::util::span<const uint8_t> SecureString::as_span() const {
+  return {reinterpret_cast<const uint8_t*>(secret_.data()), secret_.size()};
+}
+std::string_view SecureString::as_view() const {
+  return {secret_.data(), secret_.size()};
+}
+
+void SecureString::Dispose() { SecureClear(&secret_); }
+void SecureString::SecureClear(std::string* secret) {
+  secret->clear();
+  SecureClear(reinterpret_cast<uint8_t*>(secret->data()), secret->capacity());
+}
+inline void SecureString::SecureClear(uint8_t* data, size_t size) {
+  // There is various prior art for this:
+  // https://www.cryptologie.net/article/419/zeroing-memory-compiler-optimizations-and-memset_s/
+  // - libb2's `secure_zero_memory` at
+  // https://github.com/BLAKE2/libb2/blob/30d45a17c59dc7dbf853da3085b71d466275bd0a/src/blake2-impl.h#L140-L160
+  // - libsodium's `sodium_memzero` at
+  // https://github.com/jedisct1/libsodium/blob/be58b2e6664389d9c7993b55291402934b43b3ca/src/libsodium/sodium/utils.c#L78:L101
+  // Note:
+  // https://www.daemonology.net/blog/2014-09-06-zeroing-buffers-is-insufficient.html
+#if defined(_WIN32)
+  // SecureZeroMemory is meant to not be optimized away
+  SecureZeroMemory(data, size);
+#elif defined(__STDC_LIB_EXT1__)
+  // memset_s is meant to not be optimized away
+  memset_s(data, size, 0, size);
+#elif defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x30000000
+  // rely on some implementation in OpenSSL cryptographic library
+  OPENSSL_cleanse(data, size);
+#elif defined(__GLIBC__) && (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 25))
+  // explicit_bzero is meant to not be optimized away
+  explicit_bzero(data, size);
+#else
+  // Volatile pointer to memset function is an attempt to avoid
+  // that the compiler optimizes away the memset function call.
+  // pretty much what OPENSSL_cleanse above does
+  // https://github.com/openssl/openssl/blob/3423c30db3aa044f46e1f0270e2ecd899415bf5f/crypto/mem_clr.c#L22
+  static const volatile auto memset_v = &memset;
+  memset_v(data, 0, size);
+
+#  if defined(__GNUC__) || defined(__clang__)
+  // __asm__ only supported by GCC and Clang
+  // not supported by MSVC on the ARM and x64 processors
+  // https://en.cppreference.com/w/c/language/asm.html
+  // https://en.cppreference.com/w/cpp/language/asm.html
+
+  // Additional attempt on top of volatile memset_v above
+  // to avoid that the compiler optimizes away the memset function call.
+  // Assembler code that tells the compiler 'data' has side effects.
+  // https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html:
+  // - "volatile": the asm produces side effects
+  // - "memory": effectively forms a read/write memory barrier for the compiler
+  __asm__ __volatile__(""          /* no actual code */
+                       :           /* no output */
+                       : "r"(data) /* input */
+                       : "memory" /* memory side effects beyond input and output */);
+#  endif
+#endif
+}
+
+}  // namespace arrow::util
---
+++ cpp/src/arrow/util/secure_string_test.cc
@@ -0,0 +1,339 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
+
+#include "arrow/util/secure_string.h"
+
+namespace arrow::util::test {
+
+std::string_view StringArea(const std::string& string) {
+  return {string.data(), string.capacity()};
+}
+
+void AssertSecurelyCleared(const std::string_view area) {
+  // the entire area is filled with zeros
+  std::string zeros(area.size(), '\0');
+  ASSERT_EQ(area, std::string_view(zeros));
+}
+
+void AssertSecurelyCleared(const std::string& string) {
+  AssertSecurelyCleared(StringArea(string));
+}
+
+/**
+ * Checks the area has been securely cleared after some position.
+ */
+void AssertSecurelyCleared(const std::string_view area, const size_t pos) {
+  // the area after pos is filled with zeros
+  if (pos < area.size()) {
+    std::string zeros(area.size() - pos, '\0');
+    ASSERT_EQ(area.substr(pos), std::string_view(zeros));
+  }
+}
+
+/**
+ * Checks the area has been securely cleared from the secret value.
+ * Assumes the area has been released, so it might have been reclaimed and changed after
+ * cleaning. We cannot check for all-zeros, best we can check here is no secret character
+ * has leaked. If by any chance the modification produced a former key character at the
+ * right position, this will be false negative / flaky. Therefore, we check for three
+ * consecutive secret characters before we fail.
+ */
+void AssertSecurelyCleared(const std::string_view area, const std::string& secret_value) {
+  auto leaks = 0;
+  for (size_t i = 0; i < secret_value.size(); i++) {
+    if (area[i] == secret_value[i]) {
+      leaks++;
+    } else {
+      if (leaks >= 3) {
+        break;
+      }
+      leaks = 0;
+    }
+  }
+  if (leaks >= 3) {
+    FAIL() << leaks << " characters of secret leaked into " << area;
+  }
+}
+
+TEST(TestSecureString, SecureClearString) {
+  // short string
+  {
+    std::string tiny("abc");
+    auto old_area = StringArea(tiny);
+    SecureString::SecureClear(&tiny);
+    AssertSecurelyCleared(tiny);
+    AssertSecurelyCleared(old_area);
+  }
+
+  // long string
+  {
+    std::string large(1024, 'x');
+    large.resize(512, 'y');
+    auto old_area = StringArea(large);
+    SecureString::SecureClear(&large);
+    AssertSecurelyCleared(large);
+    AssertSecurelyCleared(old_area);
+  }
+
+  // empty string
+  {
+    // this creates an empty string with some non-zero characters in the string buffer
+    // we test that all those characters are securely cleared
+    std::string empty("abcdef");
+    empty.resize(0);
+    auto old_area = StringArea(empty);
+    SecureString::SecureClear(&empty);
+    AssertSecurelyCleared(empty);
+    AssertSecurelyCleared(old_area);
+  }
+}
+
+TEST(TestSecureString, Construct) {
+  // move-constructing from a string securely clears that string
+  std::string string("hello world");
+  auto old_string = StringArea(string);
+  SecureString secret_from_string(std::move(string));
+  AssertSecurelyCleared(string);
+  AssertSecurelyCleared(old_string);
+  ASSERT_FALSE(secret_from_string.empty());
+
+  // move-constructing from a secure string securely clears that secure string
+  auto old_secret_from_string_view = secret_from_string.as_view();
+  auto old_secret_from_string_value = std::string(secret_from_string.as_view());
+  SecureString secret_from_move_secret(std::move(secret_from_string));
+  ASSERT_TRUE(secret_from_string.empty());
+  AssertSecurelyCleared(old_secret_from_string_view);
+  ASSERT_FALSE(secret_from_move_secret.empty());
+  ASSERT_EQ(secret_from_move_secret.as_view(),
+            std::string_view(old_secret_from_string_value));
+
+  // copy-constructing from a secure string does not modify that secure string
+  SecureString secret_from_secret(secret_from_move_secret);
+  ASSERT_FALSE(secret_from_move_secret.empty());
+  ASSERT_EQ(secret_from_move_secret.as_view(),
+            std::string_view(old_secret_from_string_value));
+  ASSERT_FALSE(secret_from_secret.empty());
+  ASSERT_EQ(secret_from_secret, secret_from_move_secret);
+}
+
+TEST(TestSecureString, Assign) {
+  // we initialize with the first string and iteratively assign the subsequent values
+  // the first two values are local (15 chars and less), the remainder are non-local
+  // strings (larger than 15 chars) memory management of short and long strings behaves
+  // differently
+  std::vector<std::string> test_strings = {
+      "secret", "another secret", "a much longer secret", std::string(1024, 'x')};
+
+  // assert test string configuration
+  ASSERT_GE(test_strings.size(), 4);
+  for (size_t i = 1; i < test_strings.size(); i++) {
+    // we expect first two strings to be local strings
+    if (i <= 1) {
+      ASSERT_LT(test_strings[i].size(), 15 / sizeof(char));
+    } else {
+      ASSERT_GE(test_strings[i].size(), 15 / sizeof(char));
+    }
+    // the strings are increasing in size
+    if (i > 0) {
+      ASSERT_TRUE(test_strings[i].size() > test_strings[i - 1].size());
+    }
+  }
+
+  std::vector<std::string> reverse_strings = std::vector(test_strings);
+  reverse(reverse_strings.begin(), reverse_strings.end());
+
+  for (auto vec : {test_strings, reverse_strings}) {
+    auto init_string = vec[0];
+    auto strings = std::vector<std::string>(vec.begin() + 1, vec.end());
+
+    {
+      // an initialized secure string
+      std::string init_string_copy(init_string);
+      SecureString secret_from_string(std::move(init_string_copy));
+
+      // move-assigning from a string securely clears that string
+      // the earlier value of the secure string is securely cleared
+      for (auto string : strings) {
+        auto string_copy = std::string(string);
+        auto old_string_copy_area = StringArea(string_copy);
+        ASSERT_FALSE(string.empty());
+        ASSERT_FALSE(string_copy.empty());
+        auto old_secret_from_string_area = secret_from_string.as_view();
+        auto old_secret_from_string_value = std::string(secret_from_string.as_view());
+
+        secret_from_string = std::move(string_copy);
+
+        ASSERT_FALSE(string.empty());
+        ASSERT_TRUE(string_copy.empty());
+        AssertSecurelyCleared(string_copy);
+        auto secret_from_string_view = secret_from_string.as_view();
+        // the secure string can reuse the string_copy's string buffer after assignment
+        // then, string_copy's string buffer is obviously not cleared
+        if (secret_from_string_view.data() != old_string_copy_area.data()) {
+          AssertSecurelyCleared(old_string_copy_area, string);
+        }
+        ASSERT_FALSE(secret_from_string.empty());
+        ASSERT_EQ(secret_from_string.size(), string.size());
+        ASSERT_EQ(secret_from_string.length(), string.length());
+        ASSERT_EQ(secret_from_string_view, std::string_view(string));
+        if (secret_from_string_view.data() == old_secret_from_string_area.data()) {
+          // when secure string reuses the buffer, the old value must be cleared
+          AssertSecurelyCleared(old_secret_from_string_area, secret_from_string.size());
+        } else {
+          // when secure string has a new buffer, the old buffer must be cleared
+          AssertSecurelyCleared(old_secret_from_string_area,
+                                old_secret_from_string_value);
+        }
+      }
+    }
+
+    {
+      // an initialized secure string
+      std::string init_string_copy(init_string);
+      SecureString secret_from_move_secret(std::move(init_string_copy));
+
+      // move-assigning from a secure string securely clears that secure string
+      // the earlier value of the secure string is securely cleared
+      for (auto string : strings) {
+        auto string_copy = std::string(string);
+        SecureString secret_string(std::move(string_copy));
+        ASSERT_FALSE(string.empty());
+        ASSERT_TRUE(string_copy.empty());
+        ASSERT_FALSE(secret_string.empty());
+        auto old_secret_string_area = secret_string.as_view();
+        auto old_secret_string_value = std::string(secret_string.as_view());
+        auto old_secret_from_move_secret_area = secret_from_move_secret.as_view();
+        auto old_secret_from_move_secret_value =
+            std::string(secret_from_move_secret.as_view());
+
+        secret_from_move_secret = std::move(secret_string);
+
+        ASSERT_TRUE(secret_string.empty());
+        // the secure string can reuse the string_copy's string buffer after assignment
+        // then, string_copy's string buffer is obviously not cleared
+        if (old_secret_string_area.data() != secret_from_move_secret.as_view().data()) {
+          AssertSecurelyCleared(old_secret_string_area,
+                                old_secret_from_move_secret_value);
+        }
+        ASSERT_FALSE(secret_from_move_secret.empty());
+        ASSERT_EQ(secret_from_move_secret.size(), string.size());
+        ASSERT_EQ(secret_from_move_secret.length(), string.length());
+        ASSERT_EQ(secret_from_move_secret.as_view(), std::string_view(string));
+        if (old_secret_from_move_secret_area.data() ==
+            secret_from_move_secret.as_view().data()) {
+          // when secure string reuses the buffer, the old value must be cleared
+          AssertSecurelyCleared(old_secret_from_move_secret_area,
+                                secret_from_move_secret.size());
+        } else {
+          // when secure string has a new buffer, the old buffer must be cleared
+          AssertSecurelyCleared(old_secret_from_move_secret_area,
+                                old_secret_from_move_secret_value);
+        }
+      }
+    }
+
+    {
+      // an initialized secure string
+      std::string init_string_copy(init_string);
+      SecureString secret_from_copy_secret(std::move(init_string_copy));
+
+      for (auto string : strings) {
+        // copy-assigning from a secure string does not modify that secure string
+        // the earlier value of the secure string is securely cleared
+        auto string_copy = std::string(string);
+        SecureString secret_string(std::move(string_copy));
+        ASSERT_FALSE(string.empty());
+        ASSERT_TRUE(string_copy.empty());
+        ASSERT_FALSE(secret_string.empty());
+        auto old_secret_from_copy_secret_area = secret_from_copy_secret.as_view();
+        auto old_secret_from_copy_secret_value =
+            std::string(secret_from_copy_secret.as_view());
+
+        secret_from_copy_secret = secret_string;
+
+        ASSERT_FALSE(secret_string.empty());
+        ASSERT_FALSE(secret_from_copy_secret.empty());
+        ASSERT_EQ(secret_from_copy_secret.size(), string.size());
+        ASSERT_EQ(secret_from_copy_secret.length(), string.length());
+        ASSERT_EQ(secret_from_copy_secret.as_view(), std::string_view(string));
+        if (old_secret_from_copy_secret_area.data() ==
+            secret_from_copy_secret.as_view().data()) {
+          // when secure string reuses the buffer, the old value must be cleared
+          AssertSecurelyCleared(old_secret_from_copy_secret_area,
+                                secret_from_copy_secret.size());
+        } else {
+          // when secure string has a new buffer, the old buffer must be cleared
+          AssertSecurelyCleared(old_secret_from_copy_secret_area,
+                                old_secret_from_copy_secret_value);
+        }
+      }
+    }
+  }
+}
+
+TEST(TestSecureString, Compare) {
+  ASSERT_TRUE(SecureString("") == SecureString(""));
+  ASSERT_FALSE(SecureString("") != SecureString(""));
+
+  ASSERT_TRUE(SecureString("hello world") == SecureString("hello world"));
+  ASSERT_FALSE(SecureString("hello world") != SecureString("hello world"));
+
+  ASSERT_FALSE(SecureString("hello world") == SecureString("hello worlds"));
+  ASSERT_TRUE(SecureString("hello world") != SecureString("hello worlds"));
+}
+
+TEST(TestSecureString, Cardinality) {
+  ASSERT_TRUE(SecureString("").empty());
+  ASSERT_EQ(SecureString("").size(), 0);
+  ASSERT_EQ(SecureString("").length(), 0);
+
+  ASSERT_FALSE(SecureString("hello world").empty());
+  ASSERT_EQ(SecureString("hello world").size(), 11);
+  ASSERT_EQ(SecureString("hello world").length(), 11);
+}
+
+TEST(TestSecureString, AsSpan) {
+  SecureString secret("hello world");
+  const SecureString& const_secret(secret);
+  auto const_span = const_secret.as_span();
+  auto mutual_span = secret.as_span();
+
+  std::string expected = "hello world";
+  ::arrow::util::span expected_span = {reinterpret_cast<uint8_t*>(expected.data()),
+                                       expected.size()};
+  ASSERT_EQ(const_span, expected_span);
+  ASSERT_EQ(mutual_span, expected_span);
+
+  // modify secret through mutual span
+  // the const span shares the same secret, so it is changed as well
+  mutual_span[0] = 'H';
+  expected_span[0] = 'H';
+  ASSERT_EQ(const_span, expected_span);
+  ASSERT_EQ(mutual_span, expected_span);
+}
+
+TEST(TestSecureString, AsView) {
+  const SecureString secret = SecureString("hello world");
+  const std::string_view view = secret.as_view();
+  ASSERT_EQ(view, "hello world");
+}
+
+}  // namespace arrow::util::test
