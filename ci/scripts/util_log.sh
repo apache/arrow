4e2fed4230decac6aee13804854c9fd1c8b27214
Hiroyuki Sato <hiroysato@gmail.com>
2025-06-02 11:11:19 +0900

    GH-46662: [CI][Dev] Fix shellcheck SC2148 errors in ci/scripts directory

--- ci/scripts/util_log.sh
+++ ci/scripts/util_log.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+# shellcheck disable=SC2148
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
