d60505228e497829c32bda5f42eaddb3c077667e
Will Ayd <william.ayd@icloud.com>
2025-06-04 17:34:58 -0400

    GH-46714: [C++] Use hidden symbol visibility in Meson configuration

--- cpp/src/arrow/util/decimal.cc
+++ cpp/src/arrow/util/decimal.cc
@@ -1109,7 +1109,7 @@ Status Decimal32::ToArrowStatus(DecimalStatus dstatus) const {
   return arrow::ToArrowStatus(dstatus, 32);
 }
 
-std::ostream& operator<<(std::ostream& os, const Decimal32& decimal) {
+ARROW_EXPORT std::ostream& operator<<(std::ostream& os, const Decimal32& decimal) {
   os << decimal.ToIntegerString();
   return os;
 }
@@ -1136,7 +1136,7 @@ Status Decimal64::ToArrowStatus(DecimalStatus dstatus) const {
   return arrow::ToArrowStatus(dstatus, 64);
 }
 
-std::ostream& operator<<(std::ostream& os, const Decimal64& decimal) {
+ARROW_EXPORT std::ostream& operator<<(std::ostream& os, const Decimal64& decimal) {
   os << decimal.ToIntegerString();
   return os;
 }
@@ -1198,7 +1198,7 @@ Status Decimal128::ToArrowStatus(DecimalStatus dstatus) const {
   return arrow::ToArrowStatus(dstatus, 128);
 }
 
-std::ostream& operator<<(std::ostream& os, const Decimal128& decimal) {
+ARROW_EXPORT std::ostream& operator<<(std::ostream& os, const Decimal128& decimal) {
   os << decimal.ToIntegerString();
   return os;
 }
@@ -1451,7 +1451,7 @@ double Decimal256::ToDouble(int32_t scale) const {
   return Decimal256RealConversion::ToReal<double>(*this, scale);
 }
 
-std::ostream& operator<<(std::ostream& os, const Decimal256& decimal) {
+ARROW_EXPORT std::ostream& operator<<(std::ostream& os, const Decimal256& decimal) {
   os << decimal.ToIntegerString();
   return os;
 }
