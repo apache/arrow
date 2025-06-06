1e1903dc52374ce77152ef0902dbe1010ed182ca
makima <alexspace@foxmail.com>
2025-06-07 00:00:34 +0800

    fix ci

--- cpp/src/arrow/array/builder_binary.h
+++ cpp/src/arrow/array/builder_binary.h
@@ -37,6 +37,7 @@
 #include "arrow/status.h"
 #include "arrow/type.h"
 #include "arrow/util/binary_view_util.h"
+#include "arrow/util/logging.h"
 #include "arrow/util/macros.h"
 #include "arrow/util/visibility.h"
 
@@ -659,7 +660,8 @@ class ARROW_EXPORT BinaryViewBuilder : public ArrayBuilder {
 
   Result<std::pair<int32_t, int32_t>> AppendBlock(const uint8_t* value,
                                                   const int64_t length) {
-    ARROW_PREDICT_TRUE(length > TypeClass::kInlineSize);
+    ARROW_CHECK_GT(length, TypeClass::kInlineSize)
+        << "Block length should be greater than inline size";
     ARROW_ASSIGN_OR_RAISE(auto v,
                           data_heap_builder_.Append</*Safe=*/true>(value, length));
     return std::pair{v.ref.buffer_index, v.ref.offset};
