d76fb6b689ceb45e095d66c499e752eaaf1fcd7b
Hiroyuki Sato <hiroysato@gmail.com>
2025-06-08 12:13:53 +0900

    test2

--- ci/scripts/conan_build.sh
+++ ci/scripts/conan_build.sh
@@ -83,6 +83,9 @@ OS=$( uname -s )
 if [ x"$OS" = x"Linux" ]; then
   sudo apt update
   sudo apt install -y rapidjson-dev
+  dpkg -L rapidjson-dev
+  dpkg -l rapidjson-dev
+  cat /etc/os-release
 fi
 cd ${build_dir}/conan/all
 conan create . "${conan_args[@]}" "$@"
