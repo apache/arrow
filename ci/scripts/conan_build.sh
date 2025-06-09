56335824b816abbdee761daf87b5563a07e4e410
Sutou Kouhei <kou@clear-code.com>
2025-06-10 05:32:19 +0900

    GH-46736: [CI] Disable Parquet in conan-minimum (#46744)
    ### Rationale for this change
    
    #45459 introduced RapidJSON dependency to Parquet support. Conan recipe enables Parquet by default but it doesn't enable RapidJSON by default. So we can't find RapidJSON.
    
    ### What changes are included in this PR?
    
    Disable Parquet by default.
    
    We should report "Parquet support requires RapidJSON support" to Conan when we release 21.0.0.
    
    ### Are these changes tested?
    
    Yes.
    
    ### Are there any user-facing changes?
    
    No.
    * GitHub Issue: #46736
    
    Authored-by: Sutou Kouhei <kou@clear-code.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- ci/scripts/conan_build.sh
+++ ci/scripts/conan_build.sh
@@ -30,8 +30,11 @@ conan_args=()
 conan_args+=(--build=missing)
 if [ -n "${ARROW_CONAN_PARQUET:-}" ]; then
   conan_args+=(--options arrow/*:parquet=${ARROW_CONAN_PARQUET})
-  conan_args+=(--options arrow/*:with_thrift=${ARROW_CONAN_PARQUET})
   conan_args+=(--options arrow/*:with_boost=${ARROW_CONAN_PARQUET})
+  conan_args+=(--options arrow/*:with_json=${ARROW_CONAN_PARQUET})
+  conan_args+=(--options arrow/*:with_thrift=${ARROW_CONAN_PARQUET})
+else
+  conan_args+=(--options arrow/*:parquet=False)
 fi
 if [ -n "${ARROW_CONAN_WITH_BROTLI:-}" ]; then
   conan_args+=(--options arrow/*:with_brotli=${ARROW_CONAN_WITH_BROTLI})
