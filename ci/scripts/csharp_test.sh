8f3c9b2cab4b2a8e211ec548d2a80ddc629e0db6
Hiroyuki Sato <hiroysato@gmail.com>
2025-06-13 15:39:47 +0900

    Revert "Test2"
    This reverts commit c050cc94d6a8f6493e9b194582fe3755609b1770.

--- ci/scripts/csharp_test.sh
+++ ci/scripts/csharp_test.sh
@@ -31,8 +31,6 @@ if [ -z "${PYTHON}" ]; then
 fi
 ${PYTHON} -m pip install pyarrow find-libpython
 PYTHONNET_PYDLL=$(${PYTHON} -m find_libpython)
-echo "PYTHON is: $PYTHON"
-echo "find_libpython output: \"$(${PYTHON} -m find_libpython)\""
 export PYTHONNET_PYDLL
 
 pushd "${source_dir}"
