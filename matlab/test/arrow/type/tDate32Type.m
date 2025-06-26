c9cfc54bf974818eddf13d73b7f0ae1927fc261b
Sarah Gilmore <sgilmore@mathworks.com>
2025-06-26 10:53:25 -0400

    Make tDate32Type inherit from hDateType

--- matlab/test/arrow/type/tDate32Type.m
+++ matlab/test/arrow/type/tDate32Type.m
@@ -15,7 +15,7 @@
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
 
-classdef tDate32Type < hFixedWidthType
+classdef tDate32Type < hDateType
 
     properties
         ConstructionFcn = @arrow.date32
@@ -23,37 +23,11 @@ classdef tDate32Type < hFixedWidthType
         TypeID = arrow.type.ID.Date32
         BitWidth = int32(32)
         ClassName = "arrow.type.Date32Type"
+        DefaultDateUnit = arrow.type.DateUnit.Day
+        ClassConstructorFcn = @arrow.type.Date32Type
     end
 
     methods(Test)
-        function TestClass(testCase)
-            % Verify ArrowType is an object of the expected class type.
-            name = string(class(testCase.ArrowType));
-            testCase.verifyEqual(name, testCase.ClassName);
-        end
-
-        function DefaultDateUnit(testCase)
-            % Verify the default DateUnit is Day.
-            type = testCase.ArrowType;
-            actualUnit = type.DateUnit;
-            expectedUnit = arrow.type.DateUnit.Day;
-            testCase.verifyEqual(actualUnit, expectedUnit);
-        end
-
-        function DateUnitNoSetter(testCase)
-            % Verify that an error is thrown when trying to set the value
-            % of the DateUnit property.
-            type = arrow.date32();
-            testCase.verifyError(@() setfield(type, "DateUnit", "Millisecond"), "MATLAB:class:SetProhibited");
-        end
-
-        function InvalidProxy(testCase)
-            % Verify that an error is thrown when a Proxy of an unexpected
-            % type is passed to the arrow.type.Date32Type constructor.
-            array = arrow.array([1, 2, 3]);
-            proxy = array.Proxy;
-            testCase.verifyError(@() arrow.type.Date32Type(proxy), "arrow:proxy:ProxyNameMismatch");
-        end
 
         function IsEqualTrue(testCase)
             % Verifies isequal method of arrow.type.Date32Type returns true if
