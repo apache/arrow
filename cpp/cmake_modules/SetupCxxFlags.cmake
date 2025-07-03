1027aa749411bc76acf3e44a78a4d2c1533e2ee8
Antoine Pitrou <antoine@python.org>
2025-07-03 16:33:48 +0200

    Try a workaround

--- cpp/cmake_modules/SetupCxxFlags.cmake
+++ cpp/cmake_modules/SetupCxxFlags.cmake
@@ -50,6 +50,10 @@ if(ARROW_CPU_FLAG STREQUAL "x86")
   # x86/amd64 compiler flags, msvc/gcc/clang
   if(MSVC)
     set(ARROW_SSE4_2_FLAG "/arch:SSE4.2")
+    # These definitions are needed for xsimd to consider the corresponding instruction
+    # sets available, but they are not by MSVC (unlike other compilers).
+    # See https://github.com/AcademySoftwareFoundation/OpenImageIO/issues/4265
+    add_definitions(-D__SSE2__ -D__SSE4_1__ -D__SSE4_2__)
     set(ARROW_AVX2_FLAG "/arch:AVX2")
     # MSVC has no specific flag for BMI2, it seems to be enabled with AVX2
     set(ARROW_BMI2_FLAG "/arch:AVX2")
