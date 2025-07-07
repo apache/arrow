d77e98320ce6288831f41aabf87e0517815e76d0
Sarah Gilmore <74676073+sgilmore10@users.noreply.github.com>
2025-07-07 14:08:45 -0400

    Update matlab/src/matlab/+arrow/+tabular/Tabular.m
    Co-authored-by: Kevin Gurney <kevin.p.gurney@gmail.com>

--- matlab/src/matlab/+arrow/+tabular/Tabular.m
+++ matlab/src/matlab/+arrow/+tabular/Tabular.m
@@ -1,4 +1,4 @@
-%TABULAR Interface for tabular data structure.
+%TABULAR Interface that represents a tabular data structure.
 
 % Licensed to the Apache Software Foundation (ASF) under one or more
 % contributor license agreements.  See the NOTICE file distributed with
