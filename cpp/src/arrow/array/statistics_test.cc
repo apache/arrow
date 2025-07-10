673823b5a4775a8eb0e3a67e4b3e549febf4b44a
Arash Andishgar <42874930+andishgar@users.noreply.github.com>
2025-07-10 03:39:08 +0330

    GH-45639: [C++][Statistics] Add support for ARROW:average_byte_width:{exac,approximate} (#46385)
    ### Rationale for this change
    
    `ARROW:average_byte_width:exact` and `ARROW:average_byte_width:approximate` statistics attributes are missing in `arrow::ArrayStatistics`.
    
    ### What changes are included in this PR?
    
    Add `average_byte_width` and `is_average_byte_width_exact`  member variables to `arrow::ArrayStatistics`.
    
    ### Are these changes tested?
    Yes, I run the relevant unit tests
    ### Are there any user-facing changes?
    Yes
    * GitHub Issue: #45639
    
    Lead-authored-by: Arash Andishgar <arashandishgar1@gmail.com>
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- cpp/src/arrow/array/statistics_test.cc
+++ cpp/src/arrow/array/statistics_test.cc
@@ -41,6 +41,17 @@ TEST(TestArrayStatistics, DistinctCount) {
   ASSERT_EQ(29, statistics.distinct_count.value());
 }
 
+TEST(TestArrayStatistics, AverageByteWidth) {
+  ArrayStatistics statistics;
+  ASSERT_FALSE(statistics.average_byte_width.has_value());
+  ASSERT_FALSE(statistics.is_average_byte_width_exact);
+  statistics.average_byte_width = 4.2;
+  ASSERT_TRUE(statistics.average_byte_width.has_value());
+  ASSERT_DOUBLE_EQ(4.2, statistics.average_byte_width.value());
+  statistics.is_average_byte_width_exact = true;
+  ASSERT_TRUE(statistics.is_average_byte_width_exact);
+}
+
 TEST(TestArrayStatistics, Min) {
   ArrayStatistics statistics;
   ASSERT_FALSE(statistics.min.has_value());
@@ -65,7 +76,7 @@ TEST(TestArrayStatistics, Max) {
   ASSERT_FALSE(statistics.is_max_exact);
 }
 
-TEST(TestArrayStatistics, EqualityNonDoulbeValue) {
+TEST(TestArrayStatistics, Equals) {
   ArrayStatistics statistics1;
   ArrayStatistics statistics2;
 
@@ -79,6 +90,16 @@ TEST(TestArrayStatistics, EqualityNonDoulbeValue) {
   statistics1.distinct_count = 2929;
   ASSERT_NE(statistics1, statistics2);
   statistics2.distinct_count = 2929;
+  ASSERT_EQ(statistics1, statistics2);
+
+  statistics1.average_byte_width = 2.9;
+  ASSERT_NE(statistics1, statistics2);
+  statistics2.average_byte_width = 2.9;
+  ASSERT_EQ(statistics1, statistics2);
+
+  statistics1.is_average_byte_width_exact = true;
+  ASSERT_NE(statistics1, statistics2);
+  statistics2.is_average_byte_width_exact = true;
   ASSERT_EQ(statistics1, statistics2);
 
   statistics1.min = std::string("world");
