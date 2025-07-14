44f0ea7aaecadf2bc8dfde8d7ff6f89ee4bfac44
Sarah Gilmore <sgilmore@mathworks.com>
2025-07-14 10:52:22 -0400

    Fix test failures in tValidateArray.m

--- matlab/test/arrow/array/tValidateArray.m
+++ matlab/test/arrow/array/tValidateArray.m
@@ -19,80 +19,80 @@ classdef tValidateArray < matlab.unittest.TestCase
 
     methods (Test)
 
-        function InvalidValidationModeInput(test)
+        function InvalidModeInput(test)
             % Verify arrow.array.Array/validate() throws an exception if
-            % provided an invalid value for the ValidationMode name-value 
+            % provided an invalid value for the Mode name-value 
             % pair.
             array = arrow.array.Float64Array.fromMATLAB(1:5);
 
             % Cannot convert "abc" to a ValidationMode value
-            fcn = @() array.validate(ValidationMode="abc");
+            fcn = @() array.validate(Mode="abc");
             test.verifyError(fcn, "MATLAB:validation:UnableToConvert");
 
-            % ValidationMode must be scalar
+            % Mode must be scalar
             modes = [arrow.array.ValidationMode.Full arrow.array.ValidationMode.Minimal];
-            fcn = @() array.validate(ValidationMode=modes);
+            fcn = @() array.validate(Mode=modes);
             test.verifyError(fcn, "MATLAB:validation:IncompatibleSize");
 
             % ValidationMode.None is not supported
             mode = arrow.array.ValidationMode.None;
-            fcn = @() array.validate(ValidationMode=mode);
+            fcn = @() array.validate(Mode=mode);
             test.verifyError(fcn, "arrow:array:InvalidValidationMode");
         end
 
         function ValidationModeMinimalFails(test)
             % Verify arrow.array.Array/validate() throws an exception 
             % with the ID arrow:array:ValidateMinimalFailed if
-            % ValidationMode="Minimal" and the array fails the "Minimal"
+            % Mode="Minimal" and the array fails the "Minimal"
             % validation checks.
             offsets = arrow.array(int32([0 1 3 4 5]));
             values = arrow.array([1 2 3]);
             array = arrow.array.ListArray.fromArrays(offsets, values, ValidationMode="None");
-            fcn = @() array.validate(ValidationMode="Minimal");
+            fcn = @() array.validate(Mode="Minimal");
             test.verifyError(fcn, "arrow:array:ValidateMinimalFailed")
         end
 
         function ValidationModeMinimalPasses(test)
             % Verify arrow.array.Array/validate() does not throw an
-            % exception if ValidationMode="Minimal" and the array passes the
+            % exception if Mode="Minimal" and the array passes the
             % "Minimal" validation checks.
             offsets = arrow.array(int32([0 1 0]));
             values = arrow.array([1 2 3]);
             % NOTE: the array is actually invalid, but it passes the
             % "Minimal" validation checks.
-            array = arrow.array.ListArray.fromArrays(offsets, values, ValidationMode="None");
-            fcn = @() array.validate(ValidationMode="Minimal");
+            array = arrow.array.ListArray.fromArrays(offsets, values);
+            fcn = @() array.validate(Mode="Minimal");
             test.verifyWarningFree(fcn, "arrow:array:ValidateMinimalFailed")
         end
 
         function ValidationModeFullFails(test)
             % Verify arrow.array.Array/validate() throws an exception 
             % with the ID arrow:array:ValidateFullFailed if
-            % ValidationMode="Full" and the array fails the "Full"
+            % Mode="Full" and the array fails the "Full"
             % validation checks.
             offsets = arrow.array(int32([0 1 0]));
             values = arrow.array([1 2 3]);
-            array = arrow.array.ListArray.fromArrays(offsets, values, ValidationMode="None");
-            fcn = @() array.validate(ValidationMode="Full");
+            array = arrow.array.ListArray.fromArrays(offsets, values);
+            fcn = @() array.validate(Mode="Full");
             test.verifyError(fcn, "arrow:array:ValidateFullFailed")
         end
 
         function ValidationModeFullPasses(test)
             % Verify arrow.array.Array/validate() does not throw an
-            % exception if ValidationMode="Full" and the array passes
+            % exception if Mode="Full" and the array passes
             % the "full" validation checks.
             offsets = arrow.array(int32([0 1 3]));
             values = arrow.array([1 2 3]);
-            array = arrow.array.ListArray.fromArrays(offsets, values, ValidationMode="None");
-            fcn = @() array.validate(ValidationMode="Full");
+            array = arrow.array.ListArray.fromArrays(offsets, values);
+            fcn = @() array.validate(Mode="Full");
             test.verifyWarningFree(fcn);
         end
 
         function DefaultValidationModeIsMimimal(test)
-            % Verify the default ValidationMode value is "Minimal".
+            % Verify the default Mode value is "Minimal".
             offsets = arrow.array(int32([0 1 2 3]));
             values = arrow.array([1 2 3]);
-            array = arrow.array.ListArray.fromArrays(offsets, values, ValidationMode="None");
+            array = arrow.array.ListArray.fromArrays(offsets, values);
             fcn = @() array.validate();
             test.verifyWarningFree(fcn, "arrow:array:ValidateMinimalFailed")
         end
