f045c22d32e730e112aaa869198509320c688060
Will Ayd <william.ayd@icloud.com>
2025-07-17 10:30:51 -0400

    Fix ASAN failure

--- cpp/src/arrow/compute/kernels/scalar_compare.cc
+++ cpp/src/arrow/compute/kernels/scalar_compare.cc
@@ -80,7 +80,7 @@ struct ListNotEqual {
       return true;
     } else {
       RangeDataEqualsImpl range_comparer{
-          EqualOptions::Defaults(), false, left, right, left.offset, right.offset, 1,
+          EqualOptions::Defaults(), false, left, right, 0, 0, 1,
       };
 
       return !range_comparer.Compare();
