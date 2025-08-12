8c18cce7acd923ef2418e4179fbf1454af7dd5f0
Sofia Brenes <sofia@tekdatum.com>
2025-08-12 15:33:44 -0600

    Removed unnecessary import

--- cpp/src/parquet/encryption/encryption.cc
+++ cpp/src/parquet/encryption/encryption.cc
@@ -22,7 +22,7 @@
 #include <rapidjson/document.h>
 #include <string.h>
 #include <utility>
-#include <iostream>
+
 #include "arrow/util/logging_internal.h"
 #include "arrow/util/utf8.h"
 #include "parquet/encryption/encryption_internal.h"
