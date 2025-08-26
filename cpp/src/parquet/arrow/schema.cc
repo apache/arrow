697f5013a8037e422f6009ef9f216c1c170922c6
Sarah Gilmore <74676073+sgilmore10@users.noreply.github.com>
2025-08-26 11:49:35 -0400

    GH-41476: [Python][C++] Impossible to specify `is_adjusted_to_utc` for `Time` type when writing to Parquet (#47316)
    ### Rationale for this change
    
    As of today, it's not possible to write Parquet `TIME` data  whose `isAdjustedToUTC` parameter is `false`.  Instead, `isAdjustedToUTC` is hard-coded to `true` [here](https://github.com/apache/arrow/blob/2dd3ccda6437f79aa34641bd3197dd7392ae4aec/cpp/src/parquet/arrow/schema.cc#L431). 
    
    Unfortunately, some Parquet consumers only support `TIME` data if the `isAdjustedToUTC` parameter is `false`, meaning they cannot import Parquet `TIME` data generated by our Parquet Writer.  For example, the apache/spark Parquet reader only supports Parquet `TIME` columns if [`isAdjustedToUTC=false` and `units=MICROSECONDS`](https://github.com/apache/spark/blob/554f6b64f1e2b2346499f6d3340a3695244bfc84/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetSchemaConverter.scala#L309). 
    
    Adding support for writing `TIME` data with the `isAdjustedToUTC` set to `false` would unblock users who need to write Spark-compatible Parquet data.
    
    ### What changes are included in this PR?
    
    1. Added a `write_time_adjusted_to_utc` as a property to `parquet::ArrowWriterProperties`. If `true`, all `TIME` columns have their `isAdjustedToUTC` parameters set to `true`. Otherwise, `isAdjustedToUTC` is set to `false` for all `TIME` columns. This property is `false` by default. 
    2. Added `enable_write_time_adjusted_to_utc()` and `disable_write_time_adjusted_to_utc()` methods to `parquet::ArrowWriterProperties::Builder`.
    
    ### Are these changes tested?
    
    Yes. I added test case `ParquetTimeAdjustedToUTC` to test suite `TestConvertArrowSchema`.
    
    ### Are there any user-facing changes?
    
    Yes. Users can now configure the `isAdjustedToUTC` parameter for Parquet `TIME` data.
    
    NOTE: This change introduces an incompatibility. The default value for `isAdjustedToUTC` parameter is now `false` instead of `true`.
    
    ### NOTE
    
    1. I did not update the PyArrow interface because I am not familiar with that code base. I was planning on creating a new GitHub issue to track that work separately.
    2. There already exists an open PR (#43268) for addressing this issue. However, that PR was last active over a year ago and seems stale.
    
    * GitHub Issue: #41476
    
    Lead-authored-by: Sarah Gilmore <sgilmore@mathworks.com>
    Co-authored-by: Sarah Gilmore <74676073+sgilmore10@users.noreply.github.com>
    Co-authored-by: Antoine Pitrou <pitrou@free.fr>
    Signed-off-by: Sarah Gilmore <sgilmore@mathworks.com>

--- cpp/src/parquet/arrow/schema.cc
+++ cpp/src/parquet/arrow/schema.cc
@@ -420,18 +420,21 @@ Status FieldToNode(const std::string& name, const std::shared_ptr<Field>& field,
       break;
     case ArrowTypeId::TIME32:
       type = ParquetType::INT32;
-      logical_type =
-          LogicalType::Time(/*is_adjusted_to_utc=*/true, LogicalType::TimeUnit::MILLIS);
+      logical_type = LogicalType::Time(
+          /*is_adjusted_to_utc=*/arrow_properties.write_time_adjusted_to_utc(),
+          LogicalType::TimeUnit::MILLIS);
       break;
     case ArrowTypeId::TIME64: {
       type = ParquetType::INT64;
       auto time_type = static_cast<::arrow::Time64Type*>(field->type().get());
       if (time_type->unit() == ::arrow::TimeUnit::NANO) {
-        logical_type =
-            LogicalType::Time(/*is_adjusted_to_utc=*/true, LogicalType::TimeUnit::NANOS);
+        logical_type = LogicalType::Time(
+            /*is_adjusted_to_utc=*/arrow_properties.write_time_adjusted_to_utc(),
+            LogicalType::TimeUnit::NANOS);
       } else {
-        logical_type =
-            LogicalType::Time(/*is_adjusted_to_utc=*/true, LogicalType::TimeUnit::MICROS);
+        logical_type = LogicalType::Time(
+            /*is_adjusted_to_utc=*/arrow_properties.write_time_adjusted_to_utc(),
+            LogicalType::TimeUnit::MICROS);
       }
     } break;
     case ArrowTypeId::DURATION:
