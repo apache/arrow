fb5a9de6357851ac9c6e24e868594b86adc3f384
Enrico Minack <github@enrico.minack.dev>
2025-08-27 13:38:09 +0200

    Return CSecureString* instead of CSecureString&

--- python/pyarrow/_parquet_encryption.pyx
+++ python/pyarrow/_parquet_encryption.pyx
@@ -312,12 +312,12 @@ cdef void _cb_wrap_key(
 
 cdef void _cb_unwrap_key(
         handler, const c_string& wrapped_key,
-        const c_string& master_key_identifier, CSecureString& out) except *:
+        const c_string& master_key_identifier, CSecureString* out) except *:
     mkid_str = frombytes(master_key_identifier)
     wk_str = frombytes(wrapped_key)
     key = handler.unwrap_key(wk_str, mkid_str)
     cstr = <c_string>tobytes(key)
-    out = CSecureString(move(cstr))
+    out[0] = CSecureString(move(cstr))
 
 
 cdef class KmsClient(_Weakrefable):
