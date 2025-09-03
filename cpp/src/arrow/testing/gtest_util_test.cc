697c0557a7dfeeaefe244e3e0f95225ba15989e4
Will Ayd <william.ayd@icloud.com>
2025-09-03 18:05:06 -0400

    Skip broken tests

--- cpp/src/arrow/testing/gtest_util_test.cc
+++ cpp/src/arrow/testing/gtest_util_test.cc
@@ -288,6 +288,7 @@ TEST(AssertTestWithinUlp, Basics) {
   AssertWithinUlp(123.4567, 123.45670000000015, 11);
   AssertWithinUlp(123.456f, 123.456085f, 11);
 #ifndef _WIN32
+  // GH-47442
   EXPECT_FATAL_FAILURE(AssertWithinUlp(123.4567, 123.45670000000015, 10),
                        "not within 10 ulps");
   EXPECT_FATAL_FAILURE(AssertWithinUlp(123.456f, 123.456085f, 10), "not within 10 ulps");
