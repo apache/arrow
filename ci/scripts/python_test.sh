2ce531f9ed022491031e7bb4d869a2b45c76215f
Raúl Cumplido <raulcumplido@gmail.com>
2025-09-08 15:07:14 +0200

    Remove wmic check

--- ci/scripts/python_test.sh
+++ ci/scripts/python_test.sh
@@ -86,8 +86,6 @@ export PYARROW_TEST_PARQUET
 export PYARROW_TEST_PARQUET_ENCRYPTION
 export PYARROW_TEST_S3
 
-wmic OS get FreePhysicalMemory,TotalVisibleMemorySize /Value
-
 python -c "
 import os
 import sys
