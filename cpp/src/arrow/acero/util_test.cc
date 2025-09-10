10ed4020dcf2254d3295f31e0b7d2e7e86691616
Rafał Hibner <rafal.hibner@secom.com.pl>
2025-09-10 08:43:19 +0000

    Add push after shutdown test

--- cpp/src/arrow/acero/util_test.cc
+++ cpp/src/arrow/acero/util_test.cc
@@ -301,5 +301,23 @@ TEST(BackpressureConcurrentQueue, BackpressureTest) {
   ASSERT_FALSE(dummy_node.paused);
 }
 
+TEST(BackpressureConcurrentQueue, BackpressureTestStayPaused) {
+  BackpressureTestExecNode dummy_node;
+  auto ctrl = std::make_unique<TestBackpressureControl>(&dummy_node);
+  ASSERT_OK_AND_ASSIGN(auto handler, BackpressureHandler::Make(2, 4, std::move(ctrl)));
+  BackpressureConcurrentQueue<int> queue(std::move(handler));
+
+  queue.Push(6);
+  queue.Push(7);
+  queue.Push(8);
+  ASSERT_FALSE(dummy_node.paused);
+  ASSERT_FALSE(dummy_node.stopped);
+  queue.ForceShutdown();
+  for (int i = 0; i < 10; ++i) {
+    queue.Push(i);
+  }
+  ASSERT_FALSE(dummy_node.paused);
+}
+
 }  // namespace acero
 }  // namespace arrow
