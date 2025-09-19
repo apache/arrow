2b0980d09f123d894376da5fb67efbcec86796d1
Sutou Kouhei <kou@cozmixng.org>
2025-09-19 10:32:46 +0900

    Don't assign options to ensure using the default options

--- cpp/src/arrow/table_test.cc
+++ cpp/src/arrow/table_test.cc
@@ -306,11 +306,9 @@ TEST(TestTableEqualityFloatType, Approximate) {
 
   ASSERT_FALSE(table->Equals(*other_table, options));
 
-  options = options.use_atol(true).atol(1e-3);
-  ASSERT_TRUE(table->Equals(*other_table, options));
+  ASSERT_TRUE(table->Equals(*other_table, options.use_atol(true).atol(1e-3)));
 
-  options = options.use_atol(true).atol(1e-5);
-  ASSERT_FALSE(table->Equals(*other_table, options));
+  ASSERT_FALSE(table->Equals(*other_table, options.use_atol(true).atol(1e-5));
 }
 
 TEST(TestTableEqualitySameAddress, NonFloatType) {
