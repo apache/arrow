16ceade8f8b6a2720acf028ca3b6352000c60cdb
Alina (Xi) Li <96995091+alinaliBQ@users.noreply.github.com>
2025-10-01 14:19:05 -0700

    GH-47516: [C++][FlightRPC] Initial ODBC driver framework (#47517)
    ### Rationale for this change
    Add initial framework for building an ODBC driver dll
    
    ### What changes are included in this PR?
    - initial Flight SQL ODBC framework
    
    ### Are these changes tested?
    - Yes, on local Windows environment
    
    ### Are there any user-facing changes?
    - None
    * PR is extracted from PR #46099
    * GitHub Issue: #47516
    
    Authored-by: Alina (Xi) Li <alina.li@improving.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

---
+++ cpp/src/arrow/flight/sql/odbc/odbc_api.cc
@@ -0,0 +1,43 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// flight_sql_connection.h needs to be included first due to conflicts with windows.h
+#include "arrow/flight/sql/odbc/flight_sql/flight_sql_connection.h"
+
+#include "arrow/flight/sql/odbc/flight_sql/include/flight_sql/config/configuration.h"
+#include "arrow/flight/sql/odbc/flight_sql/include/flight_sql/flight_sql_driver.h"
+#include "arrow/flight/sql/odbc/odbc_api_internal.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/diagnostics.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/odbc_impl/attribute_utils.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/odbc_impl/encoding_utils.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/odbc_impl/odbc_connection.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/odbc_impl/odbc_descriptor.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/odbc_impl/odbc_environment.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/odbc_impl/odbc_statement.h"
+#include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/spi/connection.h"
+#include "arrow/util/logging.h"
+
+namespace arrow::flight::sql::odbc {
+SQLRETURN SQLAllocHandle(SQLSMALLINT type, SQLHANDLE parent, SQLHANDLE* result) {
+  ARROW_LOG(DEBUG) << "SQLAllocHandle called with type: " << type
+                   << ", parent: " << parent
+                   << ", result: " << static_cast<const void*>(result);
+
+  return SQL_INVALID_HANDLE;
+}
+
+}  // namespace arrow::flight::sql::odbc
