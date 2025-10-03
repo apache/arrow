4e6706a9feefefb23dc8b296de4dea7a376c5fda
AlenkaF <frim.alenka@gmail.com>
2025-10-03 15:28:37 +0200

    Fix macos Python 3.14 install

--- ci/scripts/install_python.sh
+++ ci/scripts/install_python.sh
@@ -48,7 +48,11 @@ full_version=${versions[$2]}
 if [ "$platform" = "macOS" ]; then
     echo "Downloading Python installer..."
 
-    fname="python-${full_version}-macos11.pkg"
+    if [ "$version" = "3.14" ];
+    then
+        fname="python-${full_version}rc3-macos11.pkg"
+    else
+        fname="python-${full_version}-macos11.pkg"
     wget "https://www.python.org/ftp/python/${full_version}/${fname}"
 
     echo "Installing Python..."
