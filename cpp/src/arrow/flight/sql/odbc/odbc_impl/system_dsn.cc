32b25bc9cf74a2966ea5e89d52a4c75048faec6f
justing-bq <62349012+justing-bq@users.noreply.github.com>
2025-10-07 09:27:37 -0700

    Establish ODBC tests
    Co-Authored-By: rscales <robscales@icloud.com>
    Co-Authored-By: justing-bq <justin.gossett@improving.com>
    Co-Authored-By: alinalibq <alina.li@improving.com>

--- cpp/src/arrow/flight/sql/odbc/odbc_impl/system_dsn.cc
+++ cpp/src/arrow/flight/sql/odbc/odbc_impl/system_dsn.cc
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include "arrow/flight/sql/odbc/odbc_impl/system_dsn.h"
+
 // platform.h includes windows.h, so it needs to be included
 // before winuser.h
 #include "arrow/flight/sql/odbc/odbc_impl/platform.h"
