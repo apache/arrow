19994527727122208e1ec3d5d779d102d2b2ab99
Sutou Kouhei <kou@clear-code.com>
2025-10-08 14:38:09 +0900

    Set PATH

--- ci/scripts/cpp_test.sh
+++ ci/scripts/cpp_test.sh
@@ -139,11 +139,12 @@ if [ "${ARROW_USE_MESON:-OFF}" = "OFF" ] && \
   cmake \
     -S "${source_dir}/examples/minimal_build" \
     -B "${build_dir}/examples/minimal_build" \
-    -DCMAKE_FIND_DEBUG_MODE=ON \
     -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}"
   cmake --build "${build_dir}/examples/minimal_build"
   pushd "${source_dir}/examples/minimal_build"
-  "${build_dir}/examples/minimal_build/arrow-example"
+  # PATH= is for Windows.
+  PATH="${CMAKE_INSTALL_PREFIX:-${ARROW_HOME}}/bin:${PATH}" \
+    "${build_dir}/examples/minimal_build/arrow-example"
   popd
 fi
 
