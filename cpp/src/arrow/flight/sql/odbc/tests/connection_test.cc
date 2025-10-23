62afc09cf67cb96b18f2ce5fa93bb03f2c5506db
Alina (Xi) Li <96995091+alinaliBQ@users.noreply.github.com>
2025-10-22 17:01:47 -0700

    GH-46096: [C++][FlightRPC] Environment and Connection Handle Allocation (#47759)
    ### Rationale for this change
    ODBC driver needs to handle environment handle and connection handle allocation and deallocation.
    
    ### What changes are included in this PR?
    - Implement SQLAllocHandle and SQLFreeHandle APIs for allocating and deallocating environment and connection handles.
    - Tests
    
    ### Are these changes tested?
    - Tested locally on Windows MSVC.
    - Will be tested in CI after https://github.com/apache/arrow/pull/47689 is merged
    
    ### Are there any user-facing changes?
    No
    * GitHub Issue: #46096
    * GitHub Issue: #46097 
    
    Authored-by: Alina (Xi) Li <alina.li@improving.com>
    Signed-off-by: David Li <li.davidm96@gmail.com>

--- cpp/src/arrow/flight/sql/odbc/tests/connection_test.cc
+++ cpp/src/arrow/flight/sql/odbc/tests/connection_test.cc
@@ -14,30 +14,96 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+#include "arrow/flight/sql/odbc/tests/odbc_test_suite.h"
 
-#ifdef _WIN32
-#  include <windows.h>
-#endif
+#include "arrow/flight/sql/odbc/odbc_impl/platform.h"
 
 #include <sql.h>
 #include <sqltypes.h>
 #include <sqlucode.h>
 
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 namespace arrow::flight::sql::odbc {
 
-TEST(SQLAllocHandle, SQLAllocHandleEnv) {
+template <typename T>
+class ConnectionTest : public T {};
+
+// GH-46574 TODO: add remote server test cases using `ConnectionRemoteTest`
+class ConnectionRemoteTest : public FlightSQLODBCRemoteTestBase {};
+using TestTypes = ::testing::Types<FlightSQLODBCMockTestBase, ConnectionRemoteTest>;
+TYPED_TEST_SUITE(ConnectionTest, TestTypes);
+
+TEST(SQLAllocHandle, TestSQLAllocHandleEnv) {
+  SQLHENV env;
+
   // Allocate an environment handle
-  SQLHENV env = nullptr;
   ASSERT_EQ(SQL_SUCCESS, SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env));
 
-  // Check for valid handle
-  ASSERT_NE(nullptr, env);
+  ASSERT_NE(env, nullptr);
 
   // Free an environment handle
   ASSERT_EQ(SQL_SUCCESS, SQLFreeHandle(SQL_HANDLE_ENV, env));
+}
+
+TEST(SQLAllocEnv, TestSQLAllocEnv) {
+  SQLHENV env;
+
+  // Allocate an environment handle
+  ASSERT_EQ(SQL_SUCCESS, SQLAllocEnv(&env));
+
+  // Free an environment handle
+  ASSERT_EQ(SQL_SUCCESS, SQLFreeEnv(env));
+}
+
+TEST(SQLAllocHandle, TestSQLAllocHandleConnect) {
+  SQLHENV env;
+  SQLHDBC conn;
+
+  // Allocate an environment handle
+  ASSERT_EQ(SQL_SUCCESS, SQLAllocEnv(&env));
+
+  // Allocate a connection using alloc handle
+  ASSERT_EQ(SQL_SUCCESS, SQLAllocHandle(SQL_HANDLE_DBC, env, &conn));
+
+  // Free a connection handle
+  ASSERT_EQ(SQL_SUCCESS, SQLFreeHandle(SQL_HANDLE_DBC, conn));
+
+  // Free an environment handle
+  ASSERT_EQ(SQL_SUCCESS, SQLFreeHandle(SQL_HANDLE_ENV, env));
+}
+
+TEST(SQLAllocConnect, TestSQLAllocHandleConnect) {
+  SQLHENV env;
+  SQLHDBC conn;
+
+  // Allocate an environment handle
+  ASSERT_EQ(SQL_SUCCESS, SQLAllocEnv(&env));
+
+  // Allocate a connection using alloc handle
+  ASSERT_EQ(SQL_SUCCESS, SQLAllocConnect(env, &conn));
+
+  // Free a connection handle
+  ASSERT_EQ(SQL_SUCCESS, SQLFreeConnect(conn));
+
+  // Free an environment handle
+  ASSERT_EQ(SQL_SUCCESS, SQLFreeEnv(env));
+}
+
+TEST(SQLFreeHandle, TestFreeNullHandles) {
+  SQLHENV env = NULL;
+  SQLHDBC conn = NULL;
+  SQLHSTMT stmt = NULL;
+
+  // Verifies attempt to free invalid handle does not cause segfault
+  // Attempt to free null statement handle
+  ASSERT_EQ(SQL_INVALID_HANDLE, SQLFreeHandle(SQL_HANDLE_STMT, stmt));
+
+  // Attempt to free null connection handle
+  ASSERT_EQ(SQL_INVALID_HANDLE, SQLFreeHandle(SQL_HANDLE_DBC, conn));
+
+  // Attempt to free null environment handle
+  ASSERT_EQ(SQL_INVALID_HANDLE, SQLFreeHandle(SQL_HANDLE_ENV, env));
 }
 
 }  // namespace arrow::flight::sql::odbc
