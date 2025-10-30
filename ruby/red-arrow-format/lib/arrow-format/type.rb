3370f153f2c67f79072421eaf53d92805acb3c95
Sutou Kouhei <kou@clear-code.com>
2025-10-31 06:50:16 +0900

    GH-48287: [Ruby] Add minimum pure Ruby Apache Arrow reader implementation
    This includes the followings:
    
    * Prepare package
      * We use red-arrow-format as package ID
      * We use Red Arrow Format as package name
    * Auto generated FlatBuffers code
      * It's generated by red-flatbuffers
    * Add minimum file format reader
      * It can read only UInt8 array
    * CI configuration

---
+++ ruby/red-arrow-format/lib/arrow-format/type.rb
@@ -0,0 +1,46 @@
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module ArrowFormat
+  class Type
+    attr_reader :name
+    def initialize(name)
+      @name = name
+    end
+  end
+
+  class IntType < Type
+    attr_reader :bit_width
+    attr_reader :signed
+    def initialize(name, bit_width, signed)
+      super(name)
+      @bit_width = bit_width
+      @signed = signed
+    end
+  end
+
+  class Int8Type < IntType
+    def initialize
+      super("Int8", 8, true)
+    end
+  end
+
+  class UInt8Type < IntType
+    def initialize
+      super("UInt8", 8, false)
+    end
+  end
+end
--- ruby/red-parquet/red-parquet.gemspec
+++ ruby/red-parquet/red-parquet.gemspec
@@ -29,7 +29,7 @@ Gem::Specification.new do |spec|
   ]
   spec.version = version_components.compact.join(".")
   spec.homepage = "https://arrow.apache.org/"
-  spec.authors = ["Apache Arrow Developers"]
+  spec.authors = ["The Apache Software Foundation"]
   spec.email = ["dev@arrow.apache.org"]
 
   spec.summary = "Red Parquet is the Ruby bindings of Apache Parquet"
