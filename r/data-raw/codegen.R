ed594dea6dbe6a0ea88c02634b5500001e882aa7
Nic Crane <thisisnic@gmail.com>
2025-11-05 19:09:41 -0500

    Restore base pipe stuff

--- r/data-raw/codegen.R
+++ r/data-raw/codegen.R
@@ -40,14 +40,16 @@ suppressPackageStartupMessages({
   library(vctrs)
 })
 
+combine_functions <- function(data) {
+  vec_cbind(data, vec_rbind(!!!pull(data, functions)))
+}
+
 get_exported_functions <- function(decorations, export_tag) {
-  out <- decorations %>%
-    filter(decoration %in% paste0(export_tag, "::export")) %>%
-    mutate(functions = map(context, decor:::parse_cpp_function)) %>%
-    {
-      vec_cbind(., vec_rbind(!!!pull(., functions)))
-    } %>%
-    select(-functions) %>%
+  out <- decorations |>
+    filter(decoration %in% paste0(export_tag, "::export")) |>
+    mutate(functions = map(context, decor:::parse_cpp_function)) |>
+    combine_functions() |>
+    select(-functions) |>
     mutate(decoration = sub("::export", "", decoration))
   message(glue(
     "*** > {n} functions decorated with [[{tags}::export]]",
