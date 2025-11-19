apache/arrow

commit 1f8cec331ecf757ac58d41e8915db0860f1de29d

MINOR: [C++][Parquet][Docs] Increase chunk_size in docs (#40705)
### Rationale for this change

Is there a reason we're using a low value here. All other examples use `128*1024` or `64*1024`.

I was stumped by this as I used it without really reading about the parameter and spent a day figuring out why my parquet writes were so slow.

### What changes are included in this PR?

Increase `chunk_size` to `64*1024`

### Are these changes tested?

Yes.

### Are there any user-facing changes?

Yes.

Authored-by: Shadab Zafar <dufferzafar0@gmail.com>
Signed-off-by: Sutou Kouhei <kou@clear-code.com>

cpp/examples/arrow/parquet_read_write.cc
@@ -120,7 +120,7 @@ arrow::Status WriteFullFile(std::string path_to_file) {
 
   ARROW_RETURN_NOT_OK(parquet::arrow::WriteTable(*table.get(),
                                                  arrow::default_memory_pool(), outfile,
-                                                 /*chunk_size=*/3, props, arrow_props));
+                                                 /*chunk_size=*/64*1024, props, arrow_props));
   return arrow::Status::OK();
 }
 

commit 303d077720f17713a191d53e25a88046645fa3a4

MINOR: [C++] Fix lint errors (#48173)
### Rationale for this change

A C++ linting error was introduced with #40705.

### What changes are included in this PR?

Linter fixed issue in `cpp/examples/arrow/parquet_read_write.cc`

Authored-by: Rok Mihevc <rok@mihevc.org>
Signed-off-by: Rok Mihevc <rok@mihevc.org>

cpp/examples/arrow/parquet_read_write.cc
@@ -118,9 +118,9 @@ arrow::Status WriteFullFile(std::string path_to_file) {
   std::shared_ptr<arrow::io::FileOutputStream> outfile;
   ARROW_ASSIGN_OR_RAISE(outfile, arrow::io::FileOutputStream::Open(path_to_file));
 
-  ARROW_RETURN_NOT_OK(parquet::arrow::WriteTable(*table.get(),
-                                                 arrow::default_memory_pool(), outfile,
-                                                 /*chunk_size=*/64*1024, props, arrow_props));
+  ARROW_RETURN_NOT_OK(
+      parquet::arrow::WriteTable(*table.get(), arrow::default_memory_pool(), outfile,
+                                 /*chunk_size=*/64 * 1024, props, arrow_props));
   return arrow::Status::OK();
 }
 