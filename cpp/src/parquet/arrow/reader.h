694a090d4a157b11f1e8572e3a900b7a333013e6
Hiroyuki Sato <hiroysato@gmail.com>
2025-11-27 20:48:17 +0900

    GH-44810: [C++][Parquet] Add arrow::Result version of parquet::arrow::FileReader::Make()

--- cpp/src/parquet/arrow/reader.h
+++ cpp/src/parquet/arrow/reader.h
@@ -126,6 +126,14 @@ class PARQUET_EXPORT FileReader {
                               std::unique_ptr<ParquetFileReader> reader,
                               std::unique_ptr<FileReader>* out);
 
+  /// Factory function to create a FileReader from a ParquetFileReader and properties
+  static ::arrow::Result<std::unique_ptr<FileReader>> Make(
+      ::arrow::MemoryPool* pool, std::unique_ptr<ParquetFileReader> reader,
+      const ArrowReaderProperties& properties);
+
+  /// Factory function to create a FileReader from a ParquetFileReader
+  static ::arrow::Result<std::unique_ptr<FileReader>> Make(
+      ::arrow::MemoryPool* pool, std::unique_ptr<ParquetFileReader> reader);
   // Since the distribution of columns amongst a Parquet file's row groups may
   // be uneven (the number of values in each column chunk can be different), we
   // provide a column-oriented read interface. The ColumnReader hides the
