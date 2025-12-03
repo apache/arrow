5eee68425ffeca27b10997a6c1d4ff7fc9aa8ca9
Antoine Pitrou <antoine@python.org>
2025-12-03 10:17:46 +0100

    Add comment and additional debug check

--- cpp/src/parquet/decoder.cc
+++ cpp/src/parquet/decoder.cc
@@ -776,6 +776,8 @@ class PlainByteArrayDecoder : public PlainDecoder<ByteArrayType> {
             // This precondition follows from those two checks.
             DCHECK_GE(len_, 4);
             auto value_len = SafeLoadAs<int32_t>(data_);
+            // `value_len <= estimated_data_length` is stricter than `value_len <= len_ - 4`
+            // due to the way `estimated_data_length` is computed.
             if (ARROW_PREDICT_FALSE(value_len < 0 || value_len > estimated_data_length)) {
               return Status::Invalid(
                   "Invalid or truncated PLAIN-encoded BYTE_ARRAY data");
@@ -789,6 +791,7 @@ class PlainByteArrayDecoder : public PlainDecoder<ByteArrayType> {
             DCHECK_GE(estimated_data_length, 0);
           }
           values_decoded += static_cast<int>(run_length);
+          DCHECK_LE(values_decoded, num_values);
           return Status::OK();
         } else {
           return helper->AppendNulls(run_length);
