3b7222b9b6bf44cf9cfc360f576712c9de89f038
Prateek Gaur <prateek.gaur@snowflake.com>
2025-12-04 18:40:24 +0000

    Add alp code
    Co-authored-by: Dhirhan Kanesalingam <dhirhan17@gmail.com>

---
+++ cpp/src/arrow/util/alp/AlpSampler.hpp
@@ -0,0 +1,110 @@
+#pragma once
+#include <optional>
+#include <vector>
+
+#include "arrow/util/span.h"
+#include "arrow/util/alp/Alp.hpp"
+
+namespace arrow {
+namespace util {
+namespace alp {
+
+/**
+ * Collects samples from data to be compressed with ALP and creates an encoding preset.
+ * Usage: Call addSample() or addSampleVector() multiple times to collect samples,
+ * then call finalize() to retrieve the resulting preset.
+ * @tparam T
+ *  The floating point type (float or double) to sample.
+ */
+template <typename T>
+class AlpSampler {
+ public:
+  /// Default constructor.
+  AlpSampler();
+
+  /// Helper struct containing the preset for ALP compression.
+  struct AlpSamplerResult {
+    AlpEncodingPreset alpPreset;
+  };
+
+  /**
+   * Add a sample of arbitrary size.
+   * The sample is internally separated into vectors on which addSampleVector() is called.
+   * @param input
+   *  The input data to sample from.
+   */
+  void addSample(arrow::util::span<const T> input);
+
+  /**
+   * Add a single vector as a sample.
+   * @param input
+   *  The input vector to add. Size should be <= AlpConstants::kAlpVectorSize.
+   */
+  void addSampleVector(arrow::util::span<const T> input);
+
+  /**
+   * Finalize sampling and generate the encoding preset.
+   * @return
+   *  An AlpSamplerResult containing the generated encoding preset.
+   */
+  AlpSamplerResult finalize();
+
+ private:
+  /// Helper struct to encapsulate settings used for sampling.
+  struct AlpSamplingParameters {
+    uint64_t numLookupValue;
+    uint64_t numSampledIncrements;
+    uint64_t numSampledValues;
+  };
+
+  /**
+   * Calculate sampling parameters for the current vector.
+   * @param numCurrentVectorValues
+   *  The number of values in the current vector.
+   * @return
+   *  The sampling parameters to use.
+   */
+  AlpSamplingParameters getAlpSamplingParameters(uint64_t numCurrentVectorValues);
+
+  /**
+   * Check if the current vector must be ignored for sampling.
+   * @param vectorsCount
+   *  The total number of vectors processed so far.
+   * @param vectorsSampledCount
+   *  The number of vectors that have been sampled so far.
+   * @param numCurrentVectorValues
+   *  The number of values in the current vector.
+   * @return
+   *  True if the current vector should be skipped, false otherwise.
+   */
+  bool mustSkipSamplingFromCurrentVector(uint64_t vectorsCount, uint64_t vectorsSampledCount,
+                                         uint64_t numCurrentVectorValues);
+
+  /// Count of vectors that have been sampled.
+  uint64_t m_vectorsSampledCount = 0;
+  /// Total count of values processed.
+  uint64_t m_totalValuesCount = 0;
+  /// Total count of vectors processed.
+  uint64_t m_vectorsCount = 0;
+  /// Number of samples stored.
+  uint64_t m_sampleStored = 0;
+  /// Samples collected from current rowgroup.
+  std::vector<std::vector<T>> m_rowgroupSample;
+
+  /// Complete vectors sampled.
+  std::vector<std::vector<T>> m_completeVectorsSampled;
+  /// Size of each sample vector.
+  const uint64_t m_sampleVectorSize;
+  /// Size of each rowgroup.
+  const uint64_t m_rowgroupSize;
+  /// Number of samples to take per vector.
+  const uint64_t m_samplesPerVector;
+  /// Number of vectors to sample per rowgroup.
+  const uint64_t m_sampleVectorsPerRowgroup;
+  /// Jump interval for rowgroup sampling.
+  const uint64_t m_rowgroupSampleJump;
+};
+
+}  // namespace alp
+}  // namespace util
+}  // namespace arrow
---
+++ cpp/src/arrow/util/alp/AlpWrapper.hpp
@@ -0,0 +1,153 @@
+#pragma once
+#include <cstddef>
+#include <optional>
+
+#include "arrow/util/alp/Alp.hpp"
+
+namespace arrow {
+namespace util {
+namespace alp {
+
+/**
+ * AlpWrapper is an interface for Adaptive Lossless floating-Point Compression (ALP)
+ * (https://dl.acm.org/doi/10.1145/3626717). For encoding, it samples the data and applies
+ * decimal compression (Alp) to floating point values.
+ * This class acts as a wrapper around the vector-based interfaces of AlpSampler and Alp.
+ */
+template <typename T>
+class AlpWrapper {
+ public:
+  /**
+   * Entry point for encoding floating point values using Alp decimal compression.
+   * @param decomp [IN]
+   *  Pointer to the input that is to be encoded
+   * @param decompSize [IN]
+   *  Size of decomp in bytes. This needs to be a multiple of sizeof(T).
+   * @param comp [OUT]
+   *  Pointer to the memory region we will encode into. The caller is responsible for ensuring this
+   *  is big enough.
+   * @param compSize [IN/OUT]
+   *  The actual size of the encoded data in bytes, expects the size of comp as input. If this is
+   *  too small, this is set to 0 and we bail out.
+   * @param enforceMode [IN]
+   *  Reserved for future use. Currently only AlpMode::kAlp is supported.
+   */
+  static void encode(const T* decomp, size_t decompSize, char* comp, size_t* compSize,
+                     std::optional<AlpMode> enforceMode = std::nullopt);
+
+  /**
+   * Entry point for decoding floating point values.
+   * @param decomp [OUT]
+   *  Pointer to the memory region we will decode into. The caller is responsible
+   *  for ensuring this is big enough.
+   * @param decompSize [IN/OUT]
+   *  The actual size of decoded data in bytes, expects the decomp size as input.
+   * @param comp [IN]
+   *  Pointer to the input that is to be decoded
+   * @param compSize [IN]
+   *  Size of the input in bytes.
+   * @tparam TargetType
+   *  The type that is used to store the output. May not be a narrowing conversion from T.
+   */
+  template <typename TargetType>
+  static void decode(TargetType* decomp, size_t* decompSize, const char* comp, size_t compSize);
+
+  /**
+   * Get the size of a compression block encoded previously with AlpWrapper::encode().
+   * @param comp [IN]
+   *  Start of the memory region containg the compression block.
+   * @param compSize [IN]
+   *  Size of the compression block.
+   * @return
+   *  The decompressed size of the block, in bytes.
+   * @tparam TargetType
+   *  The type that is used to store the output. May not be a narrowing conversion from T.
+   */
+  template <typename TargetType>
+  static uint64_t getDecompressedSize(const char* comp, uint64_t compSize);
+
+  /**
+   * Get the maximum compressed size of a uncompressed buffer.
+   * @param decompSize
+   *  The size of the uncompressed buffer in bytes.
+   * @return
+   * The maximum size of the compressed buffer.
+   */
+  static uint64_t getMaxCompressedSize(uint64_t decompSize);
+
+ private:
+  struct CompressionBlockHeader;
+
+  /**
+   * Tracks the progress of a compression operation.
+   * Used to report how much data was consumed and produced during encoding.
+   */
+  struct CompressionProgress {
+    /// Number of compressed bytes written to output.
+    uint64_t numCompressedBytesProduced = 0;
+    /// Number of input elements consumed.
+    uint64_t numUncompressedElementsTaken = 0;
+  };
+
+  /**
+   * Tracks the progress of a decompression operation.
+   * Used to report how much data was consumed and produced during decoding.
+   */
+  struct DecompressionProgress {
+    /// Number of decompressed elements written.
+    uint64_t numDecompressedElementsProduced = 0;
+    /// Number of compressed bytes consumed.
+    uint64_t numCompressedBytesTaken = 0;
+  };
+
+  /**
+   * Compress a buffer using the Alp variant.
+   * @param decomp
+   *  An array of floating point numbers containing the uncompressed data.
+   * @param elementCount
+   *  The number of of floating point numbers to be compressed.
+   * @param comp
+   *  The buffer to be compressed into.
+   * @param compSize
+   *  The size of the compression buffer.
+   * @return
+   *  The compressed size, in bytes.
+   */
+  static CompressionProgress encodeAlp(const T* decomp, uint64_t elementCount, char* comp,
+                                       size_t compSize, const AlpEncodingPreset& combinations);
+
+  /**
+   * Decompress a buffer using the Alp variant.
+   * @param decomp
+   *  The buffer to be decompressed into.
+   * @param decompElementCount
+   *  The number of of floating point numbers to be decompressed.
+   * @param comp
+   *  The compressed buffer to be decompressed.
+   * @param compSize
+   *  The size of the compressed data in the compressed buffer.
+   * @return
+   *  The number of decompressed elements.
+   * @tparam TargetType
+   *  The type that is used to store the output. May not be a narrowing conversion from T.
+   */
+  template <typename TargetType>
+  static DecompressionProgress decodeAlp(TargetType* decomp, size_t decompElementCount,
+                                         const char* comp, size_t compSize,
+                                         const AlpBitPackLayout bitPackLayout);
+
+  /**
+   * Helper function to load the CompressionBlockHeader.
+   * @param comp
+   *  The compressed buffer to be decompressed.
+   * @param compSize
+   *  The size of the compressed data in the compressed buffer.
+   * @return
+   *  The CompressionBlockHeader from comp.
+   */
+  static CompressionBlockHeader loadHeader(const char* comp, size_t compSize);
+};
+
+}  // namespace alp
+}  // namespace util
+}  // namespace arrow
