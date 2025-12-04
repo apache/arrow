2b5753ff61450a6f272c92c8630641c7b93b7446
Sten Larsson <sten@burtcorp.com>
2025-12-04 01:09:43 +0100

    Add GArrowExtractRegexSpanOptions

--- c_glib/arrow-glib/compute.hpp
+++ c_glib/arrow-glib/compute.hpp
@@ -209,3 +209,9 @@ garrow_extract_regex_options_new_raw(
   const arrow::compute::ExtractRegexOptions *arrow_options);
 arrow::compute::ExtractRegexOptions *
 garrow_extract_regex_options_get_raw(GArrowExtractRegexOptions *options);
+
+GArrowExtractRegexSpanOptions *
+garrow_extract_regex_span_options_new_raw(
+  const arrow::compute::ExtractRegexSpanOptions *arrow_options);
+arrow::compute::ExtractRegexSpanOptions *
+garrow_extract_regex_span_options_get_raw(GArrowExtractRegexSpanOptions *options);
