d5a69f4428f9d08e63c8b4b3d2c80c9bace0bcce
Prateek Gaur <prateek.gaur@snowflake.com>
2025-12-06 21:25:39 +0000

    Move hpp files to h

--- cpp/src/arrow/util/alp/alp_test.cc
+++ cpp/src/arrow/util/alp/alp_test.cc
@@ -1,20 +1,3 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
 #include <cmath>
 #include <cstdint>
 #include <random>
@@ -23,10 +6,10 @@
 #include <gtest/gtest.h>
 
 #include "arrow/testing/gtest_util.h"
-#include "arrow/util/alp/Alp.hpp"
-#include "arrow/util/alp/AlpConstants.hpp"
-#include "arrow/util/alp/AlpSampler.hpp"
-#include "arrow/util/alp/CompressFloatAlp.hpp"
+#include "arrow/util/alp/Alp.h"
+#include "arrow/util/alp/AlpConstants.h"
+#include "arrow/util/alp/AlpSampler.h"
+#include "arrow/util/alp/CompressFloatAlp.h"
 #include "arrow/util/bit_stream_utils_internal.h"
 #include "arrow/util/bpacking_internal.h"
 
