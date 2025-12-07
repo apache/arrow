3107ab561b2dca2923426414482ab5e7e05c3405
Prateek Gaur <prateek.gaur@snowflake.com>
2025-12-07 19:10:07 +0000

    Update the tests to adhere to arrow style code

--- cpp/src/arrow/util/alp/alp_test.cc
+++ cpp/src/arrow/util/alp/alp_test.cc
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #include <cmath>
 #include <cstdint>
 #include <random>
@@ -39,11 +56,11 @@ class AlpCompressionFloatTest : public ::testing::Test {
 
     // Compress
     AlpEncodingPreset preset{};  // Default preset
-    auto encoded = compressor.compressVector(input.data(), input.size(), preset);
+    auto encoded = compressor.CompressVector(input.data(), input.size(), preset);
 
     // Decompress
     std::vector<float> output(input.size());
-    compressor.decompressVector(encoded, output.data());
+    compressor.DecompressVector(encoded, AlpBitPackLayout::kNormal, output.data());
 
     // Verify
     ASSERT_EQ(output.size(), input.size());
@@ -78,8 +95,9 @@ TEST_F(AlpCompressionFloatTest, SmallValues) {
 }
 
 TEST_F(AlpCompressionFloatTest, MixedValues) {
-  std::vector<float> input = {100.5f, 200.25f, 300.125f, 400.0625f,
-                               500.03125f, 600.015625f, 700.0078125f, 800.00390625f};
+  std::vector<float> input = {100.5f,       200.25f,       300.125f,   400.0625f,
+                              500.03125f,   600.015625f,   700.0078125f,
+                              800.00390625f};
   TestCompressDecompressFloat(input);
 }
 
@@ -106,11 +124,11 @@ class AlpCompressionDoubleTest : public ::testing::Test {
 
     // Compress
     AlpEncodingPreset preset{};  // Default preset
-    auto encoded = compressor.compressVector(input.data(), input.size(), preset);
+    auto encoded = compressor.CompressVector(input.data(), input.size(), preset);
 
     // Decompress
     std::vector<double> output(input.size());
-    compressor.decompressVector(encoded, output.data());
+    compressor.DecompressVector(encoded, AlpBitPackLayout::kNormal, output.data());
 
     // Verify
     ASSERT_EQ(output.size(), input.size());
@@ -159,10 +177,10 @@ TEST(AlpIntegrationTest, LargeFloatDataset) {
 
   AlpCompression<float> compressor;
   AlpEncodingPreset preset{};
-  auto encoded = compressor.compressVector(input.data(), input.size(), preset);
+  auto encoded = compressor.CompressVector(input.data(), input.size(), preset);
 
   std::vector<float> output(input.size());
-  compressor.decompressVector(encoded, output.data());
+  compressor.DecompressVector(encoded, AlpBitPackLayout::kNormal, output.data());
 
   for (size_t i = 0; i < input.size(); ++i) {
     EXPECT_FLOAT_EQ(output[i], input[i]);
@@ -180,10 +198,10 @@ TEST(AlpIntegrationTest, LargeDoubleDataset) {
 
   AlpCompression<double> compressor;
   AlpEncodingPreset preset{};
-  auto encoded = compressor.compressVector(input.data(), input.size(), preset);
+  auto encoded = compressor.CompressVector(input.data(), input.size(), preset);
 
   std::vector<double> output(input.size());
-  compressor.decompressVector(encoded, output.data());
+  compressor.DecompressVector(encoded, AlpBitPackLayout::kNormal, output.data());
 
   for (size_t i = 0; i < input.size(); ++i) {
     EXPECT_DOUBLE_EQ(output[i], input[i]);
@@ -193,4 +211,3 @@ TEST(AlpIntegrationTest, LargeDoubleDataset) {
 }  // namespace alp
 }  // namespace util
 }  // namespace arrow
-
