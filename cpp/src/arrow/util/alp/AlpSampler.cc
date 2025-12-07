66716865e0187ece23f79b09717c4fd68609807c
Prateek Gaur <prateek.gaur@snowflake.com>
2025-12-07 18:47:14 +0000

    Update documentation to align with arrow's doxygen style

--- cpp/src/arrow/util/alp/AlpSampler.cc
+++ cpp/src/arrow/util/alp/AlpSampler.cc
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #include "arrow/util/alp/AlpSampler.h"
 
 #include <cmath>
@@ -10,6 +27,9 @@
 namespace arrow {
 namespace util {
 namespace alp {
+
+// ----------------------------------------------------------------------
+// AlpSampler implementation
 
 template <typename T>
 AlpSampler<T>::AlpSampler()
@@ -108,6 +128,9 @@ bool AlpSampler<T>::mustSkipSamplingFromCurrentVector(const uint64_t vectorsCoun
   }
   return false;
 }
+
+// ----------------------------------------------------------------------
+// Template instantiations
 
 template class AlpSampler<float>;
 template class AlpSampler<double>;
