8e85d36637197d00a46260388c0fafd474b032b6
Prateek Gaur <prateek.gaur@snowflake.com>
2025-12-07 19:21:30 +0000

    Update callers

--- cpp/src/parquet/decoder.cc
+++ cpp/src/parquet/decoder.cc
@@ -2474,7 +2474,7 @@ class AlpDecoder : public TypedDecoderImpl<DType> {
     // Fast path: decode directly into output buffer if requesting all values
     if (needs_decode_ && max_values >= this->num_values_) {
       size_t decompSize = this->num_values_ * sizeof(T);
-      ::arrow::util::alp::AlpWrapper<T>::decode(
+      ::arrow::util::alp::AlpWrapper<T>::Decode(
           buffer, &decompSize,
           reinterpret_cast<const char*>(this->data_), this->len_);
 
@@ -2489,7 +2489,7 @@ class AlpDecoder : public TypedDecoderImpl<DType> {
     if (needs_decode_) {
       decoded_buffer_.resize(this->num_values_);
       size_t decompSize = this->num_values_ * sizeof(T);
-      ::arrow::util::alp::AlpWrapper<T>::decode(
+      ::arrow::util::alp::AlpWrapper<T>::Decode(
           decoded_buffer_.data(), &decompSize,
           reinterpret_cast<const char*>(this->data_), this->len_);
       needs_decode_ = false;
@@ -2524,7 +2524,7 @@ class AlpDecoder : public TypedDecoderImpl<DType> {
     if (needs_decode_) {
       decoded_buffer_.resize(this->num_values_);
       size_t decompSize = this->num_values_ * sizeof(T);
-      ::arrow::util::alp::AlpWrapper<T>::decode(
+      ::arrow::util::alp::AlpWrapper<T>::Decode(
           decoded_buffer_.data(), &decompSize,
           reinterpret_cast<const char*>(this->data_), this->len_);
       needs_decode_ = false;
