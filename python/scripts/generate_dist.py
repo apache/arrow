33f1a7753e183240f81db734ff78838899e8c7c6
Will Ayd <william.ayd@icloud.com>
2025-12-07 21:38:43 -0500

    Use symlinks for NOTICE/LICENSE.txt

---
+++ python/scripts/generate_dist.py
@@ -0,0 +1,35 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+import pathlib
+import shutil
+
+
+def main():
+    src_dir = pathlib.Path(os.environ["MESON_SOURCE_ROOT"])
+    parent_dir = src_dir.parent.resolve()
+    dest_dir = pathlib.Path(os.environ["MESON_DIST_ROOT"]).resolve()
+
+    license_file = parent_dir / 'LICENSE.txt'
+    shutil.copy(license_file, dest_dir)
+    notice_file = parent_dir / 'NOTICE.txt'
+    shutil.copy(notice_file, dest_dir)
+
+
+if __name__ == "__main__":
+    main()
