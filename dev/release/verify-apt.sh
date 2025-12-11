d5141da8d2e1d635b711605f5ecf97db1db9d133
Raúl Cumplido <raulcumplido@gmail.com>
2025-12-11 10:17:40 +0100

    Apply suggestions from code review
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- dev/release/verify-apt.sh
+++ dev/release/verify-apt.sh
@@ -228,7 +228,8 @@ echo "::endgroup::"
 echo "::group::Prepare downgrade test"
 can_downgrade=false
 if [ "${distribution}" = 'debian' ] && [[ "$(cat /etc/debian_version)" =~ /sid$ ]]; then
-  # Skip downgrade test on Debian sid (unstable)
+  # Skip downgrade test on Debian testing.
+  # Debian testing and unstable use "${testing_code_name}/"sid" as /etc/debian_version content.
   :
 elif [ -f /etc/apt/sources.list.d/apache-arrow.sources.bak ]; then
   mv /etc/apt/sources.list.d/apache-arrow.sources \
