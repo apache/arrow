d5e2612fb26a2996245102fd35c492261f486dd0
Sten Larsson <sten@burtcorp.com>
2025-12-14 08:11:04 +0100

    Pass arrow::internal::ThraedPool as property instead of setting directly via priv

--- c_glib/arrow-glib/thread-pool.cpp
+++ c_glib/arrow-glib/thread-pool.cpp
@@ -42,12 +42,36 @@ G_DEFINE_TYPE_WITH_PRIVATE(GArrowThreadPool, garrow_thread_pool, G_TYPE_OBJECT)
   static_cast<GArrowThreadPoolPrivate *>(                                                \
     garrow_thread_pool_get_instance_private(GARROW_THREAD_POOL(obj)))
 
+enum {
+  PROP_THREAD_POOL = 1,
+};
+
 static void
 garrow_thread_pool_finalize(GObject *object)
 {
   auto priv = GARROW_THREAD_POOL_GET_PRIVATE(object);
   priv->thread_pool.~shared_ptr();
   G_OBJECT_CLASS(garrow_thread_pool_parent_class)->finalize(object);
+}
+
+static void
+garrow_thread_pool_set_property(GObject *object,
+                                guint prop_id,
+                                const GValue *value,
+                                GParamSpec *pspec)
+{
+  auto priv = GARROW_THREAD_POOL_GET_PRIVATE(object);
+
+  switch (prop_id) {
+  case PROP_THREAD_POOL:
+    priv->thread_pool =
+      *static_cast<std::shared_ptr<arrow::internal::ThreadPool> *>(
+        g_value_get_pointer(value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+    break;
+  }
 }
 
 static void
@@ -63,6 +87,15 @@ garrow_thread_pool_class_init(GArrowThreadPoolClass *klass)
   auto gobject_class = G_OBJECT_CLASS(klass);
 
   gobject_class->finalize = garrow_thread_pool_finalize;
+  gobject_class->set_property = garrow_thread_pool_set_property;
+
+  GParamSpec *spec;
+  spec = g_param_spec_pointer(
+    "thread-pool",
+    "Thread pool",
+    "The raw std::shared_ptr<arrow::internal::ThreadPool> *",
+    static_cast<GParamFlags>(G_PARAM_WRITABLE | G_PARAM_CONSTRUCT_ONLY));
+  g_object_class_install_property(gobject_class, PROP_THREAD_POOL, spec);
 }
 
 /**
@@ -80,9 +113,9 @@ garrow_thread_pool_new(guint n_threads, GError **error)
 {
   auto arrow_thread_pool_result = arrow::internal::ThreadPool::Make(n_threads);
   if (garrow::check(error, arrow_thread_pool_result, "[thread-pool][new]")) {
-    auto thread_pool = GARROW_THREAD_POOL(g_object_new(GARROW_TYPE_THREAD_POOL, NULL));
-    auto priv = GARROW_THREAD_POOL_GET_PRIVATE(thread_pool);
-    priv->thread_pool = *arrow_thread_pool_result;
+    auto arrow_thread_pool = *arrow_thread_pool_result;
+    auto thread_pool = GARROW_THREAD_POOL(
+      g_object_new(GARROW_TYPE_THREAD_POOL, "thread-pool", &arrow_thread_pool, nullptr));
     return thread_pool;
   } else {
     return NULL;
