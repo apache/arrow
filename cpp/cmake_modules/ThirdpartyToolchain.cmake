9958f52f36c739641cd7d36ae2c034a31b0702dd
Sutou Kouhei <kou@cozmixng.org>
2025-12-16 10:15:33 +0900

    Add a new line

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -3053,6 +3053,7 @@ function(build_absl)
   if(CMAKE_VERSION VERSION_LESS 3.28)
     set_property(DIRECTORY ${absl_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL TRUE)
   endif()
+
   if(APPLE)
     # This is due to upstream absl::cctz issue
     # https://github.com/abseil/abseil-cpp/issues/283
