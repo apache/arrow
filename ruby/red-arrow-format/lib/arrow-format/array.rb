3cafd6ae3385053da1b84a4472090ae2ca4c34c1
Sutou Kouhei <kou@clear-code.com>
2025-12-16 14:47:19 +0900

    GH-48553: [Ruby] Add support for reading timestamp array

--- ruby/red-arrow-format/lib/arrow-format/array.rb
+++ ruby/red-arrow-format/lib/arrow-format/array.rb
@@ -183,6 +183,12 @@ module ArrowFormat
     end
   end
 
+  class TimestampArray < TemporalArray
+    def to_a
+      apply_validity(@values_buffer.values(:s64, 0, @size))
+    end
+  end
+
   class VariableSizeBinaryLayoutArray < Array
     def initialize(type, size, validity_buffer, offsets_buffer, values_buffer)
       super(type, size, validity_buffer)
