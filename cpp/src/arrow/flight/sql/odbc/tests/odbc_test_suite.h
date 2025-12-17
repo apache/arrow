c58bfe5e81bf828370020c1dae3b4949a8889de1
Alina (Xi) Li <96995091+alinaliBQ@users.noreply.github.com>
2025-12-16 19:57:08 -0800

    GH-48312: [C++][FlightRPC] Standalone ODBC MSVC CI (#48313)
    ### Rationale for this change
    https://github.com/apache/arrow/issues/48312
    
    ### What changes are included in this PR?
    - Add new ODBC workflow for Windows MSVC
    - Added build fixes to enable build on MSVC CI
    
    ### Are these changes tested?
    Tested in CI
    ### Are there any user-facing changes?
    N/A
    * GitHub Issue: #48312
    
    Authored-by: Alina (Xi) Li <alina.li@improving.com>
    Signed-off-by: Sutou Kouhei <kou@clear-code.com>

--- cpp/src/arrow/flight/sql/odbc/tests/odbc_test_suite.h
+++ cpp/src/arrow/flight/sql/odbc/tests/odbc_test_suite.h
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#pragma once
+
 #include "arrow/testing/gtest_util.h"
 #include "arrow/util/io_util.h"
 #include "arrow/util/utf8.h"
