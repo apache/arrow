b63ada7c9f72cff237188dd7f16bf4be24f17429
Marco Arguedas <marco@tekdatum.com>
2025-12-18 18:38:26 -0600

    In types_test.cc removing a (somewhat redundant) test which caused the sanitizer to be unhappy

--- cpp/src/parquet/types_test.cc
+++ cpp/src/parquet/types_test.cc
@@ -218,11 +218,6 @@ TEST(TestIsParquetCipherSupported, SupportedCiphers) {
   ASSERT_TRUE(IsParquetCipherSupported(ParquetCipher::EXTERNAL_DBPA_V1));
 }
 
-TEST(TestIsParquetCipherSupported, UnsupportedCiphers) {
-  ParquetCipher::type unsupported_cipher = static_cast<ParquetCipher::type>(100);
-  ASSERT_FALSE(IsParquetCipherSupported(unsupported_cipher));
-}
-
 #if !(defined(_WIN32) || defined(__CYGWIN__))
 #  pragma GCC diagnostic pop
 #elif _MSC_VER
