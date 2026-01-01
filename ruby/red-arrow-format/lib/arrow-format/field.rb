c7633670a791cee22bad6a821dd0bcfd9088c906
Sutou Kouhei <kou@clear-code.com>
2026-01-01 16:52:04 +0900

    GH-48705: [Ruby] Add support for reading dictionary array

--- ruby/red-arrow-format/lib/arrow-format/field.rb
+++ ruby/red-arrow-format/lib/arrow-format/field.rb
@@ -18,10 +18,12 @@ module ArrowFormat
   class Field
     attr_reader :name
     attr_reader :type
-    def initialize(name, type, nullable)
+    attr_reader :dictionary_id
+    def initialize(name, type, nullable, dictionary_id)
       @name = name
       @type = type
       @nullable = nullable
+      @dictionary_id = dictionary_id
     end
 
     def nullable?
