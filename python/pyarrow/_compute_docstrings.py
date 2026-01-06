4a1cfcefe376a4979ef124b2d31ae6b0c8e8e68e
Ruifeng Zheng <ruifengz@apache.org>
2026-01-06 21:39:53 +0800

    fix

--- python/pyarrow/_compute_docstrings.py
+++ python/pyarrow/_compute_docstrings.py
@@ -63,12 +63,27 @@ function_doc_additions["min"] = """
     >>> arr1 = pa.array([1, 1, 2, 2, 3, 2, 2, 2])
     >>> pc.min(arr1)
     <pyarrow.Int64Scalar: 1>
+
+    Using `skip_nulls` to handle null values.
+
     >>> arr2 = pa.array([1.0, None, 2.0, 3.0])
     >>> pc.min(arr2)
     <pyarrow.DoubleScalar: 1.0>
+    >>> pc.min(arr2, skip_nulls=False)
+    <pyarrow.DoubleScalar: None>
+
+    Using `ScalarAggregateOptions` to control minimum number of non-null values.
+
     >>> arr3 = pa.array([1.0, None, float("nan"), 3.0])
     >>> pc.min(arr3)
     <pyarrow.DoubleScalar: 1.0>
+    >>> pc.min(arr3, options=pc.ScalarAggregateOptions(min_count=3))
+    <pyarrow.DoubleScalar: 1.0>
+    >>> pc.min(arr3, options=pc.ScalarAggregateOptions(min_count=4))
+    <pyarrow.DoubleScalar: None>
+
+    This function also works with string values.
+
     >>> arr4 = pa.array(["z", None, "y", "x"])
     >>> pc.min(arr4)
     <pyarrow.StringScalar: 'x'>
@@ -82,12 +97,27 @@ function_doc_additions["max"] = """
     >>> arr1 = pa.array([1, 1, 2, 2, 3, 2, 2, 2])
     >>> pc.max(arr1)
     <pyarrow.Int64Scalar: 3>
+
+    Using `skip_nulls` to handle null values.
+
     >>> arr2 = pa.array([1.0, None, 2.0, 3.0])
     >>> pc.max(arr2)
     <pyarrow.DoubleScalar: 3.0>
+    >>> pc.max(arr2, skip_nulls=False)
+    <pyarrow.DoubleScalar: None>
+
+    Using `ScalarAggregateOptions` to control minimum number of non-null values.
+
     >>> arr3 = pa.array([1.0, None, float("nan"), 3.0])
     >>> pc.max(arr3)
     <pyarrow.DoubleScalar: 3.0>
+    >>> pc.max(arr3, options=pc.ScalarAggregateOptions(min_count=3))
+    <pyarrow.DoubleScalar: 3.0>
+    >>> pc.max(arr3, options=pc.ScalarAggregateOptions(min_count=4))
+    <pyarrow.DoubleScalar: None>
+
+    This function also works with string values.
+
     >>> arr4 = pa.array(["z", None, "y", "x"])
     >>> pc.max(arr4)
     <pyarrow.StringScalar: 'z'>
@@ -100,13 +130,28 @@ function_doc_additions["min_max"] = """
     >>> import pyarrow.compute as pc
     >>> arr1 = pa.array([1, 1, 2, 2, 3, 2, 2, 2])
     >>> pc.min_max(arr1)
+
+    Using `skip_nulls` to handle null values.
+
     <pyarrow.StructScalar: [('min', 1), ('max', 3)]>
     >>> arr2 = pa.array([1.0, None, 2.0, 3.0])
     >>> pc.min_max(arr2)
     <pyarrow.StructScalar: [('min', 1.0), ('max', 3.0)]>
+    >>> pc.min_max(arr2, skip_nulls=False)
+    <pyarrow.StructScalar: [('min', None), ('max', None)]>
+
+    Using `ScalarAggregateOptions` to control minimum number of non-null values.
+
     >>> arr3 = pa.array([1.0, None, float("nan"), 3.0])
     >>> pc.min_max(arr3)
     <pyarrow.StructScalar: [('min', 1.0), ('max', 3.0)]>
+    >>> pc.min_max(arr3, options=pc.ScalarAggregateOptions(min_count=3))
+    <pyarrow.StructScalar: [('min', 1.0), ('max', 3.0)]>
+    >>> pc.min_max(arr3, options=pc.ScalarAggregateOptions(min_count=4))
+    <pyarrow.StructScalar: [('min', None), ('max', None)]>
+
+    This function also works with string values.
+
     >>> arr4 = pa.array(["z", None, "y", "x"])
     >>> pc.min_max(arr4)
     <pyarrow.StructScalar: [('min', 'x'), ('max', 'z')]>
