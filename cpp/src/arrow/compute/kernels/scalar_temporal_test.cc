311d4a3005420386fd1f544c46a27355b26d738f
Rok Mihevc <rok@mihevc.org>
2026-01-06 21:55:38 +0100

    enable std::chrono for mingw

--- cpp/src/arrow/compute/kernels/scalar_temporal_test.cc
+++ cpp/src/arrow/compute/kernels/scalar_temporal_test.cc
@@ -709,12 +709,6 @@ TEST_F(ScalarTemporalTest, TestIsLeapYear) {
 }
 
 TEST_F(ScalarTemporalTest, TestZoned1) {
-  // TODO(GH-48743): Re-enable when GCC bug is fixed
-  // https://github.com/apache/arrow/issues/48743
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=116110
-#if defined(_WIN32) && !defined(_MSC_VER)
-  GTEST_SKIP() << "Test triggers GCC bug TODO(GH-48743).";
-#endif
   std::vector<std::string> timezones = {"Pacific/Marquesas", "-09:30"};
   for (const auto& timezone : timezones) {
     auto unit = timestamp(TimeUnit::NANO, timezone);
@@ -813,12 +807,6 @@ TEST_F(ScalarTemporalTest, TestZoned1) {
 }
 
 TEST_F(ScalarTemporalTest, TestZoned2) {
-  // TODO(GH-48743): Re-enable when GCC bug is fixed
-  // https://github.com/apache/arrow/issues/48743
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=116110
-#if defined(_WIN32) && !defined(_MSC_VER)
-  GTEST_SKIP() << "Test triggers GCC bug TODO(GH-48743).";
-#endif
   for (auto u : TimeUnit::values()) {
     auto unit = timestamp(u, "Australia/Broken_Hill");
     auto month = "[1, 3, 1, 5, 1, 12, 12, 12, 1, 1, 1, 1, 12, 12, 12, 1, null]";
@@ -2780,12 +2768,6 @@ TEST_F(ScalarTemporalTestMultipleSinceGreaterUnit, CeilUTC) {
 }
 
 TEST_F(ScalarTemporalTestMultipleSinceGreaterUnit, CeilZoned) {
-  // TODO(GH-48743): Re-enable when GCC bug is fixed
-  // https://github.com/apache/arrow/issues/48743
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=116110
-#if defined(_WIN32) && !defined(_MSC_VER)
-  GTEST_SKIP() << "Test triggers GCC bug TODO(GH-48743).";
-#endif
   std::string op = "ceil_temporal";
 
   // Data for tests below was generated via lubridate with the exception
@@ -3176,12 +3158,6 @@ TEST_F(ScalarTemporalTestMultipleSinceGreaterUnit, FloorUTC) {
 }
 
 TEST_F(ScalarTemporalTestMultipleSinceGreaterUnit, FloorZoned) {
-  // TODO(GH-48743): Re-enable when GCC bug is fixed
-  // https://github.com/apache/arrow/issues/48743
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=116110
-#if defined(_WIN32) && !defined(_MSC_VER)
-  GTEST_SKIP() << "Test triggers GCC bug TODO(GH-48743).";
-#endif
   std::string op = "floor_temporal";
 
   // Data for tests below was generated via lubridate with the exception
@@ -3615,12 +3591,6 @@ TEST_F(ScalarTemporalTestMultipleSinceGreaterUnit, RoundUTC) {
 }
 
 TEST_F(ScalarTemporalTestMultipleSinceGreaterUnit, RoundZoned) {
-  // TODO(GH-48743): Re-enable when GCC bug is fixed
-  // https://github.com/apache/arrow/issues/48743
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=116110
-#if defined(_WIN32) && !defined(_MSC_VER)
-  GTEST_SKIP() << "Test triggers GCC bug TODO(GH-48743).";
-#endif
   std::string op = "round_temporal";
 
   // Data for tests below was generated via lubridate with the exception
