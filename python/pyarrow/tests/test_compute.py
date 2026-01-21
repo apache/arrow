89c7c6a2d62ad1323a954908be80891eb795ef11
Rok Mihevc <rok@mihevc.org>
2026-01-21 01:20:34 +0100

    print if assert False

--- python/pyarrow/tests/test_compute.py
+++ python/pyarrow/tests/test_compute.py
@@ -2390,7 +2390,8 @@ def test_strftime():
                     is_valid = pc.or_kleene(pc.match_substring_regex(
                         result, "^GMT[+-][0-9]+$"), pc.is_null(result))
                     assert not pc.any(pc.invert(is_valid)).as_py(), \
-                        "All timezone values should be GMT offset format (e.g. GMT+1)"
+                        "All timezone values should be GMT offset format (e.g. GMT+1)" \
+                        f"Actual: {result}"
                 else:
                     assert result.equals(expected)
 
@@ -2413,7 +2414,8 @@ def test_strftime():
             is_valid = pc.or_kleene(pc.match_substring_regex(
                 result, "^GMT[+-][0-9]+$"), pc.is_null(result))
             assert not pc.any(pc.invert(is_valid)).as_py(), \
-                "All timezone values should be GMT offset format (e.g. GMT+1)"
+                "All timezone values should be GMT offset format (e.g. GMT+1)" \
+                f"Actual: {result}"
         else:
             assert result.equals(expected)
 
