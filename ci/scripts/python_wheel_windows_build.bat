3fa0ee205fa975e473286b05af461f18fe310239
Raúl Cumplido <raulcumplido@gmail.com>
2026-01-26 18:03:30 +0100

    Try delvewheel --add-path to repair wheels so we bundle arrow CPP

--- ci/scripts/python_wheel_windows_build.bat
+++ ci/scripts/python_wheel_windows_build.bat
@@ -225,6 +225,7 @@ echo "Wheel name: %WHEEL_NAME%"
 
 %PYTHON_CMD% -m delvewheel repair -vv ^
     --ignore-existing --with-mangle ^
+    --add-path "C:\Program Files\arrow\bin" ^
     -w repaired_wheels %WHEEL_NAME% || exit /B 1
 
 popd
