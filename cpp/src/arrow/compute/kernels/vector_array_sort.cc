548b56b6b80124ae65f1668c9a4b0993f1341587
Hyukjin Kwon <gurwls223@apache.org>
2026-01-28 17:11:33 +0900

    Address a review comment

--- cpp/src/arrow/compute/kernels/vector_array_sort.cc
+++ cpp/src/arrow/compute/kernels/vector_array_sort.cc
@@ -183,8 +183,8 @@ class ArrayCompareSorter<DictionaryType> {
                                          const ArraySortOptions& options,
                                          ExecContext* ctx) {
     const auto& dict_array = checked_cast<const DictionaryArray&>(array);
-    auto dict_values = dict_array.dictionary();
-    auto dict_indices = dict_array.indices();
+    const auto& dict_values = dict_array.dictionary();
+    const auto& dict_indices = dict_array.indices();
 
     // Algorithm:
     // 1) Use the Rank function to get an exactly-equivalent-order array
