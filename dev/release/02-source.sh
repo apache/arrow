d9c07a8ea76fa2e4fc1b4624f24e638dc396e7e4
Bryce Mecum <petridish@gmail.com>
2026-02-12 11:50:23 -0800

    Fail when checksum doesn't exist

--- dev/release/02-source.sh
+++ dev/release/02-source.sh
@@ -131,6 +131,7 @@ if [ ${SOURCE_VOTE} -gt 0 ]; then
   curl_options+=(https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls)
   verify_pr_url=$(curl "${curl_options[@]}" | jq -r ".[0].html_url")
   # Read the checksum so we can include it in the vote thread email.
+  [[ -f "artifacts/${tarball}.sha512" ]] || { echo "Error: artifacts/${tarball}.sha512 must exist"; exit 1; }
   tarball_hash=$(cat "artifacts/${tarball}.sha512" | awk '{print $1}')
 
   echo "The following draft email has been created to send to the"
