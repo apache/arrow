b485e0f6ebeb0e70869233ef4064eb640e00f1e2
Bryce Mecum <petridish@gmail.com>
2026-02-12 12:56:59 -0800

    Update 02-source-test.rb

--- dev/release/02-source-test.rb
+++ dev/release/02-source-test.rb
@@ -113,7 +113,7 @@ class SourceTest < Test::Unit::TestCase
       verify_pr_url = (JSON.parse(response.read)[0] || {})["html_url"]
     end
     output = source("VOTE")
-    tarball_hash = Digest::SHA512.file(@archive_name).to_s
+    tarball_hash = Digest::SHA512.file("artifacts/#{@archive_name}).to_s
     assert_equal(<<-VOTE.strip, output[/^-+$(.+?)^-+$/m, 1].strip)
 To: dev@arrow.apache.org
 Subject: [VOTE] Release Apache Arrow #{@release_version} - RC0
