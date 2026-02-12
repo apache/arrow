8ea4395f974aac0a038af3ae11798cc229efc6f9
Bryce Mecum <petridish@gmail.com>
2026-02-12 11:53:32 -0800

    Add require

--- dev/release/02-source-test.rb
+++ dev/release/02-source-test.rb
@@ -15,6 +15,8 @@
 # specific language governing permissions and limitations
 # under the License.
 
+require 'digest'
+
 class SourceTest < Test::Unit::TestCase
   include GitRunnable
   include VersionDetectable
