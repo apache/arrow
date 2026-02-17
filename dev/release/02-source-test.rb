96fd0b1bf7a9577556c27985ac6d4d2cecb131dc
Bryce Mecum <petridish@gmail.com>
2026-02-16 17:52:23 -0800

    move requrie

--- dev/release/02-source-test.rb
+++ dev/release/02-source-test.rb
@@ -15,8 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-require 'digest'
-
 class SourceTest < Test::Unit::TestCase
   include GitRunnable
   include VersionDetectable
