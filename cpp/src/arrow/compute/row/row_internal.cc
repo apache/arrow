4198be1be9c12df9d78b9ed62aa51855bfbaf681
Antoine Pitrou <antoine@python.org>
2026-02-18 08:57:29 +0100

    Nit: reorder includes

--- cpp/src/arrow/compute/row/row_internal.cc
+++ cpp/src/arrow/compute/row/row_internal.cc
@@ -14,9 +14,10 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-#include <bit>
 
 #include "arrow/compute/row/row_internal.h"
+
+#include <bit>
 
 #include "arrow/compute/util.h"
 #include "arrow/util/logging_internal.h"
--- cpp/src/arrow/compute/util.cc
+++ cpp/src/arrow/compute/util.cc
@@ -14,9 +14,10 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-#include <bit>
 
 #include "arrow/compute/util.h"
+
+#include <bit>
 
 #include "arrow/util/logging.h"
 #include "arrow/util/ubsan.h"
