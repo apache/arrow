5b6a759dcc249b995a95c83617a23737fb2ee127
Jonathan Keane <jkeane@gmail.com>
2026-02-26 23:13:41 -0600

    Another place

--- cpp/src/arrow/util/align_util.h
+++ cpp/src/arrow/util/align_util.h
@@ -44,8 +44,15 @@ struct BitmapWordAlignParams {
 template <uint64_t ALIGN_IN_BYTES>
 inline BitmapWordAlignParams BitmapWordAlign(const uint8_t* data, int64_t bit_offset,
                                              int64_t length) {
+  // TODO: We can remove this condition once CRAN upgrades its macOS
+  // SDK from 11.3.
+#if defined(__clang__) && !defined(__cpp_lib_bitops)
+  static_assert((ALIGN_IN_BYTES != 0) && ((ALIGN_IN_BYTES & (ALIGN_IN_BYTES - 1)) == 0),
+                "ALIGN_IN_BYTES should be a positive power of two");
+#else
   static_assert(std::has_single_bit(ALIGN_IN_BYTES),
                 "ALIGN_IN_BYTES should be a positive power of two");
+#endif
   constexpr uint64_t ALIGN_IN_BITS = ALIGN_IN_BYTES * 8;
 
   BitmapWordAlignParams p;
