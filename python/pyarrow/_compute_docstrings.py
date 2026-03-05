cbc50995fb5463833f555e7a4896e8092367f10e
Ruifeng Zheng <ruifengz@apache.org>
2026-03-05 17:05:19 +0800

    fix

--- python/pyarrow/_compute_docstrings.py
+++ python/pyarrow/_compute_docstrings.py
@@ -236,7 +236,7 @@ function_doc_additions["first_last"] = """
 
     >>> arr2 = pa.array([None, 2.0, 3.0, None])
     >>> pc.first_last(arr2)
-    <pyarrow.StructScalar: [('first', 1.0), ('last', 3.0)]>
+    <pyarrow.StructScalar: [('first', 2.0), ('last', 3.0)]>
     >>> pc.first_last(arr2, skip_nulls=False)
     <pyarrow.StructScalar: [('first', None), ('last', None)]>
 
