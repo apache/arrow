90688861b3351423f549e3204d205a084532047d
Collin Brown <collinbrown95@gmail.com>
2026-03-15 04:20:26 +0000

    Add most test cases from test_filesystem and recreate a couple that were skipped because of the URI issue.

--- r/tests/testthat/test-azure.R
+++ r/tests/testthat/test-azure.R
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-library(arrow)
+
 skip_if_not_available("azure")
 
 # test_filesystem requires dplyr
