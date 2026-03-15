dee8d83ac99774385441d8b64c2c03ba8777c479
Collin Brown <collinbrown95@gmail.com>
2026-03-15 04:20:26 +0000

    Add most test cases from test_filesystem and recreate a couple that were skipped because of the URI issue.

--- r/tests/testthat/test-azure.R
+++ r/tests/testthat/test-azure.R
@@ -14,8 +14,12 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-library(arrow)
+
 skip_if_not_available("azure")
+
+# test_filesystem requires dplyr
+library(dplyr)
+
 # TODO: Add local azurite install to setup script
 # skip_if_not(nzchar(Sys.which("azurite")), message = "azurite is not installed.")
 
@@ -39,6 +43,7 @@ azure_uri <- function(...) {
   encoded_key <- curl::curl_escape(azurite_account_key)
   sprintf(template, azurite_account_name, encoded_key, azure_path(...), endpoint)
 }
+
 azure_path <- function(...) {
   # 'dir' is the container name (following the convention in the s3 tests).
   paste(dir, ..., sep = "/")
@@ -51,25 +56,20 @@ fs <- AzureFileSystem$create(
   blob_storage_scheme=azurite_blob_storage_scheme
 )
 
-fs2 <- arrow:::az_bucket(
-  bucket="test",
-  account_name=azurite_account_name,
-  account_key=azurite_account_key,
-  blob_storage_authority=azurite_blob_storage_authority,
-  blob_storage_scheme=azurite_blob_storage_scheme
-)
-
-# TODO: Factor these into tests once finished debugging.
-
 # (1) CreateDir and DeleteDir work correctly
 dir <- "test"
 fs$CreateDir(dir)
 # Clean up when we're all done
-withr::defer(fs$DeleteDir(now))
+withr::defer(fs$DeleteDir(dir))
 
-# (XX) Run default filesystem tests on azure filesystem
-# TODO: As far as I can tell, there is no way to pass an Azurite URI to write_feather
-#test_filesystem("azure", fs, azure_path, azure_uri)
+# (2) Run default filesystem tests on azure filesystem
+
+# TODO: As far as I can tell, there is no way to pass an Azurite URI to write_feather,
+# so some of the test_filesystem tests can't be run with AzureFilesystem. Some tests
+# below cover some of the skipped cases in test_filesystem.
+test_filesystem("azure", fs, azure_path, azure_uri)
+
+# (3) Test write/read parquet
 
 example_data <- tibble::tibble(
   int = c(1:3, NA_integer_, 5:10),
@@ -81,13 +81,38 @@ example_data <- tibble::tibble(
   fct = factor(letters[c(1:4, NA, NA, 7:10)])
 )
 
-# Verify that write file operation works
-write_feather(example_data, azure_uri("test.feather"))
+test_that("read/write Parquet on azure", {
+  skip_if_not_available("parquet")
+  write_parquet(example_data, fs$path(azure_path("test.parquet")))
+  expect_identical(read_parquet(fs$path(azure_path("test.parquet"))), example_data)
+})
 
-encoded_key <- curl::curl_escape(azurite_account_key)
-encoded_key
-write_feather(example_data, sprintf("abfs://devstoreaccount1:%s@127.0.0.1:10000/test/test.feather", encoded_key))
+# (4) open_dataset with a vector of azure file paths
 
-write_feather(example_data, "az://test@devstoreaccount1:Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq%2FK1SZFPTOtr%2FKBHBeksoGMGw%3D%3D@127.0.0.1:10000/test/test.feather")
+# TODO: I couldn't pass a vector of paths similar to the original test in
+# test_filesystem, but you can pass a folder containing many files.
+write_feather(example_data, fs$path(azure_path("openmulti/dataset1.feather")))
+write_feather(example_data, fs$path(azure_path("openmulti/dataset2.feather")))
+
+open_multi_fs = arrow:::az_bucket(
+  bucket=azure_path("openmulti"),
+  account_name=azurite_account_name,
+  account_key=azurite_account_key,
+  blob_storage_authority=azurite_blob_storage_authority,
+  blob_storage_scheme=azurite_blob_storage_scheme
+)
+
+test_that("open_dataset with AzureFileSystem folder", {
+  expect_identical(
+    open_dataset(
+      open_multi_fs,
+      format = "feather"
+    ) |>
+      arrange(int) |>
+      collect(),
+    rbind(example_data, example_data) |> arrange(int)
+  )
+})
+
 
 
