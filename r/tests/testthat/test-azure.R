3185eb5794f896843ae26121be0c4ca08731b79b
Collin Brown <collinbrown95@gmail.com>
2026-03-15 02:27:20 +0000

    checkpoint: resolved segfault error
    At this point, I was able to call write_feather(example_data, fs$path("test/test.feather")) successfully against Azurite.
    
    Committing progress before I make any further changes.

--- r/tests/testthat/test-azure.R
+++ r/tests/testthat/test-azure.R
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
+library(arrow)
 skip_if_not_available("azure")
 # TODO: Add local azurite install to setup script
 # skip_if_not(nzchar(Sys.which("azurite")), message = "azurite is not installed.")
@@ -26,35 +26,68 @@ skip_if_not_available("azure")
 azurite_account_name <- "devstoreaccount1"
 # Note that this is a well-known default credential for local development on Azurite.
 azurite_account_key <- "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
-azurite_blob_host <- "127.0.0.1"
+azurite_blob_host <- "host.docker.internal"
 azurite_blob_port <- "10000"
 azurite_blob_storage_authority <- sprintf("%s:%s",azurite_blob_host, azurite_blob_port)
 azurite_blob_storage_scheme <- "http"
 
 # Helper functions for Azure URIs and paths
 azure_uri <- function(...) {
-  template <- "az://%s:%s@%s?scheme=http&blob_endpoint=localhost%s%s"
+  endpoint <- sprintf("%s%s%s", azurite_blob_host, "%3A", azurite_blob_port)
+  template <- "abfs://%s:%s@%s?endpoint=%s"
   # URL encode the account key because it contains reserved characters
   encoded_key <- curl::curl_escape(azurite_account_key)
-  sprintf(template, azurite_account_name, encoded_key, azure_path(...), "%3A", azurite_blob_port)
+  sprintf(template, azurite_account_name, encoded_key, azure_path(...), endpoint)
 }
-
-azure_path <- azure_path <- function(...) {
-  # 'now' is the container name (following the convention in the s3 tests).
-  paste(now, ..., sep = "/")
+azure_path <- function(...) {
+  # 'dir' is the container name (following the convention in the s3 tests).
+  paste(dir, ..., sep = "/")
 }
 
 fs <- AzureFileSystem$create(
-  account_name="devstoreaccount1",
-  account_key="Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==",
-  blob_storage_authority="127.0.0.1:10000",
-  blob_storage_scheme="http"
+  account_name=azurite_account_name,
+  account_key=azurite_account_key,
+  blob_storage_authority=azurite_blob_storage_authority,
+  blob_storage_scheme=azurite_blob_storage_scheme
 )
 
-now <- as.character(as.numeric(Sys.time()))
-fs$CreateDir(now)
+fs2 <- arrow:::az_bucket(
+  bucket="test",
+  account_name=azurite_account_name,
+  account_key=azurite_account_key,
+  blob_storage_authority=azurite_blob_storage_authority,
+  blob_storage_scheme=azurite_blob_storage_scheme
+)
+
+# TODO: Factor these into tests once finished debugging.
+
+# (1) CreateDir and DeleteDir work correctly
+dir <- "test"
+fs$CreateDir(dir)
 # Clean up when we're all done
 withr::defer(fs$DeleteDir(now))
 
-# (1) Run default filesystem tests on azure filesystem
-test_filesystem("azure", fs, azure_path, azure_uri)
+# (XX) Run default filesystem tests on azure filesystem
+# TODO: As far as I can tell, there is no way to pass an Azurite URI to write_feather
+#test_filesystem("azure", fs, azure_path, azure_uri)
+
+example_data <- tibble::tibble(
+  int = c(1:3, NA_integer_, 5:10),
+  dbl = c(1:8, NA, 10) + 0.1,
+  dbl2 = rep(5, 10),
+  lgl = sample(c(TRUE, FALSE, NA), 10, replace = TRUE),
+  false = logical(10),
+  chr = letters[c(1:5, NA, 7:10)],
+  fct = factor(letters[c(1:4, NA, NA, 7:10)])
+)
+
+# Verify that write file operation works
+write_feather(example_data, azure_uri("test.feather"))
+
+encoded_key <- curl::curl_escape(azurite_account_key)
+encoded_key
+write_feather(example_data, sprintf("abfs://devstoreaccount1:%s@127.0.0.1:10000/test/test.feather", encoded_key))
+
+write_feather(example_data, "az://test@devstoreaccount1:Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq%2FK1SZFPTOtr%2FKBHBeksoGMGw%3D%3D@127.0.0.1:10000/test/test.feather")
+
+
