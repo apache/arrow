776c12c10e4598df817ab633a5885f6a98accea3
Antoine Pitrou <antoine@python.org>
2026-04-22 12:22:36 +0200

    Use move-constructed std::ostringstream::str()

--- cpp/src/arrow/util/logging.cc
+++ cpp/src/arrow/util/logging.cc
@@ -71,7 +71,7 @@ class CerrLog {
     if (has_logged_) {
       static std::mutex cerr_mutex;
       std::lock_guard<std::mutex> lock(cerr_mutex);
-      std::cerr << buffer_.view() << std::endl;
+      std::cerr << std::move(buffer_).str() << std::endl;
     }
     if (severity_ == ArrowLogLevel::ARROW_FATAL) {
       PrintBackTrace();
