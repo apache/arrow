087372d488075b022f0d84843cd396f313a849c0
Prateek Gaur <prateek.gaur@snowflake.com>
2026-06-03 17:47:14 +0000

    Use signed integer types consistently per Arrow style guide

--- cpp/src/arrow/util/pfor/pfor.h
+++ cpp/src/arrow/util/pfor/pfor.h
@@ -46,14 +46,13 @@ template <typename T>
 struct PforVectorInfo {
   T frame_of_reference = 0;
   uint8_t bit_width = 0;
-  uint16_t num_exceptions = 0;
+  int16_t num_exceptions = 0;
 
   /// \brief Store this info to a byte buffer (little-endian)
   void Store(uint8_t* dest) const {
     std::memcpy(dest, &frame_of_reference, sizeof(T));
     dest[sizeof(T)] = bit_width;
-    uint16_t le_exceptions = num_exceptions;  // Assume LE platform
-    std::memcpy(dest + sizeof(T) + 1, &le_exceptions, sizeof(uint16_t));
+    std::memcpy(dest + sizeof(T) + 1, &num_exceptions, sizeof(int16_t));
   }
 
   /// \brief Load this info from a byte buffer (little-endian)
@@ -61,12 +60,12 @@ struct PforVectorInfo {
     PforVectorInfo info;
     std::memcpy(&info.frame_of_reference, src, sizeof(T));
     info.bit_width = src[sizeof(T)];
-    std::memcpy(&info.num_exceptions, src + sizeof(T) + 1, sizeof(uint16_t));
+    std::memcpy(&info.num_exceptions, src + sizeof(T) + 1, sizeof(int16_t));
     return info;
   }
 
   /// \brief Serialized size in bytes
-  static constexpr uint8_t kSerializedSize = PforTypeTraits<T>::kVectorInfoSize;
+  static constexpr int64_t kStoredSize = PforTypeTraits<T>::kVectorInfoSize;
 };
 
 // ----------------------------------------------------------------------
@@ -77,7 +76,7 @@ template <typename T>
 struct PforEncodedVector {
   PforVectorInfo<T> info;
   std::vector<uint8_t> packed_values;
-  std::vector<uint16_t> exception_positions;
+  std::vector<int16_t> exception_positions;
   std::vector<T> exception_values;
 };
 
@@ -87,7 +86,7 @@ struct PforEncodedVector {
 /// \brief Result of the optimal bit width search
 struct BitWidthResult {
   uint8_t bit_width = 0;
-  uint16_t num_exceptions = 0;
+  int16_t num_exceptions = 0;
 };
 
 // ----------------------------------------------------------------------
@@ -110,14 +109,14 @@ class PforCompression {
   /// \param[in] num_elements number of elements
   /// \return the optimal bit width and exception count
   static BitWidthResult FindOptimalBitWidth(const UnsignedT* deltas,
-                                            uint32_t num_elements);
+                                            int32_t num_elements);
 
   /// \brief Encode a single vector of integers
   ///
   /// \param[in] values input integer values
   /// \param[in] num_elements number of elements (up to vector_size)
   /// \return the encoded vector with all sections
-  static PforEncodedVector<T> EncodeVector(const T* values, uint32_t num_elements);
+  static PforEncodedVector<T> EncodeVector(const T* values, int32_t num_elements);
 
   /// \brief Decode a single vector from compressed data
   ///
@@ -125,11 +124,11 @@ class PforCompression {
   /// \param[in] data pointer to the start of the vector data
   /// \param[in] num_elements number of elements in this vector
   /// \return number of bytes consumed from data
-  static size_t DecodeVector(T* values, const uint8_t* data, uint32_t num_elements);
+  static int64_t DecodeVector(T* values, const uint8_t* data, int32_t num_elements);
 
   /// \brief Calculate the serialized size of an encoded vector
-  static size_t SerializedVectorSize(const PforEncodedVector<T>& vec,
-                                     uint32_t num_elements);
+  static int64_t SerializedVectorSize(const PforEncodedVector<T>& vec,
+                                      int32_t num_elements);
 
   /// \brief Serialize an encoded vector to a byte buffer
   ///
@@ -137,8 +136,8 @@ class PforCompression {
   /// \param[in] num_elements number of elements
   /// \param[out] dest output buffer (must be large enough)
   /// \return number of bytes written
-  static size_t SerializeVector(const PforEncodedVector<T>& vec,
-                                uint32_t num_elements, uint8_t* dest);
+  static int64_t SerializeVector(const PforEncodedVector<T>& vec,
+                                 int32_t num_elements, uint8_t* dest);
 };
 
 }  // namespace pfor
