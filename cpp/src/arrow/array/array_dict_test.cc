c8ac6ef6d16fee4cd0cf6c95a3976ccc5cb4db42
Nic Crane <thisisnic@gmail.com>
2026-06-10 11:49:40 +0100

    Update to use Result returning variants

--- cpp/src/arrow/array/array_dict_test.cc
+++ cpp/src/arrow/array/array_dict_test.cc
@@ -1767,7 +1767,7 @@ TEST(TestDictionaryBuilderOrdered, TypePreservesOrderedFlag) {
   for (bool ordered : {true, false}) {
     ARROW_SCOPED_TRACE("ordered = ", ordered);
     auto dict_type = dictionary(int8(), utf8(), ordered);
-    ASSERT_OK_AND_ASSIGN(auto boxed_buider, MakeBuilder(dict_type));
+    ASSERT_OK_AND_ASSIGN(auto boxed_builder, MakeBuilder(dict_type));
 
     auto builder_type = boxed_builder->type();
     ASSERT_EQ(checked_cast<const DictionaryType&>(*builder_type).ordered(), ordered);
@@ -1778,8 +1778,7 @@ TEST(TestDictionaryBuilderOrdered, FinishPreservesOrderedFlag) {
   for (bool ordered : {true, false}) {
     ARROW_SCOPED_TRACE("ordered = ", ordered);
     auto dict_type = dictionary(int8(), utf8(), ordered);
-    std::unique_ptr<ArrayBuilder> boxed_builder;
-    ASSERT_OK(MakeBuilder(default_memory_pool(), dict_type, &boxed_builder));
+    ASSERT_OK_AND_ASSIGN(auto boxed_builder, MakeBuilder(dict_type));
     auto& builder = checked_cast<DictionaryBuilder<StringType>&>(*boxed_builder);
 
     ASSERT_OK(builder.Append("a"));
@@ -1805,8 +1804,7 @@ TEST(TestDictionaryBuilderOrdered, ListOfOrderedDictionary) {
     auto dict_type = dictionary(int8(), utf8(), ordered);
     auto list_type = list(field("item", dict_type));
 
-    std::unique_ptr<ArrayBuilder> boxed_builder;
-    ASSERT_OK(MakeBuilder(default_memory_pool(), list_type, &boxed_builder));
+    ASSERT_OK_AND_ASSIGN(auto boxed_builder, MakeBuilder(list_type));
     auto& list_builder = checked_cast<ListBuilder&>(*boxed_builder);
     auto& dict_builder =
         checked_cast<DictionaryBuilder<StringType>&>(*list_builder.value_builder());
@@ -1831,9 +1829,7 @@ TEST(TestDictionaryBuilderOrdered, MakeDictionaryBuilderPreservesOrdered) {
   for (bool ordered : {true, false}) {
     ARROW_SCOPED_TRACE("ordered = ", ordered);
     auto dict_type = dictionary(int8(), utf8(), ordered);
-    std::unique_ptr<ArrayBuilder> builder;
-    ASSERT_OK(MakeDictionaryBuilder(default_memory_pool(), dict_type,
-                                    /*dictionary=*/nullptr, &builder));
+    ASSERT_OK_AND_ASSIGN(auto builder, MakeDictionaryBuilder(dict_type));
 
     auto builder_type = builder->type();
     ASSERT_EQ(checked_cast<const DictionaryType&>(*builder_type).ordered(), ordered);
