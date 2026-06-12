25b01e09e7b6a29b91148ca3f68c1551fb37903a
alytantawyy <alytantawy@usf.edu>
2026-06-12 13:42:55 -0700

    GH-50148: [C++] Add Content-Encoding support to S3 filesystem metadata

--- cpp/src/arrow/filesystem/s3fs.cc
+++ cpp/src/arrow/filesystem/s3fs.cc
@@ -1301,6 +1301,7 @@ std::shared_ptr<const KeyValueMetadata> GetObjectMetadata(const ObjectResult& re
 
   md->Append("Content-Length", ToChars(result.GetContentLength()));
   push("Cache-Control", result.GetCacheControl());
+  push("Content-Encoding", result.GetContentEncoding());
   push("Content-Type", result.GetContentType());
   push("Content-Language", result.GetContentLanguage());
   push("ETag", result.GetETag());
@@ -1319,6 +1320,7 @@ struct ObjectMetadataSetter {
   static std::unordered_map<std::string, Setter> GetSetters() {
     return {{"ACL", CannedACLSetter()},
             {"Cache-Control", StringSetter(&ObjectRequest::SetCacheControl)},
+            {"Content-Encoding", ContentEncodingSetter()},
             {"Content-Type", ContentTypeSetter()},
             {"Content-Language", StringSetter(&ObjectRequest::SetContentLanguage)},
             {"Expires", DateTimeSetter(&ObjectRequest::SetExpires)}};
@@ -1355,6 +1357,13 @@ struct ObjectMetadataSetter {
   static Setter ContentTypeSetter() {
     return [](const std::string& str, ObjectRequest* req) {
       req->SetContentType(str);
+      return Status::OK();
+    };
+  }
+
+  static Setter ContentEncodingSetter() {
+    return [](const std::string& str, ObjectRequest* req) {
+      req->SetContentEncoding(str);
       return Status::OK();
     };
   }
