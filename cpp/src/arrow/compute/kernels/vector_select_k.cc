df573261ff60e6488af75c98e4507c6a30ca5ef3
Alexander Taepper <alexander.taepper@gmail.com>
2026-06-12 17:04:11 +0200

    update select_k doc

--- cpp/src/arrow/compute/kernels/vector_select_k.cc
+++ cpp/src/arrow/compute/kernels/vector_select_k.cc
@@ -47,9 +47,8 @@ const FunctionDoc select_k_unstable_doc(
     ("This function selects an array of indices of the first `k` ordered elements\n"
      "from the `input` array, record batch or table specified in the column keys\n"
      "(`options.sort_keys`). Output is not guaranteed to be stable.\n"
-     "Null values are considered greater than any other value and are\n"
-     "therefore ordered at the end. For floating-point types, NaNs are considered\n"
-     "greater than any other non-null value, but smaller than null values."),
+     "Null placement can be specified per sort key.\n"
+     "NaNs are ordered between Nulls and regular values."),
     {"input"}, "SelectKOptions", /*options_required=*/true);
 
 template <SortOrder order>
