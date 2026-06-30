c3c383f20637024d3cd5cb4fdc3431ff647ad998
Rok Mihevc <rok@mihevc.org>
2026-06-30 14:23:33 +0200

    Apply suggestion from @rok

--- cpp/src/arrow/ipc/reader.cc
+++ cpp/src/arrow/ipc/reader.cc
@@ -2294,10 +2294,9 @@ Result<std::shared_ptr<SparseIndex>> ReadSparseCOOIndex(
                                      /*allow_short_read=*/false));
   std::vector<int64_t> indices_shape({non_zero_length, ndim});
   int64_t indices_minimum_bytes;
-  if (MultiplyWithOverflow(non_zero_length, ndim,
-                                            &indices_minimum_bytes) ||
+  if (MultiplyWithOverflow(non_zero_length, ndim, &indices_minimum_bytes) ||
       MultiplyWithOverflow(indices_minimum_bytes, indices_elsize,
-                                            &indices_minimum_bytes) ||
+                           &indices_minimum_bytes) ||
       indices_minimum_bytes > indices_buffer->length()) {
     return Status::Invalid("shape is inconsistent to the size of indices buffer");
   }
