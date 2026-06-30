8484106194a8be43b82c238f0cff3817d5dbed19
Sayed Kaif <metsw24@gmail.com>
2026-06-30 12:38:16 +0530

    Guard read range end and check offset bound first

--- cpp/src/parquet/page_index.cc
+++ cpp/src/parquet/page_index.cc
@@ -336,10 +336,13 @@ class RowGroupPageIndexReaderImpl : public RowGroupPageIndexReader {
 
     /// Page index location must be within the range of the read range.
     int64_t index_end = 0;
-    if (::arrow::internal::AddWithOverflow(index_location.offset, index_location.length,
+    int64_t range_end = 0;
+    if (index_location.offset < index_read_range->offset ||
+        ::arrow::internal::AddWithOverflow(index_location.offset, index_location.length,
                                            &index_end) ||
-        index_location.offset < index_read_range->offset ||
-        index_end > index_read_range->offset + index_read_range->length) {
+        ::arrow::internal::AddWithOverflow(index_read_range->offset,
+                                           index_read_range->length, &range_end) ||
+        index_end > range_end) {
       throw ParquetException("Page index location [offset:", index_location.offset,
                              ",length:", index_location.length,
                              "] is out of range from previous WillNeed request [offset:",
