172c0d4a27bb68a67de4b7ebd8eb378194ce7b1a
Bryce Mecum <petridish@gmail.com>
2026-07-01 19:51:50 -0700

    revert debug messages in arrow-package.R

--- r/R/arrow-package.R
+++ r/R/arrow-package.R
@@ -190,35 +190,18 @@ configure_tzdb <- function() {
     tryCatch(
       {
         tzdb::tzdb_initialize()
-        tz_path <- tzdb::tzdb_path("text")
-        packageStartupMessage("[configure_tzdb] tzdb path: ", tz_path)
-        packageStartupMessage("[configure_tzdb] path exists: ", dir.exists(tz_path))
-        if (dir.exists(tz_path)) {
-          tz_files <- list.files(tz_path, recursive = TRUE)
-          packageStartupMessage(
-            "[configure_tzdb] tzdb contents (",
-            length(tz_files),
-            " files): ",
-            paste(head(tz_files, 10), collapse = ", "),
-            if (length(tz_files) > 10) "..."
-          )
-        }
-        set_timezone_database(tz_path)
-        packageStartupMessage("[configure_tzdb] successfully configured timezone database")
+        set_timezone_database(tzdb::tzdb_path("text"))
       },
       error = function(e) {
         packageStartupMessage(
-          "[configure_tzdb] tzdb package available but failed to initialize: ",
-          conditionMessage(e)
-        )
-        packageStartupMessage(
-          "Timezones will not be available to Arrow compute functions."
+          "The tzdb package was available but failed to initialize: ",
+          e,
         )
       }
     )
   } else {
     packageStartupMessage(
-      "[configure_tzdb] tzdb package is NOT installed. ",
+      "The tzdb package is not installed. ",
       "Timezones will not be available to Arrow compute functions. ",
       "If you get errors when using Arrow on datetimes, try running ",
       "`install.packages('tzdb')` and trying again."
