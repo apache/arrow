7607404909426d9b63805f9bc0ceed9415892c38
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2026-07-07 18:32:12 +0000

    Use more CPython Limited APIs in python/pyarrow

--- python/pyarrow/src/arrow/python/common.h
+++ python/pyarrow/src/arrow/python/common.h
@@ -23,6 +23,7 @@
 #include <utility>
 
 #include "arrow/buffer.h"
+#include "arrow/python/helpers.h"
 #include "arrow/python/pyarrow.h"
 #include "arrow/python/visibility.h"
 #include "arrow/result.h"
@@ -398,8 +399,8 @@ struct PyBytesView {
   // View the given Python object as binary-like, i.e. bytes
   Status ParseBinary(PyObject* obj) {
     if (PyBytes_Check(obj)) {
-      bytes = PyBytes_AS_STRING(obj);
-      size = PyBytes_GET_SIZE(obj);
+      bytes = PyBytes_AsString(obj);
+      size = PyBytes_Size(obj);
       is_utf8 = false;
     } else if (PyByteArray_Check(obj)) {
       bytes = PyByteArray_AS_STRING(obj);
@@ -413,8 +414,8 @@ struct PyBytesView {
       size = buffer->len;
       is_utf8 = false;
     } else {
-      return Status::TypeError("Expected bytes, got a '", Py_TYPE(obj)->tp_name,
-                               "' object");
+      return Status::TypeError("Expected bytes, got a '",
+                               internal::PyObject_StdStringTypeName(obj), "' object");
     }
     return Status::OK();
   }
@@ -425,10 +426,11 @@ struct PyBytesView {
     RETURN_IF_PYERROR();
     if (!PyBytes_Check(ref.obj())) {
       return Status::TypeError("Expected uuid.UUID.bytes to return bytes, got '",
-                               Py_TYPE(ref.obj())->tp_name, "' object");
+                               internal::PyObject_StdStringTypeName(ref.obj()),
+                               "' object");
     }
-    bytes = PyBytes_AS_STRING(ref.obj());
-    size = PyBytes_GET_SIZE(ref.obj());
+    bytes = PyBytes_AsString(ref.obj());
+    size = PyBytes_Size(ref.obj());
     is_utf8 = false;
     return Status::OK();
   }
