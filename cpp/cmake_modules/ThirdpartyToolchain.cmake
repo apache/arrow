00775397a0ccb84106da75287ffa9d05d69de2e2
Antoine Pitrou <antoine@python.org>
2026-07-21 11:03:07 +0200

    GH-50428: [C++] Better mimalloc configuration on macOS (#50549)
    ### Rationale for this change
    
    On macOS, two independently-linked mimalloc v3 instances built with default TLS settings can end up using the same hard-coded TLS slots and crash due to conflicting expectations. See upstream issue at https://github.com/microsoft/mimalloc/issues/1327
    
    This can manifest when PyArrow is loaded side-by-side with another Python extension module that bundles its own instance of mimalloc.
    
    ### What changes are included in this PR?
    
    1. Bump mimalloc to 3.4.1, for the availability of the required CMake option.
    2. Configure macOS to use C thread-local variables for thread-local storage, avoiding conflicting accesses to hard-coded TLS slots on macOS.
    3. Also, unrelatedly, make sure the default malloc is not overriden by our mimalloc build on macOS.
    
    ### Are these changes tested?
    
    By existing CI jobs.
    
    ### Are there any user-facing changes?
    
    No, just a bugfix.
    
    * GitHub Issue: #50428
    
    Lead-authored-by: Antoine Pitrou <antoine@python.org>
    Co-authored-by: Antoine Pitrou <pitrou@free.fr>
    Signed-off-by: Antoine Pitrou <antoine@python.org>

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -2582,13 +2582,25 @@ if(ARROW_MIMALLOC)
       "-DCMAKE_C_FLAGS=${MIMALLOC_C_FLAGS}"
       "-DCMAKE_INSTALL_PREFIX=${MIMALLOC_PREFIX}"
       -DMI_INSTALL_TOPLEVEL=ON
+      # Don't override default malloc
       -DMI_OVERRIDE=OFF
+      -DMI_OSX_INTERPOSE=OFF
+      -DMI_OSX_ZONE=OFF
+      # Allow usage through dlopen (i.e. when libarrow.so itself is dlopen'ed)
       -DMI_LOCAL_DYNAMIC_TLS=ON
       -DMI_BUILD_OBJECT=OFF
       -DMI_BUILD_SHARED=OFF
       -DMI_BUILD_TESTS=OFF
       # GH-47229: Force mimalloc to generate armv8.0 binary
       -DMI_NO_OPT_ARCH=ON)
+
+  if(APPLE)
+    list(APPEND
+         MIMALLOC_CMAKE_ARGS
+         # GH-50428: Make sure several mimalloc instances can cohabit in the same process
+         # (also https://github.com/microsoft/mimalloc/issues/1327#issuecomment-4964140817)
+         -DMI_TLS_MODEL_LOCAL=ON)
+  endif()
 
   externalproject_add(mimalloc_ep
                       ${EP_COMMON_OPTIONS}
