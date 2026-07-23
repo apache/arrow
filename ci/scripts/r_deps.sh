fab7f572cc5f39f81536ab7210cc881cbb6afbe4
Hiroyuki Sato <hiroysato@gmail.com>
2026-07-23 13:46:47 +0900

    GH-50609: [CI][Dev] Fix shellcheck errors in the ci/scripts/r_deps.sh

--- ci/scripts/r_deps.sh
+++ ci/scripts/r_deps.sh
@@ -43,7 +43,7 @@ fi
 # install.packages() emits warnings if packages fail to install,
 # but we want to error/fail the build.
 # options(warn=2) turns warnings into errors
-"${R_BIN}" -e "options(warn=2); install.packages('remotes'); remotes::install_cran(c('glue', 'rcmdcheck', 'sys')); remotes::install_deps(INSTALL_opts = '\"${INSTALL_ARGS}\"')"
+"${R_BIN}" -e "options(warn=2); install.packages('remotes'); remotes::install_cran(c('glue', 'rcmdcheck', 'sys')); remotes::install_deps(INSTALL_opts = '${INSTALL_ARGS}')"
 
 # Install DuckDB from github when requested
 if [ "${R_DUCKDB_DEV}" == "true" ]; then
@@ -52,6 +52,6 @@ fi
 
 # Separately install the optional/test dependencies but don't error on them,
 # they're not available everywhere and that's ok
-"${R_BIN}" -e "remotes::install_deps(dependencies = TRUE, INSTALL_opts = '\"${INSTALL_ARGS}\"')"
+"${R_BIN}" -e "remotes::install_deps(dependencies = TRUE, INSTALL_opts = '${INSTALL_ARGS}')"
 
 popd
