00d156bb9d6fd7d372cd1310f32d6dd3b077836d
Aaditya Srinivasan <aadityasri03@gmail.com>
2026-07-31 19:13:05 +0530

    Add simdjson value visitor helper

--- cpp/src/arrow/json/json_writer_internal.cc
+++ cpp/src/arrow/json/json_writer_internal.cc
@@ -100,121 +100,98 @@ void JsonWriter::Double(double value) {
 }
 
 Status JsonWriter::WriteValue(sj::value value) {
-  ARROW_ASSIGN_OR_RAISE(auto type, internal::GetSimdjsonResult(
-                                       value.type(), "Failed to determine JSON type: "));
+  return internal::VisitJsonValue(
+      value,
 
-  switch (type) {
-    case sj::json_type::object: {
-      StartObject();
+      [&](sj::object object) -> Status {
+        StartObject();
 
-      ARROW_ASSIGN_OR_RAISE(
-          auto object,
-          internal::GetSimdjsonResult(value.get_object(), "Failed to get JSON object: "));
+        for (auto field : object) {
+          ARROW_ASSIGN_OR_RAISE(
+              auto key, internal::GetSimdjsonResult(field.unescaped_key(),
+                                                    "Failed to get object key: "));
 
-      for (auto field : object) {
-        ARROW_ASSIGN_OR_RAISE(auto key,
-                              internal::GetSimdjsonResult(field.unescaped_key(),
-                                                          "Failed to get object key: "));
+          Key(key);
 
-        Key(key);
+          ARROW_ASSIGN_OR_RAISE(
+              auto field_value,
+              internal::GetSimdjsonResult(field.value(), "Failed to get object value: "));
+
+          RETURN_NOT_OK(WriteValue(field_value));
+        }
+
+        EndObject();
+        return Status::OK();
+      },
+
+      [&](sj::array array) -> Status {
+        StartArray();
+
+        for (auto element : array) {
+          ARROW_ASSIGN_OR_RAISE(
+              auto element_value,
+              internal::GetSimdjsonResult(element, "Failed to iterate JSON array: "));
+
+          RETURN_NOT_OK(WriteValue(element_value));
+        }
+
+        EndArray();
+        return Status::OK();
+      },
+
+      [&](std::string_view string_value) -> Status {
+        String(string_value);
+        return Status::OK();
+      },
+
+      [&](bool bool_value) -> Status {
+        Bool(bool_value);
+        return Status::OK();
+      },
+
+      [&]() -> Status {
+        Null();
+        return Status::OK();
+      },
+
+      [&](sj::value number_value) -> Status {
+        ARROW_ASSIGN_OR_RAISE(
+            auto number_type,
+            internal::GetSimdjsonResult(number_value.get_number_type(),
+                                        "Failed to determine JSON number type: "));
+
+        if (number_type == sj::number_type::big_integer) {
+          ARROW_ASSIGN_OR_RAISE(auto raw_json, internal::GetSimdjsonResult(
+                                                   simdjson::to_json_string(number_value),
+                                                   "Failed to get raw JSON: "));
+          RawValue(raw_json);
+          return Status::OK();
+        }
 
         ARROW_ASSIGN_OR_RAISE(
-            auto field_value,
-            internal::GetSimdjsonResult(field.value(), "Failed to get object value: "));
+            auto number, internal::GetSimdjsonResult(number_value.get_number(),
+                                                     "Failed to convert JSON number: "));
 
-        RETURN_NOT_OK(WriteValue(field_value));
-      }
+        switch (number_type) {
+          case sj::number_type::signed_integer:
+            Int64(number.get_int64());
+            break;
 
-      EndObject();
-      break;
-    }
+          case sj::number_type::unsigned_integer:
+            Uint64(number.get_uint64());
+            break;
 
-    case sj::json_type::array: {
-      StartArray();
+          case sj::number_type::floating_point_number:
+            Double(number.get_double());
+            break;
 
-      ARROW_ASSIGN_OR_RAISE(
-          auto array,
-          internal::GetSimdjsonResult(value.get_array(), "Failed to get JSON array: "));
+          case sj::number_type::big_integer:
+            // Big integers are handled above.
+            break;
+        }
 
-      for (auto element : array) {
-        ARROW_ASSIGN_OR_RAISE(
-            auto element_value,
-            internal::GetSimdjsonResult(element, "Failed to iterate JSON array: "));
-
-        RETURN_NOT_OK(WriteValue(element_value));
-      }
-
-      EndArray();
-      break;
-    }
-
-    case sj::json_type::string: {
-      ARROW_ASSIGN_OR_RAISE(
-          auto string_value,
-          internal::GetSimdjsonResult(value.get_string(), "Failed to get JSON string: "));
-
-      String(string_value);
-      break;
-    }
-
-    case sj::json_type::boolean: {
-      ARROW_ASSIGN_OR_RAISE(
-          auto bool_value,
-          internal::GetSimdjsonResult(value.get_bool(), "Failed to get JSON boolean: "));
-
-      Bool(bool_value);
-      break;
-    }
-
-    case sj::json_type::null: {
-      Null();
-      break;
-    }
-
-    case sj::json_type::number: {
-      ARROW_ASSIGN_OR_RAISE(
-          auto number_type,
-          internal::GetSimdjsonResult(value.get_number_type(),
-                                      "Failed to determine JSON number type: "));
-
-      if (number_type == sj::number_type::big_integer) {
-        ARROW_ASSIGN_OR_RAISE(auto raw_json,
-                              internal::GetSimdjsonResult(simdjson::to_json_string(value),
-                                                          "Failed to get raw JSON: "));
-        RawValue(raw_json);
-        break;
-      }
-
-      ARROW_ASSIGN_OR_RAISE(
-          auto number, internal::GetSimdjsonResult(value.get_number(),
-                                                   "Failed to convert JSON number: "));
-      switch (number_type) {
-        case sj::number_type::signed_integer:
-          Int64(number.get_int64());
-          break;
-
-        case sj::number_type::unsigned_integer:
-          Uint64(number.get_uint64());
-          break;
-
-        case sj::number_type::floating_point_number:
-          Double(number.get_double());
-          break;
-
-        case sj::number_type::big_integer:
-          // Big integers are handled before calling get_number()
-          break;
-      }
-
-      break;
-    }
-
-    case sj::json_type::unknown: {
-      return Status::Invalid("Unknown JSON type");
-    }
-  }
-
-  return Status::OK();
+        return Status::OK();
+      });
 }
 
 void JsonWriter::Null() {
--- cpp/src/arrow/util/simdjson_internal.h
+++ cpp/src/arrow/util/simdjson_internal.h
@@ -36,5 +36,54 @@ Result<T> GetSimdjsonResult(simdjson::simdjson_result<T> result, std::string_vie
   return value;
 }
 
+template <typename ObjectFn, typename ArrayFn, typename StringFn, typename BoolFn,
+          typename NullFn, typename NumberFn>
+Status VisitJsonValue(simdjson::ondemand::value value, ObjectFn&& object_fn,
+                      ArrayFn&& array_fn, StringFn&& string_fn, BoolFn&& bool_fn,
+                      NullFn&& null_fn, NumberFn&& number_fn) {
+  ARROW_ASSIGN_OR_RAISE(
+      auto type, GetSimdjsonResult(value.type(), "Failed to determine JSON type: "));
+
+  switch (type) {
+    case simdjson::ondemand::json_type::object: {
+      ARROW_ASSIGN_OR_RAISE(
+          auto object,
+          GetSimdjsonResult(value.get_object(), "Failed to get JSON object: "));
+      return object_fn(object);
+    }
+
+    case simdjson::ondemand::json_type::array: {
+      ARROW_ASSIGN_OR_RAISE(
+          auto array, GetSimdjsonResult(value.get_array(), "Failed to get JSON array: "));
+      return array_fn(array);
+    }
+
+    case simdjson::ondemand::json_type::string: {
+      ARROW_ASSIGN_OR_RAISE(
+          auto string,
+          GetSimdjsonResult(value.get_string(), "Failed to get JSON string: "));
+      return string_fn(string);
+    }
+
+    case simdjson::ondemand::json_type::boolean: {
+      ARROW_ASSIGN_OR_RAISE(
+          auto boolean,
+          GetSimdjsonResult(value.get_bool(), "Failed to get JSON boolean: "));
+      return bool_fn(boolean);
+    }
+
+    case simdjson::ondemand::json_type::null:
+      return null_fn();
+
+    case simdjson::ondemand::json_type::number:
+      return number_fn(value);
+
+    case simdjson::ondemand::json_type::unknown:
+      return Status::Invalid("Unknown JSON type");
+  }
+
+  return Status::Invalid("Unreachable");
+}
+
 }  // namespace internal
 }  // namespace arrow
