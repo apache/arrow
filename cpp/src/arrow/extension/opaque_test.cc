329cb7d8519fc019ff8cb8941b6a33fce0e54490
Aaditya Srinivasan <aadityasri03@gmail.com>
2026-08-18 23:33:48 +0530

    Replace RapidJSON with simdjson in OpaqueType

--- cpp/src/arrow/extension/opaque_test.cc
+++ cpp/src/arrow/extension/opaque_test.cc
@@ -127,11 +127,12 @@ TEST(OpaqueType, Deserialize) {
 
   auto type = internal::checked_pointer_cast<extension::OpaqueType>(
       extension::opaque(null(), "type", "vendor"));
-  EXPECT_RAISES_WITH_MESSAGE_THAT(Invalid, testing::HasSubstr("The document is empty"),
-                                  type->Deserialize(null(), R"()"));
-  EXPECT_RAISES_WITH_MESSAGE_THAT(Invalid,
-                                  testing::HasSubstr("Missing a name for object member"),
-                                  type->Deserialize(null(), R"({)"));
+  EXPECT_RAISES_WITH_MESSAGE_THAT(
+      Invalid, testing::HasSubstr("Invalid serialized JSON data for OpaqueType"),
+      type->Deserialize(null(), R"()"));
+  EXPECT_RAISES_WITH_MESSAGE_THAT(
+      Invalid, testing::HasSubstr("Invalid serialized JSON data for OpaqueType"),
+      type->Deserialize(null(), R"({)"));
   EXPECT_RAISES_WITH_MESSAGE_THAT(Invalid, testing::HasSubstr("not an object"),
                                   type->Deserialize(null(), R"([])"));
   EXPECT_RAISES_WITH_MESSAGE_THAT(Invalid, testing::HasSubstr("missing type_name"),
