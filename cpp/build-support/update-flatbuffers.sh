4722fb991bde7af77fd59bb5729ca550ab357963
Hiroyuki Sato <hiroysato@gmail.com>
2026-08-20 13:56:24 +0900

    GH-50917: [C++] Fix shellcheck errors in cpp/build-support/*-flatbuffers.sh

--- cpp/build-support/update-flatbuffers.sh
+++ cpp/build-support/update-flatbuffers.sh
@@ -24,13 +24,18 @@ set -euo pipefail
 
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 SOURCE_DIR="$CWD/../src"
-PYTHON_SOURCE_DIR="$CWD/../../python"
+#PYTHON_SOURCE_DIR="$CWD/../../python" # Unused ShellCheck (SC2034)
 FORMAT_DIR="$CWD/../../format"
-TOP="$FORMAT_DIR/.."
+#TOP="$FORMAT_DIR/.." # Unused ShellCheck (SC2034)
 FLATC="flatc --cpp --cpp-std c++11 --scoped-enums"
 
 OUT_DIR="$SOURCE_DIR/generated"
-FILES=($(find $FORMAT_DIR -name '*.fbs'))
+# Avoid word splitting (SC2207) while maintaining Bash 3 compatibility.
+# See: https://www.shellcheck.net/wiki/SC2207
+FILES=()
+while IFS= read -r file; do
+  FILES+=("$file")
+done < <(find "$FORMAT_DIR" -name '*.fbs')
 FILES+=("$SOURCE_DIR/arrow/ipc/feather.fbs")
 
 $FLATC -o "$OUT_DIR" "${FILES[@]}"
