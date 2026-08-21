ba5ca205d1617878cc6cca1350e406f51fc6654e
paultiq <104510378+paultiq@users.noreply.github.com>
2026-08-21 08:18:33 -0400

    GH-48172: [Python] Add cp315 to build (#48191)
    ### Rationale for this change
    Add [Python 3.15](https://docs.python.org/3.15/whatsnew/3.15.html) to the pyarrow nightly builds, so that issues can be detected early in the development cycles of both cpython & pyarrow. 
    
    Note: 
    - ~~CFFI and~~ Pandas doesn't have nightly 3.15 wheels, and are accordingly excluded. 
    - Numpy shipped 3.15rc1 in 2.5.2 ~~doesn't ship a 3.15 wheel, but is needed to build. It is building from source.~~
    - [3.15 Schedule](https://peps.python.org/pep-0790/) This PR targets 3.15.0rc1. rc2 is expected 2026-09-01 and final 2026-10-01. 
    
    ### What changes are included in this PR?
    - Adding cp315/cp315t to the build matrix
    - Excluding ~~cffi/~~ pandas for 3.15, as the wheels aren't yet available.
    
    ### 2026-08-13 edit
    - Bump libcst to 1.9.0
    - Update manylinux image to 2026.08.08-1 (for 3.15rc1)
    - Add -rc to PYTHON_IMAGE_TAG
    - Disable pandas tests if Pandas isn't installed: 
    
    ### Are these changes tested?
    - Yes, tested on a local Linux build using the `ninja-release-python` preset (not maximal)
    
    ### Are there any user-facing changes?
    No functional changes, other than allowing the user to use Python 3.15. 
    * GitHub Issue: #48172
    
    ### Progress Notes (2026-08-15)
    Each platform uses different sources for Python builds, each with different release schedules and paths:
    
    - [manylinux](https://github.com/pypa/manylinux) is prebuilt with latest Python builds in `/opt/python`, so using the latest manylinux build is sufficient. 3.15 wasn't available until beta 1. 
    - musllinux: Uses Alpine system python built from https://github.com/docker-library/python/pull/1123. Might need to bump ALPINE_LINUX version in [arrow/dev/tasks/python-wheels/github.linux.yml](https://github.com/apache/arrow/blob/e611f48081ec927e18e5bcb72fbd071c15c76b08/dev/tasks/python-wheels/github.linux.yml#L35). 
    - musllinux free-threaded: uses [astral's builds](https://github.com/astral-sh/python-build-standalone). Edit `python-free-threaded-wheel-musllinux-test-imports.dockerfile` + `-test-unittests.dockerfile`: set the python_patch_version, and set build_date in `compose.yaml`. 
    - macos: Downloads official build from python.org, see `ci/scripts/install_python.sh`. For early builds, needed to disable `--only-binary` since at least one dependency needed to be built (no 3.15 wheels available yet)... only macos uses --only-binary
    - Windows: installs builds using `choco`, see `python-wheel-windows-test-vs2022.dockerfile` and `python-free-threaded-wheel-windows-test-vs2022.dockerfile`
    
    For 3.15.0rc1 builds, some care was needed because certain paths needed 3.15.0 and others needed the full 3.15.0rc1. 
    
    Lead-authored-by: Paul Timmins <paul@iqmo.com>
    Co-authored-by: paul@iqmo.com <paul@iqmo.com>
    Signed-off-by: Raúl Cumplido <raulcumplido@gmail.com>

--- ci/scripts/python_wheel_windows_test.bat
+++ ci/scripts/python_wheel_windows_test.bat
@@ -26,7 +26,9 @@ set PYARROW_TEST_GANDIVA=OFF
 set PYARROW_TEST_GCS=ON
 set PYARROW_TEST_HDFS=ON
 set PYARROW_TEST_ORC=ON
-set PYARROW_TEST_PANDAS=ON
+@REM TODO: To be removed once pandas provides wheels for Python 3.15
+%PYTHON_CMD% -c "import sys; sys.exit(0 if sys.version_info < (3, 15) else 1)"
+if errorlevel 1 (set PYARROW_TEST_PANDAS=OFF) else (set PYARROW_TEST_PANDAS=ON)
 set PYARROW_TEST_PARQUET=ON
 set PYARROW_TEST_PARQUET_ENCRYPTION=ON
 set PYARROW_TEST_SUBSTRAIT=ON
