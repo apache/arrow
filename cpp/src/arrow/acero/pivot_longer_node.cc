203440693f02df7f325aa2fc71d5cc13a5ecf680
Rafał Hibner <rafal.hibner@secom.com.pl>
2026-08-26 07:30:30 +0000

    Reformat sources

--- cpp/src/arrow/acero/pivot_longer_node.cc
+++ cpp/src/arrow/acero/pivot_longer_node.cc
@@ -109,8 +109,7 @@ class PivotLongerNode : public ExecNode, public TracedNode {
       for (std::size_t i = 0; i < row_template.feature_values.size(); i++) {
         if (!row_template.feature_values[i]) {
           return Status::Invalid("Feature value at column ",
-                                 options.feature_field_names[i],
-                                 " must not be null");
+                                 options.feature_field_names[i], " must not be null");
         }
         if (feature_types[i]) {
           if (!feature_types[i]->Equals(row_template.feature_values[i]->type)) {
