27dd63e290543b1bdc9c3e1affb9c2495bac745a
Hiroyuki Sato <hiroysato@gmail.com>
2026-08-27 22:00:34 +0900

    test

--- ci/scripts/c_glib_test.sh
+++ ci/scripts/c_glib_test.sh
@@ -42,7 +42,7 @@ if [ -n "${RUNNER_DEBUG}" ]; then
 fi
 #ruby test/run-test.rb "${run_test_args[@]}"
 pushd "${build_dir}"
-DEBUGGER="lldb" ../c_glib/test/run-test.sh -t TestFlightSQLClient
+DEBUGGER="lldb" ${source_dir}/test/run-test.sh -t TestFlightSQLClient
 popd
 
 if [[ "$(uname -s)" == "Linux" ]]; then
