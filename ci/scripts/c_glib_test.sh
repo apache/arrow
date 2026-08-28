cb14f7c1393d56a7d11306bf25bcfdb464e47bde
Hiroyuki Sato <hiroysato@gmail.com>
2026-08-28 17:04:40 +0900

    rerun

--- ci/scripts/c_glib_test.sh
+++ ci/scripts/c_glib_test.sh
@@ -47,6 +47,7 @@ DEBUGGER="lldb" ${source_dir}/test/run-test.sh -t TestFlightSQLClient || true
 done
 popd
 
+
 if [[ "$(uname -s)" == "Linux" ]]; then
   # TODO(kszucs): on osx it fails to load 'lgi.corelgilua51' despite that lgi
   # was installed by luarocks
