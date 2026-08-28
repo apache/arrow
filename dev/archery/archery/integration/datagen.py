3c71b9345a83f2f7d4430f9c4b8182e86df20b8f
Alb3e3 <74142887+Alb3e3@users.noreply.github.com>
2026-08-28 23:22:27 +0400

    GH-50993: [CI][Integration] Add extension-wrapped union to integration data
    Add a new integration datagen case (extension_union) with sparse- and
    dense-union columns wrapped in an extension type, so the cross-language
    integration tests exercise the extension/union interaction. This mirrors
    the C++ IPC coverage added in GH-50623 (#50927).

--- dev/archery/archery/integration/datagen.py
+++ dev/archery/archery/integration/datagen.py
@@ -1938,6 +1938,29 @@ def generate_extension_case():
                           dictionaries=[dict0])
 
 
+def generate_extension_wrapped_union_case():
+    # Unions wrapped in an extension type, exercising the extension/union
+    # interaction across implementations (see the C++ fix in GH-50623).
+    sparse_union_type = ExtensionType(
+        'sparse-union-extension', '',
+        SparseUnionField('', [get_field('f1', 'int32'),
+                              get_field('f2', 'utf8')],
+                         type_ids=[5, 7]))
+    dense_union_type = ExtensionType(
+        'dense-union-extension', '',
+        DenseUnionField('', [get_field('f1', 'int16'),
+                             get_field('f2', 'binary')],
+                        type_ids=[10, 20]))
+
+    fields = [
+        ExtensionField('sparse_union_ext', sparse_union_type),
+        ExtensionField('dense_union_ext', dense_union_type),
+    ]
+
+    batch_sizes = [0, 7]
+    return _generate_file("extension_union", fields, batch_sizes)
+
+
 def get_generated_json_files(tempdir=None):
     tempdir = tempdir or tempfile.mkdtemp(prefix='arrow-integration-')
 
@@ -2047,6 +2070,8 @@ def get_generated_json_files(tempdir=None):
         # TODO(https://github.com/apache/arrow/issues/38045)
         .skip_format(SKIP_FLIGHT, '.NET')
         .skip_tester('Ruby'),
+
+        generate_extension_wrapped_union_case(),
     ]
 
     generated_paths = []
