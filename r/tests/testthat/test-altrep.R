5060cc5ceac0e435dcd55cd5cb45acec42038c3f
Nic Crane <thisisnic@gmail.com>
2026-09-10 17:42:24 +0100

    Apply suggestion from @thisisnic

--- r/tests/testthat/test-altrep.R
+++ r/tests/testthat/test-altrep.R
@@ -662,7 +662,6 @@ test_that("strings returned by ALTREP Elt() survive garbage collection", {
   # https://gist.github.com/traversc/a5204821451198d457edc38cceda9d90
   withr::local_options(list(arrow.use_altrep = TRUE))
   skip_on_cran()
-  skip_on_cran()
 
   # Build the string inside Arrow so that this R session never holds a CHARSXP
   # with these contents: the only one is the one Elt() creates. It is large so
