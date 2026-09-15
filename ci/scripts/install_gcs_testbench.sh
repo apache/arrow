2af9626c066c8de25b9e2ed5dea46e6c35118211
Tadeja Kadunc <tadeja.kadunc@gmail.com>
2026-09-15 15:32:33 +0200

    ignore-installed due to packaging no RECORD

--- ci/scripts/install_gcs_testbench.sh
+++ ci/scripts/install_gcs_testbench.sh
@@ -45,7 +45,7 @@ fi
 : "${PIPX_PYTHON:=${PIPX_BASE_PYTHON:-$(which python3)}}"
 
 export PIP_BREAK_SYSTEM_PACKAGES=1
-${PIPX_BASE_PYTHON} -m pip install -U pipx
+${PIPX_BASE_PYTHON} -m pip install -U --ignore-installed pipx
 
 pipx_flags=(--verbose --python "${PIPX_PYTHON}")
 if [[ $(id -un) == "root" ]]; then
