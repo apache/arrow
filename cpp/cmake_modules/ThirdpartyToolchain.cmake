275b44b0cdcd2c84bdf5f92aa3a5f3dd76863a91
Raúl Cumplido <raulcumplido@gmail.com>
2026-09-17 11:48:51 +0200

    GH-50194: [C++] Move S3 and AWS-SDK to its own libarrow_s3.so (#50195)
    ### Rationale for this change
    
    Trying to reduce the size of `libarrow.so` and remove AWS SDK on some builds. Allow for users to plug and play based on requirements and divide our functionality into cleaner modules.
    
    ### What changes are included in this PR?
    
    Unconditionally build S3 and the AWS SDK into a different module `libarrow_s3.so` outside of `libarrow.so`.
    Update bindings to link against the new `libarrow_s3.so` library.
    Update the Linux Package jobs to have the new module into a different package.
    
    ### Are these changes tested?
    
    Yes via CI
    
    ### Are there any user-facing changes?
    
    Yes, users will need to either link against `libarrow_s3.so` or register using `LoadFileSystemFactories`
    * GitHub Issue: #50194
    
    Authored-by: Raúl Cumplido <raulcumplido@gmail.com>
    Signed-off-by: Raúl Cumplido <raulcumplido@gmail.com>

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -1409,10 +1409,13 @@ endif()
 # ----------------------------------------------------------------------
 # cURL
 
-macro(find_curl)
+macro(find_curl ARROW_CURL_PACKAGE_PREFIX)
   if(NOT TARGET CURL::libcurl)
     find_package(CURL REQUIRED)
-    list(APPEND ARROW_SYSTEM_DEPENDENCIES CURL)
+  endif()
+  # CURL might be needed for Arrow (GCS, OpenTelemetry) or ArrowS3
+  if(NOT "CURL" IN_LIST ${ARROW_CURL_PACKAGE_PREFIX}_SYSTEM_DEPENDENCIES)
+    list(APPEND ${ARROW_CURL_PACKAGE_PREFIX}_SYSTEM_DEPENDENCIES CURL)
   endif()
 endmacro()
 
@@ -3656,7 +3659,7 @@ if(ARROW_WITH_OPENTELEMETRY)
 
   # cURL is required whether we build from source or use an existing installation
   # (OTel's cmake files do not call find_curl for you)
-  find_curl()
+  find_curl(ARROW)
   resolve_dependency(opentelemetry-cpp
                      COMPONENTS
                      exporters_ostream
@@ -3795,7 +3798,7 @@ if(ARROW_WITH_GOOGLE_CLOUD_CPP)
 
   # curl is required on all platforms. We always use system curl to
   # avoid conflict.
-  find_curl()
+  find_curl(ARROW)
   resolve_dependency(google_cloud_cpp_storage PC_PACKAGE_NAMES google_cloud_cpp_storage)
   get_target_property(google_cloud_cpp_storage_INCLUDE_DIR google-cloud-cpp::storage
                       INTERFACE_INCLUDE_DIRECTORIES)
@@ -4244,11 +4247,14 @@ endfunction()
 
 if(ARROW_S3)
   if(NOT WIN32)
-    # This is for adding system curl dependency.
-    find_curl()
+    find_curl(ARROW_S3)
   endif()
   # Keep this in sync with s3fs.cc
   resolve_dependency(AWSSDK
+                     ARROW_CMAKE_PACKAGE_NAME
+                     ArrowS3
+                     ARROW_PC_PACKAGE_NAME
+                     arrow-s3
                      HAVE_ALT
                      TRUE
                      REQUIRED_VERSION
@@ -4260,15 +4266,15 @@ if(ARROW_S3)
   if(ARROW_BUILD_STATIC)
     if(${AWSSDK_SOURCE} STREQUAL "SYSTEM")
       foreach(AWSSDK_LINK_LIBRARY ${AWSSDK_LINK_LIBRARIES})
-        string(APPEND ARROW_PC_LIBS_PRIVATE " $<TARGET_FILE:${AWSSDK_LINK_LIBRARY}>")
+        string(APPEND ARROW_S3_PC_LIBS_PRIVATE " $<TARGET_FILE:${AWSSDK_LINK_LIBRARY}>")
       endforeach()
     else()
       if(UNIX)
-        string(APPEND ARROW_PC_REQUIRES_PRIVATE " libcurl")
+        string(APPEND ARROW_S3_PC_REQUIRES_PRIVATE " libcurl")
       endif()
-      string(APPEND ARROW_PC_REQUIRES_PRIVATE " openssl")
+      string(APPEND ARROW_S3_PC_REQUIRES_PRIVATE " openssl")
       if(APPLE)
-        string(APPEND ARROW_PC_LIBS_PRIVATE " -framework Security")
+        string(APPEND ARROW_S3_PC_LIBS_PRIVATE " -framework Security")
       endif()
     endif()
   endif()
