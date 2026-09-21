1ed3c25a8998c8c5a76e34b0827e718e9b440bd6
AlenkaF <frim.alenka@gmail.com>
2026-09-21 10:38:41 +0200

    Fix for deprecated generic units

--- python/pyarrow/tests/test_pandas.py
+++ python/pyarrow/tests/test_pandas.py
@@ -5006,7 +5006,7 @@ def test_does_not_mutate_timedelta_dtype():
 
     assert np.dtype(np.timedelta64) == expected
 
-    df = pd.DataFrame({"a": [np.timedelta64()]})
+    df = pd.DataFrame({"a": [np.timedelta64(0, "s")]})
     t = pa.Table.from_pandas(df)
     t.to_pandas()
 
